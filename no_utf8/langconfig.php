1c21896def60f182890b4a116be646bc7f7919db
koen roggemans <koenr>
2006-02-08 22:51:47 +0000

    Now langconfig should be right

--- no_utf8/langconfig.php
+++ no_utf8/langconfig.php
@@ -1,6 +1,21 @@
 <?php
+$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Æ,Ø,Å';
+$string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
+$string['firstdayofweek'] = ' 0';
 $string['locale'] = 'no_NO.UTF-8';
+$string['oldcharset'] = 'ISO-8859-1';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Norsk';
+$string['thislanguage'] = 'Norsk - bokmål';
+$string['strftimedate'] = '%%d %%B %%Y';
+$string['strftimedateshort'] = '%%d %%B';
+$string['strftimedatetime'] = '%%d %%B %%Y, %%H:%%M';
+$string['strftimedaydate'] = '%%A, %%d %%B %%Y';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%H:%%M';
+$string['strftimedayshort'] = ' %%A, %%d %%B';
+$string['strftimedaytime'] = '%%a, %%H:%%M';
+$string['strftimemonthyear'] = ' %%B %%Y';
+$string['strftimerecent'] = '%%d %%b, %%H:%%M';
+$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%H:%%M ';
+$string['strftimetime'] = '%%H:%%M';
 ?>
