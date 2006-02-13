6917ba38917af3d2cb3eb7795bc1b71550d030f0
Mitsuhiro Yoshida <mits>
2006-02-13 14:12:58 +0000

    polished. modified description on enrol_flatfile.php. translated two new help files for database.

--- ja_utf8/enrol_flatfile.php
+++ ja_utf8/enrol_flatfile.php
@@ -1,8 +1,19 @@
-<?php // $Id$ 
-      // enrol_flatfile.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?PHP // $Id$ 
+      // enrol_flatfile.php - created with Moodle 1.6 development (2005101200)
 
 
-$string['description'] = 'この方法は、あなたが指定した場所にある特別にフォーマットされたテキストファイルを繰り返しチェックします。ファイルのフォーマットは下記のようになります:
+$string['description'] = 'この方法では、あなたが指定した場所に置いた特別にフォーマットされたテキストファイルを繰り返しチェックします。ファイルはカンマ区切りのファイルで、1行あたり4個または6個のフィールドを持ちます:
+* 処理, 任務, IDナンバー ( ユーザ ), IDナンバー　(　コース ) [, 開始日時, 終了日時]
+<pre>
+詳細:
+* 処理 = add | del
+* 任務 = student | teacher | teacheredit
+* IDナンバー ( ユーザ ) = user tableのidnumber ( idではなく ) 
+* IDナンバー　(　コース ) = course tableのidnumber ( idではなく )
+* 開始日時 = 開始日時 ( UNIXエポックからの秒数 ) - 任意
+* 終了日時 = 終了日時 ( UNIXエポックからの秒数 ) - 任意
+</pre>
+ファイルのフォーマットは下記のようになります:
 <pre>
 add, student, 5, CF101
 add, teacher, 6, CF101
