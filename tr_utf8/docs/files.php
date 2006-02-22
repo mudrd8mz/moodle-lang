65fb364c3aabb9cad8ed4f2241e03a97433cbdb4
Ethem Evlice <ethem>
2006-02-22 08:10:34 +0000

    New files from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant.

--- tr_utf8/docs/files.php
+++ tr_utf8/docs/files.php
@@ -10,11 +10,11 @@ $string['licence.html'] = "Lisans";
 $string['features.html'] = "Özellikler";
 $string['release.html'] = "Sürüm Notları";
 $string['future.html'] = "Yapılacaklar";
-$string['credits.html'] = "Krediler";
+$string['credits.html'] = "Ekip";
 
 $string['-installation'] = "Yönetim";
 $string['install.html'] = "Kurulum";
-$string['faq.html'] = "kurulum SSS";
+$string['faq.html'] = "Kurulum SSS";
 $string['installamp.html'] = "Apache, MySQL, PHP";
 $string['upgrade.html'] = "Güncelleme";
 
@@ -22,11 +22,10 @@ $string['-usage'] = "Moodle Kullanımı";
 $string['teacher.html'] = "Eğitimci El Kitabı";
 $string['other.html'] = "Diğer Belgeler";
 
-$string['-development'] = "Geliştirme";
+$string['-development'] = "Gelişim";
 $string['developer.html'] = "Geliştirici El Kitabı";
 $string['coding.html'] = "Kodlama Rehberi";
 $string['cvs.html'] = "CVS Kullanımı";
 $string['translation.html'] = "Çeviri";
-
 
 ?>
