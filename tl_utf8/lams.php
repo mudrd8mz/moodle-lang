8c789efd49b0709bb020893c968144d3716d067e
rcantada <rcantada>
2006-02-26 02:22:26 +0000

    new string file

---
+++ tl_utf8/lams.php
@@ -0,0 +1,26 @@
+<?PHP // $Id$ 
+      // lams.php - created with Moodle 1.6 development (2005101200)
+
+
+$string['createSequence'] = 'Lumikha ng bagong sunud-sunod';
+$string['editSequence'] = 'Iedit ang piniling sunud-sunod';
+$string['error'] = 'Paumanhin, may hindi malamang error na naganap.';
+$string['introduction'] = 'Panimula';
+$string['lamsoutline'] = 'Balangkas ng LAMS';
+$string['lesson'] = 'sesyon ng pag-aaral';
+$string['openauthor'] = 'Buksan ang LAMS Author';
+$string['openlearner'] = 'Buksan ang LAMS Learner';
+$string['openmonitor'] = 'Buksan ang LAMS Monitor';
+$string['refreshSequenceList'] = 'Sariwain ang listahan ng sunud-sunod';
+$string['selectExistingSequence'] = 'Pumili ng kasalukuyang sunud-sunod o lumikha ng bagong sunud-sunod.';
+$string['sequence'] = 'Pumili ng Sunud-sunod';
+$string['serverid'] = 'Ipasok ang sever ID na natanggap mula sa <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverkey'] = 'Ipasok ang susi ng server na natanggap mula sa <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverurl'] = 'Ipasok ang payak na URL na ginagamit sa pagpasok sa LAMS server. Halimbawa http://localhost:8080/lams';
+$string['useSequence'] = 'Gamitin ang piniling sunud-sunod';
+$string['visibletostudents'] = 'Ipakita ang aktibidad sa mga mag-aaral';
+$string['wikistartederror'] = 'May mga talâ na ang Wiki - hindi mababago.';
+$string['workspace'] = 'Piliin ang Workspace';
+$string['wrongversionrange'] = 'Ang $a ay maling agwat!';
+
+?>
