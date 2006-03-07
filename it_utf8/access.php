9761e1f4cc77dc79f2bcd20a32670b3c9489e871
Roberto Pinna <bobopinna>
2006-03-07 09:47:01 +0000

    First traslation by Bobo.

---
+++ it_utf8/access.php
@@ -0,0 +1,21 @@
+<?PHP // $Id$ 
+      // access.php - created with Moodle 1.6 development (2006022400)
+
+
+$string['access'] = 'Accessibilit&agrave;';
+$string['accesshelp'] = 'Aiuto accessibit&agrave;';
+$string['accesskey'] = 'Tasto, $a';
+$string['accessstatement'] = 'Definizione di accessibilit&agrave;';
+$string['monthnext'] = 'Prossimo mese';
+$string['monthprevious'] = 'Mese precedente';
+$string['showhideblock'] = 'Mostra o nascondi blocco';
+$string['sitemap'] = 'Mappa sito';
+$string['skipblock'] = 'Salta blocco';
+$string['skipnavigation'] = 'Salta navigazione';
+$string['tabledata'] = 'Tabella dati, $a';
+$string['tablelayout'] = 'Tabella formattazione, $a';
+$string['tocontent'] = 'Vai al contenuto principale';
+$string['today'] = 'Oggi';
+$string['tonavigation'] = 'Vai alla navigazione';
+
+?>
---
+++ it_utf8/lams.php
@@ -0,0 +1,30 @@
+<?PHP // $Id$ 
+      // lams.php - created with Moodle 1.6 development (2006022400)
+
+
+$string['createSequence'] = 'Creare una nuova sequenza';
+$string['editSequence'] = 'Modificare la sequenza selezionata';
+$string['error'] = 'Spiacente, &egrave; occorso un errore sconosciuto.';
+$string['introduction'] = 'Introduzione';
+$string['lams'] = '-- LAMS - Sistema di Gestione Attivit&agrave; di Apprendimento';
+$string['lamsoutline'] = 'Schema LAMS';
+$string['lesson'] = 'Sessione di apprendimento';
+$string['modulename'] = 'LAMS';
+$string['modulenameplural'] = 'LAMS';
+$string['notsetup'] = 'NOT_SET_UP';
+$string['openauthor'] = 'Aprire Autore LAMS';
+$string['openlearner'] = 'Aprire Studente LAMS';
+$string['openmonitor'] = 'Aprire Monitor LAMS';
+$string['refreshSequenceList'] = 'Aggiorna lista sequenza';
+$string['selectExistingSequence'] = 'Selezionare un sequenza esistente o creare una nuova sequenza.';
+$string['sequence'] = 'Selezionare Sequenza';
+$string['serverid'] = 'Inserire l\'identificativo (ID) del server ricevuta da <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverkey'] = 'Inserire la chiave del server ricevuta da <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverurl'] = 'Inserire l\'indirizzo (URL) di base utilizzato per accedere al server LAMS. Per esempio http://localhost:8080/lams';
+$string['useSequence'] = 'Utilizzare una sequenza selezionata';
+$string['visibletostudents'] = 'Mostrare l\'attivit&agrave; agli studenti';
+$string['wikistartederror'] = 'Wiki ha gi&agrave; elementi - non &egrave; possibile modificare.';
+$string['workspace'] = 'Selezionare Ambiente di lavoro';
+$string['wrongversionrange'] = '$a non &egrave; un intervallo corretto!';
+
+?>
