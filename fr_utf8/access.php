f9e637063ef0b0933117ae9f8c48e991fc10ae0a
Nicolas Martignoni <martignoni>
2006-03-07 14:30:40 +0000

    Accessibility: the general accessibility, usability and navigation language file.

---
+++ fr_utf8/access.php
@@ -0,0 +1,19 @@
+<?php // $Id$
+
+$string['access'] = 'Accessibilité';
+$string['accesshelp'] = 'Aide d\'accessibilité';
+$string['accesskey'] = 'Raccourci clavier, $a';
+$string['accessstatement'] = 'Déclaration d\'accessibilité';
+$string['monthnext'] = 'Mois suivant';
+$string['monthprevious'] = 'Mois précédent';
+$string['showhideblock'] = 'Afficher ou masquer le bloc';
+$string['sitemap'] = 'Carte du site';
+$string['skipblock'] = 'Sauter le bloc';
+$string['skipnavigation'] = 'Sauter la navigation';
+$string['tabledata'] = 'Données en tableau, $a';
+$string['tablelayout'] = 'Formatage en tableau, $a';
+$string['tocontent'] = 'Vers le contenu principal';
+$string['today'] = 'Aujourd\'hui';
+$string['tonavigation'] = 'Vers la navigation';
+
+?>
