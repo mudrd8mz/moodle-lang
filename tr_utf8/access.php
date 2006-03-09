875b4dcf01a564f3380cdb956000a57dfde99130
Ethem Evlice <ethem>
2006-03-09 09:42:06 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant.

---
+++ tr_utf8/access.php
@@ -0,0 +1,23 @@
+<?PHP // $Id$ 
+      // access.php - created with Moodle 1.6 development (2006030901)
+
+
+$string['access'] = 'Erişilebilirlik';
+$string['accesshelp'] = 'Erişilebilirlik yardımı';
+$string['accesskey'] = 'Erişim kısayolu, $a';
+$string['accessstatement'] = 'Erişilebilirlik ifadesi';
+$string['breadcrumb'] = 'Yol haritası';
+$string['monthnext'] = 'Sonraki ay';
+$string['monthprev'] = 'Önceki ay';
+$string['showhideblock'] = 'Bloğu gizle/göster';
+$string['sitemap'] = 'Site Haritası';
+$string['skipblock'] = 'Bloğa atla';
+$string['skipnavigation'] = 'Dolaşımı atla';
+$string['tabledata'] = 'Veri tablosu, $a';
+$string['tablelayout'] = 'Şablon tablo, $a';
+$string['tocontent'] = 'Ana içeriğe git';
+$string['today'] = 'Bugün';
+$string['tonavigation'] = 'Dolaşıma git';
+$string['youarehere'] = 'Buradasınız';
+
+?>
