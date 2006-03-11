dc3d97d0fbeb393f623528d87f8e5e1d7f3df387
Mitsuhiro Yoshida <mits>
2006-03-11 16:35:55 +0000

    added a help file and translated some strings for IMS Enterprise enrolment.

--- ja_utf8/enrol_imsenterprise.php
+++ ja_utf8/enrol_imsenterprise.php
@@ -4,15 +4,19 @@
 
 $string['basicsettings'] = '基本設定';
 $string['coursesettings'] = 'コースデータオプション';
+$string['createnewusers'] = 'Moodleに登録されていないユーザのアカウントを作成する';
 $string['cronfrequency'] = '処理の頻度';
 $string['deleteusers'] = 'IMSデータに指定されている場合、ユーザアカウントを削除する';
 $string['enrolname'] = 'IMSエンタープライズファイル';
 $string['filelockedmailsubject'] = 'インポートエラー: ユーザ登録ファイル';
+$string['fixcasepersonalnames'] = '個人名をタイトル文字にする';
 $string['fixcaseusernames'] = 'ユーザ名を小文字に変更する';
 $string['location'] = 'ファイルロケーション';
+$string['logtolocation'] = 'ログファイルの出力場所 ( 空白はログ無し )';
 $string['mailadmins'] = '管理者にメール通知する';
 $string['mailusers'] = 'ユーザにメール通知する';
 $string['miscsettings'] = 'その他';
+$string['processphoto'] = 'ユーザフォトデータをプロフィールに追加する';
 $string['usersettings'] = 'ユーザデータオプション';
 
 ?>
