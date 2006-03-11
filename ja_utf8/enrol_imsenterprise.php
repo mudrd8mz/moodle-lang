52575e78cb1520fa78de4c1d90b4ad88f9320b0b
Mitsuhiro Yoshida <mits>
2006-03-11 03:06:02 +0000

    added a new lang file.

---
+++ ja_utf8/enrol_imsenterprise.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // enrol_imsenterprise.php - created with Moodle 1.6 development (2006031000)
+
+
+$string['basicsettings'] = '基本設定';
+$string['coursesettings'] = 'コースデータオプション';
+$string['cronfrequency'] = '処理の頻度';
+$string['deleteusers'] = 'IMSデータに指定されている場合、ユーザアカウントを削除する';
+$string['enrolname'] = 'IMSエンタープライズファイル';
+$string['filelockedmailsubject'] = 'インポートエラー: ユーザ登録ファイル';
+$string['fixcaseusernames'] = 'ユーザ名を小文字に変更する';
+$string['location'] = 'ファイルロケーション';
+$string['mailadmins'] = '管理者にメール通知する';
+$string['mailusers'] = 'ユーザにメール通知する';
+$string['miscsettings'] = 'その他';
+$string['usersettings'] = 'ユーザデータオプション';
+
+?>
