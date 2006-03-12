9feec56bcce53b7e72b3c963720e4c5fbd79ff7f
Mitsuhiro Yoshida <mits>
2006-03-12 11:42:52 +0000

    translated some strings for IMS Enterprise enrolment.

--- ja_utf8/enrol_imsenterprise.php
+++ ja_utf8/enrol_imsenterprise.php
@@ -2,8 +2,11 @@
       // enrol_imsenterprise.php - created with Moodle 1.6 development (2006031000)
 
 
+$string['allowunenrol'] = 'IMSデータに学生/教師の<strong>登録解除</strong> を許可する';
 $string['basicsettings'] = '基本設定';
 $string['coursesettings'] = 'コースデータオプション';
+$string['createnewcategories'] = 'Moodleに登録されていない場合、新しい ( 隠された ) カテゴリを作成する';
+$string['createnewcourses'] = 'Moodleに登録されていない場合、新しい ( 隠された ) コースを作成する';
 $string['createnewusers'] = 'Moodleに登録されていないユーザのアカウントを作成する';
 $string['cronfrequency'] = '処理の頻度';
 $string['deleteusers'] = 'IMSデータに指定されている場合、ユーザアカウントを削除する';
@@ -17,6 +20,8 @@ $string['mailadmins'] = '管理者にメール通知する';
 $string['mailusers'] = 'ユーザにメール通知する';
 $string['miscsettings'] = 'その他';
 $string['processphoto'] = 'ユーザフォトデータをプロフィールに追加する';
+$string['sourcedidfallback'] = '「userid」が見つからない場合、ユーザIDに「sourcedid」を使用する';
+$string['truncatecoursecodes'] = 'この長さにコースコードを切り詰める';
 $string['usersettings'] = 'ユーザデータオプション';
 
 ?>
