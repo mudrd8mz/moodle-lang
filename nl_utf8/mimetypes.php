49c3b11976f8e5857e2c94884ca75b02859f5ecb
koen roggemans <koenr>
2006-03-12 11:07:22 +0000

    moving installation documentation to help files: first step (more to come: change admin.php: $string['cronwarning']  and enrol_authorize.php: $string['admincronsetup'] )

---
+++ nl_utf8/mimetypes.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // mimetypes.php - created with Moodle 1.6 development (2006031000)
+
+
+$string['application/msword'] = 'Word-document';
+$string['application/pdf'] = 'PDF-document';
+$string['application/vnd_ms-excel'] = 'Excel-rekenblad';
+$string['application/vnd_ms-powerpoint'] = 'Powerpoint-presentatie';
+$string['application/zip'] = 'Zip-bestand';
+$string['audio/mp3'] = 'MP3-geluidsbestand';
+$string['audio/wav'] = 'geluidsbestand';
+$string['document/unknown'] = 'bestand';
+$string['image/bmp'] = 'ongecomprimeerde BMP-afbeelding';
+$string['image/gif'] = 'GIF-afbeelding';
+$string['image/jpeg'] = 'JPEG-afbeelding';
+$string['text/plain'] = 'tekstbestand';
+$string['text/rtf'] = 'RTF-document';
+
+?>
