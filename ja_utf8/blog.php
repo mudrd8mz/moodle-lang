ea208071e05ba2f75629cfb10e8a760c22cc337a
Mitsuhiro Yoshida <mits>
2006-03-13 18:57:20 +0000

    added a new lang file.

---
+++ ja_utf8/blog.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // blog.php - created with Moodle 1.6 development (2006031000)
+
+
+$string['addnewentry'] = '新しいエントリの追加';
+$string['addotags'] = '公式タグの追加';
+$string['addptags'] = 'ユーザ定義タグの追加';
+$string['blockmenutitle'] = 'ブログメニュー';
+$string['blogpreferences'] = 'ブログ設定';
+$string['blogs'] = 'ブログ';
+$string['entrybodyonlydesc'] = 'エントリ説明';
+$string['entrytitle'] = 'エントリタイトル';
+$string['entryupdated'] = 'ブログエントリが更新されました。';
+$string['noguestpost'] = 'ゲストはブログを投稿できません!';
+$string['otags'] = '公式タグ';
+$string['pagesize'] = 'ページあたりのブログエントリ数';
+$string['ptags'] = 'ユーザ定義タグ';
+$string['settingsupdatederror'] = 'エラーが発生したため、ブログ設定は更新されませんでした。';
+$string['updateentrywithid'] = 'エントリの更新';
+$string['viewcourseentries'] = 'コースエントリの表示';
+$string['viewmyentries'] = 'マイエントリの表示';
+$string['viewsiteentries'] = 'サイトエントリの表示';
+
+?>
