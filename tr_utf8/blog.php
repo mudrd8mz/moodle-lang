2ae1a059ddf8fd667c2ae25e6138860c3e1dda7b
Ethem Evlice <ethem>
2006-03-13 12:56:26 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant.

---
+++ tr_utf8/blog.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // blog.php - created with Moodle 1.6 development (2006031000)
+
+
+$string['addnewentry'] = 'Yeni kayıt ekle';
+$string['addotags'] = 'Resmi etiketleri ekle';
+$string['addptags'] = 'Kullanıcı tanımlı etiketleri ekle';
+$string['blockmenutitle'] = 'Blog menüsü';
+$string['blogpreferences'] = 'Blog seçenekleri';
+$string['blogs'] = 'Bloglar';
+$string['entrybody'] = 'Blog metni';
+$string['entrybodyonlydesc'] = 'Açıklama';
+$string['entryerrornotyours'] = 'Bu size ait değil';
+$string['entrytitle'] = 'Başlık';
+$string['entryupdated'] = 'Blog güncellendi';
+$string['noguestpost'] = 'Konukar blogları göremezler';
+$string['notallowedtoedit'] = 'Bu kaydı düzeltmeye izniniz yok';
+$string['otags'] = 'Resmi etiketler';
+$string['pagesize'] = 'Her sayfadaki blog kaydı sayısı';
+$string['ptags'] = 'Kullanıcı tanımlı etiketler';
+$string['publishto'] = 'Yayınla';
+$string['publishtonoone'] = 'Kendiniz (raf)';
+$string['publishtosite'] = 'Bu sitedeki herkese';
+$string['publishtoworld'] = 'Bu dünyadaki herkese';
+$string['settingsupdatederror'] = 'Bir hata oluştu, blog seçenekleri ayarları kaydedilemedi.';
+$string['updateentrywithid'] = 'Kayıt güncelleniyor';
+$string['viewcourseentries'] = 'Kayıtlarıma bak';
+$string['viewmyentries'] = 'Kayıtlarıma bak';
+$string['viewsiteentries'] = 'Site kayıtlarına bak';
+
+?>
