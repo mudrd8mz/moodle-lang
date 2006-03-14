8cb8a340f21ba0d82b6998722212448b6de8c43f
Nicolas Martignoni <martignoni>
2006-03-14 13:48:04 +0000

    New file added

---
+++ fr_utf8/blog.php
@@ -0,0 +1,30 @@
+<?php  // $Id$
+
+$string['addnewentry'] = 'Ajouter un nouvel article';
+$string['addotags'] = 'Ajouter des balises officielles';
+$string['addptags'] = 'Ajouter des balises personnelles';
+$string['blockmenutitle'] = 'Menu blog';
+$string['blogdeleteconfirm'] = 'Supprimer ce blog&nbsp;?';
+$string['blogs'] = 'Blogs';
+$string['blogpreferences'] = 'Préférences du blog';
+$string['entrybody'] = 'Corps de l\'article du blog';
+$string['entrybodyonlydesc'] = 'Description de l\'article';
+$string['entryerrornotyours'] = 'Cet article n\'est pas vôtre';
+$string['entrytitle'] = 'Titre de l\'article';
+$string['entryupdated'] = 'Article du blog mis à jour';
+$string['noguestpost'] = 'Les invités ne peuvent écrire dans le blog&nbsp;!';
+$string['notallowedtoedit'] = 'Vous n\'êtes pas autorisé à modifier cette article';
+$string['otags'] = 'Balises officielles';
+$string['pagesize'] = 'Nombre d\'articles par pages';
+$string['ptags'] = 'Balises personnelles';
+$string['publishto'] = 'Publier pour';
+$string['publishtonoone'] = 'Vous-même (brouillon)';
+$string['publishtosite'] = 'Tout le monde sur ce site';
+$string['publishtoworld'] = 'Tout le monde (monde entier)';
+$string['settingsupdatederror'] = 'Une erreur est survenue. Les préférences de blog n\'ont pas été enregistrées';
+$string['updateentrywithid'] = 'Mise à jour de l\'article';
+$string['viewcourseentries'] = 'Afficher les articles du cours';
+$string['viewmyentries'] = 'Afficher mes articles';
+$string['viewsiteentries'] = 'Afficher les articles du site';
+
+?>
