9583687bf2f05c2ea234e62627f6b46c749e7922
Mitsuhiro Yoshida <mits>
2006-03-15 00:13:03 +0000

    polished. added a help file IMS Enterprise enrolment.

--- ja_utf8/blog.php
+++ ja_utf8/blog.php
@@ -1,17 +1,20 @@
 <?PHP // $Id$ 
-      // blog.php - created with Moodle 1.6 development (2006031000)
+      // blog.php - created with Moodle 1.6 development (2006031400)
 
 
 $string['addnewentry'] = '新しいエントリの追加';
 $string['addotags'] = '公式タグの追加';
 $string['addptags'] = 'ユーザ定義タグの追加';
 $string['blockmenutitle'] = 'ブログメニュー';
+$string['blogdeleteconfirm'] = 'このブログを削除しますか?';
 $string['blogpreferences'] = 'ブログ設定';
 $string['blogs'] = 'ブログ';
 $string['entrybodyonlydesc'] = 'エントリ説明';
+$string['entryerrornotyours'] = 'これはあなたのエントリではありません。';
 $string['entrytitle'] = 'エントリタイトル';
 $string['entryupdated'] = 'ブログエントリが更新されました。';
 $string['noguestpost'] = 'ゲストはブログを投稿できません!';
+$string['notallowedtoedit'] = 'あなたはこのエントリを編集できません。';
 $string['otags'] = '公式タグ';
 $string['pagesize'] = 'ページあたりのブログエントリ数';
 $string['ptags'] = 'ユーザ定義タグ';
