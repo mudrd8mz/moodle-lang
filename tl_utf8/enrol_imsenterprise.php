44a5057a2138cb3d7fdd4863e4890c3bc997243b
rcantada <rcantada>
2006-03-15 03:09:25 +0000

    added enrol_imsenterprise.php

---
+++ tl_utf8/enrol_imsenterprise.php
@@ -0,0 +1,35 @@
+<?PHP // $Id$ 
+      // enrol_imsenterprise.php - created with Moodle 1.6 development (2006022400)
+
+
+$string['aftersaving___'] = 'Matapos mong maisave ang kaayusan mo, baka nais mong';
+$string['allowunenrol'] = 'Pahintulutan ang datos ng IMS na <strong>magtanggal sa pagkakaenrol</strong> ng mga mag-aaral/guro';
+$string['basicsettings'] = 'Pangunahing kaayusan';
+$string['coursesettings'] = 'Mga opsiyon ng datos ng kurso';
+$string['createnewcategories'] = 'Lumikha ng mga bagong (nakatagong) kategoriya ng kurso na hindi matagpuan sa Moodle';
+$string['createnewcourses'] = 'Lumikha ng mga (nakatagong) kurso na hindi matagpuan sa Moodle';
+$string['createnewusers'] = 'Lumikha ng mga account ng user para sa mga user na hindi pa rehistrado sa Moodle';
+$string['cronfrequency'] = 'Kalimitan ng pagpoproseso';
+$string['deleteusers'] = 'Burahin ang account ng user kapag itinakda sa datos ng IMS';
+$string['description'] = 'Paulit-ulit na hahanapin at ipoproseso ng paraang ito ang pinormat-nang-espesyal na file na teksto sa lokasyong itatakda mo.  Kailangang sumunod ang file sa <a href=\'../help.php?module=enrol/imsenterprise&file=formatoverview.html\' target=\'blank\'>Ispesipikasyon ng IMS Enterprise </a> na naglalaman ng mga elementong XML na tao, pangkat, at pagkamiyembro.';
+$string['doitnow'] = 'maglunsad ng pag-angkat na IMS Enterprise ngayon';
+$string['enrolname'] = 'File na IMS Enterprise';
+$string['filelockedmail'] = 'Hindi mabura ng prosesong cron ang tekstong file na ginagamit mo para sa pag-eenrol na nakabatay-sa-IMS-file ($a).  Karaniwan ay dahil sa mali ang mga permiso nito.  Paki-ayos ang mga permiso nito para mabura ito ng Moodle, kundi ay baka maproseso ito nang paulit-ulit.';
+$string['filelockedmailsubject'] = 'Mahalagang error: File ng pag-eenrol';
+$string['fixcasepersonalnames'] = 'Gawing Titik na Pampamagat ang titik ng mga personal na pangalan';
+$string['fixcaseusernames'] = 'Gawing maliit na titik ang mga username';
+$string['location'] = 'Lokasyon ng file';
+$string['logtolocation'] = 'Lokasyon ng log file output (blanko para walang paglalog)';
+$string['mailadmins'] = 'Abisuhan ang admin sa pamamagitan ng email';
+$string['mailusers'] = 'Abisuhan ang user sa pamamagitan ng email';
+$string['miscsettings'] = 'Atbp.';
+$string['processphoto'] = 'Idagdag ang datos ng larawan ng user sa pagkakakilanlan';
+$string['processphotowarning'] = 'Babala:  Malamang na magdagdag ng trabaho sa server ang pagpoproseso ng larawan.  Iminumungkahi na huwag buhayin ang opsiyong ito kung maraming bilang ng mag-aaral ang inaasahang iproseso.';
+$string['restricttarget'] = 'Iproseso lamang ang datos kung itinakda ang sumusunod na target';
+$string['sourcedidfallback'] = 'Gamitin ang &quot;sourcedid&quot; para sa userid ng tao kung ang field ng &quot;userid&quot; ay hindi matagpuan';
+$string['truncatecoursecodes'] = 'Putulin ang mga code ng kurso sa habang ito';
+$string['usecapitafix'] = 'Lagyan ng tsek ang kahon kung gumagamit ng  &quot;Capita&quot; (medyo mali ang XML format nila)';
+$string['usersettings'] = 'Mga opsiyon ng datos ng user';
+$string['zeroisnotruncation'] = 'Ang ibig sabihin ng 0 ay walang pagputol';
+
+?>
