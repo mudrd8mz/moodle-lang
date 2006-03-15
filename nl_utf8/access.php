c4900336e2d1247253eb5e9213bb534df3b25bc8
koen roggemans <koenr>
2006-03-15 08:50:10 +0000

    initial translation

---
+++ nl_utf8/access.php
@@ -0,0 +1,23 @@
+<?PHP // $Id$ 
+      // access.php - created with Moodle 1.6 development (2006031000)
+
+
+$string['access'] = 'Toegankelijkheid';
+$string['accesshelp'] = 'Help toegankelijkheid';
+$string['accesskey'] = 'Toegangstoets, $a';
+$string['accessstatement'] = 'Toegankelijkheidsverklaring';
+$string['breadcrumb'] = 'Broodkruimelpad';
+$string['monthnext'] = 'Volgende maand';
+$string['monthprev'] = 'Vorige maand';
+$string['showhideblock'] = 'Toon of verberg blok';
+$string['sitemap'] = 'Site kaart';
+$string['skipblock'] = 'Sla een blok over';
+$string['skipnavigation'] = 'Sla navigatie over';
+$string['tabledata'] = 'Gegevenstabel, $a';
+$string['tablelayout'] = 'Opmaaktabel, $a';
+$string['tocontent'] = 'Ga naar hoofdinhoud';
+$string['today'] = 'Vandaag';
+$string['tonavigation'] = 'Ga naar navigatie';
+$string['youarehere'] = 'Je bent hier';
+
+?>
