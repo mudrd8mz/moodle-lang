64480e1a25d6ffb77c2509da8076105aefc0f133
koen roggemans <koenr>
2006-03-15 12:02:36 +0000

    initial translation

---
+++ nl_utf8/blog.php
@@ -0,0 +1,32 @@
+<?PHP // $Id$ 
+      // blog.php - created with Moodle 1.6 development (2006031400)
+
+
+$string['addnewentry'] = 'Voeg een nieuw item toe';
+$string['addotags'] = 'Voeg tags toe';
+$string['addptags'] = 'Voeg eigen tags toe';
+$string['blockmenutitle'] = 'Blog menu';
+$string['blogdeleteconfirm'] = 'Deze blog verwijderen?';
+$string['blogpreferences'] = 'Blog voorkeuren';
+$string['blogs'] = 'Blogs';
+$string['entrybody'] = 'Blog tekstveld';
+$string['entrybodyonlydesc'] = 'Beschrijving van deze tekst';
+$string['entryerrornotyours'] = 'Deze tekst is niet van jouw';
+$string['entrytitle'] = 'Titel van deze tekst';
+$string['entryupdated'] = 'Blog aangepast';
+$string['noguestpost'] = 'Gasten kunnen geen blogs posten';
+$string['notallowedtoedit'] = 'Je mag deze tekst niet bewerken';
+$string['otags'] = 'Officiële tags';
+$string['pagesize'] = 'Aantal teksten per pagina';
+$string['ptags'] = 'Gebruikerstags';
+$string['publishto'] = 'Publiceer naar';
+$string['publishtonoone'] = 'jezelf (klad)';
+$string['publishtosite'] = 'iedereen op deze site';
+$string['publishtoworld'] = 'de hele wereld';
+$string['settingsupdatederror'] = 'Er is een fout gebeurt: de voorkeursinstellingen voor deze blog konden niet aangepast worden';
+$string['updateentrywithid'] = 'Tekst aanpassen';
+$string['viewcourseentries'] = 'Bekijk de blogs van deze cursus';
+$string['viewmyentries'] = 'Bekijk mijn eigen blogs';
+$string['viewsiteentries'] = 'Bekijk de blogs van de site';
+
+?>
