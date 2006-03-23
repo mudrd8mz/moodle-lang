f5d39ecb99832c670aedd7e073df160754c4ea37
Mitsuhiro Yoshida <mits>
2006-03-23 18:44:20 +0000

    translated few strings.

--- ja_utf8/blog.php
+++ ja_utf8/blog.php
@@ -1,11 +1,13 @@
 <?PHP // $Id$ 
-      // blog.php - created with Moodle 1.6 development (2006032001)
+      // blog.php - created with Moodle 1.6 development (2006032200)
 
 
 $string['addnewentry'] = '新しいエントリの追加';
 $string['addotags'] = 'オフィシャルタグの追加';
 $string['addptags'] = 'ユーザ定義タグの追加';
 $string['blockmenutitle'] = 'ブログメニュー';
+$string['blocktagstitle'] = 'ブログタグ';
+$string['blocktitle'] = 'ブログタグのブロックタイトル';
 $string['blog'] = 'ブログ';
 $string['blogdeleteconfirm'] = 'このブログを削除してもよろしいですか?';
 $string['blogpreferences'] = 'ブログ設定';
@@ -22,6 +24,8 @@ $string['intro'] = 'このRSSフィードは、１つまたはそれ以上のブ
 $string['noentriesyet'] = '表示できるエントリはありません。';
 $string['noguestpost'] = 'ゲストはブログを投稿できません!';
 $string['notallowedtoedit'] = 'あなたはこのエントリを編集できません。';
+$string['numberofentries'] = 'エントリ: $a';
+$string['numberoftags'] = '表示するタグ数';
 $string['otags'] = 'オフィシャルタグ';
 $string['pagesize'] = 'ページあたりのブログエントリ数';
 $string['ptags'] = 'ユーザ定義タグ';
@@ -31,6 +35,7 @@ $string['publishtosite'] = 'このサイトすべて';
 $string['publishtoworld'] = '世界中';
 $string['settingsupdatederror'] = 'エラーが発生したため、ブログ設定は更新されませんでした。';
 $string['siteblog'] = 'サイトブログ: $a';
+$string['tagtext'] = 'タグテキスト';
 $string['updateentrywithid'] = 'エントリの更新';
 $string['userblog'] = 'ユーザブログ: $a';
 $string['viewcourseentries'] = 'コースエントリの表示';
