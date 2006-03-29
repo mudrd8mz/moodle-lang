ef896b95ff85d42bb173910f41d8cc86a1ae7e81
Mitsuhiro Yoshida <mits>
2006-03-29 17:03:21 +0000

    added new files for quiz.

---
+++ ja_utf8/quiz_grading.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // quiz_grading.php - created with Moodle 1.6 development (2006032200)
+
+
+$string['essayonly'] = '現在のところ手動評定は、作文問題のみに利用できます。';
+$string['grading'] = '手動評定';
+
+?>
---
+++ ja_utf8/quiz_regrade.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // quiz_regrade.php - created with Moodle 1.6 development (2006032200)
+
+
+$string['regrade'] = '最評定';
+
+?>
