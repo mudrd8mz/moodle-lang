b44c74064682049a9cedc317bf92d31844229e1c
Mitsuhiro Yoshida <mits>
2006-04-09 15:12:54 +0000

    translated new strings for blog.

--- ja_utf8/blog.php
+++ ja_utf8/blog.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // blog.php - created with Moodle 1.6 Beta 1 (2006032900)
+      // blog.php - created with Moodle 1.6 Beta 2 (2006040500)
 
 
 $string['addnewentry'] = '新しいエントリの追加';
@@ -14,12 +14,15 @@ $string['blogpreferences'] = 'ブログ設定';
 $string['blogs'] = 'ブログ';
 $string['blogtags'] = 'ブログタグ';
 $string['courseblog'] = 'コースブログ: $a';
+$string['courseblogs'] = 'ユーザはコース内のみで読むことができるブログを作成できます。';
+$string['disableblogs'] = 'ブログシステムを完全に無効にする';
 $string['entrybody'] = 'ブログエントリ本文';
 $string['entrybodyonlydesc'] = 'エントリ説明';
 $string['entryerrornotyours'] = 'これはあなたのエントリではありません。';
 $string['entrytitle'] = 'エントリタイトル';
 $string['entryupdated'] = 'ブログエントリが更新されました。';
 $string['groupblog'] = 'グループブログ: $a';
+$string['groupblogs'] = 'ユーザはグループ内のみで読むことができるブログを作成できます。';
 $string['intro'] = 'このRSSフィードは、１つまたはそれ以上のブログから自動的に生成されました。';
 $string['noentriesyet'] = '表示できるエントリはありません。';
 $string['noguestpost'] = 'ゲストはブログを投稿できません!';
@@ -28,6 +31,7 @@ $string['numberofentries'] = 'エントリ: $a';
 $string['numberoftags'] = '表示するタグ数';
 $string['otags'] = 'オフィシャルタグ';
 $string['pagesize'] = 'ページあたりのブログエントリ数';
+$string['personalblogs'] = 'ユーザは個人的なブログのみを作成できます。';
 $string['ptags'] = 'ユーザ定義タグ';
 $string['publishto'] = '公開先: ';
 $string['publishtonoone'] = 'あなた自身 ( 下書き )';
@@ -35,6 +39,7 @@ $string['publishtosite'] = 'このサイトすべて';
 $string['publishtoworld'] = '世界中';
 $string['settingsupdatederror'] = 'エラーが発生したため、ブログ設定は更新されませんでした。';
 $string['siteblog'] = 'サイトブログ: $a';
+$string['siteblogs'] = 'ユーザはサイト全体で読むことができるブログを作成できます。';
 $string['tagdatelastused'] = '日付タグは使用されています。';
 $string['tagsort'] = 'タグの並び替え';
 $string['tagtext'] = 'タグテキスト';
@@ -44,5 +49,6 @@ $string['userblog'] = 'ユーザブログ: $a';
 $string['viewcourseentries'] = 'コースエントリの表示';
 $string['viewmyentries'] = 'マイエントリの表示';
 $string['viewsiteentries'] = 'サイトエントリの表示';
+$string['worldblogs'] = 'ユーザは世界中からアクセスできるブログを作成できます。';
 
 ?>
