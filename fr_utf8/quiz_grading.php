e8837faa96fb12146123290cc50a885c879d919e
Nicolas Martignoni <martignoni>
2006-04-10 21:07:55 +0000

    Manual grading report plugin is now no longer essay-specific.

--- fr_utf8/quiz_grading.php
+++ fr_utf8/quiz_grading.php
@@ -1,6 +1,6 @@
 <?php // $Id$
 
 $string['grading'] = 'Notation manuelle';
-$string['essayonly'] = 'La notation manuelle n\'est actuellement possible que pour les questions de composition';
+$string['essayonly'] = 'Les questions suivantes doivent être notées manuellement';
 
 ?>
