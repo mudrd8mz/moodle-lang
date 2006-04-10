0d23ee8f9a2027e261538c76ed6e64fcc008fe3a
tormoda <tormoda>
2006-04-10 16:01:36 +0000

    Changed "Norsk - Nynorsk" to "Norsk - nynorsk"

--- nn_utf8/langconfig.php
+++ nn_utf8/langconfig.php
@@ -5,8 +5,6 @@
 $string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Æ,Ø,Å';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['locale'] = 'nn_NO.UTF-8';
-$string['localewin'] = 'Norwegian-Nynorsk_Norway.1252';
-$string['localewincharset'] = 'WINDOWS-1252';
 $string['oldcharset'] = 'ISO-8859-1';
 $string['strftimedate'] = '%%d %%B %%Y';
 $string['strftimedateshort'] = '%%d %%B';
@@ -18,6 +16,6 @@ $string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%H:%%M ';
 $string['strftimetime'] = '%%H:%%M';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Norsk - Nynorsk';
+$string['thislanguage'] = 'Norsk - nynorsk';
 
 ?>
