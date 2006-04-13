32ecf9409a186d284adf3efac6bd75a1f3b3820a
Nicolas Martignoni <martignoni>
2006-04-13 06:34:27 +0000

    Now there is an option to see all attempts, even for users who have since unenrolled. That is necessary in order to be able to delete these attempts, see http://moodle.org/mod/forum/discuss.php?d=34421

--- fr_utf8/quiz_overview.php
+++ fr_utf8/quiz_overview.php
@@ -1,5 +1,9 @@
 <?php // $Id$
 
+$string['attemptsonly'] = 'N\'afficher que les $a avec tentatives';
+$string['allattempts'] = 'Afficher toutes les tentativess';
+$string['allstudents'] = 'Afficher tous les $a';
+$string['noattemptsonly'] = 'N\'afficher que les $a sans tentatives';
 $string['overview'] = 'Vue d\'ensemble';
 
 ?>
