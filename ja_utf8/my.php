2f863f86fd9bd645c82a70602f3c371ddca0ed50
Mitsuhiro Yoshida <mits>
2006-04-16 21:21:30 +0000

    polished.

--- ja_utf8/my.php
+++ ja_utf8/my.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // my.php - created with Moodle 1.6 development (2005101200)
+      // my.php - created with Moodle 1.6 Beta 3 (2006041100)
 
 
-$string['mymoodle'] = 'マイ Moodle';
+$string['mymoodle'] = 'マイMoodle';
 $string['nocourses'] = '表示するコース情報がありません。';
-$string['noguest'] = 'ゲストユーザは「マイ Moodle」ページを利用できません。';
-$string['pinblocks'] = 'マイ Moodle 固定ブロックの設定';
-$string['pinblocksexplan'] = 'あなたがここで設定した全てのブロックは、全てのMoodleユーザが「マイ Moodle」画面で閲覧 ( 編集不可 ) することができます。';
+$string['noguest'] = 'ゲストユーザは「マイMoodle」ページを利用できません。';
+$string['pinblocks'] = 'マイMoodle固定ブロックの設定';
+$string['pinblocksexplan'] = 'あなたがここで設定したすべてのブロックは、すべてのMoodleユーザの「マイMoodle」画面に表示 ( 編集不可 ) されます。';
 
 ?>
