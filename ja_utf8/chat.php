bf24d4a112f6ed1eb3dd37b185841a78371aa865
Mitsuhiro Yoshida <mits>
2006-04-20 00:57:00 +0000

    translated new strings for chat.

--- ja_utf8/chat.php
+++ ja_utf8/chat.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // chat.php - created with Moodle 1.6 development (2006032200)
+      // chat.php - created with Moodle 1.6 Beta 3 (2006041900)
 
 
 $string['beep'] = 'ビープ';
@@ -8,6 +8,7 @@ $string['chatname'] = 'チャットルーム名';
 $string['chatreport'] = 'チャットセッション';
 $string['chattime'] = '次回のチャットタイム';
 $string['configmethod'] = '通常のチャットメソッドでは、クライアントは定期的にサーバにアクセスして内容を更新します。このメソッドは設定を必要とせず、どこでも使うことができますが、チャット参加者が多くなればサーバに対する多大な負担が生じます。サーバデーモンを使用する場合、Unixのシェルアクセスが必要ですが、軽快なチャット環境を提供することができます。';
+$string['confignormalupdatemode'] = 'チャットルームは、通常HTTP 1.1の<em>Keep-Alive</em>機能を使用して効果的に更新されます。 しかし、この更新はサーバに対して非常に負荷がかかります。更に進歩した方法は、チャットルームの更新に<em>Stream</em>を使用するものです。<em>Stream</em>を使用する方法 ( chatdメソッドに似ています ) は、 <em>Keep-Alive</em>機能の使用より良いものですが、あなたのサーバではサポートされていない場合もあります。';
 $string['configoldping'] = 'ユーザの応答がなくなってから、どのくらいの時間 ( 秒数 ) で退室したと見なしますか? これは単に上限であり、通常退出は非常に速く検出されます。あなたのサーバに、更に小さな値を設定する必要があります。通常のメソッドを使用している場合、2* chat_refresh_room より小さな値を<strong>設定しないでください</strong>。';
 $string['configrefreshroom'] = 'どのくらいのタイミング ( 秒数 ) でチャットルームをリフレッシュしますか? この通知を小さくすれば、チャットルームはレスポンスが良いように見えますが、多くの人がチャットをする場合、サーバにかかる負荷が高くなります。';
 $string['configrefreshuserlist'] = 'どのくらいのタイミング ( 秒数 ) でユーザリストをリフレッシュしますか?';
@@ -41,6 +42,8 @@ $string['neverdeletemessages'] = 'メッセージを削除しない';
 $string['nextsession'] = '次のスケジュールセッション';
 $string['noguests'] = 'ゲストはこのチャットを利用できません。';
 $string['nomessages'] = 'メッセージがありません。';
+$string['normalkeepalive'] = 'KeepAlive';
+$string['normalstream'] = 'Stream';
 $string['noscheduledsession'] = 'スケジュールセッションがありません。';
 $string['repeatdaily'] = '毎日同じ時間に';
 $string['repeatnone'] = '繰り返し無し - 指定した時間にのみ公開';
