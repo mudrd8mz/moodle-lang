d21ffdd660165162bd25eb2a16b1a282cec256e8
Ethem Evlice <ethem>
2006-04-22 15:47:50 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/enrol_imsenterprise.php
+++ tr_utf8/enrol_imsenterprise.php
@@ -1,18 +1,30 @@
 <?PHP // $Id$ 
-      // enrol_imsenterprise.php - created with Moodle 1.6 development (2006031000)
+      // enrol_imsenterprise.php - created with Moodle 1.6 Beta 3 (2006041900)
 
 
+$string['aftersaving...'] = 'Ayarları kaydettiğinizde şunu yapmak isteyebilirsiniz:';
+$string['allowunenrol'] = 'IMS verisine öğrencilerin/eğitimcilerin ders kaydının <strong>silinmesine</strong> izin ver';
 $string['basicsettings'] = 'Temel ayarlar';
 $string['coursesettings'] = 'Kurs verisi seçenekleri';
+$string['createnewcategories'] = 'Moodle\'da yoksa yeni kurs kategorilerini (gizli) oluştur';
+$string['createnewcourses'] = 'Moodle\'da yoksa yeni kursu (gizli) oluştur';
+$string['createnewusers'] = 'Moodle\'da kayıtlı olmayan kullanıcıların hesaplarını oluştur';
 $string['cronfrequency'] = 'İşleme sıklığı';
+$string['deleteusers'] = 'IMS verisinde belirtildiği zaman kullanıcının hesaplarını sil';
+$string['description'] = 'Bu yöntem özel olarak biçimlendirilmiş ve yerini belirttiğiniz dosyayı belirli aralıklarla kontrol edecek ve işleme alacaktır. Dosya XML olarak kişiyi, grubu ve üyeliğini içeren <a href=\'../help.php?module=enrol/imsenterprise&file=formatoverview.html\' target=\'_blank\'>IMS Enterprise tanımlamalarına</a> uygun olmalıdır.';
+$string['doitnow'] = 'hemen şimdi IMS Enterprise alması gerçekleştir';
 $string['enrolname'] = 'IMS Enterprise dosyası';
 $string['filelockedmailsubject'] = 'Önemli hata: Ders kaydı dosyası';
 $string['fixcasepersonalnames'] = 'İsimlerin ilk harflerini Büyük Harfe çevir';
 $string['fixcaseusernames'] = 'Kullanıcı adlarını küçük harfe çevir';
 $string['location'] = 'Dosyanın yeri';
+$string['logtolocation'] = 'Log dosyası yeri (kayıt tutmamak için boş bırakın)';
 $string['mailadmins'] = 'Yöneticileri epostayla bilgilendir';
 $string['mailusers'] = 'Kullanıcıları epostayla bilgilendir';
 $string['miscsettings'] = 'Çeşitli';
+$string['processphoto'] = 'Profile kullanıcı resim verisini ekle';
+$string['truncatecoursecodes'] = 'Kurs kodlarını bu uzunluğa kadar kabul et';
 $string['usersettings'] = 'Kullanıcı verisi seçenekleri';
+$string['zeroisnotruncation'] = '0 kesme olmayacağını gösterir';
 
 ?>
