559b789c0c42d6a069e90541f985aa34bc16c828
tormoda <tormoda>
2006-04-30 11:25:35 +0000

    Some new strings in moodle.php and admin.php

--- nn_utf8/admin.php
+++ nn_utf8/admin.php
@@ -1,10 +1,20 @@
 <?PHP // $Id$ 
-      // admin.php - created with Moodle 1.5.2 (2005060220)
+      // admin.php - created with Moodle 1.6 Beta 1 (2006032900)
 
 
 $string['cachetext'] = 'Levetid for tekst i kortminne';
 $string['calendarsettings'] = 'Kalender';
+$string['configsectioninterface'] = 'Grensesnitt';
+$string['configsectionmail'] = 'E-post';
+$string['configsectionmaintenance'] = 'Vedlikehald';
+$string['configsectionmisc'] = 'Diverse';
+$string['configsectionoperatingsystem'] = 'Operativsystem';
+$string['configsectionsecurity'] = 'Tryggleik';
+$string['configsectionstats'] = 'Statistikk';
+$string['configsectionuser'] = 'Brukar';
 $string['filteruploadedfiles'] = 'Filtrer opplastede filer';
+$string['helpsitemaintenance'] = 'For oppgraderingar og vedlikehald';
+$string['sitemaintenancemode'] = 'Vedlikehaldsmodus';
 $string['upgradelogs'] = 'For full funksjonalitet må de gamle loggene oppgraderes. <a href=\"$a\">Mer informasjon</a>';
 $string['upgradinglogs'] = 'Oppgraderer logger';
 
