7c3059d2705f3bb1c4e0ebb6fe7521067940d3c4
Daniel Mikšík <miksik>
2006-05-04 12:46:42 +0000

    Czech date&time formats introduced, PHP typo fixed

--- cs_utf8/langconfig.php
+++ cs_utf8/langconfig.php
@@ -1,27 +1,23 @@
-<?PHP // $Id$ 
-      // langconfig.php - created with Moodle 1.6 development (2005101200)
-
-
+<?PHP
 $string['alphabet'] = 'A,Á,B,C,Č,D,Ď,E,É,Ě,F,G,H,I,Í,J,K,L,M,N,Ň,O,Ó,P,Q,R,Ř,S,Š,T,Ť,U,Ú,Ů,V,W,X,Y,Ý,Z,Ž';
 $string['backupnameformat'] = '%%Y-%%m-%%d-%%H-%%M';
 $string['firstdayofweek'] = '1';
 $string['locale'] = 'cs_CZ.UTF-8';
-$string['localewin'] = 'Czech_Czech Republic.1250'
+$string['localewin'] = 'Czech_Czech Republic.1250';
 $string['localewincharset'] = 'WINDOWS-1250';
 $string['oldcharset'] = 'ISO-8859-2';
-$string['strftimedate'] = '%%d. %%B %%Y';
-$string['strftimedateshort'] = '%%d. %%B';
-$string['strftimedatetime'] = '%%d. %%B %%Y, %%H:%%M';
+$string['strftimedate'] = '%%e. %%B %%Y';
+$string['strftimedateshort'] = '%%e. %%B';
+$string['strftimedatetime'] = '%%e. %%B %%Y, %%H.%%M';
 $string['strftimedaydate'] = '%%A, %%d. %%B %%Y';
-$string['strftimedaydatetime'] = '%%A, %%d. %%B %%Y, %%H:%%M';
-$string['strftimedayshort'] = '%%A, %%d. %%B';
-$string['strftimedaytime'] = '%%a, %%H:%%M';
+$string['strftimedaydatetime'] = '%%A, %%e. %%B %%Y, %%H.%%M';
+$string['strftimedayshort'] = '%%A, %%e. %%B';
+$string['strftimedaytime'] = '%%a, %%H.%%M';
 $string['strftimemonthyear'] = '%%B %%Y';
-$string['strftimerecent'] = '%%d. %%b, %%H:%%M';
-$string['strftimerecentfull'] = '%%A, %%d. %%B %%Y, %%H:%%M';
-$string['strftimetime'] = '%%H:%%M';
+$string['strftimerecent'] = '%%e. %%b, %%H.%%M';
+$string['strftimerecentfull'] = '%%A, %%e. %%B %%Y, %%H.%%M';
+$string['strftimetime'] = '%%H.%%M';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Čeština';
-
 ?>
