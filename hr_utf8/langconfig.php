843633ab4b162d19068e69b93910d4f0d6871211
koen roggemans <koenr>
2006-05-07 11:10:25 +0000

    lost Croatian language pack is back, now for 1.6. If changes for 1.5 arrive, I will put them again in 1.5, otherwise there is  not much point. Dates still in English on win2K - supported ???

---
+++ hr_utf8/langconfig.php
@@ -0,0 +1,23 @@
+<?php
+$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+$string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
+$string['firstdayofweek'] = '0';
+$string['locale'] = 'hr_HR.UTF-8';
+$string['localewin'] = 'Croatian_Croatia.1250';
+$string['localewincharset'] = 'WINDOWS-1250';
+$string['oldcharset'] = 'windows-1250';
+$string['strftimedate'] = '%%d de %%B de %%Y';
+$string['strftimedateshort'] = ' %%d de %%B ';
+$string['strftimedatetime'] = ' %%d de %%B de %%Y, %%H:%%M';
+$string['strftimedaydate'] = ' %%A, %%d de %%B de %%Y ';
+$string['strftimedaydatetime'] = ' %%A, %%d de %%B de %%Y, %%H:%%M';
+$string['strftimedayshort'] = '%%A, %%d %%B';
+$string['strftimedaytime'] = '%%a, %%H:%%M';
+$string['strftimemonthyear'] = '%%B %%Y';
+$string['strftimerecent'] = ' %%d de %%b, %%H:%%M ';
+$string['strftimerecentfull'] = ' %%a, %%d de %%b de %%Y, %%H:%%M';
+$string['strftimetime'] = ' %%H:%%M';
+$string['thischarset'] = 'UTF-8';
+$string['thisdirection'] = 'ltr';
+$string['thislanguage'] = 'Hrvatski';
+?>
