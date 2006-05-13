c4902a5ece44b1f825cd5ae796f71dc5d56ca4a6
Daniel Mikšík <miksik>
2006-05-13 06:31:28 +0000

    Initial translation

---
+++ cs_utf8/block_loancalc.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // block_loancalc.php - created with Moodle 1.6 Beta 4 (2006042800)
+
+
+$string['amountofloan'] = 'Výše půjčky';
+$string['fortnightly'] = 'Čtrnáctidenně';
+$string['interestrate'] = 'Úroková sazba';
+$string['loancalc'] = 'Kalkulace půjčky';
+$string['loanterm'] = 'Délka půjčky (v letech)';
+$string['monthly'] = 'Měsíčně';
+$string['repaymentamount'] = 'Výše splátky';
+$string['repaymentfreq'] = 'Periodicita splátek';
+$string['weekly'] = 'Týdenně';
+
+?>
