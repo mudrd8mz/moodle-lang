442b0c57238789d79faf78a361da276685bcfdf9
Daniel Mikšík <miksik>
2006-05-13 06:33:36 +0000

    Added 1.6 strings + some style polishing

--- cs_utf8/block_quiz_results.php
+++ cs_utf8/block_quiz_results.php
@@ -1,21 +1,25 @@
 <?PHP // $Id$ 
-      // block_quiz_results.php - created with Moodle 1.5 + (2005060201)
+      // block_quiz_results.php - created with Moodle 1.6 Beta 4 (2006042800)
 
 
 $string['bestgrade'] = 'Nejlepší výsledek:';
 $string['bestgrades'] = '$a nejlepších:';
 $string['bestgroupgrade'] = 'Skupina s nejvyšším průměrem:';
 $string['bestgroupgrades'] = '$a skupin s nejvyšším průměrem:';
-$string['config_format_absolute'] = 'Absolutní počty';
+$string['config_format_absolute'] = 'Absolutní hodnoty';
 $string['config_format_fraction'] = 'Zlomky';
 $string['config_format_percentage'] = 'Procenta';
 $string['config_grade_format'] = 'Zobrazit známky jako:';
+$string['config_name_format'] = 'Úroveň ochrany soukromí při zobrazení výsledků:';
+$string['config_names_anon'] = 'Anonymní výsledky';
+$string['config_names_full'] = 'Zobrazit celá jména';
+$string['config_names_id'] = 'Zobrazit čísla ID';
 $string['config_no_quizzes_in_course'] = 'Tento kurz neobsahuje žádné testy. Musíte založit alespoň jeden test, abyste mohli tento blok řádně použít.';
-$string['config_select_quiz'] = 'Výsledky kterého testu má blok obsahovat?';
-$string['config_show_best'] = 'Kolik nejlepších výsledků se má zobrazovat (0 vypíná)';
-$string['config_show_worst'] = 'Kolik neslabších výsledků se má zobrazovat (0 vypíná)';
+$string['config_select_quiz'] = 'Test, jehož výsledky má blok obsahovat:';
+$string['config_show_best'] = 'Kolik nejlepších výsledků se má zobrazovat (0 ruší tuto položku):';
+$string['config_show_worst'] = 'Kolik nejslabších výsledků se má zobrazovat (0 ruší tuto položku):';
 $string['config_use_groups'] = 'Zobrazovat skupiny místo studentů (pouze pokud test podporuje skupiny)?';
-$string['configuredtoshownothing'] = 'Současné nastavení tohoto bloku neumožňuje zobrazovat jakékoliv výsledky. Měli byste tento blok nakonfigurovat nebo jej skrýt.';
+$string['configuredtoshownothing'] = 'Současné nastavení tohoto bloku neumožňuje zobrazovat žádné výsledky. Měli byste tento blok řádně nastavit, nebo jej skrýt.';
 $string['error_emptyquizid'] = 'Blok je momentálně chybně nastaven: je třeba vybrat test, jehož výsledky se mají zobrazovat.';
 $string['error_emptyquizrecord'] = 'Blok je momentálně chybně nastaven: vybraný test nebyl v databázi nalezen.';
 $string['error_nogroupsexist'] = 'Blok je momentálně chybně nastaven: je nastaven, aby zobrazoval výsledky skupin, ale v kurzu nejsou žádné skupiny nadefinovány';
