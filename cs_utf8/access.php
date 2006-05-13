1d4655719fd7dc21565c3aff0f2f33b9e973b86f
Daniel Mikšík <miksik>
2006-05-13 05:59:32 +0000

    Added 1.6 strings

--- cs_utf8/access.php
+++ cs_utf8/access.php
@@ -1,13 +1,20 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 1.6 Beta 2 (2006040500)
+      // access.php - created with Moodle 1.6 Beta 4 (2006042800)
 
 
+$string['access'] = 'Přístupnost';
+$string['accesshelp'] = 'Nápověda k přístupnosti';
+$string['accesskey'] = 'Zkratková klávesa, $a';
+$string['accessstatement'] = 'Prohlášení o přístupnosti';
+$string['breadcrumb'] = 'Breadcrumb trail';
 $string['monthnext'] = 'Další měsíc';
 $string['monthprev'] = 'Předchozí měsíc';
 $string['showhideblock'] = 'Ukázat nebo skrýt blok';
 $string['sitemap'] = 'Mapa stránek';
 $string['skipblock'] = 'Přeskočit blok';
 $string['skipnavigation'] = 'Přeskočit navigaci';
+$string['tabledata'] = 'Datová tabulka, $a';
+$string['tablelayout'] = 'Formátovací tabulka, $a';
 $string['tocontent'] = 'Přejít k hlavnímu obsahu';
 $string['tonavigation'] = 'Přejít k navigaci';
 $string['youarehere'] = 'Nacházíte se zde';
