fa76c230b9f282e96fc33fc402bc32d9a6ba5f44
Daniel Mikšík <miksik>
2006-05-13 06:30:33 +0000

    Block name change (a suggestion) -- the entry does not have to necessarily be random I suggest using "heslo" for the English "entry" in texts presented to the viewer -- sounds better, I think

--- cs_utf8/block_glossary_random.php
+++ cs_utf8/block_glossary_random.php
@@ -1,27 +1,27 @@
 <?PHP // $Id$ 
-      // block_glossary_random.php - created with Moodle 1.5 + (2005060201)
+      // block_glossary_random.php - created with Moodle 1.6 Beta 4 (2006042800)
 
 
-$string['addentry'] = 'Přidejte váš oblíbený citát!';
-$string['askaddentry'] = 'Mohou-li uživatelé přidávat záznamy do slovníku, zobraz odkaz s tímto textem';
-$string['askinvisible'] = 'Nemohou-li uživatelé přidávat záznamy do slovníku, zobraz tento text';
-$string['askviewglossary'] = 'Mohou-li si uživatelé prohlížet slovník, ale nemohou do něj vkládat položky, zobraz odkaz s tímto textem';
-$string['blockname'] = 'Náhodná položka slovníku';
-$string['intro'] = 'Ujistěte se, že máte ve svém kurzu alespoň jeden slovník s alespoň jedním záznamem. Pak můžete upravit následující nastavení';
-$string['invisible'] = '(pokračuje)';
+$string['addentry'] = 'Přidejte své vlastní heslo!';
+$string['askaddentry'] = 'Mohou-li uživatelé přidávat položky do slovníku, zobrazit odkaz s tímto textem';
+$string['askinvisible'] = 'Nemohou-li si uživatelé prohlížet slovník nebo jej upravovat, zobrazit tento text';
+$string['askviewglossary'] = 'Mohou-li si uživatelé prohlížet slovník, ale nemohou do něj vkládat záznamy, zobrazit odkaz s tímto textem';
+$string['blockname'] = 'Heslo ze slovníku';
+$string['intro'] = 'Ujistěte se, že máte ve svém kurzu alespoň jeden slovník s alespoň jedním záznamem. Pak můžete upravit následující nastavení.';
+$string['invisible'] = '(pokračování příště)';
 $string['lastmodified'] = 'Poslední upravená položka';
 $string['nextone'] = 'Další položka';
 $string['noentriesyet'] = 'Vybraný slovník zatím neobsahuje žádné položky';
-$string['notyetconfigured'] = 'Prosím, upravte nastavení tohoto bloku pomocí ikonky úprav';
+$string['notyetconfigured'] = 'Upravte prosím nastavení tohoto bloku pomocí ikony úprav';
 $string['notyetglossary'] = 'Musíte mít k dispozici alespoň jeden slovník';
 $string['random'] = 'Náhodná položka';
-$string['refresh'] = 'Počet dnů předtím, něž je vybrána nová položka';
+$string['refresh'] = 'Počet dnů předtím, než je vybrána nová položka';
 $string['select_glossary'] = 'Vybírej položky ze slovníku';
-$string['showconcept'] = 'Zobrazuj pojem (hlavičku) pro každou položku';
+$string['showconcept'] = 'Zobrazuj pojem (heslové slovo) pro každou položku';
 $string['title'] = 'Titulek';
 $string['type'] = 'Jak je vybírána nová položka';
-$string['typehelp'] = '<b>Poslední upravená položka</b> vždy zobrazuje poslední editovanou položku, zatímco <b>Náhodná položka</b> ji pokaždé náhodně vybírá. Volba <b>Další položka</b> prochází slovníkem podle pořadí položek. Tato poslední volba je obzvláště užitečná, pokud rovněž nastavíte počet dnů. Tak si můžete vytvořit blok obsahující např. <i>Citát týdne</i> či <i>Tip pro dnešní den</i>.';
-$string['viewglossary'] = 'Více citátů...';
-$string['whichfooter'] = 'Můžete nechat zobrazovat odkazy na akce slovníku, s nímž je tento blok asociován. Blok bude zobrazovat pouze ty akce, které jsou v daném slovníku povoleny.';
+$string['typehelp'] = 'Při volbě <b>Poslední upravená položka</b> se vždy zobrazuje poslední editovaná položka, při volbě <b>Náhodná položka</b> se položka pokaždé náhodně vybírá. Volba <b>Další položka</b> prochází slovníkem podle pořadí položek. Tato poslední volba je obzvláště užitečná, pokud rovněž nastavíte počet dnů. Tak si můžete vytvořit blok obsahující např. <i>Citát týdne</i> či <i>Tip pro dnešní den</i>.';
+$string['viewglossary'] = 'Další hesla...';
+$string['whichfooter'] = 'Můžete nechat zobrazovat odkazy na akce slovníku, s nímž je tento blok propojen. Blok bude zobrazovat pouze ty akce, které jsou v daném slovníku povoleny.';
 
 ?>
