1eaface50d68acf2d656a77eb3f2c9ed29a2e2fc
David Mudrak <david.mudrak@gmail.com>
2006-05-17 08:57:16 +0000

    Added for 1.6 release

---
+++ cs_utf8/my.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // my.php - created with Moodle 1.7 dev (2006050300)
+
+
+$string['mymoodle'] = 'Můj Moodle';
+$string['nocourses'] = 'Žádné dostupné informace o kurzech';
+$string['noguest'] = 'Stránka \'Můj Moodle\' není hostům k dispozici';
+$string['pinblocks'] = 'Nastavení povinných bloků pro Můj Moodle';
+$string['pinblocksexplan'] = 'Zde můžete nastavit libovolné postranní bloky, které se mají uživatelům ukazovat na jejich stránce \'Můj Moodle\'. Uživatelé nebudou moci měnit nastavení těchto povinných bloků.';
+
+?>
