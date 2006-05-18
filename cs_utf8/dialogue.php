b37aed24715229da84b2ac741d0e3904f2d3d5e5
Daniel Mikšík <miksik>
2006-05-18 11:35:18 +0000

    Three additions and two small changes

--- cs_utf8/dialogue.php
+++ cs_utf8/dialogue.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // dialogue.php - created with Moodle 1.5 + (2005060201)
+      // dialogue.php - created with Moodle 1.6 Beta 5 (2006050500)
 
 
 $string['addmynewentries'] = 'Přidat mé nové příspěvky';
@@ -9,18 +9,19 @@ $string['allowmultiple'] = 'Umožnit více než jeden rozhovor se stejnou osobou
 $string['allowstudentdialogues'] = 'Umožnit rozhovory mezi studenty';
 $string['close'] = 'Uzavřít';
 $string['closed'] = 'Uzavřeno';
+$string['closeddialogues'] = 'Uzavřené rozhovory';
 $string['confirmclosure'] = 'Chystáte se uzavřít rozhovor s osobou $a.
 Uzavřené rozhovory nemohou být znovu otevřeny. Po uzavření si můžete
 proběhnutý rozhovor prohlédnout, nemůžete však do něj již nic přidávat a pro pokračování dialogu budete muset začít nový rozhovor.<br /><br />Jste si jisti, že chcete uzavřít tento rozhovor?';
 $string['deleteafter'] = 'Odstranit uzavřené rozhovory po (počet dnů)';
 $string['dialogueclosed'] = 'Rozhovor uzavřen';
 $string['dialogueintro'] = 'Úvod do rozhovoru';
-$string['dialoguemail'] = '$a->userfrom vlozil novy prispevek do vaseho rozhovoru \'$a->dialogue\'.
+$string['dialoguemail'] = '$a->userfrom vlozil(a) novy prispevek do vaseho rozhovoru \'$a->dialogue\'.
 
 Muzete si jej prohlednout na strance rozhovoru:
 
 $a->url';
-$string['dialoguemailhtml'] = '$a->userfrom vložil nový příspěvek do vašeho rozhovoru \'<i>$a->dialogue</i>\'.<br /><br />
+$string['dialoguemailhtml'] = '$a->userfrom vložil(a) nový příspěvek do vašeho rozhovoru \'<i>$a->dialogue</i>\'.<br /><br />
 
 Můžete si jej prohlédnout na stránce <a href=\"$a->url\">rozhovoru</a>.';
 $string['dialoguename'] = 'Název rozhovoru';
@@ -39,6 +40,7 @@ $string['newentry'] = 'Nový příspěvek';
 $string['noavailablepeople'] = 'Není zde nikdo, s kým byste mohli vést rozhovor.';
 $string['nopersonchosen'] = 'Není vybrána žádná osoba';
 $string['nosubject'] = 'Nebyl vložen žádný předmět';
+$string['notavailable'] = 'Hostům nejsou rozhovory přístupné.';
 $string['notextentered'] = 'Nebyl vložen žádný text';
 $string['notstarted'] = 'Zatím jste nepřispěli do tohoto rozhovoru';
 $string['notyetseen'] = 'Zatím neprohlíženo';
@@ -51,6 +53,7 @@ $string['open'] = 'Otevřít';
 $string['openadialoguewith'] = 'Otevřít rozhovor s';
 $string['opendialogue'] = 'Otevřít rozhovor';
 $string['opendialogueentries'] = 'Otevřít příspěvky rozhovoru';
+$string['opendialogues'] = 'Otevřené rozhovory';
 $string['pane0'] = 'Otevřít rozhovor';
 $string['pane1'] = '$a rozhovorů čeká na vaše odpovědi';
 $string['pane1one'] = '1 rozhovor čeká na vaši odpověď';
@@ -66,7 +69,7 @@ $string['subject'] = 'Předmět';
 $string['subjectadded'] = 'Přidán předmět';
 $string['teachertostudent'] = 'Mezi učitelem a studentem';
 $string['typefirstentry'] = 'Sem vložte první příspěvek';
-$string['typefollowup'] = 'Sem vložte následující';
+$string['typefollowup'] = 'Sem vložte další příspěvek';
 $string['typeofdialogue'] = 'Typ rozhovoru';
 $string['typereply'] = 'Sem vložte odpověď';
 $string['viewallentries'] = 'Zobrazit $a příspěvků';
