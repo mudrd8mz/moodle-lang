e80e32fbec67cd77125040865a6dd5426633b1e0
koen roggemans <koenr>
2006-05-19 19:28:27 +0000

    new doc files from Karoly

---
+++ hu_utf8/docs/README.txt
@@ -0,0 +1,45 @@
+A Moodle dokumentációja
+--------------------
+
+Ha a dokumentációt egy másik nyelvre kívánja lefordítani,
+ajánlatos a következőket megfogadni:
+
+ 1) ne tegye az angol állományokat ebbe a könyvtárba, mert ha valamelyik hiányzik a nyelvi
+    csomagjából, helyette úgyis automatikusan az angolt használja a rendszer.
+
+ 2) a fordítást az alábbi sorrendben végezze (kezdve a fontosakkal):
+  
+    LEGFONTOSABBAK
+    |-------------------
+    |
+    |   - files.php
+    |   - install.html
+    |   - installamp.html
+    |   - upgrade.html
+    |   - faq.html
+    |
+    |  -----------------
+    |
+    |   - teacher.html
+    |   - module_files.txt
+    | 
+    |  -----------------
+    |   
+    |   - intro.html
+    |   - features.html
+    |   - release.html
+    |
+    |  -----------------
+    |
+    |   - developer.html
+    |   - cvs.html
+    |   - future.html
+    |
+    |  -----------------
+    |   
+    |   - license.html
+    |
+    |-------------------
+    LEGKEVÉSBÉ FONTOSAK
+
+ 3) Ne fordítsa le a credits.html-t: túl gyakran változik.
---
+++ hu_utf8/docs/files.php
@@ -0,0 +1,31 @@
+<?php // $Id$
+      // Names of the documentation files
+      // Files with a definition "-" are treated as spacers
+
+$string['-about'] = "A Moodle-ról";
+$string['intro.html'] = "Bevezetés";
+$string['background.html'] = "Háttér";
+$string['philosophy.html'] = "Filozófia";
+$string['licence.html'] = "Licenc";
+$string['features.html'] = "Jellemzők";
+$string['release.html'] = "Verziókkal kapcsolatos jegyzetek";
+$string['future.html'] = "A jövő";
+$string['credits.html'] = "Köszönetnyilvánítás";
+
+$string['-installation'] = "Adminisztráció";
+$string['install.html'] = "Telepítés";
+$string['faq.html'] = "Telepítési gyakran feltett kérdések";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Frissítés";
+
+$string['-usage'] = "A Moodle használata";
+$string['teacher.html'] = "Tanári kézikönyv";
+$string['other.html'] = "Egyéb dokumentumok";
+
+$string['-development'] = "Fejlesztés";
+$string['developer.html'] = "Fejlesztői kézikönyv";
+$string['coding.html'] = "Programozási útmutató";
+$string['cvs.html'] = "A CVS használata";
+$string['translation.html'] = "Fordítás";
+
+?>
