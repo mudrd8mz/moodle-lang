c9f8bd21c78ac9e809536e9d98ad769c19589e41
Daniel Mikšík <miksik>
2006-05-19 16:55:44 +0000

    Initial translation by the Brno translation group

---
+++ cs_utf8/blog.php
@@ -0,0 +1,55 @@
+<?PHP // $Id$ 
+      // blog.php - created with Moodle 1.6 Beta 5 (2006050500)
+
+
+$string['addnewentry'] = 'Vložit nový příspěvek';
+$string['addotags'] = 'Přidat globální klíčová slova';
+$string['addptags'] = 'Přidat uživatelská klíčová slova';
+$string['blockmenutitle'] = 'Nástroje blogu';
+$string['blocktagstitle'] = 'Blog – klíčová slova';
+$string['blocktitle'] = 'Nadpis pro blok s klíčovými slovy blogovacího systému';
+$string['blog'] = 'Blog';
+$string['blogdeleteconfirm'] = 'Smazat tento příspěvek?';
+$string['blogpreferences'] = 'Nastavení blogu';
+$string['blogs'] = 'Blogy';
+$string['blogtags'] = 'Klíčová slova';
+$string['courseblog'] = 'Blog kurzu: $a';
+$string['courseblogs'] = 'Uživatelé smějí číst pouze blogy účastníků stejného kurzu';
+$string['disableblogs'] = 'Zcela deaktivovat blogovací systém';
+$string['entrybody'] = 'Text příspěvku';
+$string['entrybodyonlydesc'] = 'Popis příspěvku';
+$string['entryerrornotyours'] = 'Tento příspěvek není váš';
+$string['entrytitle'] = 'Titulek příspěvku';
+$string['entryupdated'] = 'Příspěvek byl aktualizován';
+$string['groupblog'] = 'Blog skupiny: $a';
+$string['groupblogs'] = 'Uživatelé smějí číst pouze blogy uživatelů ze stejné skupiny';
+$string['intro'] = 'Tento RSS zdroj byl automaticky vygenerován z jednoho či více blogů.';
+$string['noentriesyet'] = 'Nejsou k dispozici žádné zveřejněné příspěvky';
+$string['noguestpost'] = 'Hosté nemohou psát do blogu!';
+$string['norighttodeletetag'] = 'Nemáte oprávnění ke smazání klíčového slova $a';
+$string['notallowedtoedit'] = 'Nemáte oprávnění upravovat tento příspěvek';
+$string['numberofentries'] = 'Příspěvky: $a';
+$string['numberoftags'] = 'Počet zobrazovaných klíčových slov';
+$string['otags'] = 'Globální klíčová slova';
+$string['pagesize'] = 'Počet příspěvků na stránku';
+$string['personalblogs'] = 'Uživatelé smějí číst pouze svůj vlastní blog';
+$string['ptags'] = 'Uživatelská klíčová slova';
+$string['publishto'] = 'Zveřejnit';
+$string['publishtonoone'] = 'pouze vám (pracovní verze)';
+$string['publishtosite'] = 'všem uživatelům Moodlu';
+$string['publishtoworld'] = 'komukoliv v Internetu';
+$string['settingsupdatederror'] = 'Došlo k chybě, nastavení blogu nebylo možné aktualizovat.';
+$string['siteblog'] = 'Blog stránek: $a';
+$string['siteblogs'] = 'Všichni registrovaní uživatelé smějí číst všechny blogy';
+$string['tagdatelastused'] = 'Datum jako klíčové slovo bylo naposledy použito';
+$string['tagsort'] = 'Řadit klíčová slova podle';
+$string['tagtext'] = 'Klíčové slovo';
+$string['timewithin'] = 'Zobrazit klíčová slova použitá v poslední době (dny)';
+$string['updateentrywithid'] = 'Aktualizuji příspěvek';
+$string['userblog'] = 'Uživatelský blog: $a';
+$string['viewcourseentries'] = 'Číst blogy kurzu';
+$string['viewmyentries'] = 'Číst vlastní příspěvky';
+$string['viewsiteentries'] = 'Číst všechny blogy';
+$string['worldblogs'] = 'Návštěvníci stránek smějí číst záznamy zveřejněné \'komukoliv v Internetu\'';
+
+?>
---
+++ cs_utf8/currencies.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // currencies.php - created with Moodle 1.6 Beta 5 (2006050500)
+
+
+$string['CSK'] = 'CZK (českých korun)';
+$string['EUR'] = 'EUR (eur)';
+$string['USD'] = 'USD (amerických dolarů)';
+
+?>
