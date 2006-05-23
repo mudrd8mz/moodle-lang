7cfec9ec7e786bdf56d6098d2212df9d84d76724
Daniel Mikšík <miksik>
2006-05-23 22:21:19 +0000

    Added a few 1.6 strings + some style/word-order polishing

--- cs_utf8/enrol_flatfile.php
+++ cs_utf8/enrol_flatfile.php
@@ -1,21 +1,23 @@
 <?PHP // $Id$ 
-      // enrol_flatfile.php - created with Moodle 1.7 dev (2006050300)
+      // enrol_flatfile.php - created with Moodle 1.6 Beta 5 (2006050502)
 
 
 $string['description'] = 'Tato metoda bude opakovaně kontrolovat a zpracovávat speciálně formátovaný textový soubor, jehož umístění zde určíte.
-Řádky v souboru obsahují čtyři až šest polí oddělených čárkou podle následujícího formátu:
+Řádky v souboru obsahují čtyři až šest polí oddělených čárkou v následujícím formátu:
 <pre>
 * operation, role, idnumber(user), idnumber(course) [, starttime, endtime]
-where:
+</pre>
+kde:
+<pre>
 * operation = add | del
 * role = student | teacher | teacheredit
-* idnumber(user) = idnumber in the user table NB not id
-* idnumber(course) = idnumber in the course table NB not id
-* starttime = start time (in seconds since epoch) - optional
-* endtime = end time (in seconds since epoch) - optional
+* idnumber(user) = idnumber v tabulce uživatelů, NIKOLIV id
+* idnumber(course) = idnumber v tabulce kurzů, NIKOLIV id
+* starttime = čas zpřístupnění kurzu (unixové časové razítko) - volitelné
+* endtime = čas vyškrtnutí z kurzu (unixové časové razítko) - volitelné
 </pre>
 
-Soubor může mít strukturu podobnou téhle:
+Příklad souboru:
 <pre>
 add, student, 5, CF101
 add, teacher, 6, CF101
@@ -24,11 +26,11 @@ del, student, 8, CF101
 del, student, 17, CF101
 add, student, 21, CF101, 1091115000, 1091215000
 </pre>';
-$string['enrolname'] = 'Ze souboru';
-$string['filelockedmail'] = 'Textovy soubor, ktery pouzivate pro zapisy ($a), nemuze byt odstranen procesem cron. Vetsinou je to zpusobeno spatne nastavenymi pravy. Prosim, opravte prava tak, aby mohl Moodle tento soubor odstranit. Jinak muze dochazet k jeho opakovanemu zpracovani.';
-$string['filelockedmailsubject'] = 'Dulezita chyba: Soubor se zapisy';
-$string['location'] = 'Umítění souboru';
-$string['mailadmin'] = 'Upozornit správce emailem';
-$string['mailusers'] = 'Upozornit uživatele emailem';
+$string['enrolname'] = 'Textový soubor';
+$string['filelockedmail'] = 'Procesu cron se nepodarilo odstranit textovy soubor, ktery pouzivate pro zapisy ($a). Vetsinou je to zpusobeno spatne nastavenymi pravy. Prosim, opravte prava tak, aby mohl Moodle tento soubor odstranit. Jinak muze dochazet k jeho opakovanemu zpracovani.';
+$string['filelockedmailsubject'] = 'Zavazna chyba: Soubor se zapisy';
+$string['location'] = 'Cesta k souboru';
+$string['mailadmin'] = 'Upozornit správce e-mailem';
+$string['mailusers'] = 'Upozornit uživatele e-mailem';
 
 ?>
--- cs_utf8/enrol_paypal.php
+++ cs_utf8/enrol_paypal.php
@@ -1,10 +1,10 @@
 <?PHP // $Id$ 
-      // enrol_paypal.php - created with Moodle 1.5 + (2005060201)
+      // enrol_paypal.php - created with Moodle 1.6 Beta 5 (2006050502)
 
 
-$string['business'] = 'Emailová adresa vašeho obchodního Paypal účtu';
-$string['costorkey'] = 'Prosím, vyberte jeden z následujících způsobů zápisů do kurzu.';
-$string['description'] = 'Modul \"Platba přes Paypal\" vám umožňuje nastavovat placené kurzy. Je-li cena kurzu nastavena na nulu, nejsou zájemci o zapsání do kurzu vyzývání k žádné platbě. Můžete rovněž nastavit výchozí cenu všech kurzů na vašich stránkách a ceny jednotlivých kurzů pak nastavovat individuálně. Cena nastavená v kurzu má přednost před cenou nastavenou pro celé stránky.';
+$string['business'] = 'E-mailová adresa vašeho obchodního účtu Paypal';
+$string['costorkey'] = 'Vyberte prosím jeden z následujících způsobů zápisu do kurzů.';
+$string['description'] = 'Modul \'Platba přes Paypal\' vám umožňuje nastavovat placené kurzy. Je-li cena kurzu nastavena na nulu, nejsou zájemci o zapsání do kurzu vyzýváni k platbě. Můžete rovněž nastavit výchozí cenu všech kurzů na vašich stránkách a ceny jednotlivých kurzů pak nastavovat individuálně. Cena nastavená v kurzu má přednost před cenou nastavenou pro celé stránky.';
 $string['enrolname'] = 'Platba přes Paypal';
 $string['sendpaymentbutton'] = 'Poslat platbu přes službu Paypal';
 
