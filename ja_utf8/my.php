aa2718700ab8567cea44870dc59d500c06ec781f
Mitsuhiro Yoshida <mits>
2006-05-29 21:30:49 +0000

    Updated "my moodle" strings based on /lang/en_utf8 change.

--- ja_utf8/my.php
+++ ja_utf8/my.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // my.php - created with Moodle 1.6 Beta 3 (2006041100)
+      // my.php - created with Moodle 1.7 dev (2006052400)
 
 
-$string['mymoodle'] = 'マイMoodle';
+$string['mymoodle'] = 'マイコースのオーバービュー';
 $string['nocourses'] = '表示するコース情報がありません。';
-$string['noguest'] = 'ゲストユーザは「マイMoodle」ページを利用できません。';
+$string['noguest'] = 'ゲストユーザは「コースオーバービュー」ページを利用できません。';
 $string['pinblocks'] = 'マイMoodle固定ブロックの設定';
-$string['pinblocksexplan'] = 'あなたがここで設定したすべてのブロックは、すべてのMoodleユーザの「マイMoodle」画面に表示 ( 編集不可 ) されます。';
+$string['pinblocksexplan'] = 'あなたがここで設定したブロックは、すべてのMoodleユーザの「マイMoodle」オーバービューページに表示 ( 編集不可 ) されます。';
 
 ?>
