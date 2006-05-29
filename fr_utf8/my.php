d99bc650196636259a9993e6f4b4914f82702e46
Nicolas Martignoni <martignoni>
2006-05-29 11:51:38 +0000

    Updated some "my moodle" strings

--- fr_utf8/my.php
+++ fr_utf8/my.php
@@ -1,9 +1,9 @@
 <?php // $Id$
 
-$string['mymoodle'] = 'Mon moodle';
+$string['mymoodle'] = 'Vue d\'ensemble de mes cours';
 $string['nocourses'] = 'Aucune information de cours à afficher.';
-$string['noguest'] = 'La page «&nbsp;Mon Moodle&nbsp;» n\'est pas disponible pour les invités';
+$string['noguest'] = 'La page «&nbsp;Vue d\'ensemble des cours&nbsp;» n\'est pas disponible pour les invités';
 $string['pinblocks'] = 'Configurer les blocs fixes de la page «&nbsp;Mon moodle&nbsp;»';
-$string['pinblocksexplan'] = 'Les blocs que vous configurez ici seront visibles sur la page «&nbsp;Mon moodle&nbsp;» de chaque utilisateur. Ils ne pourront être modifiés par les utilisateurs.';
+$string['pinblocksexplan'] = 'Les blocs que vous configurez ici seront visibles sur la page «&nbsp;Vue d\'ensemble des cours&nbsp;» de chaque utilisateur. Ils ne pourront être modifiés par les utilisateurs.';
 
 ?>
