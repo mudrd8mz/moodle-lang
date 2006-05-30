02825f0534c6e71ec515e8da71a07d5fc6272fdf
David Mudrak <david.mudrak@gmail.com>
2006-05-30 14:50:49 +0000

    Translated from scratch for 1.6

---
+++ cs_utf8/mimetypes.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // mimetypes.php - created with Moodle 1.7 dev (2006050300)
+
+
+$string['application/msword'] = 'Dokument aplikace Word';
+$string['application/pdf'] = 'PDF dokument';
+$string['application/vnd.ms-excel'] = 'Sešit aplikace Excel';
+$string['application/vnd.ms-powerpoint'] = 'Prezentace Powerpoint';
+$string['application/zip'] = 'ZIP archív';
+$string['audio/mp3'] = 'MP3 audio';
+$string['audio/wav'] = 'WAV audio';
+$string['document/unknown'] = 'soubor';
+$string['image/bmp'] = 'BMP obrázek (bez komprese)';
+$string['image/gif'] = 'GIF obrázek';
+$string['image/jpeg'] = 'JPEG obrázek';
+$string['text/plain'] = 'textový soubor';
+$string['text/rtf'] = 'RTF dokument';
+
+?>
---
+++ cs_utf8/quiz_overview.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // quiz_overview.php - created with Moodle 1.7 dev (2006050300)
+
+
+$string['allattempts'] = 'Ukázat všechny pokusy';
+$string['allstudents'] = '$a - ukázat vše';
+$string['attemptsonly'] = '$a - ukázat pouze ty s alespoň jedním pokusem';
+$string['noattemptsonly'] = '$a - ukázat pouze ty bez jediného pokusu';
+$string['overview'] = 'Přehled';
+
+?>
