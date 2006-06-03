edac525daf6d0fa9b843fbcf44b4155e2c22d376
Benito Arias <barias>
2006-06-03 09:08:33 +0000

    *** empty log message ***

--- es_utf8/assignment.php
+++ es_utf8/assignment.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.6 Beta 5 (2006050500)
+      // assignment.php - created with Moodle 1.7 dev (2006052400)
 
 
 $string['allowresubmit'] = 'Permitir reenvío';
@@ -29,9 +29,16 @@ para \'$a->assignment\'
 Está disponible aquí:
 
 $a->url';
+$string['emailteachermailgroup'] = 'El grupo $a->groupname $a->groupmembers ha actualizado el envío de su tarea \'$a->assignment\'
+
+Está disponible aquí:
+
+$a->url';
 $string['emailteachermailhtml'] = '$a->username ha actualizado el envío de su tarea
 para <i>\'$a->assignment\'</i><br /><br />
 Está <a href=\"$a->url\">disponible en el sitio web</a>.';
+$string['emailteachermailhtmlgroup'] = 'El grupo $a->groupname $a->groupmembers ha actualizado el envío de su tarea <i>\'$a->assignment\'</i><br /><br />
+Está disponible en el sitio web <a href=\"$a->url\"></a>.';
 $string['emailteachers'] = 'Alertas de email a los profesores';
 $string['emptysubmission'] = 'Usted aún no ha enviado nada';
 $string['existingfiledeleted'] = 'Se ha borrado el archivo: $a';
@@ -40,11 +47,13 @@ $string['feedback'] = 'Comentario';
 $string['feedbackfromteacher'] = 'Comentarios del $a';
 $string['feedbackupdated'] = 'Comentario actualizado para $a personas';
 $string['graded'] = 'Calificado';
+$string['gradingstyle'] = 'Estilo de calificación';
 $string['guestnosubmit'] = 'Lo sentimos, los invitados no pueden enviar tareas. Para poder enviar su respuesta, antes tiene que registrarse o introducir sus datos de acceso.';
 $string['guestnoupload'] = 'Lo sentimos, los invitados no pueden realizar envíos.';
 $string['helpoffline'] = '<p>Esto resulta útil cuando la tarea se realiza fuera de Moodle, e.g., en algún lugar de internet o personalmente.</p><p>Los estudiantes pueden ver una descipción de la tarea, pero no pueden subir archivos de ninguna clase al servidor. Las calificaciones funcionan normalmente, y los estudiantes recibirán notificaciones sobre la calificación obtenida.</p>';
 $string['helponline'] = '<p>Esta clase de tareas pide a los usuarios que editen un texto, utilizando las herramientas de edición habituales. Los profesores pueden calificarlas en línea, e incluso incorporar comentarios o cambios.</p><p>(Si usted está familiarizado con versiones anteriores de Moodle, este tipo de tarea hace lo mismo que hacía el módulo Diario)</p>';
 $string['helpuploadsingle'] = '<p>Este tipo de tarea permite a los participantes subir un solo archivo de cualquier tipo.</p><p>Podría ser un documento procesado en Word, o una imagen, un sitio web comprimido, o cualquier cosa que les pida que envíen.</p>';
+$string['helpuploadsinglegroup'] = '<p>Este tipo de tarea permite que cada participante suba un solo archivo, de cualquier tipo.</p> <p>Podría ser un documento hecho con Word, una imagen, una web comprimida, o cualquier otra cosa.</p> <p>El envío de este usuario es también remitido por todos los miembros del grupo a que dicho usuario pertenece. Si el curso no tiene grupos, toda la clase se considera como un grupo.</p>';
 $string['late'] = '$a después';
 $string['maximumgrade'] = 'Calificación máxima';
 $string['maximumsize'] = 'Tamaño máximo';
@@ -70,6 +79,7 @@ $string['submitted'] = 'Enviada';
 $string['typeoffline'] = 'Actividad no en línea';
 $string['typeonline'] = 'Texto en línea';
 $string['typeuploadsingle'] = 'Subir un solo archivo';
+$string['typeuploadsinglegroup'] = 'Subida de archivo en equipo';
 $string['uploadbadname'] = 'El nombre contiene caracteres incompatibles y no se pudo subir';
 $string['uploadedfiles'] = 'archivos subidos';
 $string['uploaderror'] = 'Ha ocurrido un error al guardar el archivo en el servidor';
@@ -80,6 +90,7 @@ $string['uploadnotregistered'] = '\'$a\' se subió correctamente, pero el envío
 $string['uploadsuccess'] = '\'$a\' se ha subido correctamente';
 $string['viewfeedback'] = 'Ver calificaciones y comentarios sobre la tarea';
 $string['viewsubmissions'] = 'Ver $a tareas enviadas';
+$string['wholeclass'] = 'Toda la clase';
 $string['yoursubmission'] = 'Su envío';
 
 ?>
