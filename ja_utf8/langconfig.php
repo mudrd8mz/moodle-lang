2177226005b9f8aa27ea2a8977845625e9d4def1
Mitsuhiro Yoshida <mits>
2006-06-17 00:29:59 +0000

    Changed strftime layout based on ja_utf8/moodle.php change.

--- ja_utf8/langconfig.php
+++ ja_utf8/langconfig.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // langconfig.php - created with Moodle 1.6 development (2006032200)
+      // langconfig.php - created with Moodle 1.7 dev (2006060900)
 
 
 $string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
@@ -9,15 +9,15 @@ $string['locale'] = 'ja_JP.UTF-8';
 $string['localewin'] = 'Japanese_Japan.932';
 $string['localewincharset'] = 'CP932';
 $string['oldcharset'] = 'EUC-JP';
-$string['strftimedate'] = '%%Y年 %%m月 %%d日 ';
+$string['strftimedate'] = '%%Y年 %%m月 %%d日';
 $string['strftimedateshort'] = '%%m/%%d';
-$string['strftimedatetime'] = '%%Y年 %%m月 %%d日, %%H:%%M';
+$string['strftimedatetime'] = '%%Y年 %%m月 %%d日 %%H:%%M';
 $string['strftimedaydate'] = '%%Y年 %%m月 %%d日(%%A)';
 $string['strftimedaydatetime'] = '%%Y年 %%m月 %%d日(%%A) %%H:%%M';
-$string['strftimedayshort'] = '%%Y年 %%m月 %%d日 ';
-$string['strftimedaytime'] = '(%%a)　%%H:%%M';
-$string['strftimemonthyear'] = '%%Y年 %%m月 ';
-$string['strftimerecent'] = '%%m月 %%d日, %%H:%%M';
+$string['strftimedayshort'] = '%%Y年 %%m月 %%d日';
+$string['strftimedaytime'] = '(%%a) %%H:%%M';
+$string['strftimemonthyear'] = '%%Y年 %%m月';
+$string['strftimerecent'] = '%%m月 %%d日 %%H:%%M';
 $string['strftimerecentfull'] = '%%Y年 %%m月 %%d日(%%a) %%H:%%M';
 $string['strftimetime'] = '%%H:%%M';
 $string['thischarset'] = 'UTF-8';
