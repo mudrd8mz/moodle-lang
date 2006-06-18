015b74f990b4bf5a9718bd5f7e93f406fb1a56f1
Mitsuhiro Yoshida <mits>
2006-06-18 20:40:46 +0000

    Brushed up translated strings for blog.

--- ja_utf8/blog.php
+++ ja_utf8/blog.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // blog.php - created with Moodle 1.7 dev (2006052400)
+      // blog.php - created with Moodle 1.7 dev (2006060900)
 
 
 $string['addnewentry'] = '新しいエントリの追加';
@@ -14,7 +14,7 @@ $string['blogpreferences'] = 'ブログ設定';
 $string['blogs'] = 'ブログ';
 $string['blogtags'] = 'ブログタグ';
 $string['courseblog'] = 'コースブログ: $a';
-$string['courseblogs'] = 'ユーザは、コースを共有しているユーザのブログのみ読むことができます。';
+$string['courseblogs'] = 'ユーザはコースを共有しているユーザのブログのみ読むことができる';
 $string['disableblogs'] = 'ブログシステムを完全に無効にする';
 $string['entrybody'] = 'ブログエントリ本文';
 $string['entrybodyonlydesc'] = 'エントリ説明';
@@ -23,7 +23,7 @@ $string['entrysaved'] = 'あなたのエントリが保存されました。';
 $string['entrytitle'] = 'エントリタイトル';
 $string['entryupdated'] = 'ブログエントリが更新されました。';
 $string['groupblog'] = 'グループブログ: $a';
-$string['groupblogs'] = 'ユーザは、グループを共有しているユーザのブログのみ読むことができます。';
+$string['groupblogs'] = 'ユーザはグループを共有しているユーザのブログのみ読むことができる';
 $string['intro'] = 'このRSSフィードは、１つまたはそれ以上のブログから自動的に生成されました。';
 $string['noentriesyet'] = '表示できるエントリはありません。';
 $string['noguestpost'] = 'ゲストはブログを投稿できません!';
@@ -33,24 +33,24 @@ $string['numberofentries'] = 'エントリ: $a';
 $string['numberoftags'] = '表示するタグ数';
 $string['otags'] = 'オフィシャルタグ';
 $string['pagesize'] = 'ページあたりのブログエントリ数';
-$string['personalblogs'] = 'ユーザは、自分のブログのみ読むことができます。';
+$string['personalblogs'] = 'ユーザは自分のブログのみ読むことができる';
 $string['ptags'] = 'ユーザ定義タグ';
-$string['publishto'] = '公開先: ';
+$string['publishto'] = '公開先:';
 $string['publishtonoone'] = 'あなたのみ閲覧可 ( 下書き )';
 $string['publishtosite'] = 'このサイトの誰でも閲覧可';
 $string['publishtoworld'] = '世界中の誰でも閲覧可';
 $string['settingsupdatederror'] = 'エラーが発生したため、ブログ設定は更新されませんでした。';
 $string['siteblog'] = 'サイトブログ: $a';
-$string['siteblogs'] = 'すべてのサイトユーザはすべてのブログを読むことができます。';
+$string['siteblogs'] = 'すべてのサイトユーザはすべてのブログを読むことができる';
 $string['tagdatelastused'] = '日付タグは使用されています。';
 $string['tagsort'] = 'タグの並び替え';
 $string['tagtext'] = 'タグテキスト';
-$string['timewithin'] = '指定した期間で使用されたタグを表示';
+$string['timewithin'] = '指定した期間に使用されたタグを表示';
 $string['updateentrywithid'] = 'エントリの更新';
 $string['userblog'] = 'ユーザブログ: $a';
 $string['viewcourseentries'] = 'コースエントリの表示';
 $string['viewmyentries'] = 'マイエントリの表示';
 $string['viewsiteentries'] = 'サイトエントリの表示';
-$string['worldblogs'] = '「世界中の誰でも」に設定すると、世界中の人々がエントリを読むことができます。';
+$string['worldblogs'] = '世界中の誰でもエントリを読むことができる';
 
 ?>
