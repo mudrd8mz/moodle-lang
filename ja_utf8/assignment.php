00e87b2f0c182a5ace279dbc3ba6ab48db98bf4b
Mitsuhiro Yoshida <mits>
2006-07-13 10:52:45 +0000

    Translated new strings for assignment. Brushed up a translated string.

--- ja_utf8/assignment.php
+++ ja_utf8/assignment.php
@@ -2,6 +2,7 @@
       // assignment.php - created with Moodle 1.7 dev (2006060900)
 
 
+$string['allowmultiple'] = '複数ファイルを許可する';
 $string['allowresubmit'] = '課題の再提出を許可する';
 $string['assignmentdetails'] = '課題の詳細';
 $string['assignmentmail'] = '$a->teacher があなたの課題「 $a->assignment 」に対するフィードバックを登録しました。
@@ -12,16 +13,25 @@ $a->url';
 $string['assignmentmailhtml'] = '$a->teacher があなたの課題「 <i>$a->assignment</i> 」 に対する提出課題にフィードバックを登録しました。<br /><br />フィードバックはあなたの<a href=\"$a->url\">提出課題</a>に追加されています。';
 $string['assignmentname'] = '課題名';
 $string['assignmenttype'] = '課題タイプ';
+$string['attachfile'] = 'ファイルを添付する';
+$string['attachfiletoassignment'] = 'ファイルを課題に添付する';
 $string['availabledate'] = '開始日時';
+$string['backtoassignment'] = '課題に戻る';
+$string['backtofeedback'] = 'フィードバックに戻る';
+$string['choosereviewfile'] = '課題レビューとしてアップロードするファイルを選択してください:';
 $string['comment'] = 'コメント';
 $string['commentinline'] = 'インラインコメント';
 $string['configitemstocount'] = 'オンライン課題でカウントする学生の提出課題の単位';
 $string['configmaxbytes'] = 'すべての課題に関するデフォルト最大サイズ ( コース制限および他のローカル設定に従います )';
+$string['deletecheckfile'] = '本当にこのファイルを削除してもよろしいですか?';
+$string['deleteednotification'] = 'ファイルが削除されました。';
+$string['deletefail'] = '次のファイルは削除されませんでした:';
 $string['description'] = '詳細';
 $string['duedate'] = '終了日時';
 $string['duedateno'] = '提出期限なし';
 $string['early'] = '$a 早く提出';
 $string['editmysubmission'] = '提出課題を編集';
+$string['emailstudents'] = '学生にメール通知する';
 $string['emailteachermail'] = '$a->username が「 $a->assignment 」の提出課題を更新しました。
 
 下記にて閲覧可能です:
@@ -41,33 +51,54 @@ $string['guestnosubmit'] = '申し訳ございません、ゲストは課題を�
 $string['guestnoupload'] = '申し訳ございません、ゲストはアップロードできません。';
 $string['helpoffline'] = '<p>このタイプの課題は、Moodleの外で課題が行われるときに便利です。他のウェブサイト上での課題、対面により課される課題が考えられます。</p>
 <p>学生は課題の説明を読むことはできますが、ファイル等をアップロードすることはできません。評定は通常どおり動作し、評定に関する通知メールが学生宛に送信されます。</p>';
-$string['helponline'] = '<p>このタイプの課題は、ユーザに通常の編集ツールを使用したテキストの編集を求めます。教師はオンラインでこれらを評定でき、インラインコメントの追加・変更を行うこともできます。</p>
+$string['helponline'] = '<p>このタイプの課題では、各参加者に通常の編集ツールを使用したテキストの編集を求めます。教師はオンラインでこれらを評定でき、インラインコメントの追加・変更を行うこともできます。</p>
 <p>(  あなたが古いバージョンのMoodleに慣れているのでしたら、このタイプの課題は古い日誌モジュールと同じように動作すると考えてください。 )</p>';
+$string['helpupload'] = '<p>このタイプの課題では、各参加者が1つまたは複数のあらゆる種類のファイルをアップロードすることができます。</p>
+<p>ワードプロセッサ文書、イメージ、ZIP圧縮したウェブサイト、その他あなたが参加者に提出するよう求めたファイルです。</p>
+<p>あなたが、あらゆる種類のレスポンスファイルをアップロードすることもできます。</p>';
 $string['helpuploadsingle'] = '<p>このタイプの課題では、各参加者があらゆる種類の単一ファイルをアップロードすることができます。</p> <p>ワードプロセッサ文書、イメージ、ZIP圧縮したウェブサイト、その他あなたが参加者に提出するよう求めたファイルです。</p>';
 $string['late'] = '$a 遅く提出';
+$string['markingsubmitnotification'] = '採点のためファイルが正常に送信されました。';
 $string['maximumgrade'] = '最大評価';
 $string['maximumsize'] = '最大サイズ';
 $string['modulename'] = '課題';
 $string['modulenameplural'] = '課題';
+$string['namedeletefile'] = 'あなたはこのファイルを削除しようとしています:';
 $string['newsubmissions'] = '課題が提出されました。';
 $string['noassignments'] = '課題はまだありません。';
 $string['noattempts'] = 'この課題はまだ提出されていません。';
+$string['nofilesforsubmit'] = '提出ファイルが見つかりませんでした - 本当にファイルをアップロードしましたか?';
 $string['notgradedyet'] = '未評価';
 $string['notsubmittedyet'] = '未提出';
+$string['onceassignmentsent'] = '採点のため課題を提出すると、あなたはファイルを削除または添付することができません。';
 $string['overwritewarning'] = '注意: 再度アップロードすることにより現在の提出課題は置き換えられます。';
 $string['pagesize'] = '1ページあたりの提出課題数';
 $string['preventlate'] = '提出期限後の課題提出を許可しない';
 $string['quickgrade'] = 'クイック評定を許可';
+$string['removelink'] = '削除';
+$string['responsefile'] = 'レスポンスファイル:';
+$string['responsesfromteacher'] = '教師によりアップロードされたレスポンス';
 $string['saveallfeedback'] = 'フィードバックを保存する';
+$string['sendformarking'] = '採点のため送信する';
+$string['step1'] = 'ステップ 1:';
+$string['step2'] = 'ステップ 2:';
 $string['submission'] = '提出課題';
 $string['submissionfeedback'] = '提出課題へのフィードバック';
 $string['submissions'] = '提出課題';
 $string['submissionsaved'] = '変更内容が保存されました。';
 $string['submissionsnotgraded'] = '$a 件の提出課題が評定されていません。';
+$string['submissionstatus'] = '課題提出ステータス:';
+$string['submissionstatusblank'] = '未提出';
+$string['submissionstatusdraft'] = '下書き';
+$string['submissionstatusmarked'] = '採点済み';
+$string['submissionstatusreturned'] = '返却済み';
+$string['submissionstatussubmitted'] = '提出済み';
 $string['submitassignment'] = 'このフォームを使用して課題を提出する';
+$string['submitformarking'] = '採点のため課題を提出する';
 $string['submitted'] = '提出';
 $string['typeoffline'] = 'オフライン活動';
 $string['typeonline'] = 'オンラインテキスト';
+$string['typeupload'] = 'ファイルの高度なアップロード';
 $string['typeuploadsingle'] = '単一ファイルのアップロード';
 $string['uploadbadname'] = '不正な文字がファイル名に含まれているため、このファイルをアップロードできませんでした。';
 $string['uploadedfiles'] = 'ファイルをアップロードしました。';
@@ -77,6 +108,7 @@ $string['uploadfiletoobig'] = '申し訳ございません、ファイルサイ�
 $string['uploadnofilefound'] = 'ファイルが見つかりませんでした - アップロードするファイルを選択しましたか?';
 $string['uploadnotregistered'] = '「 $a 」は正常にアップロードされましたが、登録されませんでした!';
 $string['uploadsuccess'] = '「 $a 」のアップロードが正常に完了しました。';
+$string['uploadsuccessresponse'] = 'レスポンスファイルが正常にアップロードされました。';
 $string['viewfeedback'] = '提出課題の評価とフィードバックを確認する';
 $string['viewsubmissions'] = '$a 件の提出課題を確認する';
 $string['yoursubmission'] = 'あなたの提出課題';
