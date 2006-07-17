b7a13fef176a801a79cc6a641e1c83eb7287902b
Ethem Evlice <ethem>
2006-07-17 17:20:44 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/assignment.php
+++ tr_utf8/assignment.php
@@ -1,27 +1,38 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.6 development (2006032200)
+      // assignment.php - created with Moodle 1.6 + (2006050506)
 
 
+$string['allowmultiple'] = 'Birden fazla dosyaya izin ver';
 $string['allowresubmit'] = 'Tekrar göndermeye izin ver';
 $string['assignmentdetails'] = 'Ödev ayrıntıları';
-$string['assignmentmail'] = '$a->teacher, \'$a->assignment\' isimli ödeviniz için geribildirim verdi
+$string['assignmentmail'] = '$a->teacher, \'$a->assignment\' isimli ödeviniz için geribildirim verdi.
+
+Bu bildirimi ödevinize eklenmiş olarak görebilirsiniz:
 
 $a->url';
-$string['assignmentmailhtml'] = '$a->teacher, \'<i>$a->assignment</i>\' isimli ödeviniz için geribildirim verdi.
-<br/><br/>
-<a href=\"$a->url\">Ayrıntılar</a>.';
+$string['assignmentmailhtml'] = '$a->teacher, \'<i>$a->assignment</i>\' isimli ödeviniz için geribildirim verdi.<br/><br/>
+Bu bildirimi <a href=\"$a->url\">ödevinize</a> eklenmiş olarak görebilirsiniz.';
 $string['assignmentname'] = 'Ödev adı';
 $string['assignmenttype'] = 'Ödev türü';
+$string['attachfile'] = 'Dosya iliştir';
+$string['attachfiletoassignment'] = 'Ödeve dosya iliştir';
 $string['availabledate'] = 'Başlangıç tarihi';
+$string['backtoassignment'] = 'Ödeve geri dön';
+$string['backtofeedback'] = 'Geribildirime geri dön';
+$string['choosereviewfile'] = 'Ödev incelemesi için gönderilecek bir dosya seçin';
 $string['comment'] = 'Yorum';
 $string['commentinline'] = 'Satıriçi yorum';
 $string['configitemstocount'] = 'Çevrimiçi ödevlerde öğrenci gönderileri için sayılacak tür';
 $string['configmaxbytes'] = 'Sitedeki tüm ödevler için varsayılan en yüksek ödev boyutu (ders limitleri ve diğer yerel ayarlar içindir)';
+$string['deletecheckfile'] = 'Bu dosyayı tamamen silmek istediğinizden emin misiniz?';
+$string['deleteednotification'] = 'Dosya silindi.';
+$string['deletefail'] = 'Aşağıdaki dosya silindi:';
 $string['description'] = 'Açıklama';
 $string['duedate'] = 'Teslim tarihi';
 $string['duedateno'] = 'Bitiş süresi yok';
 $string['early'] = '$a öncesi';
 $string['editmysubmission'] = 'Gönderimi düzenle';
+$string['emailstudents'] = 'Öğrencilere e-posta uyarısı';
 $string['emailteachermail'] = '$a->username, \'$a->assignment\'
 isimli gönderesini güncelledi.
 
@@ -32,15 +43,15 @@ $string['emailteachermailhtml'] = '$a->username, <i>\'$a->assignment\'</i><br />
 isimli gönderesini güncelledi.
 
 <a href=\"$a->url\">Buradan</a> ulaşabilirsiniz.';
-$string['emailteachers'] = 'Eğitmenlere eposta uyarısı';
+$string['emailteachers'] = 'Eğitimcilere e-posta uyarısı';
 $string['emptysubmission'] = 'Henüz bir şey göndermediniz';
 $string['existingfiledeleted'] = 'Varolan dosya silindi: $a';
-$string['failedupdatefeedback'] = 'Bu kullanıcının gönderdiği geribildirim güncellenemedi $a';
+$string['failedupdatefeedback'] = 'Bu kullanıcının, $a, gönderdiği geribildirim güncellenemedi';
 $string['feedback'] = 'Geribildirim';
 $string['feedbackfromteacher'] = '$a \'den geribildirim';
-$string['feedbackupdated'] = 'Bu kullanıcının gönderdiği geribildirim güncellenmiştir $a kişi';
+$string['feedbackupdated'] = '$a kişi için gönderi geribildirimleri güncellendi.';
 $string['graded'] = 'Notlandırıldı';
-$string['guestnosubmit'] = 'Üzgünüz, konukların ödev göndermesine izin verilmemektedir';
+$string['guestnosubmit'] = 'Üzgünüz, konukların ödev göndermesine izin verilmemektedir. Cevabınızı göndermeden önce giriş yapmalı veya kayıt olmalısınız.';
 $string['guestnoupload'] = 'Üzgünüz, konukların dosya göndermesine izin verilmemektedir';
 $string['helpoffline'] = '<p>Bu ödev türü site dışı bir ödev verdiğinizde kullanışlıdır.</p>
 <p>Öğrenciler ödev ile ilgili açıklamayı okurlar ancak herhangi bir dosya yükleyemezler.
@@ -49,33 +60,54 @@ her bir öğrenciye site üzerinden ödevleriyle ilgili not ve geribildirim vere
 $string['helponline'] = '<p>Bu ödev türünde normal yazı editörü kullanılarak kullanıcılardan ödev isteyebilirsiniz.</p>
 <p>Eğitimciler ödevleri düzenleyerek ve ödevlere satıriçi yorum ekleyerek ödevi çevrimiçi notlandırabilirler.</p>
 <p>(Bu uzaktan eğitim programının eski sürümünü kullanıyorsanız, bu ödev türünün eski Yazı ödevi ile aynı olduğunu göreceksiniz.)</p>';
+$string['helpupload'] = '<p>Bu ödev türü her bir katılımcının bir veya birden fazla dosya göndermesine izin verir.</p>
+<p>Bu, bir Word belgesi, resim, ziplenmiş web sayfası veya öğrencilerden ne tür bir dosya göndermelerini istediyseniz.</p>
+<p>Bu tür aynı zamanda herhangi bir dosya tipinde birden fazla yanıt yüklemenize olanak sağlar.</p>';
 $string['helpuploadsingle'] = '<p>Bu ödev türü tüm öğrencilerin herhangi bir tipte bir dosya göndermelerine izin verir.</p>
-<p>Bir Word belgesi, resim dosyası, zip veya öğrencilerden ne tür bir dosya göndermelerini istediyseniz o dosyayı göndermelerine izin verebilirsiniz.</p>';
+<p>Bir Word belgesi, resim dosyası, zip veya öğrencilerden ne tür bir dosya göndermelerini istediyseniz.</p>';
 $string['late'] = '$a sonrası';
+$string['markingsubmitnotification'] = 'Dosya işaretleme için başarılı bir şekilde gönderildi.';
 $string['maximumgrade'] = 'En yüksek not';
 $string['maximumsize'] = 'En yüksek boyut';
 $string['modulename'] = 'Ödev';
 $string['modulenameplural'] = 'Ödevler';
+$string['namedeletefile'] = 'Bu dosyayı silmek üzeresiniz:';
 $string['newsubmissions'] = 'Gönderilmiş Ödevler';
 $string['noassignments'] = 'Henüz bir ödev yok';
 $string['noattempts'] = 'Bu ödev için uygulama yapılmadı';
+$string['nofilesforsubmit'] = 'Göndermek için hiç bir dosya bulunamadı. Bir şeyler yüklediğinizden emin misiniz?';
 $string['notgradedyet'] = 'Henüz notlandırılmamış';
 $string['notsubmittedyet'] = 'Henüz gönderilmemiş';
+$string['onceassignmentsent'] = 'Ödev işaretleme için gönderilirse, artık, dosya silemez veya iliştiremezsiniz.';
 $string['overwritewarning'] = 'Uyarı: Yüklediğiniz dosyayla DEĞİŞTİRİLECEKTİR';
 $string['pagesize'] = 'Her sayfada görünen gönderi';
 $string['preventlate'] = 'Geç kalan gönderileri engelle';
 $string['quickgrade'] = 'Hızlı notlandırmaya izin ver';
+$string['removelink'] = 'Sil';
+$string['responsefile'] = 'Yanıt Dosyası:';
+$string['responsesfromteacher'] = 'Eğitimci Yanıt Dosyaları';
 $string['saveallfeedback'] = 'Tüm geribildirimlerimi kaydet';
+$string['sendformarking'] = 'İşaretleme için gönder';
+$string['step1'] = 'Adım  1:';
+$string['step2'] = 'Adım 2:';
 $string['submission'] = 'Gönderi';
 $string['submissionfeedback'] = 'Geribildirim Gönderimi';
 $string['submissions'] = 'Gönderiler';
 $string['submissionsaved'] = 'Değişiklikler kaydedildi';
 $string['submissionsnotgraded'] = '$a gönderisi notlandırılmadı';
+$string['submissionstatus'] = 'Gönderi durumu:';
+$string['submissionstatusblank'] = 'Boş';
+$string['submissionstatusdraft'] = 'Taslak';
+$string['submissionstatusmarked'] = 'İşaretlenmiş';
+$string['submissionstatusreturned'] = 'Geri dönmüş';
+$string['submissionstatussubmitted'] = 'İletilmiş';
 $string['submitassignment'] = 'Bu formu kullanarak ödevinizi gönderiniz';
-$string['submitted'] = 'Gönderilmiş';
-$string['typeoffline'] = 'Çevrimdışı Etkinlikler';
+$string['submitformarking'] = 'İşaretleme için ödev gönder';
+$string['submitted'] = 'İletilmiş';
+$string['typeoffline'] = 'Çevrimdışı etkinlik';
 $string['typeonline'] = 'Çevrimiçi yazı';
-$string['typeuploadsingle'] = 'Bir Dosya Yükle';
+$string['typeupload'] = 'Gelişmiş dosya yükleme';
+$string['typeuploadsingle'] = 'Tek dosya yükle';
 $string['uploadbadname'] = 'Dosya adı geçersiz karakterler içerdiğinden gönderilemedi.';
 $string['uploadedfiles'] = 'yüklenmiş dosyalar';
 $string['uploaderror'] = 'Sunucuya kaydederken hata oluştu';
@@ -84,8 +116,9 @@ $string['uploadfiletoobig'] = 'Bu dosya çok büyük (sınır $a byte\'dır)';
 $string['uploadnofilefound'] = 'Dosya bulunamadı - seçtiğinizden emin misiniz?';
 $string['uploadnotregistered'] = '\'$a\' yüklendi fakat gönderim kaydı yapılamadı!';
 $string['uploadsuccess'] = '\'$a\' başarıyla yüklendi';
-$string['viewfeedback'] = 'Ödevler notlarına ve geribildirimlere bak';
-$string['viewsubmissions'] = '$a\'ün gönderdiği ödevi aç';
+$string['uploadsuccessresponse'] = 'Yanıt dosyası başarılı bir şekilde yüklendi.';
+$string['viewfeedback'] = 'Ödev notlarına ve geribildirimlere bak';
+$string['viewsubmissions'] = '$a ödev gönderisine bak';
 $string['yoursubmission'] = 'Gönderiniz';
 
 ?>
