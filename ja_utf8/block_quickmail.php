9607eee707175ac3cc9711a79204262d92cdde4a
Mitsuhiro Yoshida <mits>
2006-07-27 21:01:32 +0000

    Translated a new string file for Quickmail.

---
+++ ja_utf8/block_quickmail.php
@@ -0,0 +1,37 @@
+<?PHP // $Id$ 
+      // block_quickmail.php - created with Moodle 1.7 dev (2006071800)
+
+
+$string['action'] = '操作';
+$string['allowstudents'] = '$a にクイックメールの使用を許可する';
+$string['attachment'] = '添付';
+$string['attachmentalt'] = 'メールに添付ファイルを追加する';
+$string['attachmenterror'] = '有効な添付ファイルではありません! 次のファイルは存在していません： <strong>$a</strong>';
+$string['attachmentoptional'] = '添付 ( 任意 )';
+$string['blockname'] = 'クイックメール';
+$string['check'] = 'すべてを選択';
+$string['clearhistory'] = '履歴のクリア';
+$string['composeemail'] = 'メールの作成';
+$string['composenew'] = '新しいメールの作成';
+$string['date'] = '日時';
+$string['delete'] = '削除';
+$string['deletefail'] = '削除が失敗しました。';
+$string['deletesuccess'] = '正常に削除されました。';
+$string['email'] = 'メール';
+$string['emailfail'] = 'メールエラー:';
+$string['emailfailerror'] = 'エラーが発生したため、下記のユーザにメール送信されませんでした ...';
+$string['emailhistory'] = 'クイックメール履歴';
+$string['emailstop'] = 'メールアドレスが無効にされています:';
+$string['messageerror'] = 'メッセージを入力してください!';
+$string['nogroupmembers'] = 'グループメンバなし';
+$string['notingroup'] = 'グループ外';
+$string['pleasewait'] = 'お待ちください、メールを送信しています ...';
+$string['sendemail'] = 'メールの送信';
+$string['subjecterror'] = '題名を入力してくださ!';
+$string['successfulemail'] = 'メールが正常に送信されました。';
+$string['to'] = 'To';
+$string['toerror'] = 'メールの受信者を選択してください!';
+$string['uncheck'] = 'すべての選択を解除';
+$string['view'] = '表示';
+
+?>
