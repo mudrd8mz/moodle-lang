5d60890eff40737a44fc3fefa952d4f60b3e6b41
Mitsuhiro Yoshida <mits>
2006-07-28 22:32:52 +0000

    Added 2 new language files for attendance.

---
+++ ja_utf8/attforblock.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // attforblock.php - created with Moodle 1.7 dev (2006071800)
+
+
+$string['description'] = 'あなたは、1つのコースにつき1つの出席モジュールを追加することができます。<br />このモジュールを削除しても、出席には影響しません! このモジュールは、出席ブロックのみと使用することができます。このモジュールは、コースの評定に出席点を入れるためだけに必要です。';
+$string['modulename'] = '出席 ( ブロック )';
+$string['modulenameplural'] = '出席 ( ブロック )';
+$string['modulenamesimple'] = '出席';
+
+?>
---
+++ ja_utf8/block_attendance.php
@@ -0,0 +1,65 @@
+<?PHP // $Id$ 
+      // block_attendance.php - created with Moodle 1.7 dev (2006071800)
+
+
+$string['Aacronym'] = '欠';
+$string['Afull'] = '欠席';
+$string['Eacronym'] = '早';
+$string['Efull'] = '早退';
+$string['Lacronym'] = '遅';
+$string['Lfull'] = '遅刻';
+$string['Pacronym'] = '出';
+$string['Pfull'] = '出席';
+$string['acronym'] = '頭文字';
+$string['add'] = '追加';
+$string['addmultiplesessions'] = '複数セッションの追加';
+$string['addsession'] = 'セッションの追加';
+$string['alltaken'] = 'すべて';
+$string['attendance'] = '出席';
+$string['attendanceforthecourse'] = 'コースの出席';
+$string['attendancegrade'] = '出席点';
+$string['attendancenotstarted'] = 'このコースの出席は、まだ開始されていません。';
+$string['attendancepercent'] = '出席率';
+$string['attendancereport'] = '出席レポート';
+$string['attendancesuccess'] = '出席が正常に取られました。';
+$string['attendanceupdated'] = '出席が正常に更新されました。';
+$string['changesession'] = 'セッションの変更';
+$string['createmultiplesessions'] = '複数セッションを作成する';
+$string['createonesession'] = 'コースに1つのセッションを作成する';
+$string['defaults'] = 'デフォルト';
+$string['delete'] = '削除';
+$string['deletesession'] = 'セッションの削除';
+$string['deletingsession'] = 'コースのセッションを削除';
+$string['description'] = '説明';
+$string['display'] = '表示対象';
+$string['downloadexcel'] = 'Excelフォーマットでダウンロードする';
+$string['downloadtext'] = 'テキストフォーマットでダウンロードする';
+$string['editsession'] = 'セッションの編集';
+$string['errorinaddingsession'] = 'セッションの追加中にエラーが発生しました。';
+$string['erroringeneratingsessions'] = 'セッションの生成中にエラーが発生しました。';
+$string['indetail'] = '詳細 ...';
+$string['months'] = '月';
+$string['nosessionexists'] = 'このコースにはセッションがありません。';
+$string['remarks'] = '備考';
+$string['report'] = 'レポート';
+$string['restoredefaults'] = 'デフォルトをリストアする';
+$string['session'] = 'セッション';
+$string['sessionadded'] = 'セッションが正常に追加されました。';
+$string['sessiondate'] = 'セッション年月日';
+$string['sessionscompleted'] = 'セッション完了';
+$string['sessionstartdate'] = 'セッション開始日';
+$string['sessionupdated'] = 'セッションが正常に更新されました。';
+$string['settings'] = '設定';
+$string['showdefaults'] = 'デフォルトを表示する';
+$string['status'] = 'ステータス';
+$string['strftimedm'] = '%%m/%%d';
+$string['strftimedmy'] = '%%Y/%%m/%%d';
+$string['strftimedmyw'] = '%%y/%%m/%%d (%%a)';
+$string['strftimeshortdate'] = '%%Y/%%m/%%d';
+$string['studentid'] = '学籍番号';
+$string['takeattendance'] = '出席をとる';
+$string['update'] = '更新';
+$string['week'] = '週';
+$string['weeks'] = '週';
+
+?>
