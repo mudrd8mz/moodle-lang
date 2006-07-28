d756bba60b189d8b526075b89a7ad37821d7c66f
Ethem Evlice <ethem>
2006-07-28 08:27:46 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

---
+++ tr_utf8/simpletest.php
@@ -0,0 +1,34 @@
+<?PHP // $Id$ 
+      // simpletest.php - created with Moodle 1.7 dev (2006071800)
+
+
+$string['all'] = 'TÜMÜ';
+$string['exception'] = 'İstisna';
+$string['fail'] = 'Başarısız';
+$string['ignorefile'] = 'Bu dosyada testleri yok say';
+$string['ignorethisfile'] = 'Bu test dosyasını yoksayarak testleri tekrar çalıştır';
+$string['moodleunittests'] = 'Moodle toplu testleri';
+$string['notice'] = 'Dikkat';
+$string['onlytest'] = 'Testleri sadece burada çalıştır';
+$string['pass'] = 'Geçti';
+$string['pathdoesnotexist'] = '\'$a\' yolu yok.';
+$string['retest'] = 'Testleri tekrar çalıştır';
+$string['retestonlythisfile'] = 'Sadece bu test dosyasında tekrar çalıştır';
+$string['runall'] = 'Testleri bütün dosyalarda çalıştır';
+$string['runat'] = 'Çalıştı: $a';
+$string['runonlyfile'] = 'Testleri sadece bu dosyada çalıştır';
+$string['runonlyfolder'] = 'Testleri sadece bu klasörde çalıştır';
+$string['runtests'] = 'Testleri çalıştır';
+$string['rununittests'] = 'Toplu testleri çalıştır';
+$string['showpasses'] = 'Başarısızların yanında başarılıları da göster';
+$string['showsearch'] = 'Test dosyaları için aramayı göster';
+$string['stacktrace'] = 'Stack trace:';
+$string['summary'] = '{$a->run}/{$a->total} test tamamlandı: <strong>{$a->passes}</strong> geçti, <strong>{$a->fails}</strong> hatalı ve <strong>{$a->exceptions}</strong> istisna';
+$string['thorough'] = 'Bütün testleri çalıştır (yavaş olabilir)';
+$string['uncaughtexception'] = '[{$a->getFile()}:{$a->getLine()}] dosyasında
+belirlenemeyen istisna [{$a->getMessage()}].
+TEST DURDURULDU.';
+$string['unittests'] = 'Toplu testler';
+$string['version'] = '<a href=\"http://sourceforge.net/projects/simpletest/\"> SimpleTest</a> sürüm $a kullanılarak.';
+
+?>
