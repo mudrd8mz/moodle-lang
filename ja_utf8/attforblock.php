4589b6ac7ec9734d4b623eef28c66b8744463acd
Mitsuhiro Yoshida <mits>
2006-07-29 01:16:48 +0000

    Brushed up translated strings for attendance.

--- ja_utf8/attforblock.php
+++ ja_utf8/attforblock.php
@@ -2,9 +2,9 @@
       // attforblock.php - created with Moodle 1.7 dev (2006071800)
 
 
-$string['description'] = 'あなたは、1つのコースにつき1つの出席モジュールを追加することができます。<br />このモジュールを削除しても、出席には影響しません! このモジュールは、出席ブロックのみと使用することができます。このモジュールは、コースの評定に出席点を入れるためだけに必要です。';
-$string['modulename'] = '出席 ( ブロック )';
-$string['modulenameplural'] = '出席 ( ブロック )';
-$string['modulenamesimple'] = '出席';
+$string['description'] = 'あなたは、1つのコースにつき1つの出欠モジュールを追加することができます。<br />このモジュールを削除しても、出欠には影響しません! このモジュールは、出欠ブロックのみと使用することができます。このモジュールは、コースの評定に出欠点を入れるためだけに必要です。';
+$string['modulename'] = '出欠 ( ブロック )';
+$string['modulenameplural'] = '出欠 ( ブロック )';
+$string['modulenamesimple'] = '出欠';
 
 ?>
