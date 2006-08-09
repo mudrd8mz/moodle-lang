f8fadde3dd9138f29298654ed50dd9a8e9e9596c
Mitsuhiro Yoshida <mits>
2006-08-09 16:28:05 +0000

    Translated new strings.

---
+++ ja_utf8/role.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // role.php - created with Moodle 1.7 dev (2006080900)
+
+
+$string['assignroles'] = '任務の割り当て';
+$string['currentcontext'] = 'コンテクスト';
+$string['currentrole'] = '任務';
+$string['existingusers'] = '割り当て済みユーザ: $a';
+$string['overrideroles'] = '任務のオーバーライド';
+$string['potentialusers'] = '潜在的なユーザ: $a';
+
+?>
