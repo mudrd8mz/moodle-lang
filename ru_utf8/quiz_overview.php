ba8c6414238a87c4e0b18e0e6b1267df19c46fe2
Alex Djachenko <djachenko>
2006-08-16 11:39:51 +0000

    From Aleksand Kocherov

---
+++ ru_utf8/quiz_overview.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // quiz_overview.php - created with Moodle 1.6 (2006050506)
+
+
+$string['allattempts'] = 'Все попытки';
+$string['allstudents'] = 'Все $a';
+$string['attemptsonly'] = '$a которые сделали попытки';
+$string['noattemptsonly'] = '$a которые не сделали попытки';
+$string['overview'] = 'Просмотр';
+
+?>
---
+++ ru_utf8/quiz_responses.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // quiz_responses.php - created with Moodle 1.6 (2006050506)
+
+
+$string['pagesize'] = 'Попыток на одной странице:';
+
+?>
