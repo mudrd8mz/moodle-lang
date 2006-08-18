f259c4eac2423c162f9b018e3b8d4331e23063c5
Lee Chan-young <yogibear270>
2006-08-18 13:21:36 +0000

    1.7DEV  added files

---
+++ ko_utf8/role.php
@@ -0,0 +1,60 @@
+<?PHP // $Id$ 
+      // role.php - created with Moodle 1.7 dev (2006081000)
+
+
+$string['allow'] = '허용';
+$string['assignroles'] = '역할 부여';
+$string['blog:create'] = '블로그 새로 만들기';
+$string['blog:manageentries'] = '입력내용 관리';
+$string['blog:manageofficialtags'] = '태그 관리';
+$string['blog:managepersonaltags'] = '개인태그 관리';
+$string['blog:view'] = '블로그 보기';
+$string['calendar:manageentries'] = '일정 관리하기';
+$string['calendar:manageownentries'] = '개인 일정 관리';
+$string['capability'] = '능력';
+$string['category:create'] = '범주 만들기';
+$string['category:delete'] = '범주 삭제';
+$string['category:update'] = '범주 갱신';
+$string['category:visibility'] = '숨겨진 범주 보기';
+$string['course:activityvisibility'] = '활동 감추기/보기';
+$string['course:create'] = '배움터 생성';
+$string['course:delete'] = '배움터 삭제';
+$string['course:manageactivities'] = '활동 관리';
+$string['course:managegroups'] = '모둠 관리';
+$string['course:managescales'] = '척도 관리';
+$string['course:update'] = '배움터 설정 갱신';
+$string['course:view'] = '배움터 보기';
+$string['course:viewhiddenactivities'] = '숨겨진 활동 보기';
+$string['course:viewparticipants'] = '참여자 보기';
+$string['course:viewscales'] = '척도 보기';
+$string['course:visibility'] = '배움터 숨기기/보기';
+$string['currentcontext'] = '현재의 내용';
+$string['currentrole'] = '현재 역할';
+$string['existingusers'] = '$a 기존 사용자';
+$string['inherit'] = '따름';
+$string['legacy:admin'] = '위임받은 역활 : 관리자';
+$string['legacy:coursecreator'] = '위임받은 역활 : 배움터 생성자';
+$string['legacy:editingteacher'] = '위임받은 역활 : 교사(편집가능)';
+$string['legacy:guest'] = '위임받은 역활 : 손님';
+$string['legacy:student'] = '위임받은 역활 : 학생';
+$string['legacy:teacher'] = '위임받은 역활 : 교사(편집기능 없음)';
+$string['overrideroles'] = '권한 위양';
+$string['potentialusers'] = '$a 잠재적 사용자';
+$string['prevent'] = '막힘';
+$string['prohibit'] = '금지';
+$string['role:assign'] = '사용자에게 역할을 부여함';
+$string['role:manage'] = '역할을 생성하고 관리함';
+$string['site:accessallgroups'] = '모든 모듬에 접속 기능';
+$string['site:backup'] = '배움터 백업';
+$string['site:config'] = '사이트 설정을 변경';
+$string['site:doanything'] = '모든 일을 할 수 있도록 허용';
+$string['site:import'] = '다른 배움터를 가져옴';
+$string['site:manageblocks'] = '사이트 전역 블록 관리';
+$string['site:restore'] = '배움터 복구';
+$string['site:viewfullnames'] = '언제나 사용자의 성명을 볼 수 있음';
+$string['user:create'] = '사용자 생성';
+$string['user:delete'] = '사용자 삭제';
+$string['user:update'] = '사용자 명세를 갱신';
+$string['user:viewdetails'] = '사용자 명세를 봄';
+
+?>
---
+++ ko_utf8/simpletest.php
@@ -0,0 +1,32 @@
+<?PHP // $Id$ 
+      // simpletest.php - created with Moodle 1.7 dev (2006071800)
+
+
+$string['all'] = '모두';
+$string['exception'] = '예외';
+$string['fail'] = '실패';
+$string['ignorefile'] = '파일 점검 무시';
+$string['ignorethisfile'] = '이 테스트 파일을 무시하고 재 실행';
+$string['moodleunittests'] = '무를 항목 점검 : $a';
+$string['notice'] = '주의';
+$string['onlytest'] = '한 항목만 점검';
+$string['pass'] = '통과';
+$string['pathdoesnotexist'] = '$a 경로가 존재하지 않습니다.';
+$string['retest'] = '점검 재 시도';
+$string['retestonlythisfile'] = '이 파일만 재 점검';
+$string['runall'] = '모든 파일에 대해 점검';
+$string['runat'] = '$ 에서 실행';
+$string['runonlyfile'] = '본 파일에 대해서만 점검 실행';
+$string['runonlyfolder'] = '본 폴더에 대해서만 점검 실행';
+$string['runtests'] = '점검 실행';
+$string['rununittests'] = '항목 점검 실행';
+$string['showpasses'] = '전 항목 보기';
+$string['showsearch'] = '점검 파일 검색결과 보기';
+$string['stacktrace'] = '스택 추적 :';
+$string['summary'] = '{$a->run}/{$a->total} 사례 점검 완료 : <strong>{$a->passes}</strong> 사례 통과, <strong>{$a->fails}</strong> 사례 실패, <strong>{$a->exceptions}</strong> 사례가 제외 되었음.';
+$string['thorough'] = '상세 점검(느릴 수 있음)';
+$string['uncaughtexception'] = '알려지지 않은 예외항목인 [{$a->getMessage()}] 이 [{$a->getFile()}:{$a->getLine()}] 에서 발견되어 점검이 취소됨.';
+$string['unittests'] = '항목 점검';
+$string['version'] = '<a href=\"http://sourceforge.net/projects/simpletest/\">SimpleTest</a> version $a 를 사용중임.';
+
+?>
