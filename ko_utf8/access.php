fefd9032c251d09902d3135b3f5182f84d105205
Lee Chan-young <yogibear270>
2006-08-18 11:07:54 +0000

    after Uni conversion, till correction.

--- ko_utf8/access.php
+++ ko_utf8/access.php
@@ -1,16 +1,17 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 1.6 development (2006032200)
+      // access.php - created with Moodle 1.7 dev (2006071800)
 
 
-$string['access'] = '접근';
-$string['accesshelp'] = '접근 도움말';
-$string['accesskey'] = '접근 키, $a';
-$string['accessstatement'] = '접근 사항';
+$string['access'] = '접속';
+$string['accesshelp'] = '접속 도움말';
+$string['accesskey'] = '접속 키, $a';
+$string['accessstatement'] = '접속 사항';
 $string['breadcrumb'] = 'Breadcrumb trail';
-$string['monthnext'] = '다음달';
-$string['monthprev'] = '지난달';
+$string['monthnext'] = '다음 달';
+$string['monthprev'] = '지난 달';
 $string['showhideblock'] = '블록 보기/감추기';
 $string['sitemap'] = '사이트 맵';
+$string['skipa'] = '$a 건너 뜀';
 $string['skipblock'] = '블록 생략';
 $string['skipnavigation'] = '네비게이션 생략';
 $string['tabledata'] = '데이터 데이블, $a';
