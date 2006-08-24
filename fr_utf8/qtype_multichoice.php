eeea90bf9699981f901219db3f7ff6d7c2f1020e
Nicolas Martignoni <martignoni>
2006-08-24 13:58:25 +0000

    MDL-5969 Let multiple choice questions have feedback that does not depend on the chosen answer
    In passing, I fixed MDL-6297 Export of multichoice questions in Moodle XML format doesn't save shuffle option too.

---
+++ fr_utf8/qtype_multichoice.php
@@ -0,0 +1,19 @@
+<?php // $Id$ 
+
+$string['answersingleno'] = 'Réponses multiples autorisées';
+$string['answersingleyes'] = 'Une seule réponse';
+$string['choiceno'] = 'Choix $a';
+$string['choices'] = 'Choix possibles';
+$string['editingmultichoice'] = 'Modification d\'une question à choix multiples';
+$string['fillouttwochoices'] = 'Vous devez donner au moins deux choix. Les choix laissés vides ne seront pas utilisés.';
+$string['fractionsaddwrong'] = 'La somme des notes positives que vous avez choisies n\'est pas égale à 100%%.<br />Leur somme est de $a%%.<br />Voulez-vous revenir en arrière pour corriger cette question ?';
+$string['fractionsnomax'] = 'Une des réponses doit être à 100%%, de sorte qu\'il soit<br />possible d\'obtenir la note maximale pour cette question.<br />Voulez-vous revenir en arrière pour corriger cette question ?';
+$string['feedback'] = 'Feedback';
+$string['notenoughanswers'] = 'Ce type de question requiert au moins $a réponses';
+$string['overallcorrectfeedback'] = 'Feedback pour les réponses correctes';
+$string['overallpartiallycorrectfeedback'] = 'Feedback pour les réponses partiellement correctes';
+$string['overallincorrectfeedback'] = 'Feedback pour les réponses fausses';
+$string['shuffleanswers'] = 'Mélanger les questions';
+$string['singleanswer'] = 'Choisir une réponse.';
+
+?>
