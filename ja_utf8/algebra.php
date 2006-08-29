5d87dba77256fce0aae31db2341f8ce8bec8e8ba
Mitsuhiro Yoshida <mits>
2006-08-29 02:03:11 +0000

    Brushed up a translated help file.

--- ja_utf8/algebra.php
+++ ja_utf8/algebra.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // algebra.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005021601)
+      // algebra.php - created with Moodle 1.7 dev (2006082800)
 
 
 $string['filtername'] = '代数表記法';
