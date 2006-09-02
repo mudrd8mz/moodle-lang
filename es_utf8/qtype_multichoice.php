8700743a8e23384fbdb731cb1df03b061b19a5cb
Benito Arias <barias>
2006-09-02 12:00:59 +0000

    *** empty log message ***

---
+++ es_utf8/qtype_multichoice.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // qtype_multichoice.php - created with Moodle 1.6 (2006050506)
+
+
+$string['answerhowmany'] = '¿Una o varias respuestas?';
+$string['answersingleno'] = 'Se permiten varias respuestas';
+$string['answersingleyes'] = 'Sólo una respuesta';
+$string['choiceno'] = 'Elegir $a';
+$string['choices'] = 'Opciones disponibles';
+$string['editingmultichoice'] = 'Editando una pregunta de opción múltiple';
+$string['feedback'] = 'Retroalimentación';
+$string['fillouttwochoices'] = 'Debe rellenar al menos dos opciones. Las que deje en blanco no se usarán.';
+$string['fractionsaddwrong'] = 'Las calificaciones positivas que ha elegido no suman el 100%%<br />sino únicamente el $a%%<br />¿Desea volver y arreglarlo?';
+$string['fractionsnomax'] = 'Una de las respuestas debería ser el 100%%, de modo que fuera <br />posible alcanzar la calificación total de esta pregunta.<br />¿Desea volver y arreglarlo?';
+$string['notenoughanswers'] = 'Este tipo de pregunta requiere al menos $a respuestas';
+$string['overallcorrectfeedback'] = 'Retroalimentación para cualquier respuesta correcta';
+$string['overallincorrectfeedback'] = 'Retroalimentación para cualquier respuesta incorrecta';
+$string['overallpartiallycorrectfeedback'] = 'Retroalimentación para cualquier respuesta parcialmente correcta';
+$string['shuffleanswers'] = 'Barajar respuestas';
+$string['singleanswer'] = 'Seleccione una respuesta.';
+
+?>
