753b1aa25ce3189c6c1c70346d78cd7e229fbc91
koen roggemans <koenr>
2006-09-04 20:53:33 +0000

    New strings from Zivana

---
+++ sr_lt_utf8/cloze_jr.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // cloze_jr.php - created with Moodle 1.6.1+ (2006050512)
+
+
+$string['clozetypeunsupported'] = 'Nije moguće umetnuti pitanje tipa <em> $a </em> u pitanje u formi teksta sa prazninama za unos tačnih odgovora.';
+$string['displayclozequestion'] = 'I evo novog pitanja u formi teksta sa prazninama (sa umetnutim tačnim odgovorima):';
+$string['displayquestions'] = 'Sledeća pitanja iz kategorije <em> $a </em> biće korišćena u novom pitanju u formi teksta sa prazninama za unos tačnih odgovora.';
+$string['donotclick'] = '<strong>Nemojte</strong> koristiti donji link, jer nije urađen fizički izvoz pitanja u datoteku...<br />Kliknite na dugme za <strong>nastavak</strong> i uočite novo pitanje u formi teksta sa prazninama za unos tačnih odgovora u kategoriji <em> $a </em>.';
+
+?>
