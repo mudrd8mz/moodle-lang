87289be6c0e436deece3946c32a6e70477d12e5c
Lee Chan-young <yogibear270>
2006-09-06 13:41:26 +0000

    New born in Korean

---
+++ ko_utf8/qtype_multichoice.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // qtype_multichoice.php - created with Moodle 1.7 dev (2006090300)
+
+
+$string['answerhowmany'] = '단답 혹은 다답';
+$string['answersingleno'] = '다답 허용';
+$string['answersingleyes'] = '단답만';
+$string['choiceno'] = '$a 번 선택';
+$string['choices'] = '가능한 선택';
+$string['editingmultichoice'] = '선다형 문항 편집';
+$string['feedback'] = '피드백';
+$string['fillouttwochoices'] = '최소한 답안은 둘 이상 작성해야 합니다. 나머지 빈 칸은 사용되지 않습니다.';
+$string['fractionsaddwrong'] = '선택한 정답의 점수 합계가 100%%가 아닙니다.<br />현재 합계는 $a%% 입니다.<br />되돌아가서 이를 바로 잡겠습니까?';
+$string['fractionsnomax'] = '답안 중 하나는 100%%가 되어야 이 문항에 대해 점수를 매길 수 있습니다.<br />되돌아가서 이를 바로 잡겠습니까?';
+$string['notenoughanswers'] = '이러한 유형의 문항은 적어도 $a 개의 답안이 있어야 합니다.';
+$string['overallcorrectfeedback'] = '정답에 대한 반응';
+$string['overallincorrectfeedback'] = '오답에 대한 반응';
+$string['overallpartiallycorrectfeedback'] = '부분적으로 맞은 답에 대한 반응';
+$string['shuffleanswers'] = '답안을 섞음';
+$string['singleanswer'] = '답 하나 고르기';
+
+?>
