83a900f4702cf96363442ec2fee956e0281d7732
Lee Chan-young <yogibear270>
2006-09-15 10:39:58 +0000

    Correct some terms

--- ko_utf8/block_rss_client.php
+++ ko_utf8/block_rss_client.php
@@ -1,41 +1,43 @@
-<?PHP // $Id$
+<?PHP // $Id$ 
+      // block_rss_client.php - created with Moodle 1.7 dev (2006091212)
 
 
-
-$string['addfeed'] = '새로운 feed URL 추가하기';
+$string['addfeed'] = '새로운 취재원 주소 추가하기';
 $string['addheadlineblock'] = 'RSS 헤드라인 블록 추가하기';
-$string['addnew'] = '뉴스 추가하기';
-$string['choosefeedlabel'] = '이 블록에서 사용가능하게 하고 싶은 feeds를 선택하시오';
+$string['addnew'] = '새로 추가';
+$string['choosefeedlabel'] = '이 블록에서 사용하고 싶은 취재원을 선택하시오';
 $string['clientchannellink'] = '소스 사이트';
-$string['clientnumentries'] = '한 feed에 보여줄 전체 default  숫자';
-$string['clientshowchannellinklabel'] = '전시된 원래 사이트에 링크 할 수 있습니까?(만약 아무런 feed링크가 뉴스feed에 제공되지 않으면 아무런 링크도 보이지 않을 것이라는것을 주의히사오)';
-$string['clientshowimagelabel'] = '만약 가능하다면 체널이미지를 보여주시오';
-$string['configblock'] = '이 블록을 배열하시오';
-$string['couldnotfindfeed'] = '아이디와 맞는 feed를 찾지 못했습니다.';
-$string['customtitlelabel'] = '제목(feed가 제공한 제목을 사용할 빈칸을 남기시오)';
-$string['deletefeedconfirm'] = '이 feed를 지우시겠습니까?';
-$string['displaydescriptionlabel'] = '각 링크의 설명서를 보이시겠습니까?';
-$string['editfeeds'] = 'RSS/Atom 뉴스 feed 로부터 편집,서명하거나 서명하지 마시오 ';
-$string['editnewsfeeds'] = '새로운 feed를 편집';
+$string['clientnumentries'] = '한 취재원이 보여줄 기본 기사 수';
+$string['clientshowchannellinklabel'] = '송출한 원래 사이트에 링크 할 수 있습니까?(만약 아무런 취재원링크가 뉴스feed에 제공되지 않으면 아무런 링크도 보이지 않을 것이라는 것을 주의히사오)';
+$string['clientshowimagelabel'] = '만약 가능하다면 채널 이미지를 보여주시오';
+$string['configblock'] = '이 블록을 설정하시오';
+$string['couldnotfindfeed'] = '아이디와 맞는 취재원 찾지 못했습니다.';
+$string['customtitlelabel'] = '제목(취재원이 제공한 제목을 사용할 빈칸을 남기시오)';
+$string['deletefeedconfirm'] = '이 취재원을 지우겠습니까?';
+$string['displaydescriptionlabel'] = '각 링크의 설명을 보이겠습니까?';
+$string['editfeeds'] = 'RSS/Atom 뉴스 취재원으로부터 편집,구독하거나 구독하지 마시오';
+$string['editnewsfeeds'] = '새소식 취재원을 편집';
 $string['editrssblock'] = '헤드라인 블록을 편집';
-$string['feed'] = '피드';
-$string['feedadded'] = '새로운 피드 추가';
-$string['feeddeleted'] = '새로운 피드 삭제';
-$string['feeds'] = '새로운 피드';
-$string['feedsaddedit'] = '피드 추가/삭제';
-$string['feedsconfigurenewinstance'] = '이 블록을 RSS feeds를 배열하려면 여기를 클릭';
-$string['feedstitle'] = '원격 RSS feeds';
-$string['feedupdated'] = '새로운 feed 업데이트';
-$string['findmorefeeds'] = '더 많은 rss feeds 찾기';
-$string['managefeeds'] = '나의 모든 feeds 관리하기';
-$string['nofeeds'] = '이 사이트를 위해 한정할 rss feeds 가 없습니다';
+$string['feed'] = '취재원';
+$string['feedadded'] = '새로운 취재원 추가';
+$string['feeddeleted'] = '새소식 취재원 삭제';
+$string['feeds'] = '새소식 취재원';
+$string['feedsaddedit'] = '취재원 추가/삭제';
+$string['feedsconfigurenewinstance'] = '이 블록에 RSS 취재원을 설정하려면 여기를 클릭';
+$string['feedstitle'] = '원격 RSS 취재원';
+$string['feedupdated'] = '새로운 취재원 업데이트';
+$string['findmorefeeds'] = '더 많은 RSS 취재원 찾기';
+$string['managefeeds'] = '내 취재원 관리하기';
+$string['nofeeds'] = '이 사이트에 대한 정의된 RSS 취재원이 없습니다';
 $string['pickfeed'] = '새로운 feed 선택';
-$string['remotenewsfeed'] = '원격 뉴스 feed ';
-$string['seeallfeeds'] = '모든 피드 보기';
-$string['shownumentrieslabel'] = '한 블럭에 보여줄 완벽한 최대 숫자';
-$string['submitters'] = '누가 새로운 rss feeds를 정의하게 허락하였는가? 정의된  feeds는 당신의 사이트 어느 페이지에서도 사용 가능하다';
+$string['remotenewsfeed'] = '원격 뉴스 취재원';
+$string['seeallfeeds'] = '모든 취재원 보기';
+$string['sharedfeed'] = '공유 취재원';
+$string['shownumentrieslabel'] = '한 블럭에 보여줄  최대 숫자';
+$string['submitters'] = '누가 새로운 RSS 취재원을 정의하게 허락하였는가? 정의된 취재원은 당신의 사이트 어느 페이지에서도 사용 가능하다';
 $string['timeout'] = 'rss 끊김 막기';
-$string['timeoutdesc'] = '케쉬에 있는 rss feed 를 위한 분단위의 시간';
-$string['updatefeed'] = '새로운 뉴스 feed URL 업데이트';
-$string['validatefeed'] = 'feed 확인';
+$string['timeoutdesc'] = '케쉬에 있는 RSS 취재원을 위한 분단위의 시간';
+$string['updatefeed'] = '새로운 뉴스 취재원 주소 갱신';
+$string['validatefeed'] = '취재원 확인';
+
 ?>
