5571a9777869068bf69a3838a2b07e2e3d5d7c57
Jaime Villate <villate>
2006-09-22 14:45:31 +0000

    More strings translated

---
+++ pt_utf8/block_search_forums.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_search_forums.php - created with Moodle 1.6.1+ (2006050513)
+
+
+$string['advancedsearch'] = 'Pesquisa avançada';
+$string['blocktitle'] = 'Procurar nos fóruns';
+
+?>
