6bfc2cdde8ccb7efbfaaf92faf716be1e66673a0
Mitsuhiro Yoshida <mits>
2006-09-23 17:54:27 +0000

    Brushed up translated strings for block learning resources.

--- ja_utf8/block_learning_resources.php
+++ ja_utf8/block_learning_resources.php
@@ -1,26 +1,26 @@
 <?PHP // $Id$ 
-      // block_learning_resources.php - created with Moodle 1.7 dev (2006092201)
+      // block_learning_resources.php - created with Moodle 1.7 dev (2006092302)
 
 
 $string['actions'] = '操作';
-$string['addlink'] = 'リンクを追加';
-$string['are_you_sure'] = 'ほんとうに削除しますか？';
+$string['addlink'] = 'リンクを追加する';
+$string['are_you_sure'] = '本当に次のリンクを削除してもよろしいですか:';
 $string['blockname'] = '学習資料';
-$string['defaultshow'] = 'デフォールトで表示';
+$string['defaultshow'] = 'デフォルトで表示する';
 $string['delete'] = '削除';
-$string['delete_resources'] = 'リソースを削除';
-$string['deleted'] = 'リソースは削除されました。';
+$string['delete_resources'] = 'リソースを削除する';
+$string['deleted'] = 'リソースが削除されました。';
 $string['display'] = '表示';
 $string['links'] = 'リンク';
 $string['linktext'] = 'リンクテキスト';
-$string['manage_links'] = 'リンクを追加/編集';
+$string['manage_links'] = 'リンクを追加/編集する';
 $string['modify'] = '編集';
-$string['newwindow_label'] = 'リンクを新規ウィンドウで開く';
+$string['newwindow_label'] = 'リンクを新しいウィンドウで開く:';
 $string['no'] = 'いいえ';
 $string['none'] = '表示するリソースはありません。';
-$string['not_exist'] = '選択したリソースは存在しません。';
+$string['not_exist'] = '選択したリソースは存在しません!';
 $string['notes'] = '追加テキスト';
 $string['url'] = 'URL';
-$string['yes'] = 'はい';
+$string['yes'] = 'Yes';
 
 ?>
