63d9d0959766dd5cd8ce06bf1d2deefc7755de5e
Mitsuhiro Yoshida <mits>
2006-09-23 07:16:51 +0000

    Added a translated file for block learning resources.

---
+++ ja_utf8/block_learning_resources.php
@@ -0,0 +1,26 @@
+<?PHP // $Id$ 
+      // block_learning_resources.php - created with Moodle 1.7 dev (2006092201)
+
+
+$string['actions'] = '操作';
+$string['addlink'] = 'リンクを追加';
+$string['are_you_sure'] = 'ほんとうに削除しますか？';
+$string['blockname'] = '学習資料';
+$string['defaultshow'] = 'デフォールトで表示';
+$string['delete'] = '削除';
+$string['delete_resources'] = 'リソースを削除';
+$string['deleted'] = 'リソースは削除されました。';
+$string['display'] = '表示';
+$string['links'] = 'リンク';
+$string['linktext'] = 'リンクテキスト';
+$string['manage_links'] = 'リンクを追加/編集';
+$string['modify'] = '編集';
+$string['newwindow_label'] = 'リンクを新規ウィンドウで開く';
+$string['no'] = 'いいえ';
+$string['none'] = '表示するリソースはありません。';
+$string['not_exist'] = '選択したリソースは存在しません。';
+$string['notes'] = '追加テキスト';
+$string['url'] = 'URL';
+$string['yes'] = 'はい';
+
+?>
