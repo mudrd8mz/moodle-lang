a4fd277eb9aad35c2b90aa21c95d1c7eea08ddb3
Nicolas Martignoni <martignoni>
2006-09-25 13:06:08 +0000

    This is a first cut of a new formslib.php a library of classes for creating and securely processing forms in Moodle, based on PEAR QuickForms. Only uses XHTML and CSS and no table tags.

---
+++ fr_utf8/form.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+
+$string['denotesreq'] = '$a désigne un champ requis.';
+$string['requiredelement'] = 'Champ requis field.';
+$string['nonexistentformelements'] = 'Tentative d\'ajout de bouton d\'aide à des éléments non existants&nbsp;: $a';
+
+?>
