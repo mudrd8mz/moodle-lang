319c6c332e49959b298606b7207f4b82afdf526b
Jaime Villate <villate>
2006-09-25 13:17:52 +0000

    Initial translation

---
+++ pt_utf8/access.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // access.php - created with Moodle 1.6.2 (2006050521)
+
+
+$string['access'] = 'Acessibilidade';
+$string['accesshelp'] = 'Ajuda para acessibilidade';
+$string['accesskey'] = 'Tecla de acesso, $a';
+$string['accessstatement'] = 'Declaração de acessibilidade';
+$string['breadcrumb'] = 'Rasto de migalhas';
+$string['monthnext'] = 'Próximo mês';
+$string['monthprev'] = 'Mês prévio';
+$string['showhideblock'] = 'Mostrar ou ocultar bloco';
+$string['sitemap'] = 'Mapa do sítio';
+$string['skipblock'] = 'Prescindir bloco';
+$string['skipnavigation'] = 'Prescindir navegação';
+$string['tabledata'] = 'Tabela de dados, $a';
+$string['tablelayout'] = 'Tabela de esquema, $a';
+$string['tocontent'] = 'Ir para o conteúdo principal';
+$string['tonavigation'] = 'Ir para a navegação';
+$string['youarehere'] = 'Você está aqui';
+
+?>
---
+++ pt_utf8/block_glossary_random.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // block_glossary_random.php - created with Moodle 1.6.2 (2006050521)
+
+
+$string['addentry'] = 'Adicione as suas frases célebres favoritas!';
+$string['blockname'] = 'Termo aleatório do glossário';
+$string['intro'] = 'Verifique que esta disciplina tenha pelo menos um glossário com pelo menos um termo. A seguir, poderá ajustar as configurações seguintes';
+$string['invisible'] = '(continua...)';
+$string['lastmodified'] = 'Últimmo termo modificado';
+$string['nextone'] = 'Próximo termo';
+$string['noentriesyet'] = 'Ainda não existe nenhum termo no glossário escolhido.';
+$string['random'] = 'Termo aleatório';
+
+?>
