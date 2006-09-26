0c67a929ba0086460376209d57e325113bd2634f
Nicolas Martignoni <martignoni>
2006-09-26 06:51:54 +0000

    major cleanup of unsupported and legacy external pages in admin/; created new hidden unsupported category; some other minor fixes and changes

--- fr_utf8/admin.php
+++ fr_utf8/admin.php
@@ -416,6 +416,7 @@ $string['timezonenotforced'] = 'Les utilisateurs peuvent choisir leur fuseau hor
 $string['unicodeupgradenotice'] = 'Dans la version 1.6 de Moodle, toutes les langues sont en encodage Unicode. Pour finir la mise à jour de ce site, il est nécessaire de convertir tout le contenu de votre base de données en encodage Unicode à l\'aide de notre script de migration. <a href=\"utfdbmigrate.php\">Cliquez ici pour lancer le script de migration maintenant</a>&nbsp;!';
 $string['uninstall'] = 'Désinstaller les langues sélectionnées';
 $string['uninstallconfirm'] = 'Vous êtes sur le point de supprimer totalement la langue $a. Voulez-vous continuer&nbsp;?';
+$string['unsupported'] = 'Non supporté';
 $string['updatecomponent'] = 'Mise à jour du composant';
 $string['updatelangs'] = 'Mettre à jour toutes les langues installées';
 $string['updatetimezones'] = 'Mettre à jour les fuseaux horaires';
