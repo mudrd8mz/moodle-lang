35f6845fd19c4c2a8a47ff2c0f87fea240711b48
Lee Chan-young <yogibear270>
2006-09-26 13:35:16 +0000

    New born in Korean

---
+++ ko_utf8/form.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 1.7 dev (2006092600)
+
+
+$string['denotesreq'] = '$a 는 필요한 필드를 나태냄';
+$string['nonexistentformelements'] = '존재하지 않는 양식 요소 $a 에 도움말 단추를 추가하려 함';
+$string['requiredelement'] = '펼요한 필드';
+
+?>
