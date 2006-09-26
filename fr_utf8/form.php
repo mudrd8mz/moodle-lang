0e3800f3c6b746a5872cc6c2e73cb11f37b91737
Nicolas Martignoni <martignoni>
2006-09-26 12:36:45 +0000

    * added error message for setHelpButton method not existing * modified htmleditor class so that differences in formatting for this element are defined by css but use the same html * added type name of element classes to css wrapper class names

--- fr_utf8/form.php
+++ fr_utf8/form.php
@@ -3,5 +3,6 @@
 $string['denotesreq'] = '$a désigne un champ requis.';
 $string['requiredelement'] = 'Champ requis field.';
 $string['nonexistentformelements'] = 'Tentative d\'ajout de bouton d\'aide à des éléments non existants&nbsp;: $a';
+$string['nomethodforaddinghelpbutton'] = 'Il n\'y a pas de méthode pour ajouter un bouton d\'aide sur un élément $a->name (classe $a->classname) de formulaire';
 
 ?>
