47a41c6a3b2bffdaa4f705c292afc8b395754ed2
Lee Chan-young <yogibear270>
2006-09-28 13:37:16 +0000

    Translate added strings & correct some terms

--- ko_utf8/access.php
+++ ko_utf8/access.php
@@ -1,12 +1,14 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 1.7 dev (2006071800)
+      // access.php - created with Moodle 1.7 dev (2006092800)
 
 
-$string['access'] = '접속';
-$string['accesshelp'] = '접속 도움말';
-$string['accesskey'] = '접속 키, $a';
-$string['accessstatement'] = '접속 사항';
-$string['breadcrumb'] = 'Breadcrumb trail';
+$string['access'] = '접근 가능성';
+$string['accesshelp'] = '접근 가능성 도움말';
+$string['accesskey'] = '접속키, $a';
+$string['accessstatement'] = '접근 가능성 선언';
+$string['activitynext'] = '다음 활동';
+$string['activityprev'] = '이전 활동';
+$string['breadcrumb'] = '방문 경로';
 $string['monthnext'] = '다음 달';
 $string['monthprev'] = '지난 달';
 $string['showhideblock'] = '블록 보기/감추기';
@@ -14,9 +16,9 @@ $string['sitemap'] = '사이트 맵';
 $string['skipa'] = '$a 건너 뜀';
 $string['skipblock'] = '블록 생략';
 $string['skipnavigation'] = '네비게이션 생략';
-$string['tabledata'] = '데이터 데이블, $a';
+$string['tabledata'] = '데이터 테이블, $a';
 $string['tablelayout'] = '배치 테이블, $a';
-$string['tocontent'] = '주 컨텐츠로 가기';
+$string['tocontent'] = '주 내용으로 가기';
 $string['tonavigation'] = '네비게이션으로 가기';
 $string['youarehere'] = '당신이 있는 곳';
 
