47f3bdfd31f0c31254a7789d4c80a4b03dcdf410
Mitsuhiro Yoshida <mits>
2006-10-04 23:44:11 +0000

    Brushed up translated strings for block course list.

--- ja_utf8/block_course_list.php
+++ ja_utf8/block_course_list.php
@@ -1,10 +1,10 @@
 <?PHP // $Id$ 
-      // block_course_list.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004121600)
+      // block_course_list.php - created with Moodle 1.7 dev (2006100401)
 
 
-$string['allcourses'] = '管理者は全てのコースユーザを閲覧可';
-$string['blockname'] = 'コースリスト';
-$string['configadminview'] = 'コースリストブロック内で管理者に何を表示しますか?';
+$string['allcourses'] = '管理者はすべてのコースを閲覧可';
+$string['blockname'] = 'コース一覧';
+$string['configadminview'] = 'コース一覧ブロックで管理者に何を表示しますか?';
 $string['owncourses'] = '管理者は自分のコースのみ閲覧可';
 
 ?>
