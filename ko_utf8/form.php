253eeba6909483a91887e9939e1e731d1c47a6b7
Lee Chan-young <yogibear270>
2006-10-15 01:32:29 +0000

    Translate added strings

--- ko_utf8/form.php
+++ ko_utf8/form.php
@@ -1,8 +1,18 @@
 <?PHP // $Id$ 
-      // form.php - created with Moodle 1.7 dev (2006092601)
+      // form.php - created with Moodle 1.8 dev (2006101001)
 
 
 $string['denotesreq'] = '$a 는 필요한 필드를 나태냄';
+$string['err_alphanumeric'] = '알파벳이나 숫자만 입력하라.';
+$string['err_email'] = '쓸 수 있는 이메일 주소를 입력하.';
+$string['err_lettersonly'] = '문자만 입력하라.';
+$string['err_maxlength'] = '$a->format 글자 미만의 문자를 입력하라.';
+$string['err_minlength'] = '최소 $a->format 글자 이상의 문자를 입력하라.';
+$string['err_nonzero'] = '0이 아닌 숫자만 입력하라.';
+$string['err_nopunctuation'] = '쉼표나 세미콜론 등이 없는 문자만 입력하라.';
+$string['err_numeric'] = '숫자만 입력하라.';
+$string['err_rangelength'] = '{$a->format[0]} 에서 {$a->format[1]} 글자의 문자만 입력하라.';
+$string['err_required'] = '적당한 값을 입력하라.';
 $string['nomethodforaddinghelpbutton'] = '양식 요소 $a->name (학급 $a->classname)에 도움말 단추를 추가할 방법이 없습니다.';
 $string['nonexistentformelements'] = '존재하지 않는 양식 요소 $a 에 도움말 단추를 추가하려 함';
 $string['requiredelement'] = '펼요한 필드';
