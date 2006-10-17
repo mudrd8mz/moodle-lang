e8ad0043db6af44f0c511e2dab42643809866a5a
Andrea Bicciolo <andreabix>
2006-10-17 14:41:27 +0000

    Traduzione di Claudio Maggi

--- it_utf8/blog.php
+++ it_utf8/blog.php
@@ -1,40 +1,58 @@
 <?PHP // $Id$ 
-      // blog.php - created with Moodle 1.6 development (2006032200)
+      // blog.php - created with Moodle 1.7 beta (2006101000)
 
 
-$string['addnewentry'] = 'Aggiungi un nuovo elemento';
+$string['addnewentry'] = 'Nuovo intervento';
 $string['addotags'] = 'Aggiungi tag ufficiali';
 $string['addptags'] = 'Aggiungi tag definiti dall\'utente';
 $string['blockmenutitle'] = 'Menu Blog';
+$string['blocktagstitle'] = 'Tag dei blog';
+$string['blocktitle'] = 'Titolo blocco tag dei blog';
 $string['blog'] = 'Blog';
-$string['blogdeleteconfirm'] = 'Cancellare questo blog?';
+$string['blogdeleteconfirm'] = 'Cancella questo blog?';
 $string['blogpreferences'] = 'Preferenze blog';
 $string['blogs'] = 'Blog';
-$string['blogtags'] = 'Tag blog';
+$string['blogtags'] = 'Tag dei blog';
 $string['courseblog'] = 'Blog del corso: $a';
-$string['entrybody'] = 'Corpo del elemento del blog';
-$string['entrybodyonlydesc'] = 'Descrizione elemento';
-$string['entryerrornotyours'] = 'Questo elemento non è vostro';
-$string['entrytitle'] = 'Titolo elemento';
-$string['entryupdated'] = 'Elemento del blog aggiornato';
+$string['courseblogs'] = 'Gli utenti possono vedere i blog solo delle persone che condividono un corso';
+$string['disableblogs'] = 'Disabilita completamente il sistema dei blog';
+$string['emptybody'] = 'Il corpo di un intervento di blog non può essere vuoto';
+$string['emptytitle'] = 'Il titolo di un intervento di blog non può essere vuoto';
+$string['entrybody'] = 'Corpo dell\'intervento del blog';
+$string['entrybodyonlydesc'] = 'Descrizione intervento';
+$string['entryerrornotyours'] = 'Questo intervento non è vostro';
+$string['entrysaved'] = 'Il tuo intervento è stato salvato';
+$string['entrytitle'] = 'Titolo intervento';
+$string['entryupdated'] = 'Intervento del blog aggiornato';
 $string['groupblog'] = 'Blog del gruppo: $a';
+$string['groupblogs'] = 'Gli utenti possono vedere i blog solo delle persone che appartengono allo stesso gruppo';
 $string['intro'] = 'Questo RSS è stato creato automaticamente da uno o più blog.';
-$string['noentriesyet'] = 'Qui non ci sono elementi visibili';
-$string['noguestpost'] = 'Gli ospiti non possono inserire blog!';
-$string['notallowedtoedit'] = 'Non vi è permesso modificare questo elemento';
+$string['noentriesyet'] = 'Qui non ci sono interventi visibili';
+$string['noguestpost'] = 'Gli ospiti non possono inserire interventi nel blog!';
+$string['norighttodeletetag'] = 'Non hai l\'autorizzazione di cancellare questo tag - $a';
+$string['notallowedtoedit'] = 'Non vi è permesso modificare questo intervento';
+$string['numberofentries'] = 'Interventi: $a';
+$string['numberoftags'] = 'Numero tag da mostrare';
 $string['otags'] = 'Tag ufficiali';
-$string['pagesize'] = 'Numero di elementi del blog per Pagina';
+$string['pagesize'] = 'Numero di interventi del blog per Pagina';
+$string['personalblogs'] = 'Gli utenti possono vedere solo i propri blog';
 $string['ptags'] = 'Tag definiti dall\'utente';
-$string['publishto'] = 'Pubblicare in';
+$string['publishto'] = 'Pubblica in';
 $string['publishtonoone'] = 'Nessuno (Bozza)';
 $string['publishtosite'] = 'Tutti in questo sito';
 $string['publishtoworld'] = 'Tutto il mondo';
 $string['settingsupdatederror'] = 'È occorso un errore, le impostazioni delle preferenze del blog non possono essere aggiornate';
 $string['siteblog'] = 'Blog del sito: $a';
-$string['updateentrywithid'] = 'Aggiornamento elemento';
+$string['siteblogs'] = 'Tutti gli utenti del sito possono vedere tutti gli interventi nei blog';
+$string['tagdatelastused'] = 'Data ultima utilizzazione tag';
+$string['tagsort'] = 'Ordina elenco tag per';
+$string['tagtext'] = 'Testo tag';
+$string['timewithin'] = 'Mostra le tag utilizzate da';
+$string['updateentrywithid'] = 'Aggiornamento intervento';
 $string['userblog'] = 'Blog utente: $a';
-$string['viewcourseentries'] = 'Visualizza elementi del corso';
-$string['viewmyentries'] = 'Visualizza i miei elementi';
-$string['viewsiteentries'] = 'Visualizza elementi del sito';
+$string['viewcourseentries'] = 'Interventi del corso';
+$string['viewmyentries'] = 'Miei interventi';
+$string['viewsiteentries'] = 'Interventi del sito';
+$string['worldblogs'] = 'Tutti possono leggere gli interventi impostati come pubblici';
 
 ?>
