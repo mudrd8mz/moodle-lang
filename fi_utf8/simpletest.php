46af4dc1d95a611b18ec634cf92dbab4d5c6276a
Petri Asikainen <paca70>
2006-10-17 12:15:32 +0000

    Updates

---
+++ fi_utf8/simpletest.php
@@ -0,0 +1,25 @@
+<?PHP // $Id$ 
+      // simpletest.php - created with Moodle 1.7 beta (2006101000)
+
+
+$string['all'] = 'KAIKKI';
+$string['exception'] = 'Poikkeus';
+$string['fail'] = 'Epäonnistui';
+$string['ignorefile'] = 'Älä huomioi testejä tiedossa';
+$string['ignorethisfile'] = 'Suorita testit uudelleen huomioimatta tätä testitiedostoa';
+$string['moodleunittests'] = 'Moodle testit: $a';
+$string['notice'] = 'Huomaa';
+$string['onlytest'] = 'Suorita vain testit';
+$string['pass'] = 'Onnistui';
+$string['pathdoesnotexist'] = 'Polkua $a ei ole';
+$string['retest'] = 'Suorita testit uudelleen';
+$string['retestonlythisfile'] = 'Suorita vain tämä testi uudelleen.';
+$string['runall'] = 'Suorita kaikki testit uudelleen.';
+$string['runat'] = 'Suorita testi klo $a';
+$string['runonlyfile'] = 'Suorita testit tastä tiedostosta';
+$string['runonlyfolder'] = 'Suorita testit tastä hakemistosta';
+$string['runtests'] = 'Suorita testit';
+$string['showpasses'] = 'Näytä myös onnistuneet testit';
+$string['showsearch'] = 'Näytä testitiedostojen haku';
+
+?>
