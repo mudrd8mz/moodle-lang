204679d671a767230c3b7cb491e828caaadaecf7
Petri Asikainen <paca70>
2006-10-17 19:03:59 +0000

    Update

---
+++ fi_utf8/enrol_manual.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // enrol_manual.php - created with Moodle 1.7 beta (2006101000)
+
+
+$string['description'] = 'Oletustapa kurssille rekisteröitymisessä.
+Tämä tarjoaa kaksi tapaa joilla käyttäjät voivat rekisteröityä kurssille.
+<ul>
+<li>Opettaja tai ylläpitäjä voivat rekisteröidä käyttäjän käsin kurssin asetuksista<7li>
+<li>Kursilla voi olla avain ja jokainen joka tietää avaimen voi rekisteröidä itsensä kurssille</lI>
+</ul>';
+$string['enrolname'] = 'Sisäinen rekisteröityminen';
+
+?>
