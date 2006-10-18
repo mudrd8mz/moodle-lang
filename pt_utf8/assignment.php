75d4fad88e0e9b5b42f7e110cab17961160e4999
Jaime Villate <villate>
2006-10-18 14:16:00 +0000

    Added contributions from the group at traducao.moodlept.org

--- pt_utf8/assignment.php
+++ pt_utf8/assignment.php
@@ -1,7 +1,8 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.2 Beta (2004022400)
+      // assignment.php - created with Moodle 1.6.2 (2006050521)
 
 
+$string['allowmultiple'] = 'Permitir vários ficheiros';
 $string['allowresubmit'] = 'Permitir enviar novamente';
 $string['assignmentdetails'] = 'Detalhes do trabalho';
 $string['assignmentmail'] = '$a->teacher  comentou o seu trabalho
@@ -15,15 +16,46 @@ $string['assignmentmailhtml'] = '$a->teacher comentou o seu trabalho
 O comentário encontra-se anexo no fim do <a href=\"$a->url\">seu trabalho</a>.';
 $string['assignmentname'] = 'Nome do trabalho';
 $string['assignmenttype'] = 'Tipo de trabalho';
+$string['attachfile'] = 'Anexar ficheiro';
+$string['attachfiletoassignment'] = 'Anexar ficheiro(s) ao trabalho';
+$string['availabledate'] = 'Disponível de';
+$string['backtoassignment'] = 'Regressar ao Trabalho';
+$string['backtofeedback'] = 'Regressar ao comentário';
+$string['choosereviewfile'] = 'Escolha ficheiro para upload como revisão de Trabalho';
+$string['comment'] = 'Comentário';
+$string['commentinline'] = 'Comente inline';
+$string['configitemstocount'] = 'Natureza dos items a ser contados para submissões dos alunos em atribuições online.';
 $string['configmaxbytes'] = 'Tamanho máximo, por omissão, de todos os trabalhos enviados para o sítio (sujeito a limites de cada disciplina e limites locais)';
+$string['deletecheckfile'] = 'Tem a certeza que deseja eliminar este ficheiro?';
+$string['deleteednotification'] = 'O ficheiro foi eliminado.';
+$string['deletefail'] = 'O seguinte ficheiro na foi eliminado:';
 $string['description'] = 'Descrição';
 $string['duedate'] = 'Data de entrega';
 $string['duedateno'] = 'Sem data limite';
 $string['early'] = '$a cedo';
+$string['editmysubmission'] = 'Editar a minha submissão';
+$string['emailstudents'] = 'Enviar alertas a alunos por email';
+$string['emailteachermail'] = '$a->username actualizou a submissão atribuida para \'$a->assignment\'
+
+Está disponível aqui:
+
+$a->url';
+$string['emailteachermailhtml'] = '$a->username actualizou a submissão atribuida para <i>\'$a->assignment\'</i><br /><br /> Está <a href=\"$a->url\">disponível no site</a>';
+$string['emailteachers'] = 'Emails de alerta para professores';
+$string['emptysubmission'] = 'Ainda não submeteu nada';
 $string['existingfiledeleted'] = 'O seguinte ficheiro foi apagado: $a';
 $string['failedupdatefeedback'] = 'Falhou a actualização dos comentários ao trabalho do utilizador $a';
 $string['feedback'] = 'Os seus comentários';
+$string['feedbackfromteacher'] = 'Comentários sobre $a';
 $string['feedbackupdated'] = 'Foram actualizados os comentários aos trabalhos de $a pessoas';
+$string['graded'] = 'Classificado';
+$string['guestnosubmit'] = 'Desculpe, aos visitantes não é permitido submeter um a atribuição. Tem que se registar antes de submeter a resposta';
+$string['guestnoupload'] = 'Desculpe, aos visitantes não é permitido carregar ficheiros';
+$string['helpoffline'] = '<p>É útil quando a atribuição é executada fora do Moodle.</p><p>Os alunos podem ver uma descrição da atribuição, mas não podem carregar ficheiros nem nada disso. Classifique trabalhos normalmente, e os alunos receberão notificações das suas notas</p>';
+$string['helponline'] = '<p>Este tipo de trabalho requer que os utilizadores editem um texto, utilizando as ferramentas de edição normais. Os professores poderão classificá-los Online, podendo também adicionar comentários ou fazer alterações.</p><p>(Se estiver familiarizado com versões antigas do Moodle, este tipo de trabalho faz precisamente a mesma coisa que o antigo módulo JORNAL.)</p>';
+$string['helpupload'] = '<p>Este tipo de trabalho permite que sejam enviados vários ficheiros.</p>
+<p>Os ficheiros podem ser de qualquer tipo (documento de word, imagens, ficheiros zipados, etc.)</p>';
+$string['helpuploadsingle'] = '<p>Este tipo de trabalho permite a cada participante carregar um ficheiro simples, de qualquer tipo.</p> <p>Poderá ser um documento Word, ou uma imagem, ou um web site, ou qualquer coisa que submeta.</p>';
 $string['late'] = '$a atrasado';
 $string['maximumgrade'] = 'Nota máxima';
 $string['maximumsize'] = 'Tamanho máximo';
