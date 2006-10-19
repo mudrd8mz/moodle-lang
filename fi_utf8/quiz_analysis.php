7bbd65bb3f3d8437a1157643e49d277ba33d6b3d
Petri Asikainen <paca70>
2006-10-19 17:45:00 +0000

    Updates

--- fi_utf8/quiz_analysis.php
+++ fi_utf8/quiz_analysis.php
@@ -1,5 +1,17 @@
-<?PHP // $Id:
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.7 beta (2006101000)
 
 
+$string['analysis'] = 'Kohteen analyysi';
+$string['analysisdownload'] = 'Lataa analyysi';
+$string['analysisoptions'] = 'Analyysin valinnat';
+$string['analysistitle'] = 'Kohteen analyysitalukko';
+$string['attemptsall'] = 'Kaikki yritykset';
+$string['attemptselection'] = 'Montako yritystä analysoidaan kultakin käyttäjältä';
+$string['attemptsfirst'] = 'esimmäinen yritys';
+$string['attemptshighest'] = 'korkein arvio';
+$string['attemptslast'] = 'viimeinen yritys';
+$string['downloadooo'] = 'Lataa OpenOffice.org muodossa';
+$string['facilitytitle'] = '%% Oikein <br>';
 
 ?>
