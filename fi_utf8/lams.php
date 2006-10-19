bcbddf72b284191a115938b51cd434bba9b95528
Petri Asikainen <paca70>
2006-10-19 16:54:03 +0000

    Updates

--- fi_utf8/lams.php
+++ fi_utf8/lams.php
@@ -1,2 +1,29 @@
-<?php // $Id$
+<?PHP // $Id$ 
+      // lams.php - created with Moodle 1.7 beta (2006101000)
+
+
+$string['createSequence'] = 'Luo uusi jakso';
+$string['editSequence'] = 'Muokkaa valittua jaksoa';
+$string['error'] = 'Pahus, sattui virhe';
+$string['introduction'] = 'Johdanto';
+$string['lams:manage'] = 'Hallitse LAMS aktiviteettejä';
+$string['lams:participate'] = 'Osallistu LAMS aktiviteetteihin';
+$string['lamsoutline'] = 'LAMS Yhteenveto';
+$string['lesson'] = 'oppitunti';
+$string['notsetup'] = 'EI_MÄÄRITELTY';
+$string['openauthor'] = 'Avaa LAMS Hallinta';
+$string['openlearner'] = 'Avaa LAMS Opiskelija';
+$string['openmonitor'] = 'Avaa LAMS Monitori';
+$string['refreshSequenceList'] = 'Päivitä jaksolista';
+$string['selectExistingSequence'] = 'Valitse jakso tai luo uusi jakso';
+$string['sequence'] = 'Valitse jakso';
+$string['serverid'] = 'Anna palvelintunnus joka on saatu <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverkey'] = 'Anna palvelinavain joka on saatu <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverurl'] = 'Anna LAMS-palvelimen osoite esim. http://localhost:8080/lams';
+$string['useSequence'] = 'Käytä valittua jaksoa';
+$string['visibletostudents'] = 'Näytä tapahtumat opiskelijoille';
+$string['wikistartederror'] = 'Wikissä on jo sivuja - sitä ei voi muuttaa';
+$string['workspace'] = 'Valitse työtila';
+$string['wrongversionrange'] = '$a ei ole kelvollinen alue.';
+
 ?>
