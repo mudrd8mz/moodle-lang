8486f5e7163a77a23ea5b2bf6676c4a26b75007b
Petri Asikainen <paca70>
2006-10-21 18:50:36 +0000

    Updates

---
+++ fi_utf8/mimetypes.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // mimetypes.php - created with Moodle 1.7 beta (2006101000)
+
+
+$string['application/msword'] = 'Word-tiedosto';
+$string['application/pdf'] = 'Pdf-tiedosto';
+$string['application/vnd.ms-excel'] = 'Exec-taulukko';
+$string['application/vnd.ms-powerpoint'] = 'Powerpoint-esitys';
+$string['application/zip'] = 'zip-paketti';
+$string['audio/mp3'] = 'Mp3-äänitiedosto';
+$string['audio/wav'] = 'äänitiedosto';
+$string['document/unknown'] = 'tiedosto';
+$string['image/bmp'] = 'Bmb-kuva';
+$string['image/gif'] = 'Gif-kuva';
+$string['image/jpeg'] = 'Jpeg-kuva';
+$string['text/plain'] = 'tekstitiedosto';
+$string['text/rtf'] = 'Rtf-tiedosto';
+
+?>
