bbad384c77f344f6be81abc10018c93d21160a5b
Petri Asikainen <paca70>
2006-10-21 18:19:16 +0000

    Updates

--- fi_utf8/hotpot.php
+++ fi_utf8/hotpot.php
@@ -1,23 +1,25 @@
-<?PHP // $Id:
-
-
-      // hotpot.php - created with Moodle 1.5.2 + (2005060222)
+<?PHP // $Id$ 
+      // hotpot.php - created with Moodle 1.7 beta (2006101000)
 
 
 $string['abandoned'] = 'jätetty kesken';
-$string['allattempts'] = 'kaikki vastausyritykset';
-$string['allmycourses'] = 'kaikki kurssini';
+$string['addquizchain'] = 'Lisää kysymysketju';
+$string['allusers'] = 'Kaikki käyttäjät';
+$string['alwaysopen'] = 'Aina avoinna';
+$string['attemptsall'] = 'Kaikki yritykset';
+$string['attemptsbest'] = 'Paras yritys';
+$string['attemptsfirst'] = 'Ensimmäinen yritys';
+$string['attemptslast'] = 'Viimeinen yritys';
 $string['average'] = 'Keskiarvo';
-$string['bestattempt'] = 'paras vastausyritys';
 $string['checks'] = 'Tarkistuksia';
 $string['clues'] = 'Vihjeitä';
+$string['completed'] = 'Suoritettu';
 $string['copiedtoclipboard'] = 'Tämän sivun sisältö on kopioitu leikepöydälle';
 $string['copytoclipboard'] = 'Kopioi leikepöydälle';
 $string['correct'] = 'Oikein';
 $string['deleteabandoned'] = 'Poista kesken jätetyt';
 $string['deleteabandonedcheck'] = 'Haluatko varmasti poistaa kaikki $a kesken jätettyä vastausyritystä?';
-$string['deletedattempts'] = '$a vastausyritystä poistettiin';
-$string['deletedresponses'] = '$a vastausta poistettiin';
+$string['displaycoursenext'] = 'Näytä kurssin sivu seuraavaksi';
 $string['enterafilename'] = 'Kirjoita tiedoston nimi';
 $string['filetype'] = 'Tiedoston tyyppi';
 $string['forceplugins'] = 'Käytä medialaajennuksia';
@@ -33,7 +35,7 @@ $string['navigation_bar'] = 'Moodlen navigointipalkki';
 $string['navigation_buttons'] = 'Hot Potatoes kyselyn painikkeet';
 $string['navigation_frame'] = 'Moodlen navigointikehys';
 $string['navigation_give_up'] = 'Yksittäinen \"Luovuta\"-nappula';
-$string['navigation_iframe'] = 'Sisällytetty &lt;IFRAME&gt;';
+$string['navigation_iframe'] = 'Sisällytetty <IFRAME>';
 $string['navigation_none'] = 'Ei mitään';
 $string['noactivity'] = 'Ei toimintoja';
 $string['noresponses'] = 'Ei löydetty tietoa yksittäisistä kysymyksistä ja vastauksista.';
@@ -44,6 +46,7 @@ $string['outputformat_mobile'] = 'mobiili';
 $string['outputformat_v3'] = 'v3';
 $string['outputformat_v4'] = 'v4';
 $string['outputformat_v5'] = 'v5';
+$string['outputformat_v5_plus'] = 'v5+';
 $string['outputformat_v6'] = 'v6';
 $string['outputformat_v6_plus'] = 'v6+';
 $string['penalties'] = 'Pistevähennykset';
@@ -51,12 +54,11 @@ $string['questionshort'] = 'Q-$a';
 $string['quiztype'] = 'Kyselyn tyyppi';
 $string['resultssaved'] = 'Kyselyn tulokset tallennettiin';
 $string['score'] = 'Pisteet';
-$string['selectattempt'] = 'Valitse vastausyritys';
 $string['showhtmlsource'] = 'Näytä HTML-lähde';
 $string['shownextquiz'] = 'Näytä seuraava kysely';
 $string['showxmlsource'] = 'Näytä XML-lähde';
 $string['showxmltree'] = 'Näytä XML-puu';
-$string['thiscourse'] = 'tämä kurssi';
+$string['updatequizchain'] = 'Päivitä kysymysketju';
 $string['weighting'] = 'Painotus';
 $string['wrong'] = 'Väärin';
 
