dfa08fad4072a7986b2c98a9505d97d6b5f1f223
Andrea Bicciolo <andreabix>
2006-10-23 13:04:52 +0000

    Traduzione di Claudio Maggi

---
+++ it_utf8/simpletest.php
@@ -0,0 +1,32 @@
+<?PHP // $Id$ 
+      // simpletest.php - created with Moodle 1.7 beta (2006101000)
+
+
+$string['all'] = 'TUTTI';
+$string['exception'] = 'Eccezione';
+$string['fail'] = 'Errore';
+$string['ignorefile'] = 'Ignora i test nel file';
+$string['ignorethisfile'] = 'Riesegui i test ignorando questo file di test.';
+$string['moodleunittests'] = 'Test di Moodle: $a';
+$string['notice'] = 'Nota';
+$string['onlytest'] = 'Esegui i test solamente in';
+$string['pass'] = 'Eseguito con successo';
+$string['pathdoesnotexist'] = 'Il percorso \'$a\' non esiste';
+$string['retest'] = 'Riesegui i test';
+$string['retestonlythisfile'] = 'Riesegui solo questo file di test.';
+$string['runall'] = 'Esegui i test da tutti i file di test';
+$string['runat'] = 'Eseguito il $a.';
+$string['runonlyfile'] = 'Esegui solo i test in qusto file';
+$string['runonlyfolder'] = 'Esegui solo i test in questa cartella';
+$string['runtests'] = 'Esegui';
+$string['rununittests'] = 'Esegui i test';
+$string['showpasses'] = 'Mostra sia successi che errori';
+$string['showsearch'] = 'Mostra la ricerca dei file di test';
+$string['stacktrace'] = 'Traccia dello stack:';
+$string['summary'] = '{$a->run}/{$a->total} casi di test completi: <strong>{$a->passes}</strong> successi, <strong>{$a->fails}</strong> errori e <strong>{$a->exceptions}</strong> eccezioni.';
+$string['thorough'] = 'Esegui un test completo (può durare parecchio)';
+$string['uncaughtexception'] = 'Eccezione non prevista [{$a->getMessage()}] in [{$a->getFile()}:{$a->getLine()}] TEST INTERROTTO.';
+$string['unittests'] = 'Test';
+$string['version'] = 'Utilizzato <a href=\"http://sourceforge.net/projects/simpletest/\">SimpleTest</a> versione $a.';
+
+?>
