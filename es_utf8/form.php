1328a5e03851a25a2beb14f1a486b1b6e2faa71c
Benito Arias <barias>
2006-10-30 11:09:31 +0000

    *** empty log message ***

---
+++ es_utf8/form.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 1.6.2 (2006050520)
+
+
+$string['denotesreq'] = '$a denota que debe suministrar un valor en este campo.';
+$string['err_alphanumeric'] = 'Debe escribir sólo letras o números aquí.';
+$string['err_email'] = 'Debe escribir una dirección de email válida aquí.';
+$string['err_lettersonly'] = 'Debe escribir sólo letras aquí.';
+$string['err_maxlength'] = 'Debe escribir no más de $a->format caracteres aquí.';
+$string['err_minlength'] = 'Debe escribir al menos $a->format caracteres aquí.';
+$string['err_nonzero'] = 'Debe escribir aquí un número que no comience con cero.';
+$string['err_nopunctuation'] = 'No debe escribir ningún signo de puntuación aquí.';
+$string['err_numeric'] = 'Debe escribir un número aquí.';
+$string['err_rangelength'] = 'Debe escribir entre {$a->format[0]} y {$a->format[1]} caracteres aquí.';
+$string['err_required'] = 'Debe suministrar un valor aquí.';
+$string['nomethodforaddinghelpbutton'] = 'No hay posibilidad de añadir un botón de ayuda al elemento del formulario $a->name (class $a->classname)';
+$string['nonexistentformelements'] = 'Intentando añadir botones de ayuda a elementos de formulario inexistentes: $a';
+$string['requiredelement'] = 'Debe suministrar un valor aquí.';
+
+?>
