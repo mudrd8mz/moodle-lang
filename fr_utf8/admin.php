42fda9eaeec39d18cae92a1c6945d7c8b4f251ac
Nicolas Martignoni <martignoni>
2006-10-31 14:34:17 +0000

    Fixed double enableajax in previous commit MDL-7296

--- fr_utf8/admin.php
+++ fr_utf8/admin.php
@@ -4,7 +4,6 @@
 $string['experimental'] = 'Expérimental';
 $string['enableglobalsearch'] = 'Activer la recherche globale';
 $string['configenableglobalsearch'] = 'Ce réglage active le moteur de recherche plein texte global des ressources et activités, qui n\'est pas compatible avec PHP 4.';
-$string['enableajax'] = 'Activer AJAX';
 $string['autologinguests'] = 'Connecter automatiquement les invités';
 $string['search'] = 'Recherche';
 $string['searchresults'] = 'Résultats';
