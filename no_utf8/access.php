51c1ba9043e6b37f9e4ac08a1596d91fb61d5038
solveigb <solveigb>
2006-11-01 12:59:11 +0000

    1. translation

---
+++ no_utf8/access.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // access.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['access'] = 'Tilgjengelighet';
+$string['accesshelp'] = 'Hjelp om tilgjengelighet';
+$string['accesskey'] = 'Adgangsnøkkel, $a';
+$string['accessstatement'] = 'Adgangsmelding';
+$string['breadcrumb'] = 'Smulespor';
+$string['monthnext'] = 'Neste måned';
+$string['monthprev'] = 'Forrige måned';
+$string['showhideblock'] = 'Vis eller skjul boks';
+$string['sitemap'] = 'Oversikt over nettsted';
+$string['skipblock'] = 'Hopp over boks';
+$string['skipnavigation'] = 'Hopp over navigering';
+$string['tabledata'] = 'Datatabell, $a';
+$string['tablelayout'] = 'Layouttabell, $a';
+$string['tocontent'] = 'Gå til hovedinnhold';
+$string['tonavigation'] = 'Gå til navigering';
+$string['youarehere'] = 'Du befinner deg her';
+
+?>
