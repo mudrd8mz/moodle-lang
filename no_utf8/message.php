9fde619688ccb8e43d3028aa367dea2b97d57790
solveigb <solveigb>
2006-11-02 12:15:29 +0000

    Corrected some spelling errors, modified some chosen words and completed all entries.

--- no_utf8/message.php
+++ no_utf8/message.php
@@ -1,48 +1,48 @@
 <?PHP // $Id$ 
-      // message.php - created with Moodle 1.5.2 + (2005060222)
+      // message.php - created with Moodle 1.6.3 (2006050530)
 
 
 $string['addcontact'] = 'Legg til kontakt';
-$string['addsomecontacts'] = 'For å sende ei melding til noen, eller for å lage en snarvei til dem på denne siden, kan du bruke <a href=\"$a\">search tab</a>en  over.';
-$string['addsomecontactsincoming'] = 'Disse beskjedene er fra folk som ikke er i kontaktlista di. for å legge dem til blant dine kontakter kan du klikke på \"Legg til kontakt\"-ikonet  ved siden av navnet deres. ';
+$string['addsomecontacts'] = 'For å sende ei melding til noen, eller for å lage en snarvei til dem på denne siden, kan du bruke <a href=\"$a\">søkefanen</a> over.';
+$string['addsomecontactsincoming'] = 'Disse beskjedene er fra folk som ikke er i kontaktlisten din. For å legge dem til blant dine kontakter, kan du klikke på \"Legg til kontakt\"-ikonet  ved siden av navnet deres.';
 $string['ago'] = 'for $a siden';
 $string['allmine'] = 'Alle beskjeder fra eller til meg';
-$string['allstudents'] = 'Alle beskjedeer mellom studenter i klassen';
+$string['allstudents'] = 'Alle beskjeder mellom studenter i kurset';
 $string['allusers'] = 'Alle beskjeder fra alle brukere';
-$string['backupmessageshelp'] = 'Hvis du skrur på denne vil meldinger bli inkludert i de utomatiske sikkerhetskopiene av nettstedet.';
-$string['beepnewmessage'] = 'Gi lyd når det kommer en ny beskjed (kan kreve plugin i nettleseren din)';
+$string['backupmessageshelp'] = 'Hvis denne er slått på, vil meldinger bli inkludert i de automatiske sikkerhetskopiene av nettstedet.';
+$string['beepnewmessage'] = 'Gi lyd når det kommer en ny beskjed (kan kreve plugin i nettleseren din som kan spille av lyd, som f.eks. Quicktime)';
 $string['blockcontact'] = 'Blokker kontakten';
-$string['blockedmessages'] = '$a beksjed(er) fra bokkerte brukere';
-$string['blocknoncontacts'] = 'Blokker alle beskjeder fra folk som ikke er på kontaklista mi';
-$string['contactlistempty'] = 'Kontaktlista di er for tiden tom';
+$string['blockedmessages'] = '$a beskjed(er) til/fra bokkerte brukere';
+$string['blocknoncontacts'] = 'Blokker alle beskjeder fra folk som ikke er på kontaklisten min';
+$string['contactlistempty'] = 'Kontaktlisten din er for tiden tom';
 $string['contacts'] = 'Kontakter';
 $string['context'] = 'kontekst';
 $string['deletemessagesdays'] = 'Antallet dager før gamle beskjeder blir slettet';
 $string['discussion'] = 'Diskusjon';
-$string['emailmessages'] = 'E-postbeksjeder når jeg ikke er på nett';
-$string['emailtagline'] = 'Denne e-posten er en kopi av ei melding sendt til deg på \"$a\"';
+$string['emailmessages'] = 'E-postbeskjeder når jeg ikke er på nett';
+$string['emailtagline'] = 'Denne e-posten er en kopi av en melding sendt til deg på \"$a\"';
 $string['emptysearchstring'] = 'Du må søke etter noe';
 $string['formorethan'] = 'I mer enn';
-$string['includeblockedusers'] = 'ta med blokkerte brukere';
-$string['incomingcontacts'] = 'Incoming Contacts ($a)';
+$string['includeblockedusers'] = 'Ta med blokkerte brukere';
+$string['incomingcontacts'] = 'Innkommende kontakter ($a)';
 $string['keywords'] = 'Nøkkelord';
 $string['keywordssearchresults'] = 'Søkeresultater: $a meldinger funnet';
 $string['maxmessages'] = 'Maks antall meldinger å vise i diskusjonshistorien din';
 $string['message'] = 'Melding';
 $string['messagehistory'] = 'Meldingshistorie';
 $string['messages'] = 'Meldinger';
-$string['messaging'] = 'Melding';
+$string['messaging'] = 'Å sende meldinger';
 $string['mycontacts'] = 'Mine kontakter';
 $string['newsearch'] = 'Nytt søk';
 $string['nomessages'] = 'Ingen meldinger';
 $string['nomessagesfound'] = 'Ingen meldinger ble funnet';
-$string['nosearchresults'] = 'søket ditt fikk ingen resultater';
+$string['nosearchresults'] = 'Søket ditt ga ingen resultater';
 $string['offline'] = 'Ikke på nett';
 $string['offlinecontacts'] = 'Kontakter som ikke er på nett ($a)';
 $string['online'] = 'På nett';
 $string['onlinecontacts'] = 'Kontakter på nett ($a)';
 $string['onlyfromme'] = 'Bare meldinger fra meg';
-$string['onlymycourses'] = 'Bare i mine klasser';
+$string['onlymycourses'] = 'Bare i mine kurs';
 $string['onlytome'] = 'Bare meldinger til meg';
 $string['pagerefreshes'] = 'Denne siden oppdateres hvert $a sekund';
 $string['readmessages'] = '$a les meldinger';
@@ -55,14 +55,14 @@ $string['sendmessage'] = 'Send melding';
 $string['sendmessageto'] = 'Send melding til $a';
 $string['settings'] = 'Innstillinger';
 $string['settingssaved'] = 'Innstillingene dine har blitt lagret';
-$string['showmessagewindow'] = 'Vis automatisk meldingsvindu når jeg får nye meldinger (Nettleseren din må være inntillt lik at den ikke blokkerer oppsprettsvinduer fra denne siden)';
+$string['showmessagewindow'] = 'Vis automatisk meldingsvindu når jeg får nye meldinger (Nettleseren din må være inntillt slik at den ikke blokkerer oppsprettsvinduer fra denne siden)';
 $string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
 $string['timenosee'] = 'Minutter siden jeg var på nett sist';
 $string['timesent'] = 'Tidspunkt for sending';
-$string['unblockcontact'] = 'Ta kontakt inn i varmen';
+$string['unblockcontact'] = 'Opphev blokkering av kontakt';
 $string['unreadmessages'] = '$a uleste meldinger';
 $string['userisblockingyou'] = 'Denne brukeren har blokkert deg slik at du ikke skal kunne sende han/henne meldinger.';
-$string['userisblockingyounoncontact'] = 'Denne brukeren aksepterer bare meldinger fra folk i kontaktlista si. Du står ikke der.';
+$string['userisblockingyounoncontact'] = 'Denne brukeren aksepterer bare meldinger fra folk i kontaktlisten sin. Du står ikke der.';
 $string['userssearchresults'] = 'Søkeresultat: $a brukere funnet';
 
 ?>
