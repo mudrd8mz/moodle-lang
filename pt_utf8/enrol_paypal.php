d054c9bf6e483a246d14e49fe72442fbf2976325
Jaime Villate <villate>
2006-11-02 09:50:41 +0000

    Completed the translation

--- pt_utf8/enrol_paypal.php
+++ pt_utf8/enrol_paypal.php
@@ -1,8 +1,11 @@
 <?PHP // $Id$ 
-      // enrol_paypal.php - created with Moodle 1.4 development (2004062600)
+      // enrol_paypal.php - created with Moodle 1.6.2 (2006050521)
 
 
-$string['enrolname'] = '&quot;Paypal&quot;';
-$string['sendpaymentbutton'] = 'Enviar pagamento através de &quot;Paypal&quot;';
+$string['business'] = 'Endereço de correio electrónico da sua conta comercial no \"Paypal\"';
+$string['costorkey'] = 'Por favor escolha um dos métodos de inscrição que se seguem.';
+$string['description'] = 'O módulo \"Paypal\" permite-lhe criar disciplinas com inscrição paga. Se o custo de uma disciplina for nulo, então não será pedida nenhuma taxa de inscrição aos alunos. Pode configurar aqui uma taxa global de inscrição para qualquer disciplina no sítio, e depois poderá alterar essa taxa para cada disciplina individualmente. A taxa de inscrição de uma disciplina tem precedência sobre a taxa global.';
+$string['enrolname'] = '\"Paypal\"';
+$string['sendpaymentbutton'] = 'Enviar pagamento através de \"Paypal\"';
 
 ?>
--- pt_utf8/error.php
+++ pt_utf8/error.php
@@ -1,19 +1,73 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.4 development (2004062600)
+      // error.php - created with Moodle 1.6.2 (2006050521)
 
 
+$string['adminprimarynoedit'] = 'Os dados do administrador principal não podem ser editado
+s por outros';
+$string['cannotcreatelangdir'] = 'Não é possível criar o directório para pacotes linguísticos';
+$string['cannotcreatetempdir'] = 'Não é possível criar o directório temporal';
+$string['cannotdownloadcomponents'] = 'Não é possível descarregar a cópia dos componentes';
+$string['cannotdownloadzipfile'] = 'Não é possível descarregar a cópia do ficheiro ZIP';
+$string['cannotfindcomponent'] = 'Não foi encontrado um componente';
+$string['cannotsavemd5file'] = 'Não é possível gravar ficheiro md5';
+$string['cannotsavezipfile'] = 'Não  é possível gravar ficheiro ZIP';
+$string['cannotunzipfile'] = 'Não é possível descompactar ficheiro ZIP';
+$string['cmunknown'] = 'Não foi encontrado esse módulo de disciplina';
+$string['componentisuptodate'] = 'O componente está actualizado';
+$string['confirmsesskeybad'] = 'Lamentamos mas a sua chave se sessão não pode ser confirmada para poder levar a cabo esta acção. Esta característica de segurança previne execuções, acidentais ou maliciosas, de funções importantes em seu nome. Certifique-se por favor que quis realmente executar esta função.';
 $string['coursegroupunknown'] = 'Faltou especificar a disciplina correspondente ao grupo $a';
+$string['downloadedfilecheckfailed'] = 'Falhou a verificação do ficheiro descarregado.';
+$string['errorcleaningdirectory'] = 'Erro na limpeza do directório \"$a\"';
+$string['errorcopyingfiles'] = 'Erro na cópia de ficheiros';
+$string['errorcreatingdirectory'] = 'Erro na criação do directório \"$a\"';
+$string['errorcreatingfile'] = 'Erro na criação do ficheiro \"$a\"';
 $string['erroronline'] = 'Erro na linha $a';
-$string['fieldrequired'] = 'o campo &quot;$a&quot; é obrigatório';
+$string['errorreadingfile'] = 'Erro na leitura do ficheiro \"$a\"';
+$string['errorunzippingfiles'] = 'Erro na descompactação de ficheiros ZIP';
+$string['fieldrequired'] = 'o campo \"$a\" é obrigatório';
+$string['filenotfound'] = 'Lamentamos mas o ficheiro pedido não foi encontrado';
+$string['forumblockingtoomanyposts'] = 'Já ultrapassou o limite permitido de envio de mensagens para este fórum.';
 $string['groupalready'] = 'O utilizador já pertence ao grupo $a';
 $string['groupunknown'] = 'O grupo $a não está associado à disciplina indicada';
-$string['invalidfieldname'] = '&quot;$a&quot; não é um nome de campo válido';
-$string['missingfield'] = 'O campo ';
+$string['guestnoeditprofile'] = 'Os visitantes nãoe editar o seu perfil';
+$string['guestnoeditprofileother'] = 'O perfil do visitante não pode ser editado';
+$string['invalidcourse'] = 'Disciplina não válida';
+$string['invalidfieldname'] = '\"$a\" não é um nome de campo válido';
+$string['invalidfiletype'] = '\"$a\" não é um tipo de ficheiro válido';
+$string['invalidmd5'] = 'md5 não válido';
+$string['invalidxmlfile'] = '\"$a\" não é um ficheiro XML válido';
+$string['missingfield'] = 'O campo';
+$string['missingrequiredfield'] = 'Falta algum campo necessário';
+$string['modulemissingcode'] = 'O módulo $a não inclui o código necessário para executar esta função';
 $string['modulerequirementsnotmet'] = 'O módulo \"$a->modulename\" ($a->moduleversion) não pode ser instalado. Para a sua instalação é necessária uma versão mais recente do Moodle (actualmente está a usar a versão $a->currentmoodle e precisa da versão $a->requiremoodle).';
+$string['mustbeteacher'] = 'O acesso a esta página está reservado aos docentes';
+$string['noinstances'] = 'Não há nenhum recurso do tipo $a nesta disciplina!';
+$string['nonmeaningfulcontent'] = 'Conteúdo não significativo';
+$string['noparticipatorycms'] = 'Lamentamos mas não exit nenhum módulo de disciplina participativo para o qual possa enviar um relatório.';
 $string['notavailable'] = 'Isso não está disponível actualmente';
+$string['onlyadmins'] = 'Unicamente os administradores podem fazer isso';
+$string['onlyeditingteachers'] = 'Unicamente os docentes com permissão de edição podem fazer isso';
+$string['onlyeditown'] = 'Só pode editar a sua própria informação';
+$string['pagenotexist'] = 'Ocorreu um erro não habitual (tentativa de acesso a página que não existe)';
+$string['pleasereport'] = 'Se tiver tempo, por favor diga-nos o que estava a tentar fazer quando ocorreu esse erro.';
+$string['pluginrequirementsnotmet'] = 'O plugin \"$a->pluginname\" ($a->pluginversion) não pôde ser instalado porque precisa de uma versão mais recente do Moodle (actualmente está a utilizar a versão $a->currentmoodle, mas precisa da $a->requiremoodle).';
+$string['processingstops'] = 'O processamento terminou aqui. Os restantes registos foram ignorados.';
+$string['remotedownloadnotallowed'] = 'O seu servidor não permite descarregar componentes  (a variável allow_url_fopen está desactivada).<br /><br />Terá que descarregar <a href=\"$a->url\">$a->url</a> manualmente, copia-lo para  \"$a->dest\" no seu servidor e descompacta-lo.';
 $string['restricteduser'] = 'Lamentamos, mas na sua conta actual \"$a\" não lhe permite fazer isso.';
-$string['unknowncourse'] = 'Disciplina designada de ';
+$string['sendmessage'] = 'Enviar mensagem';
+$string['sessionerroruser'] = 'A sua sessão expirou. Por favor entre novamente no sistema.';
+$string['sessionerroruser2'] = 'Foi detectado um erro no servidor que afecta a sua sessão actual. Por favor entre novamente no sistema ou reinicie o seu navegador.';
+$string['sessionipnomatch'] = 'Lamentamos, mas parece que o seu IP mudou desde quando entrou inicialmente no sistema. Esta característica de segurança previne que os crackers lhe roubem a identidade enquanto estiver neste sítio. Normalmente não deveria receber este tipo de mensagem - peça ajuda ao administrador do sítio.';
+$string['statscatchupmode'] = 'Os relatórios de actividade encomtram-se em modo de actualização. Até agora foram processados $a->daysdone dia(s) e estão pendentes $a->dayspending. Tente mais tarde!';
+$string['unknowncourse'] = 'Disciplina designada de';
+$string['unknownuseraction'] = 'Lamento, mas não reconheço essa acção de utilizador.';
 $string['usernotaddederror'] = 'O utilizador \"$a\" não foi adicionado - erro desconhecido';
 $string['usernotaddedregistered'] = 'O utilizador \"$a\" não foi adicionado - já se encontra registado';
+$string['usernotavailable'] = 'Os dados desse utilizador não estão acessíveis para si.';
+$string['usernotrenamedexists'] = 'O nome de utilizador não foi alterado -- o novo nome de utilizador já está a ser usado por outra pessoa.';
+$string['usernotrenamedmissing'] = 'O nome de utilizador não foi alterado -- não foi encontrado o nove de utilizador antigo.';
+$string['wrongdestpath'] = 'Caminho de destino errado.';
+$string['wrongsourcebase'] = 'URL errada para a fonte.';
+$string['wrongzipfilename'] = 'Nome de ficheiro ZIP errado.';
 
 ?>
