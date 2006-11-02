35d7a4fdd1d69d813dea54ebada08f4cbcb93a7e
Mitsuhiro Yoshida <mits>
2006-11-02 01:49:50 +0000

    Added a new string file for feedback.

---
+++ ja_utf8/feedback.php
@@ -0,0 +1,49 @@
+<?PHP // $Id$ 
+      // feedback.php - created with Moodle 1.8 dev (2006102600)
+
+
+$string['analysis'] = '分析';
+$string['anonymous'] = '匿名';
+$string['average'] = '平均';
+$string['delete_entry'] = 'エントリを削除する';
+$string['delete_item'] = '質問を削除する';
+$string['delete_template'] = 'テンプレートを削除する';
+$string['delete_templates'] = 'テンプレートを削除する ...';
+$string['description'] = '説明';
+$string['edit_item'] = '質問を編集する';
+$string['edit_items'] = '質問を編集する ...';
+$string['item_name'] = '質問';
+$string['label'] = 'ラベル';
+$string['line_labels'] = 'ラベル';
+$string['line_values'] = 'レーティング';
+$string['modulename'] = 'フィードバック';
+$string['modulenameplural'] = 'フィードバック活動';
+$string['move_item'] = 'この質問を移動する';
+$string['movedown_item'] = 'この質問を下げる';
+$string['moveup_item'] = 'この質問を上げる';
+$string['page'] = 'ページ';
+$string['pagebreak'] = 'ページブレーク';
+$string['picture'] = '写真';
+$string['preview'] = 'プレビュー';
+$string['public'] = '公開';
+$string['publish_stats_on_students'] = '学生に分析を表示する';
+$string['question'] = '質問';
+$string['questions'] = '質問';
+$string['required'] = '必須';
+$string['responses'] = 'レスポンス';
+$string['separator_decimal'] = '.';
+$string['separator_thousand'] = ',';
+$string['show_all'] = 'すべてを表示する';
+$string['show_entries'] = 'レスポンスを表示する';
+$string['show_entry'] = 'レスポンスを表示する';
+$string['start'] = '開始';
+$string['stop'] = '終了';
+$string['template'] = 'テンプレート';
+$string['template_saved'] = 'テンプレートが保存されました。';
+$string['templates'] = 'テンプレート';
+$string['textarea_width'] = '幅';
+$string['textfield_size'] = 'テキストフィールド幅';
+$string['use_this_template'] = 'このテンプレートを使用する';
+$string['using_templates'] = 'テンプレートを使用する';
+
+?>
