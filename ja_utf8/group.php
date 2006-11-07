8f28ac1fdf4c6ae43cc2fd0cbdcc9b4049876860
Mitsuhiro Yoshida <mits>
2006-11-07 15:20:21 +0000

    Added a new translated strings for groups.

---
+++ ja_utf8/group.php
@@ -0,0 +1,35 @@
+<?PHP // $Id$ 
+      // group.php - created with Moodle 1.8 dev (2006102600)
+
+
+$string['cancel'] = 'キャンセル';
+$string['creategroup'] = 'グループを作成する';
+$string['creategrouping'] = 'グルーピングを作成する';
+$string['defaultgroupingname'] = 'グルーピング';
+$string['defaultgroupname'] = 'グループ';
+$string['defaultgroupprefix'] = 'グループ';
+$string['deletegrouping'] = 'グルーピングを削除する';
+$string['deleteselectedgroup'] = '選択したグループを削除する';
+$string['editgroupingpermissions'] = 'グルーピングパーミッションを編集する';
+$string['editgroupingsettings'] = 'グルーピング設定を編集する';
+$string['editgroupsettings'] = 'グループ設定を編集する';
+$string['group'] = 'グループ';
+$string['groupinfo'] = '選択したグループの情報';
+$string['groupinfomembers'] = '選択したメンバーの情報';
+$string['groupinfopeople'] = '選択した人の情報';
+$string['grouping'] = 'グルーピング';
+$string['groupingdescription'] = 'グルーピング説明';
+$string['groupingname'] = 'グルーピング名';
+$string['groupings'] = 'グルーピング';
+$string['groupmembers'] = 'グループメンバー';
+$string['groupmode'] = 'グループモード';
+$string['groupmodeforce'] = 'グループモードを強制する';
+$string['groupmy'] = 'マイグループ';
+$string['groupname'] = 'グループ名';
+$string['groups'] = 'グループ';
+$string['groupsseparate'] = '分離グループ';
+$string['groupsvisible'] = '可視グループ';
+$string['hidepicture'] = '写真を隠す';
+$string['removeselectedusers'] = '選択したユーザを削除する';
+
+?>
