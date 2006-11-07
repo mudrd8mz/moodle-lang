5fdd965c15de37f2cdcdce0fa9cc4c35572065e4
Roberto Pinna <bobopinna>
2006-11-07 16:42:10 +0000

    First translation

---
+++ it_utf8/enrol_manual.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // enrol_manual.php - created with Moodle 1.8 dev (2006102600)
+
+
+$string['description'] = 'Questo è il metodo standard di iscrizione. Ci sono due pricipali modi con cui uno studente può essere iscritto ad un particolare corso.
+<ul>
+<li>Un docente o un amministratore può iscriverli manualmente utilizzando il collegamento nel menu di Amministrazione del corso.</li>
+<li>Un corso può avere una password definita, conosciuta come \"chiave d\'accesso\". Tutti quelli che conoscono questa chiave possono iscriversi al corso.</li>
+</ul>';
+$string['enrolname'] = 'Iscrizione interna';
+
+?>
