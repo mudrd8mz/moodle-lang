4e432948d692e6c7ab505aa8b79b9686d209b5a3
Ethem Evlice <ethem>
2006-11-08 12:00:22 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/form.php
+++ tr_utf8/form.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // form.php - created with Moodle 1.8 dev (2006101001)
+      // form.php - created with Moodle 1.8 dev (2006102600)
 
 
 $string['denotesreq'] = '$a gerekli alanları gösterir.';
@@ -13,6 +13,7 @@ $string['err_nopunctuation'] = 'Buraya noktalama işareti girmemelisiniz.';
 $string['err_numeric'] = 'Buraya bir sayı girmelisiniz.';
 $string['err_rangelength'] = 'Buraya {$a->format[0]} ile {$a->format[1]} karakter arasında girmelisiniz.';
 $string['err_required'] = 'Buraya bir değer girmelisiniz.';
+$string['general'] = 'Genel Ayarlar';
 $string['nomethodforaddinghelpbutton'] = '$a->name (sınıf $a->classname) form ögesi için yardım butonu ekleme metodu yok.';
 $string['nonexistentformelements'] = 'Var olmayan form ögelerine yardım butonları eklenmeye çalışılıyor: $a';
 $string['requiredelement'] = 'Gerekli alan.';
---
+++ tr_utf8/group.php
@@ -0,0 +1,41 @@
+<?PHP // $Id$ 
+      // group.php - created with Moodle 1.8 dev (2006102600)
+
+
+$string['adduserstogroup'] = 'Kullanıcıları gruba ekle';
+$string['cancel'] = 'İptal';
+$string['creategroup'] = 'Grup oluştur';
+$string['defaultgroupdescription'] = 'Varsayılan grup açıklaması';
+$string['defaultgroupname'] = 'Grup';
+$string['defaultgroupprefix'] = 'Grup';
+$string['deleteselectedgroup'] = 'Seçili grubu sil';
+$string['editgroupsettings'] = 'Grup ayarlarını düzenle';
+$string['enrolmentkey'] = 'Kayıt anahtarı';
+$string['group'] = 'Grup';
+$string['groupdescription'] = 'Grup açıklaması';
+$string['groupfor'] = 'grup için';
+$string['groupinfo'] = 'Seçili grup hakkında bilgi';
+$string['groupinfomembers'] = 'Seçili üyeler hakkında bilgi';
+$string['groupinfopeople'] = 'Seçili kişiler hakkında bilgi';
+$string['groupmembers'] = 'Grup üyeleri';
+$string['groupmemberssee'] = 'Grup üyelerine bak';
+$string['groupmembersselected'] = 'Seçili grubun üyeleri';
+$string['groupmode'] = 'Grup modu';
+$string['groupmodeforce'] = 'Grup modu zorunluluğu';
+$string['groupmy'] = 'Grubum';
+$string['groupname'] = 'Grup adı';
+$string['groupnotamember'] = 'Üzgünüz, bu grubun üyesi değilsiniz';
+$string['groups'] = 'Gruplar';
+$string['groupsseparate'] = 'Ayrı gruplar';
+$string['groupsvisible'] = 'Görünür gruplar';
+$string['hidepicture'] = 'Resmi gizle';
+$string['javascriptrequired'] = 'Bu sayfa, Javascriptin etkin olmasını gerektirir.';
+$string['membersofselectedgroup'] = 'Seçili grubun üyeleri';
+$string['newpicture'] = 'Yeni resim';
+$string['numberofgroups'] = 'Grup sayısı';
+$string['numberofstudents'] = 'Öğrenci sayısı';
+$string['prefixforgroupnames'] = 'Grup adları için önek';
+$string['removeselectedusers'] = 'Seçili kullanıcıları sil';
+$string['save'] = 'Kaydet';
+
+?>
