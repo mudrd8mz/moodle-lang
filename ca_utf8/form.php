8e19f88bb808f6f1a43afaf7c407c6179cb63392
Carles Bellver <carlesbellver>
2006-11-10 12:59:59 +0000

    *** empty log message ***

---
+++ ca_utf8/form.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 1.7+ (2006101007)
+
+
+$string['denotesreq'] = '$a denota un camp requerit.';
+$string['nomethodforaddinghelpbutton'] = 'No hi ha cap mètode per a afegir un botó d\'ajuda a l\'element del formulari $a->name (classe $a->classname)';
+$string['nonexistentformelements'] = 'S\'està intentant afegir botons d\'ajuda a elements del formulari inexistents: $a';
+$string['requiredelement'] = 'Camp requerit.';
+
+?>
