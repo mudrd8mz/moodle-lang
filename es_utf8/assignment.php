8019d509b64b2ce1977ea845d8a57333985d20a2
Benito Arias <barias>
2006-11-10 09:11:17 +0000

    *** empty log message ***

--- es_utf8/assignment.php
+++ es_utf8/assignment.php
@@ -1,8 +1,10 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.6 (2006050506)
+      // assignment.php - created with Moodle 1.7 (2006101007)
 
 
-$string['allowmultiple'] = 'Permitir archivos múltiples';
+$string['allowdeleting'] = 'Permitir eliminar';
+$string['allowmaxfiles'] = 'Número máximo de archivos subidos';
+$string['allownotes'] = 'Permitir notas';
 $string['allowresubmit'] = 'Permitir reenvío';
 $string['assignment:grade'] = 'Calificar tarea';
 $string['assignment:submit'] = 'Enviar tarea';
@@ -17,20 +19,15 @@ $string['assignmentmailhtml'] = 'El profesor $a->teacher ha hecho comentarios so
 Puede verlos <a href=\"$a->url\">aquí</a>.';
 $string['assignmentname'] = 'Nombre de la tarea';
 $string['assignmenttype'] = 'Tipo de tarea';
-$string['attachfile'] = 'Adjuntar archivo';
-$string['attachfiletoassignment'] = 'Adjuntar archivo(s) a la tarea';
 $string['availabledate'] = 'Disponible en';
-$string['backtoassignment'] = 'Volver a la Tarea';
-$string['backtofeedback'] = 'Volver a la Retroalimentación';
-$string['choosereviewfile'] = 'Seleccionar archivo a subir como revisión de la tarea:';
 $string['comment'] = 'Comentario';
 $string['commentinline'] = 'Comentario en línea';
 $string['configitemstocount'] = 'Naturaleza de los ítems a contar en los envíos de los estudiantes en tareas fuera de línea.';
 $string['configmaxbytes'] = 'Tamaño máximo permitido por defecto para todas las tareas del sitio (sujeto a los límites del curso y otras variables del servidor)';
-$string['deletecheckfile'] = '¿Está absolutamente seguro de que desea eliminar este archivo?';
-$string['deleteednotification'] = 'El archivo ha sido eliminado.';
-$string['deletefail'] = 'El siguiente archivo no ha sido eliminado:';
+$string['confirmdeletefile'] = '¿Está totalmente seguro de que quiere eliminar este archivo?<br /><strong>$a</strong>';
+$string['deletefilefailed'] = 'No se pudo eliminar el archivo.';
 $string['description'] = 'Descripción';
+$string['draft'] = 'Borrador';
 $string['duedate'] = 'Fecha de entrega';
 $string['duedateno'] = 'No hay fecha de entrega';
 $string['early'] = '$a antes';
@@ -42,16 +39,9 @@ para \'$a->assignment\'
 Está disponible aquí:
 
 $a->url';
-$string['emailteachermailgroup'] = 'El grupo $a->groupname $a->groupmembers ha actualizado el envío de su tarea \'$a->assignment\'
-
-Está disponible aquí:
-
-$a->url';
 $string['emailteachermailhtml'] = '$a->username ha actualizado el envío de su tarea
 para <i>\'$a->assignment\'</i><br /><br />
 Está <a href=\"$a->url\">disponible en el sitio web</a>.';
-$string['emailteachermailhtmlgroup'] = 'El grupo $a->groupname $a->groupmembers ha actualizado el envío de su tarea <i>\'$a->assignment\'</i><br /><br />
-Está disponible en el sitio web <a href=\"$a->url\"></a>.';
 $string['emailteachers'] = 'Alertas de email a los profesores';
 $string['emptysubmission'] = 'Usted aún no ha enviado nada';
 $string['existingfiledeleted'] = 'Se ha borrado el archivo: $a';
@@ -60,27 +50,29 @@ $string['feedback'] = 'Comentario';
 $string['feedbackfromteacher'] = 'Comentarios del $a';
 $string['feedbackupdated'] = 'Comentario actualizado para $a personas';
 $string['graded'] = 'Calificado';
-$string['gradingstyle'] = 'Estilo de calificación';
 $string['guestnosubmit'] = 'Lo sentimos, los invitados no pueden enviar tareas. Para poder enviar su respuesta, antes tiene que registrarse o introducir sus datos de acceso.';
 $string['guestnoupload'] = 'Lo sentimos, los invitados no pueden realizar envíos.';
 $string['helpoffline'] = '<p>Esto resulta útil cuando la tarea se realiza fuera de Moodle, e.g., en algún lugar de internet o personalmente.</p><p>Los estudiantes pueden ver una descipción de la tarea, pero no pueden subir archivos de ninguna clase al servidor. Las calificaciones funcionan normalmente, y los estudiantes recibirán notificaciones sobre la calificación obtenida.</p>';
 $string['helponline'] = '<p>Esta clase de tareas pide a los usuarios que editen un texto, utilizando las herramientas de edición habituales. Los profesores pueden calificarlas en línea, e incluso incorporar comentarios o cambios.</p><p>(Si usted está familiarizado con versiones anteriores de Moodle, este tipo de tarea hace lo mismo que hacía el módulo Diario)</p>';
 $string['helpupload'] = '<p>Este tipo de tarea permite a cada participante subir uno o varios archivos, de cualquier tipo.</p>
 <p>Puede ser un documento en Word, imágenes, una web comprimida, o cualquier otro archivo que se les pida que envíen.</p>
-<p>Usted puede de igual modo subir múltiples archivos de respuesta de cualquier tipo.</p> ';
+<p>Usted puede de igual modo subir múltiples archivos de respuesta de cualquier tipo.</p>';
 $string['helpuploadsingle'] = '<p>Este tipo de tarea permite a los participantes subir un solo archivo de cualquier tipo.</p><p>Podría ser un documento procesado en Word, o una imagen, un sitio web comprimido, o cualquier cosa que les pida que envíen.</p>';
-$string['helpuploadsinglegroup'] = '<p>Este tipo de tarea permite que cada participante suba un solo archivo, de cualquier tipo.</p> <p>Podría ser un documento hecho con Word, una imagen, una web comprimida, o cualquier otra cosa.</p> <p>El envío de este usuario es también remitido por todos los miembros del grupo a que dicho usuario pertenece. Si el curso no tiene grupos, toda la clase se considera como un grupo.</p>';
+$string['hideintro'] = 'Ocultar descripción antes de la fecha disponible';
 $string['late'] = '$a después';
-$string['markingsubmitnotification'] = 'El archivo ha sido subido con éxito para su calificación.';
 $string['maximumgrade'] = 'Calificación máxima';
 $string['maximumsize'] = 'Tamaño máximo';
 $string['modulename'] = 'Tarea';
 $string['modulenameplural'] = 'Tareas';
-$string['namedeletefile'] = 'Está a punto de eliminar este archivo:';
 $string['newsubmissions'] = 'Tareas enviadas';
 $string['noassignments'] = 'Aún no hay tareas';
 $string['noattempts'] = 'No se ha intentado realizar esta tarea';
-$string['nofilesforsubmit'] = 'No se ha encontrado ningún archivo para remitir: ¿está seguro de que lo ha subido?';
+$string['nofiles'] = 'No se han enviado archivos';
+$string['nofilesyet'] = 'Aún no se han enviado archivos';
+$string['notavailableyet'] = 'Lo sentimos, esta tarea ya no está disponible.<br />Las instrucciones sobre la tarea se mostrarán aquí en la fecha que aparece más abajo.';
+$string['notes'] = 'Notas';
+$string['notesempty'] = 'No entrada';
+$string['notesupdateerror'] = 'Error al actualizar notas';
 $string['notgradedyet'] = 'No calificado aún';
 $string['notsubmittedyet'] = 'Aún no ha enviado esta tarea';
 $string['onceassignmentsent'] = 'Una vez que la tarea ha sido enviada para ser calificada, ya no podrá eliminarla ni adjuntar archivo(s).';
@@ -88,32 +80,26 @@ $string['overwritewarning'] = 'Advertencia: Si envía un nuevo archivo REEMPLAZA
 $string['pagesize'] = 'Envíos mostrados por página';
 $string['preventlate'] = 'Impedir envíos retrasados';
 $string['quickgrade'] = 'Permitir calificación rápida';
-$string['removelink'] = 'Quitar';
-$string['responsefile'] = 'Archivo de respuesta:';
-$string['responsesfromteacher'] = 'Respuestas subidas por el profesor';
+$string['responsefiles'] = 'Archivos de respuesta';
+$string['reviewed'] = 'Revisado';
 $string['saveallfeedback'] = 'Guardar todos mis comentarios';
 $string['sendformarking'] = 'Enviar para calificación';
-$string['step1'] = 'Paso 1:';
-$string['step2'] = 'Paso 2:';
 $string['submission'] = 'Envío';
+$string['submissiondraft'] = 'Borrador del envío';
 $string['submissionfeedback'] = 'Comentario sobre la tarea';
 $string['submissions'] = 'Envíos';
 $string['submissionsaved'] = 'Sus cambios se han guardado';
 $string['submissionsnotgraded'] = '$a envíos no calificados';
-$string['submissionstatus'] = 'Estado del envío:';
-$string['submissionstatusblank'] = 'En blanco';
-$string['submissionstatusdraft'] = 'Borrador';
-$string['submissionstatusmarked'] = 'Calificado';
-$string['submissionstatusreturned'] = 'Devuelto';
-$string['submissionstatussubmitted'] = 'Enviado';
 $string['submitassignment'] = 'Envíe su tarea usando este formulario';
+$string['submitedformarking'] = 'La tarea ya fue enviada para su revisión y no puede actualizarse';
 $string['submitformarking'] = 'Enviar tarea para calificación';
 $string['submitted'] = 'Enviada';
+$string['submittedfiles'] = 'Archivos enviados';
 $string['typeoffline'] = 'Actividad no en línea';
 $string['typeonline'] = 'Texto en línea';
 $string['typeupload'] = 'Subida avanzada de archivos';
 $string['typeuploadsingle'] = 'Subir un solo archivo';
-$string['typeuploadsinglegroup'] = 'Subida de archivo en equipo';
+$string['unfinalize'] = 'Volver a borrador';
 $string['uploadbadname'] = 'El nombre contiene caracteres incompatibles y no se pudo subir';
 $string['uploadedfiles'] = 'archivos subidos';
 $string['uploaderror'] = 'Ha ocurrido un error al guardar el archivo en el servidor';
@@ -122,10 +108,8 @@ $string['uploadfiletoobig'] = 'Lo sentimos, su archivo es demasiado grande (el l
 $string['uploadnofilefound'] = 'No seleccionó ningún archivo: ¿está seguro de haber seleccionado uno para subir?';
 $string['uploadnotregistered'] = '\'$a\' se subió correctamente, pero el envío no se registró!.';
 $string['uploadsuccess'] = '\'$a\' se ha subido correctamente';
-$string['uploadsuccessresponse'] = 'Archivo de respuesta subido con éxito.';
 $string['viewfeedback'] = 'Ver calificaciones y comentarios sobre la tarea';
 $string['viewsubmissions'] = 'Ver $a tareas enviadas';
-$string['wholeclass'] = 'Toda la clase';
 $string['yoursubmission'] = 'Su envío';
 
 ?>
