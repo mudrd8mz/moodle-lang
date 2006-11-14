f4f7555f4c86c8fe642cfc54106c3cf2fbf050e6
Mitsuhiro Yoshida <mits>
2006-11-14 10:22:42 +0000

    Fixed wrong Paypal spelling based on /lang/en_utf8 fix - MDL-7838.

--- ja_utf8/enrol_paypal.php
+++ ja_utf8/enrol_paypal.php
@@ -2,10 +2,10 @@
       // enrol_paypal.php - created with Moodle 1.8 dev (2006102600)
 
 
-$string['business'] = 'Paypalビジネスアカウントのメールアドレス';
+$string['business'] = 'PayPalビジネスアカウントのメールアドレス';
 $string['costorkey'] = '下記の登録方法から1つを選択してください。';
-$string['description'] = 'Paypalモジュールでは有料のコースを設定することができます。コースの費用がゼロの場合、学生は支払いを求められません。サイト全体に適用されるデフォルトの費用をここで設定することも、個々に設定することもできます。コース費用は、サイト全体の費用に優先されます。';
-$string['enrolname'] = 'Paypal';
-$string['sendpaymentbutton'] = 'Paypalで支払う';
+$string['description'] = 'PayPalモジュールでは、有料のコースを設定することができます。コースの費用がゼロの場合、学生は支払いを求められません。サイト全体に適用されるデフォルトの費用をここで設定することも、個々に設定することもできます。コース費用は、サイト全体の費用に優先されます。';
+$string['enrolname'] = 'PayPal';
+$string['sendpaymentbutton'] = 'PayPal経由で支払いを送信する';
 
 ?>
