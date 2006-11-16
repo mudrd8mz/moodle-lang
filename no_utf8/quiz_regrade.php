6c5b48ea6b808bd4c1140dec8cc827d9451c398e
solveigb <solveigb>
2006-11-16 23:10:54 +0000

    New translation

---
+++ no_utf8/quiz_regrade.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // quiz_regrade.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['regrade'] = 'Sett karakterer på nytt';
+
+?>
---
+++ no_utf8/quiz_responses.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // quiz_responses.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['pagesize'] = 'Forsøk pr side:';
+$string['reportresponses'] = 'Detaljerte svar';
+$string['responses'] = 'Detaljerte svar';
+$string['responsesoptions'] = 'Svaralternativer';
+$string['responsestitle'] = 'Detaljerte svar';
+
+?>
