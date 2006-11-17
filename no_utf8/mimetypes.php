453e2bbd938797100efa25052a48ede82f46312f
solveigb <solveigb>
2006-11-17 21:39:24 +0000

    First translation. All strings translated.

---
+++ no_utf8/mimetypes.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // mimetypes.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['application/msword'] = 'Word-dokument';
+$string['application/pdf'] = 'PDF-dokument';
+$string['application/vnd.ms-excel'] = 'Excel regneark';
+$string['application/vnd.ms-powerpoint'] = 'Powerpoint presentasjon';
+$string['application/zip'] = 'zip arkiv';
+$string['audio/mp3'] = 'MP3 lydfil';
+$string['audio/wav'] = 'lydfil';
+$string['document/unknown'] = 'fil';
+$string['image/bmp'] = 'ukomprimert BMP bilde';
+$string['image/gif'] = 'GIF bilde';
+$string['image/jpeg'] = 'JPEG bilde';
+$string['text/plain'] = 'tekstfil';
+$string['text/rtf'] = 'RTF-dokument';
+
+?>
