8ee7ba554268a411d5b010f4e696896332d72a1e
gniske <gniske>
2006-11-18 01:06:31 +0000

    First Revision

---
+++ da_utf8/my.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // my.php - created with Moodle 1.8 dev (2006102600)
+
+
+$string['mymoodle'] = 'Oversigt over mine kurser';
+$string['nocourses'] = 'Der er ikke kurser at vise';
+$string['noguest'] = '\'Kursusoversigten\' er ikke tilgængelig for gæster.';
+$string['pinblocks'] = 'Konfigurer faste paneler i kursusoversigten.';
+$string['pinblocksexplan'] = 'Alle panelindstillinger du konfigurere her vil være synlige (og kan ikke rettes) af andre brugere på deres \'Kursusoversigts\' side.';
+
+?>
