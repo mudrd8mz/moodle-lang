f3d1b4ed86ac4b8e42043b0cc33245c6fa29b7f9
Mitsuhiro Yoshida <mits>
2006-11-18 04:42:20 +0000

    Added a new translated string file for form.

---
+++ ja_utf8/form.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 1.8 dev (2006102600)
+
+
+$string['denotesreq'] = '$a は必須入力フィールドを意味します。';
+$string['err_alphanumeric'] = 'ここには文字または数字のみ入力してください。';
+$string['err_email'] = 'ここには有効なメールアドレスを入力してください。';
+$string['err_lettersonly'] = 'ここには文字のみ入力してください。';
+$string['err_maxlength'] = 'ここには最大 $a->format 文字を入力してください。';
+$string['err_minlength'] = 'ここには少なくとも $a->format 文字を入力してください。';
+$string['err_nonzero'] = 'ここにはゼロから開始されない数字を入力してください。';
+$string['err_nopunctuation'] = 'ここには句読点を入力しないでください。';
+$string['err_numeric'] = 'ここには数字を入力してください。';
+$string['err_rangelength'] = 'ここには {$a->format[0]} から {$a->format[1]} の文字を入力してください。';
+$string['err_required'] = 'ここには値を入力してください。';
+$string['general'] = '一般設定';
+$string['modstandardels'] = '一般モジュール設定';
+$string['nomethodforaddinghelpbutton'] = 'フォームエレメント $a->name (クラス$a->classname) にヘルプボタンを追加する方法がありません。';
+$string['nonexistentformelements'] = '存在しないフォームエレメント $a にヘルプボタンを追加しようとしています。';
+$string['requiredelement'] = '必須入力フィールド';
+
+?>
