131b78319795415224a8b1ee2d5ed50dccd1b3f9
gniske <gniske>
2006-11-24 23:16:35 +0000

    made corrections and added missing strings

--- da_utf8/block_search_forums.php
+++ da_utf8/block_search_forums.php
@@ -1,8 +1,8 @@
 <?PHP // $Id$ 
-      // block_search_forums.php - created with Moodle 1.6 development (2005101200)
+      // block_search_forums.php - created with Moodle 1.8 dev (2006102600)
 
 
 $string['advancedsearch'] = 'Advanceret søgning';
-$string['blocktitle'] = 'Søg forums';
+$string['blocktitle'] = 'Søg i fora';
 
 ?>
---
+++ da_utf8/blog.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // blog.php - created with Moodle 1.6 + (2006050506)
+
+
+$string['addnewentry'] = 'Blog - nyt indlæg';
+$string['blogdeleteconfirm'] = 'SKAL denne blog slettes?';
+$string['blogpreferences'] = 'Blogindstillinger';
+$string['entrybody'] = 'Blogtekst';
+$string['entrysaved'] = 'Bloggen er gemt';
+$string['entrytitle'] = 'Overskrift';
+$string['entryupdated'] = 'Bloggen er opdateret';
+$string['otags'] = 'Officielle nøgleord';
+$string['ptags'] = 'Brugeroprettede nøgleord';
+$string['publishto'] = 'Tilgængelig for';
+$string['publishtonoone'] = 'Dig selv (kladde)';
+$string['publishtosite'] = 'Alle på danskonline.dk';
+$string['publishtoworld'] = 'Alle på Internettet';
+$string['updateentrywithid'] = 'Rediger blog';
+$string['viewmyentries'] = 'Vis mine blog';
+$string['viewsiteentries'] = 'Vis andres blog';
+
+?>
