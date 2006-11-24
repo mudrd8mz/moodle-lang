225f40b02a1bdb4378779f8441f987c93f5307bd
gniske <gniske>
2006-11-24 23:25:13 +0000

    made corrections and added missing strings

--- da_utf8/assignment.php
+++ da_utf8/assignment.php
@@ -1,8 +1,14 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.6 development (2005101200)
+      // assignment.php - created with Moodle 1.8 dev (2006102600)
 
 
+$string['allowdeleting'] = 'Tillad sletning';
+$string['allowmaxfiles'] = 'Maksimale antal uploadede filer';
+$string['allownotes'] = 'Tillad noter';
 $string['allowresubmit'] = 'Tillad gentagelser';
+$string['assignment:grade'] = 'Bedøm opgaven';
+$string['assignment:submit'] = 'Aflever opgaven';
+$string['assignment:view'] = 'Vis opgaven';
 $string['assignmentdetails'] = 'Opgave detaljer';
 $string['assignmentmail'] = '$a->teacher har kikket det du har afleveret igennem og kommenteret det \'$a->assignment\'
 
@@ -16,26 +22,30 @@ $string['assignmenttype'] = 'Opgave type';
 $string['availabledate'] = 'Tilgængelig fra';
 $string['comment'] = 'Kommentar';
 $string['commentinline'] = 'Indlejret kommentar';
-$string['configmaxbytes'] = 'General maksimum filstørrelse for alle afleveringsopgaver på sitet (Underlagt kursus begrænsninger og andre lokale indstillinger)';
+$string['configmaxbytes'] = 'Generel maksimum filstørrelse for alle afleveringsopgaver på sitet (Underlagt kursus begrænsninger og andre lokale indstillinger)';
+$string['deletefilefailed'] = 'Det lykkedes ikke at slette filen';
 $string['description'] = 'Beskrivelse';
+$string['draft'] = 'Kladde';
 $string['duedate'] = 'Afleveringsdato';
 $string['duedateno'] = 'Ingen afleveringsfrist';
 $string['early'] = '$a for tidlig';
-$string['editmysubmission'] = 'Ret min aflevering';
+$string['editmysubmission'] = 'Besvar opgaven';
+$string['emailstudents'] = 'Email besked til eleverne';
 $string['emailteachermail'] = '$a->username har opdateret deres opgaveafleveringer for \'$a->assignment\'
 
 Det kan hentes her:
 $a->url';
 $string['emailteachermailhtml'] = '$a->username har opdateret deres opgaveaflevering for <i>\'$a->assignment\'</i><br /><br />Det kan hentes på <a href=\"$a->url\">websiden</a>.';
-$string['emailteachers'] = 'E-mail advarsel til lærere';
+$string['emailteachers'] = 'E-mail notifikation til lærere';
 $string['emptysubmission'] = 'Du har ikke afleveret noget endnu';
 $string['existingfiledeleted'] = 'Eksisterende fil er blevet slettet: $a';
 $string['failedupdatefeedback'] = 'Mislykkedes at opdatere feedback på aflevering for bruger $a';
 $string['feedback'] = 'Feedback';
 $string['feedbackfromteacher'] = 'Feedback fra $a';
-$string['feedbackupdated'] = 'Afleverings feedback opdateret for $a ';
+$string['feedbackupdated'] = 'Afleverings feedback opdateret for $a';
+$string['graded'] = 'Bedømt';
 $string['guestnoupload'] = 'Desværre, gæster kan ikke uploade.';
-$string['helpoffline'] = '<p>Denne er brugbar når selve opgaven udføres udenfor Moodle. Det kunne være et andet sted på nettet, eller i klassen.</p><p>Eleverne kan se en beskrivelse af opgaven, men kan ikke uploade filer eller ligenene. Pointgivning fungere normalt, og elever vil få besked når deres indsats er blevet vurderet.</p>';
+$string['helpoffline'] = '<p>Denne er brugbar når selve opgaven udføres udenfor Moodle. Det kunne være et andet sted på nettet, eller i klassen. - Eller den kan bruges hvis man vil have at besvarelsen skal lægges i et forum.</p><p>Eleverne kan se en beskrivelse af opgaven, men kan ikke uploade filer eller ligenene. Pointgivning fungerer normalt, og elever vil få besked når deres indsats er blevet vurderet.</p>';
 $string['helponline'] = '<p>Denne opgave kræver at eleverne skriver noget tekst. Underviseren kan vurdere vurdere dem online og tilmed rette, eller kommentere i selve teksten.</p><p>(Hvis du kender tidligere versioner af Moodle, er denne opgavetype den sammen som den der førhen hed Journal)</p>';
 $string['helpuploadsingle'] = '<p>Denne type opgave tillader deltagere at uploade en enkelt fil af vilkårlig type.</p> <p>Det kan være et worddokument, et billede, et zippet website, eller hvad underviseren nu beder eleverne om at aflevere.</p>';
 $string['late'] = '$a for sent';
@@ -45,31 +55,39 @@ $string['modulename'] = 'Opgave';
 $string['modulenameplural'] = 'Opgaver';
 $string['newsubmissions'] = 'Opgaver afleveret';
 $string['noassignments'] = 'Der er ingen opgaver endnu';
-$string['noattempts'] = 'Der er ikke nogen der afleveret noget til denne opgave endnu.';
+$string['noattempts'] = 'Ingen har afleveret noget til denne opgave endnu.';
+$string['nofiles'] = 'Ingen filer er afleveret';
+$string['nofilesyet'] = 'Ingen filer er afleveret endnu';
+$string['notes'] = 'Noter';
+$string['notesupdateerror'] = 'Der opstod en fejl under opdatering af noter';
 $string['notgradedyet'] = 'Ikke bedømt endnu';
 $string['notsubmittedyet'] = 'Ikke afleveret endnu';
 $string['overwritewarning'] = 'Advarsel: hvis du uploader igen, vil det erstatte den nuværende version';
 $string['pagesize'] = 'Vis antal afleveringer pr side';
-$string['preventlate'] = 'Modtag ikke opgaver afleveret for sent';
+$string['preventlate'] = 'Modtag kun opgaver afleveret til tiden';
 $string['quickgrade'] = 'Hurtig vurdering tilladt';
+$string['reviewed'] = 'Gennemset';
 $string['saveallfeedback'] = 'Gem alt mit feedback';
+$string['sendformarking'] = 'Send til markering';
 $string['submission'] = 'Aflevering';
+$string['submissiondraft'] = 'Afleveringskladde';
 $string['submissionfeedback'] = 'Afleverings feedback';
 $string['submissions'] = 'Afleveringer';
 $string['submissionsaved'] = 'Ændringer er gemt';
 $string['submitassignment'] = 'Aflevér din opgave ved hjælp af denne formular';
 $string['submitted'] = 'Afleveret';
+$string['submittedfiles'] = 'Afleverede filer';
 $string['typeoffline'] = 'Offline aktivitet';
 $string['typeonline'] = 'Online tekst';
 $string['typeuploadsingle'] = 'Upload en enkelt fil';
-$string['uploadbadname'] = 'Dette filnavn indeholdt ugyldige tegn og kunne ikke uploades';
-$string['uploadedfiles'] = 'uploaded filer';
+$string['uploadbadname'] = 'Dette filnavn indeholdt ugyldige tegn og kunne ikke uploades.';
+$string['uploadedfiles'] = 'uploadede filer';
 $string['uploaderror'] = 'Fejl mens filen blev gemt på serveren';
-$string['uploadfailnoupdate'] = 'Filen var OK uploaded, men din aflevering blev ikke opdateret';
+$string['uploadfailnoupdate'] = 'Filen var OK uploadet, men din aflevering blev ikke opdateret';
 $string['uploadfiletoobig'] = 'Beklager, men filen er for stor (grænse er $a bytes)';
-$string['uploadnofilefound'] = 'Ingen filer fundet, er du sikker på at du har valgt at uploade en? Det kan være at filen er for stor. ';
-$string['uploadnotregistered'] = '\'$a\' blev uploaded OK, men afleveringen blev ikke registreret';
-$string['uploadsuccess'] = 'Uploaded \'$a\' korrekt';
+$string['uploadnofilefound'] = 'Ingen filer fundet, er du sikker på at du har valgt at uploade en? Det kan være at filen er for stor.';
+$string['uploadnotregistered'] = '\'$a\' blev uploadet OK, men afleveringen blev ikke registreret';
+$string['uploadsuccess'] = 'Uploadet \'$a\' korrekt';
 $string['viewfeedback'] = 'Se opgave karakterer og feedback';
 $string['viewsubmissions'] = 'Se $a afleverede opgaver';
 $string['yoursubmission'] = 'Din aflevering';
