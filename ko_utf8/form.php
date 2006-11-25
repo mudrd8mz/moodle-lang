b4a08311e1ebf49e2192a98dcb4f95a75f159f42
Lee Chan-young <yogibear270>
2006-11-25 12:50:46 +0000

    Translate added strings

--- ko_utf8/form.php
+++ ko_utf8/form.php
@@ -1,7 +1,8 @@
 <?PHP // $Id$ 
-      // form.php - created with Moodle 1.8 dev (2006102600)
+      // form.php - created with Moodle 1.8 dev (2006112200)
 
 
+$string['addfields'] = '양식에 $a 필드 추가';
 $string['denotesreq'] = '$a 는 필요한 필드를 나타냅니다.';
 $string['err_alphanumeric'] = '알파벳이나 숫자만 입력하라.';
 $string['err_email'] = '쓸 수 있는 이메일 주소를 입력하.';
@@ -14,6 +15,7 @@ $string['err_numeric'] = '숫자만 입력하라.';
 $string['err_rangelength'] = '{$a->format[0]} 에서 {$a->format[1]} 글자의 문자만 입력하라.';
 $string['err_required'] = '적당한 값을 입력하라.';
 $string['general'] = '일반 설정';
+$string['miscellaneoussettings'] = '기타 설정';
 $string['modstandardels'] = '일반 모듈 설정';
 $string['nomethodforaddinghelpbutton'] = '양식 요소 $a->name (학급 $a->classname)에 도움말 단추를 추가할 방법이 없습니다.';
 $string['nonexistentformelements'] = '존재하지 않는 양식 요소 $a 에 도움말 단추를 추가하려 함';
