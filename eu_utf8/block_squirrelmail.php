460b60a7c933b063d58adf292aac173baaef022d
Abel Camacho <ueu_ueu>
2006-11-27 09:08:57 +0000

    SUE

---
+++ eu_utf8/block_squirrelmail.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+$string['blockname'] = 'Irakasleen webmaila';
+$string['adminview'] = 'Zein da zure squirrelmailaren URLa?';
+$string['mustlogin'] = 'Webmailera sartzeko logeatuta egon behar duzu';
+$string['username'] = 'Erabiltzaile-izena';
+$string['password'] = 'Pasahitza';
+$string['go'] = 'Joan';
+?>
