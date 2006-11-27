9cdef9f4892e0dfcd89d0e3f37ca6385d19880ce
Jaime Villate <villate>
2006-11-27 16:51:57 +0000

    Made the translation consistent with other files and completed missing 1.7 strings

--- pt_utf8/blog.php
+++ pt_utf8/blog.php
@@ -1,56 +1,59 @@
 <?PHP // $Id$ 
-      // blog.php - created with Moodle 1.6 (2006050506)
+      // blog.php - created with Moodle 1.6.2 (2006050521)
 
 
-$string['addnewentry'] = 'Adicionar nova entrada';
+$string['addnewentry'] = 'Adicionar novo tópico';
 $string['addotags'] = 'Adicionar separadores oficiais';
 $string['addptags'] = 'Adicionar separadores definidos pelo utilizador';
 $string['blockmenutitle'] = 'Menu do Blog';
 $string['blocktagstitle'] = 'Separadores do Blog';
-$string['blocktitle'] = 'Título do bloco dos separadores do Blog ';
+$string['blocktitle'] = 'Título do bloco dos separadores do Blog';
 $string['blog'] = 'Blog';
 $string['blogdeleteconfirm'] = 'Apagar o Blog?';
 $string['blogpreferences'] = 'Preferências do Blog';
 $string['blogs'] = 'Blogs';
 $string['blogtags'] = 'Separadores do Blog';
 $string['courseblog'] = 'Blog da Disciplina: $a';
-$string['courseblogs'] = 'Utilizadores apenas podem ver blogs de pessoas que partilham a Disciplina';
-$string['disableblogs'] = 'Desactivar completamente o sistema de Blogs';
-$string['entrybody'] = 'Corpo de entrada do Blog';
-$string['entrybodyonlydesc'] = 'Descrição de entrada';
-$string['entryerrornotyours'] = 'Esta entrada não é sua';
-$string['entrysaved'] = 'A sua entrada foi gravada';
-$string['entrytitle'] = 'Título de entrada';
-$string['entryupdated'] = 'Actualizada entrada do Blog';
-$string['groupblog'] = 'Blog do Grupo: $a';
-$string['groupblogs'] = 'Utilizadores apenas podem ver blogs de pessoas que partilham um Grupo';
-$string['intro'] = 'Este feed RSS foi gerado automaticamente a partir de um ou mais blogs';
-$string['noentriesyet'] = 'Não há entradas vísiveis aqui';
+$string['courseblogs'] = 'Os utilizadores apenas podem ver blogs de pessoas que partilham uma disciplina';
+$string['disableblogs'] = 'Desactivar completamente o sistema de blogs';
+$string['emptybody'] = 'O corpo de um tópico do blog não pode estar vazio';
+$string['emptytitle'] = 'O título de um tópico do blog não pode estar vazio';
+$string['entrybody'] = 'Corpo do tópico do blog';
+$string['entrybodyonlydesc'] = 'Descrição do tópico';
+$string['entryerrornotyours'] = 'Este tópico não é seu';
+$string['entrysaved'] = 'O seu tópico foi gravado';
+$string['entrytitle'] = 'Título do tópico';
+$string['entryupdated'] = 'Actualizado o tópico do blog';
+$string['groupblog'] = 'Blog do grupo: $a';
+$string['groupblogs'] = 'Os utilizadores apenas podem ver blogs de pessoas que partilham um grupo';
+$string['intro'] = 'Esta porção RSS foi gerada automaticamente a partir de um ou mais blogs';
+$string['noentriesyet'] = 'Aqui não há tópicos vísiveis';
 $string['noguestpost'] = 'Convidados não podem escrever nos blogs!';
 $string['norighttodeletetag'] = 'Não tem permissões para apagar este separador - $a';
-$string['notallowedtoedit'] = 'Não está autorizado a editar esta entrada';
-$string['numberofentries'] = 'Entradas: $a';
+$string['notallowedtoedit'] = 'Não está autorizado a editar este tópico';
+$string['numberofentries'] = 'Tópicos: $a';
 $string['numberoftags'] = 'Número de separadores a mostrar';
 $string['otags'] = 'Separadores oficiais';
-$string['pagesize'] = 'Número de entradas do blog por página';
-$string['personalblogs'] = 'Utilizadores apenas podem ver os seus blogs';
-$string['ptags'] = 'Utilizar separadores definidos';
+$string['pagesize'] = 'Número de tópicos do blog por página';
+$string['personalblogs'] = 'Os utilizadores apenas podem ver os seus blogs';
+$string['ptags'] = 'Separadores definidos pelo utilizador (separados por vírgulas)';
 $string['publishto'] = 'Publicar para';
 $string['publishtonoone'] = 'O próprio (esboço)';
-$string['publishtosite'] = 'Qualquer pessoa neste site';
-$string['publishtoworld'] = 'Qualquer pessoa no Mundo';
+$string['publishtosite'] = 'Qualquer pessoa neste sítio';
+$string['publishtoworld'] = 'Qualquer pessoa no mundo';
 $string['settingsupdatederror'] = 'Ocorreu um erro, a definição de preferências do blog não pôde ser actualizada';
-$string['siteblog'] = 'Blog do site: $a';
-$string['siteblogs'] = 'Todos os utilizadores do site podem ver todas as entradas dos blogs';
+$string['siteblog'] = 'Blog do sítio: $a';
+$string['siteblogs'] = 'Todos os utilizadores do sítio podem ver todos os 
+tópicos dos blogs';
 $string['tagdatelastused'] = 'Data da última utilização do separador';
-$string['tagsort'] = 'Mostrar separadores ordenados';
+$string['tagsort'] = 'Mostrar separadores ordenados por';
 $string['tagtext'] = 'Separador de texto';
 $string['timewithin'] = 'Mostrar separadores utilizados durante estes dias';
-$string['updateentrywithid'] = 'Actualizando entrada';
+$string['updateentrywithid'] = 'Actualizando tópico';
 $string['userblog'] = 'Blog do utilizador: $a';
-$string['viewcourseentries'] = 'Ver entradas na Disciplina';
-$string['viewmyentries'] = 'Ver as minhas entradas';
-$string['viewsiteentries'] = 'Ver entradas no site';
-$string['worldblogs'] = 'O Mundo pode ler as entradas definidas para ser acessíveis ao Mundo';
+$string['viewcourseentries'] = 'Ver tópicos na disciplina';
+$string['viewmyentries'] = 'Ver os meus tópicos';
+$string['viewsiteentries'] = 'Ver tópicos no sítio';
+$string['worldblogs'] = 'O mundo pode ler os tópicos designados como acessíveis por todo o mundo';
 
 ?>
