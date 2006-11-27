791fb110fe3c8dd7ba0b204e849c5fa2a98594c9
Nicolas Martignoni <martignoni>
2006-11-27 14:04:21 +0000

    Typo corrected

--- fr_utf8/quiz_overview.php
+++ fr_utf8/quiz_overview.php
@@ -1,7 +1,7 @@
 <?php // $Id$
 
 $string['attemptsonly'] = 'N\'afficher que les $a avec tentatives';
-$string['allattempts'] = 'Afficher toutes les tentativess';
+$string['allattempts'] = 'Afficher toutes les tentatives';
 $string['allstudents'] = 'Afficher tous les $a';
 $string['noattemptsonly'] = 'N\'afficher que les $a sans tentatives';
 $string['overview'] = 'Vue d\'ensemble';
