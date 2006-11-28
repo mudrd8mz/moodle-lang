c95e240524989c2b6cad79824cbdcf76caad42d5
Nicolas Martignoni <martignoni>
2006-11-28 21:15:54 +0000

    fix for MDL-7494, add information to help screen readers deal with cloze questions

--- fr_utf8/qtype_multichoice.php
+++ fr_utf8/qtype_multichoice.php
@@ -5,6 +5,7 @@ $string['answersingleno'] = 'Réponses multiples autorisées';
 $string['answersingleyes'] = 'Une seule réponse';
 $string['choiceno'] = 'Réponse $a';
 $string['choices'] = 'Réponses possibles';
+$string['clozeaid'] = 'Tapez le mot manquant';
 $string['editingmultichoice'] = 'Modification d\'une question à choix multiples';
 $string['fillouttwochoices'] = 'Vous devez donner au moins deux réponses. Les réponses laissés vides ne seront pas utilisées.';
 $string['fractionsaddwrong'] = 'La somme des notes positives que vous avez choisies n\'est pas égale à 100%%.<br />Leur somme est de $a%%.<br />Voulez-vous revenir en arrière pour corriger cette question&nbsp;?';
