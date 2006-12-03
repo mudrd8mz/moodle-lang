416f3622ed78474e288cc26ea48405cf0c8ca296
Nicolas Martignoni <martignoni>
2006-12-03 12:27:00 +0000

    Sort strings

--- fr_utf8/form.php
+++ fr_utf8/form.php
@@ -1,24 +1,23 @@
 <?php // $Id$
 
+$string['addfields'] = 'Ajouter $a champs au formulaire';
 $string['denotesreq'] = '$a indique que le champ doit être renseigné.';
-$string['requiredelement'] = 'Champ requis';
-$string['nonexistentformelements'] = 'Tentative d\'ajout de bouton d\'aide à des éléments non existants&nbsp;: $a';
-$string['nomethodforaddinghelpbutton'] = 'Il n\'y a pas de méthode pour ajouter un bouton d\'aide sur un élément $a->name (classe $a->classname) de formulaire';
-//error messages for standard rules
-$string['err_required'] = 'Vous devez remplir ce champ.';
-$string['err_maxlength'] = 'Vous ne pouvez pas taper plus de $a->format caractères dans ce champ.';
-$string['err_minlength'] = 'Vous devez taper au moins $a->format caractères dans ce champ.';
-$string['err_numeric'] = 'Vous devez spécifier un nombre dans ce champ.';
-$string['err_rangelength'] = 'Vous devez taper entre {$a->format[0]} et {$a->format[1]} caractères dans ce champ.';
+$string['err_alphanumeric'] = 'Vous ne devez saisir que des chiffres ou des lettres dans ce champ.';
 $string['err_email'] = 'Vous devez indiquer une adresse de courriel valable dans ce champ.';
 $string['err_lettersonly'] = 'Vous ne devez saisir que des lettres dans ce champ.';
-$string['err_alphanumeric'] = 'Vous ne devez saisir que des chiffres ou des lettres dans ce champ.';
-$string['err_nopunctuation'] = 'Vous ne devez saisir aucune ponctuation dans ce champ.';
+$string['err_maxlength'] = 'Vous ne pouvez pas taper plus de $a->format caractères dans ce champ.';
+$string['err_minlength'] = 'Vous devez taper au moins $a->format caractères dans ce champ.';
 $string['err_nonzero'] = 'Vous ne devez pas saisir de nombre commençant par 0 dans ce champ.';
+$string['err_nopunctuation'] = 'Vous ne devez saisir aucune ponctuation dans ce champ.';
 $string['err_numeric'] = 'Vous devez saisir un nombre dans ce champ.';
+$string['err_numeric'] = 'Vous devez spécifier un nombre dans ce champ.';
+$string['err_rangelength'] = 'Vous devez taper entre {$a->format[0]} et {$a->format[1]} caractères dans ce champ.';
+$string['err_required'] = 'Vous devez remplir ce champ.';
 $string['general'] = 'Réglages généraux';
-$string['modstandardels'] = 'Réglages communs aux modules';
 $string['miscellaneoussettings'] = 'Réglages divers';
-$string['addfields'] = 'Ajouter $a champs au formulaire';
+$string['modstandardels'] = 'Réglages communs aux modules';
+$string['nomethodforaddinghelpbutton'] = 'Il n\'y a pas de méthode pour ajouter un bouton d\'aide sur un élément $a->name (classe $a->classname) de formulaire';
+$string['nonexistentformelements'] = 'Tentative d\'ajout de bouton d\'aide à des éléments non existants&nbsp;: $a';
+$string['requiredelement'] = 'Champ requis';
 
 ?>
