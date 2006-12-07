ea40784ef0386391926735497ef5ce56481359a4
Mitsuhiro Yoshida <mits>
2006-12-07 21:25:30 +0000

    Translated new strings for form.

--- ja_utf8/form.php
+++ ja_utf8/form.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // form.php - created with Moodle 1.8 dev (2006112200)
+      // form.php - created with Moodle 1.8 dev (2006120700)
 
 
 $string['addfields'] = '$a フィールドをフォームに追加する';
+$string['day'] = '日';
 $string['denotesreq'] = '$a は必須入力フィールドを意味します。';
 $string['err_alphanumeric'] = 'ここには文字または数字のみ入力してください。';
 $string['err_email'] = 'ここには有効なメールアドレスを入力してください。';
@@ -15,10 +16,14 @@ $string['err_numeric'] = 'ここには数字を入力してください。';
 $string['err_rangelength'] = 'ここには {$a->format[0]} から {$a->format[1]} の文字を入力してください。';
 $string['err_required'] = 'ここには値を入力してください。';
 $string['general'] = '一般設定';
+$string['hour'] = '時';
+$string['minute'] = '分';
 $string['miscellaneoussettings'] = 'その他の設定';
 $string['modstandardels'] = '一般モジュール設定';
+$string['month'] = '月';
 $string['nomethodforaddinghelpbutton'] = 'フォームエレメント $a->name (クラス$a->classname) にヘルプボタンを追加する方法がありません。';
 $string['nonexistentformelements'] = '存在しないフォームエレメント $a にヘルプボタンを追加しようとしています。';
 $string['requiredelement'] = '必須入力フィールド';
+$string['year'] = '年';
 
 ?>
