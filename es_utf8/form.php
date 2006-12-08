49571dbfb7bc8ee3668d74582c82e0af7c54ce3c
Benito Arias <barias>
2006-12-08 23:46:06 +0000

    *** empty log message ***

--- es_utf8/form.php
+++ es_utf8/form.php
@@ -3,6 +3,7 @@
 
 
 $string['addfields'] = 'Agregar $a campos al formulario';
+$string['day'] = 'Día';
 $string['denotesreq'] = '$a denota que debe suministrar un valor en este campo.';
 $string['err_alphanumeric'] = 'Debe escribir sólo letras o números aquí.';
 $string['err_email'] = 'Debe escribir una dirección de email válida aquí.';
@@ -15,10 +16,14 @@ $string['err_numeric'] = 'Debe escribir un número aquí.';
 $string['err_rangelength'] = 'Debe escribir entre {$a->format[0]} y {$a->format[1]} caracteres aquí.';
 $string['err_required'] = 'Debe suministrar un valor aquí.';
 $string['general'] = 'Ajustes generales';
+$string['hour'] = 'Hora';
+$string['minute'] = 'Minuto';
 $string['miscellaneoussettings'] = 'Ajustes varios';
 $string['modstandardels'] = 'Ajustes comunes del módulo';
+$string['month'] = 'Mes';
 $string['nomethodforaddinghelpbutton'] = 'No hay posibilidad de añadir un botón de ayuda al elemento del formulario $a->name (class $a->classname)';
 $string['nonexistentformelements'] = 'Intentando añadir botones de ayuda a elementos de formulario inexistentes: $a';
 $string['requiredelement'] = 'Debe suministrar un valor aquí.';
+$string['year'] = 'Año';
 
 ?>
