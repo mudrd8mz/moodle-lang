f6a386403d9998fa0106b33aa6e6dd469e3e9cff
rcantada <rcantada>
2006-12-12 22:52:50 +0000

    ---------------------------------------------------------------------- admin.php moodle.php  CVS: Added Files: CVS: 	form.php role.php  CVS: ---------------------------------------------------------------------- added form.php, role.php to tl lang pack.

---
+++ tl_utf8/form.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 1.8 dev (2006112200)
+
+
+$string['addfields'] = 'Idagdag ang $a pitak sa porma';
+$string['day'] = 'Araw';
+$string['denotesreq'] = '$a ay nangangahulugan na kinakailangang pitak.';
+$string['err_alphanumeric'] = 'Tanging titik o bilang lamang ang ipasok ninyo dito.';
+$string['err_email'] = 'Kailangan mong magpasok ng tanggap na email address dito.';
+$string['err_lettersonly'] = 'anging titik lamang ang ipasok ninyo dito.';
+$string['err_maxlength'] = 'Kailangan mong magpasok ng hindi hihigit sa $a->format titik dito.';
+$string['err_minlength'] = 'Kailangan mong magpasok ng hindi bababa sa $a->format titik dito.';
+$string['err_nonzero'] = 'Kailangan mong magpasok ng bilang na hindi nagsisimula sa 0 dito.';
+$string['err_nopunctuation'] = 'Hindi ka dapat magpasok ng bantas dito.';
+$string['err_numeric'] = 'Kailangan mong magpasok ng bilang dito.';
+$string['err_rangelength'] = 'Kailangan mong magpasok ng titik na nasa pagitan ng {$a->format[0]} at {$a->format[1]} dito.';
+$string['err_required'] = 'Kailangan mong magbigay ng halaga dito.';
+$string['general'] = 'Pangkalahatang Kaayusan';
+$string['hour'] = 'Oras';
+$string['minute'] = 'Minuto';
+$string['miscellaneoussettings'] = 'Atbp. Kaayusan';
+$string['modstandardels'] = 'Pinagsasaluhang Kaayusan ng mga Modyul';
+$string['month'] = 'Buwan';
+$string['nomethodforaddinghelpbutton'] = 'Walang paraan para sa pagdaragdag ng tulong na buton sa elemento ng porma na $a->name (class $a->classname)';
+$string['nonexistentformelements'] = 'Nagtatangkang magdagdag ng tulong na buton sa elemento ng porma na wala naman: $a';
+$string['requiredelement'] = 'Kinakailangang pitak';
+$string['year'] = 'Taon';
+
+?>
