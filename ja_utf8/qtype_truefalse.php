18e43f684e9e55b088eeb149380675314db4fad8
Mitsuhiro Yoshida <mits>
2006-12-23 00:11:31 +0000

    Translated a new string for quiz. Added a new string file for qtype true/false.

---
+++ ja_utf8/qtype_truefalse.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // qtype_truefalse.php - created with Moodle 1.8 dev (2006120700)
+
+
+$string['correctanswer'] = '正解';
+$string['editingtruefalse'] = '○/×問題の編集';
+$string['false'] = '×';
+$string['feedbackfalse'] = 'フィードバック (×)';
+$string['feedbacktrue'] = 'フィードバック (○)';
+$string['true'] = '○';
+$string['truefalse'] = '○/×';
+
+?>
