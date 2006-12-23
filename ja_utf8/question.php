b1d9911e7b50c2c62388a98419a5929f36af4c85
Mitsuhiro Yoshida <mits>
2006-12-23 05:20:34 +0000

    Added a translated string file for question.

---
+++ ja_utf8/question.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // question.php - created with Moodle 1.8 dev (2006120700)
+
+
+$string['categorydoesnotexist'] = 'このカテゴリは存在しません。';
+$string['editingquestion'] = '問題の編集';
+$string['missingimportantcode'] = 'この問題タイプには、重要なコードがありません: $a';
+$string['notenoughdatatoeditaquestion'] = '問題ID、カテゴリIDおよび問題タイプが指定されていません。';
+$string['questiondoesnotexist'] = 'この問題は存在しません。';
+$string['unknownquestiontype'] = '不明な問題タイプ: $a';
+
+?>
