4600658a75304d403fc3c1024aab358af4bef907
Nicolas Martignoni <martignoni>
2006-12-23 10:33:16 +0000

    MDL-8003 - part - Use formslib for question editing forms

---
+++ fr_utf8/qtype_truefalse.php
@@ -0,0 +1,11 @@
+<?php // $Id$ 
+
+$string['correctanswer'] = 'Réponse correcte';
+$string['editingtruefalse'] = 'Modifier une question Vrai/Faux';
+$string['false'] = 'Faux';
+$string['feedbackfalse'] = 'Feedback (Faux)';
+$string['feedbacktrue'] = 'Feedback (Vrai)';
+$string['true'] = 'Vrai';
+$string['truefalse'] = 'Vrai/Faux';
+
+?>
---
+++ fr_utf8/question.php
@@ -0,0 +1,10 @@
+<?php // $Id$
+
+$string['categorydoesnotexist'] = 'Cette catégorie n\'existe pas';
+$string['editingquestion'] = 'Modifier une question';
+$string['missingimportantcode'] = 'Il manque à ce type de question un code important&nbsp;: $a.';
+$string['notenoughdatatoeditaquestion'] = 'L\'identifiant de la question, l\'identifiant de la catégorie et le type de question n\'ont pas été spécifiés.';
+$string['questiondoesnotexist'] = 'Cette question n\'existe pas';
+$string['unknownquestiontype'] = 'Type de question inconnu&nbsp;: $a.';
+
+?>
