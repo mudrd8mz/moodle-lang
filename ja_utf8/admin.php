e714888f0d1bed54e099e2f01c5614341635aaf0
Mitsuhiro Yoshida <mits>
2006-12-23 05:32:12 +0000

    Translated new strings for debug display.

--- ja_utf8/admin.php
+++ ja_utf8/admin.php
@@ -67,6 +67,7 @@ http://site.example.com/admin.cron.php?password=opensesame
 </pre>空白の場合、パスワードは要求されません。';
 $string['configdbsessions'] = 'この設定を有効にした場合、現在のセッション情報はデータベースに保存されます。これは特に規模の大きい/アクセス数の多いサイト、またはクラスタサーバに構築されたサイトに向いています。ほとんどのサイトでは、セッションの保存をサーバディスクに行うよう、この設定を「No」のままにしてください。この設定を変更すると、 (あなたも含めて) 現在のユーザがログアウトされます。あなたがMySQLを使用している場合、my.cnf (またはmy.ini) の「max_allowed_packet」 を少なくとも4Mにしてください。';
 $string['configdebug'] = 'この設定を有効にした場合、PHPのerror_reportingが増加し、より多くの警告が表示されるようになります。これは、開発者のみに便利な設定です。';
+$string['configdebugdisplay'] = 'この設定を有効にした場合、エラー報告はHTMLページに表示されますが、一般的にXHTML、JS、cookiesおよびHTTPヘッダを壊します。この設定を無効にした場合、エラー報告はサーバログに記録され、デバッグしやすくなります。PHP設定のerror_logで、どのログを記録するかコントロールできます。';
 $string['configdefaultallowedmodules'] = '上位カテゴリに分類される<b>コースが作成される場合</b>、デフォルトでどのモジュールを許可しますか?';
 $string['configdefaultcourseroleid'] = 'コースに登録されたユーザには、このロールが自動的に割り当てられます。';
 $string['configdefaultrequestcategory'] = 'ユーザからリクエストされたコースは、自動的にこのカテゴリに配置されます。';
@@ -207,6 +208,7 @@ $string['dbsessions'] = 'セッション情報にデータベースを使用す�
 $string['debug'] = 'デバッグメッセージ';
 $string['debugall'] = 'ALL: すべての妥当なPHPデバッグメッセージを表示する';
 $string['debugdeveloper'] = 'DEVELOPER: 開発者のための特別Moodleデバッグメッセージ';
+$string['debugdisplay'] = 'デバックメッセージを表示する';
 $string['debugging'] = 'デバッグ';
 $string['debugminimal'] = 'MINIMAL: 致命的なエラーのみ表示する';
 $string['debugnone'] = 'NONE: デバッグ情報を表示しない';
