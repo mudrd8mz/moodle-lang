9e7bb0bd83172cc536c1a791086865349b640fa0
Lee Chan-young <yogibear270>
2006-12-24 14:47:13 +0000

    New Strings, translated!

---
+++ ko_utf8/qtype_truefalse.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // qtype_truefalse.php - created with Moodle 1.8 dev (2006120700)
+
+
+$string['correctanswer'] = '정답';
+$string['editingtruefalse'] = '진/위 문항 편집';
+$string['false'] = '거짓';
+$string['feedbackfalse'] = '반응(거짓)';
+$string['feedbacktrue'] = '반응(참)';
+$string['true'] = '참';
+$string['truefalse'] = '진/위';
+
+?>
