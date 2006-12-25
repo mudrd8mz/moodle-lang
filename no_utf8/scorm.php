07ba6a8db2144915690bed818cc3f54b1c01b70a
solveigb <solveigb>
2006-12-25 22:48:17 +0000

    New translations

---
+++ no_utf8/scorm.php
@@ -0,0 +1,49 @@
+<?PHP // $Id$ 
+      // scorm.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['advanced'] = 'Parametre';
+$string['attempt'] = 'forsøk';
+$string['attempts'] = 'forsøk';
+$string['autocontinue'] = 'Fortsett automatisk';
+$string['averageattempt'] = 'Gjennomsnittsforsøk';
+$string['chooseapacket'] = 'Velg eller oppdater en pakke';
+$string['completed'] = 'Fullført';
+$string['coursepacket'] = 'Kurspakke';
+$string['coursestruct'] = 'Kursstruktur';
+$string['details'] = 'Spor detaljer';
+$string['firstaccess'] = 'Første aksess';
+$string['firstattempt'] = 'Første forsøk';
+$string['fullscreen'] = 'Fyll hele skjermen';
+$string['gradeaverage'] = 'Gjennomsnittlig karakter';
+$string['gradescoes'] = 'Læringsobjekter';
+$string['height'] = 'Høyde';
+$string['hidden'] = 'Skjult';
+$string['hidenav'] = 'Skjul navigasjonsknapper';
+$string['iframe'] = 'Inneværende vindu';
+$string['incomplete'] = 'Ufullstendig';
+$string['lastattempt'] = 'Siste forsøk';
+$string['notattempted'] = 'Ikke forsøkt';
+$string['objectives'] = 'Mål';
+$string['organization'] = 'Organisasjon';
+$string['organizations'] = 'Organisasjoner';
+$string['prev'] = 'Forrige';
+$string['report'] = 'Rapport';
+$string['result'] = 'Resultat';
+$string['scoes'] = 'Læringsobjekter';
+$string['sided'] = 'På venstre side';
+$string['status'] = 'Status';
+$string['statusbar'] = 'Vis statuslinjen';
+$string['student_response'] = 'Svar';
+$string['syntax'] = 'Syntaksfeil';
+$string['totaltime'] = 'Tid';
+$string['trackingloose'] = 'ADVARSEL: Sporingsdata for denne pakken vil mistes!';
+$string['type'] = 'Type';
+$string['validateascorm'] = 'Valider en pakke';
+$string['validation'] = 'Valider resultat';
+$string['value'] = 'Verdi';
+$string['viewallreports'] = 'Se på rapporter for $a forsøk';
+$string['width'] = 'Vidde';
+$string['window'] = 'Vindu';
+
+?>
