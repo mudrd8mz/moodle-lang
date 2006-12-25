99d7ba7c6ce6b20615f394db285fcf8aa6c80a19
solveigb <solveigb>
2006-12-25 22:42:29 +0000

    More translations.

--- no_utf8/assignment.php
+++ no_utf8/assignment.php
@@ -1,7 +1,8 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.2 (2004032000)
+      // assignment.php - created with Moodle 1.6.3 (2006050530)
 
 
+$string['allowmultiple'] = 'Tillat flere filer';
 $string['allowresubmit'] = 'Tillat ny innlevering';
 $string['assignmentdetails'] = 'Oppgavedetaljer';
 $string['assignmentmail'] = '$a->teacher har sendt en tilbakemelding på din
@@ -15,27 +16,62 @@ innlevering \'<i>$a->assignment</i>\'<br /><br />
 Du kan se tilbakemeldingen <a href=\"$a->url\">her:</a>.';
 $string['assignmentname'] = 'Oppgavenavn';
 $string['assignmenttype'] = 'Oppgavetype';
+$string['attachfile'] = 'Legg ved fil';
+$string['attachfiletoassignment'] = 'Legg fil(er) til oppgaven';
+$string['availabledate'] = 'Tilgjengelig fra';
+$string['backtoassignment'] = 'Tilbake til oppgaven';
+$string['backtofeedback'] = 'Tilbake til tilbakemelding';
+$string['choosereviewfile'] = 'Velg en fil å laste opp som en bedømmelse av oppgaven';
+$string['comment'] = 'Kommentar';
+$string['commentinline'] = 'Kommenter i tekst';
 $string['configmaxbytes'] = 'Standard maksimum oppgavestørrelse for alle oppgavene på sidene (overordnet klassenes grenser og andre lokale innstillinger)';
-$string['description'] = 'Selve oppgaveteksten';
+$string['deletecheckfile'] = 'Er du helt sikker på at du ønsker å slette denne filen?';
+$string['deleteednotification'] = 'Filen er slettet.';
+$string['deletefail'] = 'Den følgende filen har ikke blitt slettet:';
+$string['description'] = 'Beskrivelse';
 $string['duedate'] = 'Innleveringsfrist';
 $string['duedateno'] = 'Ingen innleveringsfrist';
 $string['early'] = '$a før fristen';
+$string['editmysubmission'] = 'Rediger innleveringen min';
+$string['emailstudents'] = 'Epostvarsel til studentene';
+$string['emailteachermail'] = '$a->username har oppdatert sin oppgaveinnlevering for \'$a->assignment\'
+
+Den er tilgjengelig her: 
+$a->url';
+$string['emailteachers'] = 'Epostvarsel til lærere';
+$string['emptysubmission'] = 'Du har ikke levert noenting ennå';
 $string['existingfiledeleted'] = 'Eksisterende fil er slettet: $a';
 $string['failedupdatefeedback'] = 'Kunne ikke oppdatere tilbakemelding for levering til $a';
 $string['feedback'] = 'Tilbakemelding';
+$string['feedbackfromteacher'] = 'Tilbakemelding fra $a';
 $string['feedbackupdated'] = 'Tilbakemelding oppdatert for $a personer';
+$string['graded'] = 'Karaktersatt';
+$string['guestnoupload'] = 'Beklager, gjester har ikke lov til å laste opp';
+$string['helpoffline'] = '<p>Dette er nyttig når oppgaven utføres uten Moodle. Det kan være et annet sted på nettet eller ansikt-til-ansikt.</p><p>Studenter kan se en beskrivelse av oppgaven, men de kan ikke laste opp filer eller annet. Karaktersetting fungerer vanligvis, og studenter vil få melding om sine karakterer.</p>';
 $string['late'] = '$a for sent';
 $string['maximumgrade'] = 'Maks karakter/poeng';
 $string['maximumsize'] = 'Maks filstørrelse';
-$string['modulename'] = 'Oppgave';
-$string['modulenameplural'] = 'Oppgaver';
+$string['modulename'] = 'Innlevering';
+$string['modulenameplural'] = 'Innlevering';
+$string['namedeletefile'] = 'Du holder på å slette denne filen:';
 $string['newsubmissions'] = 'Leverte oppgaver';
 $string['notgradedyet'] = 'Ikke satt karakter enda';
 $string['notsubmittedyet'] = 'Ikke levert enda';
 $string['overwritewarning'] = 'Advarsel: Laster du opp på nytt vil du OVERSKRIVE din leverte oppgave';
+$string['pagesize'] = 'Innleveringer vist pr side';
+$string['preventlate'] = 'Forhindre sene innleveringer';
+$string['quickgrade'] = 'Tillat rask karaktersetting';
+$string['removelink'] = 'Fjern';
 $string['saveallfeedback'] = 'Lagre og send all(e) tilbakemelding(er)';
+$string['step1'] = 'Trinn 1:';
+$string['step2'] = 'Trinn 2:';
+$string['submission'] = 'Innlevering';
 $string['submissionfeedback'] = 'Tilbakemelding';
 $string['submissions'] = 'Leveringer';
+$string['submissionsaved'] = 'Dine endringer er lagret';
+$string['submissionstatusdraft'] = 'Kladd';
+$string['submissionstatusreturned'] = 'Returnert';
+$string['submissionstatussubmitted'] = 'Innlevert';
 $string['submitassignment'] = 'Lever din oppgave med nettskjemaet nedenfor';
 $string['submitted'] = 'Levert';
 $string['typeoffline'] = 'Frakoplet aktivitet (Oppgaven skal IKKE leveres over nett med Moodle)';
