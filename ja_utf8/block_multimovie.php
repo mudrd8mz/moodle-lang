ca71088f754552ba577d6d893969c714581b2fa2
Mitsuhiro Yoshida <mits>
2006-12-27 16:34:52 +0000

    Added a new lang file for Multimovie block.

---
+++ ja_utf8/block_multimovie.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // block_multimovie.php - created with Moodle 1.8 dev (2006120400)
+
+
+$string['configcontent'] = 'コンテンツ';
+$string['configtitle'] = 'タイトル';
+$string['configtype'] = 'タイプ';
+$string['leaveblanktohide'] = '隠す場合は空白';
+$string['multimovie'] = 'MultiMovie';
+$string['newmultimovieblock'] = '新しいMultiMovieブロック';
+$string['unconfiguredblock'] = '未設定ブロック';
+
+?>
