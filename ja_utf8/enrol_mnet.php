1650aa2d24cacb52cd5866ba590b73843440d26d
Mitsuhiro Yoshida <mits>
2007-01-04 06:41:09 +0000

    Add translated enrol mnet lang file.

---
+++ ja_utf8/enrol_mnet.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // enrol_mnet.php - created with Moodle 1.8 dev (2007010300)
+
+
+$string['allcategories'] = '潜在的なカテゴリ $a';
+$string['allcourses'] = '潜在的なコース $a';
+$string['allow_allcourses'] = 'すべてのリモートコースで登録を許可する。';
+$string['allowedcategories'] = '許可されたカテゴリ $a';
+$string['allowedcourses'] = '許可されたコース $a';
+$string['allowedcourseslinktext'] = 'ここで許可されたコースおよびカテゴリを編集してください。';
+$string['enrolname'] = 'Moodleネットワーキング';
+$string['mnet_enrol_name'] = 'Moodleネットワーキング登録';
+$string['nocategoriesdefined'] = 'コースカテゴリが見つかりませんでした。<a href=\"$a\">ここで</a>新しいカテゴリを設定してください。';
+$string['nocoursesdefined'] = 'コースが見つかりませんでした。<a href=\"$a\">ここで</a>新しいコースを設定してください。';
+
+?>
