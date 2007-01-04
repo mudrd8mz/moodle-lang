6f2f87220e3f8071fd8926dda546ddb6cb826253
Mitsuhiro Yoshida <mits>
2007-01-04 05:40:30 +0000

    Add translated mnet lang file.

---
+++ ja_utf8/mnet.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // mnet.php - created with Moodle 1.8 dev (2007010300)
+
+
+$string['accesslevel'] = 'アクセスレベル';
+$string['hostname'] = 'ホスト名';
+$string['hostsettings'] = 'ホスト設定';
+$string['id'] = 'ID';
+$string['mnet'] = 'Moodleネットワーキング';
+$string['mnetlog'] = 'ログ';
+$string['mnetpeers'] = 'Moodleネットワークピア';
+$string['mnetservices'] = 'サービス';
+$string['mnetsettings'] = 'Moodleネットワーク設定';
+$string['moodleloc'] = 'Moodleロケーション';
+$string['net'] = 'ネットワーキング';
+$string['networksettings'] = 'ネットワーク設定';
+$string['off'] = 'Off';
+$string['on'] = 'On';
+$string['publickey'] = '公開鍵';
+$string['remotehost'] = 'リモートハブ';
+$string['remotehosts'] = 'リモートホスト';
+$string['restore'] = 'リストア';
+$string['settings'] = '設定';
+$string['ssoaccesscontrol'] = 'SSOアクセスコントロール';
+$string['system'] = 'システム';
+$string['trustedhosts'] = '信頼できるホスト';
+$string['version'] = 'バージョン';
+$string['yourhost'] = 'あなたのホスト';
+$string['yourpeers'] = 'あなたのピア';
+
+?>
