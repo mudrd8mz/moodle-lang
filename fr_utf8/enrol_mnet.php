70b40fc69b61913ae0d91c41513bb4a4f495d10b
Nicolas Martignoni <martignoni>
2007-01-04 11:54:37 +0000

    Correction of encoding

--- fr_utf8/enrol_mnet.php
+++ fr_utf8/enrol_mnet.php
@@ -1,25 +1,25 @@
 <?php // $Id$
 
-$string['description'] = 'Description de l\'inscription via RÈseau Moodle.';
-$string['enrolname'] = 'RÈseau Moodle';
+$string['description'] = 'Description de l\'inscription via R√©seau Moodle.';
+$string['enrolname'] = 'R√©seau Moodle';
 $string['allow_allcourses'] = 'Permettre l\'inscription dans tous les cours distants.';
-$string['allcategories'] = '$a catÈgories potentielles';
-$string['allowedcategories'] = '$a catÈgories autorisÈes';
-$string['nocategoriesdefined'] = 'Aucune catÈgorie trouvÈe. Veuillez dÈfinir de nouvelles catÈgories <a href=\"$a\">ici</a>.';
+$string['allcategories'] = '$a cat√©gories potentielles';
+$string['allowedcategories'] = '$a cat√©gories autoris√©es';
+$string['nocategoriesdefined'] = 'Aucune cat√©gorie trouv√©e. Veuillez d√©finir de nouvelles cat√©gories <a href=\"$a\">ici</a>.';
 $string['allcourses'] = '$a cours potentiels';
-$string['allowedcourses'] = '$a cours autorisÈs';
-$string['nocoursesdefined'] = 'Aucun cours trouvÈ. Veuillez dÈfinir de nouveaux cours <a href=\"$a\">ici</a>.';
-$string['allowedcourseslinktext'] = 'Modifiez les cours et catÈgories autorisÈs ici.';
-$string['mnet_enrol_name'] = 'Inscription via RÈseau Moodle';
-$string['mnet_enrol_description'] = 'En publianz ce service, vous autorisez les administrateurs de $a ‡ inscrire leurs Ètudiants ‡ des cours sur votre Moodle.<br />
+$string['allowedcourses'] = '$a cours autoris√©s';
+$string['nocoursesdefined'] = 'Aucun cours trouv√©. Veuillez d√©finir de nouveaux cours <a href=\"$a\">ici</a>.';
+$string['allowedcourseslinktext'] = 'Modifiez les cours et cat√©gories autoris√©s ici.';
+$string['mnet_enrol_name'] = 'Inscription via R√©seau Moodle';
+$string['mnet_enrol_description'] = 'En publianz ce service, vous autorisez les administrateurs de $a √† inscrire leurs √©tudiants √† des cours sur votre Moodle.<br />
 <ul>
-<li><em>DÈpendance</em>&nbsp;: vous devez Ègalement <strong>publier</strong> le service SSO (fournisseur de service) pour $a.</li>
-<li><em>DÈpendance</em>&nbsp;: vous devez Ègalement <strong>vous abonner</strong> au service SSO (fournisseur d\'identitÈ) de $a.</li>
+<li><em>D√©pendance</em>&nbsp;: vous devez √©galement <strong>publier</strong> le service SSO (fournisseur de service) pour $a.</li>
+<li><em>D√©pendance</em>&nbsp;: vous devez √©galement <strong>vous abonner</strong> au service SSO (fournisseur d\'identit√©) de $a.</li>
 </ul><br />
-En vous abonnant ‡ ce service, vous pourrez inscrire vos Ètudiants aux cours sur $a.<br />
+En vous abonnant √† ce service, vous pourrez inscrire vos √©tudiants aux cours sur $a.<br />
 <ul>
-<li><em>DÈpendance</em>&nbsp;: vous devez Ègalement <strong>vous abonner</strong> au service SSO (fournisseur de service) de $a.</li>
-<li><em>DÈpendance</em>&nbsp;: vous devez Ègalement <strong>publier</strong> le service SSO (fournisseur d\'identitÈ) pour $a.</li>
+<li><em>D√©pendance</em>&nbsp;: vous devez √©galement <strong>vous abonner</strong> au service SSO (fournisseur de service) de $a.</li>
+<li><em>D√©pendance</em>&nbsp;: vous devez √©galement <strong>publier</strong> le service SSO (fournisseur d\'identit√©) pour $a.</li>
 </ul><br />';
 
 ?>
