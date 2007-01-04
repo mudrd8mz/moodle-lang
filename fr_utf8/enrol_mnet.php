4619eda2a8e5b930613ac0a2effdf90c0e86aedc
Nicolas Martignoni <martignoni>
2007-01-04 11:53:24 +0000

    mnet: new enrolment plugin

---
+++ fr_utf8/enrol_mnet.php
@@ -0,0 +1,25 @@
+<?php // $Id$
+
+$string['description'] = 'Description de l\'inscription via Réseau Moodle.';
+$string['enrolname'] = 'Réseau Moodle';
+$string['allow_allcourses'] = 'Permettre l\'inscription dans tous les cours distants.';
+$string['allcategories'] = '$a catégories potentielles';
+$string['allowedcategories'] = '$a catégories autorisées';
+$string['nocategoriesdefined'] = 'Aucune catégorie trouvée. Veuillez définir de nouvelles catégories <a href=\"$a\">ici</a>.';
+$string['allcourses'] = '$a cours potentiels';
+$string['allowedcourses'] = '$a cours autorisés';
+$string['nocoursesdefined'] = 'Aucun cours trouvé. Veuillez définir de nouveaux cours <a href=\"$a\">ici</a>.';
+$string['allowedcourseslinktext'] = 'Modifiez les cours et catégories autorisés ici.';
+$string['mnet_enrol_name'] = 'Inscription via Réseau Moodle';
+$string['mnet_enrol_description'] = 'En publianz ce service, vous autorisez les administrateurs de $a à inscrire leurs étudiants à des cours sur votre Moodle.<br />
+<ul>
+<li><em>Dépendance</em>&nbsp;: vous devez également <strong>publier</strong> le service SSO (fournisseur de service) pour $a.</li>
+<li><em>Dépendance</em>&nbsp;: vous devez également <strong>vous abonner</strong> au service SSO (fournisseur d\'identité) de $a.</li>
+</ul><br />
+En vous abonnant à ce service, vous pourrez inscrire vos étudiants aux cours sur $a.<br />
+<ul>
+<li><em>Dépendance</em>&nbsp;: vous devez également <strong>vous abonner</strong> au service SSO (fournisseur de service) de $a.</li>
+<li><em>Dépendance</em>&nbsp;: vous devez également <strong>publier</strong> le service SSO (fournisseur d\'identité) pour $a.</li>
+</ul><br />';
+
+?>
