1de032dbb1b31db12cae897a44c2b7525556bc2f
Mitsuhiro Yoshida <mits>
2007-01-04 06:22:07 +0000

    Translated new strings for mnet.

--- ja_utf8/mnet.php
+++ ja_utf8/mnet.php
@@ -3,9 +3,21 @@
 
 
 $string['accesslevel'] = 'アクセスレベル';
+$string['current_transport'] = '現在のトランスポート';
+$string['currentkey'] = '現在の公開鍵';
+$string['databaseerror'] = '詳細をデータベースに書き込めませんでした。';
+$string['deletekeycheck'] = '本当にこのキーを削除してもよろしいですか?';
+$string['expireyourkey'] = 'このキーを削除する';
+$string['forbidden-transport'] = 'あなたが使用を試みているトランスポートメソッドは、許可されていません。';
 $string['hostname'] = 'ホスト名';
 $string['hostsettings'] = 'ホスト設定';
 $string['id'] = 'ID';
+$string['idhelp'] = 'この値は自動的に割り当てられました。変更することはできません。';
+$string['illegalchar-host'] = 'あなたのホスト名に次の無効な文字が含まれています: $a';
+$string['illegalchar-moodlehome'] = 'あなたのMoodleロケーションに無効な文字が含まれています。';
+$string['invalidpubkey'] = '有効なSSLキーではありません。';
+$string['invalidurl'] = '無効なURLパラメータです。';
+$string['ipaddress'] = 'IPアドレス';
 $string['mnet'] = 'Moodleネットワーキング';
 $string['mnetlog'] = 'ログ';
 $string['mnetpeers'] = 'Moodleネットワークピア';
@@ -24,7 +36,10 @@ $string['settings'] = '設定';
 $string['ssoaccesscontrol'] = 'SSOアクセスコントロール';
 $string['system'] = 'システム';
 $string['trustedhosts'] = '信頼できるホスト';
+$string['verifysignature-error'] = '署名認証に失敗しました。エラーが発生しました。';
+$string['verifysignature-invalid'] = '署名認証に失敗しました。このペイロード (データ本体) は、あなたが署名したものではないと思われます。';
 $string['version'] = 'バージョン';
+$string['wrong-ip'] = 'あなたのIPアドレスは、記録されているレコードと合致しません。';
 $string['yourhost'] = 'あなたのホスト';
 $string['yourpeers'] = 'あなたのピア';
 
