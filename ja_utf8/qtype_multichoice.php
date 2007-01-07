028bf2c5392a383ec582c7279d81d2e368d68673
Mitsuhiro Yoshida <mits>
2007-01-07 22:14:31 +0000

    Translated new strings for qtype multichocie. Brushed up translated strings for qtype multichocie.

--- ja_utf8/qtype_multichoice.php
+++ ja_utf8/qtype_multichoice.php
@@ -1,22 +1,27 @@
 <?PHP // $Id$ 
-      // qtype_multichoice.php - created with Moodle 1.8 dev (2006112200)
+      // qtype_multichoice.php - created with Moodle 1.8 dev (2007010404)
 
 
+$string['addmorechoiceblanks'] = 'さらに {no} 件の選択肢入力欄を追加する';
 $string['answerhowmany'] = '単一または複数解答?';
-$string['answersingleno'] = '複数解答を許可';
-$string['answersingleyes'] = '単一の解答のみ';
+$string['answersingleno'] = '複数解答を許可する';
+$string['answersingleyes'] = '単一の解答のみ許可する';
 $string['choiceno'] = '選択肢 $a';
 $string['choices'] = '利用可能な選択肢';
 $string['clozeaid'] = 'テキストを入力してください。';
+$string['correctfeedback'] = '正解すべてに対するフィードバック';
 $string['editingmultichoice'] = '多肢選択問題の編集';
 $string['feedback'] = 'フィードバック';
 $string['fillouttwochoices'] = '2つ以上の選択肢を入力してください。空白の選択肢は使用されません。';
 $string['fractionsaddwrong'] = 'あなたが選択したプラス評点の合計が、100%%に達していません。<br />現在の評点合計は $a%% です。<br />戻ってこの問題を修正しますか?';
 $string['fractionsnomax'] = 'この問題で満点を取ることができるよう、<br />答えの1つは、100%%でなければなりません。 <br />戻ってこの問題を修正しますか?';
+$string['incorrectfeedback'] = '不正解すべてに対するフィードバック';
 $string['notenoughanswers'] = 'このタイプの問題には少なくとも $a の解答が必要です。';
 $string['overallcorrectfeedback'] = '正解すべてに対するフィードバック';
+$string['overallfeedback'] = '全体のフィードバック';
 $string['overallincorrectfeedback'] = '不正解すべてに対するフィードバック';
 $string['overallpartiallycorrectfeedback'] = '部分的な正解すべてに対するフィードバック';
+$string['partiallycorrectfeedback'] = '部分的な正解すべてに対するフィードバック';
 $string['shuffleanswers'] = '答えをシャッフルする';
 $string['singleanswer'] = '1つの答えを選択してください。';
 
