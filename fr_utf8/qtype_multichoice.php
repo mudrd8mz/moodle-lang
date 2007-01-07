025cd255854d4ece7188de214c0b9998a446b102
Nicolas Martignoni <martignoni>
2007-01-07 17:48:05 +0000

    various formslib work including bug fixes for MDL-8039 id attribute collision between forms and page ids. Also added editor helpbutton. This is a fix for MDL-6941 need the ability to associate several helptopics with htmleditor. Also started on converting forms for question types to formslib.

--- fr_utf8/qtype_multichoice.php
+++ fr_utf8/qtype_multichoice.php
@@ -1,20 +1,25 @@
 <?php // $Id$ 
 
+$string['addmorechoiceblanks'] = 'Emplacement pour {no} réponses supplémentaires';
 $string['answerhowmany'] = 'Une seule ou plusieurs réponses&nbsp;?';
 $string['answersingleno'] = 'Réponses multiples autorisées';
 $string['answersingleyes'] = 'Une seule réponse';
 $string['choiceno'] = 'Réponse $a';
 $string['choices'] = 'Réponses possibles';
 $string['clozeaid'] = 'Tapez le mot manquant';
+$string['correctfeedback'] = 'Pour toute réponse correcte';
 $string['editingmultichoice'] = 'Modification d\'une question à choix multiples';
 $string['fillouttwochoices'] = 'Vous devez donner au moins deux réponses. Les réponses laissés vides ne seront pas utilisées.';
 $string['fractionsaddwrong'] = 'La somme des notes positives que vous avez choisies n\'est pas égale à 100%%.<br />Leur somme est de $a%%.<br />Voulez-vous revenir en arrière pour corriger cette question&nbsp;?';
 $string['fractionsnomax'] = 'Une des réponses doit être à 100%%, de sorte qu\'il soit<br />possible d\'obtenir la note maximale pour cette question.<br />Voulez-vous revenir en arrière pour corriger cette question&nbsp;?';
 $string['feedback'] = 'Feedback';
+$string['incorrectfeedback'] = 'Pour toute réponse incorrecte';
 $string['notenoughanswers'] = 'Ce type de question requiert au moins $a réponses';
 $string['overallcorrectfeedback'] = 'Feedback pour les réponses correctes';
+$string['overallfeedback'] = 'Feedback général';
 $string['overallpartiallycorrectfeedback'] = 'Feedback pour les réponses partiellement correctes';
 $string['overallincorrectfeedback'] = 'Feedback pour les réponses fausses';
+$string['partiallycorrectfeedback'] = 'Pour toute réponse partiellement correcte';
 $string['shuffleanswers'] = 'Mélanger les questions';
 $string['singleanswer'] = 'Choisir une réponse.';
 
---
+++ fr_utf8/qtype_shortanswer.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+
+$string['addmoreanswerblanks'] = '{no} emplacements supplémenaires vides';
+$string['answerno'] = 'Réponse $a';
+$string['filloutoneanswer'] = 'Vous devez fournir au moins une réponse possible. Les réponses laissées vides ne seront pas utilisées. Le caractère «&nbsp;*&nbsp;» peut être utilisé comme jocker pour remplacer n\'importe quel caractère. La première réponse correspondante sera utilisée pour déterminer le score et le feedback.';
+
+?>
