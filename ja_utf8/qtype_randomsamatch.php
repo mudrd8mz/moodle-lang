981b534fa4509f8bcea426a173cc50d40873ba9b
Mitsuhiro Yoshida <mits>
2007-01-08 17:11:49 +0000

    Add a new translated language file for qtype randomsamatch.

---
+++ ja_utf8/qtype_randomsamatch.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qtype_randomsamatch.php - created with Moodle 1.8 dev (2007010404)
+
+
+$string['nosaincategory'] = 'あなたが選択したカテゴリ「 $a->catname 」には、記述問題がありません。いくつかの記述問題を含んだ異なるカテゴリを選択してください。';
+$string['notenoughsaincategory'] = 'あなたが選択したカテゴリ「 $a->catname 」には、$a->nosaquestions 問の問題のみ含まれています。異なるカテゴリを選択して、さらに問題を含むようにするか、あなたが設定した選択問題数を減らしてください。';
+
+?>
