5c3878aa5db0904be18792cb5f7e9b1be3d88c30
Mitsuhiro Yoshida <mits>
2007-01-08 16:50:41 +0000

    Translated new strings for Multimedia Plugins.

--- ja_utf8/mediaplugin.php
+++ ja_utf8/mediaplugin.php
@@ -1,7 +1,10 @@
 <?PHP // $Id$ 
-      // mediaplugin.php - created with Moodle 1.8 dev (2006102600)
+      // mediaplugin.php - created with Moodle 1.8 dev (2007010404)
 
 
 $string['filtername'] = 'マルチメディアプラグイン';
+$string['flashanimation'] = 'Flashアニメーション';
+$string['flashvideo'] = 'Flashビデオ';
+$string['mp3audio'] = 'MP3オーディオ';
 
 ?>
