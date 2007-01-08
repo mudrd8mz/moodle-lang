a56d18348fbd12a26eaa82ede7a320a2718b0d26
Nicolas Martignoni <martignoni>
2007-01-08 13:47:03 +0000

    New strings for accessibility

--- fr_utf8/mediaplugin.php
+++ fr_utf8/mediaplugin.php
@@ -2,5 +2,9 @@
 
 $string['filtername'] = "Extensions multimédia";
 
+$string['flashanimation'] = 'Animation Flash';
+$string['flashvideo'] = 'Vidéo Flash';
+$string['mp3audio'] = 'Son MP3';
+
 ?>
 
