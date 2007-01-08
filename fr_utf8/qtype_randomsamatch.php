b983389ba15b1f657a6a06eec36f5434c310e0d6
Nicolas Martignoni <martignoni>
2007-01-08 16:35:55 +0000

    migrated randomsamatch to formslib. fixed small typo in quiz mod_form

---
+++ fr_utf8/qtype_randomsamatch.php
@@ -0,0 +1,6 @@
+<?php // $Id$
+
+$string['notenoughsaincategory'] = 'Il n\'y a que $a->nosaquestions question(s) à réponse courte dans la catégorie choisie «&nbsp;$a->catname&nbsp;». Veuillez choisir une autre catégorie, créer plus de questions dans cette catégorie ou réduire le nombre choisi de questions.';
+$string['nosaincategory'] = 'Il n\'y a pas de questions à réponse courte dans la catégorie choisie «&nbsp;$a->catname&nbsp;». Veuillez choisir une autre catégorie ou créer plus de questions dans cette catégorie.';
+
+?>
