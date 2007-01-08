adcb668b6867e9ee99b0ca97ea8c038c3fe07784
Nicolas Martignoni <martignoni>
2007-01-08 09:40:56 +0000

    mnet: New lang file for describing auth/mnet services

---
+++ fr_utf8/auth_mnet.php
@@ -0,0 +1,8 @@
+<?php // $Id$ 
+
+$string['sso_idp_name'] = 'SSO (fournisseur d\'identité)';
+$string['sso_idp_description'] = 'La publication de ce service permet à vos utilisateurs d\'utiliser le site Moodle $a sans devoir s\'y reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi vous <strong>abonner</strong> au service SSO (fournisseur de service) sur $a.</li></ul><br /> L\'abonnement à ce service permet à des utilisateurs authentifiés par le serveur $a d\'accéder à votre site devoir sans se reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi <strong>publier</strong> le service SSO (fournisseur de service) de $a.</li></ul><br />';
+$string['sso_sp_name'] = 'SSO (fournisseur de service)';
+$string['sso_sp_description'] = 'La publication de ce service permet à des utilisateurs authentifiés par le serveur $a d\'accéder à votre site devoir sans se reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi vous <strong>abonner</strong> au service SSO (fournisseur d\'identité) de $a.</li></ul><br />L\'abonnement à ce service permet à vos utilisateurs d\'utiliser le site Moodle $a sans devoir s\'y reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi <strong>publier</strong> le service SSO (fournisseur d\'identité) de $a.</li></ul><br />';
+
+?>
