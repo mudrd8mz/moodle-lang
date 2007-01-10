96b7b427a2ececbd1267153b637b6a357c905d6b
Mitsuhiro Yoshida <mits>
2007-01-10 09:33:40 +0000

    Add a new translated language file for qtype calculated.

---
+++ ja_utf8/qtype_calculated.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // qtype_calculated.php - created with Moodle 1.8 dev (2007010404)
+
+
+$string['addmoreanswerblanks'] = 'さらに答え入力欄を追加する';
+$string['addmoreunitblanks'] = 'さらに $a 件の単位を追加する';
+$string['answerhdr'] = '答え $a';
+$string['atleastoneanswer'] = '少なくとも1つの答えを入力してください。';
+$string['correctanswershows'] = '正解の表示';
+$string['correctanswershowsformat'] = 'フォーマット';
+$string['mustbenumeric'] = 'ここには数字を入力してください。';
+$string['mustnotbenumeric'] = 'ここには数字を入力できません。';
+$string['tolerance'] = '許容誤差 &plusmn;';
+$string['youmustenteramultiplierhere'] = 'ここには乗数を入力してください。';
+
+?>
