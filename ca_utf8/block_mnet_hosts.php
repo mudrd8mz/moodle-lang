7713111aa45b2b24ba25a48142200a8e7dc14269
Carles Bellver <carlesbellver>
2007-01-11 13:40:41 +0000

    Moodle 1.8dev

---
+++ ca_utf8/block_mnet_hosts.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_mnet_hosts.php - created with Moodle 1.8 dev (2007010404)
+
+
+$string['mnet_hosts'] = 'Servidors en Xarxa';
+$string['server'] = 'Servidor';
+
+?>
---
+++ ca_utf8/question.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // question.php - created with Moodle 1.8 dev (2007010404)
+
+
+$string['categorydoesnotexist'] = 'Aquesta categoria no existeix';
+$string['editingquestion'] = 'S\'està editant una pregunta';
+$string['missingimportantcode'] = 'Aquest tipus de pregunta manca de codi important: $a.';
+$string['notenoughdatatoeditaquestion'] = 'No s\'han especificat l\'identificador de la pregunta, ni el de la categoria i el tipus de pregunta.';
+$string['questiondoesnotexist'] = 'Aquesta pregunta no existeix';
+$string['unknownquestiontype'] = 'Tipus de pregunta desconegut: $a.';
+
+?>
