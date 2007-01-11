9a60d0cb673871a6ea0f68633a3107f31bbe6059
koen roggemans <koenr>
2007-01-11 22:25:41 +0000

    New language pack: Laotian By Somsack INTHASONEsomsacki@yahoo.com

---
+++ lo_utf8/README.txt
@@ -0,0 +1,21 @@
+	Moodle v1.7+
+	--------------
+/*
+	Moodle Language Pack in Lao Unicode Language
+	By Mr. Somsack INTHASONE
+	@ Computer Science Department,
+	Faculty of Science,National University of Laos ( Dongdok Campus)
+	Vientiane Capital, Lao PDR.
+
+	e-Mail: somsacki@yahoo.com or somsacki@nuol.edu.la
+	Mobile: 856-20-2245128
+	Tel@home: 856-21-7708226
+*/
+
+Usage:
+Unzip lo_utf8.zip to the lang folder (lang/lo_utf8)
+
+For client PC needs to have the Saysettha OT.ttf font.
+If not, you  can download from http://fos.nuol.edu.la/do/font-lao/Saysettha%20OT.ttf  other Lao websites.
+
+
---
+++ lo_utf8/langconfig.php
@@ -0,0 +1,38 @@
+<?PHP // $Id$ 
+      // langconfig.php - created with Moodle 1.7 beta + (2006101003)
+/*
+	Lao Language has translated. 
+	By Mr. Somsack INTHASONE
+	@ Computer Science Department,
+	Faculty of Science,National University of Laos ( Dongdok Campus)
+	Vientiane Capital, Lao PDR.
+
+	e-Mail: somsacki@yahoo.com or somsacki@nuol.edu.la
+	Mobile: 856-20-2245128
+	Tel@home: 856-21-7708226
+*/
+
+
+$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+$string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
+$string['firstdayofweek'] = '0';
+$string['locale'] = 'lo_LA.UTF-8';
+$string['localewin'] = 'Lao_Laos.UTF-8';
+$string['localewincharset'] = 'UTF-8';
+$string['oldcharset'] = 'UTF-8';
+$string['strftimedate'] = '%%d %%B %%Y';
+$string['strftimedateshort'] = '%%d %%B';
+$string['strftimedatetime'] = '%%d %%B %%Y, %%I:%%M %%p';
+$string['strftimedaydate'] = '%%A, %%d %%B %%Y';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
+$string['strftimedayshort'] = '%%A, %%d %%B';
+$string['strftimedaytime'] = '%%a, %%H:%%M';
+$string['strftimemonthyear'] = '%%B %%Y';
+$string['strftimerecent'] = '%%d %%b, %%H:%%M';
+$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%I:%%M %%p';
+$string['strftimetime'] = '%%I:%%M %%p';
+$string['thischarset'] = 'UTF-8';
+$string['thisdirection'] = 'ltr';
+$string['thislanguage'] = 'Laotian';
+
+?>
