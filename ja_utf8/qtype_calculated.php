868ec13bf20b23cf49c3d17e418d511976835db0
Mitsuhiro Yoshida <mits>
2007-01-11 20:55:55 +0000

    Translated new strings for qtype calculated.

--- ja_utf8/qtype_calculated.php
+++ ja_utf8/qtype_calculated.php
@@ -4,12 +4,13 @@
 
 $string['addmoreanswerblanks'] = 'さらに答え入力欄を追加する';
 $string['addmoreunitblanks'] = 'さらに $a 件の単位を追加する';
-$string['answerhdr'] = '答え $a';
+$string['answerhdr'] = '答え';
 $string['atleastoneanswer'] = '少なくとも1つの答えを入力してください。';
 $string['correctanswershows'] = '正解の表示';
 $string['correctanswershowsformat'] = 'フォーマット';
 $string['mustbenumeric'] = 'ここには数字を入力してください。';
 $string['mustnotbenumeric'] = 'ここには数字を入力できません。';
+$string['nextpage'] = '次のページ';
 $string['tolerance'] = '許容誤差 &plusmn;';
 $string['youmustenteramultiplierhere'] = 'ここには乗数を入力してください。';
 
