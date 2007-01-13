272b7fb0efca12ab5d958e75b11fb2ed1d0aa7dd
Mitsuhiro Yoshida <mits>
2007-01-13 21:34:11 +0000

    Add a new translated language file for qtype datasetdependent.

---
+++ ja_utf8/qtype_datasetdependent.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // qtype_datasetdependent.php - created with Moodle 1.8 dev (2007011200)
+
+
+$string['additem'] = 'アイテムを追加する';
+$string['calclength'] = '有効数字';
+$string['calcmax'] = '最大';
+$string['calcmin'] = '最小';
+$string['generate'] = '生成';
+$string['itemno'] = 'アイテム $a';
+$string['itemtoadd'] = '追加するアイテム';
+$string['minmax'] = '値域';
+$string['param'] = '変数 {<strong>$a</strong>}';
+
+?>
