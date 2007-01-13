0ab2a762e8658b24e75ba069a0b6354c0c9287ee
Mitsuhiro Yoshida <mits>
2007-01-13 21:11:56 +0000

    Add a new translated language file for mnet.

---
+++ ja_utf8/auth_mnet.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.8 dev (2007011200)
+
+
+$string['sso_idp_description'] = 'このサービスを公開することで、あなたのユーザは再度ログインせずに $a のMoodleサイトを散策することができます。<ul><li><em>従属関係</em>: あなたは $a のSSO (サービスプロバイダ) サービスに<strong>登録</strong>する必要があります。</li></ul><br />このサービスに登録することで、$a からの信頼できるユーザは、再度ログインせずにあなたのサイトにアクセスできます。<ul><li><em>従属関係</em>: あなたは $a にSSO (サービスプロバイダ) サービスを<strong>公開</strong>する必要があります。</li></ul><br />';
+$string['sso_idp_name'] = 'SSO (アイデンティティプロバイダ)';
+$string['sso_sp_description'] = 'このサービスを公開することで、$a からの信頼できるユーザは、再度ログインせずにあなたのサイトにアクセスできます。<ul><li><em>従属関係</em>: あなたは $a のSSO (アイデンティティプロバイダ) サービスに<strong>登録</strong>する必要があります。</li></ul><br />このサービスに登録することで、あなたのユーザは再度ログインせずに $a のMoodleサイトを散策することができます。<ul><li><em>従属関係</em>: あなたは $a にSSO (アイデンティティプロバイダ) サービスを<strong>公開</strong>する必要があります。</li></ul><br />';
+$string['sso_sp_name'] = 'SSO (サービスプロバイダ)';
+
+?>
