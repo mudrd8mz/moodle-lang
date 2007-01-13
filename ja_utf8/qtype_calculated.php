5e954c666379b2cf42c9c30b5954bddf56ece0ec
Mitsuhiro Yoshida <mits>
2007-01-13 21:14:49 +0000

    Translated a new string for qtype calculated.

--- ja_utf8/qtype_calculated.php
+++ ja_utf8/qtype_calculated.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // qtype_calculated.php - created with Moodle 1.8 dev (2007010404)
+      // qtype_calculated.php - created with Moodle 1.8 dev (2007011200)
 
 
 $string['addmoreanswerblanks'] = 'さらに答え入力欄を追加する';
@@ -8,6 +8,7 @@ $string['answerhdr'] = '答え';
 $string['atleastoneanswer'] = '少なくとも1つの答えを入力してください。';
 $string['correctanswershows'] = '正解の表示';
 $string['correctanswershowsformat'] = 'フォーマット';
+$string['makecopynextpage'] = '次のページ (新しい問題)';
 $string['mustbenumeric'] = 'ここには数字を入力してください。';
 $string['mustnotbenumeric'] = 'ここには数字を入力できません。';
 $string['nextpage'] = '次のページ';
