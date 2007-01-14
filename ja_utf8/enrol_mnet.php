52cd6def22480b14636a539d0dc511ee924eed5d
Mitsuhiro Yoshida <mits>
2007-01-14 17:25:28 +0000

    Translated new strings for enrol mnet.

--- ja_utf8/enrol_mnet.php
+++ ja_utf8/enrol_mnet.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_mnet.php - created with Moodle 1.8 dev (2007010300)
+      // enrol_mnet.php - created with Moodle 1.8 dev (2007011200)
 
 
 $string['allcategories'] = '潜在的なカテゴリ $a';
@@ -8,7 +8,9 @@ $string['allow_allcourses'] = 'すべてのリモートコースで登録を許�
 $string['allowedcategories'] = '許可されたカテゴリ $a';
 $string['allowedcourses'] = '許可されたコース $a';
 $string['allowedcourseslinktext'] = 'ここで許可されたコースおよびカテゴリを編集してください。';
+$string['description'] = 'Moodleネットーワーキング経由のユーザ登録方法に関する説明です。';
 $string['enrolname'] = 'Moodleネットワーキング';
+$string['mnet_enrol_description'] = 'このサービスを公開することで、あなたのサーバに作成したコースに $a の管理者は学生を受講登録することができます。<br/><ul><li><em>従属関係</em>: あなたはSSO (サービスプロバイダ) サービスを $a に<strong>公開</strong>する必要があります。</li><li><em>従属関係</em>: あなたは $a のSSO (アイデンティティプロバイダ) サービスに<strong>登録</strong>する必要があります。</li></ul><br/>このサービスに登録することで、あなたの学生を $a のコースに受講登録することができます。<br/><ul><li><em>従属関係</em>: あなたは $a のSSO (サービスプロバイダ) サービスに<strong>登録</strong>する必要があります。</li></ul><li><em>従属関係</em>: あなたはSSO (アイデンティティプロバイダ) サービスを $a に<strong>公開</strong>する必要があります。</li></ul><br/>';
 $string['mnet_enrol_name'] = 'Moodleネットワーキング登録';
 $string['nocategoriesdefined'] = 'コースカテゴリが見つかりませんでした。<a href=\"$a\">ここで</a>新しいカテゴリを設定してください。';
 $string['nocoursesdefined'] = 'コースが見つかりませんでした。<a href=\"$a\">ここで</a>新しいコースを設定してください。';
