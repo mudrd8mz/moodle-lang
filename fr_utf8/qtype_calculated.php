0d440cc0a6d2086d5096ecf7207f0b92f6ec8927
Nicolas Martignoni <martignoni>
2007-01-14 10:56:22 +0000

    Strings sorted

--- fr_utf8/qtype_calculated.php
+++ fr_utf8/qtype_calculated.php
@@ -1,15 +1,15 @@
 <?php // $Id$
 
-$string['correctanswershows'] = 'Correct answer shows';
-$string['correctanswershowsformat'] = 'Format';
-$string['tolerance'] = 'Tolérance &plusmn;';
 $string['addmoreanswerblanks'] = 'Ajouter un autre emplacement de réponse vide.';
 $string['addmoreunitblanks'] = 'Emplacements vides pour $a unités supplémentaires';
 $string['answerhdr'] = 'Réponse';
 $string['atleastoneanswer'] = 'Vous devez fournir au moins une réponse.';
+$string['correctanswershows'] = 'Correct answer shows';
+$string['correctanswershowsformat'] = 'Format';
 $string['makecopynextpage'] = 'Page suivante (nouvelle question)';
 $string['mustbenumeric'] = 'Vous devez saisir un nombre dans ce champ.';
 $string['mustnotbenumeric'] = 'Ceci ne peut pas être un nombre.';
+$string['tolerance'] = 'Tolérance &plusmn;';
 $string['youmustenteramultiplierhere'] = 'Vous devez saisir un multiplicateur dans ce champ.';
 
 ?>
