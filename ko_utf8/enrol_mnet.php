ca210375ac98324d5e107caf09d017fb6f3eaa3b
Lee Chan-young <yogibear270>
2007-01-16 13:30:20 +0000

    new filles - translated.

---
+++ ko_utf8/enrol_mnet.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // enrol_mnet.php - created with Moodle 1.8 dev (2007011200)
+
+
+$string['allcategories'] = '$a 잠재적 범주';
+$string['allcourses'] = '$a 잠재적 배움터';
+$string['allow_allcourses'] = '모든 원격 배움터에서의 출석을 허용';
+$string['allowedcategories'] = '$a 허용된 범주';
+$string['allowedcourses'] = '$a 허용된 배움터';
+$string['allowedcourseslinktext'] = '이곳에서 허용된 범주와 배움터 편집';
+$string['description'] = '무들 네트웍을 통한 출석에 대한 설명';
+$string['enrolname'] = '무들 네트웍';
+$string['mnet_enrol_description'] = '여러분의 서버에 만들어 놓은 배움터에 $a 의 관리자가 허용한 학생들이 출석할 수 있도록 이 서비스를 공개한다.<br/>
+<ul><li><em>필요조건</em>: 반드시 SSO(서비스 제공자) 서비스를 $a에게 <strong>개방</strong>해야만 합니다.</li><li><em>필요조건</em>: 또한 반드시 SSO(자료 제공자)로서 $a 에 <strong>등록</strong>되어 있어야만 합니다.</li></ul><br />이 서비스에 등록함으로써 여러분의 학생들은 $a사이트 배움터에 출석할 수 있게 됩니다.<br/> <ul><li><em>필요조건</em>: 반드시 $a의 SSO(서비스 제공자)로 <strong>등록</strong>해야만 합니다.</li><li><em>필요조건</em>: 반드시 SSO(자료 제공자) 서비스를 $a에게 <strong>개방</strong>해야만 합니다.</li></ul><br />';
+$string['mnet_enrol_name'] = '무들 네트웍을 통한 출석';
+$string['nocategoriesdefined'] = '배움터 범주가 없음. <a href=\"$a\">이 곳</a>에서 새로운 범주를 정의하시오.';
+$string['nocoursesdefined'] = '배움터가 없음. <a href=\"$a\">이 곳</a>에서 새로운 배움터를 정의하시오.';
+
+?>
