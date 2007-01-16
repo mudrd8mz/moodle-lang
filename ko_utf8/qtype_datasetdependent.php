fa5a8f274feea07e8f0eaaefe0e2efba71b30fa5
Lee Chan-young <yogibear270>
2007-01-16 13:35:58 +0000

    new filles - translated.

---
+++ ko_utf8/qtype_datasetdependent.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // qtype_datasetdependent.php - created with Moodle 1.8 dev (2007011200)
+
+
+$string['additem'] = '문항 추가';
+$string['calcdistribution'] = '분산';
+$string['calclength'] = '유효숫자';
+$string['calcmax'] = '최대';
+$string['calcmin'] = '최소';
+$string['generate'] = '생성';
+$string['itemno'] = '문제 $a';
+$string['itemtoadd'] = '추가할 문항';
+$string['loguniform'] = '로그유니폼';
+$string['minmax'] = '값의 범위';
+$string['param'] = '계수 {<strong>$a</strong>}';
+$string['uniform'] = '유니폼';
+
+?>
