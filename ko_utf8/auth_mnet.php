2aa14be5d3dae59acb306666b280633ea4b57ab6
Lee Chan-young <yogibear270>
2007-01-16 13:57:54 +0000

    new filles - translated.

---
+++ ko_utf8/auth_mnet.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.8 dev (2007011200)
+
+
+$string['sso_idp_description'] = '이 서비스를 공개함으로서 여러분의 사용자는 다시 로그인 하지 않고도 $a 사이트에 접속할 수 있게 됩니다. <ul><li><em>필요조건</em>: 반드시 $a 의 SSO(서비스 제공자)로 <strong>등록</strong>되어 있어야만 합니다.</li></ul><br />이 서비스를 등록함으로써 $a 에서 인증된 사용자들은 다시 로그인하지 않아도 여러분의 사이트들을 탐방할 수 있게 됩니다. <ul><li><em>필요조건</em>: 또한 반드시 SSO(서비스 제공자)로 $a 에게 <strong>개방</strong>해야만 합니다.</li></ul><br />';
+$string['sso_idp_name'] = 'SSO (자료 제공자)';
+$string['sso_sp_description'] = '이 서비스를 공개함으로서 $a 로부터 인증된 사용자는 다시 로그인 하지 않고도 여러분의 사이트에 접속할 수 있게 됩니다. <ul><li><em>필요조건</em>: 반드시 $a 의 SSO(자료 제공자)로 <strong>등록</strong>해야만 합니다.</li></ul><br />이 서비스에 등록하면 여러분의 사용자들은 다시 로그인하지 않아도 $a 무들 사이트들을 탐방할 수 있게 됩니다. <ul><li><em>필요조건</em>: 또한 반드시 SSO(자료제공자)로 $a 에게 <strong>개방</strong>해야만 합니다.</li></ul><br />';
+$string['sso_sp_name'] = 'SSO (서비스 제공자)';
+
+?>
