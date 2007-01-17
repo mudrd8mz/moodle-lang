755ea2910fe721724efc288689be81dacee04598
Mitsuhiro Yoshida <mits>
2007-01-17 18:57:35 +0000

    Add new stringsfor qtype datasetdependent.

--- ja_utf8/qtype_datasetdependent.php
+++ ja_utf8/qtype_datasetdependent.php
@@ -6,10 +6,13 @@ $string['additem'] = 'アイテムを追加する';
 $string['calclength'] = '有効数字';
 $string['calcmax'] = '最大';
 $string['calcmin'] = '最小';
-$string['generate'] = '生成';
+$string['deletelastitem'] = '最後のアイテムを削除する';
 $string['itemno'] = 'アイテム $a';
 $string['itemtoadd'] = '追加するアイテム';
 $string['minmax'] = '値域';
 $string['param'] = '変数 {<strong>$a</strong>}';
+$string['replacewithrandom'] = 'ランダム値と置換する';
+$string['reuseifpossible'] = '可能な場合、前の値を再利用する';
+$string['youmustaddatleastoneitem'] = 'この問題を保存する前に少なくとも1つのデータセットを追加してください。';
 
 ?>
