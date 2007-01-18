65e35fd440584fab4a5729bf364902b14af6c517
Nicolas Martignoni <martignoni>
2007-01-18 20:20:13 +0000

    more work on the datasetdependent / calculated question creation forms

--- fr_utf8/qtype_calculated.php
+++ fr_utf8/qtype_calculated.php
@@ -9,6 +9,7 @@ $string['correctanswershowsformat'] = 'Format';
 $string['makecopynextpage'] = 'Page suivante (nouvelle question)';
 $string['mustbenumeric'] = 'Vous devez saisir un nombre dans ce champ.';
 $string['mustnotbenumeric'] = 'Ceci ne peut pas être un nombre.';
+$string['nextitemtoadd'] = '«&nbsp;Élément à ajouter&nbsp;» suivant';
 $string['nextpage'] = 'Page suivante';
 $string['tolerance'] = 'Tolérance &plusmn;';
 $string['youmustenteramultiplierhere'] = 'Vous devez saisir un multiplicateur dans ce champ.';
--- fr_utf8/qtype_datasetdependent.php
+++ fr_utf8/qtype_datasetdependent.php
@@ -1,16 +1,21 @@
 <?php // $Id$
 
 $string['itemno'] = 'Élément $a';
+$string['deletelastitem'] = 'Supprimer dernier élément';
 $string['param'] = 'Paramètre {<strong>$a</strong>}';
 $string['additem'] = 'Ajouter élément';
 $string['itemtoadd'] = 'Élément à ajouter';
-$string['calclength'] = 'Chiffres significatifs';
+$string['calclength'] = 'Décimales';
 $string['calcdistribution'] = 'Distribution';
 $string['uniform'] = 'Uniforme';
 $string['loguniform'] = 'Log-uniforme';
-$string['generate'] = 'Générer';
 $string['calcmin'] = 'Minimum';
 $string['calcmax'] = 'Maximum';
 $string['minmax'] = 'Intervalle des valeurs';
+$string['replacewithrandom'] = 'Remplacer par une valeur aléatoire';
+$string['getnextnow'] = 'Obtenir un nouvel «&nbsp;élément à ajouter&nbsp;»';
+$string['forceregeneration'] = 'Imposer une regénération';
+$string['reuseifpossible'] = 'Réutiliser la valeur précédente si disponible';
+$string['youmustaddatleastoneitem'] = 'Veuillez ajouter au moins un élément du jeu de données avant d\'enregistrer cette question.';
 
 ?>
