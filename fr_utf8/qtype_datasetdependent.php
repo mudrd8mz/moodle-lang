d932625597697ac3dd85bb6e1ec2e3876c7ba9c6
Nicolas Martignoni <martignoni>
2007-01-18 20:20:46 +0000

    Sort strings

--- fr_utf8/qtype_datasetdependent.php
+++ fr_utf8/qtype_datasetdependent.php
@@ -1,21 +1,21 @@
 <?php // $Id$
 
-$string['itemno'] = 'Élément $a';
-$string['deletelastitem'] = 'Supprimer dernier élément';
-$string['param'] = 'Paramètre {<strong>$a</strong>}';
 $string['additem'] = 'Ajouter élément';
-$string['itemtoadd'] = 'Élément à ajouter';
-$string['calclength'] = 'Décimales';
 $string['calcdistribution'] = 'Distribution';
-$string['uniform'] = 'Uniforme';
-$string['loguniform'] = 'Log-uniforme';
-$string['calcmin'] = 'Minimum';
+$string['calclength'] = 'Décimales';
 $string['calcmax'] = 'Maximum';
-$string['minmax'] = 'Intervalle des valeurs';
-$string['replacewithrandom'] = 'Remplacer par une valeur aléatoire';
-$string['getnextnow'] = 'Obtenir un nouvel «&nbsp;élément à ajouter&nbsp;»';
+$string['calcmin'] = 'Minimum';
+$string['deletelastitem'] = 'Supprimer dernier élément';
 $string['forceregeneration'] = 'Imposer une regénération';
+$string['getnextnow'] = 'Obtenir un nouvel «&nbsp;élément à ajouter&nbsp;»';
+$string['itemno'] = 'Élément $a';
+$string['itemtoadd'] = 'Élément à ajouter';
+$string['loguniform'] = 'Log-uniforme';
+$string['minmax'] = 'Intervalle des valeurs';
+$string['param'] = 'Paramètre {<strong>$a</strong>}';
+$string['replacewithrandom'] = 'Remplacer par une valeur aléatoire';
 $string['reuseifpossible'] = 'Réutiliser la valeur précédente si disponible';
+$string['uniform'] = 'Uniforme';
 $string['youmustaddatleastoneitem'] = 'Veuillez ajouter au moins un élément du jeu de données avant d\'enregistrer cette question.';
 
 ?>
