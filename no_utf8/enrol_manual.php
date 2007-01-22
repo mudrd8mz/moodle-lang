7dd474d88b61d5c5e056c2c43ce05fb441b2f726
solveigb <solveigb>
2007-01-22 14:40:23 +0000

    Added to repository. Completed translation. Should be reviewed.

---
+++ no_utf8/enrol_manual.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // enrol_manual.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['description'] = 'Dette er standard form for innmelding. Det er to hovedmåter en student kan bli innmeldt på et spesielt kurs. 
+<ul>
+<li>En lærer eller administrator kan melde dem inn manuelt ved hjelp av lenken i Kursadministrasjonsmenyen innen kurset.</li>
+<li>Det kan være definert et passord for et kurs, kalt \"innmeldingsnøkkelen\". Alle som kjenner denne nøkkelen er i stand til å legge seg selv inn i et kurs.</li>
+</ul>';
+$string['enrolname'] = 'Intern påmelding.';
+
+?>
