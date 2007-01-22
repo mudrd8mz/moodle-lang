0271923ee78f7e240da6188f41b476ae0687c3c1
solveigb <solveigb>
2007-01-22 14:44:09 +0000

    Added to repository. Started translation.

---
+++ no_utf8/data.php
@@ -0,0 +1,33 @@
+<?PHP // $Id$ 
+      // data.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['action'] = 'Handling';
+$string['add'] = 'Legg til post';
+$string['addafield'] = 'Legg til et felt';
+$string['addcomment'] = 'Legg til kommentar';
+$string['addtemplate'] = 'Legg til mal';
+$string['allowcomments'] = 'Tillat kommentarer?';
+$string['allowratings'] = 'Tillat vurdering av oppføringer?';
+$string['approve'] = 'Godkjenn';
+$string['ascending'] = 'Stigende';
+$string['atmaxentry'] = 'Du har lagt inn det maksimale antall oppføringer som er tillatt!';
+$string['autogenallforms'] = 'Generer alle standardmaler';
+$string['autolinkurl'] = 'Lag akelapperutomatisk lenke til URL';
+$string['availablefromdate'] = 'Tilgjengelig fra';
+$string['availabletags'] = 'Tilgjengelige mer';
+$string['savesettings'] = 'Lagre innstillinger';
+$string['savetemplate'] = 'Lagre mal';
+$string['singletemplate'] = 'Enkelt mal';
+$string['teachersandstudents'] = '$a->teachers og $a->students';
+$string['templates'] = 'Maler';
+$string['templatesaved'] = 'Malen er lagret';
+$string['text'] = 'Tekst';
+$string['textarea'] = 'Tekstområde';
+$string['type'] = 'Felttype';
+$string['uploadfile'] = 'Last opp fil';
+$string['url'] = 'Url';
+$string['viewfromdate'] = 'Kan sees fra';
+$string['viewtodate'] = 'Kan sees til';
+
+?>
---
+++ no_utf8/enrol_authorize.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$ 
+      // enrol_authorize.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['adminacceptccs'] = 'Hvilke typer kredittkort vil bli akseptert?';
+$string['adminauthorizewide'] = 'Innstillinger for hele nettstedet';
+$string['delete'] = 'Ødelegg';
+$string['missingaddress'] = 'Adresse mangler';
+$string['missingcc'] = 'Kortnummer mangler';
+$string['missingccexpire'] = 'Utløpsdato mangler';
+$string['missingcctype'] = 'Korttype mangler';
+$string['missingzip'] = 'Postnummer mangler';
+$string['nameoncard'] = 'Navnet på kortet';
+$string['new'] = 'Ny';
+$string['paymentmanagement'] = 'Betalingshåndtering';
+
+?>
