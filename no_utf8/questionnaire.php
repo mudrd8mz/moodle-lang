46c6367483e8cc511cf5268afdd6a44f78a4671f
solveigb <solveigb>
2007-01-22 14:37:00 +0000

    Completed translation. Should be reviewed.

---
+++ no_utf8/questionnaire.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // questionnaire.php - created with Moodle 1.6.3 (2006050530)
+
+
+$string['alreadyfilled'] = 'Du har allerede fyllt ut dette spørreskjemaet for oss. Tusen takk.';
+$string['modulename'] = 'Spørreskjema';
+$string['modulenameplural'] = 'Spørreskjema';
+$string['mustcomplete'] = '<b>Du må fullføre dette spørreskjemaet <i>nå</i> for å registrere resultene. Du kan ikke fullføre det senere</b><br /> <br />';
+$string['notavail'] = 'Det spørreskjemaet er ikke tilgjengelig ennå. Forsøk igjen senere.';
+$string['qmanage'] = 'Administrer spørreundersøkelser';
+$string['qmanagetitle'] = 'Spørreundersøkelsesadministrasjon phpESP';
+$string['qtype'] = 'Type';
+$string['questionnaireid'] = 'Spørreundersøkelse';
+$string['respondenttype'] = 'Svartype';
+
+?>
