35c7ace8deb702789469faaff68250e63cbff5c0
Nicolas Martignoni <martignoni>
2007-01-23 19:29:40 +0000

    Mnet:  Update user's enrolment information on jump/land

--- fr_utf8/auth_mnet.php
+++ fr_utf8/auth_mnet.php
@@ -2,6 +2,7 @@
 
 $string['sso_idp_name'] = 'SSO (fournisseur d\'identité)';
 $string['sso_idp_description'] = 'La publication de ce service permet à vos utilisateurs d\'utiliser le site Moodle $a sans devoir s\'y reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi vous <strong>abonner</strong> au service SSO (fournisseur de service) sur $a.</li></ul><br /> L\'abonnement à ce service permet à des utilisateurs authentifiés par le serveur $a d\'accéder à votre site devoir sans se reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi <strong>publier</strong> le service SSO (fournisseur de service) de $a.</li></ul><br />';
+$string['sso_mnet_login_refused'] = 'L\'utilisateur $a[0] n\'est pas autorisé à se connecter depuis $a[1].';
 $string['sso_sp_name'] = 'SSO (fournisseur de service)';
 $string['sso_sp_description'] = 'La publication de ce service permet à des utilisateurs authentifiés par le serveur $a d\'accéder à votre site devoir sans se reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi vous <strong>abonner</strong> au service SSO (fournisseur d\'identité) de $a.</li></ul><br />L\'abonnement à ce service permet à vos utilisateurs d\'utiliser le site Moodle $a sans devoir s\'y reconnecter.<ul><li><em>Dépendance</em>&nbsp;: vous devez aussi <strong>publier</strong> le service SSO (fournisseur d\'identité) de $a.</li></ul><br />';
 
