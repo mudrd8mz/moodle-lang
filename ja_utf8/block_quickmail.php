1575d11dfdfae925ea28da39d9f1e9497d15dfab
Mitsuhiro Yoshida <mits>
2007-01-30 20:13:18 +0000

    Translated new strings for quickmail.

--- ja_utf8/block_quickmail.php
+++ ja_utf8/block_quickmail.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_quickmail.php - created with Moodle 1.7 dev (2006071800)
+      // block_quickmail.php - created with Moodle 1.8 dev (2007012500)
 
 
 $string['action'] = '操作';
 $string['allowstudents'] = '$a にクイックメールの使用を許可する';
+$string['areyousure'] = '本当に履歴中のすべてのメールを削除してもよろしいですか?';
 $string['attachment'] = '添付';
 $string['attachmentalt'] = 'メールに添付ファイルを追加する';
 $string['attachmenterror'] = '有効な添付ファイルではありません! 次のファイルは存在していません： <strong>$a</strong>';
@@ -11,8 +12,7 @@ $string['attachmentoptional'] = '添付 (任意)';
 $string['blockname'] = 'クイックメール';
 $string['check'] = 'すべてを選択';
 $string['clearhistory'] = 'すべての履歴をクリア';
-$string['composeemail'] = 'メールの作成';
-$string['composenew'] = '新しいメールの作成';
+$string['compose'] = '作成';
 $string['date'] = '日時';
 $string['delete'] = '削除';
 $string['deletefail'] = '削除が失敗しました。';
@@ -20,18 +20,16 @@ $string['deletesuccess'] = '正常に削除されました。';
 $string['email'] = 'メール';
 $string['emailfail'] = 'メールエラー:';
 $string['emailfailerror'] = 'エラーが発生したため、下記のユーザにメール送信されませんでした ...';
-$string['emailhistory'] = 'クイックメール履歴';
 $string['emailstop'] = 'メールアドレスが無効にされています:';
+$string['history'] = '履歴';
 $string['messageerror'] = 'メッセージを入力してください!';
 $string['nogroupmembers'] = 'グループメンバーなし';
 $string['notingroup'] = 'グループ外';
-$string['pleasewait'] = 'お待ちください、メールを送信しています ...';
 $string['sendemail'] = 'メールを送信する';
 $string['subjecterror'] = '題名を入力してください!';
 $string['successfulemail'] = 'メールが正常に送信されました。';
 $string['to'] = 'To';
 $string['toerror'] = 'メールの受信者を選択してください!';
 $string['uncheck'] = 'すべての選択を解除';
-$string['view'] = '表示';
 
 ?>
