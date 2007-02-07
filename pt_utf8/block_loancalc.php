15186a55766c4dd42bc2443ab10d11867e580cb0
Jaime Villate <villate>
2007-02-07 20:06:05 +0000

    Improved the translation.

--- pt_utf8/block_loancalc.php
+++ pt_utf8/block_loancalc.php
@@ -1,15 +1,15 @@
 <?PHP // $Id$ 
-      // block_loancalc.php - created with Moodle 1.6 (2006050506)
+      // block_loancalc.php - created with Moodle 1.7.1+ (2006101010)
 
 
 $string['amountofloan'] = 'Valor do empréstimo';
-$string['fortnightly'] = 'Fortnightly';
-$string['interestrate'] = 'Taxa de interesse';
-$string['loancalc'] = 'Cálculo do empréstimo';
-$string['loanterm'] = 'Término do empréstimo (Anos)';
+$string['fortnightly'] = 'Cada noite';
+$string['interestrate'] = 'Taxa de juros';
+$string['loancalc'] = 'Cálculadora de empréstimos';
+$string['loanterm'] = 'Praço do empréstimo (anos)';
 $string['monthly'] = 'Mensal';
-$string['repaymentamount'] = 'Valor Pagamento';
-$string['repaymentfreq'] = 'Frequencia Pagamento';
+$string['repaymentamount'] = 'Valor prestações';
+$string['repaymentfreq'] = 'Frequência das prestações';
 $string['weekly'] = 'Semanal';
 
 ?>
