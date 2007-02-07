7cad85606b14d209091ff7dedda3043b8ee01676
Jaime Villate <villate>
2007-02-07 20:42:13 +0000

    Improved and completed the translation.

--- pt_utf8/block_rss_client.php
+++ pt_utf8/block_rss_client.php
@@ -1,42 +1,48 @@
 <?PHP // $Id$ 
-      // block_rss_client.php - created with Moodle 1.6 (2006050506)
+      // block_rss_client.php - created with Moodle 1.7.1+ (2006101010)
 
 
-$string['addfeed'] = 'Adicionar um URL alimentador de notícias';
-$string['addheadlineblock'] = 'Adicionar bloco RSS headline';
+$string['addfeed'] = 'Adicionar URL de um alimentador de notícias';
+$string['addheadlineblock'] = 'Adicionar bloco de titulares RSS';
 $string['addnew'] = 'Adicionar novo';
-$string['choosefeedlabel'] = 'Escolher quais os feeds que ficarão disponíveis neste bloco:';
-$string['clientchannellink'] = 'Site de origem...';
-$string['clientnumentries'] = 'O número de entradas por defeito a mostrar por feed.';
-$string['clientshowchannellinklabel'] = 'Deverá ser mostrado um link para o site original (link do canal)? (Note que se nenhum link do feed de notícias for fornecido, nenhum link será mostrado):';
+$string['blockname'] = 'Cliente RSS';
+$string['choosefeedlabel'] = 'Escolher quais as listas RSS que ficarão disponíveis neste bloco:';
+$string['clientchannellink'] = 'Sítio de origem...';
+$string['clientnumentries'] = 'Número de tópicos a mostrar na lista RSS, por omissão.';
+$string['clientshowchannellinklabel'] = 'Deverá mostrar-se um apontador para o sítio original (apontador do canal)? (Note que se não for dado nenhum apontador não será apresentado nada):';
 $string['clientshowimagelabel'] = 'Mostrar imagem do canal se disponível:';
 $string['configblock'] = 'Configurar este bloco';
-$string['couldnotfindfeed'] = 'Não consegue encontrar o feed com ID';
-$string['customtitlelabel'] = 'Título habitual (deixe em branco para utilizar o título fornecido pelo feed):';
-$string['deletefeedconfirm'] = 'Tem a certeza que deseja remover este feed?';
-$string['displaydescriptionlabel'] = 'Mostrar descrição de cada link?';
-$string['editfeeds'] = 'Editar, subscrever ou desistir de feed de notícias RSS/Atom';
-$string['editnewsfeeds'] = 'Editar feed de notícias';
-$string['editrssblock'] = 'Editar bloco RSS Headline';
-$string['feed'] = 'Feed';
-$string['feedadded'] = 'Adicionado feed de notícias';
-$string['feeddeleted'] = 'Removido feed de notícias';
-$string['feeds'] = 'Feeds de notícias';
-$string['feedsaddedit'] = 'Adicionar/Editar Feeds';
-$string['feedsconfigurenewinstance'] = 'Clique aqui para configurar este bloco para mostrar feeds RSS';
-$string['feedstitle'] = 'Feeds RSS remotos';
-$string['feedupdated'] = 'Actualizado feed de notícias';
-$string['findmorefeeds'] = 'Encontrar mais feeds RSS';
-$string['managefeeds'] = 'Controlar todos os meus feeds';
-$string['nofeeds'] = 'Não há feeds RSS definidos para este site';
-$string['pickfeed'] = 'Apanhar um feed de notícias';
-$string['remotenewsfeed'] = 'Feed de notícias remoto';
-$string['seeallfeeds'] = 'Ver todos os feeds';
-$string['shownumentrieslabel'] = 'Número máximo de entradas a mostrar por bloco';
-$string['submitters'] = 'A quem será permitido definir um novo feed RSS? Os feeds definidos estão disponíneis para qualquer página no site';
+$string['couldnotfindfeed'] = 'Não foi possível encontrar a lista RSS com identificador';
+$string['customtitlelabel'] = 'Título habitual (deixe em branco para utilizar o título fornecido pelo canal):';
+$string['deletefeedconfirm'] = 'Tem a certeza que deseja remover esta lista RSS?';
+$string['displaydescriptionlabel'] = 'Mostrar descrição de cada apontador?';
+$string['editfeeds'] = 'Editar, subscrever ou desistir do canal de notícias RSS';
+$string['editnewsfeeds'] = 'Editar lista RSS de notícias';
+$string['editrssblock'] = 'Editar bloco de titulares RSS';
+$string['feed'] = 'Lista RSS';
+$string['feedadded'] = 'Adicionada lista de notícias RSS';
+$string['feeddeleted'] = 'Lista de notícias RSS removida';
+$string['feeds'] = 'Lista de notícias RSS';
+$string['feedsaddedit'] = 'Adicionar/Editar lista RSS';
+$string['feedsconfigurenewinstance'] = 'Clique aqui para configurar este bloco para mostrar listas RSS';
+$string['feedstitle'] = 'Listas RSS remotas';
+$string['feedupdated'] = 'Lista de notícias RSS actualizada';
+$string['findmorefeeds'] = 'Encontrar mais listas RSS';
+$string['managefeeds'] = 'Gerir todas as listas RSS';
+$string['nofeeds'] = 'Não há listas RSS definidas para este sítio';
+$string['pickfeed'] = 'Seleccionar uma lista de notícias RSS';
+$string['remotenewsfeed'] = 'Lista de notícias RSS  remota';
+$string['rss_client:createprivatefeeds'] = 'Criar lista RSS privada';
+$string['rss_client:createsharedfeeds'] = 'Criar lista RSS partilhada';
+$string['rss_client:manageanyfeeds'] = 'Gerir qualquer lista RSS';
+$string['rss_client:manageownfeeds'] = 'Gerir listas RSS próprias';
+$string['seeallfeeds'] = 'Ver todas as listas RSS';
+$string['sharedfeed'] = 'Lista RSS partilhada';
+$string['shownumentrieslabel'] = 'Número máximo de titulares a mostrar por bloco';
+$string['submitters'] = 'A quem será permitido definir uma nova lista RSS? As listas RSS definidas estão disponíneis para qualquer página no sítio.';
 $string['timeout'] = 'Bloco RSS parado';
-$string['timeoutdesc'] = 'Tempo, em minutos, para um feed RSS ficar em cache';
-$string['updatefeed'] = 'Actualizar um URL de feed de notícias:';
-$string['validatefeed'] = 'Validar feed';
+$string['timeoutdesc'] = 'Tempo, em minutos, para uma lista RSS ficar em cache';
+$string['updatefeed'] = 'Actualizar URL de uma lista de notícias RSS:';
+$string['validatefeed'] = 'Validar lista RSS';
 
 ?>
