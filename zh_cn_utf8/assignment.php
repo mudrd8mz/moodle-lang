308aaaefc2d522b52cbf3c955b139663e88f99bc
sunner_sun <sunner_sun>
2007-02-11 06:53:37 +0000

    Fix several strings

--- zh_cn_utf8/assignment.php
+++ zh_cn_utf8/assignment.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.7+ (2006101007)
+      // assignment.php - created with Moodle 1.7.1+ (2006101010)
 
 
 $string['allowdeleting'] = '允许删除';
@@ -23,7 +23,7 @@ $string['comment'] = '评论';
 $string['commentinline'] = '内嵌点评';
 $string['configitemstocount'] = '学生提交的在线作业的计数单位';
 $string['configmaxbytes'] = '缺省情况下站点中作业的最大尺寸(同时受课程和其它本地设置的限制)';
-$string['confirmdeletefile'] = '您完全确信要删除此文件吗?<br /><strong>$a</strong>';
+$string['confirmdeletefile'] = '您完全确信要删除此文件吗？<br /><strong>$a</strong>';
 $string['deletefilefailed'] = '文件删除失败';
 $string['description'] = '描述';
 $string['draft'] = '草稿';
@@ -49,12 +49,16 @@ $string['guestnoupload'] = '抱歉，访客用户不允许提交作业';
 $string['helpoffline'] = '<p>如果学生在Moodle之外完成作业（比如在其它网站，或者与教师直接交流），那么可以使用这种类型的作业。</p><p> 学生可以看到作业的描述，但不能上传包括文件在内的任何信息。教师依然可以对作业评分，学生也会得到关于他们的分数的通知。</p>';
 $string['helponline'] = '<p>这种类型的作业让学生使用普通的编辑工具来编辑文本。教师可以对作业在线评分，甚至可以修改和嵌入点评到学生的答案里。</p>
 <p>（如果您熟悉旧版本的Moodle，那么这种类型的作业完成和旧的日志模块相同的工作。）</p>';
-$string['helpupload'] = '<p>此类作业允许每个参与者以任何格式上传一个或多个文件。这些文件可能是word处理的文档、图片、zipped的网页或者任何您要求提交的内容。</p>
-<p>此类作业也允许您上传多种响应文件。响应文件也可以是先前上交的，可以用于每个参与者的不同文件使用的。</p>
-<p>参与者们也可以录入注释来描述提交的文件，声明状态或任何其它文本信息。</p>
-<p>此类作业的提交必须是参与者手工定稿的。您可以在任何时间回顾当前的状态，未完成的作业会被标记为草稿。您可以把任何未打分的作业恢复成草稿的状态。</p>';
+$string['helpupload'] = '<p>这种类型的作业允许每个参与者上传任何类型的一个或多个文件。
+它们可以是Word文档，图片，打包了的网站，或者任何您想让他们提交的。</p>
+
+<p>此类作业也允许您上传多个反馈文件。反馈文件也可以在作业提交前就上传，这样可以给每个参与者不同的文件。</p>
+
+<p>参与者们也可以输入注释来说明提交的文件、进度状况或任何其它文本信息。</p>
+
+<p>此类作业的提交必由参与者手工设定为结束。您可以在任何时间检查当前的状态，未完成的作业会被标记为草稿。您可以把任何未打分的作业恢复成草稿的状态。</p>';
 $string['helpuploadsingle'] = '<p>这种类型的作业允许每个参与者上传任何类型的一个文件。</p> <p>可以是一个Word文档，一个图片，一个打包了的网站，或者任何您想让他们提交的。</p>';
-$string['hideintro'] = '在有效日期前隐藏的描述';
+$string['hideintro'] = '在开始日期前隐藏描述';
 $string['late'] = '$a 之后';
 $string['maximumgrade'] = '最高得分';
 $string['maximumsize'] = '大小限制';
@@ -63,23 +67,23 @@ $string['modulenameplural'] = '作业';
 $string['newsubmissions'] = '已交的作业';
 $string['noassignments'] = '尚无作业';
 $string['noattempts'] = '尚无人尝试做此作业';
-$string['nofiles'] = '无提交的文件';
-$string['nofilesyet'] = '无文件已经提交';
-$string['notavailableyet'] = '对不起，此作业已经无效了。<br />将在给出的日期下显示出作业指令。';
+$string['nofiles'] = '没有提交任何文件';
+$string['nofilesyet'] = '还没有文件被提交';
+$string['notavailableyet'] = '对不起，此作业还未开始。<br />下面的日期后，将在此显示作业说明。';
 $string['notes'] = '注释';
-$string['notesempty'] = '未登录';
+$string['notesempty'] = '无';
 $string['notesupdateerror'] = '在更新注释时有错';
 $string['notgradedyet'] = '没有评分';
 $string['notsubmittedyet'] = '未交';
-$string['onceassignmentsent'] = '一旦发送了为打分的作业，您将不再能删除或粘贴文件（或多个文件）了。您确定要继续吗？';
+$string['onceassignmentsent'] = '一旦发送了评分请求，您将不再能删除或上传文件了。您确定要继续吗？';
 $string['overwritewarning'] = '警告: 重新上载将覆盖您已交的作业';
 $string['pagesize'] = '每页显示作业数';
 $string['preventlate'] = '不允许迟交';
 $string['quickgrade'] = '允许快速评分';
-$string['responsefiles'] = '响应文件';
+$string['responsefiles'] = '反馈文件';
 $string['reviewed'] = '复习';
 $string['saveallfeedback'] = '保存我的所有反馈';
-$string['sendformarking'] = '为打分发送';
+$string['sendformarking'] = '发送评分请求';
 $string['submission'] = '提交';
 $string['submissiondraft'] = '提交草稿';
 $string['submissionfeedback'] = '对提交信息的反馈';
@@ -87,13 +91,13 @@ $string['submissions'] = '提交信息';
 $string['submissionsaved'] = '已经保存了您的修改';
 $string['submissionsnotgraded'] = '$a 个作业尚未评分';
 $string['submitassignment'] = '用此表单上交作业';
-$string['submitedformarking'] = '为打分的作业已经提交了，而且不能再更新了';
-$string['submitformarking'] = '为作业打分的最终提交';
+$string['submitedformarking'] = '作业正在等待被评分，而且不能再更新了';
+$string['submitformarking'] = '最终版本，可以评分';
 $string['submitted'] = '已交';
 $string['submittedfiles'] = '提交的文件';
 $string['typeoffline'] = '离线活动项目';
 $string['typeonline'] = '在线文本';
-$string['typeupload'] = '高级的文件上传';
+$string['typeupload'] = '高级文件上传';
 $string['typeuploadsingle'] = '上载单个文件';
 $string['unfinalize'] = '还原成草稿';
 $string['uploadbadname'] = '该文件名含有怪异字符，无法上载';
