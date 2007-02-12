fbcb1860d5a9a111879d8d0537fabb91d8ee9882
Benito Arias <barias>
2007-02-12 12:20:11 +0000

    *** empty log message ***

---
+++ es_utf8/qtype_calculated.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // qtype_calculated.php - created with Moodle 1.7 (2006101007)
+
+
+$string['addmoreanswerblanks'] = 'Agregar otra respuesta en blanco.';
+$string['addmoreunitblanks'] = 'Espacios en blanco para $a unidades más';
+$string['answerhdr'] = 'Respuesta';
+$string['atleastoneanswer'] = 'Necesita proporcionar al menos una respuesta.';
+$string['correctanswershows'] = 'La respuesta correcta muestra';
+$string['correctanswershowsformat'] = 'Formato';
+$string['makecopynextpage'] = 'Página siguiente (nueva pregunta)';
+$string['mustbenumeric'] = 'Debe escribir un número.';
+$string['mustnotbenumeric'] = 'No puede ser un número.';
+$string['nextitemtoadd'] = 'Siguiente \'Item a agregar\'';
+$string['nextpage'] = 'Siguiente Página';
+$string['tolerance'] = 'Tolerancia ±';
+$string['youmustenteramultiplierhere'] = 'Debe escribir un multiplicador.';
+
+?>
