12c07a664bc6b2d4c2dd31a32ac4093f0de8dd46
koen roggemans <koenr>
2007-02-18 22:00:46 +0000

    new strings from Finjon Kiang

--- zh_tw_utf8/assignment.php
+++ zh_tw_utf8/assignment.php
@@ -1,1 +1,109 @@
-<?PHP // $Id$       // assignment.php - created with Moodle 1.0.8.1 (2003011200)$string['allowresubmit'] = "批改後再繳交";$string['assignmentdetails'] = "作業詳細說明";$string['assignmentname'] = "作業名稱";$string['assignmenttype'] = "作業型態";$string['description'] = "說明";$string['duedate'] = "繳交日期";$string['early'] = "\$a 之前";$string['failedupdatefeedback'] = "給使用者 \$a 回饋訊息時: 失敗 ";$string['feedback'] = "回饋";$string['feedbackupdated'] = "替 \$a 個人更新繳交後回饋";$string['late'] = "\$a 之後";$string['maximumgrade'] = "最高得分";$string['maximumsize'] = "檔案大小限制";$string['modulename'] = "作業";$string['modulenameplural'] = "作業";$string['newsubmissions'] = "作業已繳交";$string['notsubmittedyet'] = "尚未繳交作業";$string['overwritewarning'] = "警告: 再次上載作業 將取代前次繳交內容";$string['saveallfeedback'] = "儲存所有回饋";$string['submissionfeedback'] = "繳交後的回饋";$string['submissions'] = "繳交";$string['submitassignment'] = "請利用本表單繳交作業";$string['submitted'] = "已送出";$string['typeoffline'] = "離線作業";$string['typeuploadsingle'] = "上載一個檔案";$string['uploadbadname'] = "上載檔案包含奇怪檔名 上載不成功 ";$string['uploadedfiles'] = "已上載的檔案";$string['uploaderror'] = "儲存資料於伺服器時發生錯誤";$string['uploadfailnoupdate'] = "檔案上載成功 但是無法更新作業繳交訊息!";$string['uploadfiletoobig'] = "抱歉, 檔案過大 不得超過 \$a bytes)";$string['uploadnofilefound'] = "沒有檔案名稱 - 你確定已經選擇上載檔案檔名了嗎?";$string['uploadnotregistered'] = "'\$a' 上載成功,但是尚未登記!";$string['uploadsuccess'] = "上載 '\$a' 成功 ";$string['viewfeedback'] = "檢視作業分數和回饋訊息";$string['viewsubmissions'] = "檢視 \$a 份已繳交作業";$string['yoursubmission'] = "個人已繳交的作業";?>
+<?PHP // $Id$ 
+      // assignment.php - created with Moodle 1.6 + (2006050507)
+
+
+$string['allowmultiple'] = '允許多個檔案';
+$string['allowresubmit'] = '批改後再繳交';
+$string['assignmentdetails'] = '作業詳細說明';
+$string['assignmentmail'] = '$a->teacher 對您的作業 $a->assignment 做了意見的回饋。您可以從 $a->url 來觀看。';
+$string['assignmentmailhtml'] = '$a->teacher 對您的作業 $a->assignment 做了意見的回饋。您可以從 $a->url 來觀看。';
+$string['assignmentname'] = '作業名稱';
+$string['assignmenttype'] = '作業型態';
+$string['attachfile'] = '附加檔案';
+$string['attachfiletoassignment'] = '附加檔案到作業中';
+$string['availabledate'] = '開始於';
+$string['backtoassignment'] = '回到作業';
+$string['backtofeedback'] = '回到意見';
+$string['choosereviewfile'] = '選擇要上傳的作業檔案：';
+$string['comment'] = '評語';
+$string['commentinline'] = '線上評論';
+$string['configitemstocount'] = '學生繳交線上作業的一般計算數量';
+$string['configmaxbytes'] = '本網站對所有作業的預設最大作業檔案上傳限制。';
+$string['deletecheckfile'] = '您確定要刪除這個檔案？';
+$string['deleteednotification'] = '檔案刪除了。';
+$string['deletefail'] = '下面檔案無法刪除：';
+$string['description'] = '說明';
+$string['duedate'] = '繳交日期';
+$string['duedateno'] = '未過期';
+$string['early'] = '$a 之前';
+$string['editmysubmission'] = '編輯我的作業';
+$string['emailstudents'] = '發信通知學生';
+$string['emailteachermail'] = '$a->username己更新他們在\'$a->assignment\'的作業
+連結在此:
+$a->url';
+$string['emailteachermailhtml'] = '$a->username 己更新他們在<i>\'$a->assignment\'</i>的作業，<br /><br />在 <a href=\"$a->url\">這裏</a>。';
+$string['emailteachers'] = '寄信通知教師';
+$string['emptysubmission'] = '您尚未上傳任何檔案';
+$string['existingfiledeleted'] = '目前的檔案 $a 已被刪除。';
+$string['failedupdatefeedback'] = '給使用者 $a 回饋訊息時: 失敗';
+$string['feedback'] = '回饋';
+$string['feedbackfromteacher'] = '$a 的回饋';
+$string['feedbackupdated'] = '替 $a 個人更新繳交後回饋';
+$string['graded'] = '已評分';
+$string['guestnosubmit'] = '抱歉，訪客無法繳交作業；提供答案前您必須先登入或註冊';
+$string['guestnoupload'] = '抱歉，訪客無上傳權限';
+$string['helpoffline'] = '<p>此功能在作業是在moodle外完成時特別有用，可能是在網路上或面對面時。</p><p>學生可以看見作業的說明，但是無法上傳任何東西或檔案，評分功能依舊正常運作，而且學生可以收到他們的分數通知。</p>';
+$string['helponline'] = '<p>這種型式的作業允許每位學生使用一般的文字編輯器編輯文字，教師可以線上評分，或線上評語。</p>
+<p>(如果您比較熟悉舊的Moodle，此作業型式和舊的日誌模組功能雷同。)</p>';
+$string['helpupload'] = '<p>這個類型的作業允許參與者上傳一到多個任意類型的檔案。</p>
+<p>也許是 Word 文件、圖片、一個 zip 格式壓縮檔案或是任何您要求他們上傳的資料。</p>
+<p>這個類型也允許您上傳多個任意類型的回應檔案。</p>';
+$string['helpuploadsingle'] = '<p>這種型式的作業允許每位學生上傳一個任何型式的檔案 。</p> <p>可能是文字檔或圖片，
+壓縮的網頁或您要求他們上傳的各種檔案。</p>';
+$string['late'] = '$a 之後';
+$string['markingsubmitnotification'] = '檔案已經上傳而且標示為完成。';
+$string['maximumgrade'] = '最高得分';
+$string['maximumsize'] = '檔案大小限制';
+$string['modulename'] = '作業';
+$string['modulenameplural'] = '作業';
+$string['namedeletefile'] = '您正要刪除這個檔案：';
+$string['newsubmissions'] = '作業已繳交';
+$string['noassignments'] = '尚未有指定的作業';
+$string['noattempts'] = '尚未有人回答過此試題';
+$string['nofilesforsubmit'] = '沒有發現要上傳的檔案，您確定選擇了嗎？';
+$string['notgradedyet'] = '目前未評分';
+$string['notsubmittedyet'] = '尚未繳交作業';
+$string['onceassignmentsent'] = '當作業標示為完成，您就不再能夠刪除或是附加檔案。';
+$string['overwritewarning'] = '警告: 再次上載作業 將取代前次繳交內容';
+$string['pagesize'] = '每頁顯示的作業數量';
+$string['preventlate'] = '禁止遲交';
+$string['quickgrade'] = '允許快速評分';
+$string['removelink'] = '移除';
+$string['responsefile'] = '回應檔案：';
+$string['responsesfromteacher'] = '教師上傳的回應檔案';
+$string['saveallfeedback'] = '儲存所有回饋';
+$string['sendformarking'] = '傳送後標示';
+$string['step1'] = '步驟一：';
+$string['step2'] = '步驟二：';
+$string['submission'] = '繳交';
+$string['submissionfeedback'] = '繳交後的回饋';
+$string['submissions'] = '繳交';
+$string['submissionsaved'] = '您的變更己儲存';
+$string['submissionsnotgraded'] = '$a 份作業尚未評分';
+$string['submissionstatus'] = '繳交狀態：';
+$string['submissionstatusblank'] = '空白';
+$string['submissionstatusdraft'] = '草稿';
+$string['submissionstatusmarked'] = '標示';
+$string['submissionstatusreturned'] = '退回';
+$string['submissionstatussubmitted'] = '已上傳';
+$string['submitassignment'] = '請利用本表單繳交作業';
+$string['submitformarking'] = '上傳後標示作業';
+$string['submitted'] = '已送出';
+$string['typeoffline'] = '離線作業';
+$string['typeonline'] = '線上編輯';
+$string['typeupload'] = '進階檔案上傳';
+$string['typeuploadsingle'] = '上載一個檔案';
+$string['uploadbadname'] = '上載檔案包含奇怪檔名 上載不成功';
+$string['uploadedfiles'] = '已上載的檔案';
+$string['uploaderror'] = '儲存資料於伺服器時發生錯誤';
+$string['uploadfailnoupdate'] = '檔案上載成功 但是無法更新作業繳交訊息!';
+$string['uploadfiletoobig'] = '抱歉， 檔案過大 不得超過 $a bytes)';
+$string['uploadnofilefound'] = '沒有檔案名稱 - 您確定已經選擇上載檔案檔名了嗎?';
+$string['uploadnotregistered'] = '$a 上載成功，但是尚未登記!';
+$string['uploadsuccess'] = '上載 $a 成功';
+$string['uploadsuccessresponse'] = '回應檔案上傳成功';
+$string['viewfeedback'] = '檢視作業分數和回饋訊息';
+$string['viewsubmissions'] = '檢視 $a 份已繳交作業';
+$string['yoursubmission'] = '個人已繳交的作業';
+
+?>
--- zh_tw_utf8/journal.php
+++ zh_tw_utf8/journal.php
@@ -1,1 +1,33 @@
-<?PHP // $Id$#------------------------------------------------------------$string['modulename'] = "心得報告";$string['modulenameplural'] = "心得報告";#------------------------------------------------------------$string['alwaysopen'] = "永遠開放使用";$string['blankentry'] = "空白內容";$string['daysavailable'] = "可觀看天數";$string['editingends'] = "可編輯期限已過";$string['editingended'] = "可編輯期限已過";$string['journalname'] = "心得報告名稱";$string['journalquestion'] = "歷程紀錄問題";$string['journalrating1'] = "不滿意";$string['journalrating2'] = "滿意";$string['journalrating3'] = "超棒";$string['newjournalentries'] = "新的歷程紀錄";$string['noentry'] = "沒有紀錄";$string['notopenuntil'] = "本心得報告目前無法使用,直到";$string['notstarted'] = "尚未啟動心得報告";$string['noratinggiven'] = "沒有評比紀錄";$string['overallrating'] = "整體評價";$string['rate'] = "評價";$string['startoredit'] = "編輯個人心得報告";$string['viewallentries'] = "檢視 \$a 份心得報告";$string['feedbackupdated'] = "更新 \$a 的資料";$string['saveallfeedback'] = "儲存所有紀錄";$string['strview']="瀏覽";?>
+<?PHP // $Id$ 
+      // journal.php - created with Moodle 1.6 (2006050506)
+
+
+$string['alwaysopen'] = '永遠開放使用';
+$string['blankentry'] = '空白內容';
+$string['daysavailable'] = '可觀看天數';
+$string['editingended'] = '可編輯期限已過';
+$string['editingends'] = '可編輯期限已過';
+$string['entries'] = '項目';
+$string['feedbackupdated'] = '更新 $a 的資料';
+$string['journalmail'] = '$a->teacher 已經在“ $a->journal ”中為您留下了些意見，您可以透過網址瀏覽： $a->url';
+$string['journalmailhtml'] = '$a->teacher 已經在<i>“ $a->journal ”</i>中為您寫了反饋意見。<br/><br/> 您可以在<a href=\"$a->url\">這裏</a>看到反饋意見：';
+$string['journalname'] = '心得報告名稱';
+$string['journalquestion'] = '歷程紀錄問題';
+$string['journalrating1'] = '不滿意';
+$string['journalrating2'] = '滿意';
+$string['journalrating3'] = '超棒';
+$string['mailsubject'] = '心得回饋';
+$string['modulename'] = '心得報告';
+$string['modulenameplural'] = '心得報告';
+$string['newjournalentries'] = '新的歷程紀錄';
+$string['noentry'] = '沒有紀錄';
+$string['noratinggiven'] = '沒有評比紀錄';
+$string['notopenuntil'] = '本心得報告目前無法使用，直到';
+$string['notstarted'] = '尚未啟動心得報告';
+$string['overallrating'] = '整體評價';
+$string['rate'] = '評價';
+$string['saveallfeedback'] = '儲存所有紀錄';
+$string['startoredit'] = '編輯個人心得報告';
+$string['viewallentries'] = '檢視 $a 份心得報告';
+
+?>
