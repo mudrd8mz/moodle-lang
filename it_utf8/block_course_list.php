94f13f1d0538c6df1f9c368c58e56cacbdf5d331
Andrea Bicciolo <andreabix>
2007-02-26 15:21:16 +0000

    Added translation for new strings and updated older translations.

--- it_utf8/block_course_list.php
+++ it_utf8/block_course_list.php
@@ -1,10 +1,11 @@
 <?PHP // $Id$ 
-      // block_course_list.php - created with Moodle 1.5 ALPHA (2005043000)
+      // block_course_list.php - created with Moodle 1.8 Beta + (2007021400)
 
 
-$string['allcourses'] = 'L\'amministratore vede tutti i corsi';
-$string['blockname'] = 'Lista Corsi';
-$string['configadminview'] = 'Cosa dovrebbe visualizzare l\'Amministratore nel blocco \'Lista dei corsi\'?';
-$string['owncourses'] = 'L\'amministratore vede solo i suoi corsi';
+$string['allcourses'] = 'L\'amministratore vede tutti i corsi disponibili';
+$string['blockname'] = 'Elenco Corsi';
+$string['configadminview'] = 'Cosa dovrebbe vedere l\'Amministratore nel blocco \"Elenco corsi\"?';
+$string['confighideallcourseslink'] = 'Non mostrare agli utenti il link \"Tutti i corsi\" in fondo al blocco.';
+$string['owncourses'] = 'L\'amministratore vede solo i corsi nei quali partecipa';
 
 ?>
