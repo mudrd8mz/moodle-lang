bc3c0e01d3af77dd01b23ac184b2889ed0021252
Nicolas Martignoni <martignoni>
2007-02-27 18:25:27 +0000

    Added string for 'Hide All courses' setting.

--- fr_utf8/block_course_list.php
+++ fr_utf8/block_course_list.php
@@ -3,6 +3,7 @@
 $string['blockname'] = 'Liste des cours';
 $string['allcourses'] = 'L\'administrateur voit tous les cours';
 $string['configadminview'] = 'Configuration de l\'affichage de l\'administrateur';
+$string['confighideallcourseslink'] = 'Cacher le lien «&nbsp;Tous les cours&nbsp;» au bas du bloc. Ce réglage ne modifie pas l\'affichage du bloc pour les administrateurs';
 $string['owncourses'] = 'L\'administrateur ne voit que ses propres cours';
 
 ?>
