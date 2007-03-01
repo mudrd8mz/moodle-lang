17ce14024abbc2291cc98c7e0d1d97ffee7656d5
Nicolas Martignoni <martignoni>
2007-03-01 09:28:15 +0000

    mprove validation code to match the recent improvements in shortanswer validation.
    Also do some validation on the units entered to stop duplicate units being defined.

--- fr_utf8/qtype_numerical.php
+++ fr_utf8/qtype_numerical.php
@@ -1,9 +1,11 @@
 <?php // $Id$
 
 $string['addmoreanswerblanks'] = '{no} emplacements supplémentaires vides';
-$string['answerno'] = 'Réponse $a';
-$string['unithdr'] = 'Unité $a';
 $string['addmoreunitblanks'] = '{no} emplacements pour plus d\'unités';
+$string['answerno'] = 'Réponse $a';
+$string['errorrepeatedunit'] = 'Il ne peut y avoir deux unités du même nom.';
+$string['errornomultiplier'] = 'Vous devez indiquer un multiplicateur pour cette unité.';
 $string['notenoughanswers'] = 'Vous devez fournir au moins une réponse.';
+$string['unithdr'] = 'Unité $a';
 
 ?>
