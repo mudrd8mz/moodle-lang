17264cd5b614473e7be9a12257ce338afcb2dd58
Nicolas Martignoni <martignoni>
2007-03-08 21:34:46 +0000

    Added some strings for Pierre Pichet

--- fr_utf8/qtype_calculated.php
+++ fr_utf8/qtype_calculated.php
@@ -6,12 +6,21 @@ $string['answerhdr'] = 'Réponse';
 $string['atleastoneanswer'] = 'Vous devez fournir au moins une réponse.';
 $string['correctanswershows'] = 'Correct answer shows';
 $string['correctanswershowsformat'] = 'Format';
+$string['existingcategory1'] = 'utilisera un jeu de données partagé déjà existant';
+$string['keptcategory1'] = 'utilisera le même jeu de données partagé existant que précédemment';
+$string['keptlocal1'] = 'utilisera le même jeu de données privé existant que précédemment';
 $string['makecopynextpage'] = 'Page suivante (nouvelle question)';
+$string['mandatoryhdr'] = 'Caractère joker obligatoire présent dans les réponses';
 $string['mustbenumeric'] = 'Vous devez saisir un nombre dans ce champ.';
 $string['mustnotbenumeric'] = 'Ceci ne peut pas être un nombre.';
+$string['newcategory1'] = 'utilisera un nouveau jeu de données partagé';
+$string['newlocal1'] = 'utilisera un nouveau jeu de données privé';
 $string['nextitemtoadd'] = '«&nbsp;Élément à ajouter&nbsp;» suivant';
 $string['nextpage'] = 'Page suivante';
+$string['nodataset'] = 'rien - ce n\'est pas un caractère joker';
+$string['possiblehdr'] = 'Possible wild cards present only in the question text';
 $string['tolerance'] = 'Tolérance &plusmn;';
+$string['usedinquestion'] = 'Utilisé dans la question';
 $string['youmustenteramultiplierhere'] = 'Vous devez saisir un multiplicateur dans ce champ.';
 
 ?>
