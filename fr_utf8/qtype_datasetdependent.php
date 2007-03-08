5dd8c5c199ba0f91085b49b57b5cdc6864abf311
Nicolas Martignoni <martignoni>
2007-03-08 21:50:49 +0000

    Added some strings for Pierre Pichet

--- fr_utf8/qtype_datasetdependent.php
+++ fr_utf8/qtype_datasetdependent.php
@@ -1,21 +1,45 @@
 <?php // $Id$
 
 $string['additem'] = 'Ajouter élément';
+$string['atleastonerealdataset'] = 'Un jeu de données réel au moins doit être présent dans le texte de la question';
+$string['atleastonewildcard'] = 'Un caractère joker au moins doit être présent dans la formule de la réponse ou dans le texte de la question';
 $string['calcdistribution'] = 'Distribution';
 $string['calclength'] = 'Décimales';
 $string['calcmax'] = 'Maximum';
 $string['calcmin'] = 'Minimum';
+$string['dataitemdefined'] = 'avec $a valeurs numériques déjà définies est disponible';
+$string['datasetrole'] = 'Les caractères joker <strong>{x..}</strong> seront remplacés par une valeur numérique correspondant à leur jeu de données';
 $string['deletelastitem'] = 'Supprimer dernier élément';
+$string['existingcategory1'] = 'une lettre d\'un ensemble de lettres déjà existant et utilisé dans d\'autres questions de cette catégorie';
+$string['existingcategory2'] = 'une fichier d\'un ensemble de fichiers déjà existant et utilisé dans d\'autres questions de cette catégorie';
+$string['existingcategory3'] = 'une fichier d\'un ensemble de liens et utilisé dans d\'autres questions de cette catégorie';
 $string['forceregeneration'] = 'Imposer une regénération';
 $string['getnextnow'] = 'Obtenir un nouvel «&nbsp;élément à ajouter&nbsp;»';
 $string['itemno'] = 'Élément $a';
+$string['itemscount'] = 'Éléments<br/>Nombre';
 $string['itemtoadd'] = 'Élément à ajouter';
+$string['keptcategory1'] = 'une lettre d\'un ensemble de lettres réutilisable de la même catégorie que précédemment';
+$string['keptcategory2'] = 'un fichier d\'un ensemble de fichiers réutilisable de la même catégorie que précédemment';
+$string['keptcategory3'] = 'un lien d\'un ensemble de liens réutilisable de la même catégorie que précédemment';
+$string['keptlocal1'] = 'une lettre de l\'ensemble de lettres privé de la même question que précédemment';
+$string['keptlocal2'] = 'un fichier de l\'ensemble de fichiers privé de la même question que précédemment';
+$string['keptlocal3'] = 'un lien de l\'ensemble de liens privé de la même question que précédemment';
 $string['loguniform'] = 'Log-uniforme';
 $string['minmax'] = 'Intervalle des valeurs';
+$string['newcategory1'] = 'une lettre d\'un nouvel ensemble de lettres pouvant être aussi utilisé dans d\'autres questions de cette catégorie';
+$string['newcategory2'] = 'un fichier d\'un nouvel ensemble de fichiers pouvant être aussi utilisé dans d\'autres questions de cette catégorie';
+$string['newcategory3'] = 'un lien d\'un nouvel ensemble de liens pouvant être aussi utilisé dans d\'autres questions de cette catégorie';
+$string['newlocal1'] = 'une lettre d\'un nouvel ensemble de lettres qui ne sera utilisé que dans cette question';
+$string['newlocal2'] = 'un fichier d\'un nouvel ensemble de fichiers qui ne sera utilisé que dans cette question';
+$string['newlocal3'] = 'un lien d\'un nouvel ensemble de liens qui ne sera utilisé que dans cette question';
+$string['nodataset'] = 'rien - ce n\'est pas un caractère jocker';
 $string['param'] = 'Paramètre {<strong>$a</strong>}';
 $string['replacewithrandom'] = 'Remplacer par une valeur aléatoire';
 $string['reuseifpossible'] = 'Réutiliser la valeur précédente si disponible';
+$string['sharedwildcard'] = 'Caractère jocker partagé';
+$string['sharedwildcards'] = 'Caractères jockers partagés';
 $string['uniform'] = 'Uniforme';
+$string['updatedatasetparam'] = 'Mettre à jour les paramètre du jeu de données';
 $string['youmustaddatleastoneitem'] = 'Veuillez ajouter au moins un élément du jeu de données avant d\'enregistrer cette question.';
 
 ?>
