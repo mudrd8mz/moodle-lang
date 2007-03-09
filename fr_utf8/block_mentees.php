0388c52cdedd20d825c4f7078f2f19b85dd740af
Nicolas Martignoni <martignoni>
2007-03-09 07:24:12 +0000

    Fix for MDL-8782, adding a mentees block

---
+++ fr_utf8/block_mentees.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+
+$string['blockname'] = 'Participants suivis';
+$string['configtitle'] = 'Titre du bloc';
+$string['leaveblanktohide'] = 'Laisser vide pour cacher le titre';
+$string['newmenteesblock'] = '(Nouveau bloc participants suivis)';
+
+?>
