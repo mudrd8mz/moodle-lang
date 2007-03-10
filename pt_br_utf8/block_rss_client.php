8bca5b7b536d0eaa6f44283f23f3e2b6b2146e2b
Paula de Waal <papita>
2007-03-10 22:08:22 +0000

    new contributions from henning, lima, sentelhas, tamio

--- pt_br_utf8/block_rss_client.php
+++ pt_br_utf8/block_rss_client.php
@@ -1,42 +1,7 @@
 <?PHP // $Id$ 
-      // block_rss_client.php - created with Moodle 1.6.1 (2006050510)
+      // block_rss_client.php - created with Moodle 1.9 dev (2007021401)
+      // local modifications from http://127.0.0.1/moodle19dev
 
 
-$string['addfeed'] = 'Adicionar URL do alimentador de notícias:';
-$string['addheadlineblock'] = 'Adicionar título do box RSS';
-$string['addnew'] = 'Adicionar Novo';
-$string['choosefeedlabel'] = 'Escolher os alimentadores que devem ser disponíveis neste box:';
-$string['clientchannellink'] = 'Site de origem...';
-$string['clientnumentries'] = 'O número predefinido de itens mostrados em cada alimentador.';
-$string['clientshowchannellinklabel'] = 'Mostrar link ao site de origem? (Se o alimentador de notícias nao fornecer o link, ele não estará disponível) :';
-$string['clientshowimagelabel'] = 'Mostrar imagem do canal, se disponível :';
-$string['configblock'] = 'Configurar este box';
-$string['couldnotfindfeed'] = 'Nenhum alimentador encontrado com este id';
-$string['customtitlelabel'] = 'Título padrão (deixar em branco para usar o título fornecido pelo alimentador):';
-$string['deletefeedconfirm'] = 'Tem certeza que deseja cancelar este alimentador?';
-$string['displaydescriptionlabel'] = 'Mostrar todas as descrições dos links?';
-$string['editfeeds'] = 'Editar, fazer ou cancelar assinatura de alimentadores de notícias RSS/Atom';
-$string['editnewsfeeds'] = 'Editar alimentadores de notícias';
-$string['editrssblock'] = 'Editar título do box RSS';
-$string['feed'] = 'Alimentador';
-$string['feedadded'] = 'Alimentador de notícias adicionado';
-$string['feeddeleted'] = 'Alimentador de notícias cancelado';
-$string['feeds'] = 'Alimentadores de Notícias';
-$string['feedsaddedit'] = 'Adicionar/Editar Alimentadores';
-$string['feedsconfigurenewinstance'] = 'Clique aqui para configurar este box e mostrar alimentadores RSS.';
-$string['feedstitle'] = 'Alimentador RSS remoto';
-$string['feedupdated'] = 'Alimentador de notícias atualizado';
-$string['findmorefeeds'] = 'Procurar outros alimentadores RSS';
-$string['managefeeds'] = 'Administrar todos os meus alimentadores';
-$string['nofeeds'] = 'Nenhum alimentador RSS definido neste site.';
-$string['pickfeed'] = 'Selecionar um alimentador de notícias';
-$string['remotenewsfeed'] = 'Alimentador de notícias remoto';
-$string['seeallfeeds'] = 'Mostrar todos os alimentadores';
-$string['shownumentrieslabel'] = 'Número máximo de itens mostrados em cada box.';
-$string['submitters'] = 'Quem pode definir novos alimentadores RSS? Os alimentadores definidos podem ser disponibilizados em todas as páginas do site.';
-$string['timeout'] = 'blockrsstimeout';
-$string['timeoutdesc'] = 'Duração em minutos do alimentador rss na cache.';
-$string['updatefeed'] = 'Atualizar URL do alimentador de notícias :';
-$string['validatefeed'] = 'Validar alimentador';
 
 ?>
