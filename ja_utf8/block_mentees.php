14aba1f2d995a7e6dc7e764a08b880c4a86484da
Mitsuhiro Yoshida <mits>
2007-03-10 05:13:13 +0000

    Adding a new translated string file for mentees block MDL-8782.

---
+++ ja_utf8/block_mentees.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // block_mentees.php - created with Moodle 1.9 dev (2007021401)
+
+
+$string['blockname'] = 'Mentees';
+$string['configtitle'] = 'ブロックタイトル';
+$string['leaveblanktohide'] = '空白でタイトルを非表示';
+$string['newmenteesblock'] = '(新しいMenteesブロック)';
+
+?>
