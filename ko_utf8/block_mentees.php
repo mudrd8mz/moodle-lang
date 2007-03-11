9c323647fa046495135419e16d363f3e955b4dd1
Lee Chan-young <yogibear270>
2007-03-11 08:58:19 +0000

    translate new file

---
+++ ko_utf8/block_mentees.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // block_mentees.php - created with Moodle 1.9 dev (2007021401)
+
+
+$string['blockname'] = '피상담자';
+$string['configtitle'] = '블록 제목';
+$string['leaveblanktohide'] = '제목을 감추려면 공백으로 놓아둠';
+$string['newmenteesblock'] = '(새 피상담자 블록)';
+
+?>
