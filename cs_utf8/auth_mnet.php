3890d3bd2287916b291780c93ea06e32e1c29038
David Mudrak <david.mudrak@gmail.com>
2007-03-11 23:19:48 +0000

    Added strings sent by Jiri Zajic

---
+++ cs_utf8/auth_mnet.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.8 Beta (2007020400)
+
+
+$string['sso_idp_description'] = 'Publikujte tuto službu, abyste umožnili vašim uživatelům roaming na stránkách $a Moodle, aniž by se se zde odhlašovali a znovu přihlašovali. <ul><li><em>Závislost</em>: Musíte být <strong>registrováni</strong> také k poskytovateli služby SSO na $a.</li></ul><br />';
+$string['sso_idp_name'] = 'Poskytovatel identity SSO';
+$string['sso_mnet_login_refused'] = 'Uživatel $a[0] nemá povoleno přihlašovat se z $a[1].';
+$string['sso_sp_description'] = 'Publikujte tuto službu, abyste umožnili vašim přihlášeným uživatelům přistupovat na vaše stránky, aniž by se se zde odhlašovali a znovu přihlašovali. <ul><li><em>Závislost</em>: Musíte být <strong>registrováni</strong> také k poskytovateli služby SSO na $a.</li></ul><br />';
+$string['sso_sp_name'] = 'Poskytovatel služby SSO';
+
+?>
