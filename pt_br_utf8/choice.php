b976a8b7cbaf9ef58c29ae1174cb520b73a37241
Paula de Waal <papita>
2007-03-11 18:27:47 +0000

    correcting empty files

--- pt_br_utf8/choice.php
+++ pt_br_utf8/choice.php
@@ -1,7 +1,51 @@
 <?PHP // $Id$ 
       // choice.php - created with Moodle 1.9 dev (2007021401)
-      // local modifications from http://127.0.0.1/moodle19dev
 
 
+$string['addmorechoices'] = 'Adicionar outras opções';
+$string['allowupdate'] = 'Permitir a atualização da escolha feita';
+$string['answered'] = 'Respondido';
+$string['choice'] = 'Escolha $a';
+$string['choice:choose'] = 'Registrar escolha';
+$string['choice:deleteresponses'] = 'Excluir respostas';
+$string['choice:downloadresponses'] = 'Baixar respostas';
+$string['choice:readresponses'] = 'Ler respostas';
+$string['choiceclose'] = 'Até';
+$string['choicename'] = 'Nome da Escolha';
+$string['choiceopen'] = 'Abrir';
+$string['choicetext'] = 'Texto da Escolha';
+$string['displayhorizontal'] = 'Disposição Horizontal';
+$string['displaymode'] = 'Formato de visualização';
+$string['displayvertical'] = 'Disposição Vertical';
+$string['fillinatleastoneoption'] = 'Você deve dar pelo menos duas respostas possíveis';
+$string['full'] = '(Completo)';
+$string['havetologin'] = 'Você deve fazer o acesso ao site antes de enviar a sua resposta';
+$string['limit'] = 'Limitar';
+$string['limitanswers'] = 'Limitar o número de respostas permitidas';
+$string['modulename'] = 'Escolha';
+$string['modulenameplural'] = 'Escolhas';
+$string['mustchooseone'] = 'Você deve escolher uma resposta antes de salvar.';
+$string['noguestchoose'] = 'Sinto muito, visitantes não podem fazer escolhas.';
+$string['noresultsviewable'] = 'Não é possível visualizar os resultados neste momento.';
+$string['notanswered'] = 'Nenhuma resposta';
+$string['notopenyet'] = 'Esta atividade será acessível a partir de $a';
+$string['privacy'] = 'Privacidade dos resultados';
+$string['publish'] = 'Publicar resultados';
+$string['publishafteranswer'] = 'Mostrar os resultados ao estudante só depois que ele tiver dado a sua resposta';
+$string['publishafterclose'] = 'Mostrar resultados aos estudantes apenas após o fechamento do período de escolha';
+$string['publishalways'] = 'Mostrar sempre os resultados aos estudantes';
+$string['publishanonymous'] = 'Publique resultados de forma anônima, sem mostrar o nome do aluno';
+$string['publishnames'] = 'Publique resultados completos, mostrando os nomes dos alunos e os resultados';
+$string['publishnot'] = 'Não mostrar os resultados aos alunos';
+$string['responses'] = 'Respostas';
+$string['responsesto'] = 'Respostas a $a';
+$string['savemychoice'] = 'Gravar a minha resposta';
+$string['showunanswered'] = 'Mostrar coluna \"sem respostas\"';
+$string['spaceleft'] = 'espaço disponível';
+$string['spacesleft'] = 'espaços disponíveis';
+$string['taken'] = 'Escolhido';
+$string['timerestrict'] = 'Aceitar respostas apenas neste período';
+$string['viewallresponses'] = 'Ver $a respostas';
+$string['yourselection'] = 'A sua escolha';
 
 ?>
