e3128573483d3e5a983750a6ee82afe81e2cddb6
Paula de Waal <papita>
2007-03-11 19:17:31 +0000

    new files

---
+++ pt_br_utf8/qtype_truefalse.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // qtype_truefalse.php - created with Moodle 1.9 dev (2007021401)
+
+
+$string['correctanswer'] = 'Resposta certa';
+$string['editingtruefalse'] = 'Editar questão verdadeiro/falso';
+$string['false'] = 'Falso';
+$string['feedbackfalse'] = 'Feedback (falso)';
+$string['feedbacktrue'] = 'Feedback (verdadeiro)';
+$string['true'] = 'Verdadeiro';
+$string['truefalse'] = 'Verdadeiro/Falso';
+
+?>
