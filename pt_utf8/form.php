32eb2b250ee6d54a660a51afe4f2d10ef800497c
Jaime Villate <villate>
2007-03-12 09:09:49 +0000

    First translation of new file form.php

---
+++ pt_utf8/form.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 1.7.1+ (2006101010)
+
+
+$string['denotesreq'] = '$a indica um campo obrigatório.';
+$string['nomethodforaddinghelpbutton'] = 'Não existe nenhum método para adicionar um botão de ajuda ao elemento de formulário $a->name (classe $a->classname)';
+$string['nonexistentformelements'] = 'Tentativa de adicionar botões de ajuda a elementos de formulário não existentes: $a';
+$string['requiredelement'] = 'Campo obrigatório.';
+
+?>
