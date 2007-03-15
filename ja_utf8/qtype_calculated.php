949c93fbe397c16e9f27e034d68455f5ed32a122
Mitsuhiro Yoshida <mits>
2007-03-15 19:47:16 +0000

    Translated new strings for calculated question type. Translated new strings for database dependent question type.

--- ja_utf8/qtype_calculated.php
+++ ja_utf8/qtype_calculated.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // qtype_calculated.php - created with Moodle 1.9 dev (2007021401)
+      // qtype_calculated.php - created with Moodle 1.9 dev (2007021500)
 
 
 $string['addmoreanswerblanks'] = 'さらに答え入力欄を追加する';
@@ -12,14 +12,18 @@ $string['existingcategory1'] = 'は既存の共有データセットを使用し
 $string['keptcategory1'] = 'は前と同じ既存の共有データセットを使用します。';
 $string['keptlocal1'] = 'は前と同じ既存のプライベートデータセットを使用します。';
 $string['makecopynextpage'] = '次のページ (新しい問題)';
+$string['mandatoryhdr'] = '答えの中の必須ワイルドカード';
 $string['mustbenumeric'] = 'ここには数字を入力してください。';
 $string['mustnotbenumeric'] = 'ここには数字を入力できません。';
 $string['newcategory1'] = 'は新しい共有データセットを使用します。';
 $string['newlocal1'] = 'は新しいプライベートデータセットを使用します。';
 $string['nextitemtoadd'] = '次のアイテムの追加方法';
 $string['nextpage'] = '次のページ';
+$string['nodataset'] = 'なし - これはワイルドカードではありません';
 $string['nosharedwildcard'] = 'このカテゴリに共有ワイルドカードはありません。';
+$string['possiblehdr'] = '問題テキストの中のみのワイルドカード';
 $string['tolerance'] = '許容誤差 &plusmn;';
+$string['usedinquestion'] = '問題で使用';
 $string['youmustenteramultiplierhere'] = 'ここには乗数を入力してください。';
 
 ?>
--- ja_utf8/qtype_datasetdependent.php
+++ ja_utf8/qtype_datasetdependent.php
@@ -1,20 +1,39 @@
 <?PHP // $Id$ 
-      // qtype_datasetdependent.php - created with Moodle 1.9 dev (2007021401)
+      // qtype_datasetdependent.php - created with Moodle 1.9 dev (2007021500)
 
 
 $string['additem'] = 'アイテムを追加する';
+$string['atleastonerealdataset'] = '問題テキストの中には、少なくとも1つの実データセットを入れる必要があります。';
+$string['atleastonewildcard'] = '答えの公式または問題テキストの中には、少なくとも1つのワイルドカードを入れる必要があります。';
 $string['calcdistribution'] = '分布';
 $string['calclength'] = '有効数字';
 $string['calcmax'] = '最大';
 $string['calcmin'] = '最小';
+$string['datasetrole'] = 'ワイルドカード「<strong>{x..}</strong>」は、データセットの数値と置換されます';
 $string['deletelastitem'] = '最後のアイテムを削除する';
+$string['existingcategory1'] = '文字 - このカテゴリ内の他の問題ですでに使用されている既存の文字セットより';
+$string['existingcategory2'] = 'ファイル - このカテゴリ内の他の問題ですでに使用されている既存のファイルセットより';
+$string['existingcategory3'] = 'リンク - このカテゴリ内の他の問題ですでに使用されている既存のリンクセットより';
 $string['forceregeneration'] = 'データを再生成する';
 $string['getnextnow'] = '追加するアイテムを今から取得する';
 $string['itemno'] = 'アイテム $a';
 $string['itemscount'] = 'アイテム<br/>カウント';
 $string['itemtoadd'] = '追加するアイテム';
+$string['keptcategory1'] = '文字 - 同一カテゴリ内の再利用可能文字セットより';
+$string['keptcategory2'] = 'ファイル - 同一カテゴリ内の再利用可能ファイルセットより';
+$string['keptcategory3'] = 'リンク - 同一カテゴリ内の再利用可能リンクセットより';
+$string['keptlocal1'] = '文字 - 同一問題内のプライベート文字セットより';
+$string['keptlocal2'] = 'ファイル - 同一問題内のプライベートファイルセットより';
+$string['keptlocal3'] = 'リンク - 同一問題内のプライベートリンクセットより';
 $string['loguniform'] = '対数一様分布';
 $string['minmax'] = '値域';
+$string['newcategory1'] = '文字 - このカテゴリ内の他の問題でも使用される新しい文字セットより';
+$string['newcategory2'] = 'ファイル - このカテゴリ内の他の問題でも使用される新しいファイルセットより';
+$string['newcategory3'] = 'リンク - このカテゴリ内の他の問題でも使用される新しいリンクセットより';
+$string['newlocal1'] = '文字 - この問題のみで使用される新しい文字セットより';
+$string['newlocal2'] = 'ファイル - この問題のみで使用される新しいファイルセットより';
+$string['newlocal3'] = 'リンク - この問題のみで使用される新しいリンクセットより';
+$string['nodataset'] = 'なし - これはワイルドカードではありません';
 $string['param'] = '変数 {<strong>$a</strong>}';
 $string['replacewithrandom'] = 'ランダム値と置換する';
 $string['reuseifpossible'] = '可能な場合、前の値を再利用する';
