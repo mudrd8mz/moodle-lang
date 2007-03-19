38ed8bd71a49bb7d2ecd150bb824c2108011f080
Benito Arias <barias>
2007-03-19 11:54:21 +0000

    *** empty log message ***

---
+++ es_utf8/auth_mnet.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.7 (2006101007)
+
+
+$string['sso_idp_name'] = 'SSO (Proveedor de Identidad)';
+$string['sso_sp_name'] = 'SSO (Proveedor de Servicios)';
+
+?>
---
+++ es_utf8/block_mnet_hosts.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_mnet_hosts.php - created with Moodle 1.7 (2006101007)
+
+
+$string['mnet_hosts'] = 'Servidores de Red';
+$string['server'] = 'Servidor';
+
+?>
