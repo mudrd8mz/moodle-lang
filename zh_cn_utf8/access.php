91428f1969cdddfe0728d2ad983133a630ac3a85
sunner_sun <sunner_sun>
2007-03-20 15:05:53 +0000

    Merge some modification from Shanghai Normal University

--- zh_cn_utf8/access.php
+++ zh_cn_utf8/access.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 1.7+ (2006101007)
+      // access.php - created with Moodle 1.7.1+ (2006101011)
 
 
 $string['access'] = '进入';
@@ -8,7 +8,7 @@ $string['accesskey'] = '进入验证,{$a}';
 $string['accessstatement'] = '进入声明';
 $string['activitynext'] = '下一个活动';
 $string['activityprev'] = '前一个活动';
-$string['breadcrumb'] = '[面包屑踪迹]当前位置导航';
+$string['breadcrumb'] = '网页导航';
 $string['monthnext'] = '下月';
 $string['monthprev'] = '上月';
 $string['showhideblock'] = '显示或隐藏版块';
