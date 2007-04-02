278102239a7ff257eb5fb75e90cb6389d1d9be50
Benito Arias <barias>
2007-04-02 19:22:51 +0000

    *** empty log message ***

---
+++ es_utf8/attforblock.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // attforblock.php - created with Moodle 1.7 (2006101007)
+
+
+$string['description'] = 'Puede agregar sólo un módulo Asistencia por curso.<br />Si elimina este módulo, ello no afectará a las asistencias. Puede usar este módulo sólo con el bloque Asistencia. Este módulo sólo necesita para incluir calificaciones de las asistencias en las calificaciones de los cursos principales.';
+$string['modulename'] = 'Asistencia (para Bloque)';
+$string['modulenameplural'] = 'Asistencias (para Bloque)';
+$string['modulenamesimple'] = 'Asistencia';
+
+?>
