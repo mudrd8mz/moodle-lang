4f80c655591f0ff8a6a52d05bfd89f015b95bcf9
Benito Arias <barias>
2007-04-05 08:11:35 +0000

    *** empty log message ***

---
+++ es_utf8/block_mentees.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // block_mentees.php - created with Moodle 1.8 + (2007021502)
+
+
+$string['blockname'] = 'Mentees';
+$string['configtitle'] = 'Título del Bloque';
+$string['leaveblanktohide'] = 'dejar en blanco para ocultar el título';
+$string['newmenteesblock'] = '(nuevo Bloque Mentees)';
+
+?>
