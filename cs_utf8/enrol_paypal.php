226136f9acaa3ba81a3b57ae96ada6bb96f6f158
David Mudrak <david.mudrak@gmail.com>
2007-04-07 13:13:12 +0000

    Trying to commit enrol_paypal.php with Gruntspud CVS client (should support UTF-8)

--- cs_utf8/enrol_paypal.php
+++ cs_utf8/enrol_paypal.php
@@ -1,11 +1,12 @@
 <?PHP // $Id$ 
-      // enrol_paypal.php - created with Moodle 1.6 Beta 5 (2006050502)
+      // enrol_paypal.php - created with Moodle 1.8 + (2007021502)
 
 
-$string['business'] = 'E-mailová adresa vašeho obchodního účtu Paypal';
+$string['business'] = 'E-mailová adresa vašeho obchodního účtu PayPal';
 $string['costorkey'] = 'Vyberte prosím jeden z následujících způsobů zápisu do kurzů.';
-$string['description'] = 'Modul \'Platba přes Paypal\' vám umožňuje nastavovat placené kurzy. Je-li cena kurzu nastavena na nulu, nejsou zájemci o zapsání do kurzu vyzýváni k platbě. Můžete rovněž nastavit výchozí cenu všech kurzů na vašich stránkách a ceny jednotlivých kurzů pak nastavovat individuálně. Cena nastavená v kurzu má přednost před cenou nastavenou pro celé stránky.';
-$string['enrolname'] = 'Platba přes Paypal';
-$string['sendpaymentbutton'] = 'Poslat platbu přes službu Paypal';
+$string['description'] = 'Modul \'Platba přes PayPal\' vám umožňuje nastavovat placené kurzy. Je-li cena kurzu nastavena na nulu, nejsou zájemci o zapsání do kurzu vyzýváni k platbě. Můžete rovněž nastavit výchozí cenu všech kurzů na vašich stránkách a ceny jednotlivých kurzů pak nastavovat individuálně. Cena nastavená v kurzu má přednost před cenou nastavenou pro celé stránky.';
+$string['enrolname'] = 'Platba přes PayPal';
+$string['paypalaccepted'] = 'Platby přes PayPal přijímány';
+$string['sendpaymentbutton'] = 'Poslat platbu přes službu PayPal';
 
 ?>
