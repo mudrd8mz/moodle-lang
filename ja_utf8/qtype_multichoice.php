2b6d954c8ad4a248695efc7f0de002753a5a909f
Mitsuhiro Yoshida <mits>
2007-04-14 01:23:22 +0000

    Translated new strings for quiz MDL-9342. Updated a translated help file for quiz based on /lang/en_utf8 change MDL-9342.

--- ja_utf8/qtype_multichoice.php
+++ ja_utf8/qtype_multichoice.php
@@ -1,9 +1,14 @@
 <?PHP // $Id$ 
-      // qtype_multichoice.php - created with Moodle 1.8 dev (2007010404)
+      // qtype_multichoice.php - created with Moodle 1.9 dev (2007041100)
 
 
 $string['addmorechoiceblanks'] = 'さらに {no} 件の選択肢入力欄を追加する';
 $string['answerhowmany'] = '単一または複数解答?';
+$string['answernumbering'] = '選択肢に番号付けしますか?';
+$string['answernumbering123'] = '1.  2.  3. ...';
+$string['answernumberingABC'] = 'A.  B.  C. ...';
+$string['answernumberingabc'] = 'a.  b.  c., ...';
+$string['answernumberingnone'] = '番号付けなし';
 $string['answersingleno'] = '複数解答を許可する';
 $string['answersingleyes'] = '単一の解答のみ許可する';
 $string['choiceno'] = '選択肢 $a';
@@ -12,19 +17,19 @@ $string['clozeaid'] = 'テキストを入力してください。';
 $string['correctfeedback'] = '正解すべてに対するフィードバック';
 $string['editingmultichoice'] = '多肢選択問題の編集';
 $string['errfractionsaddwrong'] = 'あなたが選択したプラス評点の合計が、100%%に達していません。<br />現在の評点合計は $a%% です。';
-$string['errfractionsnomax'] = 'この問題で満点を取ることができるよう、<br />答えの1つは、100%%でなければなりません。';
+$string['errfractionsnomax'] = 'この問題で満点を取ることができるよう、<br />選択肢の1つは、100%%でなければなりません。';
 $string['feedback'] = 'フィードバック';
 $string['fillouttwochoices'] = '2つ以上の選択肢を入力してください。空白の選択肢は使用されません。';
 $string['fractionsaddwrong'] = 'あなたが選択したプラス評点の合計が、100%%に達していません。<br />現在の評点合計は $a%% です。<br />戻ってこの問題を修正しますか?';
-$string['fractionsnomax'] = 'この問題で満点を取ることができるよう、<br />答えの1つは、100%%でなければなりません。 <br />戻ってこの問題を修正しますか?';
+$string['fractionsnomax'] = 'この問題で満点を取ることができるよう、<br />答えの1つは、100%%でなければなりません。 <br />前のページに戻ってこの問題を修正しますか?';
 $string['incorrectfeedback'] = '不正解すべてに対するフィードバック';
-$string['notenoughanswers'] = 'このタイプの問題には少なくとも $a の解答が必要です。';
+$string['notenoughanswers'] = 'このタイプの問題には少なくとも $a の選択肢が必要です。';
 $string['overallcorrectfeedback'] = '正解すべてに対するフィードバック';
 $string['overallfeedback'] = '全体のフィードバック';
 $string['overallincorrectfeedback'] = '不正解すべてに対するフィードバック';
 $string['overallpartiallycorrectfeedback'] = '部分的な正解すべてに対するフィードバック';
 $string['partiallycorrectfeedback'] = '部分的な正解すべてに対するフィードバック';
-$string['shuffleanswers'] = '答えをシャッフルする';
+$string['shuffleanswers'] = '選択肢をシャッフルしますか?';
 $string['singleanswer'] = '1つの答えを選択してください。';
 
 ?>
