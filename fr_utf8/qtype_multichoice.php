dd1ee15afba7f2d42e6e30ad282d7e1e30ec5e3d
Nicolas Martignoni <martignoni>
2007-04-14 07:58:30 +0000

    MDL-9342 - Let the question author choose how the options are numbered in Multiple choices questions.

--- fr_utf8/qtype_multichoice.php
+++ fr_utf8/qtype_multichoice.php
@@ -2,6 +2,11 @@
 
 $string['addmorechoiceblanks'] = 'Emplacement pour {no} réponses supplémentaires';
 $string['answerhowmany'] = 'Une seule ou plusieurs réponses&nbsp;?';
+$string['answernumbering'] = 'Numéroter les choix&nbsp;?';
+$string['answernumbering123'] = '1., 2., 3., ...';
+$string['answernumberingabc'] = 'a., b., c., ...';
+$string['answernumberingABC'] = 'A., B., C., ...';
+$string['answernumberingnone'] = 'Sans numérotation';
 $string['answersingleno'] = 'Réponses multiples autorisées';
 $string['answersingleyes'] = 'Une seule réponse';
 $string['choiceno'] = 'Réponse $a';
@@ -10,19 +15,19 @@ $string['clozeaid'] = 'Tapez le mot manquant';
 $string['correctfeedback'] = 'Pour toute réponse correcte';
 $string['editingmultichoice'] = 'Modification d\'une question à choix multiples';
 $string['errfractionsaddwrong'] = 'La somme des notes positives que vous avez indiquées positive ne donne pas 100%%<br />Leur somme est de $a%%';
-$string['errfractionsnomax'] = 'L\'une des réponses devrait être à 100%%, afin qu\'il soit<br />possible d\'obtenir une note maximale pour cette question.';
+$string['errfractionsnomax'] = 'L\'un des choix proposés devrait être à 100%%, afin qu\'il soit<br />possible d\'obtenir une note maximale pour cette question.';
+$string['feedback'] = 'Feedback';
 $string['fillouttwochoices'] = 'Vous devez donner au moins deux réponses. Les réponses laissés vides ne seront pas utilisées.';
 $string['fractionsaddwrong'] = 'La somme des notes positives que vous avez choisies n\'est pas égale à 100%%.<br />Leur somme est de $a%%.<br />Voulez-vous revenir en arrière pour corriger cette question&nbsp;?';
 $string['fractionsnomax'] = 'Une des réponses doit être à 100%%, de sorte qu\'il soit<br />possible d\'obtenir la note maximale pour cette question.<br />Voulez-vous revenir en arrière pour corriger cette question&nbsp;?';
-$string['feedback'] = 'Feedback';
 $string['incorrectfeedback'] = 'Pour toute réponse incorrecte';
 $string['notenoughanswers'] = 'Ce type de question requiert au moins $a réponses';
 $string['overallcorrectfeedback'] = 'Feedback pour les réponses correctes';
 $string['overallfeedback'] = 'Feedback général';
-$string['overallpartiallycorrectfeedback'] = 'Feedback pour les réponses partiellement correctes';
 $string['overallincorrectfeedback'] = 'Feedback pour les réponses fausses';
+$string['overallpartiallycorrectfeedback'] = 'Feedback pour les réponses partiellement correctes';
 $string['partiallycorrectfeedback'] = 'Pour toute réponse partiellement correcte';
-$string['shuffleanswers'] = 'Mélanger les questions';
+$string['shuffleanswers'] = 'Mélanger les réponses possibles&nbsp;?';
 $string['singleanswer'] = 'Choisir une réponse.';
 
 ?>
