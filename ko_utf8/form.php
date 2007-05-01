628c21725d3df429e6cd0b1b9cd0260fc461bfa2
Lee Chan-young <yogibear270>
2007-05-01 10:36:25 +0000

    translate new strings and some corrections

--- ko_utf8/form.php
+++ ko_utf8/form.php
@@ -1,21 +1,21 @@
 <?PHP // $Id$ 
-      // form.php - created with Moodle 1.9 dev (2007040400)
+      // form.php - created with Moodle 1.9 dev (2007043001)
 
 
 $string['addfields'] = '양식에 $a 필드 추가';
 $string['advancedelement'] = '상세 항목';
 $string['day'] = '일';
 $string['display'] = '표시';
-$string['err_alphanumeric'] = '알파벳이나 숫자만 입력하라.';
-$string['err_email'] = '쓸 수 있는 이메일 주소를 입력하.';
-$string['err_lettersonly'] = '문자만 입력하라.';
-$string['err_maxlength'] = '$a->format 글자 미만의 문자를 입력하라.';
-$string['err_minlength'] = '최소 $a->format 글자 이상의 문자를 입력하라.';
-$string['err_nonzero'] = '0이 아닌 숫자만 입력하라.';
-$string['err_nopunctuation'] = '쉼표나 세미콜론 등이 없는 문자만 입력하라.';
-$string['err_numeric'] = '숫자만 입력하라.';
-$string['err_rangelength'] = '{$a->format[0]} 에서 {$a->format[1]} 글자의 문자만 입력하라.';
-$string['err_required'] = '적당한 값을 입력하라.';
+$string['err_alphanumeric'] = '알파벳이나 숫자만 입력하시오.';
+$string['err_email'] = '쓸 수 있는 이메일 주소를 입력하시오.';
+$string['err_lettersonly'] = '문자만 입력하시오.';
+$string['err_maxlength'] = '$a->format 글자 미만의 문자를 입력하시오.';
+$string['err_minlength'] = '최소 $a->format 글자 이상의 문자를 입력하시오.';
+$string['err_nonzero'] = '0이 아닌 숫자만 입력하시오.';
+$string['err_nopunctuation'] = '쉼표나 세미콜론 등이 없는 문자만 입력하시오.';
+$string['err_numeric'] = '숫자만 입력하시오.';
+$string['err_rangelength'] = '{$a->format[0]} 에서 {$a->format[1]} 글자의 문자만 입력하시오.';
+$string['err_required'] = '적당한 값을 입력하시오.';
 $string['general'] = '일반 설정';
 $string['hideadvanced'] = '자세한 내용 숨김';
 $string['hour'] = '시';
@@ -25,14 +25,15 @@ $string['modstandardels'] = '일반 모듈 설정';
 $string['month'] = '월';
 $string['nomethodforaddinghelpbutton'] = '양식 요소 $a->name (학급 $a->classname)에 도움말 단추를 추가할 방법이 없습니다.';
 $string['nonexistentformelements'] = '존재하지 않는 양식 요소 $a 에 도움말 단추를 추가하려 함';
-$string['optional'] = '선택적';
+$string['optional'] = '선택사항';
 $string['requiredelement'] = '필수 항목';
-$string['revealpassword'] = '암호노출';
 $string['security'] = '보안';
 $string['showadvanced'] = '자세한 설명 보임';
 $string['somefieldsrequired'] = '이 양식에는 필수 항목이 있습니다';
 $string['timing'] = '타이밍';
+$string['unmaskpassword'] = '암호보임';
 $string['year'] = '년';
+$string['revealpassword'] = '암호노출'; // ORPHANED
 $string['requiredfield'] = '이것은 필수 항목입니다'; // ORPHANED
 
 ?>
