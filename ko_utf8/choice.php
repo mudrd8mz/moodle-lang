3d94a9cc79115d3c76026dd8b32b5cc96e1c284d
Lee Chan-young <yogibear270>
2007-05-03 05:42:01 +0000

    translate new strings and some corrections

--- ko_utf8/choice.php
+++ ko_utf8/choice.php
@@ -1,12 +1,12 @@
 <?PHP // $Id$ 
-      // choice.php - created with Moodle 1.8 dev (2006112200)
+      // choice.php - created with Moodle 1.9 dev (2007050300)
 
 
 $string['addmorechoices'] = '선택사항을 더 추가하시오';
-$string['allowupdate'] = '선택한 것을 수정할 수 있도록 허용합니다.';
+$string['allowupdate'] = '선택한 것을 수정할 수 있도록 허용';
 $string['answered'] = '응답함';
 $string['choice'] = '간편 설문';
-$string['choice:choose'] = '간편 설문 기록';
+$string['choice:choose'] = '설문 선택';
 $string['choice:deleteresponses'] = '설문 응답 삭제';
 $string['choice:downloadresponses'] = '응답내용 내려받기';
 $string['choice:readresponses'] = '응답내용 보기';
@@ -14,9 +14,9 @@ $string['choiceclose'] = '까지';
 $string['choicename'] = '조사명';
 $string['choiceopen'] = '열기';
 $string['choicetext'] = '설문 내용';
-$string['displayhorizontal'] = '수평으로 배치하시오';
+$string['displayhorizontal'] = '수평으로 배치';
 $string['displaymode'] = '화면 배치';
-$string['displayvertical'] = '수직으로 배치하시오';
+$string['displayvertical'] = '수직으로 배치';
 $string['fillinatleastoneoption'] = '최소 두 개 이상의  답안을 제시할 필요가 있습니다.';
 $string['full'] = '가득참';
 $string['havetologin'] = '설문 응답을 제출하기 전에 로그인해야 합니다.';
@@ -30,21 +30,22 @@ $string['noresultsviewable'] = '지금은 결과를 볼 수 없습니다.';
 $string['notanswered'] = '아직 답을 제출하지 않았습니다';
 $string['notopenyet'] = '죄송합니다만, 이 활동은 $a까지는 이용할 수 없습니다.';
 $string['privacy'] = '결과의 정보보호';
-$string['publish'] = '결과를 공개합니다';
-$string['publishafteranswer'] = '응답한 후 학생들에게 결과를 공개합니다.';
-$string['publishafterclose'] = '설문이 종료된 후에 학생들에게 결과를 공개합니다.';
-$string['publishalways'] = '언제나 학생들에게 결과를 공개합니다.';
-$string['publishanonymous'] = '학생이름을 제시하지 말고 익명으로 결과를 공개합니다.';
-$string['publishnames'] = '이름과 학생의 선택을 공개하고 완전한 결과를 공개합니다.';
-$string['publishnot'] = '학생들에게 결과를 공개하지 않습니다.';
+$string['publish'] = '결과 공개';
+$string['publishafteranswer'] = '응답한 후 학생들에게 결과 공개';
+$string['publishafterclose'] = '설문이 종료된 후에 학생들에게 결과 공개';
+$string['publishalways'] = '언제나 학생들에게 결과 공개';
+$string['publishanonymous'] = '이름을 명시하지 않고 익명으로 결과 공개';
+$string['publishnames'] = '이름과 학생의 선택 등, 완전한 결과를 공개';
+$string['publishnot'] = '학생들에게 결과를 공개하지 않음';
+$string['removemychoice'] = '선택사항 제거';
 $string['responses'] = '응답';
-$string['responsesto'] = '$a 에 응답하다';
-$string['savemychoice'] = '나의 선택을 컴퓨터에 저장합니다';
+$string['responsesto'] = '$a 에 대한 응답';
+$string['savemychoice'] = '선택사항 저장';
 $string['showunanswered'] = '응답 없는 항목을 보임';
 $string['spaceleft'] = '이용가능한 공간';
 $string['spacesleft'] = '이용가능한 공간들';
 $string['taken'] = '선택';
-$string['timerestrict'] = '이 기간중에 답하도록 제한합니다.';
+$string['timerestrict'] = '이 기간중에 답하도록 제한';
 $string['viewallresponses'] = '$a 에대한 학생들의 응답 보고 피드백 쓰기';
 $string['yourselection'] = '당신의 선택';
 
