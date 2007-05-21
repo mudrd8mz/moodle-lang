17687936adb66ff4806f6a7395078d1e833936db
johnharald <johnharald>
2007-05-21 22:36:35 +0000

    New Translation

---
+++ no_utf8/podcast.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // podcast.php - created with Moodle 1.9 dev (2007021400)
+      // local modifications from http://localhost
+
+
+$string['Title'] = 'Tittel';
+$string['add_ok'] = 'Artikkel lagt til';
+$string['browse'] = 'Se gjennom';
+$string['category'] = 'Kategori';
+$string['delete'] = 'Slett artikkel';
+$string['delete_ok'] = 'Slettet!';
+$string['desc'] = 'Beskrivelse';
+$string['directory'] = 'Katalog';
+$string['edit'] = 'Rediger';
+$string['extensions'] = 'Nøkkelord (separert av komme)';
+$string['file'] = 'Fil';
+$string['image_img'] = 'Bilde';
+$string['image_url'] = 'Bilde URL';
+$string['itune'] = '(med Itunes)';
+$string['lang'] = 'Språk';
+$string['name'] = 'Tittel';
+$string['owner'] = 'Forfatter';
+$string['owner_email'] = 'Epost';
+$string['rss'] = 'Abonner på';
+$string['save'] = 'Lagre';
+$string['title_empty'] = 'Tittelen på filen kan ikke være tom!';
+$string['titles'] = 'Titler';
+$string['update_ok'] = 'Artikkelen er endret';
+$string['view'] = 'Se på';
+
+?>
