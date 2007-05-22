32803a0ebef75afa595b6f79358c5fbe5ba1d57c
johnharald <johnharald>
2007-05-22 12:32:21 +0000

    New Translation

---
+++ no_utf8/blog.php
@@ -0,0 +1,59 @@
+<?PHP // $Id$ 
+      // blog.php - created with Moodle 1.7.1+ (2006101010)
+      // local modifications from http://pdl.asp-hedmark.no
+
+
+$string['addnewentry'] = 'Legg til nytt innlegg';
+$string['addotags'] = 'Legg til offisielle tagger';
+$string['addptags'] = 'Legg til brukerdefinerte tagger';
+$string['blockmenutitle'] = 'Blogg Meny';
+$string['blocktagstitle'] = 'Blogg Tagger';
+$string['blocktitle'] = 'Bloggtag bokstittel';
+$string['blog'] = 'Blogg';
+$string['blogdeleteconfirm'] = 'Slett denne bloggen?';
+$string['blogpreferences'] = 'Blogg innstillinger';
+$string['blogs'] = 'Blogger';
+$string['blogtags'] = 'Bloggtagger';
+$string['courseblog'] = 'Kursblogg: $a';
+$string['courseblogs'] = 'Brukere kan bare se bloggene til de som er på samme kurs';
+$string['disableblogs'] = 'Slå av bloggsystemet fullstendig';
+$string['emptybody'] = 'Bloggen kan ikke være blank';
+$string['emptytitle'] = 'Bloggtittelen kan ikke være blank';
+$string['entrybody'] = 'Bloggtekst';
+$string['entrybodyonlydesc'] = 'Beskrivelse av innlegget';
+$string['entryerrornotyours'] = 'Dette innlegget er ikke ditt';
+$string['entrysaved'] = 'Innlegget ditt er lagret';
+$string['entrytitle'] = 'Tittel på innlegg';
+$string['entryupdated'] = 'Blogginnlegg oppdatert';
+$string['groupblog'] = 'Gruppeblogg: $a';
+$string['groupblogs'] = 'Brukere kan bare se blogger til de de er på kurs med';
+$string['intro'] = 'RSS matinga ble automatisk laget fra en eller flere logger';
+$string['noentriesyet'] = 'Ingen synlige innlegg her';
+$string['noguestpost'] = 'Gjester kan ikke sende innlegg!';
+$string['norighttodeletetag'] = 'Du har ikke lov til å slette denne taggen - $a';
+$string['notallowedtoedit'] = 'Du har ikke lov til å redigere dette innlegget';
+$string['numberofentries'] = 'Innlegg: $a';
+$string['numberoftags'] = 'Antall tagger å vise';
+$string['otags'] = 'Offisielle tagger';
+$string['pagesize'] = 'Antall blogginnlegg per side';
+$string['personalblogs'] = 'Brukerne kan bare se sine egne blogger';
+$string['ptags'] = 'Brukerdefinerte tagger';
+$string['publishto'] = 'Publiser til';
+$string['publishtonoone'] = 'Degselv (kladd)';
+$string['publishtosite'] = 'Alle på dette nettstedet';
+$string['publishtoworld'] = 'Alle i hele verden';
+$string['settingsupdatederror'] = 'En feil har oppstått, blogginnstillingene kunne ikke bli oppdatert.';
+$string['siteblog'] = 'Sideblogg: $a';
+$string['siteblogs'] = 'Alle brukerne kan se alle blogginnlegg';
+$string['tagdatelastused'] = 'Datotaggen ble sist brukt';
+$string['tagsort'] = 'Sorter tagvisning ved';
+$string['tagtext'] = 'Tag tekst';
+$string['timewithin'] = 'Vis merkelapper fra de siste dagene';
+$string['updateentrywithid'] = 'Oppdaterer innlegg';
+$string['userblog'] = 'Brukerblogg: $a';
+$string['viewcourseentries'] = 'Se kursinnlegg';
+$string['viewmyentries'] = 'Se mine innlegg';
+$string['viewsiteentries'] = 'Se stedsinnlegg';
+$string['worldblogs'] = 'Alle i hele verden kan lese innlegg som blir satt til tilgang fra hele verden';
+
+?>
