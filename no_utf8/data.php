ceaef8fc9bf19afbe890c77f3a3962d73859d3dd
johnharald <johnharald>
2007-05-25 16:25:55 +0000

    Updated for Moodle 1.8. Translation by Alf Martin Johnsen

--- no_utf8/data.php
+++ no_utf8/data.php
@@ -1,14 +1,16 @@
 <?PHP // $Id$ 
-      // data.php - created with Moodle 1.6.3 (2006050530)
+      // data.php - created with Moodle 1.8 + (2007021503)
 
 
 $string['action'] = 'Handling';
 $string['add'] = 'Legg til post';
 $string['addafield'] = 'Legg til et felt';
 $string['addcomment'] = 'Legg til kommentar';
+$string['addentries'] = 'Legg til';
 $string['addtemplate'] = 'Legg til mal';
 $string['allowcomments'] = 'Tillat kommentarer?';
 $string['allowratings'] = 'Tillat vurdering av oppføringer?';
+$string['alttext'] = 'Alternativ tekst';
 $string['approve'] = 'Godkjenn';
 $string['ascending'] = 'Stigende';
 $string['atmaxentry'] = 'Du har lagt inn det maksimale antall oppføringer som er tillatt!';
@@ -16,6 +18,19 @@ $string['autogenallforms'] = 'Generer alle standardmaler';
 $string['autolinkurl'] = 'Lag akelapperutomatisk lenke til URL';
 $string['availablefromdate'] = 'Tilgjengelig fra';
 $string['availabletags'] = 'Tilgjengelige mer';
+$string['availabletodate'] = 'Tilgjengelig for';
+$string['blank'] = 'Blank';
+$string['buttons'] = 'Knapper';
+$string['cancel'] = 'Avbryt';
+$string['checkbox'] = 'Valgboks';
+$string['chooseorupload'] = 'Velg fil';
+$string['columns'] = 'Kolonner';
+$string['comment'] = 'Kommentar';
+$string['commentdeleted'] = 'Kommentaren er slettet';
+$string['commentempty'] = 'Kommentaren var tom';
+$string['comments'] = 'Kommentarer';
+$string['commentsaved'] = 'Kommentaren lagret';
+$string['commentsn'] = '$a kommentar(er)';
 $string['savesettings'] = 'Lagre innstillinger';
 $string['savetemplate'] = 'Lagre mal';
 $string['singletemplate'] = 'Enkelt mal';
