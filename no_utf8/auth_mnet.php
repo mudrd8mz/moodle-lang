ef6a50c16d1f557095c3741e8da5fd0caf0e31ea
johnharald <johnharald>
2007-05-25 15:57:04 +0000

    New translation made by Alf Martin Johnsen/efaktor.no

---
+++ no_utf8/auth_mnet.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.8 + (2007021503)
+
+
+$string['sso_idp_description'] = 'Publiser denne tjenesten for å tillate at dine brukere kobles opp mot $a Moodle portalen uten å logge inn på nytt.
+<ul><li><em>Avhengighet</em>: Du må også <strong>abonnere</strong> på SSO (Singel Sign On) tjenesten på $a.</li></ul><br />Abonner på denne tjenesten for å tillate at godkjente brukere på $a får tilgang til din Moodle-server uten å logge inn på nytt. <ul><li><em>Avhengighet</em>: Du må også <strong>publisere</strong> SSO tjenesten til $a.</li></ul><br />';
+$string['sso_idp_name'] = 'SSO (Identitetstilbyder)';
+$string['sso_mnet_login_refused'] = 'Brukernavnet $a[0] har ikke tillatelse til  å logge inn fra $a[1].';
+$string['sso_sp_description'] = 'Aktiviser denne tjenesten hvis du vil tillate at brukere fra $a kan få tilgang hos deg uten å logge inn på nytt. <ul><li><em>Avhengighet</em>: Du må også <strong>abonnere</strong> på SSO (Identitetstilbyder) tjenesten på $a.</li></ul><br />Abonner på denne tjenesten  hvis du vil tillate at dine brukere å koble seg opp mot $a Moodle portalen uten å logge inn på nytt. <ul><li><em>Avhengighet</em>: Du må også <strong>publisere</strong> SSO (Identitetstilbyder) tjenesten til $a.</li></ul><br />';
+$string['sso_sp_name'] = 'SSO (Tjenestetilbyder)';
+
+?>
