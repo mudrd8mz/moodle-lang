a151fe30f53006cccea8d685ed7ffc5d44a70085
Nicolas Martignoni <martignoni>
2007-05-30 18:11:14 +0000

    Typo corrected, thanks to Severin Terrier

--- fr_utf8/enrol_mnet.php
+++ fr_utf8/enrol_mnet.php
@@ -11,7 +11,7 @@ $string['allowedcourses'] = '$a cours autorisés';
 $string['nocoursesdefined'] = 'Aucun cours trouvé. Veuillez définir de nouveaux cours <a href=\"$a\">ici</a>.';
 $string['allowedcourseslinktext'] = 'Modifier les cours et catégories autorisés';
 $string['mnet_enrol_name'] = 'Inscription via Réseau Moodle';
-$string['mnet_enrol_description'] = 'En publianz ce service, vous autorisez les administrateurs de $a à inscrire leurs étudiants à des cours sur votre Moodle.<br />
+$string['mnet_enrol_description'] = 'En publiant ce service, vous autorisez les administrateurs de $a à inscrire leurs étudiants à des cours sur votre Moodle.<br />
 <ul>
 <li><em>Dépendance</em>&nbsp;: vous devez également <strong>publier</strong> le service SSO (fournisseur de service) pour $a.</li>
 <li><em>Dépendance</em>&nbsp;: vous devez également <strong>vous abonner</strong> au service SSO (fournisseur d\'identité) de $a.</li>
