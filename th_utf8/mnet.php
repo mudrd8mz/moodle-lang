9334d582838004002022f8d8a76f4e48471a17c7
Dr.Wim Singhanart <minkowski>
2007-06-10 08:13:15 +0000

    Thai Language for upto Moodle 1.9

---
+++ th_utf8/mnet.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // mnet.php - created with Moodle 1.9 dev (2007060502)
+
+
+$string['enrolments'] = 'การลงทะเบียน';
+$string['mnetenrol'] = 'การลงทะเบียน';
+$string['mnetlog'] = 'บันทึกการใช้งาน';
+$string['mnetpeers'] = 'เครื่องลูกข่าย';
+$string['mnetservices'] = 'บริการ';
+$string['mnetsettings'] = 'การตั้งค่าระบบเครือข่ายของ Moodle';
+$string['moodleloc'] = 'ที่ตั้งของ Moodle';
+$string['net'] = 'ระบบเครือข่าย';
+$string['networksettings'] = 'การตั้งค่าเครือข่าย';
+$string['settings'] = 'การตั้งค่า';
+$string['ssoaccesscontrol'] = 'การควบคุมการเข้าถึง SSO';
+$string['trustedhosts'] = 'โฮสต์ XML-RPC';
+
+?>
