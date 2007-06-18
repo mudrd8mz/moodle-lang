b99ecb0d4d4127e7d3b8246f2b1caa2a1218d58d
komlenov <komlenov>
2007-06-18 10:59:54 +0000

    Moodle 1.8.1 updates

---
+++ sr_cr_utf8/question.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // question.php - created with Moodle 1.8.1+ (2007021510)
+
+
+$string['categorydoesnotexist'] = 'Ова категорија не постоји';
+$string['editingquestion'] = 'Уређивање питања';
+$string['fractionsnomax'] = 'Један од одговора би требао носити 100%% резултата тако да је могуће добити максималан могући број бодова за ово питање.';
+$string['missingimportantcode'] = 'Овом типу питања недостаје важан код: $a.';
+$string['notenoughdatatoeditaquestion'] = 'Нису специфицирани ни идентификатор питања, ни идентификатор категорије са типом питања.';
+$string['questionbank'] = 'База питања';
+$string['questiondoesnotexist'] = 'Ово питање не постоји';
+$string['unknownquestiontype'] = 'Непознат тип питања: $a.';
+
+?>
---
+++ sr_lt_utf8/question.php
@@ -0,0 +1,15 @@
+﻿<?PHP // $Id$ 
+      // question.php - created with Moodle 1.8.1+ (2007021510)
+
+
+$string['categorydoesnotexist'] = 'Ova kategorija ne postoji';
+$string['editingquestion'] = 'Uređivanje pitanja';
+$string['fractionsnomax'] = 'Jedan od odgovora bi trebao nositi 100%% rezultata tako da je moguće dobiti maksimalan mogući broj bodova za ovo pitanje.';
+$string['missingimportantcode'] = 'Ovom tipu pitanja nedostaje važan kod: $a.';
+$string['notenoughdatatoeditaquestion'] = 'Nisu specificirani ni identifikator pitanja, ni identifikator kategorije sa tipom pitanja.';
+$string['questionbank'] = 'Baza pitanja';
+$string['questiondoesnotexist'] = 'Ovo pitanje ne postoji';
+$string['unknownquestiontype'] = 'Nepoznat tip pitanja: $a.';
+
+?>
+
