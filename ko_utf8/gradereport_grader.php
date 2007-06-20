73be4f383d23e6ba5b7cfaaf66f23b797d467b17
Lee Chan-young <yogibear270>
2007-06-20 11:59:54 +0000

    new files

---
+++ ko_utf8/gradereport_grader.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // gradereport_grader.php - created with Moodle 1.9 dev (2007060502)
+
+
+$string['grader:manage'] = '성적표 관리';
+$string['grader:view'] = '성적표 보기';
+$string['modulename'] = '성적표';
+
+?>
---
+++ ko_utf8/gradereport_user.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // gradereport_user.php - created with Moodle 1.9 dev (2007060502)
+
+
+$string['modulename'] = '내 성적표';
+$string['user:view'] = '내 성적표 보기';
+
+?>
