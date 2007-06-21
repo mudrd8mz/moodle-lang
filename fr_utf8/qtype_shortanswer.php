3d2a6388137380625ce1215d01b41564610c604e
Nicolas Martignoni <martignoni>
2007-06-21 18:37:40 +0000

    Typos in "joker" corrected

--- fr_utf8/qtype_shortanswer.php
+++ fr_utf8/qtype_shortanswer.php
@@ -2,6 +2,6 @@
 
 $string['addmoreanswerblanks'] = '{no} emplacements supplémentaires vides';
 $string['answerno'] = 'Réponse $a';
-$string['filloutoneanswer'] = 'Vous devez fournir au moins une réponse possible. Les réponses laissées vides ne seront pas utilisées. Le caractère «&nbsp;*&nbsp;» peut être utilisé comme jocker pour remplacer n\'importe quel caractère. La première réponse correspondante sera utilisée pour déterminer le score et le feedback.';
+$string['filloutoneanswer'] = 'Vous devez fournir au moins une réponse possible. Les réponses laissées vides ne seront pas utilisées. Le caractère «&nbsp;*&nbsp;» peut être utilisé comme joker pour remplacer n\'importe quel caractère. La première réponse correspondante sera utilisée pour déterminer le score et le feedback.';
 
 ?>
