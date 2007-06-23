8bec8cb0b6edc835707c9eb4af61c0b37a31fed8
koen roggemans <koenr>
2007-06-23 14:46:03 +0000

    cleaned mongolian language pack: 2 empty files, credits on top of files, weird timezones.php file, duplicate readme

--- mn_utf8/block_rss_client.php
+++ mn_utf8/block_rss_client.php
@@ -1,13 +1,4 @@
-
-<!--
-Энэхvv файлын орчуулгын ажлыг "Нээлттэй эхтэй зайн сургалтын програм хангамжуудын vнэлгээ, нутагшуулалт" (www.pandora-asia.org) тєслийн хvрээнд ИнфоКон ХХК гvйцэтгэв. Энэхvv тєсєл нь/ Канадын Олон Улсын Хєгжлийн Агентлагын санхvvжилтээр хэрэгжсэн болно.
-
-The translation of this file carried out within a framework of Evaluation and Adaptation of Open Source Software for DL in Asia <http://www.pandora-asia.org/panprojects.php?main=panprojects_3.htm> (www.pandora-asia.org) project implemented by InfoCon LLC with financial support from the Pan Asia Networking (PAN) Programme Initiative of the International Development Research Centre (IDRC), under a grant to the Virtual University of Pakistan for the project, PANdora - Distance and Open Resource Access. 
--->
-
-
-
-<?PHP // $Id$ 
+?PHP // $Id$ 
       // block_rss_client.php - created with Moodle 1.7+ (2006101009)
 
 
