d23a3164850098b825beefc0cbc317c71c0c0641
Nicolas Martignoni <martignoni>
2007-06-24 21:34:25 +0000

    MDL-10237 - Improve wording on true/false edit form

--- fr_utf8/qtype_truefalse.php
+++ fr_utf8/qtype_truefalse.php
@@ -3,8 +3,8 @@
 $string['correctanswer'] = 'Réponse correcte';
 $string['editingtruefalse'] = 'Modifier une question Vrai/Faux';
 $string['false'] = 'Faux';
-$string['feedbackfalse'] = 'Feedback (Faux)';
-$string['feedbacktrue'] = 'Feedback (Vrai)';
+$string['feedbackfalse'] = 'Feedback pour la réponse «&nbsp;Faux&nbsp;»';
+$string['feedbacktrue'] = 'Feedback pour la réponse «&nbsp;Vrai&nbsp;»';
 $string['true'] = 'Vrai';
 $string['truefalse'] = 'Vrai/Faux';
 
