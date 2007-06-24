2cd906a97b4564a4e3d0e9899a36070f1f760b7a
Mitsuhiro Yoshida <mits>
2007-06-24 17:50:35 +0000

    Updated translated strings for question type Itrue/false based on lang/en_utf8 change MDL-10237.

--- ja_utf8/qtype_truefalse.php
+++ ja_utf8/qtype_truefalse.php
@@ -1,12 +1,12 @@
 <?PHP // $Id$ 
-      // qtype_truefalse.php - created with Moodle 1.8 dev (2007012500)
+      // qtype_truefalse.php - created with Moodle 1.9 dev (2007062301)
 
 
 $string['correctanswer'] = '正解';
 $string['editingtruefalse'] = '○/×問題の編集';
 $string['false'] = '×';
-$string['feedbackfalse'] = 'フィードバック (×)';
-$string['feedbacktrue'] = 'フィードバック (○)';
+$string['feedbackfalse'] = '解答「×」に対するフィードバック';
+$string['feedbacktrue'] = '解答「○」に対するフィードバック';
 $string['true'] = '○';
 $string['truefalse'] = '○/×問題';
 
