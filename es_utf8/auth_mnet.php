09a1724d3b9361f891eae5781a3c1bc0887f9a1e
Benito Arias <barias>
2007-06-30 09:49:46 +0000

    *** empty log message ***

--- es_utf8/auth_mnet.php
+++ es_utf8/auth_mnet.php
@@ -1,8 +1,17 @@
-<?PHP // $Id$ 
+<?PHP // $Id$
       // auth_mnet.php - created with Moodle 1.7 (2006101007)
 
 
+$string['sso_idp_description']='Publique este servicio para permitir a sus usuarios divagar al sitio Moodle $a sin tener que reiniciar sesión allí. '.
+                               '<ul><li><em>Dependencia</em>: Usted debe también <strong>suscribirse</strong> al servicio SSO (Proveedor de servicios) en $a.</li></ul><br />'.
+                               'Suscríbase a este servicio para permitir a los usuarios autenticados de $a acceder a su sitio sin tener que reiniciar sesión. '.
+                               '<ul><li><em>Dependencia</em>: Usted debe también <strong>publicar</strong> el servicio SSO (proveedor de servicios) a $a.</li></ul><br />';
 $string['sso_idp_name'] = 'SSO (Proveedor de Identidad)';
+$string['sso_mnet_login_refused']= 'Al nombre de usuario $a[0] no le es permitido iniciar sesión desde $a[1].';
+$string['sso_sp_description']= 'Publique este servicio para permitir a usuarios autenticados acceder a su sitio desde $a sin tener que reinciar sesión.'.
+                               '<ul><li><em>Dependecia</em>: Usted debe también <strong>subscribirse</strong> al servicio SSO (Proveedor de identidad) en $a.</li></ul><br />'.
+                               'Suscríbase a este servicio para permitir a sus usuarios divagar al sitio Moodle $a sin tener que reiniciar sesión allí. '.
+                               '<ul><li><em>Dependencia</em>: Usted debe también <strong>publicar</strong> el servicio SSO (Proveedor de identidad) a $a.</li></ul><br />';
 $string['sso_sp_name'] = 'SSO (Proveedor de Servicios)';
 
 ?>
