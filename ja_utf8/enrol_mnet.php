e27df64ca0b22362514450a10b9b96ff0b98c63f
Mitsuhiro Yoshida <mits>
2007-07-10 18:39:33 +0000

    Brushed up a translated string for Moodle networking.

--- ja_utf8/enrol_mnet.php
+++ ja_utf8/enrol_mnet.php
@@ -10,7 +10,7 @@ $string['allowedcourses'] = '許可されたコース $a';
 $string['allowedcourseslinktext'] = 'ここで許可されたコースおよびカテゴリを編集してください。';
 $string['description'] = 'Moodleネットーワーキング経由のユーザ登録方法に関する説明です。';
 $string['enrolname'] = 'Moodleネットワーキング';
-$string['mnet_enrol_description'] = 'このサービスを公開することで、あなたがあなたのサーバに作成したコースに $a の管理者は自分の学生を受講登録することができます。<br/><ul><li>依存関係: あなたはSSO (サービスプロバイダ) サービスを $a に<strong>公開</strong>する必要があります。</li><li>依存関係: あなたは $a のSSO (アイデンティティプロバイダ) サービスに<strong>登録</strong>する必要があります。</li></ul><br/>このサービスに登録することで、あなたの学生を $a のコースに受講登録することができます。<br/><ul><li>依存関係: あなたは $a のSSO (サービスプロバイダ) サービスに<strong>登録</strong>する必要があります。
+$string['mnet_enrol_description'] = 'このサービスを公開することで、あなたがあなたのサーバに作成したコースに対して $a の管理者は自分の学生を受講登録することができます。<br/><ul><li>依存関係: あなたはSSO (サービスプロバイダ) サービスを $a に<strong>公開</strong>する必要があります。</li><li>依存関係: あなたは $a のSSO (アイデンティティプロバイダ) サービスに<strong>登録</strong>する必要があります。</li></ul><br/>このサービスに登録することで、あなたの学生を $a のコースに受講登録することができます。<br/><ul><li>依存関係: あなたは $a のSSO (サービスプロバイダ) サービスに<strong>登録</strong>する必要があります。
 </li><li>依存関係: あなたはSSO (アイデンティティプロバイダ) サービスを $a に<strong>公開</strong>する必要があります。</li></ul><br/>';
 $string['mnet_enrol_name'] = 'Moodleネットワーキング登録';
 $string['nocategoriesdefined'] = 'コースカテゴリが見つかりませんでした。<a href=\"$a\">ここで</a>新しいカテゴリを設定してください。';
