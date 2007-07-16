c2755e79c466280b34a0824334b281e29215f594
Miroslav Fikar <fikar>
2007-07-16 07:40:31 +0000

    New files

---
+++ sk_utf8/docs/files.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$
+
+$string['-about'] = "čo je Moodle";
+$string['intro.html'] = "Úvod";
+$string['background.html'] = "História";
+$string['philosophy.html'] = "Východiská";
+$string['licence.html'] = "Licencia";
+$string['features.html'] = "Vlastnosti";
+$string['release.html'] = "Poznámky k verzii";
+$string['future.html'] = "Budúcnosť";
+$string['credits.html'] = "Poďakovanie";
+
+$string['-installation'] = "Administratíva";
+$string['install.html'] = "Inštalácia";
+$string['faq.html'] = "Inštalácia - FAQ";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Aktualizácia";
+
+$string['-usage'] = "Používanie Moodlie";
+$string['teacher.html'] = "Príručka učiteľa";
+$string['other.html'] = "Ďalšia dokumentácia";
+
+$string['-development'] = "Vývoj";
+$string['developer.html'] = "Príručka vývojára";
+$string['coding.html'] = "Sprievodca kódovaním";
+$string['cvs.html'] = "Používanie CVS";
+$string['translation.html'] = "Preklad"; 
+?>
