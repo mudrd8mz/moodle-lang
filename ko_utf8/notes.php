54096e50f48ab44532800f9c071efcf3a7431b21
Lee Chan-young <yogibear270>
2007-07-18 12:36:14 +0000

    New Born files.

---
+++ ko_utf8/notes.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 1.9 dev (2007071501)
+
+
+$string['abovenormal'] = '중상';
+$string['addnewnote'] = '새 알림글 추가';
+$string['belownormal'] = '중하';
+$string['bynameondate'] = '$a->name - $a->date';
+$string['content'] = '전달 내용';
+$string['course'] = '배움터';
+$string['coursenotes'] = '배움터 알림';
+$string['created'] = '생성됨';
+$string['deleteconfirm'] = '이 내용을 삭제할까요?';
+$string['groupaddnewnote'] = '모두에게 새 알림 추가';
+$string['high'] = '상';
+$string['low'] = '하';
+$string['nocontent'] = '전달 내용을 채워 넣으세요';
+$string['nonotes'] = '알림이 없습니다';
+$string['normal'] = '중';
+$string['notes'] = '전달사항';
+$string['notesnotvisible'] = '내용을 볼 수 없습니다.';
+$string['nouser'] = '사용자를 선택해야 합니다';
+$string['personal'] = '개인';
+$string['personalnotes'] = '개인적 알림';
+$string['publishstate'] = '전달대상';
+$string['rating'] = '중요도';
+$string['site'] = '사이트';
+$string['sitenotes'] = '사이트 알림';
+$string['unknown'] = '모름';
+
+?>
