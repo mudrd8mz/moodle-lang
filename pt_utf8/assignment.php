efb1a42952e7c79b32ba5ed6d240ce2cbb322614
Jaime Villate <villate>
2007-07-18 14:40:56 +0000

    Additions/corrections contributed by Odilia Fernandes.

--- pt_utf8/assignment.php
+++ pt_utf8/assignment.php
@@ -1,52 +1,62 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.7.1+ (2006101010)
+      // assignment.php - created with Moodle 1.7+ (2006101009)
 
 
-$string['allowresubmit'] = 'Permitir enviar novamente';
+$string['allowdeleting'] = 'Permitir eliminar';
+$string['allowmaxfiles'] = 'Número máximo de envio de ficheiros';
+$string['allownotes'] = 'Permitir notas';
+$string['allowresubmit'] = 'Permitir submeter novamente';
+$string['assignment:grade'] = 'Categoria do trabalho';
+$string['assignment:submit'] = 'Submeter trabalho';
+$string['assignment:view'] = 'Ver trabalho';
 $string['assignmentdetails'] = 'Detalhes do trabalho';
 $string['assignmentmail'] = '$a->teacher  comentou o seu trabalho
 \'$a->assignment\'
 
-O comentário foi anexado no fim do seu trabalho:
+O comentário foi anexado no final do seu trabalho:
 
     $a->url';
 $string['assignmentmailhtml'] = '$a->teacher comentou o seu trabalho
-\'<i>$a->assignment</i>\'<br /><br />
-O comentário encontra-se anexo no fim do <a href=\"$a->url\">seu trabalho</a>.';
+\'<i>$a->assignment</i>\'<br /><br />.
+O comentário foi anexado no final do <a href=\"$a->url\">seu trabalho</a>.';
 $string['assignmentname'] = 'Nome do trabalho';
 $string['assignmenttype'] = 'Tipo de trabalho';
 $string['availabledate'] = 'Disponível de';
 $string['comment'] = 'Comentário';
-$string['commentinline'] = 'Comente inline';
-$string['configitemstocount'] = 'Natureza dos items a ser contados para submissões dos alunos em atribuições online.';
-$string['configmaxbytes'] = 'Tamanho máximo, por omissão, de todos os trabalhos enviados para o sítio (sujeito a limites de cada disciplina e limites locais)';
+$string['commentinline'] = 'Comentário em-linha';
+$string['configitemstocount'] = 'Natureza dos items a ser contados para submissões dos alunos em trabalhos em-linha.';
+$string['configmaxbytes'] = 'Tamanho máximo, por omissão, de todos os trabalhos enviados para a plataforma (sujeito a limites de cada disciplina e limites locais)';
+$string['confirmdeletefile'] = 'tem a certeza que pretende eliminar este ficheiro? <br /><strong>$a</strong>';
+$string['deletefilefailed'] = 'A eliminação do ficheiro falhou';
 $string['description'] = 'Descrição';
+$string['draft'] = 'Rascunho';
 $string['duedate'] = 'Data de entrega';
-$string['duedateno'] = 'Sem data limite';
+$string['duedateno'] = 'Sem data de entrega';
 $string['early'] = '$a cedo';
 $string['editmysubmission'] = 'Editar a minha submissão';
-$string['emailstudents'] = 'Enviar alertas a alunos por email';
-$string['emailteachermail'] = '$a->username actualizou a submissão atribuida para \'$a->assignment\'
+$string['emailstudents'] = 'Emails de alertas para alunos';
+$string['emailteachermail'] = '$a->username actualizou a submissão ao seu trabalho para \'$a->assignment\'
 
 Está disponível aqui:
 
 $a->url';
-$string['emailteachermailhtml'] = '$a->username actualizou a submissão atribuida para <i>\'$a->assignment\'</i><br /><br /> Está <a href=\"$a->url\">disponível no site</a>';
+$string['emailteachermailhtml'] = '$a->username actualizou a submissão ao seu trabalho para <i>\'$a->assignment\'</i><br /><br /> Está <a href=\"$a->url\">disponível no sítio</a>';
 $string['emailteachers'] = 'Emails de alerta para professores';
-$string['emptysubmission'] = 'Ainda não submeteu nada';
-$string['existingfiledeleted'] = 'O seguinte ficheiro foi apagado: $a';
+$string['emptysubmission'] = 'Você ainda não submeteu nada';
+$string['existingfiledeleted'] = 'O seguinte ficheiro foi eliminado: $a';
 $string['failedupdatefeedback'] = 'Falhou a actualização dos comentários ao trabalho do utilizador $a';
 $string['feedback'] = 'Os seus comentários';
 $string['feedbackfromteacher'] = 'Comentários sobre $a';
 $string['feedbackupdated'] = 'Foram actualizados os comentários aos trabalhos de $a pessoas';
 $string['graded'] = 'Classificado';
-$string['guestnosubmit'] = 'Desculpe, aos visitantes não é permitido submeter um a atribuição. Tem que se registar antes de submeter a resposta';
-$string['guestnoupload'] = 'Desculpe, aos visitantes não é permitido carregar ficheiros';
-$string['helpoffline'] = '<p>É útil quando a atribuição é executada fora do Moodle.</p><p>Os alunos podem ver uma descrição da atribuição, mas não podem carregar ficheiros nem nada disso. Classifique trabalhos normalmente, e os alunos receberão notificações das suas notas</p>';
+$string['guestnosubmit'] = 'Desculpe, aos visitantes não é permitido submeter um trabalho. Tem efectuar o login ou registar-se antes de submeter a resposta';
+$string['guestnoupload'] = 'Desculpe, aos visitantes não é permitido enviar ficheiros';
+$string['helpoffline'] = '<p>É útil quando o trabalho é executado fora do Moodle.</p><p>Os alunos podem ver uma descrição do trabalho, mas não podem carregar ficheiros nem nada disso. Classifique os trabalhos normalmente, e os alunos receberão notificações das suas notas.</p>';
 $string['helponline'] = '<p>Este tipo de trabalho requer que os utilizadores editem um texto, utilizando as ferramentas de edição normais. Os professores poderão classificá-los Online, podendo também adicionar comentários ou fazer alterações.</p><p>(Se estiver familiarizado com versões antigas do Moodle, este tipo de trabalho faz precisamente a mesma coisa que o antigo módulo JORNAL.)</p>';
 $string['helpupload'] = '<p>Este tipo de trabalho permite que sejam enviados vários ficheiros.</p>
 <p>Os ficheiros podem ser de qualquer tipo (documento de word, imagens, ficheiros zipados, etc.)</p>';
-$string['helpuploadsingle'] = '<p>Este tipo de trabalho permite a cada participante carregar um ficheiro simples, de qualquer tipo.</p> <p>Poderá ser um documento Word, ou uma imagem, ou um web site, ou qualquer coisa que submeta.</p>';
+$string['helpuploadsingle'] = '<p>Este tipo de trabalho permite a cada participante enviar um ficheiro simples, de qualquer tipo.</p> <p>Poderá ser um documento Word, uma imagem, uma página web, ou qualquer coisa que submeta.</p>';
+$string['hideintro'] = 'Oculte a descrição antes da data indicada';
 $string['late'] = '$a atrasado';
 $string['maximumgrade'] = 'Nota máxima';
 $string['maximumsize'] = 'Tamanho máximo';
@@ -54,29 +64,41 @@ $string['modulename'] = 'Trabalho';
 $string['modulenameplural'] = 'Trabalhos';
 $string['newsubmissions'] = 'Trabalhos enviados';
 $string['noassignments'] = 'Ainda não existem trabalhos';
-$string['noattempts'] = 'Não têm sido feita nenhuma tentativa neste trabalho';
-$string['notgradedyet'] = 'Ainda não avaliado';
-$string['notsubmittedyet'] = 'Ainda não enviado';
-$string['onceassignmentsent'] = 'Uma vez for enviado este trabalho, para ser avaliado, não poderá apagá-lo nem juntar nenhum ficheiro anexo.';
+$string['noattempts'] = 'Não têm sido feitas tentativas neste trabalho';
+$string['nofiles'] = 'Não foram submetidos ficheiros';
+$string['nofilesyet'] = 'Ainda não foram submetidos ficheiros';
+$string['notavailableyet'] = 'Desculpe, mas este traalho ainda não está disponível. <br/> O trabalho será mostrado na data indicada em baixo.';
+$string['notes'] = 'Notas';
+$string['notesempty'] = 'Sem entradas';
+$string['notesupdateerror'] = 'Erro ao actualizar as notas';
+$string['notgradedyet'] = 'Ainda não foi avaliado';
+$string['notsubmittedyet'] = 'Ainda não foi enviado';
+$string['onceassignmentsent'] = 'Uma vez o trabalho enviado, para avaliação, não poderá ser eliminado nem anexado ficheiros.';
 $string['overwritewarning'] = 'Atenção: se enviar novamente, o seu envio anterior será substituído.';
 $string['pagesize'] = 'Número de envios mostrados por página';
-$string['preventlate'] = 'Impedir enviar fora de praço';
+$string['preventlate'] = 'Impedir enviar fora do prazo';
 $string['quickgrade'] = 'Permitir avaliação rápida';
+$string['responsefiles'] = 'Ficheiros de resposta';
+$string['reviewed'] = 'Revisto';
 $string['saveallfeedback'] = 'Gravar todos os meus comentários';
 $string['sendformarking'] = 'Enviado para avaliação';
 $string['submission'] = 'Envio';
+$string['submissiondraft'] = 'Submeter rascunho';
 $string['submissionfeedback'] = 'Comentários ao trabalho';
 $string['submissions'] = 'Trabalhos enviados';
 $string['submissionsaved'] = 'As suas alterações foram gravadas';
 $string['submissionsnotgraded'] = '$a trabalhos enviados, sem avaliar';
 $string['submitassignment'] = 'Envie o seu trabalho usando este formulário';
+$string['submitedformarking'] = 'O trabalho já foi submetido e não pode ser alterado';
 $string['submitformarking'] = 'Enviar trabalho para ser avaliado';
 $string['submitted'] = 'Enviado';
+$string['submittedfiles'] = 'Ficheiros submetidos';
 $string['typeoffline'] = 'Trabalho de casa';
 $string['typeonline'] = 'Texto em-linha';
-$string['typeupload'] = 'Envio de ficheiros avançado';
+$string['typeupload'] = 'Envio avançado de ficheiros';
 $string['typeuploadsingle'] = 'Envio de um único ficheiro';
-$string['uploadbadname'] = 'O nome deste ficheiro continha caracteres estranhos e não pode ser enviado';
+$string['unfinalize'] = 'Voltar ao rascunho';
+$string['uploadbadname'] = 'O nome deste ficheiro continha caracteres inválidos e não pode ser enviado';
 $string['uploadedfiles'] = 'Ficheiro enviado';
 $string['uploaderror'] = 'Apareceu um erro na gravação do ficheiro no servidor';
 $string['uploadfailnoupdate'] = 'O ficheiro foi recebido bem, mas não foi possível actualizar o seu trabalho!';
@@ -84,7 +106,7 @@ $string['uploadfiletoobig'] = 'Desculpe, mas esse ficheiro é muito grande (o li
 $string['uploadnofilefound'] = 'Não foi encontrado nenhum ficheiro - tem a certeza que seleccionou algum para enviar?';
 $string['uploadnotregistered'] = '\'$a\' foi recebido correctamente mas o envio não ficou registado!';
 $string['uploadsuccess'] = '\'$a\' enviado com sucesso!';
-$string['viewfeedback'] = 'Ver a nota e comentários ao seu trabalho';
+$string['viewfeedback'] = 'Ver notas e comentários ao seu trabalho';
 $string['viewsubmissions'] = 'Ver $a trabalhos enviados';
 $string['yoursubmission'] = 'O seu envio';
 
