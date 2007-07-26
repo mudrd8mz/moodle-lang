ba46b3edf04226e1286c72069d24f7290d545dfa
johnharald <johnharald>
2007-07-26 07:12:02 +0000

    Ny oversettelse

---
+++ no_utf8/question.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // question.php - created with Moodle 1.9 dev (2007071300)
+
+
+$string['categorydoesnotexist'] = 'Kategorien finne ikke';
+$string['editingquestion'] = 'Rediger et spørsmål';
+$string['fractionsnomax'] = 'Et av svarene bør ha en score på 100%% slik at det er mulig å få toppkarakter for spørsmålet.';
+$string['makechildof'] = 'Lag et underpunkt til \'$a\'';
+$string['maketoplevelitem'] = 'Gå til toppnivå';
+$string['missingimportantcode'] = 'Spørsmålstypen mangler en viktig kode: $a.';
+$string['notenoughdatatoeditaquestion'] = 'Hverken spørsmålsid, eller kategori-id og spørsmålstype eller blitt angitt.';
+$string['questionbank'] = 'Spørsmålsbank';
+$string['questiondoesnotexist'] = 'Spørsmålet finnes ikke';
+$string['unknownquestiontype'] = 'Ukjent spørsmålstype: $a.';
+
+?>
