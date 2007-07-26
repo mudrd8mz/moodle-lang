936355ec8aa850c688dd7be8c072dfc8368b377c
Mitsuhiro Yoshida <mits>
2007-07-26 15:27:41 +0000

    Translated new strings for grade outcomes report.

--- ja_utf8/gradereport_outcomes.php
+++ ja_utf8/gradereport_outcomes.php
@@ -1,7 +1,11 @@
 <?PHP // $Id$ 
-      // gradereport_outcomes.php - created with Moodle 1.9 dev (2007072402)
+      // gradereport_outcomes.php - created with Moodle 1.9 dev (2007072500)
 
 
+$string['addoutcome'] = 'アウトカムを追加する';
+$string['courseoutcomes'] = 'コースアウトカム';
+$string['coursespecoutcome'] = 'コースアウトカム';
 $string['modulename'] = 'アウトカム';
+$string['usedgradeitem'] = '評定項目数';
 
 ?>
