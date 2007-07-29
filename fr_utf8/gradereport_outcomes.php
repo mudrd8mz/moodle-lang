fb42c5ad832d1f139d628d2e7beba6eb2cad4a4d
Nicolas Martignoni <martignoni>
2007-07-29 19:41:33 +0000

    adding some strings

--- fr_utf8/gradereport_outcomes.php
+++ fr_utf8/gradereport_outcomes.php
@@ -1,5 +1,9 @@
 <?php // $Id$ 
 
+$string['addoutcome'] = 'Ajouter un objectif';
+$string['courseoutcomes'] = 'Objectif du cours';
+$string['coursespecoutcome'] = 'Objectifs du cours';
 $string['modulename'] = 'Objectifs';
+$string['usedgradeitem'] = 'Nombre d\'éléments d\'évaluation';
 
 ?>
