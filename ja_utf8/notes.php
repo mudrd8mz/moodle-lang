f51278b4e77b64a2147d081f52466239c784cf37
Mitsuhiro Yoshida <mits>
2007-07-30 05:43:08 +0000

    Fixing translated strings for notes based on lang/en_utf8 change.

--- ja_utf8/notes.php
+++ ja_utf8/notes.php
@@ -1,29 +1,25 @@
 <?PHP // $Id$ 
-      // notes.php - created with Moodle 1.9 dev (2007071300)
+      // notes.php - created with Moodle 1.9 dev (2007072500)
 
 
-$string['abovenormal'] = '標準以上';
 $string['addnewnote'] = '新しいノートを追加する';
-$string['belownormal'] = '標準以下';
 $string['bynameondate'] = '更新: $a->name - $a->date';
-$string['content'] = 'ノートコンテンツ';
+$string['content'] = 'コンテンツ';
 $string['course'] = 'コース';
 $string['coursenotes'] = 'コースノート';
 $string['created'] = '作成日時';
 $string['deleteconfirm'] = 'このノートを削除してもよろしいですか?';
+$string['editnote'] = 'ノートの編集';
 $string['groupaddnewnote'] = '全員の新しいノートを追加する';
-$string['high'] = '高い';
-$string['low'] = '低い';
 $string['nocontent'] = 'ノートコンテンツを入力してください。';
 $string['nonotes'] = 'ノートはありません。';
-$string['normal'] = '標準';
+$string['note'] = 'ノート';
 $string['notes'] = 'ノート';
 $string['notesnotvisible'] = 'あなたはこのノートを閲覧することができません。';
 $string['nouser'] = 'ユーザを選択してください。';
 $string['personal'] = 'パーソナル';
 $string['personalnotes'] = 'パーソナルノート';
 $string['publishstate'] = 'ステータス';
-$string['rating'] = '評価';
 $string['site'] = 'サイト';
 $string['sitenotes'] = 'サイトノート';
 $string['unknown'] = '不明';
