a642d2756149febca689b3d74ebb0d810fb73011
Carles Bellver <carlesbellver>
2007-08-02 12:39:03 +0000

    *** empty log message ***

---
+++ ca_utf8/tag.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 1.9 dev (2007080103)
+
+
+$string['addtagtomyinterests'] = 'Afegeix \"$a\" als meus interessos';
+$string['description'] = 'Descripció';
+$string['edittag'] = 'Edita aquesta etiqueta';
+$string['flag'] = 'Senyalador';
+$string['flagasinappropriate'] = 'Senyala com a inapropiat';
+$string['helprelatedtags'] = 'Etiquetes relacionades separades per comes';
+$string['managetags'] = 'Gestiona etiquetes';
+$string['name'] = 'Nom';
+$string['noresultsfor'] = 'Cap resultat per a \"$a\"';
+$string['owner'] = 'Propietari';
+$string['relatedtags'] = 'Etiquetes relacionades';
+$string['reset'] = 'Reinicia';
+$string['resetflag'] = 'Reinicia senyalador';
+$string['responsiblewillbenotified'] = 'La persona responsable rebrà una notificació';
+$string['search'] = 'Cerca';
+$string['searchresultsfor'] = 'Resultats de la cerca: \"$a\"';
+
+?>
