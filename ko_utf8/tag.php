a4326eedd7ecdb8b450df5a884cefa8b8a52f294
Lee Chan-young <yogibear270>
2007-08-04 15:02:44 +0000

    new files

---
+++ ko_utf8/tag.php
@@ -0,0 +1,37 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 1.9 dev (2007080202)
+
+
+$string['addtagtomyinterests'] = '관심분야에 \"$a\" 추가';
+$string['count'] = '계수';
+$string['delete'] = '삭제';
+$string['deleted'] = '삭제됨';
+$string['description'] = '설명';
+$string['edittag'] = '이 태그 편집';
+$string['flag'] = '표식';
+$string['flagasinappropriate'] = '부적절 표시';
+$string['helprelatedtags'] = '관련 태그를 컴마로 분리';
+$string['id'] = 'id';
+$string['managetags'] = '태그 관리';
+$string['name'] = '이름';
+$string['noresultsfor'] = '\"$a\"에 대한 결과 없음';
+$string['owner'] = '소유자';
+$string['relatedtags'] = '관련 태그';
+$string['reset'] = '초기화';
+$string['resetflag'] = '표식 초기화';
+$string['responsiblewillbenotified'] = '책임있는 사람에게는 통보됨';
+$string['search'] = '검색';
+$string['searchresultsfor'] = '\"$a\"에 대한 검색 결과';
+$string['searchtags'] = '태그 검색';
+$string['select'] = '선택';
+$string['tag'] = '태그';
+$string['tagdescription'] = '태그 설명';
+$string['tags'] = '태그';
+$string['tagsaredisabled'] = '태그를 사용할 수 없음';
+$string['thingstaggedwith'] = '\"$a->name\"로 $a->count 항목 검색됨';
+$string['thistaghasnodesc'] = '이 태그에는 설명이 없음';
+$string['timemodified'] = '수정됨';
+$string['updatetag'] = '갱신';
+$string['userstaggedwith'] = '사용자가 \"$a\" 로 덧붙임';
+
+?>
