4f3e54f35af6a5027f22e741c88510fafa5ff01e
koen roggemans <koenr>
2007-08-05 14:45:39 +0000

    mdl-9196

--- en_us_utf8/langconfig.php
+++ en_us_utf8/langconfig.php
@@ -1,14 +1,14 @@
 <?php
 $string['locale'] = 'en_US.UTF-8';
 $string['oldcharset'] = 'ISO-8859-1';
-$string['strftimedate'] = '%%B %%d %%Y';
+$string['strftimedate'] = '%%B %%d, %%Y';
 $string['strftimedateshort'] = '%%B %%d';
 $string['strftimedatetime'] = '%%B %%d %%Y, %%I:%%M %%p';
-$string['strftimedaydate'] = '%%A, %%B %%d %%Y';
-$string['strftimedaydatetime'] = '%%A, %%B %%d %%Y, %%I:%%M %%p';
+$string['strftimedaydate'] = '%%A, %%B %%d, %%Y';
+$string['strftimedaydatetime'] = '%%A, %%B %%d, %%Y, %%I:%%M %%p';
 $string['strftimedayshort'] = '%%A, %%B %%d';
 $string['strftimerecent'] = '%%I:%%M %%p,%%b %%d';
-$string['strftimerecentfull'] = '%%a, %%b %%d %%Y, %%I:%%M %%p';
+$string['strftimerecentfull'] = '%%a, %%b %%d, %%Y, %%I:%%M %%p';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'English';
