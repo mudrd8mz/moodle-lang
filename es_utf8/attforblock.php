ba6ddafc02ed44628d6bcbb7fa94ba5934e25903
Benito Arias <barias>
2007-08-07 09:21:07 +0000

    *** empty log message ***

--- es_utf8/attforblock.php
+++ es_utf8/attforblock.php
@@ -1,10 +1,17 @@
 <?PHP // $Id$ 
-      // attforblock.php - created with Moodle 1.7 (2006101007)
+      // attforblock.php - created with Moodle 1.8 + (2007021502)
 
 
+$string['attforblock:changepreferences'] = 'Cambio de preferencias';
+$string['attforblock:export'] = 'Exportar informes';
+$string['attforblock:manageattendances'] = 'Gestionar asistencia';
+$string['attforblock:takeattendances'] = 'Tomar asistencia';
+$string['attforblock:view'] = 'Ver asistencia';
+$string['attforblock:viewreports'] = 'Ver informes';
 $string['description'] = 'Puede agregar sólo un módulo Asistencia por curso.<br />Si elimina este módulo, ello no afectará a las asistencias. Puede usar este módulo sólo con el bloque Asistencia. Este módulo sólo necesita para incluir calificaciones de las asistencias en las calificaciones de los cursos principales.';
 $string['modulename'] = 'Asistencia (para Bloque)';
 $string['modulenameplural'] = 'Asistencias (para Bloque)';
-$string['modulenamesimple'] = 'Asistencia';
+$string['notfound'] = 'No se ha encontrado actividad de asistencia en este curso.';
+$string['modulenamesimple'] = 'Asistencia'; // ORPHANED
 
 ?>
