b2f005d39739b8fa3b720cc4e296a99c63113331
koen roggemans <koenr>
2007-08-08 12:29:15 +0000

    new strings for 1.9

---
+++ nl_utf8/gradereport_outcomes.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // gradereport_outcomes.php - created with Moodle 1.9 dev (2007080202)
+
+
+$string['addoutcome'] = 'Voeg een competentie toe';
+$string['courseoutcomes'] = 'Cursuscompetenties';
+$string['coursespecoutcome'] = 'Cursuscompetenties';
+$string['modulename'] = 'Competentierapport';
+$string['usedgradeitem'] = 'Aantal te beoordelen onderdelen';
+
+?>
