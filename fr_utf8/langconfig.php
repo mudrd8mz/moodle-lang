35b664b2cb4aaca325fdc413910238747cd10f05
Nicolas Martignoni <martignoni>
2007-08-10 15:13:15 +0000

    MDL-10791 improve handling of localised floating point numbers in moodle; replaced/fixed some float_format() that were used instead of round(); expect some more patches soon

--- fr_utf8/langconfig.php
+++ fr_utf8/langconfig.php
@@ -2,8 +2,9 @@
 
 $string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
-$string['decpoint'] = '.'; // decimal point, for some languages it is ',' if this is changed, must set thousandsep
+$string['decsep'] = ','; // decimal point separator, for some languages it is ','
 $string['firstdayofweek'] = '1';
+$string['listsep'] = ';'; // if decsep is ',', then usually ';' is used instead here; these two can not be the same
 $string['locale'] = 'fr_FR.UTF-8';
 $string['localewin'] = 'French_France.1252';
 $string['localewincharset'] = 'WINDOWS-1252';
@@ -22,6 +23,5 @@ $string['strftimetime'] = '%%H:%%M';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Français';
-$string['thousandsep'] = ''; // thousand separator, set to '' if none, if this is set, must set decpoint
 
 ?>
