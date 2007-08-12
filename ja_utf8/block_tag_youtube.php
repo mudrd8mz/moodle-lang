64dfa6877377e441869cf14c9db6d942e468b9ad
Mitsuhiro Yoshida <mits>
2007-08-12 21:52:25 +0000

    Translated new strings for question. Adding a new  translated lang file for youtube MDL-10169.

---
+++ ja_utf8/block_tag_youtube.php
@@ -0,0 +1,23 @@
+<?PHP // $Id$ 
+      // block_tag_youtube.php - created with Moodle 1.9 dev (2007081001)
+
+
+$string['anycategory'] = 'すべてのカテゴリ';
+$string['autosvehicles'] = '自動車と乗り物';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'カテゴリ';
+$string['comedy'] = 'コメディ';
+$string['configtitle'] = 'タイトル';
+$string['entertainment'] = 'エンターテイメント';
+$string['filmsanimation'] = '映画とアニメ';
+$string['gadgetsgames'] = 'ゲームとガジェット';
+$string['howtodiy'] = 'ハウツーDIY';
+$string['music'] = '音楽';
+$string['newspolitics'] = 'ニュースと政治';
+$string['numberofvideos'] = 'ビデオ数';
+$string['peopleblogs'] = 'ブログと人';
+$string['petsanimals'] = 'ペットと動物';
+$string['sports'] = 'スポーツ';
+$string['travel'] = '旅行と訪問スポット';
+
+?>
