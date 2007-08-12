4ad4106a78ad3ac74f78a79de2b1eabac5e4df0e
Mitsuhiro Yoshida <mits>
2007-08-12 23:25:20 +0000

    Adding a new translated lang file for Flickr MDL-10169.

---
+++ ja_utf8/block_tag_flickr.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // block_tag_flickr.php - created with Moodle 1.9 dev (2007081001)
+
+
+$string['blockname'] = 'Flickr';
+$string['configtitle'] = 'タイトル';
+$string['date-posted-asc'] = '投稿日の昇順';
+$string['date-posted-desc'] = '投稿日の降順';
+$string['date-taken-asc'] = '撮影日の昇順';
+$string['date-taken-desc'] = '撮影日の降順';
+$string['defaulttile'] = 'Flickr';
+$string['interestingness-asc'] = '興味の昇順';
+$string['interestingness-desc'] = '興味の降順';
+$string['numberofphotos'] = '写真数';
+$string['relevance'] = '妥当性';
+$string['sortby'] = '並び替え';
+
+?>
