bfefa050c64acf4bfb404cd28736af91a33b9e27
Nicolas Martignoni <martignoni>
2007-08-13 07:53:28 +0000

    Added new blocks for flickr and youtube to tags  (thanks, Luiz!)   MDL-10169

---
+++ fr_utf8/block_tag_flickr.php
@@ -0,0 +1,18 @@
+<?php // $Id$
+
+$string['blockname'] = 'Flickr';
+$string['configtitle'] = 'Titre';
+$string['getfromphotoset'] = 'Obtenir les photos du photoset d\'identifiant';
+$string['date-posted-asc'] = 'Date d\'importation &#8593;';
+$string['date-posted-desc'] = 'Date d\'importation &#8595;';
+$string['date-taken-asc'] = 'Date de prise de vue &#8593;';
+$string['date-taken-desc'] = 'Date de prise de vue &#8595;';
+$string['defaulttile'] = 'Flickr';
+$string['includerelatedtags'] = 'Inclure les balises dans la recherche';
+$string['interestingness-asc'] = 'Intérêt &#8593;';
+$string['interestingness-desc'] = 'Intérêt &#8595;';
+$string['numberofphotos'] = 'Nombre de photos';
+$string['relevance'] = 'Relevance';
+$string['sortby'] = 'Trier par';
+                                   
+?>
---
+++ fr_utf8/block_tag_youtube.php
@@ -0,0 +1,22 @@
+<?php // $Id$
+
+$string['blockname'] = 'Youtube';
+$string['configtitle'] = 'Titre';
+$string['numberofvideos'] = 'Nombre de vidéos';
+$string['category'] = 'Catégorie';
+$string['anycategory'] = 'Toute catégorie';
+$string['includeonlyvideosfromplaylist'] = 'N\'inclure que les vidéos de la liste de lecture d\'identifiant';
+$string['filmsanimation'] = 'Films & animations';
+$string['autosvehicles'] = 'Autos & véhicules';
+$string['comedy'] = 'Comédie';
+$string['entertainment'] = 'Entertainment';
+$string['music'] = 'Musique';
+$string['newspolitics'] = 'Nouvelles & politique';
+$string['peopleblogs'] = 'Personnes & blogs';
+$string['petsanimals'] = 'Animaux domestiques & autre animaux';
+$string['howtodiy'] = 'Instructions & bricolage';
+$string['sports'] = 'Sports';
+$string['travel'] = 'Voyages & Lieux';
+$string['gadgetsgames'] = 'Gadgets & jeux';
+
+?>
