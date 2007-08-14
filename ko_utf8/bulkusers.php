7679347872ff670b6236a8ed0aa00d3862451627
Lee Chan-young <yogibear270>
2007-08-14 06:52:51 +0000

    new files

---
+++ ko_utf8/bulkusers.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // bulkusers.php - created with Moodle 1.9 dev (2007081001)
+
+
+$string['action'] = '활동';
+$string['addall'] = '모두 추가';
+$string['addsel'] = '선택항목 추가';
+$string['filtered'] = '걸러짐';
+$string['removeall'] = '모두 제외';
+$string['removesel'] = '선택항목 제외';
+$string['selected'] = '선택됨';
+$string['usersinlist'] = '사용자 목록';
+
+?>
---
+++ ko_utf8/filters.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // filters.php - created with Moodle 1.9 dev (2007081001)
+
+
+$string['actfilterhdr'] = '활성화된 필터';
+$string['addfilter'] = '필터 추가';
+$string['anycategory'] = '모든 범주';
+$string['anycourse'] = '모든 배움터';
+$string['anyfield'] = '모든 항목';
+$string['anyrole'] = '모든 역할';
+$string['anyvalue'] = '모든 값';
+$string['categoryrole'] = '범주관리 역활';
+$string['contains'] = '포함';
+$string['courserole'] = '배움터 역할';
+$string['doesnotcontain'] = '제외';
+$string['endswith'] = '끝말';
+$string['isanyvalue'] = '모두';
+$string['isdefined'] = '정의 됨';
+$string['isempty'] = '비었음';
+$string['isequalto'] = '동일';
+$string['isnotdefined'] = '정의 않됨';
+$string['isnotequalto'] = '다름';
+$string['newfilter'] = '새로운 필터';
+$string['removeall'] = '모두 제거';
+$string['removeselected'] = '선택사항 제거';
+$string['setfilter'] = '필터 설정';
+$string['startswith'] = '시작';
+
+?>
