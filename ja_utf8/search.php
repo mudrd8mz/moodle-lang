12f1b4b79c9ab67b58559d9dfcca287eaac7fe2f
Mitsuhiro Yoshida <mits>
2007-08-16 03:51:56 +0000

    Adding a new translated lang file for search.

---
+++ ja_utf8/search.php
@@ -0,0 +1,32 @@
+<?PHP // $Id$ 
+      // search.php - created with Moodle 1.9 Beta (2007081500)
+
+
+$string['advancedsearch'] = '検索オプション';
+$string['all'] = 'すべて';
+$string['author'] = '著者';
+$string['authorname'] = '著者名';
+$string['createanindex'] = 'インデックスを作成する。';
+$string['database'] = 'データベース';
+$string['datadirectory'] = 'データディレクトリ';
+$string['documents'] = 'ドキュメント';
+$string['enteryoursearchquery'] = 'キーワードを入力してください。';
+$string['errors'] = 'エラー';
+$string['ittook'] = '検索時間:';
+$string['noindexmessage'] = '管理: 検索インデックスが作成されていないようです。';
+$string['normalsearch'] = '標準検索';
+$string['search'] = '検索';
+$string['searching'] = '検索 ...';
+$string['seconds'] = '秒';
+$string['statistics'] = '統計';
+$string['thesewordsmustappear'] = '次の文字を含む';
+$string['thesewordsmustnotappear'] = '次の文字を含まない';
+$string['title'] = 'タイトル';
+$string['tofetchtheseresults'] = '&nbsp;';
+$string['totalsize'] = '合計サイズ';
+$string['type'] = 'タイプ';
+$string['versiontoolow'] = '申し訳ございません、検索にはPHP 5.0.0またはそれ以降のバージョンが必要です。';
+$string['whichmodulestosearch?'] = 'どのモジュールを検索しますか?';
+$string['wordsintitle'] = '次の文字をタイトルに含む';
+
+?>
