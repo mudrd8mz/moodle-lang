6db20dd7ee4f975e612a994462432c5a69c97f32
Mitsuhiro Yoshida <mits>
2007-08-19 05:08:02 +0000

    Translated new strings for global search.

--- ja_utf8/search.php
+++ ja_utf8/search.php
@@ -1,24 +1,38 @@
 <?PHP // $Id$ 
-      // search.php - created with Moodle 1.9 Beta (2007081500)
+      // search.php - created with Moodle 1.9 Beta + (2007081600)
 
 
 $string['advancedsearch'] = '検索オプション';
 $string['all'] = 'すべて';
 $string['author'] = '著者';
 $string['authorname'] = '著者名';
+$string['back'] = '戻る';
+$string['checkdb'] = 'データベースのチェック';
+$string['checkdir'] = 'ディレクトリのチェック';
 $string['createanindex'] = 'インデックスを作成する。';
 $string['database'] = 'データベース';
 $string['datadirectory'] = 'データディレクトリ';
+$string['deletionsinindex'] = 'インデックスの削除';
 $string['documents'] = 'ドキュメント';
+$string['documentsindatabase'] = 'データベースのドキュメント';
+$string['documentsinindex'] = 'インデックスのドキュメント';
 $string['enteryoursearchquery'] = 'キーワードを入力してください。';
 $string['errors'] = 'エラー';
+$string['filesinindexdirectory'] = 'インデックスディレクトリのファイル';
 $string['ittook'] = '検索時間:';
+$string['next'] = '次へ';
 $string['noindexmessage'] = '管理: 検索インデックスが作成されていないようです。';
 $string['normalsearch'] = '標準検索';
+$string['resultsreturnedfor'] = '件が見つかりました - 検索キーワード:';
+$string['runindexer'] = 'インデックサ (実) の実行';
+$string['runindexertest'] = 'インデックサテストの実行';
+$string['score'] = 'スコア';
 $string['search'] = '検索';
 $string['searching'] = '検索 ...';
 $string['seconds'] = '秒';
+$string['solutions'] = 'ソリューション';
 $string['statistics'] = '統計';
+$string['thesewordshelpimproverank'] = '次の文字が検索ランクを改善する';
 $string['thesewordsmustappear'] = '次の文字を含む';
 $string['thesewordsmustnotappear'] = '次の文字を含まない';
 $string['title'] = 'タイトル';
