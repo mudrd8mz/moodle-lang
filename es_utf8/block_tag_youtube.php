5e8b3a5efe76cd3c4f0c6ace9f1d7e5de92b60c4
Benito Arias <barias>
2007-08-22 08:50:56 +0000

    *** empty log message ***

---
+++ es_utf8/block_tag_youtube.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // block_tag_youtube.php - created with Moodle 1.9 Beta + (2007081600)
+      // local modifications from http://127.0.0.1/moodle
+
+
+$string['anycategory'] = 'Cualquier categoría';
+$string['autosvehicles'] = 'Coches y vehículos';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'Categoría';
+$string['comedy'] = 'Comedia';
+$string['configtitle'] = 'Título';
+$string['entertainment'] = 'Entretenimiento';
+$string['filmsanimation'] = 'Películas y animación';
+$string['gadgetsgames'] = 'Artilugios y juegos';
+$string['includeonlyvideosfromplaylist'] = 'Incluir sólo videos de la lista con id';
+$string['music'] = 'Música';
+$string['newspolitics'] = 'Noticias y política';
+$string['numberofvideos'] = 'Número de videos';
+$string['peopleblogs'] = 'Gente y blogs';
+$string['petsanimals'] = 'Animales';
+$string['sports'] = 'Deportes';
+$string['travel'] = 'Viajes y lugares';
+
+?>
---
+++ es_utf8/bulkusers.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // bulkusers.php - created with Moodle 1.9 Beta + (2007081600)
+      // local modifications from http://127.0.0.1/moodle
+
+
+$string['addall'] = 'Agregar todos a la selección';
+$string['addsel'] = 'Agregar a la selección';
+$string['available'] = 'Disponible';
+$string['removeall'] = 'Limpiar selección';
+$string['removesel'] = 'Eliminar de la selección';
+$string['selected'] = 'Seleccionados';
+$string['usersfound'] = '$a usuario(s) encontrado(s).';
+$string['usersinlist'] = 'Usuarios en lista';
+$string['usersselected'] = '$a usuario(s) seleccionado(s).';
+
+?>
