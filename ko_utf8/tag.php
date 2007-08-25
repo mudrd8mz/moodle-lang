e9ed8e33988a50578a6950248df0469c7d2815a5
Lee Chan-young <yogibear270>
2007-08-25 00:11:58 +0000

    translation on added strings

--- ko_utf8/tag.php
+++ ko_utf8/tag.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // tag.php - created with Moodle 1.9 dev (2007081001)
+      // tag.php - created with Moodle 1.9 Beta + (2007082400)
 
 
 $string['addtagtomyinterests'] = '관심분야에 \"$a\" 추가';
+$string['changename'] = '이름 변경';
 $string['count'] = '계수';
 $string['delete'] = '삭제';
 $string['deleted'] = '삭제됨';
@@ -14,6 +15,8 @@ $string['helprelatedtags'] = '관련 태그를 컴마로 분리';
 $string['id'] = 'id';
 $string['managetags'] = '태그 관리';
 $string['name'] = '이름';
+$string['namesalreadybeeingused'] = '이미 쓰인 이름';
+$string['newname'] = '새 이름';
 $string['noresultsfor'] = '\"$a\"에 대한 결과 없음';
 $string['owner'] = '소유자';
 $string['relatedtags'] = '관련 태그';
@@ -31,6 +34,7 @@ $string['tagsaredisabled'] = '태그를 사용할 수 없음';
 $string['thingstaggedwith'] = '\"$a->name\"로 $a->count 항목 검색됨';
 $string['thistaghasnodesc'] = '이 태그에는 설명이 없음';
 $string['timemodified'] = '수정됨';
+$string['updated'] = '갱신됨';
 $string['updatetag'] = '갱신';
 $string['userstaggedwith'] = '사용자가 \"$a\" 로 덧붙임';
 $string['withselectedtags'] = '선택한 태그';
