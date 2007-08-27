ed1b4f8203fa5c310d7ffadc362db17068eec998
Adam Pawelczak <adamtpaw>
2007-08-27 11:25:40 +0000

    update pod v1.9 - niekompletny

--- pl_utf8/access.php
+++ pl_utf8/access.php
@@ -1,16 +1,19 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 1.6 Beta 5 (2006050500)
+      // access.php - created with Moodle 1.9 Beta + (2007082200)
 
 
 $string['access'] = 'Dostępność';
-$string['accesshelp'] = 'Pomoc nt. dostępności ';
+$string['accesshelp'] = 'Pomoc nt. dostępności';
 $string['accesskey'] = 'Klucz dostępu, $a';
 $string['accessstatement'] = 'Orzeczenie dostępności';
-$string['breadcrumb'] = 'Ślad ';
+$string['activitynext'] = 'Następna aktywność';
+$string['activityprev'] = 'Poprzednia akywność';
+$string['breadcrumb'] = 'Ślad';
 $string['monthnext'] = 'Następny miesiąc';
 $string['monthprev'] = 'Poprzedni miesiąc';
 $string['showhideblock'] = 'Pokaż lub ukryj blok';
 $string['sitemap'] = 'Mapa witryny';
+$string['skipa'] = 'Pomiń $a';
 $string['skipblock'] = 'Pomiń blok';
 $string['skipnavigation'] = 'Pomiń nawigację';
 $string['tabledata'] = 'Tabela danych, $a';
--- pl_utf8/assignment.php
+++ pl_utf8/assignment.php
@@ -1,39 +1,73 @@
-<?php
-//assigment.php
-//Wydział Inżynierii Produkcji - Instytu Organizacji Systemów Produkcji - www.wip.edu.pl, www.electurer.edu.pl
-$string['allowdeleting'] = "Zezwalaj na kasowanie";
-$string['allowmaxfiles'] = "Maksymalna liczba załadowanych plików";
-$string['allownotes'] = "Zezwalaj na notatki";
-$string['assignment:grade'] = "Oceń zadanie";
-$string['assignment:submit'] = "Zgłoś zadanie";
-$string['assignment:view'] = "Zobacz zadanie";
-$string['confirmdeletefile'] = "Czy jesteś pewien że chcesz skasować plik?<br /><strong>\$a</strong>";
-$string['deletefilefailed'] = "Skasowanie pliku niepowiodło się";
-$string['draft'] = "Wersja robocza";
-$string['emailstudents'] = "Ostrzeżenie emailowe do studentów";
-$string['helpupload'] = "<p>Ten typ zadania pozwala każdemu uczestnikowi załadować jeden lub więcej plików w dowolnym formacie. 
+<?PHP // $Id$ 
+      // assignment.php - created with Moodle 1.9 Beta + (2007082200)
+
+
+$string['allowdeleting'] = 'Zezwalaj na kasowanie';
+$string['allowmaxfiles'] = 'Maksymalna liczba załadowanych plików';
+$string['allownotes'] = 'Zezwalaj na notatki';
+$string['allowresubmit'] = 'Pozolić na powtórne dostarczenie';
+$string['assignment:grade'] = 'Oceń zadanie';
+$string['assignment:submit'] = 'Zgłoś zadanie';
+$string['assignment:view'] = 'Zobacz zadanie';
+$string['assignmentdetails'] = 'Szczegóły zadnia';
+$string['assignmentmail'] = '$a->teacher wyłał(a) informację zwrotną dotyczącą Twojego przesłanego zadania dla \'$a->assignment\'
+
+Możesz zobaczyć ją. Jest dołączona do wysłanego przez Ciebie zadania $a->url';
+$string['assignmentmailhtml'] = '$a->teacher wysłał(a) informację zwrotną dotyczącą Twojego przesłanego zadania dla \'<i>$a->assignment</i>\'<br /><br />
+Możesz zobaczyć ją. Jest dołączona do wysłanego przez Ciebie zadania <a href=\"$a->url\">przesłane zadanie</a>.';
+$string['assignmentname'] = 'Nazwa zadania';
+$string['assignmenttype'] = 'Typ zadania';
+$string['availabledate'] = 'Dostępne z';
+$string['comment'] = 'Komentarz';
+$string['configmaxbytes'] = 'Domyślny maksymalny rozmiar wszystkich zadań w serwisie (możliwy do ograniczenia na poziomie kursu bądź w innych ustawieniach lokalnych)';
+$string['confirmdeletefile'] = 'Czy jesteś pewien że chcesz skasować plik?<br /><strong>$a</strong>';
+$string['deletefilefailed'] = 'Skasowanie pliku niepowiodło się';
+$string['description'] = 'Opis';
+$string['draft'] = 'Wersja robocza';
+$string['duedate'] = 'Wymagane do';
+$string['duedateno'] = 'Brak terminu przesłania';
+$string['early'] = '$a wcześnie';
+$string['editmysubmission'] = 'Zmodyfikuj moją przesłaną pracę';
+$string['emailstudents'] = 'Ostrzeżenie emailowe do studentów';
+$string['emailteachermail'] = '$a->username zmodyfikował(a) swoją przesłana wcześniejpracę do \'$a->assignment\'
+
+Jest ona dostępna tutaj:
+
+$a->url';
+$string['helpupload'] = '<p>Ten typ zadania pozwala każdemu uczestnikowi załadować jeden lub więcej plików w dowolnym formacie. 
 	Mogą to być pliki edytora tekstowego, obrazki, spakowana strona internetowa lub cokolwiek o czego zgłoszenie poprosisz.</p>
 <p>Ten typ umożliwia także załadowanie wieli plików odpowiedzi. Pliki odpowiedzi mogą zostać załadowane przed zgłoszeniem co może 
 być wykorzystane przy przypisywaniu innych plików do pracy dla każdego uczestnika.</p>
 <p> Uczestnicy mogą także wprowadzać notatki opisujące zgłoszone pliki, stan zaawansowania pracy lub inne dowolne informacje tekstowe.</p>
 <p>Zgłoszenie tego typu zadania musi zostać ręcznie sfinalizowane przez uczestnika. Można zobaczyć obecny stan 
 w dowolnym czasie, nieskończone zadania są oznaczone jako Wersja Wstępna. Można cofnąć każde nieocenione zadanie do stanu wersji 
-wstępnej.</p>";
-$string['hideintro'] = "Ukryj opis przed nastaniem daty dostępności";
-$string['nofiles'] = "Żaden plik nie został zgłoszony";
-$string['nofilesyet'] = "Jeszcze nie zgłoszono plików";
-$string['notavailableyet'] = "Przepraszamy, to zadanie jeszcze nie jest dostępne.<br />Instrukcje do zadania zostaną wyświetlone w dniu podanym poniżej.";
-$string['notes'] = "Notatki";
-$string['notesempty'] = "Brak wpisu";
-$string['notesupdateerror'] = "Błąd podczas aktualizacji notatki";
-$string['onceassignmentsent'] = "Po wysłaniu zadania do oceny, nie będzie możliwości skasowania lub dołączenia plików. Czy chcesz kontynuować?";
-$string['responsefiles'] = "Pliki odpowiedzi";
-$string['reviewed'] = "Przejrzane";
-$string['sendformarking'] = "Wyślij do oceny";
-$string['submissiondraft'] = "Wersja robocza zgłoszenia";
-$string['submitedformarking'] = "Zadanie zostało już zgłoszone do oceny i nie może być zaktualizowane.";
-$string['submitformarking'] = "Ostateczne zgłoszenie do oceny zadania";
-$string['submittedfiles'] = "Zgłoszone pliki";
-$string['typeupload'] = "Zaawansowane ładowanie plików";
-$string['unfinalize'] = "Powrót do wersji roboczej";
+wstępnej.</p>';
+$string['hideintro'] = 'Ukryj opis przed nastaniem daty dostępności';
+$string['nofiles'] = 'Żaden plik nie został zgłoszony';
+$string['nofilesyet'] = 'Jeszcze nie zgłoszono plików';
+$string['notavailableyet'] = 'Przepraszamy, to zadanie jeszcze nie jest dostępne.<br />Instrukcje do zadania zostaną wyświetlone w dniu podanym poniżej.';
+$string['notes'] = 'Notatki';
+$string['notesempty'] = 'Brak wpisu';
+$string['notesupdateerror'] = 'Błąd podczas aktualizacji notatki';
+$string['onceassignmentsent'] = 'Po wysłaniu zadania do oceny, nie będzie możliwości skasowania lub dołączenia plików. Czy chcesz kontynuować?';
+$string['responsefiles'] = 'Pliki odpowiedzi';
+$string['reviewed'] = 'Przejrzane';
+$string['sendformarking'] = 'Wyślij do oceny';
+$string['submissiondraft'] = 'Wersja robocza zgłoszenia';
+$string['submitedformarking'] = 'Zadanie zostało już zgłoszone do oceny i nie może być zaktualizowane.';
+$string['submitformarking'] = 'Ostateczne zgłoszenie do oceny zadania';
+$string['submitted'] = 'Dostarczone';
+$string['submittedfiles'] = 'Zgłoszone pliki';
+$string['typeoffline'] = 'Aktywność offline';
+$string['typeonline'] = 'Tekst online';
+$string['typeupload'] = 'Zaawansowane ładowanie plików';
+$string['typeuploadsingle'] = 'Prześlij pojedyńczy plik,.';
+$string['unfinalize'] = 'Powrót do wersji roboczej';
+$string['uploadbadname'] = 'Ta nazwa pliku zawierała dziwne znaki i nie może być przesłana';
+$string['uploadedfiles'] = 'przesłane pliki';
+$string['uploadsuccess'] = 'Przesłano \'$a\'.';
+$string['viewfeedback'] = 'Zobacz oceny zadań i informacje zwrotne';
+$string['viewsubmissions'] = 'Zobacz $a wysłanych zadań';
+$string['yoursubmission'] = 'Twoja wysyłka';
+
 ?>
