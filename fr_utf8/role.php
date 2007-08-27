aa6598421420ca0b981c1345cff1cb9f0f4443c0
Nicolas Martignoni <martignoni>
2007-08-27 16:21:28 +0000

    Fixed one more string   MDL-10989

--- fr_utf8/role.php
+++ fr_utf8/role.php
@@ -68,6 +68,7 @@ $string['grade:hide'] = 'Cacher/afficher les notes ou éléments';
 $string['grade:import'] = 'Importer des notes';
 $string['grade:lock'] = 'Verrouiller les notes ou éléments';
 $string['grade:manage'] = 'Gérer les éléments d\'évaluation';
+$string['grade:manageoutcomes'] = 'Gérer les objectifs d\'évaluation';
 $string['grade:override'] = 'Court-circuiter les notes';
 $string['grade:unlock'] = 'Déverrouiller les notes ou éléments';
 $string['grade:view'] = 'Voir ses propres notes';
