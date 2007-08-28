c0333bea1d796d07d8b18a8e64da84f249d627dd
Mitsuhiro Yoshida <mits>
2007-08-28 17:32:45 +0000

    Translated new strings for bulk user opertaions MDL-10181. Adding a translated help file for bulk user opertaions MDL-10181.

--- ja_utf8/bulkusers.php
+++ ja_utf8/bulkusers.php
@@ -1,13 +1,16 @@
 <?PHP // $Id$ 
-      // bulkusers.php - created with Moodle 1.9 Beta + (2007082000)
+      // bulkusers.php - created with Moodle 1.9 Beta + (2007082801)
 
 
-$string['addall'] = 'すべてを選択に追加する';
-$string['addsel'] = '選択に追加する';
+$string['addall'] = 'すべての利用可能なユーザを追加する';
+$string['addsel'] = '選択した利用可能なユーザを追加する';
 $string['available'] = '利用可能';
-$string['removeall'] = 'すべてを選択から削除する';
-$string['removesel'] = '選択から削除する';
+$string['deleteall'] = 'すべてのユーザをクリアする';
+$string['deletesel'] = '選択したユーザをクリアする';
+$string['removeall'] = 'すべての利用可能なユーザを削除する';
+$string['removesel'] = '選択した利用可能なユーザを削除する';
 $string['selected'] = '選択済み';
+$string['selectedlist'] = '選択済みユーザ一覧 ...';
 $string['usersfound'] = '$a ユーザが見つかりました。';
 $string['usersinlist'] = 'ユーザ一覧';
 $string['usersselected'] = '$a ユーザが選択されました。';
