503fa4c361818ecf76793b0bb76e949bdd4b1037
Nicolas Martignoni <martignoni>
2007-08-28 16:49:05 +0000

    MDL-10181, adding subtracting mechanism for bulk user opertaions, and add some help

--- fr_utf8/bulkusers.php
+++ fr_utf8/bulkusers.php
@@ -1,11 +1,14 @@
 <?php //$Id$
 
-$string['addall'] = 'Tout ajouter à la sélection';
-$string['addsel'] = 'Ajouter à la sélection';
+$string['addall'] = 'Ajouter tous les utilisateurs disponibles';
+$string['addsel'] = 'Ajouter les utilisateurs sélectionnés disponibles';
 $string['available'] = 'Disponible(s)';
-$string['removeall'] = 'Annuler la sélection';
-$string['removesel'] = 'Retirer de la sélection';
+$string['deleteall'] = 'Retirer tous les utilisateurs';
+$string['deletesel'] = 'Retirer les utilisateurs sélectionnés';
+$string['removeall'] = 'Supprimer tous les utilisateurs disponibles';
+$string['removesel'] = 'Supprimer les utilisateurs sélectionnés disponibles';
 $string['selected'] = 'Sélectionné(s)';
+$string['selectedlist'] = 'Liste des utilisateurs sélectionnés...';
 $string['usersfound'] = '$a utilisateur(s) trouvé(s).';
 $string['usersinlist'] = 'Utilisateurs listés';
 $string['usersselected'] = '$a utilisateur(s) sélectionné(s).';
