0f9c6c2d1f85517587089e2a44a41b136e76b853
Nicolas Martignoni <martignoni>
2007-08-31 19:48:19 +0000

    Correct encoding

--- fr_utf8/userkey.php
+++ fr_utf8/userkey.php
@@ -1,14 +1,14 @@
 <?php // $Id$
 
-$string['createnewkey'] = 'Créer une nouvelle clef utilisateur';
-$string['createuserkey'] = 'Créer clef utilisateur';
+$string['createnewkey'] = 'CrÃ©er une nouvelle clef utilisateur';
+$string['createuserkey'] = 'CrÃ©er clef utilisateur';
 $string['deletekeyconfirm'] = 'Voulez-vous vraiment supprimer cette clef utilisateur&nbsp;?';
 $string['edituserkey'] = 'Modifier clef utilisateur';
 $string['keyiprestriction'] = 'Restriction IP de clef';
-$string['keyiprestrictionhelp'] = 'Tapez une adresse IP ou une plage d\'adresses IP qui seront les seules adresses IP qui pourront accéder à ces données. Laissez vide pour désactiver la restriction IP (pas recommandé).';
+$string['keyiprestrictionhelp'] = 'Tapez une adresse IP ou une plage d\'adresses IP qui seront les seules adresses IP qui pourront accÃ©der Ã  ces donnÃ©es. Laissez vide pour dÃ©sactiver la restriction IP (pas recommandÃ©).';
 $string['keymanager'] = 'Gestionnaire de clefs';
 $string['keyvaliduntil'] = 'Clef valable jusqu\'au';
-$string['keyvaliduntilhelp'] = 'Choisir une date optionnelle après laquelle la clef ne sera plus valable (recommandée pour plus de sécurité).';
+$string['keyvaliduntilhelp'] = 'Choisir une date optionnelle aprÃ¨s laquelle la clef ne sera plus valable (recommandÃ©e pour plus de sÃ©curitÃ©).';
 $string['keyvalue'] = 'Valeur de la clef';
 $string['newuserkey'] = 'Nouvelle clef utilisateur';
 $string['userkey'] = 'Clef utilisateur';
