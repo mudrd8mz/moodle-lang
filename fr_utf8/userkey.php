ed96aae94f95f367c18b843bee30b193c889802a
Nicolas Martignoni <martignoni>
2007-09-01 10:19:05 +0000

    MDL-11082 big refactoring in grade export - export tracking and grade formatting still not finished

--- fr_utf8/userkey.php
+++ fr_utf8/userkey.php
@@ -1,11 +1,9 @@
 <?php // $Id$
 
-$string['configenablepublishing'] = 'Active la publication des exportations et importations&nbsp;: il est possible d\'accéder aux notes exportées par une URL, sans avoir nécessité de se connecter au site Moodle. Les notes peuvent être importées en passant par une telle URL (ce qui signifie qu\'un Moodle peut importer les notes d\'un autre Moodle).';
 $string['createnewkey'] = 'Créer une nouvelle clef utilisateur';
 $string['createuserkey'] = 'Créer clef utilisateur';
 $string['deletekeyconfirm'] = 'Voulez-vous vraiment supprimer cette clef utilisateur&nbsp;?';
 $string['edituserkey'] = 'Modifier clef utilisateur';
-$string['enablepublishing'] = 'Activer la publication';
 $string['keyiprestriction'] = 'Restriction IP de clef';
 $string['keyiprestrictionhelp'] = 'Tapez une adresse IP ou une plage d\'adresses IP qui seront les seules adresses IP qui pourront accéder à ces données. Laissez vide pour désactiver la restriction IP (pas recommandé).';
 $string['keymanager'] = 'Gestionnaire de clefs';
