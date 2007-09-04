948cbdab52b3baec7bd771a4655ded438c4aa943
Ethem Evlice <ethem>
2007-09-04 14:06:38 +0000

    new strings

---
+++ tr_utf8/block_mentees.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // block_mentees.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['blockname'] = 'Danışmanlar';
+$string['configtitle'] = 'Blok başlığı';
+$string['leaveblanktohide'] = 'başlığı gizlemek için boş bırakın';
+$string['newmenteesblock'] = '(yeni Danışman bloğu)';
+
+?>
