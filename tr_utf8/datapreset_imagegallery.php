adcb7c3fc6b56f0512728241727acfed5905b64e
Ethem Evlice <ethem>
2007-09-04 14:30:33 +0000

    new strings

---
+++ tr_utf8/datapreset_imagegallery.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // datapreset_imagegallery.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['modulename'] = 'Resim Galerisi';
+
+?>
---
+++ tr_utf8/notes.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['addnewnote'] = 'Yeni not ekle';
+$string['addnewnoteselect'] = 'Hakkında not yazılacak kullanıcıları seçiniz';
+$string['bynameondate'] = 'yazan: $a->name - $a->date';
+$string['content'] = 'İçerik';
+$string['course'] = 'kurs';
+$string['coursenotes'] = 'Kurs notları';
+$string['created'] = 'oluşturulma';
+$string['deleteconfirm'] = 'Bu not silinsin mi?';
+$string['editnote'] = 'Notu düzenle';
+$string['groupaddnewnote'] = 'Hepsi için bir not ekle';
+$string['nocontent'] = 'Not içeriği boş olamaz';
+$string['nonotes'] = 'Bu tip için yazılmış bir not henüz yok';
+$string['note'] = 'Not';
+$string['notes'] = 'Notlar';
+$string['notesnotvisible'] = 'Notları görme izniniz yok';
+$string['nouser'] = 'Bir kullanıcı seçmelisiniz';
+$string['personal'] = 'kişisel';
+$string['personalnotes'] = 'Kişisel notlar';
+$string['publishstate'] = 'Durum';
+$string['site'] = 'site';
+$string['sitenotes'] = 'Site notları';
+$string['unknown'] = 'bilinmiyor';
+
+?>
