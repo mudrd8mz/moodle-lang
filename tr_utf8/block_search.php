935cb1a7e087932ae42b7b4a75c04ed127a84863
Ethem Evlice <ethem>
2007-09-04 14:13:55 +0000

    *** empty log message ***

---
+++ tr_utf8/block_search.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // block_search.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['blockname'] = 'Genel Arama';
+$string['bytes'] = 'byte (sınırsız yapmak için 0)';
+$string['configbuttonlabel'] = 'Buton etiketi';
+$string['configenablefileindexing'] = 'Dosya indexlemeyi etkinleştir';
+$string['configfiletypes'] = 'İşlenecek dosya tiplerei';
+$string['configlimitindexbody'] = 'İndexlenen gövde boyutu sınırı';
+$string['configpdftotextcmd'] = 'Pdftotext komutu yolu';
+$string['configsearchtext'] = 'Metin ara';
+$string['configwordtotextcmd'] = 'Doctotext komutu yolu';
+$string['configwordtotextenv'] = 'MSWord dönüştürücü için ortam ayarı';
+$string['go'] = 'Git!';
+$string['searchmoodle'] = 'Moodle Ara';
+
+?>
