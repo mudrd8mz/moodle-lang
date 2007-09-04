fc86fdeb62ca5147d8d98549812a4c917b1f6206
Ethem Evlice <ethem>
2007-09-04 14:21:25 +0000

    new strings

---
+++ tr_utf8/block_tag_youtube.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // block_tag_youtube.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['anycategory'] = 'Herhangi Kategori';
+$string['autosvehicles'] = 'Otomobiller & Araçlar';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'Kategori';
+$string['comedy'] = 'Komedi';
+$string['configtitle'] = 'Başlık';
+$string['entertainment'] = 'Eğlence';
+$string['filmsanimation'] = 'Filmler & Çizgi filmler';
+$string['gadgetsgames'] = 'Faydalı Araçlar & Oyunlar';
+$string['howtodiy'] = 'Nasıl yapılır & DIY';
+$string['music'] = 'Müzik';
+$string['newspolitics'] = 'Haber & Politika';
+$string['numberofvideos'] = 'Video sayısı';
+$string['peopleblogs'] = 'Kişiler & Bloglar';
+$string['petsanimals'] = 'Hayvanlar';
+$string['sports'] = 'Spor';
+$string['travel'] = 'Yolculuk & Yerler';
+
+?>
