1e2417f4ca2808bd7a82e9bd1ad00ad3d553f0b5
Ethem Evlice <ethem>
2007-09-04 14:37:41 +0000

    new strings

---
+++ tr_utf8/search.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // search.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['advancedsearch'] = 'Gelişmiş arama';
+
+?>
---
+++ tr_utf8/userkey.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // userkey.php - created with Moodle 1.9 Beta + (2007083101)
+      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+
+
+$string['createnewkey'] = 'Yeni bir kullanıcı anahtarı oluştur';
+$string['createuserkey'] = 'Kullanıcı anahtarı oluştur';
+$string['deletekeyconfirm'] = 'Bu kullanıcı anahtarını gerçekten silmek istediğinizden emin misin?';
+$string['edituserkey'] = 'kullanıcı anahtarı düzenle';
+$string['keyiprestriction'] = 'Anahtar IP kısıtlaması';
+$string['keyiprestrictionhelp'] = 'Bu veriye erişecek belirli bir IP adresi veya IP adres aralığı giriniz. IP kısıtlaması istemiyorsanız boş bırakın (tavsiye edilmez).';
+$string['keymanager'] = 'Anahtar yöneticisi';
+$string['keyvaliduntil'] = 'Anahtar geçerliliği';
+$string['keyvaliduntilhelp'] = 'Anahtarın geçersiz sayılacağı isteğe bağlı bir tarih seçin (eksta güvenlik için tavsiye edilir).';
+$string['keyvalue'] = 'Anahtar değeri';
+$string['newuserkey'] = 'Yeni kullanıcı anahtarı';
+$string['userkey'] = 'Kullanıcı anahtarı';
+$string['userkeys'] = 'Kullanıcı anahtarları';
+
+?>
