995a30e5f0204564dad3f03b669843175c48e144
Mitsuhiro Yoshida <mits>
2007-09-04 17:50:10 +0000

    Translated new strings for question MDL-10812.

--- ja_utf8/question.php
+++ ja_utf8/question.php
@@ -1,12 +1,16 @@
 <?PHP // $Id$ 
-      // question.php - created with Moodle 1.9 Beta + (2007082400)
+      // question.php - created with Moodle 1.9 Beta + (2007083101)
 
 
 $string['adminreport'] = 'あなたの問題データベースで可能性のある不具合をレポートします。';
+$string['broken'] = 'これは存在しないファイルをポイントする「壊れたリンク」です。';
+$string['byandon'] = '<em>$a->time</em> (ユーザ: <em>$a->user</em>)';
 $string['categorycurrent'] = '現在のカテゴリ';
 $string['categorycurrentuse'] = 'このカテゴリを使用する';
 $string['categorydoesnotexist'] = 'このカテゴリは存在しません。';
 $string['categorymoveto'] = 'カテゴリに保存する';
+$string['created'] = '作成日時';
+$string['createdmodifiedheader'] = '作成日時/最終更新日時';
 $string['defaultfor'] = '$a のデフォルト';
 $string['defaultinfofor'] = 'コンテクスト「 $a 」で共有される問題のデフォルトカテゴリ';
 $string['editingcategory'] = 'カテゴリの編集';
@@ -14,6 +18,8 @@ $string['editingquestion'] = '問題の編集';
 $string['erroraccessingcontext'] = 'コンテクストにアクセスできません。';
 $string['errorfilecannotbecopied'] = 'エラー: ファイル $a をコピーできません。';
 $string['errorfilecannotbemoved'] = 'エラー: ファイル $a を移動できません。';
+$string['errorfileschanged'] = 'エラー: 問題からリンクされているファイルがフォームが表示された後、変更されました。';
+$string['exportcategory'] = 'カテゴリのエクスポート';
 $string['filesareacourse'] = 'コースファイルエリア';
 $string['filesareasite'] = 'サイトファイルエリア';
 $string['filestomove'] = '$a にファイルを移動/コピーしますか?';
@@ -25,12 +31,15 @@ $string['linkedfiledoesntexist'] = 'リンクファイル $a がありません�
 $string['makechildof'] = '「 $a 」の子にする';
 $string['maketoplevelitem'] = 'トップレベルに移動する';
 $string['missingimportantcode'] = 'この問題タイプには、重要なコードがありません: $a';
+$string['modified'] = '最終更新日時';
 $string['movecategory'] = 'カテゴリを移動する';
 $string['moveq'] = '問題を移動する';
 $string['moveqtoanothercontext'] = '問題を他のコンテクストに移動する';
 $string['movingcategory'] = 'カテゴリの移動';
+$string['movingcategoryandfiles'] = '本当にカテゴリ「 {$a->name} 」およびすべての子カテゴリを「 {$a->contextto} 」のコンテクストに移動してもよろしいですか?<br /> {$a->urlcount} のファイルが {$a->fromareaname} の問題よりリンクされているようです。これらのファイルを {$a->toareaname} にコピーまたは移動してもよろしいですか?';
 $string['movingcategorynofiles'] = '本当にカテゴリ「 {$a->name} 」およびすべての子カテゴリを「 {$a->contextto} 」のコンテクストに移動してもよろしいですか?';
 $string['movingquestions'] = '問題およびすべてのファイルの移動';
+$string['movingquestionsandfiles'] = '本当に問題 {$a->questions} を<strong>「 {$a->tocontext} 」</strong>のコンテクストに移動してもよろしいですか?<br /> {$a->urlcount} のファイルが {$a->fromareaname} の問題よりリンクされているようです。これらのファイルを {$a->toareaname} にコピーまたは移動してもよろしいですか?';
 $string['movingquestionsnofiles'] = '本当に問題 {$a->questions} を<strong>「 {$a->tocontext} 」</strong>のコンテクストに移動してもよろしいですか?<br />{$a->fromareaname} の問題からリンクされているファイルは<strong>ありません</strong>。';
 $string['needtochoosecat'] = 'このカテゴリを移動するにはカテゴリを選択してください。または「キャンセル」をクリックしてください。';
 $string['nopermissionadd'] = 'あなたには問題をここに追加するパーミッションがありません。';
@@ -40,6 +49,7 @@ $string['notenoughdatatomovequestions'] = 'あなたが移動したい問題の�
 $string['permissionedit'] = 'この問題を編集する';
 $string['permissionmove'] = 'この問題を移動する';
 $string['permissionsaveasnew'] = 'この問題を新しい問題として保存する';
+$string['permissionto'] = 'あなたのパーミッションは下記のとおりです:';
 $string['published'] = '共有';
 $string['questionbank'] = '問題バンク';
 $string['questioncatsfor'] = '「 $a 」の問題カテゴリ';
