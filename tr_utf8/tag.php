d1b919ec5a527a2b21d2915ef44bc55692424559
Ethem Evlice <ethem>
2007-09-05 08:49:02 +0000

    new strings

---
+++ tr_utf8/tag.php
@@ -0,0 +1,48 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 1.9 Beta + (2007083101)
+
+
+$string['addtagtomyinterests'] = 'İlgi alanlarıma ekle: \"$a\"';
+$string['blocktagstitle'] = 'Etiketler';
+$string['changename'] = 'Etiket adını değiştir';
+$string['changetype'] = 'Etiket tipini değiştir';
+$string['count'] = 'Sayaç';
+$string['delete'] = 'Sil';
+$string['deleted'] = 'Silindi';
+$string['description'] = 'Açıklama';
+$string['edittag'] = 'Bu etiketi düzenle';
+$string['flag'] = 'İşaret';
+$string['flagasinappropriate'] = 'Uygunsuz olarak işaretle';
+$string['helprelatedtags'] = 'Virgülle ayrılmış ilişkili etiketler';
+$string['id'] = 'id';
+$string['managetags'] = 'Etiketleri yönet';
+$string['name'] = 'Etiket adı';
+$string['namesalreadybeeingused'] = 'Etiket isimleri zaten kullanılıyor';
+$string['newname'] = 'Yeni etiket adı';
+$string['noresultsfor'] = 'Sonuç yok: \"$a\"';
+$string['owner'] = 'Sahibi';
+$string['relatedblogs'] = 'Bu etiketle işaretli son bloglar';
+$string['relatedtags'] = 'İlişkili etiketler';
+$string['reset'] = 'Temizle';
+$string['resetflag'] = 'İşareti kaldır';
+$string['responsiblewillbenotified'] = 'Sorumlu kişi bilgilendirilecek';
+$string['search'] = 'Ara';
+$string['searchresultsfor'] = 'Arama sonuçları: \"$a\"';
+$string['searchtags'] = 'Etiketleri ara';
+$string['seeallblogs'] = 'Bu etiketle işaretli bütün bloglara bak';
+$string['select'] = 'Seç';
+$string['tag'] = 'Etiket';
+$string['tagdescription'] = 'Etiket açıklaması';
+$string['tags'] = 'Etiketler';
+$string['tagsaredisabled'] = 'Etiketler etkin değil';
+$string['tagtype'] = 'Etiket tipi';
+$string['thingstaggedwith'] = '\"$a->name\" ismiyle $a->count kayıt var';
+$string['thistaghasnodesc'] = 'Bu etiketin açıklaması henüz yok.';
+$string['timemodified'] = 'Düzenleme';
+$string['typechanged'] = 'Etiket tipi değiştirildi';
+$string['updated'] = 'Güncelleme';
+$string['updatetag'] = 'Güncelle';
+$string['userstaggedwith'] = '\"$a\" etiketli kullanıcılar';
+$string['withselectedtags'] = 'Seçilen etiketlerle...';
+
+?>
