52e7959c3a83132d98a27c99fd383bcc8831d048
Ethem Evlice <ethem>
2007-09-05 09:12:55 +0000

    new strings

---
+++ tr_utf8/filters.php
@@ -0,0 +1,33 @@
+<?PHP // $Id$ 
+      // filters.php - created with Moodle 1.9 Beta + (2007083101)
+
+
+$string['actfilterhdr'] = 'Etkin süzgeçler (ORed dahil)';
+$string['addfilter'] = 'Süzgeç ekle';
+$string['anycategory'] = 'herhangi kategori';
+$string['anycourse'] = 'herhangi kurs';
+$string['anyfield'] = 'herhangi alan';
+$string['anyrole'] = 'herhangi rol';
+$string['anyvalue'] = 'herhangi değer';
+$string['categoryrole'] = 'Kategori rolü';
+$string['contains'] = 'içerirse';
+$string['courserole'] = 'Kurs rolü';
+$string['doesnotcontain'] = 'içermezse';
+$string['endswith'] = 'ile biterse';
+$string['firstaccess'] = 'İlk erişim';
+$string['isafter'] = '$a den sonraysa';
+$string['isanyvalue'] = 'herhangi bir değerse';
+$string['isbefore'] = '$a den önceyse';
+$string['isbetween'] = '$a[0]  ve $a[1] arasında';
+$string['isdefined'] = 'tanımlıysa';
+$string['isempty'] = 'boşsa';
+$string['isequalto'] = 'eşitse';
+$string['isnotdefined'] = 'tanımlanmamışsa';
+$string['isnotequalto'] = 'eşit değilse';
+$string['newfilter'] = 'Yeni süzgeç';
+$string['removeall'] = 'Tümünü sil';
+$string['removeselected'] = 'Seçiliyi sil';
+$string['setfilter'] = 'Süzgeç ayarla';
+$string['startswith'] = 'ile başlarsa';
+
+?>
