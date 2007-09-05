8660f280a8f3c2f7f2303861bd0c4405be64019c
Adam Pawelczak <adamtpaw>
2007-09-05 11:40:04 +0000

    for moddle v1.9

--- pl_utf8/choice.php
+++ pl_utf8/choice.php
@@ -1,35 +1,43 @@
 <?PHP // $Id$ 
-      // choice.php - created with Moodle 1.6 development (2005072200)
+      // choice.php - created with Moodle 1.9 Beta + (2007082803)
 
 
 $string['addmorechoices'] = 'Dodaj głosowanie';
 $string['allowupdate'] = 'Zezwól na aktualizację głosowania';
-$string['answered'] = ' z udzieloną odpowiedzią';
+$string['answered'] = 'z udzieloną odpowiedzią';
 $string['choice'] = 'Wybór $a';
+$string['choice:choose'] = 'Zapisz wybór';
+$string['choice:deleteresponses'] = 'Usuń odpowiedzi';
+$string['choice:downloadresponses'] = 'Wczytaj odpowiedzi';
+$string['choice:readresponses'] = 'Przeczytaj odpowiedzi';
 $string['choiceclose'] = 'Do';
-$string['choicename'] = ' Nazwa głosowania';
+$string['choicename'] = 'Nazwa głosowania';
 $string['choiceopen'] = 'Otwórz';
-$string['choicetext'] = ' Treść pytania';
+$string['choicetext'] = 'Treść pytania';
 $string['displayhorizontal'] = 'Wyświetl w poziomie';
 $string['displaymode'] = 'Opcje wyświetlania';
 $string['displayvertical'] = 'Wyświetl w pionie';
+$string['fillinatleastoneoption'] = 'Musisz wskazać co najmniej dwie z możliwych odpowiedzi.';
 $string['full'] = '(pełne)';
 $string['havetologin'] = 'Musisz się zalogować zanim odpowiesz';
 $string['limit'] = 'Liczba możliwych głosów';
 $string['limitanswers'] = 'Ogranicz liczbę możliwych odpowiedzi';
-$string['modulename'] = ' Głosowanie';
-$string['modulenameplural'] = ' Głosowania';
+$string['modulename'] = 'Głosowanie';
+$string['modulenameplural'] = 'Głosowania';
 $string['mustchooseone'] = 'Musisz wybrać odpowiedź przed zapisaniem. W tej chwili nic nie zostało zapisane';
-$string['notanswered'] = ' Jeszcze nie udzielono odpowiedzi';
+$string['noguestchoose'] = 'Niestety, goście nie są uprawnieni do odpowiedzi';
+$string['noresultsviewable'] = 'Rezultatny nie mogą być obecnie przeglądane';
+$string['notanswered'] = 'Jeszcze nie udzielono odpowiedzi';
 $string['notopenyet'] = 'Niestety ten moduł jest nieczynny do $a';
 $string['privacy'] = 'Prywatność wyników';
-$string['publish'] = ' Opublikuj wyniki';
+$string['publish'] = 'Opublikuj wyniki';
 $string['publishafteranswer'] = 'Pokaż wyniki studentom po ich odpowiedzi';
 $string['publishafterclose'] = 'Pokaż wyniki studentom tylko po zamkniętu głosowania';
 $string['publishalways'] = 'Zawsze pokazuj wyniki studentom';
 $string['publishanonymous'] = 'Opublikuj wyniki anonimowo, bez pokazywania imion studentów';
 $string['publishnames'] = 'Opublikuj wyniki w pełni, pokazując imiona studentów i ich wybory';
 $string['publishnot'] = 'Nie publikuj wyników studentom';
+$string['removemychoice'] = 'Usuń mój wybór';
 $string['responses'] = 'Odpowiedzi';
 $string['responsesto'] = 'Odpowiedzi na $a';
 $string['savemychoice'] = 'Zapisz mój wybór';
@@ -38,7 +46,7 @@ $string['spaceleft'] = 'Ilość wolnego miejsca';
 $string['spacesleft'] = 'Ilość wolnych miejsc';
 $string['taken'] = 'Weź';
 $string['timerestrict'] = 'Ogranicz odpowiadanie do tego czasu';
-$string['viewallresponses'] = ' Przejrzyj $a odpowiedzi';
+$string['viewallresponses'] = 'Przejrzyj $a odpowiedzi';
 $string['yourselection'] = 'Twój wybór';
 
 ?>
