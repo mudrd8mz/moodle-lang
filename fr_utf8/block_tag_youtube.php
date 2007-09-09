756afe13932b00dbd7ba40c36e3fb9128c5162d9
Nicolas Martignoni <martignoni>
2007-09-09 16:06:46 +0000

    MDL-11158, "& needs fix to &amp; in ... for 'en' and other language packs"

--- fr_utf8/block_tag_youtube.php
+++ fr_utf8/block_tag_youtube.php
@@ -6,17 +6,17 @@ $string['numberofvideos'] = 'Nombre de vidéos';
 $string['category'] = 'Catégorie';
 $string['anycategory'] = 'Toute catégorie';
 $string['includeonlyvideosfromplaylist'] = 'N\'inclure que les vidéos de la liste de lecture d\'identifiant';
-$string['filmsanimation'] = 'Films & animations';
-$string['autosvehicles'] = 'Autos & véhicules';
+$string['filmsanimation'] = 'Films &amp; animations';
+$string['autosvehicles'] = 'Autos &amp; véhicules';
 $string['comedy'] = 'Comédie';
 $string['entertainment'] = 'Entertainment';
 $string['music'] = 'Musique';
-$string['newspolitics'] = 'Nouvelles & politique';
-$string['peopleblogs'] = 'Personnes & blogs';
-$string['petsanimals'] = 'Animaux domestiques & autre animaux';
-$string['howtodiy'] = 'Instructions & bricolage';
+$string['newspolitics'] = 'Nouvelles &amp; politique';
+$string['peopleblogs'] = 'Personnes &amp; blogs';
+$string['petsanimals'] = 'Animaux domestiques &amp; autres animaux';
+$string['howtodiy'] = 'Instructions &amp; bricolage';
 $string['sports'] = 'Sports';
-$string['travel'] = 'Voyages & Lieux';
-$string['gadgetsgames'] = 'Gadgets & jeux';
+$string['travel'] = 'Voyages &amp; Lieux';
+$string['gadgetsgames'] = 'Gadgets &amp; jeux';
 
 ?>
