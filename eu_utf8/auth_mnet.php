ea7b1e95ba768f29f43956c58d93cbbccb45275e
Abel Camacho <ueu_ueu>
2007-09-10 10:49:28 +0000

    SUEk koordinatutako itzulpena

---
+++ eu_utf8/auth_mnet.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.8.2+ (2007021520)
+
+
+$string['sso_idp_description'] = 'Publika ezazu zerbitzu hau zure erabiltzaileak zure $a Moodle gunera bertan berriz identifikatu gabe joan ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzuan (Zerbitzu-hornitzailea) <strong>harpidetu</strong> beharko duzu $a.</li></ul><br />-n. Harpidetu zerbitzu honetan $a-n autentifikatutako erabiltzaileei zure gunean saioa berriz hasi beharrik izan gabe sartzen uzteko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzua (Zerbitzu-hornitzailea) <strong>publikatu</strong> beharko duzu $a.</li></ul><br />-n.';
+$string['sso_idp_name'] = 'SSO (Nortasun-hornitzailea)';
+$string['sso_mnet_login_refused'] = '$a[0] izeneko erabiltzaileak ezin du $a[1]-tik saioa hasi.';
+$string['sso_sp_description'] = 'Publika ezazu zerbitzu hau autentifikatutako erabiltzaileak zure gunera $a-tik bertan berriz identifikatu gabe sartu ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzuan (Zerbitzu-hornitzailea) <strong>harpidetu</strong> beharko duzu $a.</li></ul><br />-n. Publika ezazu zerbitzu hau zure erabiltzaileak zure $a Moodle gunera bertan berriz identifikatu gabe joan ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzua (Zerbitzu-hornitzailea) <strong>publikatu</strong> beharko duzu $a.</li></ul><br />-n.';
+$string['sso_sp_name'] = 'SSO (Zerbitzu-hornitzailea)';
+
+?>
---
+++ eu_utf8/qtype_randomsamatch.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qtype_randomsamatch.php - created with Moodle 1.8.2+ (2007021520)
+
+
+$string['nosaincategory'] = 'Ez dago erantzun laburreko galderarik aukeratutako \'$a->catname\' kategorian. Aukera ezazu beste kategoria bat eta sartu galdera bat edo beste bertan.';
+$string['notenoughsaincategory'] = '$a->nosaquestions erantzun laburreko galdera bakarrik dago aukeratutako \'$a->catname\' kategorian. Aukeratu beste kategoria bat, sartu bertan galdera bat edo beste edo gutxitu aukeratutako galdera kopurua.';
+
+?>
