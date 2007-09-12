a77d3921ffea2e16d968cbe02d25eb04a5a7a797
Abel Camacho <ueu_ueu>
2007-09-12 07:17:25 +0000

    SUEk koordinatutako itzulpena

---
+++ eu_utf8/question.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // question.php - created with Moodle 1.8.2+ (2007021520)
+
+
+$string['categorydoesnotexist'] = 'Ez dago kategoria hau';
+$string['editingquestion'] = 'Galdera bat editatzen';
+$string['fractionsnomax'] = 'Erantzunetako batek %% 100 izan behar du galdera honetako gehienezko puntuazioa lortu ahal izateko.';
+$string['missingimportantcode'] = 'Galdera-mota honek kode garrantzitsu bat falta du: $a.';
+$string['notenoughdatatoeditaquestion'] = 'Ez da galderaren, kategoriaren eta galdera-motaren id-a zehaztu.';
+$string['questionbank'] = 'Galdera-bankua';
+$string['questiondoesnotexist'] = 'Ez dago galdera hau';
+$string['unknownquestiontype'] = 'Galdera-mota ezezaguna: $a.';
+
+?>
