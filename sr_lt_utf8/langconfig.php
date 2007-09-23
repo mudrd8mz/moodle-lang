254df79dc1dfcd8462c16fad2ed54881254a2cb0
komlenov <komlenov>
2007-09-23 09:21:02 +0000

    Recent updates

--- sr_lt_utf8/langconfig.php
+++ sr_lt_utf8/langconfig.php
@@ -1,9 +1,9 @@
 <?PHP // $Id$ 
-      // langconfig.php - created with Moodle 1.8.2+
+      // langconfig.php - created with Moodle 1.8.1+ (2007021510)
 
-	  
-$string['alphabet'] = 'A,B,C,C,C,D,�,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,�,T,U,V,W,X,Y,Z,�';
-$string['backupnameformat'] = '%%d.%%m.%%Y.-%%H.%%M';
+
+$string['alphabet'] = 'A,B,C,Č,Ć,D,Dž,Đ,E,F,G,H,I,J,K,L,Lj,M,N,Nj,O,P,Q,R,S,Š,T,U,V,W,X,Y,Z,Ž';
+$string['backupnameformat'] = '%%d%%m%%Y-%%H%%M';
 $string['decsep'] = ',';
 $string['firstdayofweek'] = '1';
 $string['listsep'] = ';';
