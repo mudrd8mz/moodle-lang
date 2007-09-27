d9b4c075892a8b6f45ae111319ebaec80fb7e835
Nicolas Martignoni <martignoni>
2007-09-27 18:41:06 +0000

    MDL-10985 merged the xml and xmlurl grade import plug-in

--- fr_utf8/gradeimport_xml.php
+++ fr_utf8/gradeimport_xml.php
@@ -1,5 +1,9 @@
 <?php // $Id$ 
 
+$string['errbadxmlformat'] = 'Erreur - format XML incorrect';
+$string['errduplicateidnumber'] = 'Erreur - identifiant en double';
+$string['errincorrectidnumber'] = 'Erreur - identifiant incorrect';
+$string['fileurl'] = 'URL du fichier distant';
 $string['modulename'] = 'Fichier XML';
 $string['xml:view'] = 'Importer des notes en XML';
 
