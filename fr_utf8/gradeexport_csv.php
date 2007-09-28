e66f9ecaff89c28b16df3f27b70f98dbc771ec95
Nicolas Martignoni <martignoni>
2007-09-28 20:30:24 +0000

    MDL-11451 grade publishing security/privacy improved, first part

--- fr_utf8/gradeexport_csv.php
+++ fr_utf8/gradeexport_csv.php
@@ -1,6 +1,7 @@
 <?php // $Id$ 
 
 $string['modulename'] = 'Fichier CSV';
-$string['cvs:view'] = 'Exporter des notes en CSV';
+$string['cvs:view'] = 'Exporter les notes en CSV';
+$string['cvs:publish'] = 'Publier l\'exportation en CSV des notes';
 
 ?>
