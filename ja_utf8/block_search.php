4b6eb7043b37dfe808eca82cf6aaa7bad1ebc43c
Mitsuhiro Yoshida <mits>
2007-09-29 18:25:41 +0000

    Adding a new translated lang file for global search block MDL-11406.

---
+++ ja_utf8/block_search.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // block_search.php - created with Moodle 1.9 Beta + (2007092806)
+
+
+$string['blockname'] = 'グローバルサーチ';
+$string['bytes'] = 'バイト (0は制限なしを意味します)';
+$string['configbuttonlabel'] = 'ボタンラベル';
+$string['configenablefileindexing'] = 'インデックシングを有効にする';
+$string['configfiletypes'] = '検索対象ファイルタイプ';
+$string['configlimitindexbody'] = 'インデックスボディサイズの制限';
+$string['configpdftotextcmd'] = 'pdftotextコマンドのパス';
+$string['configsearchtext'] = '検索テキスト';
+$string['configwordtotextcmd'] = 'doctotextコマンドのパス';
+$string['configwordtotextenv'] = 'MSWordコンバータの環境設定';
+$string['go'] = 'Go!';
+$string['searchmoodle'] = 'Moodleの検索';
+
+?>
