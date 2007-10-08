fa9e27d0519b42234ab15d617ddf4c2db04f0bb9
Mitsuhiro Yoshida <mits>
2007-10-08 19:42:34 +0000

    Translated new strings for question.

--- ja_utf8/question.php
+++ ja_utf8/question.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // question.php - created with Moodle 1.9 Beta + (2007100500)
+      // question.php - created with Moodle 1.9 Beta + (2007100802)
 
 
 $string['adminreport'] = 'あなたの問題データベースで可能性のある不具合をレポートします。';
@@ -9,9 +9,15 @@ $string['categorycurrent'] = '現在のカテゴリ';
 $string['categorycurrentuse'] = 'このカテゴリを使用する';
 $string['categorydoesnotexist'] = 'このカテゴリは存在しません。';
 $string['categorymoveto'] = 'カテゴリに保存する';
+$string['changepublishstatuscat'] = 'コース内の <a href=\"$a->caturl\">カテゴリ「 $a->name 」</a> の共有ステータスが <strong>$a->changefrom から $a->changeto</strong> に変更されます。';
 $string['copy'] = '$a からコピーおよびリンクを変更する。';
 $string['created'] = '作成日時';
 $string['createdmodifiedheader'] = '作成日時/最終更新日時';
+$string['cwrqpfs'] = 'ランダム問題がサブカテゴリより問題を選択しています。';
+$string['cwrqpfsinfo'] = '<p>Moodle 1.9へのアップグレード中、問題カテゴリは異なるコンテクストに移動されます。あなたのサイトのいくつかの問題カテゴリおよび問題では、共有ステータスを変更する必要があります。これは、(このサイトに関して) 小テストの1つまたはそれ以上の「ランダム」問題が共有および未共有のカテゴリ内から混在して問題を選択するよう設定されている場合に生じる、まれなケースに対処するため必要です。「ランダム」問題がサブカテゴリ群より問題を選択し、ランダム問題が作成される1またはそれ以上のサブカテゴリが親カテゴリに対して異なる共有ステータスを持っている場合、この問題が生じます。</P>
+
+<p>Moodle 1.9へのアップグレード中、「ランダム」問題が選択されている次のカテゴリの親カテゴリは、「ランダム」問題が作成されているカテゴリと同じ共有ステータスに変更されます。次のカテゴリでは、共有ステータスが変更されます。影響を受ける問題は、あなたが小テストより削除するまで、すべての小テストで継続して動作します。</p>';
+$string['cwrqpfsnoprob'] = 'あなたのサイト内で「サブカテゴリから問題を選択しているランダム問題」に関する問題に影響している問題カテゴリはありません。';
 $string['defaultfor'] = '$a のデフォルト';
 $string['defaultinfofor'] = 'コンテクスト「 $a 」で共有される問題のデフォルトカテゴリ';
 $string['donothing'] = 'ファイルをコピーまたは移動またはリンクを変更しない。';
@@ -47,7 +53,7 @@ $string['movingquestionsandfiles'] = '本当に問題 {$a->questions} を<strong
 $string['movingquestionsnofiles'] = '本当に問題 {$a->questions} を<strong>「 {$a->tocontext} 」</strong>のコンテクストに移動してもよろしいですか?<br />{$a->fromareaname} の問題からリンクされているファイルは<strong>ありません</strong>。';
 $string['needtochoosecat'] = 'このカテゴリを移動するにはカテゴリを選択してください。または「キャンセル」をクリックしてください。';
 $string['nopermissionadd'] = 'あなたには問題をここに追加するパーミッションがありません。';
-$string['noprobs'] = 'あなたの問題データベースに問題が見つかりませんでした。';
+$string['noprobs'] = 'あなたの問題データベースに問題は見つかりませんでした。';
 $string['notenoughdatatoeditaquestion'] = '問題ID、カテゴリIDおよび問題タイプが指定されていません。';
 $string['notenoughdatatomovequestions'] = 'あなたが移動したい問題の問題IDを提供してください。';
 $string['permissionedit'] = 'この問題を編集する';
