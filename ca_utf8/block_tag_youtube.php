82ed3622698c40e404e8d17f543fda0051669ec9
Carles Bellver <carlesbellver>
2007-10-08 12:22:21 +0000

    Moodle 1.9...

---
+++ ca_utf8/block_tag_youtube.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // block_tag_youtube.php - created with Moodle 1.9 Beta + (2007092806)
+
+
+$string['anycategory'] = 'Qualsevol categoria';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'Categoria';
+$string['configtitle'] = 'Títol';
+$string['includeonlyvideosfromplaylist'] = 'Inclou només vídeos de la llista de reproducció amb ID';
+$string['numberofvideos'] = 'Nombre de vídeos';
+
+?>
