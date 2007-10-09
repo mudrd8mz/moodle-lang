df455c27ca84d41b83d0eb451c56e8865338ddde
Jaime Villate <villate>
2007-10-09 07:33:49 +0000

    Translated new strings.

--- pt_utf8/form.php
+++ pt_utf8/form.php
@@ -2,9 +2,38 @@
       // form.php - created with Moodle 1.7.1+ (2006101010)
 
 
-$string['denotesreq'] = '$a indica um campo obrigatório.';
+$string['addfields'] = 'Adicionar $a campo(s) ao formulário';
+$string['advancedelement'] = 'Elemento avaçado';
+$string['day'] = 'Dia';
+$string['display'] = 'Mostrar';
+$string['err_alphanumeric'] = 'Aqui pode usar unicamente letras ou números';
+$string['err_email'] = 'Aqui tem que escrever um endereço de correio válido';
+$string['err_lettersonly'] = 'Aqui pode escrever unicamente letras';
+$string['err_maxlength'] = 'Aqui só pode escrever, no máximo, $a->format caracteres.';
+$string['err_minlength'] = 'Aqui deverá escrever, no mínimo, $a->format caracteres.';
+$string['err_nonzero'] = 'Aqui tem que escrever um número que não comece por 0.';
+$string['err_nopunctuation'] = 'Aqui não pode usar símbolos de pontuação.';
+$string['err_numeric'] = 'Aqui tem que escrever um número';
+$string['err_rangelength'] = 'Aqui tem que escrever entre {$a->format[0]} e {$a->format[1]} caracteres';
+$string['err_required'] = 'Aqui deverá dar um valor';
+$string['general'] = 'Geral';
+$string['hideadvanced'] = 'Ocultar opções avançadas';
+$string['hour'] = 'Hora';
+$string['minute'] = 'Minuto';
+$string['miscellaneoussettings'] = 'Configurações diversas';
+$string['modstandardels'] = 'Configurações comuns de módulos';
+$string['month'] = 'Mês';
 $string['nomethodforaddinghelpbutton'] = 'Não existe nenhum método para adicionar um botão de ajuda ao elemento de formulário $a->name (classe $a->classname)';
 $string['nonexistentformelements'] = 'Tentativa de adicionar botões de ajuda a elementos de formulário não existentes: $a';
+$string['optional'] = 'Opcional';
+$string['othersettings'] = 'Outras configurações';
 $string['requiredelement'] = 'Campo obrigatório.';
+$string['revealpassword'] = 'Divulgar';
+$string['security'] = 'Segurança';
+$string['showadvanced'] = 'Mostrar opções avançadas';
+$string['somefieldsrequired'] = 'Há campos obrigatórios neste formulário, assinalados com $a';
+$string['timing'] = 'Sincronização';
+$string['unmaskpassword'] = 'Desmascarar';
+$string['year'] = 'Ano';
 
 ?>
