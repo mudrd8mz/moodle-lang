133ce3365b67064fb04963e1244913972ca4edcc
David Mudrak <david.mudrak@gmail.com>
2007-10-10 21:18:14 +0000

    New strings for 1.9-beta. There is a new term introduced: "tags". This is used across the site as a part of blogs and user profiles. I have translated it as "klicova slova" (which means "keywords" actually) as there is no generally accepted Czech word fot this term. Other possibility might be "znacka". This is to be discussed yet.

--- cs_utf8/access.php
+++ cs_utf8/access.php
@@ -1,13 +1,16 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 1.8.1+ (2007021510)
+      // access.php - created with Moodle 1.9 Beta + (2007100301)
 
 
+$string['access'] = 'Přístupnost';
 $string['accesshelp'] = 'Nápověda k přístupnosti';
 $string['accesskey'] = 'Zkratková klávesa, $a';
 $string['accessstatement'] = 'Prohlášení o přístupnosti';
 $string['activitynext'] = 'Další činnost';
 $string['activityprev'] = 'Předchozí činnost';
 $string['breadcrumb'] = 'Navigační lišta';
+$string['currenttopic'] = 'Aktuální téma';
+$string['currentweek'] = 'Stávající týden';
 $string['monthnext'] = 'Další měsíc';
 $string['monthprev'] = 'Předchozí měsíc';
 $string['showhideblock'] = 'Ukázat nebo skrýt blok';
@@ -20,6 +23,5 @@ $string['tablelayout'] = 'Formátovací tabulka, $a';
 $string['tocontent'] = 'Přejít k hlavnímu obsahu';
 $string['tonavigation'] = 'Přejít k navigaci';
 $string['youarehere'] = 'Nacházíte se zde';
-$string['access'] = 'Přístupnost'; // ORPHANED
 
 ?>
