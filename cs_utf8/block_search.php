f6633487e1f14453cb566d9b9768e9dbfe15819c
David Mudrak <david.mudrak@gmail.com>
2007-10-10 21:41:26 +0000

    Moved from block langspace into the core one. Initial translation.

---
+++ cs_utf8/block_search.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // block_search.php - created with Moodle 1.9 Beta + (2007082000)
+
+
+$string['blockname'] = 'Globální vyhledávání';
+$string['bytes'] = 'bajtů (nula znamená bez limitu)';
+$string['configbuttonlabel'] = 'Popisek tlačítka';
+$string['configenablefileindexing'] = 'Povolit indexování souborů';
+$string['configfiletypes'] = 'Jaké typy souborů prohledávat';
+$string['configlimitindexbody'] = 'Limit velikosti indexovaného souboru';
+$string['configpdftotextcmd'] = 'Cesta k souboru pdftotext';
+$string['configsearchtext'] = 'Hledaný text';
+$string['configwordtotextcmd'] = 'Cesta k souboru doctotext';
+$string['configwordtotextenv'] = 'Nastavení prostředí pro konvertor MSWord';
+$string['go'] = 'Hledej';
+$string['searchmoodle'] = 'Prohledej Moodle';
+
+?>
