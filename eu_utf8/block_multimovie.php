ed040cf410c056943ff285d84ace13e426f7aadb
Abel Camacho <ueu_ueu>
2007-10-11 07:07:02 +0000

    SUEk koordinatutako itzulpena

---
+++ eu_utf8/block_multimovie.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // block_multimovie.php - created with Moodle 1.7.1+ (2006101010)
+
+
+$string['configcontent'] = 'Edukia';
+$string['configtitle'] = 'Izenburua';
+$string['configtype'] = 'Mota';
+$string['leaveblanktohide'] = 'Ezkutatzeko zuriz utzi';
+$string['multimovie'] = 'Multipelikula';
+$string['newmultimovieblock'] = 'Multipelikula bloke berria';
+$string['unconfiguredblock'] = 'Ezarpenik gabeko blokea';
+
+?>
