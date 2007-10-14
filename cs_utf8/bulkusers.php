e75170bb60618a1850b42bad0a9ce48759a7d7bd
David Mudrak <david.mudrak@gmail.com>
2007-10-14 23:17:25 +0000

    Translated some strings. Missing strings will be added after button functionality test. I am not able to test because of MDL-11767

---
+++ cs_utf8/bulkusers.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // bulkusers.php - created with Moodle 1.9 Beta + (2007100902)
+
+
+$string['addall'] = 'Přidat všechny dostupné uživatele';
+$string['addsel'] = 'Přidat vybrané dostupné uživatele';
+$string['available'] = 'Dostupní';
+$string['selected'] = 'Vybraní';
+$string['selectedlist'] = 'Seznam vybraných uživatelů';
+$string['usersfound'] = 'Počet nalezených uživatelů: $a';
+$string['usersinlist'] = 'Uživatelé v seznamu';
+$string['usersselected'] = 'Počet vybraných uživatelů: $a';
+
+?>
