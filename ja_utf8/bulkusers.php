4b7e20654676cade87d818af93911db301b64af5
Mitsuhiro Yoshida <mits>
2007-10-17 05:10:46 +0000

    Updated translated strings and a help file for bulk user selection buttons based on lang/en_utf8 change MDL-11553.

--- ja_utf8/bulkusers.php
+++ ja_utf8/bulkusers.php
@@ -1,19 +1,19 @@
 <?PHP // $Id$ 
-      // bulkusers.php - created with Moodle 1.9 Beta + (2007100303)
+      // bulkusers.php - created with Moodle 2.0 dev (2007101500)
 
 
-$string['addall'] = '「利用可能のすべてのユーザ」を「選択済み」に追加する';
-$string['addsel'] = '「利用可能で選択したユーザ」を「選択済み」に追加する';
+$string['addall'] = '「利用可能」のすべてのユーザを「選択済み」に追加する';
+$string['addsel'] = '「利用可能」で選択したユーザを「選択済み」に追加する';
 $string['available'] = '利用可能';
-$string['deleteall'] = '「選択済みのすべてのユーザ」を削除する';
-$string['deletesel'] = '「選択済みで選択したユーザ」を削除する';
-$string['removeall'] = '「利用可能のすべてのユーザ」を「選択済み」から削除する';
-$string['removesel'] = '「選択済みで選択したユーザ」を削除する';
+$string['deleteall'] = '「選択済み」のすべてのユーザをクリアする';
+$string['deletesel'] = '「選択済み」で選択したユーザをクリアする';
 $string['selected'] = '選択済み';
 $string['selectedlist'] = '選択済みユーザ一覧 ...';
 $string['usersfound'] = '$a ユーザが見つかりました。';
 $string['usersinlist'] = 'ユーザ一覧';
 $string['usersselected'] = '$a ユーザが選択されました。';
+$string['removeall'] = '「利用可能のすべてのユーザ」を「選択済み」から削除する'; // ORPHANED
+$string['removesel'] = '「選択済みで選択したユーザ」を削除する'; // ORPHANED
 $string['action'] = '操作'; // ORPHANED
 $string['filtered'] = 'フィルタ済み'; // ORPHANED
 
