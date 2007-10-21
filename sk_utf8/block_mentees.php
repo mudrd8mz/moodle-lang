27337bf8f7918b77ff779999b05e624bcc4e9925
Miroslav Fikar <fikar>
2007-10-21 08:35:42 +0000

    Incorrect translation of mentees

--- sk_utf8/block_mentees.php
+++ sk_utf8/block_mentees.php
@@ -2,9 +2,9 @@
       // block_mentees.php - created with Moodle 1.8.2+ (2007021520)
 
 
-$string['blockname'] = 'Mentori';
+$string['blockname'] = 'Zverenci';
 $string['configtitle'] = 'Názov bloku';
 $string['leaveblanktohide'] = 'Ponechajte prázdne na skrytie názvu';
-$string['newmenteesblock'] = '(nový blok Mentori)';
+$string['newmenteesblock'] = '(nový blok Zverenci)';
 
 ?>
