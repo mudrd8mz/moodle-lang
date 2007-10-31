daa7414e5fa8357aaa3cff137d6fb8925250a8b3
Mitsuhiro Yoshida <mits>
2007-10-31 21:23:42 +0000

    Removing a translated csv plugin lang file based on /lang/en_utf8 removal MDL-11930. Updated a translated help file for surverys based on lang/en_utf8 URL correction MDL-11941.

--- ja_utf8/gradeexport_csv.php
+++
@@ -1,9 +0,0 @@
-<?PHP // $Id$ 
-      // gradeexport_csv.php - created with Moodle 1.9 Beta + (2007092501)
-
-
-$string['cvs:publish'] = 'CSV評定エクスポートを公開する';
-$string['cvs:view'] = 'CSV評定エクスポートを使用する';
-$string['modulename'] = 'CSVファイル';
-
-?>
