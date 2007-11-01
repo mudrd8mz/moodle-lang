ad40c7d791265d3125ad2e3859249f8826301798
Mitsuhiro Yoshida <mits>
2007-11-01 19:44:39 +0000

    Translated a new string for bulk user upload MDL-11996.

--- ja_utf8/admin.php
+++ ja_utf8/admin.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // admin.php - created with Moodle 2.0 dev (2007101500)
+      // admin.php - created with Moodle 2.0 dev (2007101501)
 
 
 $string['accessdenied'] = 'アクセスが拒否されました。';
@@ -294,6 +294,7 @@ $string['enablerecordcache'] = 'レコードキャッシュを有効にする';
 $string['enablerssfeeds'] = 'RSSフィードを有効にする';
 $string['enablestats'] = '統計を有効にする';
 $string['enabletrusttext'] = 'コンテンツ信頼を有効にする';
+$string['encoding'] = 'エンコーディング';
 $string['environment'] = '動作環境';
 $string['environmenterrortodo'] = 'このバージョンのMoodleをインストールする前に、発見された動作環境の問題 (エラー) を解消してください!';
 $string['environmenterrorupgrade'] = 'このバージョンのMoodleをアップグレードする前に、発見された動作環境の問題 (エラー) を解消してください! これらの必要条件に関して修正せずにアップグレードした場合、データ喪失等の問題が生じる可能性があります。本当にアップグレードを続けてもよろしいですか?';
