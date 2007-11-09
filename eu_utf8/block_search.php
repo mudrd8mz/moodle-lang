0b08facc47ad75ece31b88be7e3490962e847195
Abel Camacho <ueu_ueu>
2007-11-09 11:01:49 +0000

    SUEk koordinatutako itzulpena

---
+++ eu_utf8/block_search.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // block_search.php - created with Moodle 1.8.2+ (2007021520)
+
+
+$string['blockname'] = 'Bilaketa orokorra';
+$string['bytes'] = 'byt-ak (0-k mugarik gabe adirazten du)';
+$string['configbuttonlabel'] = 'Botoiaren etiketa';
+$string['configenablefileindexing'] = 'Fitxategien indaxazioa gaitu';
+$string['configfiletypes'] = 'Kudeatutako fitxategi-motak';
+$string['configlimitindexbody'] = 'Indexatutako gorputzaren tamaina-muga';
+$string['configpdftotextcmd'] = 'pdftotext komandorako bidea';
+$string['configsearchtext'] = 'Bilatu testua';
+$string['configwordtotextcmd'] = 'doctotext komandorako bidea';
+$string['configwordtotextenv'] = 'MSWord bihurtzailerako inguruaren ezarpena';
+$string['go'] = 'Joan!';
+$string['searchmoodle'] = 'Bilatu Moodle-n';
+
+?>
