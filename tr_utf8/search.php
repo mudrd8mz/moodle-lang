20bf84fd49a86f84bbb9d9923cfc8255681d681f
Ethem Evlice <ethem>
2007-11-12 15:01:12 +0000

    new strings

--- tr_utf8/search.php
+++ tr_utf8/search.php
@@ -1,8 +1,42 @@
 <?PHP // $Id$ 
-      // search.php - created with Moodle 1.9 Beta + (2007083101)
-      // local modifications from http://ibsumer.cu.edu.tr/moodlehead
+      // search.php - created with Moodle 2.0 dev (2007101503)
 
 
 $string['advancedsearch'] = 'Gelişmiş arama';
+$string['all'] = 'Tümü';
+$string['author'] = 'Yazar';
+$string['authorname'] = 'Yazar adı';
+$string['back'] = 'Geri';
+$string['beadmin'] = 'Bu sayfayı kullanmak için yönetici olmalısınız.';
+$string['checkdb'] = 'Veritabanını Kontrol Et';
+$string['checkdbadvice'] = 'Herhangi bir sorun için veritabanınızı kontrol edin.';
+$string['checkdir'] = 'Klasör kontrol et';
+$string['checkdiradvice'] = 'Veri klasörünün mevcut ve yazılabilir olduğundan emin olun.';
+$string['commenton'] = 'Yorum';
+$string['createanindex'] = 'index oluştur';
+$string['createdon'] = 'Oluşturulma';
+$string['database'] = 'Veritabanı';
+$string['databasestate'] = 'Veritabanı durumu indexleniyor';
+$string['datadirectory'] = 'Veri klasörü';
+$string['doctype'] = 'Belge tipi';
+$string['documents'] = 'belgeler';
+$string['documentsindatabase'] = 'Veritabanındaki belgeler';
+$string['documentsinindex'] = 'Indexteki belgeler';
+$string['enteryoursearchquery'] = 'Arama sorgunuzu giriniz';
+$string['errors'] = 'Hatalar';
+$string['filesinindexdirectory'] = 'Index klasöründeki dosyalar';
+$string['globalsearchdisabled'] = 'Genel arama etkin değil.';
+$string['next'] = 'Sonraki';
+$string['normalsearch'] = 'Normal arama';
+$string['search'] = 'Ara';
+$string['searching'] = 'Aranıyor...';
+$string['seconds'] = 'saniye';
+$string['solutions'] = 'Çözümler';
+$string['statistics'] = 'İstatistikler';
+$string['title'] = 'Başlık';
+$string['totalsize'] = 'Toplam Boyut';
+$string['type'] = 'Tipi';
+$string['whichmodulestosearch?'] = 'Hangi modüller aranacak?';
+$string['wordsintitle'] = 'Başlıktaki sözcükler';
 
 ?>
