af82bc341af8162ec8ba0f9e7530f31a3e9f160a
Ethem Evlice <ethem>
2007-11-12 14:14:51 +0000

    new strings

--- tr_utf8/enrol_manual.php
+++ tr_utf8/enrol_manual.php
@@ -1,12 +1,13 @@
 <?PHP // $Id$ 
-      // enrol_manual.php - created with Moodle 1.6 development (2006031000)
+      // enrol_manual.php - created with Moodle 2.0 dev (2007101503)
 
 
-$string['description'] = 'Bu varsayılan ders kaydı yöntemidir. Bir öğrencinin bir derse katılabilmesi için iki yol vardır.
+$string['description'] = 'Bu, varsayılan ders kaydı yöntemidir. Bir öğrencinin bir derse katılabilmesi için iki yol vardır.
 <ul>
 <li>Eğitmen veya yönetici, Kurs Yönetim menüsündeki bağlantıyı tıklayarak elle öğrenciyi derse kaydedebilir.
 <li>Bir kursun \"kayıt anahtarı\" denen bir şifresi olabilir. Bu şifreyi bilenler kendi kendilerini derse kaydedebilirler.
 </ul>';
 $string['enrolname'] = 'Dahili Kayıt';
+$string['keyholderrole'] = 'Bir kursun kayıt anahtarına sahip kullanıcı rolünü seçiniz. Kursa kaydolmaya çalışan öğrencilere bu yetkili kişi gösterilir.';
 
 ?>
