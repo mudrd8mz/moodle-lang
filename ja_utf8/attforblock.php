b62d7f99526e365101824f58b27c5ce73d5c4231
Mitsuhiro Yoshida <mits>
2007-12-10 22:05:18 +0000

    Adding a new translated help file for attendance. Brushed up a translated string for attendance.

--- ja_utf8/attforblock.php
+++ ja_utf8/attforblock.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // attforblock.php - created with Moodle 1.9 dev (2007071703)
+      // attforblock.php - created with Moodle 2.0 dev (2007101504)
 
 
 $string['attforblock:changepreferences'] = '属性の変更';
@@ -8,10 +8,10 @@ $string['attforblock:manageattendances'] = '出欠の管理';
 $string['attforblock:takeattendances'] = '出欠を取る';
 $string['attforblock:view'] = '出欠の表示';
 $string['attforblock:viewreports'] = 'レポートの表示';
-$string['description'] = 'あなたは、1つのコースにつき1つの出欠モジュールを追加することができます。<br />このモジュールを削除しても、出欠には影響しません! このモジュールは、出欠ブロックのみと使用することができます。このモジュールは、コースの評定に出欠点を入れるためだけに必要です。';
+$string['description'] = 'あなたは、1つのコースにつき1つの出欠モジュールを追加することができます。';
 $string['modulename'] = '出欠';
 $string['modulenameplural'] = '出欠';
-$string['notfound'] = 'このコースに出欠活動がありません!';
+$string['notfound'] = 'このコースには出欠活動がありません!';
 $string['modulenamesimple'] = '出欠'; // ORPHANED
 
 ?>
