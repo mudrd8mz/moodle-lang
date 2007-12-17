765f2f1b72dd8bc89b4e77b8a88dad9556d08af7
Jaime Villate <villate>
2007-12-17 11:06:14 +0000

    Initial translatio of new files in version 1.9

---
+++ pt_utf8/datapreset_imagegallery.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // datapreset_imagegallery.php - created with Moodle 1.7.1+ (2006101010)
+
+
+$string['modulename'] = 'Galeria de Imagens';
+
+?>
---
+++ pt_utf8/filters.php
@@ -0,0 +1,42 @@
+<?PHP // $Id$ 
+      // filters.php - created with Moodle 1.7.1+ (2006101010)
+
+
+$string['actfilterhdr'] = 'Filtros activos';
+$string['addfilter'] = 'Adicionar filtro';
+$string['anycategory'] = 'qualquer categoria';
+$string['anycourse'] = 'qualquer disciplina';
+$string['anyfield'] = 'qualquer campo';
+$string['anyrole'] = 'qualquer papel';
+$string['anyvalue'] = 'qualquer valor';
+$string['categoryrole'] = 'Papel da categoria';
+$string['contains'] = 'contém';
+$string['courserole'] = 'Papel da disciplina';
+$string['courserolelabel'] = '$a->label é $a->rolename em $a->coursename de $a->categoryname';
+$string['courserolelabelerror'] = '$a->label erro: a disciplina $a->coursename não existe';
+$string['datelabelisafter'] = '$a->label é posterior a $a->after';
+$string['datelabelisbefore'] = '$a->label é anterior a $a->before';
+$string['datelabelisbetween'] = '$a->label é entre $a->after e $a->before';
+$string['doesnotcontain'] = 'não contém';
+$string['endswith'] = 'terminado em';
+$string['firstaccess'] = 'Primeiro acesso';
+$string['globalrolelabel'] = '$a->label é $a->value';
+$string['isafter'] = 'é posterior a';
+$string['isanyvalue'] = 'tem qualqer valor';
+$string['isbefore'] = 'é anterior a';
+$string['isdefined'] = 'está definido';
+$string['isempty'] = 'está vazio';
+$string['isequalto'] = 'é igual a';
+$string['isnotdefined'] = 'não está definido';
+$string['isnotequalto'] = 'não é igual a';
+$string['newfilter'] = 'Novo filtro';
+$string['profilelabel'] = '$a->label: $a->profile $a->operator $a->value';
+$string['profilelabelnovalue'] = '$a->label: $a->profile $a->operator';
+$string['removeall'] = 'Retirar todos os filtros';
+$string['removeselected'] = 'Retirar os seleccionados';
+$string['selectlabel'] = '$a->label $a->operator $a->value';
+$string['startswith'] = 'começa com';
+$string['textlabel'] = '$a->label $a->operator $a->value';
+$string['textlabelnovalue'] = '$a->label $a->operator';
+
+?>
