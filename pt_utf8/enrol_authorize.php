2f3d4aa10f3c5b61e3b56072c008d8ce3eab4ff1
Jaime Villate <villate>
2007-12-18 11:11:18 +0000

    Contribution from ISEP.

--- pt_utf8/enrol_authorize.php
+++ pt_utf8/enrol_authorize.php
@@ -1,20 +1,24 @@
 <?PHP // $Id$ 
-      // enrol_authorize.php - created with Moodle 1.6.2 (2006050521)
+      // enrol_authorize.php - created with Moodle 1.7.1+ (2006101010)
 
 
 $string['adminacceptccs'] = 'Que tipos de cartões de crédito serão aceites?';
+$string['adminaccepts'] = 'Selecione métodos de pagamento permitidos e os seus tipos';
+$string['adminauthcode'] = 'Se um cartão de crédito de um utilizador não for (capturado) directamente na Internet,obter autorização do código pelo telefone através do banco do cliente.';
 $string['adminauthorizeccapture'] = 'Revisão da ordem e Definições da captação programada (Scheduled)';
 $string['adminauthorizeemail'] = 'Definições de envio de Email';
 $string['adminauthorizesettings'] = 'Definições Authorize.net';
 $string['adminauthorizewide'] = 'Definições Site-Wide';
 $string['adminavs'] = 'Seleccione se tem activado o Address Verification System (AVS) na sua conta authorize.net. Irá pedir dados sobre o endereço, como Rua, Estado, País quando o utilizador preencher o formulário de pagamento';
-$string['admincronsetup'] = 'O script de manutenção cron.php não correu pelo menos nas última 24 Horas.<br /> O Cron tem que estar activo se quiser utilizar a característica de captação programada (scheduled-capture).<br />Activar</b> \'Authorize.net plugin\' e <b>setup cron</b> properly; ou <b>uncheck an_review</b> again.<br />Se desactivar a scheduled-capture, as transacções serão canceladas a menos que as tenha revisto no prazo de 30 dias.<br />Verifique <b>an_review</b> e insira <b>\'0\' no an_capture_day</b> campo<br />Se quiser <b>manualmente</b> aceite/negue os pagamentos a 30 dias.';
+$string['adminconfighttps'] = 'Verifique ter \"<a href=\"$a->url\"> ligado loginhttps</a>\" para usar este plugin<br /> em Administração >> Variáveis>> Segurança >> Segurança HTTP';
+$string['adminconfighttpsgo'] = 'Visite <a href=\"$a->url\">página segura</a> para configurar este plugin.';
+$string['admincronsetup'] = 'O script de manutenção cron.php não tem sido executado pelo menos nas últimas 24 Horas.<br /> O cron tem que estar activo se quiser utilizar a característica de captação programada (scheduled-capture).<br /><strong>Activar</strong> \'Authorize.net plugin\' e <strong>setup cron</strong> apropriadamente; ou <strong>desactive an_review</strong> novamente.<br />Se desactivar a captação programada, as transacções serão canceladas a menos que as tenha revisto no prazo de 30 dias.<br />Verifique <strong>an_review</strong> e insira \'0\' no campo <strong>an_capture_day</strong><br />se quiser aceitar/negar os pagamentos a 30 dias manualmente.';
 $string['adminemailexpired'] = 'Isto é útil para \'Captação Manual\'. Os Administradores são notificados <b>$a</b> dias previamente antes de expirarem os pedidos pendentes.';
 $string['adminemailexpiredsort'] = 'Quando o número de pedido pendentes a expirar são enviados para os professores via email, quais são os importantes?';
 $string['adminemailexpiredsortcount'] = 'Número dos pedidos';
 $string['adminemailexpiredsortsum'] = 'Total das quantidades';
 $string['adminemailexpiredteacher'] = 'Se activar a Captação Manual (ver acima) e os professores puderem controlar os pagamentos, o professor pode ser notificado sobre as ordens pendentes a expirar. Será enviado um email para cada professor de disciplina a dizer quantas ordens pendentes estão a expirar.';
-$string['adminemailexpsetting'] = '(0=desactivar envio de email, defeito=2, max=5)<br />(Definições de Captação Manual para envios de email: cron=activo, an_review=verificado, an_capture_day=0, an_emailexpired=1-5)';
+$string['adminemailexpsetting'] = '(0=desactivar envio de email, por omissão=2, max=5)<br />(Definições de Captação Manual para envios de email: cron=activo, an_review=verificado, an_capture_day=0, an_emailexpired=1-5)';
 $string['adminhelpcapturetitle'] = 'Dia de captação programada';
 $string['adminhelpreviewtitle'] = 'Revisão dos pedidos';
 $string['adminneworder'] = 'Exmº Sr. Administrador,
@@ -38,13 +42,18 @@ $string['adminnewordersubject'] = '$a->course: Novo pedido pendente ($a->orderid
 $string['adminpendingorders'] = 'Desactivou a característica de captação programada.<br />O total $a->count de transacções com estatuto de \'Captação Autorizada/Pendente\' será cancelado a menos que active isto.<br />Para aceitar/negar pagamentos vá à página <a href=\'$a->url\'>Controlo de Pagamentos</a>.';
 $string['adminreview'] = 'Rever pedido antes de processar o cartão de crédito.';
 $string['adminteachermanagepay'] = 'Os professores podem controlar os pagamentos da Disciplina.';
+$string['allpendingorders'] = 'Todas as ordens pendentes';
 $string['amount'] = 'Quantidade';
 $string['anlogin'] = 'Authorize.net: Nome Login';
 $string['anpassword'] = 'Authorize.net: Palavra-Passe';
 $string['anreferer'] = 'Defina o URL referente se tiver configurado isto na sua conta authorize.net. Isto enviará uma linha \"Referente: URL\" incluida no seu pedido web.';
 $string['antestmode'] = 'Corra as transacções apenas em modo de teste (não será despejado dinheiro)';
 $string['antrankey'] = 'Authorize.net: Chave de Transacção';
+$string['approvedreview'] = 'Revisão aprovada';
 $string['authcaptured'] = 'Autorizado / Captado';
+$string['authcode'] = 'Código de autorização';
+$string['authorize:managepayments'] = 'Controlar Pagamentos';
+$string['authorize:uploadcsv'] = '(Upload/carregar) ficheiro CSV';
 $string['authorizedpendingcapture'] = 'Autorizado / Captação Pendente';
 $string['avsa'] = 'Endereço (Rua) igual, Código Postal não';
 $string['avsb'] = 'Informação do Endereço não fornecida';
@@ -57,8 +66,8 @@ $string['avsresult'] = '<b>Resultado AVS:</b> $a';
 $string['avss'] = 'Serviço não suportado pelo editor';
 $string['avsu'] = 'Informação do endereço indisponível';
 $string['avsw'] = 'Código Postal de 9 digitos igual, o endereço (Rua) não';
-$string['avsx'] = 'O enderço (Rua) e Código Postal de 9 digitos igual';
-$string['avsy'] = 'O enderço (Rua) e Código Postal de 5 digitos igual';
+$string['avsx'] = 'O endereço (Rua) e Código Postal de 9 digitos igual';
+$string['avsy'] = 'O endereço (Rua) e Código Postal de 5 digitos igual';
 $string['avsz'] = 'Código Postal de 5 digitos igual, o endereço (Rua) não';
 $string['canbecredit'] = 'Pode ser reembolsado para $a->upto';
 $string['cancelled'] = 'Cancelado';
@@ -78,25 +87,50 @@ $string['chooseone'] = 'Preencha um ou ambos dos campos seguintes';
 $string['costdefaultdesc'] = '<strong>Nas configurações da disciplina, introduza -1</strong> para usar o custo por defeito.';
 $string['cutofftime'] = 'Interrupção na transacção. Quando foi a última transacção estabelecida?';
 $string['delete'] = 'Destruir';
-$string['description'] = 'O módulo Authorize.net permite-lhe definir Disciplinas pagas através de fornecedores CC. Se o custo para qualquer Disciplina for zero, não serão pedidos pagamentos aos alunos para entrar. Duas formas de definir custos (1) um custo Site-Wide por defeito para todo o site (2) uma definição na Disciplina que poderá ser ajustada para cada Disciplina individualmente. O custo do curso cancela o custo por defeito.<br/><br/><b>Nota: </b> se incorporar uma chave do inscrição nas definições da Disciplina, então os alunos terão também a opção de registo através de uma chave. Isto é útil caso queira ter alunos pagantes e outros não-pagantes.';
+$string['description'] = 'O módulo Authorize.net permite-lhe definir Disciplinas pagas através de fornecedores CC. Se o custo para qualquer Disciplina for zero, não serão pedidos pagamentos aos alunos para entrar. Duas formas de definir custos (1) um custo Site-Wide por defeito para todo o site (2) uma definição na Disciplina que poderá ser ajustada para cada Disciplina individualmente. O custo do curso cancela o custo por defeito.<br /><br /><b>Nota: </b> se incorporar uma chave do inscrição nas definições da Disciplina, então os alunos terão também a opção de registo através de uma chave. Isto é útil caso queira ter alunos pagantes e outros não-pagantes.';
+$string['echeckabacode'] = 'Número de Banco ABA';
+$string['echeckaccnum'] = 'Número de conta de Banco';
+$string['echeckacctype'] = 'Tipo de conta de Banco';
+$string['echeckbankname'] = 'Nome do Banco';
+$string['echeckbusinesschecking'] = '(Confirmação de Negócio)';
+$string['echeckchecking'] = '(Confirmando/ A Confirmar)';
+$string['echeckfirslasttname'] = 'Dono da conta de Banco';
+$string['echecksavings'] = 'Poupanças';
 $string['enrolname'] = 'Gateway de Pagamento Authorize.net';
 $string['expired'] = 'Expirou';
+$string['haveauthcode'] = 'Já tenho um código de autorização';
 $string['howmuch'] = 'Quanto?';
-$string['httpsrequired'] = 'Lamentamos informá-lo mas o seu pedido não pode ser processado agora. A configuração do site não pode ser ajustado correctamente.<br/><br/>Por favor não insira o número do cartão do crédito a menos que veja um cadeado amarelo no browser. Se o símbolo aparecer, significa que a página encripta todos os dados emitidos entre o cliente e o servidor. Então a transacção da informação entre os dois computadores é protegida, pelo que o seu número do cartão de crédito não será capturado na Internet.';
+$string['httpsrequired'] = 'Lamentamos informá-lo mas o seu pedido não pode ser processado agora. A configuração do site não pode ser ajustado correctamente.<br /><br />Por favor não insira o número do cartão do crédito a menos que veja um cadeado amarelo no browser. Se o símbolo aparecer, significa que a página encripta todos os dados emitidos entre o cliente e o servidor. Então a transacção da informação entre os dois computadores é protegida, pelo que o seu número do cartão de crédito não será capturado na Internet.';
+$string['invalidaba'] = 'Número ABA inválido';
+$string['invalidaccnum'] = 'Número de conta inválido';
+$string['invalidacctype'] = 'Tipo de conta inválido';
 $string['logindesc'] = 'Esta opção tem que estar ON. <br /><br />Certifique-se que colocou <a href=\"$a->url\">loginhttps ON</a> in Admin >> Variables >> Security.<br /><br />Colocando isto ON fará com que o Moodle utilize o protocolo de ligação segura https para o login e páginas de pagamento.';
+$string['logininfo'] = 'Nome de Login, password e chaves de transacções não são (mostradas/visíveis/visionadas) devido a questões de segurança. Não será necessário introduzir de novo estes campos se estes já foram configurados anteriormente.
+Deverá visualizar um texto a verde á esquerda da (caixa de texto) se certos campos já foram configurados.
+Se introduz estes campos pela primeira vez, o nome do Login(*) é obrigatório e <strong> também </strong> deve introduzir a chave de transacção(#1)  <strong> ou </strong> a password(#2) na caixa de texto apropriada. Recomendamos que introduza a chave de transacção devido a precauções de questões de segurança. Se quiser apagar a password actual, (coloque um viste na checkbox).';
+$string['methodcc'] = 'Cartão de crédito';
+$string['methodecheck'] = '(eCheck)(ACH)';
+$string['missingaba'] = 'Número ABA em falta';
 $string['missingaddress'] = 'Falta Endereço';
+$string['missingbankname'] = 'Nome do Banco em falta';
 $string['missingcc'] = 'Falta Número do Cartão de Crédito';
+$string['missingccauthcode'] = 'Código de autorização em falta';
 $string['missingccexpire'] = 'Falta Data de expiração';
 $string['missingcctype'] = 'Falta Tipo do Cartão de Crédito';
 $string['missingcvv'] = 'Falta Verificação';
 $string['missingzip'] = 'Falta Código Postal';
+$string['mypaymentsonly'] = 'Mostrar apenas os meus pagamentos';
 $string['nameoncard'] = 'Nome no Cartão';
 $string['new'] = 'Novo';
 $string['noreturns'] = 'Sem retorno!';
 $string['notsettled'] = 'Não estabelecida';
 $string['orderid'] = 'ID Pedido';
 $string['paymentmanagement'] = 'Controlo de Pagamentos';
+$string['paymentmethod'] = 'Método de pagamento';
 $string['paymentpending'] = 'O seu pagamento está pendente para esta Disciplina com o número de pedido $a->orderid. Veja <a href=\'$a->url\'>Detalhes de pedido</a>.';
+$string['pendingecheckemail'] = 'Estimado (responsável/administrador/etc..)
+Existem echecks $a->count pendentes e é necessário fazer o upload para um ficheiro CSV para obter os utilizadores inscritos.';
+$string['pendingechecksubject'] = '$a->course: eChecks pendentes ($a->count)';
 $string['pendingordersemail'] = 'Exmº Sr. Administrador,
 
 $a->pending transacções para a Disciplina \"$a->course\" expirarão a menos que aceite os pagamentos a $a->days dias.
@@ -123,6 +157,8 @@ $string['reason11'] = 'Uma transacção duplicada foi submetida';
 $string['reason13'] = 'O ID de login do comerciante é inválido ou a conta está inactiva';
 $string['reason16'] = 'A transacção não foi encontrada';
 $string['reason17'] = 'O comerciante não aceita este tipo de cartão de crédito.';
+$string['reason245'] = '(Este tipo de eCheck não é permitido quando usar o formulário de pagamento)';
+$string['reason246'] = 'Este tipo de eCheck não é permitido';
 $string['reason27'] = 'A transacção resultou numa má combinação AVS. O endereço fornecido não é igual ao enderço da factura do possuidor do cartão.';
 $string['reason28'] = 'O comerciante não aceita este tipo de cartão de crédito.';
 $string['reason30'] = 'A configuração com o processador é inválida. Contacte o fornecedor do serviço comercial.';
@@ -136,10 +172,12 @@ $string['reason50'] = 'Esta transacção está à espera de ser estabelecida e n
 $string['reason51'] = 'A soma de todos os créditos desta transacção é maior que o valor original da transacção.';
 $string['reason54'] = 'A transacção referida não reúne os critérios para emitir um crédito.';
 $string['reason55'] = 'A soma de todos os créditos desta transacção excederá o valor originalmente debitado.';
+$string['reason56'] = 'Este estabelecimento aceita apenas transacções eCheck(ACH); Transacções de cartão de crédito não são aceites.';
 $string['refund'] = 'Reembolso';
 $string['refunded'] = 'Reembolsado';
 $string['returns'] = 'Retornos';
 $string['reviewday'] = 'Captar o cartão de crédito automaticamente a não ser que um professor ou administrador revejam o pedido em <b>$a</b> dias. O CRON TEM QUE ESTAR ACTIVO.<br />(0 dias significa que irá desactivar a captação programada, significando que o professor ou administrador irão rever o pedido manualmente. A transacção será cancelada se desactivar a captação programada a não ser que a reveja em 30 dias).';
+$string['reviewfailed'] = 'Revisão falhada';
 $string['reviewnotify'] = 'O seu pagamento vai ser revisto. Aguarde um email dentro de alguns dias do seu professor.';
 $string['sendpaymentbutton'] = 'Enviar pagamento';
 $string['settled'] = 'Estabelecido';
@@ -149,7 +187,11 @@ $string['tested'] = 'Testado';
 $string['testmode'] = 'Modo de Teste';
 $string['testwarning'] = 'A captação/vácuo/crédito parecem estar a funcionar em modo de teste, mas nenhum registo foi actualizado ou inserido na base de dados.';
 $string['transid'] = 'ID da Transacção';
+$string['underreview'] = '(Em revisão/A rever)';
 $string['unenrolstudent'] = 'Desinscrever Aluno?';
+$string['uploadcsv'] = '(Upload/a carregar) ficheiro CSV';
+$string['usingccmethod'] = 'Inscrever através de <a href=\"$a->url\"><strong>Cartão de Crédito</strong></a>';
+$string['usingecheckmethod'] = 'Inscrever através de <a href=\"$a->url\"><strong>eCheck</strong></a>';
 $string['void'] = 'Vácuo';
 $string['voidyes'] = 'A transacção será cancelada. Tem a certeza?';
 $string['welcometocoursesemail'] = 'Caro aluno,
--- pt_utf8/error.php
+++ pt_utf8/error.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.6.2 (2006050521)
+      // error.php - created with Moodle 1.7.1+ (2006101010)
 
 
 $string['adminprimarynoedit'] = 'Os dados do administrador principal não podem ser editado
 s por outros';
+$string['cannotassignrole'] = 'Não pode atribuir papel na disciplina';
 $string['cannotcreatelangdir'] = 'Não é possível criar o directório para pacotes linguísticos';
 $string['cannotcreatetempdir'] = 'Não é possível criar o directório temporal';
 $string['cannotdownloadcomponents'] = 'Não é possível descarregar a cópia dos componentes';
@@ -12,9 +13,12 @@ $string['cannotfindcomponent'] = 'Não foi encontrado um componente';
 $string['cannotsavemd5file'] = 'Não é possível gravar ficheiro md5';
 $string['cannotsavezipfile'] = 'Não  é possível gravar ficheiro ZIP';
 $string['cannotunzipfile'] = 'Não é possível descompactar ficheiro ZIP';
+$string['cantunenrollfrommetacourse'] = 'Não pode cancelar a inscrição desta meta-disciplina';
+$string['cantunenrollinthisrole'] = 'Não pode cancelar a inscrição desta disciplina enquanto estiver no seu actual cargo.';
 $string['cmunknown'] = 'Não foi encontrado esse módulo de disciplina';
 $string['componentisuptodate'] = 'O componente está actualizado';
 $string['confirmsesskeybad'] = 'Lamentamos mas a sua chave se sessão não pode ser confirmada para poder levar a cabo esta acção. Esta característica de segurança previne execuções, acidentais ou maliciosas, de funções importantes em seu nome. Certifique-se por favor que quis realmente executar esta função.';
+$string['couldnotassignrole'] = 'Um sério mas não especificado erro ocorreu durante a atribuição de um cargo para si.';
 $string['coursegroupunknown'] = 'Faltou especificar a disciplina correspondente ao grupo $a';
 $string['downloadedfilecheckfailed'] = 'Falhou a verificação do ficheiro descarregado.';
 $string['errorcleaningdirectory'] = 'Erro na limpeza do directório \"$a\"';
@@ -29,21 +33,28 @@ $string['filenotfound'] = 'Lamentamos mas o ficheiro pedido não foi encontrado'
 $string['forumblockingtoomanyposts'] = 'Já ultrapassou o limite permitido de envio de mensagens para este fórum.';
 $string['groupalready'] = 'O utilizador já pertence ao grupo $a';
 $string['groupunknown'] = 'O grupo $a não está associado à disciplina indicada';
-$string['guestnoeditprofile'] = 'Os visitantes nãoe editar o seu perfil';
+$string['guestnoeditprofile'] = 'Os visitantes não podem editar o seu perfil';
 $string['guestnoeditprofileother'] = 'O perfil do visitante não pode ser editado';
 $string['invalidcourse'] = 'Disciplina não válida';
 $string['invalidfieldname'] = '\"$a\" não é um nome de campo válido';
 $string['invalidfiletype'] = '\"$a\" não é um tipo de ficheiro válido';
 $string['invalidmd5'] = 'md5 não válido';
+$string['invalidrequest'] = 'Pedido Inválido';
+$string['invalidrole'] = 'Cargo Inválido';
 $string['invalidxmlfile'] = '\"$a\" não é um ficheiro XML válido';
+$string['loginasnoenrol'] = 'Não pode inscrever ou cancelar a inscrição aquando da execução da sessão \"Entrar como\"';
+$string['loginasonecourse'] = 'Não pode entrar nesta disciplina.<br/> Tem que terminar a sessão \"Entrar como\" antes de entrar noutra disciplina qualquer.';
 $string['missingfield'] = 'O campo';
 $string['missingrequiredfield'] = 'Falta algum campo necessário';
 $string['modulemissingcode'] = 'O módulo $a não inclui o código necessário para executar esta função';
 $string['modulerequirementsnotmet'] = 'O módulo \"$a->modulename\" ($a->moduleversion) não pode ser instalado. Para a sua instalação é necessária uma versão mais recente do Moodle (actualmente está a usar a versão $a->currentmoodle e precisa da versão $a->requiremoodle).';
 $string['mustbeteacher'] = 'O acesso a esta página está reservado aos docentes';
+$string['nocontext'] = 'Desculpe, mas essa disciplina não é um contexto válido';
 $string['noinstances'] = 'Não há nenhum recurso do tipo $a nesta disciplina!';
+$string['nologinas'] = 'Não tem permissões  para entrar como esse utilizador.';
 $string['nonmeaningfulcontent'] = 'Conteúdo não significativo';
-$string['noparticipatorycms'] = 'Lamentamos mas não exit nenhum módulo de disciplina participativo para o qual possa enviar um relatório.';
+$string['noparticipatorycms'] = 'Lamentamos mas não exite nenhum módulo de disciplina participativo para o qual possa enviar um relatório.';
+$string['nopermissions'] = 'Desculpe, mas não tem actualmente permissões para fazer essa operação ($a)';
 $string['notavailable'] = 'Isso não está disponível actualmente';
 $string['onlyadmins'] = 'Unicamente os administradores podem fazer isso';
 $string['onlyeditingteachers'] = 'Unicamente os docentes com permissão de edição podem fazer isso';
@@ -59,6 +70,8 @@ $string['sessionerroruser'] = 'A sua sessão expirou. Por favor entre novamente 
 $string['sessionerroruser2'] = 'Foi detectado um erro no servidor que afecta a sua sessão actual. Por favor entre novamente no sistema ou reinicie o seu navegador.';
 $string['sessionipnomatch'] = 'Lamentamos, mas parece que o seu IP mudou desde quando entrou inicialmente no sistema. Esta característica de segurança previne que os crackers lhe roubem a identidade enquanto estiver neste sítio. Normalmente não deveria receber este tipo de mensagem - peça ajuda ao administrador do sítio.';
 $string['statscatchupmode'] = 'Os relatórios de actividade encomtram-se em modo de actualização. Até agora foram processados $a->daysdone dia(s) e estão pendentes $a->dayspending. Tente mais tarde!';
+$string['unicodeupgradeerror'] = 'Desculpe, mas a sua base de dados não se encontra com codificação \"UNICODE\", e esta versão do Moodle não tem a opção para migrar a sua base de dados para \"UNICODE\". Por favor, 
+actualize para Moodle 1.7.x primeiro e execute a migração \"UNICODE\" a partir da página Admin. Depois de isso estar feito, deve conseguir migrar para Moodle $a';
 $string['unknowncourse'] = 'Disciplina designada de';
 $string['unknownuseraction'] = 'Lamento, mas não reconheço essa acção de utilizador.';
 $string['usernotaddederror'] = 'O utilizador \"$a\" não foi adicionado - erro desconhecido';
