1e9b2978ddc2d18cda9627dccb22e716bcf93c39
Nicolas Martignoni <martignoni>
2007-12-20 12:45:02 +0000

    MDL-12635 - Manual grading report does not check capabilites - also, only show the tabs for the reports people can access.

--- fr_utf8/quiz_grading.php
+++ fr_utf8/quiz_grading.php
@@ -1,6 +1,7 @@
 <?php // $Id$
 
-$string['grading'] = 'Notation manuelle';
 $string['essayonly'] = 'Les questions suivantes doivent être notées manuellement';
+$string['grading'] = 'Notation manuelle';
+$string['gradingnotallowed'] = 'Vous n\'avez pas l\'autorisation de noter manuellement les réponses de ce test';
 
 ?>
