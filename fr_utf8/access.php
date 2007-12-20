70580a65bdf5f4f58a6d049e6b95988657f3e929
Nicolas Martignoni <martignoni>
2007-12-20 14:52:44 +0000

    Fixes reopened MDL-7493 "Make sure standard themes are visible under "Windows High Contrast Black" mode" - foreground show/hide block icon/ALT text switched with Javscript.

--- fr_utf8/access.php
+++ fr_utf8/access.php
@@ -9,11 +9,13 @@ $string['activityprev'] = 'Activité précédente';
 $string['breadcrumb'] = 'Barre de navigation';
 $string['currentweek'] = 'Cette semaine';
 $string['currenttopic'] = 'Ce thème';
+$string['hideblocka'] = 'Cacher bloc $a';
 $string['monthnext'] = 'Mois suivant';
 $string['monthprev'] = 'Mois précédent';
-$string['showhideblock'] = 'Afficher ou masquer le bloc';
-$string['skipa'] = 'Sauter $a';
+$string['showblocka'] = 'Afficher bloc $a';
+$string['showhideblock'] = 'Afficher ou masquer le bloc'; // Legacy as of 1.9beta3
 $string['sitemap'] = 'Carte du site';
+$string['skipa'] = 'Sauter $a';
 $string['skipblock'] = 'Sauter le bloc';
 $string['skipnavigation'] = 'Sauter la navigation';
 $string['tabledata'] = 'Données en tableau, $a';
@@ -21,5 +23,6 @@ $string['tablelayout'] = 'Formatage en tableau, $a';
 $string['tocontent'] = 'Passer au contenu principal';
 $string['tonavigation'] = 'Vers la navigation';
 $string['youarehere'] = 'Vous êtes ici';
+
 
 ?>
