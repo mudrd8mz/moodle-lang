5d08d3f90ea63270efad544a27212f3dacb34d50
Nicolas Martignoni <martignoni>
2007-12-20 14:38:03 +0000

    Typo corrected

--- fr_utf8/block_course_list.php
+++ fr_utf8/block_course_list.php
@@ -5,7 +5,7 @@ $string['blockname'] = 'Liste des cours';
 $string['allcourses'] = 'L\'administrateur voit tous les cours';
 $string['configadminview'] = 'Configuration de l\'affichage de l\'administrateur';
 $string['confighideallcourseslink'] = 'Cacher le lien «&nbsp;Tous les cours&nbsp;» au bas du bloc. Ce réglage ne modifie pas l\'affichage du bloc pour les administrateurs';
-$string['hideallcourseslink'] = 'Cacher le lien «&nbsp;Tous les cours&nbsp;';
+$string['hideallcourseslink'] = 'Cacher le lien «&nbsp;Tous les cours&nbsp;»';
 $string['owncourses'] = 'L\'administrateur ne voit que ses propres cours';
 
 ?>
