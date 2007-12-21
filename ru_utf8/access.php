c8dce6af5364e0b14b31918c5e0111264d20514c
Alex Djachenko <djachenko>
2007-12-21 15:23:11 +0000

    100% перевод

--- ru_utf8/access.php
+++ ru_utf8/access.php
@@ -1,17 +1,21 @@
-<?PHP 
-// $Id$ 
-// access.php - created with Moodle 1.6 (2006050506)
+<?PHP // $Id$ 
+      // access.php - created with Moodle 1.8.2+ (2007021520)
 
 
 $string['access'] = 'Доступность';
 $string['accesshelp'] = 'Доступность помощи';
 $string['accesskey'] = 'Ключ доступа';
 $string['accessstatement'] = 'Доступность отчета';
+$string['activitynext'] = 'Следующий элемент курса';
+$string['activityprev'] = 'Предыдущий элемент курса';
 $string['breadcrumb'] = 'Намеченный путь';
+$string['currenttopic'] = 'Текущая тема';
+$string['currentweek'] = 'Текущая неделя';
 $string['monthnext'] = 'Следующий месяц';
 $string['monthprev'] = 'Предыдущий месяц';
 $string['showhideblock'] = 'Показать или убрать блок';
 $string['sitemap'] = 'Карта сайта';
+$string['skipa'] = 'Пропустить $a';
 $string['skipblock'] = 'Пропустить блок';
 $string['skipnavigation'] = 'Пропустить навигацию';
 $string['tabledata'] = 'Данные таблицы';
