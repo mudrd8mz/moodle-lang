28a3b08f2880e3eb851cedb1ff7725f602f70872
koen roggemans <koenr>
2007-12-29 09:39:11 +0000

    New strings from Ghop project http://code.google.com/p/google-highly-open-participation-moodle/issues/detail?id=65

---
+++ th_utf8/form.php
@@ -0,0 +1,40 @@
+<?PHP // $Id$ 
+      // form.php - created with Moodle 2.0 dev (2007101506)
+
+
+$string['addfields'] = 'เพิ่มช่อง $a ลงในฟอร์ม';
+$string['advancedelement'] = 'ตัวเลือกขั้นสูง';
+$string['day'] = 'วัน';
+$string['display'] = 'แสดงผล';
+$string['err_alphanumeric'] = 'คุณสามารถกรอกได้เฉพาะ ตัวเลข หรือ ตัวอักษร เท่านั้น';
+$string['err_email'] = 'กรุณากรอกอีเมล์ให้ถูกต้อง';
+$string['err_lettersonly'] = 'คุณสามารถกรอกได้เฉพาะตัวอักษรเท่านั้น';
+$string['err_maxlength'] = 'คุณไม่สามารถกรอกเกิน $a->format ตัวอักษรได้';
+$string['err_minlength'] = 'คุณต้องกรอกอย่างน้อย $a->format ตัวอักษร';
+$string['err_nonzero'] = 'คุณไม่สามารถกรอกตัวเลขขึ้นต้นด้วย 0 ได้';
+$string['err_nopunctuation'] = 'คุณไม่สามารถใส่เครื่องหมายวรรคตอนได้';
+$string['err_numeric'] = 'คุณต้องใส่ตัวเลขเท่านั้น';
+$string['err_rangelength'] = 'คุณสามารถใส่ได้ตั้งแต่ {$a->format[0]} ถึง {$a->format[1]} ตัวอักษร';
+$string['err_required'] = 'คุณต้องกรอกช่องนี้';
+$string['general'] = 'ทั่วไป';
+$string['hideadvanced'] = 'ซ่อนตัวเลือกขั้นสูง';
+$string['hour'] = 'ชั่วโมง';
+$string['minute'] = 'นาที';
+$string['miscellaneoussettings'] = 'การตั้งค่าเบ็ดเตล็ด';
+$string['modstandardels'] = 'การตั้งค่าโมดูลปกติ';
+$string['month'] = 'เดือน';
+$string['nomethodforaddinghelpbutton'] = 'ไม่สามารถสร้างปุ่มช่วยเหลือให้กับช่อง $a->name (คลาส $a->classname )';
+$string['nonexistentformelements'] = 'คุณพยายามสร้างปุ่มช่วยเหลือให้กับช่อง $a ที่ไม่มีอยู่';
+$string['optional'] = 'ไม่บังคับ';
+$string['othersettings'] = 'การตั้งค่าอื่นๆ';
+$string['requiredelement'] = 'ช่องที่ต้องกรอก';
+$string['revealpassword'] = 'แสดงรหัส';
+$string['security'] = 'ความปลอดภัย';
+$string['selectallornone'] = 'เลือกทั้งหมด / ไม่เลือกเลย';
+$string['showadvanced'] = 'แสดงตัวเลือกขั้นสูง';
+$string['somefieldsrequired'] = 'คุณต้องกรอกข้อมูลในช่องที่ขึ้น $a';
+$string['timing'] = 'จับเวลา';
+$string['unmaskpassword'] = 'ไม่ปกปิด';
+$string['year'] = 'ปี';
+
+?>
