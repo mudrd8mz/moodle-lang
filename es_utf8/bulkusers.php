0d75df716ea3c5447fae726a82345b5924862af0
Benito Arias <barias>
2008-01-02 11:42:32 +0000

    *** empty log message ***

--- es_utf8/bulkusers.php
+++ es_utf8/bulkusers.php
@@ -1,13 +1,16 @@
 <?PHP // $Id$ 
-      // bulkusers.php - created with Moodle 1.9 Beta + (2007081600)
-      // local modifications from http://127.0.0.1/moodle
+      // bulkusers.php - created with Moodle 1.8.2+ (2007021520)
 
 
 $string['addall'] = 'Agregar todos a la selección';
 $string['addsel'] = 'Agregar a la selección';
+$string['allfilteredusers'] = 'Todos filtrados ($a->count/$a->total)';
+$string['allselectedusers'] = 'Todos seleccionados ($a->count/$a->total)';
+$string['allusers'] = 'Todos los usuarios ($a)';
 $string['available'] = 'Disponible';
-$string['deleteall'] = 'Quitar todos los usuarios';
-$string['deletesel'] = 'Quitar usuarios seleccionados';
+$string['confirmmessage'] = '¿Realmente desea enviar el mensaje a todos los usuarios?<br />$a';
+$string['nofilteredusers'] = 'No se han encontrado usuarios (0/$a)';
+$string['noselectedusers'] = 'No hay usuarios seleccionados';
 $string['removeall'] = 'Eliminar todos los usuarios disponibles';
 $string['removesel'] = 'Eliminar los usuarios disponibles seleccionados';
 $string['selected'] = 'Seleccionados';
@@ -15,5 +18,7 @@ $string['selectedlist'] = 'Lista de usuarios seleccionados...';
 $string['usersfound'] = '$a usuario(s) encontrado(s).';
 $string['usersinlist'] = 'Usuarios en lista';
 $string['usersselected'] = '$a usuario(s) seleccionado(s).';
+$string['deleteall'] = 'Quitar todos los usuarios'; // ORPHANED
+$string['deletesel'] = 'Quitar usuarios seleccionados'; // ORPHANED
 
 ?>
