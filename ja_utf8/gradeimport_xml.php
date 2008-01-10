620a7736cef01dfb70456f23e18600697eca30bc
Mitsuhiro Yoshida <mits>
2008-01-10 17:27:06 +0000

    Translated new strings for XML grade import error MDL-12913.

--- ja_utf8/gradeimport_xml.php
+++ ja_utf8/gradeimport_xml.php
@@ -1,10 +1,13 @@
 <?PHP // $Id$ 
-      // gradeimport_xml.php - created with Moodle 1.9 Beta + (2007092501)
+      // gradeimport_xml.php - created with Moodle 2.0 dev (2007101506)
 
 
 $string['errbadxmlformat'] = 'エラー - 不正なXMLフォーマットです。';
+$string['errduplicategradeidnumber'] = 'エラー - このコースのIDナンバー「 $a 」に2つの評定項目があります。インポートすることはできません。';
 $string['errduplicateidnumber'] = 'エラー - IDナンバーが重複しています。';
+$string['errincorrectgradeidnumber'] = 'エラー - インポートファイルのIDナンバー「 $a 」が、どの評定項目とも合致しません。';
 $string['errincorrectidnumber'] = 'エラー - IDナンバーが正しくありません。';
+$string['errincorrectuseridnumber'] = 'エラー - インポートファイルのIDナンバー「 $a 」が、どのユーザとも合致しません。';
 $string['fileurl'] = 'リモートファイルURI';
 $string['modulename'] = 'XMLファイル';
 $string['xml:publish'] = 'XMLからの評定インポートを公開する';
