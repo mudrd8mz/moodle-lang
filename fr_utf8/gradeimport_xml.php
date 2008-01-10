fe318df650218740e1329d6dd4a2ec4be361884e
Nicolas Martignoni <martignoni>
2008-01-10 20:37:55 +0000

    MDL-12913 - Error messages when doing an XML grade import are not very helpful, include the idnumber that is causing the problem in the error message.

--- fr_utf8/gradeimport_xml.php
+++ fr_utf8/gradeimport_xml.php
@@ -1,10 +1,14 @@
 <?php // $Id$ 
 
 $string['errbadxmlformat'] = 'Erreur - format XML incorrect';
+$string['errduplicategradeidnumber'] = 'Erreur - il y a deux notes de même identifiant «&nbsp;$a&nbsp;» dans ce cours. Cela ne devrait pas être possible.';
 $string['errduplicateidnumber'] = 'Erreur - identifiant en double';
+$string['errincorrectgradeidnumber'] = 'Erreur - l\'identifiant «&nbsp;$a&nbsp;» du fichier d\'importation ne correspond à aucun élément d\'évaluation.';
 $string['errincorrectidnumber'] = 'Erreur - identifiant incorrect';
+$string['errincorrectuseridnumber'] = 'Erreur - l\'identifiant «&nbsp;$a&nbsp;» du fichier d\'importation ne correspond à aucun utilisateur.';
 $string['fileurl'] = 'URL du fichier distant';
 $string['modulename'] = 'Fichier XML';
 $string['xml:view'] = 'Importer des notes en XML';
 $string['xml:publish'] = 'Publier les notes importées depuis XML';
+
 ?>
