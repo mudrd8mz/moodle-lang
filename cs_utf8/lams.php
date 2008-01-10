f41531961b3621c78b7aa0e4707e30de54afde24
David Mudrak <david.mudrak@gmail.com>
2008-01-10 00:52:31 +0000

    XHTML strict. Added missing string. Double dash replaced by minus sign.

--- cs_utf8/lams.php
+++ cs_utf8/lams.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // lams.php - created with Moodle 1.8 + (2007021502)
+      // lams.php - created with Moodle 1.9 Beta 3 (2007101506)
 
 
 $string['createSequence'] = 'Vytvořit novou sekvenci';
@@ -9,23 +9,26 @@ $string['introduction'] = 'Úvod';
 $string['lams'] = '-- LAMS - Learning Activity Management System --';
 $string['lams:manage'] = 'Správa aktivit LAMS';
 $string['lams:participate'] = 'Zapojit se do aktivit LAMS';
-$string['lamsoutline'] = 'LAMS – Přehled';
+$string['lamsoutline'] = 'LAMS - Přehled';
 $string['lesson'] = 'studijní setkání (session)';
 $string['modulename'] = 'LAMS';
 $string['modulenameplural'] = 'LAMS';
-$string['notsetup'] = 'NOT_SET_UP';
+$string['notsetup'] = 'NENASTAVENO';
 $string['openauthor'] = 'Otevřít LAMS Author';
 $string['openlearner'] = 'Otevřít LAMS Learner';
 $string['openmonitor'] = 'Otevřít LAMS Monitor';
 $string['refreshSequenceList'] = 'Aktualizovat seznam sekvencí';
 $string['selectExistingSequence'] = 'Vybrat stávají sekvenci nebo vytvořit novou sekvenci';
 $string['sequence'] = 'Vybrat sekvenci';
-$string['serverid'] = 'Vložit ID serveru získané od <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
-$string['serverkey'] = 'Vložit tajný klíč serveru získaný od <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
-$string['serverurl'] = 'Vložit základní URL pro přístup na server LAMS. Například http://localhost:8080/lams';
+$string['server_id'] = 'ID serveru';
+$string['server_key'] = 'Klíč serveru';
+$string['server_url'] = 'URL serveru';
+$string['serverid'] = 'Vložte ID serveru získané od <a href=\"http://www.lamsinternational.com\" target=\"_blank\">LAMS international</a>.';
+$string['serverkey'] = 'Vložte tajný klíč serveru získaný od <a href=\"http://www.lamsinternational.com\" target=\"_blank\">LAMS international</a>.';
+$string['serverurl'] = 'Vložte základní URL pro přístup na server LAMS. Například http://localhost:8080/lams';
 $string['useSequence'] = 'Použít vybranou sekvenci';
 $string['visibletostudents'] = 'Zobrazit činnost studentům';
-$string['wikistartederror'] = 'Ve Wiki už jsou příspěvky – změna není možná.';
+$string['wikistartederror'] = 'Ve Wiki už jsou příspěvky - změna není možná.';
 $string['workspace'] = 'Vybrat pracovní plochu';
 $string['wrongversionrange'] = '$a není správný údaj!';
 
