1a9acc75815f4d6b52b8a330a00e76b529a9fe29
koen roggemans <koenr>
2008-01-12 21:14:53 +0000

    errormessages import

--- nl_utf8/gradeimport_xml.php
+++ nl_utf8/gradeimport_xml.php
@@ -1,10 +1,13 @@
 <?PHP // $Id$ 
-      // gradeimport_xml.php - created with Moodle 1.9 Beta + (2007100903)
+      // gradeimport_xml.php - created with Moodle 2.0 dev (2007101506)
 
 
 $string['errbadxmlformat'] = 'Fout - XML slecht opgemaakt';
+$string['errduplicategradeidnumber'] = 'Fout - er zijn twee beoordelingsitems met idnummer \'$a\' in deze cursus. Dit is niet mogelijk.';
 $string['errduplicateidnumber'] = 'Fout - duplicaat IDnummer';
+$string['errincorrectgradeidnumber'] = 'Fout - idnummer \'$a\' van het importbestand komt met geen enkel beoordelingsitem overeen.';
 $string['errincorrectidnumber'] = 'Fout - fout IDnummer';
+$string['errincorrectuseridnumber'] = 'Fout - idnummer \'$a\' van het importbestand  heeft geen bijpassende gebruiker';
 $string['fileurl'] = 'URL bestand';
 $string['modulename'] = 'XML-bestand';
 $string['xml:publish'] = 'Publiceer importcijfers uit XML';
