d45dd65409757ec4f4e01e6b1685458bce6e909d
Lee Chan-young <yogibear270>
2008-01-14 09:37:24 +0000

    translation on added strings

--- ko_utf8/gradeimport_xml.php
+++ ko_utf8/gradeimport_xml.php
@@ -1,10 +1,13 @@
 <?PHP // $Id$ 
-      // gradeimport_xml.php - created with Moodle 1.9 Beta + (2007100500)
+      // gradeimport_xml.php - created with Moodle 2.0 dev (2007101506)
 
 
 $string['errbadxmlformat'] = '오류 - 잘못된 XML 형식';
+$string['errduplicategradeidnumber'] = '오류 - 이 강좌의 식별번호 \'$a\'를 갖는 점수 항목이 두개 입니다. 이것은 처리할 수 없습니다';
 $string['errduplicateidnumber'] = '오류 - 중복된 idnumber';
-$string['errincorrectidnumber'] = '오류- 틀린 idnumber';
+$string['errincorrectgradeidnumber'] = '오류 - 가져오는 파일의 식별번호 \'$a\'가 어떤 점수항목과도 일치하지 않음다음으로';
+$string['errincorrectidnumber'] = '오류- 틀린 식별번호';
+$string['errincorrectuseridnumber'] = '오류 - 가져오는 파일의 식별번호 \'$a\'가 어떤 사용자와도 일치하지 않음';
 $string['fileurl'] = '원격 파일 URL';
 $string['modulename'] = 'XML 파일 형식으로 가져옴';
 $string['xml:publish'] = 'XML으로부터 성적 가져오기 공개';
