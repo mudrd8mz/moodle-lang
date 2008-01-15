94437be8d9a3e035776f0a5a2c2b0a6da1cedc1c
Jaime Villate <villate>
2008-01-15 12:40:16 +0000

    Initial translation

---
+++ pt_utf8/notes.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 2.0 dev (2007101506)
+
+
+$string['addnewnote'] = 'Adicionar nova observação';
+$string['addnewnoteselect'] = 'Seleccionar utilizadores sobre os quais escrever observações';
+$string['bynameondate'] = 'por $a->name - $a->date';
+$string['content'] = 'Conteúdo';
+$string['course'] = 'Disciplina';
+$string['coursenotes'] = 'Observações da disciplina';
+$string['created'] = 'Criada';
+$string['deleteconfirm'] = 'Apagar esta observação?';
+$string['deletenotes'] = 'Apagar todas as observações';
+$string['editnote'] = 'Editar observação';
+$string['groupaddnewnote'] = 'Adicionar nova observação para todos';
+$string['nocontent'] = 'O conteúdo da observação não pode ficar vazio';
+$string['nonotes'] = 'Não existem ainda observações deste tipo';
+$string['note'] = 'Observação';
+$string['notes'] = 'Observações';
+$string['notesnotvisible'] = 'Não tem autorização para ver as observações';
+$string['nouser'] = 'Deverá seleccionar um utilizador';
+$string['personal'] = 'Pessoal';
+$string['personalnotes'] = 'Observações pessoais';
+$string['publishstate'] = 'Estado';
+$string['site'] = 'sítio';
+$string['sitenotes'] = 'Observações do sítio';
+$string['unknown'] = 'desconhecido';
+
+?>
