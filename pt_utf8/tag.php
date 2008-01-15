a0927f93a205b9f07e798acdc958138527baebb6
Jaime Villate <villate>
2008-01-15 13:21:16 +0000

    Initial translation

---
+++ pt_utf8/tag.php
@@ -0,0 +1,50 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 2.0 dev (2007101506)
+
+
+$string['addtagtomyinterests'] = 'Adicionar \"$a\" aos meus interesses';
+$string['blocktagstitle'] = 'Palavras chave';
+$string['changename'] = 'Modificar nome da palavra chave';
+$string['changetype'] = 'Modificar o tipo da palavra chave';
+$string['count'] = 'Número total';
+$string['delete'] = 'Apagar';
+$string['deleted'] = 'Apagada';
+$string['description'] = 'Descrição';
+$string['edittag'] = 'Editar esta palavra chave';
+$string['flag'] = 'Indicador';
+$string['flagasinappropriate'] = 'Indicar como inapropriada';
+$string['helprelatedtags'] = 'Palavras chave relacionadas, separadas por vírgulas';
+$string['id'] = 'Identificador';
+$string['managetags'] = 'Gerir palavras chaves';
+$string['name'] = 'Nome da palavra chave';
+$string['namesalreadybeeingused'] = 'Os nomes das palavras chave já estão a ser usados';
+$string['newname'] = 'Novo nome de palavra chave';
+$string['noresultsfor'] = 'Não existem resultados para \"$a\"';
+$string['owner'] = 'Proprietário';
+$string['relatedblogs'] = 'Entradas de blogue recentes com esta palavra chave';
+$string['relatedtags'] = 'Palavras chave relacionadas';
+$string['reset'] = 'Reiniciar';
+$string['resetflag'] = 'Indicador de reiniciar';
+$string['responsiblewillbenotified'] = 'Os responsável não será notificado';
+$string['search'] = 'Procurar';
+$string['searchresultsfor'] = 'Procurar resultados para \"$a\"';
+$string['searchtags'] = 'Procurar palavras chave';
+$string['seeallblogs'] = 'Ver todos os blogues com esta palavra chave';
+$string['select'] = 'Seleccionar';
+$string['tag'] = 'Palavra chave';
+$string['tagdescription'] = 'Descrição da palavra chave';
+$string['tags'] = 'Palavras chave';
+$string['tagsaredisabled'] = 'As palavras chaves estão deshabilitadas';
+$string['tagtype'] = 'Tipo de palavra chave';
+$string['tagtype_default'] = 'Por omissão';
+$string['tagtype_official'] = 'Oficial';
+$string['thingstaggedwith'] = '$a->count coisas associadas a \"$a->name\"';
+$string['thistaghasnodesc'] = 'Esta palavra chave não tem conteúdo actualmente';
+$string['timemodified'] = 'Modificada';
+$string['typechanged'] = 'Tipo da palavra chave modificado';
+$string['updated'] = 'Actualizada';
+$string['updatetag'] = 'Actualizar';
+$string['userstaggedwith'] = 'Utilizadores associados a \"$a\"';
+$string['withselectedtags'] = 'Com as palavras chaves seleccionadas...';
+
+?>
