a09dc4ba8ca0d2f930da9fb817e376675001f898
Ethem Evlice <ethem>
2008-01-21 20:18:11 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/filters.php
+++ tr_utf8/filters.php
@@ -1,8 +1,8 @@
 <?PHP // $Id$ 
-      // filters.php - created with Moodle 1.9 Beta + (2007083101)
+      // filters.php - created with Moodle 2.0 dev (2007101506)
 
 
-$string['actfilterhdr'] = 'Etkin süzgeçler (ORed dahil)';
+$string['actfilterhdr'] = 'Etkin süzgeçler';
 $string['addfilter'] = 'Süzgeç ekle';
 $string['anycategory'] = 'herhangi kategori';
 $string['anycourse'] = 'herhangi kurs';
@@ -12,22 +12,32 @@ $string['anyvalue'] = 'herhangi değer';
 $string['categoryrole'] = 'Kategori rolü';
 $string['contains'] = 'içerirse';
 $string['courserole'] = 'Kurs rolü';
+$string['datelabelisafter'] = '$a->label, $a->after\'dan sonradır';
+$string['datelabelisbefore'] = '$a->label, $a->before\'dan öncedir';
+$string['datelabelisbetween'] = '$a->label, $a->after ve $a->before arasındadır';
 $string['doesnotcontain'] = 'içermezse';
 $string['endswith'] = 'ile biterse';
 $string['firstaccess'] = 'İlk erişim';
-$string['isafter'] = '$a den sonraysa';
+$string['globalrolelabel'] = '$a->label, $a->value\'dir';
+$string['isafter'] = 'den sonraysa';
 $string['isanyvalue'] = 'herhangi bir değerse';
-$string['isbefore'] = '$a den önceyse';
-$string['isbetween'] = '$a[0]  ve $a[1] arasında';
+$string['isbefore'] = 'den önceyse';
 $string['isdefined'] = 'tanımlıysa';
 $string['isempty'] = 'boşsa';
 $string['isequalto'] = 'eşitse';
 $string['isnotdefined'] = 'tanımlanmamışsa';
 $string['isnotequalto'] = 'eşit değilse';
 $string['newfilter'] = 'Yeni süzgeç';
+$string['profilelabel'] = '$a->label: $a->profile $a->operator $a->value';
+$string['profilelabelnovalue'] = '$a->label: $a->profile $a->operator';
 $string['removeall'] = 'Tümünü sil';
 $string['removeselected'] = 'Seçiliyi sil';
-$string['setfilter'] = 'Süzgeç ayarla';
+$string['selectlabel'] = '$a->label $a->operator $a->value';
 $string['startswith'] = 'ile başlarsa';
+$string['tablenosave'] = 'Yukarıdaki tablodaki değişiklikler otomatikmen kaydedilir.';
+$string['textlabel'] = '$a->label $a->operator $a->value';
+$string['textlabelnovalue'] = '$a->label $a->operator';
+$string['isbetween'] = '$a[0]  ve $a[1] arasında'; // ORPHANED
+$string['setfilter'] = 'Süzgeç ayarla'; // ORPHANED
 
 ?>
