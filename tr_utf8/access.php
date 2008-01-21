de7155e1db3f1794618ca33c607360eb3c4708b4
Ethem Evlice <ethem>
2008-01-21 18:06:45 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/access.php
+++ tr_utf8/access.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // access.php - created with Moodle 2.0 dev (2007101504)
+      // access.php - created with Moodle 2.0 dev (2007101506)
 
 
 $string['access'] = 'Erişilebilirlik';
@@ -11,9 +11,10 @@ $string['activityprev'] = 'Önceki etkinlik';
 $string['breadcrumb'] = 'Yol haritası';
 $string['currenttopic'] = 'Bu konu';
 $string['currentweek'] = 'Bu hafta';
+$string['hideblocka'] = '$a bloğunu sakla';
 $string['monthnext'] = 'Sonraki ay';
 $string['monthprev'] = 'Önceki ay';
-$string['showhideblock'] = 'Bloğu gizle/göster';
+$string['showblocka'] = '$a bloğunu göster';
 $string['sitemap'] = 'Site haritası';
 $string['skipa'] = '$a atla';
 $string['skipblock'] = 'Bloğa atla';
@@ -23,5 +24,6 @@ $string['tablelayout'] = 'Şablon tablo, $a';
 $string['tocontent'] = 'Ana içeriğe git';
 $string['tonavigation'] = 'Dolaşıma git';
 $string['youarehere'] = 'Buradasınız';
+$string['showhideblock'] = 'Bloğu gizle/göster'; // ORPHANED
 
 ?>
