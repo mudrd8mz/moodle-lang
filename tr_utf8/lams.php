17b23f9160579d1364819c06bef170f38b99de8b
Ethem Evlice <ethem>
2008-01-22 15:25:44 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/lams.php
+++ tr_utf8/lams.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // lams.php - created with Moodle 1.7 dev (2006092410)
+      // lams.php - created with Moodle 2.0 dev (2007101506)
 
 
 $string['createSequence'] = 'Yeni sahne oluştur';
@@ -20,8 +20,11 @@ $string['openmonitor'] = 'LAMS Monitor\'u Aç';
 $string['refreshSequenceList'] = 'Sahne listesini yenile';
 $string['selectExistingSequence'] = 'Var olan bir sahneyi seçin veya yeni bir sahne oluşturun.';
 $string['sequence'] = 'Sahne Seç';
-$string['serverid'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alınan Sunucu Kimliğini girin.';
-$string['serverkey'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alınan Sunucu Anahtarını girin.';
+$string['server_id'] = 'Sunucu ID';
+$string['server_key'] = 'Sunucu anahtarı';
+$string['server_url'] = 'Sunucu URL';
+$string['serverid'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alınan Sunucu ID girin.';
+$string['serverkey'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alınan sunucu anahtarını girin.';
 $string['serverurl'] = 'LAMS sunucusuna erişim için ana adresi girin. Örnek: http://localhost:8080/lams';
 $string['useSequence'] = 'Seçili sahneyi kullan';
 $string['visibletostudents'] = 'Etkinliği öğrencilere göster';
