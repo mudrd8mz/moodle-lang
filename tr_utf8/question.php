a18eb04391c88f4a00281d53cf5fed6c4432f83d
Ethem Evlice <ethem>
2008-01-22 15:59:58 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/question.php
+++ tr_utf8/question.php
@@ -1,12 +1,53 @@
 <?PHP // $Id$ 
-      // question.php - created with Moodle 1.8 dev (2006120700)
+      // question.php - created with Moodle 2.0 dev (2007101506)
 
 
+$string['broken'] = 'Bu bir \"kırık bağlantı\"dır. Olmayan bir dosyayı gösteriyor.';
+$string['byandon'] = '<em>$a->time</em> tarihinde <em>$a->user</em> tarafından';
+$string['categorycurrent'] = 'Şimdiki Kategori';
+$string['categorycurrentuse'] = 'Bu Kategoriyi Kullan';
 $string['categorydoesnotexist'] = 'Bu kategori yok';
+$string['categorymoveto'] = 'Kategoride Sakla';
+$string['changepublishstatuscat'] = '\"$a->coursename\" kursundaki <a href=\"$a->caturl\">\"$a->name\" kategorisi</a>nin paylaşım durumu $a->changefrom, <strong>$a->changeto</strong> olarak değiştirildi.';
+$string['copy'] = 'Buradan $a kopyala ve linkleri değiştir.';
+$string['created'] = 'Oluşturma';
+$string['createdmodifiedheader'] = 'Oluşturma / Son Kaydetme';
+$string['defaultfor'] = '$a için varsayılan';
+$string['defaultinfofor'] = 'Sorular için varsayılan kategori \'$a\' bağlamında paylaşıldı.';
+$string['donothing'] = 'Dosyaları kopyalama/taşıma veya linkleri değiştirme.';
+$string['editingcategory'] = 'Kategori düzenleniyor';
 $string['editingquestion'] = 'Soru düzenleniyor';
+$string['erroraccessingcontext'] = 'Bağlama erişilemedi';
+$string['errorfilecannotbecopied'] = '$a dosyasını kopyalarken hata.';
+$string['errorfilecannotbemoved'] = '$a dosyasını taşırken hata.';
+$string['exportcategory'] = 'Kategoriyi dışa ver';
+$string['filesareacourse'] = 'kurs dosyaları alanı';
+$string['filesareasite'] = 'site dosyaları alanı';
+$string['filestomove'] = 'Dosyalar buraya taşınsın/kopyalansın mı:  $a?';
+$string['fractionsnomax'] = 'Bu sorudan tam not alınabilmesi için sorulardan en az bir tanesinin puanı %%100 olması gerekir.';
+$string['getcategoryfromfile'] = 'Kategoriyi dosyadan getir';
+$string['getcontextfromfile'] = 'Bağlamı dosyadan getir';
+$string['ignorebroken'] = 'Kırık linkleri yok say';
+$string['linkedfiledoesntexist'] = '$a bağlantılı dosyası yok';
 $string['missingimportantcode'] = 'Bu soru tipinin önemli bir kodu eksik: $a.';
+$string['modified'] = 'Son kaydetme';
+$string['movecategory'] = 'Kategori Taşı';
+$string['moveq'] = 'Soruları taşı';
+$string['moveqtoanothercontext'] = 'Soruyu başka bağlama taşı';
+$string['movingcategory'] = 'Kategori Taşınıyor';
 $string['notenoughdatatoeditaquestion'] = 'Ne soru id, ne de kategori id ve soru tipi belirtilmedi.';
+$string['permissionedit'] = 'Bu soruyu düzenle';
+$string['permissionmove'] = 'Bu soruyu taşı';
+$string['permissionsaveasnew'] = 'Bu soruyu yeni soru olarak kaydet';
+$string['permissionto'] = 'İzinleriniz:';
+$string['published'] = 'paylaşıldı';
+$string['questionbank'] = 'Soru bankası';
+$string['questioncatsfor'] = '\'$a\' için Soru Kategorileri';
 $string['questiondoesnotexist'] = 'Bu soru yok';
+$string['tofilecategory'] = 'Kategoriyi dosyaya yaz';
+$string['tofilecontext'] = 'Bağlamı dosyaya yaz';
+$string['unknown'] = 'Bilinmeyen';
 $string['unknownquestiontype'] = 'Bilinmeyen soru tipi: $a.';
+$string['unpublished'] = 'paylaşılmadı';
 
 ?>
