0e60fffb9e11f174f87b0f331f99c65983054a77
Adam Pawelczak <adamtpaw>
2008-01-28 09:33:56 +0000

    New file for 1.9

---
+++ pl_utf8/qtype_datasetdependent.php
@@ -0,0 +1,43 @@
+<?PHP // $Id$ 
+      // qtype_datasetdependent.php - created with Moodle 1.9 Beta 3 (2007101506)
+      // local modifications from http://localhost/moodle
+
+
+$string['additem'] = 'Dodaj element';
+$string['atleastonerealdataset'] = 'Treść pytania powinna zawierać co najmniej jeden rzeczywisty zestaw';
+$string['atleastonewildcard'] = 'W formule odpowiedzi powinno pojawić się co najmniej jedno wyrażenie zastępcze';
+$string['calcdistribution'] = 'Rozkład';
+$string['calclength'] = 'Miejsca dziesiętne';
+$string['calcmax'] = 'Maksimum';
+$string['calcmin'] = 'Minimum';
+$string['datasetrole'] = 'Wyrażenia zastępcze <strong>{x..}</strong> zostaną zastąpione przez wartość liczbową z przypisanego im zestawu';
+$string['deletelastitem'] = 'Usuń ostatni element';
+$string['existingcategory1'] = 'literał z istniejącego zestawu literałów używanych przez inne pytania w tej kategorii';
+$string['existingcategory2'] = 'plik z istniejącego zestawu plików używanych przez inne pytania w tej kategorii';
+$string['existingcategory3'] = 'łącze z istniejącego zestawu łączy używanych przez inne pytania w tej kategorii';
+$string['forceregeneration'] = 'wymuś regenerację';
+$string['getnextnow'] = 'Wygeneruj nowy \'element do dodania\'';
+$string['item(s)'] = 'Element(y)';
+$string['itemno'] = 'Element $a';
+$string['itemscount'] = 'Liczba<br/>elementów';
+$string['itemtoadd'] = 'Element do dodania';
+$string['keptcategory1'] = 'literał z tej samej';
+$string['lastitem(s)'] = 'Ostatni(e) element(y)';
+$string['minmax'] = 'Zakres wartości';
+$string['newcategory1'] = 'literał z nowego zestawu literałów, może być również użyty w innych pytaniach w tej kategorii';
+$string['newcategory2'] = 'plik z nowego zestawu plików, może być również użyty w innych pytaniach w tej kategorii';
+$string['newcategory3'] = 'łącze z nowego zestawu łączy, może być użyty w innych pytaniach w tej kategorii';
+$string['newlocal1'] = 'literał z nowego zestawu literałów, będzie użyty tylko w tym pytaniu';
+$string['newlocal2'] = 'plik z nowego zestawu plików, będzie użyty tylko w tym pytaniu';
+$string['newlocal3'] = 'łącze z nowego zestawu łączy, będzie użyte tylko w tym pytaniu';
+$string['nodataset'] = 'nic - to nie jest wyrażenie zastępcze';
+$string['param'] = 'Parametr {<strong>$a</strong>}';
+$string['replacewithrandom'] = 'Zastąp losową wartością';
+$string['reuseifpossible'] = 'użyj ponownie poprzedniej wartości, jeśli dostępna';
+$string['sharedwildcard'] = 'współdzielone wyrażenie zastępcze';
+$string['sharedwildcards'] = 'Współdzielone wyrażenia zastępcze';
+$string['uniform'] = 'Jednostajny';
+$string['updatedatasetparam'] = 'Aktualizuj parametry zestawu';
+$string['youmustaddatleastoneitem'] = 'Musisz dodać przynajmniej jeden element zestawu zanim zapiszesz to pytanie.';
+
+?>
