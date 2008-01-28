edcabab07655fe4aa6c2bc72724a4eaabb906990
Mitsuhiro Yoshida <mits>
2008-01-28 04:47:42 +0000

    Brushed up a translated string for choice.

--- ja_utf8/choice.php
+++ ja_utf8/choice.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // choice.php - created with Moodle 2.0 dev (2007101506)
+      // choice.php - created with Moodle 2.0 dev (2007101507)
 
 
 $string['addmorechoices'] = '選択肢の追加';
@@ -34,7 +34,7 @@ $string['notopenyet'] = '申し訳ございません、この活動は $a まで
 $string['privacy'] = '投票結果のプライバシー';
 $string['publish'] = '投票結果の公開';
 $string['publishafteranswer'] = '投票した後、学生に投票結果を表示する';
-$string['publishafterclose'] = 'この投票が終了した後、学生に投票結果を表示する';
+$string['publishafterclose'] = 'この投票全体が終了した場合のみ、学生に投票結果を表示する';
 $string['publishalways'] = '常に学生に投票結果を表示する';
 $string['publishanonymous'] = '投票結果を匿名で公開する、学生の氏名を表示しない';
 $string['publishnames'] = '投票結果をすべて公開する、学生の氏名および投票内容を表示する';
