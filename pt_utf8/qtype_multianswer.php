bc50a091bdf4ad0bdcb49bdc6739d3fca9d25420
Jaime Villate <villate>
2008-01-29 21:10:03 +0000

    Initial translation

---
+++ pt_utf8/qtype_multianswer.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.9 Beta 4 (2007101506)
+
+
+$string['decodeverifyquestiontext'] = 'DESCODIFIQUE E VERIFQUE O TEXTO DA PERGUNTA';
+
+?>
