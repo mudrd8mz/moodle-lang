6fc0bca44aa2a21c196adec2c4be518747a906fd
Miroslav Fikar <fikar>
2008-02-02 16:47:34 +0000

    Globalne vyhladavanie - prva verzia

---
+++ sk_utf8/search.php
@@ -0,0 +1,60 @@
+<?PHP // $Id$ 
+      // search.php - created with Moodle 1.8.4+ (2007021541)
+
+
+$string['advancedsearch'] = 'Pokročilé vyhľadávanie';
+$string['all'] = 'Všetko';
+$string['author'] = 'Autor';
+$string['authorname'] = 'Meno autora';
+$string['back'] = 'Späť';
+$string['beadmin'] = 'Táto stránka vyžaduje prihlásenie administrátora.';
+$string['checkdb'] = 'Skontrolovať databázu';
+$string['checkdbadvice'] = 'Skontrolujte databázu v prípade problémov.';
+$string['checkdir'] = 'Skontrolovať adresár';
+$string['checkdiradvice'] = 'Zabezpečte, aby existoval dátový adresár a bolo možné doň zapisovať.';
+$string['commenton'] = 'Comment on';
+$string['createanindex'] = 'vytvorenie indexu';
+$string['createdon'] = 'Vytvorený';
+$string['database'] = 'Databáza';
+$string['databasestate'] = 'Stav indexovej databázy';
+$string['datadirectory'] = 'Dátový adresár';
+$string['deletionsinindex'] = 'Vymazania v indexe';
+$string['doctype'] = 'Typ dokumentu';
+$string['documents'] = 'dokumentoch';
+$string['documentsfor'] = 'Dokumenty pre';
+$string['documentsindatabase'] = 'Dokumenty v databáze';
+$string['documentsinindex'] = 'Dokumenty v indexe';
+$string['duration'] = 'Trvanie';
+$string['emptydatabaseerror'] = 'Databázová tabuľka neexistuje alebo neobsahuje položky indexu.';
+$string['enteryoursearchquery'] = 'Zadávanie vyhľadávaného textu';
+$string['errors'] = 'Chyby';
+$string['filesinindexdirectory'] = 'Súbory v adresári indexu';
+$string['globalsearchdisabled'] = 'Globálne vyhľadávanie nie je povolené.';
+$string['invalidindexerror'] = 'Indexovací adresár obsahuje neplatný index alebo vôbec nič.';
+$string['ittook'] = 'Trvalo ';
+$string['next'] = 'Ďalej';
+$string['noindexmessage'] = 'Admin: Zdá sa, že chýba vyhľadávací index. Prosím';
+$string['normalsearch'] = 'Normálne vyhľadávanie';
+$string['openedon'] = 'otvorený dňa';
+$string['resultsreturnedfor'] = 'výsledkov vyhľadávania nájdených pre';
+$string['runindexer'] = 'Spustiť indexovanie (skutočné)';
+$string['runindexertest'] = 'Spustiť testovanie indexovania';
+$string['score'] = 'Skóre';
+$string['search'] = 'Vyhľadávanie';
+$string['searching'] = 'Vyhľadávanie v ...';
+$string['seconds'] = ' sekúnd';
+$string['solutions'] = 'Riešenia';
+$string['statistics'] = 'Štatistiky';
+$string['thesewordshelpimproverank'] = 'Tieto slová pomôžu zlepšiť rank';
+$string['thesewordsmustappear'] = 'Tieto slová musia byť';
+$string['thesewordsmustnotappear'] = 'Tieto slová nesmú byť';
+$string['title'] = 'Názov';
+$string['tofetchtheseresults'] = 'nájsť tieto výsledky';
+$string['totalsize'] = 'Celková veľkosť';
+$string['type'] = 'Typ';
+$string['uncompleteindexingerror'] = 'Indexovanie nebolo úspešne ukončené, reštartujte ho.';
+$string['versiontoolow'] = 'Globálne vyhľadávanie vyžaduje PHP 5.0.0 alebo novšie (používate verziu )';
+$string['whichmodulestosearch?'] = 'Moduly, v ktorých sa bude vyhľadávať:';
+$string['wordsintitle'] = 'Slová v nadpise';
+
+?>
