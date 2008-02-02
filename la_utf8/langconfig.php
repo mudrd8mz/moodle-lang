71d25c9ea2d5cbf6a650de891de131c753c8db07
koen roggemans <koenr>
2008-02-02 11:06:14 +0000

    New language pack: Latin GHOP project. by Nick (novanasa@gmail.com)

---
+++ la_utf8/langconfig.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // langconfig.php - created with Moodle 1.7 beta + (2006101003)
+
+
+$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,Q,R,S,T,U,V,X,Y,Z,Ā,Ē,Ī,Ō,Ū,Ȳ,Æ';
+$string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
+$string['firstdayofweek'] = '0';
+$string['locale'] = 'la.UTF-8';
+$string['localewin'] = 'English_Australia.1252';
+$string['localewincharset'] = '';
+$string['oldcharset'] = 'ISO-8859-1';
+$string['strftimedate'] = '%%d %%B %%Y';
+$string['strftimedateshort'] = '%%d %%B';
+$string['strftimedatetime'] = '%%d %%B %%Y, %%I:%%M %%p';
+$string['strftimedaydate'] = '%%A, %%d %%B %%Y';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
+$string['strftimedayshort'] = '%%A, %%d %%B';
+$string['strftimedaytime'] = '%%a, %%H:%%M';
+$string['strftimemonthyear'] = '%%B %%Y';
+$string['strftimerecent'] = '%%d %%b, %%H:%%M';
+$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%I:%%M %%p';
+$string['strftimetime'] = '%%I:%%M %%p';
+$string['thischarset'] = 'UTF-8';
+$string['thisdirection'] = 'ltr';
+$string['thislanguage'] = 'Latin';
+$string['parentlanguage'] = 'en_utf8';
+
+?>
