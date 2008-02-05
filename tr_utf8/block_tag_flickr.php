cee4bfa86491b19360f1d51d8855fe48f2cbef14
Ethem Evlice <ethem>
2008-02-05 09:41:44 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/block_tag_flickr.php
+++ tr_utf8/block_tag_flickr.php
@@ -1,9 +1,20 @@
 <?PHP // $Id$ 
-      // block_tag_flickr.php - created with Moodle 2.0 dev (2007101503)
+      // block_tag_flickr.php - created with Moodle 2.0 dev (2007101508)
 
 
 $string['blockname'] = 'Flickr';
+$string['configtitle'] = 'Başlık';
+$string['date-posted-asc'] = 'Gönderim tarihi ARTAN';
+$string['date-posted-desc'] = 'Gönderim tarihi AZALAN';
+$string['date-taken-asc'] = 'Alma tarihi ARTAN';
+$string['date-taken-desc'] = 'Alma tarihi AZALAN';
 $string['defaulttile'] = 'Flickr';
+$string['getfromphotoset'] = 'Bu fotoğraf albümünün fotoğraflarını getir';
+$string['includerelatedtags'] = 'Sorguda ilgili etiketleri dahil et';
+$string['interestingness-asc'] = 'İlgi ARTAn';
+$string['interestingness-desc'] = 'İlgi AZALAN';
+$string['numberofphotos'] = 'Fotoğraf sayısı';
+$string['relevance'] = 'İlgi';
 $string['sortby'] = 'Sırala';
 
 ?>
