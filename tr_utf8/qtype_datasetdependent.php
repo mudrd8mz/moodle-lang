f3dd6398664b4da0c501a7ce9107db9c8010e47e
Ethem Evlice <ethem>
2008-02-05 10:42:57 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/qtype_datasetdependent.php
+++ tr_utf8/qtype_datasetdependent.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // qtype_datasetdependent.php - created with Moodle 1.8 dev (2007012500)
+      // qtype_datasetdependent.php - created with Moodle 2.0 dev (2007101508)
 
 
 $string['additem'] = 'Öge ekle';
@@ -10,8 +10,11 @@ $string['calcmin'] = 'En az';
 $string['deletelastitem'] = 'Son ögeyi sil';
 $string['forceregeneration'] = 'yenilemeye zorla';
 $string['getnextnow'] = '\'Eklenecek yeni öge\'yi getir';
-$string['itemno'] = '$a ögesi';
+$string['item(s)'] = 'öge(ler)';
+$string['itemno'] = 'Öge $a';
+$string['itemscount'] = 'Öge<br />Sayı';
 $string['itemtoadd'] = 'Eklenecek öge';
+$string['lastitem(s)'] = 'son öge(ler)';
 $string['minmax'] = 'Değerler farkı';
 $string['param'] = 'Parametre {<strong>$a</strong>}';
 $string['replacewithrandom'] = 'Rasgele bir değerle değiştir';
--- tr_utf8/qtype_numerical.php
+++ tr_utf8/qtype_numerical.php
@@ -1,10 +1,13 @@
 <?PHP // $Id$ 
-      // qtype_numerical.php - created with Moodle 1.8 dev (2007012500)
+      // qtype_numerical.php - created with Moodle 2.0 dev (2007101508)
 
 
-$string['addmoreanswerblanks'] = 'Daha fazla Cevap {yok} için boşluk';
-$string['addmoreunitblanks'] = 'Daha fazla Birim {yok} için boşluk';
-$string['answerno'] = '$a cevabı';
+$string['addmoreanswerblanks'] = 'Daha fazla {no} Cevap için boşluk';
+$string['addmoreunitblanks'] = 'Daha fazla {no} Birim için boşluk';
+$string['answermustbenumberorstar'] = 'Cevap sayı veya \'*\' olmalı.';
+$string['answerno'] = 'Cevap $a';
+$string['errornomultiplier'] = 'Bu birim için bir çarpan belirtmelisiniz.';
+$string['errorrepeatedunit'] = 'Aynı isimle iki birim olamaz.';
 $string['notenoughanswers'] = 'En az bir cevap girmelisiniz.';
 $string['unithdr'] = '$a birim';
 
