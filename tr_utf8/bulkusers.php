3c7eeb8ca8362b871b80d973f5080ec9c418db8d
Ethem Evlice <ethem>
2008-02-07 10:40:54 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/bulkusers.php
+++ tr_utf8/bulkusers.php
@@ -1,15 +1,24 @@
 <?PHP // $Id$ 
-      // bulkusers.php - created with Moodle 2.0 dev (2007101503)
+      // bulkusers.php - created with Moodle 2.0 dev (2007101508)
 
 
-$string['addall'] = 'Tümünü seçime ekle';
+$string['addall'] = 'Tümünü ekle';
 $string['addsel'] = 'Seçime ekle';
-$string['deleteall'] = 'Tüm kullanıcıları temizle';
-$string['deletesel'] = 'Seçili kullanıcıları temizle';
+$string['allfilteredusers'] = 'Tüm filtrelenmişler ($a->count/$a->total)';
+$string['allselectedusers'] = 'Tüm seçililer ($a->count/$a->total)';
+$string['allusers'] = 'Tüm kullanıcılar ($a)';
+$string['available'] = 'Seçilebilir';
+$string['confirmmessage'] = 'Bu kullanıcılara yukarıdaki mesajı göndermek istediğinizden emin misiniz?<br />$a';
+$string['nofilteredusers'] = 'Kullanıcı bulunamadı (0/$a)';
+$string['noselectedusers'] = 'Seçili kullanıcı yok';
+$string['removeall'] = 'Tümünü kaldır';
+$string['removesel'] = 'Seçimden kaldır';
 $string['selected'] = 'Seçili';
-$string['selectedlist'] = 'Seçili kullanıcılarla...';
+$string['selectedlist'] = 'Seçili kullanıcı listesi...';
 $string['usersfound'] = '$a kullanıcı bulundu.';
 $string['usersinlist'] = 'Listedeki kullanıcılar';
 $string['usersselected'] = '$a kullanıcı seçildi.';
+$string['deleteall'] = 'Tüm kullanıcıları temizle'; // ORPHANED
+$string['deletesel'] = 'Seçili kullanıcıları temizle'; // ORPHANED
 
 ?>
