37e45285c5fcf2902e2e4adc16ab25bdbffa2cbc
Ethem Evlice <ethem>
2008-02-07 10:25:16 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/block_online_users.php
+++ tr_utf8/block_online_users.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_online_users.php - created with Moodle 1.8 Beta (2007020400)
+      // block_online_users.php - created with Moodle 2.0 dev (2007101508)
 
 
 $string['blockname'] = 'Çevrimiçi Kullanıcılar';
-$string['configtimetosee'] = 'Bir kullanıcı, hiçbir etkinlik göstermediğinde kaç dakika sonra çevrimdışı sayılacak?';
+$string['configtimetosee'] = 'Bir kullanıcı, çevrimiçi olarak etkinlik göstermediği algılandıktan sonra dakika olarak geçecek süre';
 $string['periodnminutes'] = 'Son $a dakika';
+$string['timetosee'] = 'Etkinlik göstermediğinde sil (dakika)';
 
 ?>
