a0ed35910dc8920334c6480050e7a60f45390bc6
Ethem Evlice <ethem>
2008-02-07 10:08:02 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/auth_mnet.php
+++ tr_utf8/auth_mnet.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // auth_mnet.php - created with Moodle 1.8 dev (2007012500)
+      // auth_mnet.php - created with Moodle 2.0 dev (2007101508)
 
 
 $string['sso_idp_name'] = 'SSO (Kimlik Sağlayıcı)';
+$string['sso_mnet_login_refused'] = '$a[0] kullanıcısı, burdan, $a[1], giriş yapmaya yetkili değil.';
 $string['sso_sp_name'] = 'SSO (Hizmet Sağlayıcı)';
 
 ?>
