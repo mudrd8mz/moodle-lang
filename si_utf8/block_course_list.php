d59da52b52fe2a2ef813f55c7f066c6e633b2f47
Malinda Siriwardana <malindahs>
2008-02-14 03:33:05 +0000

    malinda

--- si_utf8/block_course_list.php
+++ si_utf8/block_course_list.php
@@ -1,11 +1,13 @@
 <?PHP // $Id$ 
-      // block_course_list.php - created with Moodle 1.8.2+ (2007021520)
+      // block_course_list.php - created with Moodle 2.0 dev (2007101508)
 
 
-$string['allcourses'] = 'පරිපාලක සියලුම පාඨමාලා දකී';
+$string['adminview'] = 'පරිපාලකවරයාගේ දර්ශනය';
+$string['allcourses'] = 'පරිපාලකවරයා සියලුම පාඨමාලා දකී';
 $string['blockname'] = 'පාඨමාලා ලයිස්තුව';
-$string['configadminview'] = 'පරිපාලක විසින් පාඨමාලා ලයිස්තු‍ කොටසේ කුමක් දැකිය යුතුද?';
+$string['configadminview'] = 'පරිපාලකවරයා විසින් පාඨමාලා ලයිස්තු‍ කොටසේ කුමක් දැකිය යුතුද?';
 $string['confighideallcourseslink'] = 'කොටසේ පතුලේ ඇති \"සියළු පාඨමාලා\" සබැදිය සඟවන්න';
-$string['owncourses'] = 'පරිපාලක තමන්ගේ පාඨමාලා පමණක් දකී';
+$string['hideallcourseslink'] = 'සියලුම පාඨමාලා සබැදියන් සඟවන්න';
+$string['owncourses'] = 'පරිපාලකවරයා තමන්ගේ පාඨමාලා පමණක් දකී';
 
 ?>
--- si_utf8/block_online_users.php
+++ si_utf8/block_online_users.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_online_users.php - created with Moodle 1.8.2+ (2007021520)
+      // block_online_users.php - created with Moodle 2.0 dev (2007101508)
 
 
 $string['blockname'] = 'සබැදි පරිශීලකයන්';
 $string['configtimetosee'] = 'පරිශීලකයන් තවදුරටත් සබැදී ඇති ලෙස නොසලකා හරිනු ලැබීම සදහා වූ අක්‍රියශීලී කාලසීමාව මිනිත්තු වලින්';
 $string['periodnminutes'] = 'අවසන් මිනිත්තු $a';
+$string['timetosee'] = 'අක්‍රියශීලී වීමෙන් පසු ඉවත් කරන්න (මිනිත්තු)';
 
 ?>
