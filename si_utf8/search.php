ef0d2128d787acd920cf3aae0ea5c500d14272d5
Malinda Siriwardana <malindahs>
2008-02-15 03:31:14 +0000

    malinda

---
+++ si_utf8/search.php
@@ -0,0 +1,48 @@
+<?PHP // $Id$ 
+      // search.php - created with Moodle 2.0 dev (2007101508)
+
+
+$string['advancedsearch'] = 'උසස් සෙවීම';
+$string['all'] = 'සියලු';
+$string['author'] = 'කර්තෘ';
+$string['authorname'] = 'කර්තෘ නම';
+$string['back'] = 'ආපසු';
+$string['checkdb'] = 'දත්තගබඩාව පරීක්ෂා කරන්න';
+$string['checkdbadvice'] = 'ඕනෑම ගැ‍ටලුවක් සඳහා ඔබගේ දත්තගබඩාව පරීක්ෂා කරන්න';
+$string['checkdir'] = 'ඩිරෙක්ටරිය පරීක්ෂා කරන්න';
+$string['commenton'] = 'විස්තර/විවේචන කරන්න';
+$string['createanindex'] = 'සූචියක් සාදන්න';
+$string['createdon'] = 'සාදන ලද්දේ';
+$string['database'] = 'දත්ත ගබජාව';
+$string['databasestate'] = 'දත්තගබඩා තත්වය සූචිගත කිරීම';
+$string['datadirectory'] = 'දත්ත ඩිරෙක්ටරිය';
+$string['deletionsinindex'] = 'සූචියෙන් ඉවත් කරන්න';
+$string['doctype'] = 'ලේඛන ප්‍රවර්ගය';
+$string['documents'] = 'ලේඛන';
+$string['documentsfor'] = 'සඳහා ලේඛන';
+$string['documentsindatabase'] = 'දත්තගබඩාවේ ඇති ලේඛන';
+$string['documentsinindex'] = 'සූචියේ ඇති ලේඛන';
+$string['duration'] = 'කාලසීමාව';
+$string['enteryoursearchquery'] = 'ඔබගේ සෙවීම් විමසුම ඇතුලත් කරන්න';
+$string['errors'] = 'දෝෂ';
+$string['ittook'] = 'එය ගන්නා ලදි';
+$string['next'] = 'මීලඟ';
+$string['noindexmessage'] = 'පරිපාලකවරයා:එවැනි සූචියක් ඇති බව නොපෙනේ. කරුණාකර';
+$string['normalsearch'] = 'සාමාන්‍ය සෙවීම';
+$string['openedon'] = 'විවෘත කරන ලද්දේ';
+$string['score'] = 'ලකුණු';
+$string['search'] = 'සෙවීම';
+$string['searching'] = 'තුල සෙවීම';
+$string['seconds'] = 'තප්පර';
+$string['solutions'] = 'විසඳුම';
+$string['statistics'] = 'සංඛ්‍ය ලේඛන';
+$string['thesewordsmustappear'] = 'මෙම වචන දර්ශනය විය යුතුයි';
+$string['thesewordsmustnotappear'] = 'මෙම වචන දර්ශනය නොවිය යුතුයු';
+$string['title'] = 'මාතෘකාව';
+$string['tofetchtheseresults'] = 'මෙම ප්‍රතිඵල ඇතුලත් කිරීම සඳහා';
+$string['totalsize'] = 'සම්පූර්ණ ප්‍රමාණය';
+$string['type'] = 'ප්‍රවර්ගය';
+$string['whichmodulestosearch?'] = 'කවර මොඩියුලයක් සෙවීම සඳහාද?';
+$string['wordsintitle'] = 'මාතෘකාවේ ඇති වචන';
+
+?>
---
+++ si_utf8/tag.php
@@ -0,0 +1,23 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 2.0 dev (2007101508)
+
+
+$string['blocktagstitle'] = 'ටැගයන්';
+$string['changename'] = 'ටැගයේ නම වෙනස් කරන්න';
+$string['changetype'] = 'ටැගයේ වර්ගය වෙනස් කරන්න';
+$string['count'] = 'එකතුව';
+$string['delete'] = 'ඉවත් කරන්න';
+$string['deleted'] = 'ඉවත් කරන ලදි';
+$string['description'] = 'විස්තරය';
+$string['edittag'] = 'මෙම ටැගය සංස්කරණය කරන්න';
+$string['flag'] = 'සංඥා';
+$string['flagasinappropriate'] = 'නුසුදුසු ලෙස සංඥා කරන්න';
+$string['helprelatedtags'] = 'කොමාවකින් වෙන්කරන ලද සම්බන්ධිත ටැගයන්';
+$string['id'] = 'හැදුනුම් අංකය';
+$string['managetags'] = 'ටැගයන් කළමනාකරණය';
+$string['name'] = 'ටැගයේ නම';
+$string['namesalreadybeeingused'] = 'ටැග නාමයන් දැනටමත් භාවිතා කර ඇත';
+$string['newname'] = 'නව ටැග නම';
+$string['owner'] = 'අයිතිකරු';
+
+?>
