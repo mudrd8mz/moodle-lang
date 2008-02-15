8fe2c8b2c2fab59e52fab6f872f9a14ee73d8a65
Malinda Siriwardana <malindahs>
2008-02-15 04:47:32 +0000

    malinda

---
+++ si_utf8/filters.php
@@ -0,0 +1,43 @@
+<?PHP // $Id$ 
+      // filters.php - created with Moodle 2.0 dev (2007101508)
+
+
+$string['actfilterhdr'] = 'සක්‍රිය පෙරනයන්';
+$string['addfilter'] = 'පෙරනයක් එක් කරන්න';
+$string['anycategory'] = 'ඕනෑම ප්‍රභේදයක්';
+$string['anycourse'] = 'ඕනෑම පාඨමාලාවක්';
+$string['anyfield'] = 'ඕනෑම ක්ෂේත්‍රයක්';
+$string['anyrole'] = 'ඕනෑම භූමිකාවක්';
+$string['anyvalue'] = 'ඕනෑම අගයක්';
+$string['categoryrole'] = 'ප්‍රභේද භූමිකා';
+$string['contains'] = 'අඩංගුවේ';
+$string['courserole'] = 'පාඨමාලා භූමිකාව';
+$string['courserolelabel'] = '$a->categoryname යටතේ ඇති $a->coursename වේ $a->label ය $a->rolename කි';
+$string['courserolelabelerror'] = '$a->label දෝෂය: $a->coursename පාඨමාලාව නොපවතී';
+$string['datelabelisafter'] = '$a->after ට පසු $a->label ය';
+$string['datelabelisbefore'] = '$a->before ට පෙර $a->label ය';
+$string['datelabelisbetween'] = '$a->label ය $a->after ට පසු හා $a->before ට පෙර';
+$string['doesnotcontain'] = 'නොපවතී';
+$string['endswith'] = 'අවසන් වන්නේ';
+$string['firstaccess'] = 'පළමු පිවිසුම';
+$string['globalrolelabel'] = '$a->label යනු $a->value වේ';
+$string['isafter'] = 'ට පසු';
+$string['isanyvalue'] = 'ඕනෑම අගයකි';
+$string['isbefore'] = '‍ට පෙර';
+$string['isdefined'] = 'අර්ථ දක්වා ඇත';
+$string['isempty'] = 'හිස්ව ඇත';
+$string['isequalto'] = 'ට සමාන වේ';
+$string['isnotdefined'] = 'අර්ථ දක්වා නැත';
+$string['isnotequalto'] = 'ට සමාන නොවේ';
+$string['newfilter'] = 'නව පෙරනය';
+$string['profilelabel'] = '$a->label: $a->profile $a->operator $a->value';
+$string['profilelabelnovalue'] = '$a->label: $a->profile $a->operator';
+$string['removeall'] = 'සියලුම පෙරනයන් ඉවත් කරන්න';
+$string['removeselected'] = 'තෝරා ගැනීම ඉවත් කරන්න';
+$string['selectlabel'] = '$a->label $a->operator $a->value';
+$string['startswith'] = 'ආරම්භ කරන්න';
+$string['tablenosave'] = 'ඉහත වගුවේ වෙනස්කම් ස්වයංක්‍රීයවම සුරක්ෂිත කරන්න';
+$string['textlabel'] = '$a->label $a->operator $a->value';
+$string['textlabelnovalue'] = '$a->label $a->operator';
+
+?>
