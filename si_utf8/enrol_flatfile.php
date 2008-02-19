e18f8d532449bf8192ab2622ad356ca4658f8a52
Malinda Siriwardana <malindahs>
2008-02-19 15:27:37 +0000

    malinda

--- si_utf8/enrol_flatfile.php
+++ si_utf8/enrol_flatfile.php
@@ -1,10 +1,11 @@
 <?PHP // $Id$ 
-      // emailprotect.php - created with Moodle 1.6 + (2006050507)
+      // enrol_flatfile.php - created with Moodle 2.0 dev (2007101508)
 
-$string['enrolname'] = 'පැතලි ෆයිල්';
-$string['filelockedmailsubject'] = 'වැදගත් දෝෂයකි: එන් ‍රෝල් වීමේ ෆයිල් එක';
-$string['location'] = 'ෆයිල් ස්ථානය';
-$string['mailadmin'] = 'ඇඩ්මින්ට විද්‍යුත් තැපැල් පණිවිඩයක් මගින් දන්වන්න';
+
+$string['enrolname'] = 'පැතලි ගොනු';
+$string['filelockedmailsubject'] = 'වැදගත් දෝෂයකි: ලියාපදිංචිවීමේ ගොනුව';
+$string['location'] = 'ගොනු ස්ථානය';
+$string['mailadmin'] = 'පරිපාලකවරයාට විද්‍යුත් තැපැල් පණිවිඩයක් මගින් දන්වන්න';
 $string['mailusers'] = 'භාවිතා කරන්නන්ට විද්‍යුත් තැපැල් පණිවිඩයක් මගින් දන්වන්න';
 
 ?>
