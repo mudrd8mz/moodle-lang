ee52daba546fe9c38396ea595e5c9fde83aec647
Malinda Siriwardana <malindahs>
2008-02-19 09:28:53 +0000

    malinda

--- si_utf8/tag.php
+++ si_utf8/tag.php
@@ -18,6 +18,26 @@ $string['managetags'] = 'ටැගයන් කළමනාකරණය';
 $string['name'] = 'ටැගයේ නම';
 $string['namesalreadybeeingused'] = 'ටැග නාමයන් දැනටමත් භාවිතා කර ඇත';
 $string['newname'] = 'නව ටැග නම';
+$string['noresultsfor'] = '\"$a\" සඳහා ප්‍රතිඵල නැත';
 $string['owner'] = 'අයිතිකරු';
+$string['relatedblogs'] = 'මෙම ටැගය සහිත මෑත කාලීන බ්ලොග ඇතුලත් කිරීම්';
+$string['relatedtags'] = 'සම්බන්ධිත ටැග';
+$string['reset'] = 'නැවත සැකසීම';
+$string['resetflag'] = 'සංඥා නැවත සැකසීම';
+$string['search'] = 'සෙවීම';
+$string['searchtags'] = 'ටැග සෙවීම';
+$string['select'] = 'තෝරන්න';
+$string['tag'] = 'ටැග';
+$string['tagdescription'] = 'ටැග විස්තර';
+$string['tags'] = 'ටැග';
+$string['tagsaredisabled'] = 'ටැග අක්‍රිය කරන්න';
+$string['tagtype'] = 'ටැග වර්ග';
+$string['tagtype_default'] = 'සාමාන්‍යයෙන් පවතින තත්වය';
+$string['tagtype_official'] = 'නිළ';
+$string['thistaghasnodesc'] = 'මෙම ටැගය සඳහා දැනට විස්තරයක් නැත';
+$string['timemodified'] = 'වෙනස් කරන ලද';
+$string['typechanged'] = 'ටැග වර්ගය වෙනස් කර ඇත';
+$string['updated'] = 'යාවත් කාලීන කරන ලදි';
+$string['updatetag'] = 'යාවත් කාලීන කරන්න';
 
 ?>
