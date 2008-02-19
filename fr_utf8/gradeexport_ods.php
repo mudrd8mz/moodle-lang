ceed15645e6dee60c8c868d027cec0bc7706c9db
Nicolas Martignoni <martignoni>
2008-02-19 15:46:59 +0000

    MDL-11811 OpenDocument is correct name for file format

--- fr_utf8/gradeexport_ods.php
+++ fr_utf8/gradeexport_ods.php
@@ -1,7 +1,7 @@
 <?php // $Id$ 
 
-$string['modulename'] = 'Feuille de calcul OpenOffice';
-$string['ods:view'] = 'Exporter les notes vers Openoffice';
+$string['modulename'] = 'Feuille de calcul OpenDocument';
+$string['ods:view'] = 'Exporter les notes vers OpenDocument';
 $string['ods:publish'] = 'Publier l\'exportation en ODS des notes';
 
 ?>
