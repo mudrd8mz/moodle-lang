ced3661511a337f2ae6f64bfb966c73dd5ae85ef
Carles Bellver <carlesbellver>
2008-02-20 17:10:50 +0000

    *** empty log message ***

--- ca_utf8/quiz_grading.php
+++ ca_utf8/quiz_grading.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // quiz_grading.php - created with Moodle 1.6.1+ (2006050511)
+      // quiz_grading.php - created with Moodle 1.9 Beta 4 (2007101508)
 
 
 $string['essayonly'] = 'Les preguntes següents s\'han de qualificar a mà';
 $string['grading'] = 'Qualificació manual';
+$string['gradingnotallowed'] = 'No teniu permís per a qualificar a mà les respostes d\'aquest qüestionari';
 
 ?>
