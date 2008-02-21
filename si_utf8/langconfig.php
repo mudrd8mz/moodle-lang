5e4befd858941b58e2976ebde03da30533ced66d
Malinda Siriwardana <malindahs>
2008-02-21 09:49:21 +0000

    unknow charactors - resolving - malinda

--- si_utf8/langconfig.php
+++ si_utf8/langconfig.php
@@ -1,5 +1,5 @@
-<?php
-$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+﻿<?php
+$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z ';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['firstdayofweek'] = '0';
 $string['locale'] = 'si_LK.UTF-8';
