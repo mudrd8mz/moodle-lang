ff6361b60a0d71b32d74fcfe6d7d1a54bdb10c61
Malinda Siriwardana <malindahs>
2008-02-28 01:57:54 +0000

    new strings

--- si_utf8/tag.php
+++ si_utf8/tag.php
@@ -1,7 +1,10 @@
 <?PHP // $Id$ 
-      // tag.php - created with Moodle 2.0 dev (2007101508)
+      // tag.php - created with Moodle 2.0 dev (2007101508.05)
 
 
+$string['addedotag'] = '\"$a\" නිල ටැගයක් ලෙස එක් කරන ලදි.';
+$string['addotags'] = 'නිල ටැග එක් කරන්න';
+$string['addtagtomyinterests'] = 'මගේ ලැදියාවන්ට \"$a\" එක් කරන්න';
 $string['blocktagstitle'] = 'ටැගයන්';
 $string['changename'] = 'ටැගයේ නම වෙනස් කරන්න';
 $string['changetype'] = 'ටැගයේ වර්ගය වෙනස් කරන්න';
@@ -19,13 +22,19 @@ $string['name'] = 'ටැගයේ නම';
 $string['namesalreadybeeingused'] = 'ටැග නාමයන් දැනටමත් භාවිතා කර ඇත';
 $string['newname'] = 'නව ටැග නම';
 $string['noresultsfor'] = '\"$a\" සඳහා ප්‍රතිඵල නැත';
+$string['otags'] = 'නිල ටැග';
 $string['owner'] = 'අයිතිකරු';
+$string['ptags'] = 'පරීශීලක අර්ථ දක්වන ලද ටැග (කොමාවකින් වෙන් කරන ලදි)';
 $string['relatedblogs'] = 'මෙම ටැගය සහිත මෑත කාලීන බ්ලොග ඇතුලත් කිරීම්';
 $string['relatedtags'] = 'සම්බන්ධිත ටැග';
+$string['removetagfrommyinterests'] = 'මගේ ලැදියාවන්ගෙන් \"$a\" ඉවත් කරන්න';
 $string['reset'] = 'නැවත සැකසීම';
 $string['resetflag'] = 'සංඥා නැවත සැකසීම';
+$string['responsiblewillbenotified'] = 'වග කිවයුතු පුද්ගලයා වෙත දන්වනු ලැබේ';
 $string['search'] = 'සෙවීම';
+$string['searchresultsfor'] = '\"$a\" සෙවීම් ප්‍රතිඵල';
 $string['searchtags'] = 'ටැග සෙවීම';
+$string['seeallblogs'] = 'මෙම ටැගය ඇති සියලුම බ්ලොගයන් බලන්න';
 $string['select'] = 'තෝරන්න';
 $string['tag'] = 'ටැග';
 $string['tagdescription'] = 'ටැග විස්තර';
@@ -34,10 +43,13 @@ $string['tagsaredisabled'] = 'ටැග අක්‍රිය කරන්න';
 $string['tagtype'] = 'ටැග වර්ග';
 $string['tagtype_default'] = 'සාමාන්‍යයෙන් පවතින තත්වය';
 $string['tagtype_official'] = 'නිළ';
+$string['thingstaggedwith'] = '\"$a->name\" ටැගගත කරන ලද $a->count ද්‍රව්‍ය';
 $string['thistaghasnodesc'] = 'මෙම ටැගය සඳහා දැනට විස්තරයක් නැත';
 $string['timemodified'] = 'වෙනස් කරන ලද';
 $string['typechanged'] = 'ටැග වර්ගය වෙනස් කර ඇත';
 $string['updated'] = 'යාවත් කාලීන කරන ලදි';
 $string['updatetag'] = 'යාවත් කාලීන කරන්න';
+$string['userstaggedwith'] = '\"$a\" මගින් ටැගගත කරන ලද පරිශීලකයන්';
+$string['withselectedtags'] = 'තෝරාගන්නා ලද ටැග සමග...';
 
 ?>
