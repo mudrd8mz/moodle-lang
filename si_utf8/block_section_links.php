6ae351ac54f373de7b8c04655f44444eb550d252
Malinda Siriwardana <malindahs>
2008-03-01 01:05:22 +0000

    *** empty log message ***

--- si_utf8/block_section_links.php
+++ si_utf8/block_section_links.php
@@ -1,8 +1,8 @@
 <?PHP // $Id$ 
-      // block_section_links.php - created with Moodle 1.8.2+ (2007021520)
+      // block_section_links.php - created with Moodle 2.0 dev (2007101508.05)
 
 
-$string['blockname'] = 'අංශ සබැදුම්';
+$string['blockname'] = 'අංශ සබැඳුම්';
 $string['jumptocurrenttopic'] = 'පවතින මාතෘකාවට පනින්න';
 $string['jumptocurrentweek'] = 'පවතින සතියට පනින්න';
 $string['topics'] = 'මාතෘකා';
