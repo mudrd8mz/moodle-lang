027f61399487839153d82fc4b601d12ab9ed6fb4
Nicolas Martignoni <martignoni>
2008-03-03 14:27:03 +0000

    Changed the translation of "Tag".

--- fr_utf8/tag.php
+++ fr_utf8/tag.php
@@ -1,55 +1,56 @@
 <?php // $Id$
 
-// Trouver un meilleur terme que 'balise' pour 'tag'
-$string['addedotag'] = '«&nbsp;$a&nbsp;» a été ajouté comme balise officielle.';
-$string['addotags'] = 'Ajouter des balises officielles';
+// Trouver un meilleur terme que 'balise' pour 'tag': Done
+// On utilise Tag, qui est passé dans les habitudes francophones
+$string['addedotag'] = '«&nbsp;$a&nbsp;» a été ajouté comme tag officiel.';
+$string['addotags'] = 'Ajouter des tags officiels';
 $string['addtagtomyinterests'] =  'Ajouter «&nbsp;$a&nbsp;» à mes centres d\'intérêts';
-$string['blocktagstitle'] =  'Balises';
-$string['changename'] = 'Modifier nom de balise';
-$string['changetype'] = 'Modifier type de balise';
+$string['blocktagstitle'] =  'Tags';
+$string['changename'] = 'Modifier nom du tag';
+$string['changetype'] = 'Modifier type du tag';
 $string['count'] = 'Compte';
 $string['description'] = 'Description';
 $string['delete'] = 'Supprimer';
 $string['deleted'] = 'Supprimé';
-$string['edittag'] = 'Modifier cette balise';
+$string['edittag'] = 'Modifier ce tag';
 $string['flag'] = 'Fanion';
 $string['flagasinappropriate'] = 'Marquer comme inapproprié';
-$string['helprelatedtags'] = 'Balises reliées, séparées par des virgules';
+$string['helprelatedtags'] = 'Tags reliés, séparées par des virgules';
 $string['id'] = 'id';
-$string['managetags'] = 'Gérer les balises';
-$string['name'] = 'Nom de balise';
-$string['namesalreadybeeingused'] = 'Noms de balise déjà utilisés';
-$string['newname'] = 'Nouveau nom de balise';
+$string['managetags'] = 'Gérer les tags';
+$string['name'] = 'Nom du tag';
+$string['namesalreadybeeingused'] = 'Noms des tags déjà utilisés';
+$string['newname'] = 'Nouveau nom de tag';
 $string['noresultsfor'] = 'Aucun résultat pour «&nbsp;$a&nbsp;»';
 $string['owner'] = 'Possesseur';
-$string['officialtag'] = 'Balise officielle';
-$string['otags'] = 'Balises officielles';
-$string['ptags'] = 'Balises personnelles (séparées par des virgules)';
-$string['relatedblogs'] = 'Articles de blog récents avec cette balise';
-$string['relatedtags'] = 'Balises ';
+$string['officialtag'] = 'Tag officiel';
+$string['otags'] = 'Tags officiels';
+$string['ptags'] = 'Tags personnels (séparées par des virgules)';
+$string['relatedblogs'] = 'Articles de blog récents avec ce tag';
+$string['relatedtags'] = 'Tags ';
 $string['removetagfrommyinterests'] = 'Retirer «&nbsp;$a&nbsp;» de mes centres d\'intérêts';
 $string['responsiblewillbenotified'] = 'La personne responsable sera avertie';
 $string['resetflag'] = 'Réinitialisation fanion';
 $string['reset'] = 'Réinitialisation';
 $string['search'] = 'Chercher';
 $string['searchresultsfor'] = 'Résultats de recherche pour «&nbsp;$a&nbsp;»';
-$string['searchtags'] = 'Chercher des balises';
-$string['seeallblogs'] = 'Voir tous les blogs avec cette balise';
+$string['searchtags'] = 'Chercher des tags';
+$string['seeallblogs'] = 'Voir tous les blogs avec ce tag';
 $string['select'] = 'Sélectionner';
-$string['tagdescription'] = 'Description balise';
-$string['tag'] = 'Balise';
-$string['tagtype'] = 'Type de balise';
+$string['tagdescription'] = 'Description tag';
+$string['tag'] = 'Tag';
+$string['tagtype'] = 'Type de tag';
 $string['tagtype_official'] = 'Officiel';
 $string['tagtype_default'] = 'Défaut';
-$string['tags'] = 'Balises';
-$string['tagsaredisabled'] = 'Les balises sont désactivées';
+$string['tags'] = 'Tags';
+$string['tagsaredisabled'] = 'Les tags sont désactivés';
 $string['thingstaggedwith'] = '$a->count objets marqués par «&nbsp;$a->name&nbsp;»';
-$string['thistaghasnodesc'] = 'Cette balise n\'a actuellement pas de description.';
+$string['thistaghasnodesc'] = 'Ce tag n\'a actuellement pas de description.';
 $string['timemodified'] = 'Modifié';
-$string['typechanged'] = 'Type de la balise modifié';
-$string['userstaggedwith'] = 'Utilisateurs avec la balise «&nbsp;$a&nbsp;»';
+$string['typechanged'] = 'Type du tag modifié';
+$string['userstaggedwith'] = 'Utilisateurs avec le tag «&nbsp;$a&nbsp;»';
 $string['updatetag'] = 'Modifier';
 $string['updated'] = 'Modifié';
-$string['withselectedtags'] = 'Avec les balises sélectionnées...';
+$string['withselectedtags'] = 'Avec les tags sélectionnés...';
 
 ?>
