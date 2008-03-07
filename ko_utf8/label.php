b1c4763d30d20d805010b1add77986322aea9a66
Lee Chan-young <yogibear270>
2008-03-07 09:59:26 +0000

    change some trems

--- ko_utf8/label.php
+++ ko_utf8/label.php
@@ -2,8 +2,8 @@
       // label.php - created with Moodle 1.7 dev (2006092800)
 
 
-$string['labeltext'] = '표지 문서';
+$string['labeltext'] = '표지 내용';
 $string['modulename'] = '표지';
-$string['modulenameplural'] = '표지들';
+$string['modulenameplural'] = '표지모음';
 
 ?>
