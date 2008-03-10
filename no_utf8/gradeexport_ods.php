d629f20e70e21b430c9abbce851bd9e4a2221052
John Harald Gartland <johnhg>
2008-03-10 21:53:44 +0000

    New file in Moodle 1.9

---
+++ no_utf8/gradeexport_ods.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // gradeexport_ods.php - created with Moodle 1.9 (2007101509)
+      // local modifications from http://localhost
+
+
+$string['modulename'] = 'OpenDocument regneark';
+$string['ods:publish'] = 'Publiser ODS karaktereksport';
+$string['ods:view'] = 'Bruk OpenDocument karaktereksport';
+
+?>
