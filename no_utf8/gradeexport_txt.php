011e4687babca08529987e36e6d875e682c38fef
John Harald Gartland <johnhg>
2008-03-10 17:34:00 +0000

    New file in Moodle 1.9

---
+++ no_utf8/gradeexport_txt.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // gradeexport_txt.php - created with Moodle 1.9 (2007101509)
+      // local modifications from http://localhost
+
+
+$string['modulename'] = 'Ren tekstfil';
+$string['txt:publish'] = 'Publiser TXT karaktereksport';
+$string['txt:view'] = 'Bruk ren tekst karaktereksport';
+
+?>
