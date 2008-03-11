137608ff2cb56e900a7747692b6cda19aff4c50b
John Harald Gartland <johnhg>
2008-03-11 22:25:40 +0000

    New file in Moodle 1.9

---
+++ no_utf8/gradereport_outcomes.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // gradereport_outcomes.php - created with Moodle 1.9 (2007101509)
+      // local modifications from http://localhost
+
+
+$string['addoutcome'] = 'Legg til et læringsmål';
+$string['courseoutcomes'] = 'Kurslæringsmål';
+$string['coursespecoutcome'] = 'Kurslæringsmål';
+$string['modulename'] = 'Læringsmålrapport';
+$string['outcomes:view'] = 'Se læringsmålrapporten';
+$string['usedgradeitem'] = 'Antall karakterenheter';
+
+?>
