b487a0a098de9f2cac7ec7b676916dbb5071f94c
John Harald Gartland <johnhg>
2008-03-12 22:07:00 +0000

    New file in Moodle 1.9

---
+++ no_utf8/bulkusers.php
@@ -0,0 +1,22 @@
+<?PHP // $Id$ 
+      // bulkusers.php - created with Moodle 1.9 (2007101509)
+
+
+$string['addall'] = 'Legg til alle';
+$string['addsel'] = 'Legg til utvalg';
+$string['allfilteredusers'] = 'Alle filtrerte ($a->count/$a->total)';
+$string['allselectedusers'] = 'Alle valgte ($a->count/$a->tatal';
+$string['allusers'] = 'Alle brukere ($a)';
+$string['available'] = 'Tilgjengelig';
+$string['confirmmessage'] = 'Vil du virkelig sende denne meldingen til alle brukerne over?<br/>$a';
+$string['nofilteredusers'] = 'Ingen brukere funnet (0/$a)';
+$string['noselectedusers'] = 'Ingen brukere valgt';
+$string['removeall'] = 'Fjern alle';
+$string['removesel'] = 'Ta bort fra seksjon';
+$string['selected'] = 'Valgt';
+$string['selectedlist'] = 'Valgt bruker liste';
+$string['usersfound'] = '$a bruker(e) funnet.';
+$string['usersinlist'] = 'Brukere på listen';
+$string['usersselected'] = '$a bruker(e) valgt.';
+
+?>
