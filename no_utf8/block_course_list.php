9e462f56f014c88f1827e3ead533659142a77111
John Harald Gartland <johnhg>
2008-03-12 17:53:12 +0000

    Changes in 1.9

--- no_utf8/block_course_list.php
+++ no_utf8/block_course_list.php
@@ -1,11 +1,13 @@
 <?PHP // $Id$ 
-      // block_course_list.php - created with Moodle 1.8 + (2007021503)
+      // block_course_list.php - created with Moodle 1.9 (2007101509)
 
 
+$string['adminview'] = 'Administrasjonsvisning';
 $string['allcourses'] = 'Administratorer ser alle klasser';
 $string['blockname'] = 'Liste over klasser';
 $string['configadminview'] = 'Hva skal administratoren se i boksen med lista over klasser?';
 $string['confighideallcourseslink'] = 'Skjul \"Alle kurs\" lenken i bunnen av blokken. Administratorer vil fortsatt se den.';
+$string['hideallcourseslink'] = 'Skul alle kurslenker';
 $string['owncourses'] = 'Administratoren ser sine egne klasser';
 
 ?>
