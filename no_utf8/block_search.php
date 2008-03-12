f1ae1def7e4a653ec93e82959b62553b34f59ea3
John Harald Gartland <johnhg>
2008-03-12 21:54:22 +0000

    New file in Moodle 1.9

---
+++ no_utf8/block_search.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // block_search.php - created with Moodle 1.9 (2007101509)
+
+
+$string['blockname'] = 'Globalt søk';
+$string['bytes'] = 'bytes (0 står for ingen grense)';
+$string['configbuttonlabel'] = 'Knappetittel';
+$string['configenablefileindexing'] = 'Slå på filindeksering';
+$string['configfiletypes'] = 'Filtyper en kan bruke';
+$string['configlimitindexbody'] = 'Hvor stor kan indeksen være';
+$string['configpdftotextcmd'] = 'Sti til kommandoen pdftotext';
+$string['configsearchtext'] = 'Søketekst';
+$string['configwordtotextcmd'] = 'Sti til kommandoen doctotext';
+$string['configwordtotextenv'] = 'Innstillinger for MSWord konverteringsmodulen';
+$string['go'] = 'Kjør!';
+$string['searchmoodle'] = 'Søk Moodle';
+
+?>
---
+++ no_utf8/block_tag_flickr.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // block_tag_flickr.php - created with Moodle 1.9 (2007101509)
+
+
+$string['blockname'] = 'Flickr';
+$string['configtitle'] = 'Tittel';
+$string['date-posted-asc'] = 'Dato postet Stigende';
+$string['date-posted-desc'] = 'Data postet Synkende';
+$string['date-taken-asc'] = 'Dato tatt Stigende';
+$string['date-taken-desc'] = 'Data tatt Synkende';
+$string['defaulttile'] = 'Flickr';
+$string['getfromphotoset'] = 'Hent foto fra fotosett med id';
+$string['includerelatedtags'] = 'Inkluder relaterte tags i spørringen';
+$string['interestingness-asc'] = 'Interresanthet Stigende';
+$string['interestingness-desc'] = 'Interresanthet Synkende';
+$string['numberofphotos'] = 'Antall foto';
+$string['relevance'] = 'Relevanse';
+$string['sortby'] = 'Sorter etter';
+
+?>
