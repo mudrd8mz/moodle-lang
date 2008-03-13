d625d0cc1950c48c12a58e38f05d7ad8a9e081da
John Harald Gartland <johnhg>
2008-03-13 22:25:04 +0000

    New file in Moodle 1.9

---
+++ no_utf8/block_tag_youtube.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // block_tag_youtube.php - created with Moodle 1.9 (2007101509)
+
+
+$string['anycategory'] = 'Hvilkensomhelst kategori';
+$string['autosvehicles'] = 'Biler &amp; Kjøretøy';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'Kategori';
+$string['comedy'] = 'Komedie';
+$string['configtitle'] = 'Tittel';
+$string['entertainment'] = 'Underholdning';
+$string['filmsanimation'] = 'Filmer &amp; Animasjon';
+$string['gadgetsgames'] = 'Dingser &amp; Spill';
+$string['howtodiy'] = 'Hvordan-gjøre &amp; Gjør-det-selv';
+$string['includeonlyvideosfromplaylist'] = 'Inkluder bare videoer med id fra spillelisten';
+$string['music'] = 'Musikk';
+$string['newspolitics'] = 'Nyheter &amp; Politikk';
+$string['numberofvideos'] = 'Antall videoer';
+$string['peopleblogs'] = 'Folk &amp; Blogger';
+$string['petsanimals'] = 'Kjeledyr &amp; Dyr';
+$string['sports'] = 'Sport';
+$string['travel'] = 'Reiser &amp; Plasser';
+
+?>
