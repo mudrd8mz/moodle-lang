24c6597ebf45961d27bb82fb09edc874b370999a
Ethem Evlice <ethem>
2008-03-19 16:06:10 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/auth_mnet.php
+++ tr_utf8/auth_mnet.php
@@ -1,9 +1,11 @@
 <?PHP // $Id$ 
-      // auth_mnet.php - created with Moodle 2.0 dev (2007101508)
+      // auth_mnet.php - created with Moodle 2.0 dev (Build: 20080307) (2008030700)
 
 
+$string['sso_idp_description'] = 'Kullanıcılarınızın $a Moodle sitesinde yeniden giriş yapmaksızın gezinebilmesi için bu servisi duyur.<ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Servis Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />$a sitesinden yetkilendirilmiş kullanıcıların sitenize yeniden giriş yapmaksızın erişebilmesi için bu servise abone ol.<ul><li><em>Bağımlılık</em>: SSO (Servis Sağlayıcı) servisini ayrıca $a sitesine <strong>duyurmalısınız.</strong></li></ul><br />';
 $string['sso_idp_name'] = 'SSO (Kimlik Sağlayıcı)';
-$string['sso_mnet_login_refused'] = '$a[0] kullanıcısı, burdan, $a[1], giriş yapmaya yetkili değil.';
-$string['sso_sp_name'] = 'SSO (Hizmet Sağlayıcı)';
+$string['sso_mnet_login_refused'] = '$a[0] kullanıcısı, $a[1] sitesinden giriş yapmaya yetkili değil.';
+$string['sso_sp_description'] = '$a sitesinden yetkilendirilmiş kullanıcıların sitenize yeniden giriş yapmaksızın erişebilmesi için bu servisi duyur.<ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Kimlik Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />Kullanıcılarınızın $a Moodle sitesinde yeniden giriş yapmaksızın gezinebilmesi için bu servise abone ol.<ul><li><em>Bağımlılık</em>: SSO (Kimlik Sağlayıcı) servisini ayrıca $a sitesine <strong>duyurmalısınız.</strong></li></ul><br />';
+$string['sso_sp_name'] = 'SSO (Servis Sağlayıcı)';
 
 ?>
