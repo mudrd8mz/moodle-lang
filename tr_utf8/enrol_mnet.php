8763ce2e5eb0414c00cfd40a505c7cd6cb32f496
Ethem Evlice <ethem>
2008-03-19 16:59:10 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

--- tr_utf8/enrol_mnet.php
+++ tr_utf8/enrol_mnet.php
@@ -1,17 +1,19 @@
 <?PHP // $Id$ 
-      // enrol_mnet.php - created with Moodle 1.8 dev (2007012500)
+      // enrol_mnet.php - created with Moodle 2.0 dev (Build: 20080307) (2008030700)
 
 
-$string['allcategories'] = '$a potansiyel kategori';
-$string['allcourses'] = '$a potansiyel kurs';
-$string['allow_allcourses'] = 'Tüm uzak kurslara kayda izin ver.';
+$string['allcategories'] = '$a olası kategori';
+$string['allcourses'] = '$a olası ders';
+$string['allow_allcourses'] = 'Uzaktaki tüm derslere kayda izin ver.';
 $string['allowedcategories'] = '$a izin verilen kategori';
-$string['allowedcourses'] = '$a izin verilen kurs';
-$string['allowedcourseslinktext'] = 'İzin verilen kursları ve kategorileri düzenle';
+$string['allowedcourses'] = '$a izin verilen ders';
+$string['allowedcourseslinktext'] = 'İzin verilen dersleri ve kategorileri düzenle';
 $string['description'] = 'Moodle Ağı Ders Kaydı';
 $string['enrolname'] = 'Moodle Ağı';
-$string['mnet_enrol_name'] = 'Moodle Ağı Kaydı';
-$string['nocategoriesdefined'] = 'Kategori bulunamadı. <a href=\"$a\">Buradan</a> yeni kategori tanımlayın.';
-$string['nocoursesdefined'] = 'Kurs bulunamadı. <a href=\"$a\">Buradan</a> yeni kurs tanımlayın.';
+$string['mnet_enrol_description'] = '$a sitesi yöneticilerine kendi öğrencilerini sunucunuzda oluşturulan derslere kaydedebilmesine izin vermek için bu servisi duyur.<br /><ul><li><em>Bağımlılık</em>: $a sitesine SSO (Servis Sağlayıcı) servisini ayrıca <strong>duyurmalısınız</strong>.</li><li><em>Bağımlılık</em>: $a sitesindeki SSO (Kimlik Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />$a sitesindeki derslere öğrencilerinizi kaydedebilmek için bu servise abone olun.<br /><ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Servis Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li><li><em>Bağımlılık</em>: $a sitesine SSO (Kimlik Sağlayıcı) servisini ayrıca <strong>duyurmalısınız</strong>.</li></ul><br />';
+$string['mnet_enrol_name'] = 'Moodle Ağı Ders Kaydı';
+$string['mnetlocalforexternal'] = 'Dışarıdan gelen kullanıcılar için yerel dersler';
+$string['nocategoriesdefined'] = 'Ders kategorisi bulunamadı. <a href=\"$a\">Buradan</a> yeni kategorileri tanımlayın.';
+$string['nocoursesdefined'] = 'Ders bulunamadı. <a href=\"$a\">Buradan</a> yeni dersleri tanımlayın.';
 
 ?>
