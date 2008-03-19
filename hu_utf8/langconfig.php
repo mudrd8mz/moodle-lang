d8475dd8c843a001693c8ca24eaacd4b66f24838
koen roggemans <koenr>
2008-03-19 21:20:32 +0000

    dates messed up

--- hu_utf8/langconfig.php
+++ hu_utf8/langconfig.php
@@ -3,7 +3,7 @@
 
 
 $string['alphabet'] = 'A,Á,B,C,CS,D,DZ,DZS,E,É,F,G,GY,H,I,Í,J,K,L,M,N,NY,O,Ó,Ö,Ő,P,Q,R,S,SZ,T,TY,U,Ú,Ü,Ű,V,W,X,Y,Z,ZS';
-$string['backupnameformat'] = '%%É%%h%%n-%%Ó%%P';
+$string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['decsep'] = ',';
 $string['firstdayofweek'] = '0';
 $string['listsep'] = '\;';
@@ -13,15 +13,15 @@ $string['localewincharset'] = '';
 $string['oldcharset'] = 'ISO-8859-1';
 $string['strftimedate'] = '%%d %%B %%Y';
 $string['strftimedateshort'] = '%%d %%B';
-$string['strftimedatetime'] = '%%d %%B %%Y, %%I:%%M %%p';
+$string['strftimedatetime'] = '%%d %%B %%Y, %%H:%%M';
 $string['strftimedaydate'] = '%%A, %%d %%B %%Y';
-$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
-$string['strftimedayshort'] = '%%A, %%d %%B';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%H:%%M';
+$string['strftimedayshort'] = '%%A %%d %%B';
 $string['strftimedaytime'] = '%%a, %%H:%%M';
 $string['strftimemonthyear'] = '%%B %%Y';
 $string['strftimerecent'] = '%%d %%b, %%H:%%M';
-$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%I:%%M %%p';
-$string['strftimetime'] = '%%I:%%M %%p';
+$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%H:%%M';
+$string['strftimetime'] = '%%H:%%M';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Magyar';
