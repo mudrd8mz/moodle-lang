1ceae2e302d35292e35cb722c4fbbf5bda08df69
John Harald Gartland <johnhg>
2008-03-25 20:48:26 +0000

    Changes in 1.9

--- no_utf8/censor.php
+++ no_utf8/censor.php
@@ -2,7 +2,7 @@
       // censor.php - created with Moodle 1.9 (2007101509)
 
 
-$string['badwords'] = 'shit,fucked,fucker,fuck,dickhead, dick,cockhead,,asshole,arsehole,prick,bitch, ,slut,wanker,pikk,fitte,bastard,dildo,masturbate, orgasme,penis,nigger, pussy, homo';
+$string['badwords'] = 'shit,fucked,fucker,fuck,dickhead, dick,cockhead,,asshole,arsehole,prick,bitch, ,slut,wanker,pikk,fitte,bastard,dildo,masturbate, orgasme,penis,nigger,pussy,homo';
 $string['filtername'] = 'Ordsensurering';
 
 ?>
