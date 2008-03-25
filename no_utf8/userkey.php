af95c57248e32fa62d0f06a9f704b9da824193a5
John Harald Gartland <johnhg>
2008-03-25 21:08:46 +0000

    New file in Moodle 1.9

---
+++ no_utf8/userkey.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$ 
+      // userkey.php - created with Moodle 1.9 (2007101509)
+
+
+$string['createnewkey'] = 'Lag ny brukernøkkel';
+$string['createuserkey'] = 'Lag brukernøkkel';
+$string['deletekeyconfirm'] = 'Vil du virkelig slette denne brukernøkkelen?';
+$string['edituserkey'] = 'Rediger brukernøkkel';
+$string['keyiprestriction'] = 'Nøkkel IP restriksjon';
+$string['keymanager'] = 'Nøkkelbehandler';
+$string['keyvaliduntil'] = 'Nøkkel gyldig til';
+$string['keyvalue'] = 'Nøkkelverdi';
+$string['newuserkey'] = 'Ny brukernøkkel';
+$string['userkey'] = 'Brukernøkkel';
+$string['userkeys'] = 'Brukernøkler';
+
+?>
