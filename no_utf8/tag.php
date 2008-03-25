2226fb5964212d6843a6c3351196bde005a5819f
John Harald Gartland <johnhg>
2008-03-25 21:17:47 +0000

    New file in Moodle 1.9

---
+++ no_utf8/tag.php
@@ -0,0 +1,56 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 1.9 (2007101509)
+
+
+$string['addedotag'] = '\"$a\" ble lagt til som en offisiell merkelapp';
+$string['addotags'] = 'Legg til offisielle merkelapper';
+$string['addtagtomyinterests'] = 'Legg \"$a\" til mine interesser.';
+$string['blocktagstitle'] = 'Merkelapper';
+$string['changename'] = 'Endre navn på merkelapp';
+$string['changetype'] = 'Endre type på merkelapp';
+$string['count'] = 'Tell';
+$string['delete'] = 'Slett';
+$string['deleted'] = 'Slettet';
+$string['description'] = 'Beskrivelse';
+$string['edittag'] = 'Rediger denne merkelappen';
+$string['flag'] = 'Flagg';
+$string['flagasinappropriate'] = 'Flagg som upassende';
+$string['helprelatedtags'] = 'Kommaseparerte relaterte merkelapper';
+$string['id'] = 'id';
+$string['managetags'] = 'Administrer merkelapper';
+$string['name'] = 'Navn på merkelapp';
+$string['namesalreadybeeingused'] = 'Merkelappnavnet finnes allerede';
+$string['newname'] = 'Nytt merkelappnavn';
+$string['noresultsfor'] = 'Ikke noe resultat for \"$a\"';
+$string['officialtag'] = 'Offisiell merkelapp';
+$string['otags'] = 'Offisielle merkelapper';
+$string['owner'] = 'Eier';
+$string['ptags'] = 'Brukerdefinerte merkelapper (kommaseparert)';
+$string['relatedblogs'] = 'Nylige blogg-innlegg med denne merkelappen';
+$string['relatedtags'] = 'Relaterte merkelapper';
+$string['removetagfrommyinterests'] = 'Fjern \"$a\" fra mine interesser.';
+$string['reset'] = 'Nullstill';
+$string['resetflag'] = 'Nullstill flagg';
+$string['responsiblewillbenotified'] = 'Ansvarlig person vil bli kontaktet';
+$string['search'] = 'Søk';
+$string['searchresultsfor'] = 'Søkeresultater for \"$a\"';
+$string['searchtags'] = 'Søk merkelapper';
+$string['seeallblogs'] = 'Se alle blogger med denne merkelappen';
+$string['select'] = 'Velg';
+$string['tag'] = 'Merkelapp';
+$string['tagdescription'] = 'Merkelappbeskrivelse';
+$string['tags'] = 'Merkelapper';
+$string['tagsaredisabled'] = 'Merkelapper er slått av';
+$string['tagtype'] = 'Merkelapptype';
+$string['tagtype_default'] = 'Standard';
+$string['tagtype_official'] = 'Offisiell';
+$string['thingstaggedwith'] = '$a->count ting med merkelappen \"$a->name\"';
+$string['thistaghasnodesc'] = 'Denne merkelappen har for tiden ingen beskrivelse';
+$string['timemodified'] = 'Endret';
+$string['typechanged'] = 'Merkelapptype endret';
+$string['updated'] = 'Oppdatert';
+$string['updatetag'] = 'Oppdater';
+$string['userstaggedwith'] = 'Brukere flagget med \"$a\"';
+$string['withselectedtags'] = 'Utfør med følgende merkelapper...';
+
+?>
