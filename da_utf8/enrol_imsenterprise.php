4516bc4c28ca36169ddb1e6a49ae05c38168cd23
Bente Olsen <benteo>
2008-03-30 12:32:07 +0000

    edits

--- da_utf8/enrol_imsenterprise.php
+++ da_utf8/enrol_imsenterprise.php
@@ -1,8 +1,8 @@
 <?PHP // $Id$ 
-      // enrol_imsenterprise.php - created with Moodle 1.7+ (2006101007)
+      // enrol_imsenterprise.php - created with Moodle 1.9 + (Build: 20080304) (2007101509)
 
 
-$string['aftersaving...'] = 'Når du har gemt dine indstillinger kan det tænkes at';
+$string['aftersaving...'] = 'NÃ¥r du har gemt dine indstillinger, vil du mÃ¥ske...';
 $string['allowunenrol'] = 'Tillad IMS data at <strong>framelde</strong> elever/undervisere';
 $string['basicsettings'] = 'Basale indstillinger';
 $string['coursesettings'] = 'Kursusdata indstillinger';
@@ -11,25 +11,25 @@ $string['createnewcourses'] = 'Opret nye (skjulte) kurser hvis de ikke findes i 
 $string['createnewusers'] = 'Opret brugerkonti for brugere der endnu ikke er registreret i Moodle';
 $string['cronfrequency'] = 'Opdateringsfrekvens';
 $string['deleteusers'] = 'Slet brugerkonti hvis det er angivet i IMS data';
-$string['description'] = 'Denne metode vil med jævne mellemrum gennemgå og behandle en specielt formateret tekstfil et sted du angiver. Filen skal overholde <a href=\'../help.php?module=enrol/imsenterprise&file=formatoverview.html\' target=\'_blank\'>IMS Enterprise specifikationen</a> indeholdene person, gruppe og tilmedinger i XML format.';
-$string['doitnow'] = 'udfør en IMS Enterprise import nu';
+$string['description'] = 'Denne metode vil med jÃ¦vne mellemrum gennemgÃ¥ og behandle en specielt formateret tekstfil et sted du angiver. Filen skal overholde <a href=\'../help.php?module=enrol/imsenterprise&file=formatoverview.html\' target=\'_blank\'>IMS Enterprise specifikationen</a> indeholdene person, gruppe og tilmedinger i XML format.';
+$string['doitnow'] = 'udfÃ¸r en IMS Enterprise import nu';
 $string['enrolname'] = 'IMS Enterprise fil';
 $string['filelockedmail'] = 'Den tekstfil der bruges til IMS-filbaseret tilmeldinger ($a) kan ikke slettes af cron-jobbet. Dette skyldes ofte et problem med rettigheder da Moodle ikke har rettigheder til at slette filen. I modsat fald vil den blive behandlet flere gange.';
 $string['filelockedmailsubject'] = 'Alvorlig fejl: Tilmeldingsfejl';
 $string['fixcasepersonalnames'] = 'Ret personnavne til stort forbogstav';
-$string['fixcaseusernames'] = 'Ret brugernavne til små bogstaver';
-$string['imsrolesdescription'] = 'IMS Enterprise specifikationen indeholder 8 forskellige rolletyper. Vælg venligst hvordan de skal sammenkædes med rollerne i Moodle og om nogle af dem skal ignoreres.';
+$string['fixcaseusernames'] = 'Ret brugernavne til smÃ¥ bogstaver';
+$string['imsrolesdescription'] = 'IMS Enterprise specifikationen indeholder 8 forskellige rolletyper. VÃ¦lg venligst hvordan de skal sammenkÃ¦des med rollerne i Moodle og om nogle af dem skal ignoreres.';
 $string['location'] = 'Filplacering';
 $string['logtolocation'] = 'Placering af logfilen (hvis blank vil der ikke blive logget)';
-$string['mailadmins'] = 'Advisér admins med email';
-$string['mailusers'] = 'Advisér brugere med email';
+$string['mailadmins'] = 'Adviser admins via e-mail';
+$string['mailusers'] = 'Adviser brugere med e-mail';
 $string['miscsettings'] = 'Forskelligt';
-$string['processphoto'] = 'Tilføj brugerfoto til profil';
-$string['processphotowarning'] = 'Advarsel: Billedbehandling vil sansynligvis belaste serveren en del. Du bør overveje om du vil aktivere denne indstilling, hvis det er et meget stort antal elever, der skal behandles.';
-$string['restricttarget'] = 'Behandl kun data hvis de følgende steder er defineret';
+$string['processphoto'] = 'TilfÃ¸j brugerfoto til profil';
+$string['processphotowarning'] = 'Advarsel: Billedbehandling vil sansynligvis belaste serveren en del. Du bÃ¸r overveje om du vil aktivere denne indstilling, hvis det er et meget stort antal elever, der skal behandles.';
+$string['restricttarget'] = 'Behandl kun data hvis de fÃ¸lgende steder er defineret';
 $string['sourcedidfallback'] = 'Brug &quot;kildeid&quot; som en persons brugerid hvis personens &quot;brugerid&quot; felt ikke kunne findes.';
-$string['truncatecoursecodes'] = 'Afkort kursuskoder til denne længde';
-$string['usecapitafix'] = 'Afmærk dette felt hvis der bruges &quot;Capita&quot; (deres XMLformat er en smule ukorrekt)';
+$string['truncatecoursecodes'] = 'Afkort kursuskoder til denne lÃ¦ngde';
+$string['usecapitafix'] = 'AfmÃ¦rk dette felt hvis der bruges &quot;Capita&quot; (deres XMLformat er en smule ukorrekt)';
 $string['usersettings'] = 'Brugerdata indstillinger';
 $string['zeroisnotruncation'] = '0 angiver at der ikke skal ske afkortning';
 
