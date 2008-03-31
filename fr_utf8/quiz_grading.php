3c1dc72c61f687167c4edd9fb88b62637f36c680
Nicolas Martignoni <martignoni>
2008-03-31 18:46:05 +0000

    Terminology clarification for grades, notes, etc.

--- fr_utf8/quiz_grading.php
+++ fr_utf8/quiz_grading.php
@@ -2,6 +2,6 @@
 
 $string['essayonly'] = 'Les questions suivantes doivent être notées manuellement';
 $string['grading'] = 'Évaluation manuelle';
-$string['gradingnotallowed'] = 'Vous n\'avez pas l\'autorisation de noter manuellement les réponses de ce test';
+$string['gradingnotallowed'] = 'Vous n\'avez pas l\'autorisation d\'évaluer manuellement les réponses de ce test';
 
 ?>
