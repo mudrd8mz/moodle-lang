4aa0ed214d74455d813d10462d5654f349d58d06
Nicolas Martignoni <martignoni>
2008-03-31 17:48:03 +0000

    Wording enhancement

--- fr_utf8/block_quiz_results.php
+++ fr_utf8/block_quiz_results.php
@@ -1,9 +1,9 @@
 <?php // $Id$
 
-$string['bestgrade'] = 'La meilleure note&nbsp;:';
-$string['bestgrades'] = 'Les $a meilleures notes&nbsp;:';
-$string['bestgroupgrade'] = 'Le groupe avec la meilleure moyenne&nbsp;:';
-$string['bestgroupgrades'] = 'Les $a groupes avec la meilleure moyenne&nbsp;:';
+$string['bestgrade'] = 'La note la plus haute&nbsp;:';
+$string['bestgrades'] = 'Les $a notes les plus hautes&nbsp;:';
+$string['bestgroupgrade'] = 'Le groupe avec la moyenne la plus haute&nbsp;:';
+$string['bestgroupgrades'] = 'Les $a groupes avec la moyenne la plus haute&nbsp;:';
 $string['config_format_absolute'] = 'Nombres absolus';
 $string['config_format_fraction'] = 'Fractions';
 $string['config_format_percentage'] = 'Pourcents';
@@ -14,8 +14,8 @@ $string['config_names_id'] = 'N\'afficher que les identifiants numériques';
 $string['config_names_anon'] = 'Résultats anonymes';
 $string['config_no_quizzes_in_course'] = 'Ce cours ne comporte pas de test. Avant de pouvoir utiliser ce bloc correctement, vous devez ajouter au moins un test.';
 $string['config_select_quiz'] = 'De quel test voulez-vous afficher les résultats dans ce blocs&nbsp;?';
-$string['config_show_best'] = 'Combien de meilleures notes doivent être affichées (0 pour désactiver)&nbsp;?';
-$string['config_show_worst'] = 'Combien de plus mauvaises notes doivent être affichées (0 pour désactiver)&nbsp;?';
+$string['config_show_best'] = 'Combien de notes les plus hautes doivent être affichées (0 pour désactiver)&nbsp;?';
+$string['config_show_worst'] = 'Combien de notes les plus bases doivent être affichées (0 pour désactiver)&nbsp;?';
 $string['config_use_groups'] = 'Afficher les groupes au lieu des étudiants (si le test utilise les groupes)&nbsp;?';
 $string['configuredtoshownothing'] = 'La configuration de ce bloc ne permet l\'affichage d\'aucun résultat. Veuillez le configurer correctement ou le cacher.';
 $string['error_emptyquizid'] = 'Une erreur est survenue avec ce bloc&nbsp;: veuillez sélectionner le test dont il doit afficher les résultats.';
