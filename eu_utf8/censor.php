c0116f87b3101c9132471e9b98f96c30fc57f39f
Eloy Lafuente (stronk7) <stronk7>
2008-04-03 22:30:56 +0000

    Converting from iso-8859-1 to utf-8.

--- eu_utf8/censor.php
+++ eu_utf8/censor.php
@@ -2,7 +2,7 @@
 
 $string['filtername'] = 'Hitzen zentsura';
 $string['badwords'] = 'kaka,puta,urdanga,txortan,zakila,kikirrio,alua,ipurdi,ipurtzulo,kabroi,orgasmo,porno,bagina,masturbazio,putakume,bibragailu,sodomizatu,popatik,
-mierda,jodido,joder,follar,capullo,polla,nabo,cipote,coño,culo,verga,puta,fulana,zorra,corrida,gilipollas,cerdo,vibrador,masturbación,orgasmo,pene,negro,chocho,vagina,
-merde,enculé,bite,couille,pute,putain,niquer,gode,godemichet,nègre,
+mierda,jodido,joder,follar,capullo,polla,nabo,cipote,coÃ±o,culo,verga,puta,fulana,zorra,corrida,gilipollas,cerdo,vibrador,masturbaciÃ³n,orgasmo,pene,negro,chocho,vagina,
+merde,enculÃ©,bite,couille,pute,putain,niquer,gode,godemichet,nÃ¨gre,
 shit,fucked,fucker,fuck,dickhead,dick,cockhead,cock,cunt,asshole,arsehole,prick,bitch,jism,whore,slut,wanker,wank,bastard,dildo,masturbate,orgasm,penis,nigger,pussy';
 ?>
