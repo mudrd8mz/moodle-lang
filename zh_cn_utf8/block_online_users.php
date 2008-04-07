04dd592eed9c439ea1dd368f30e1e4b7cfaeaffa
Ling Li <liling>
2008-04-07 07:52:36 +0000

    Add missing strings

--- zh_cn_utf8/block_online_users.php
+++ zh_cn_utf8/block_online_users.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_online_users.php - created with Moodle 1.4.3 + (2004083132)
+      // block_online_users.php - created with Moodle 1.9 + (Build: 20080320) (2007101509)
 
 
 $string['blockname'] = '在线用户';
 $string['configtimetosee'] = '认为多少分钟内访问过网站的用户是在线的';
 $string['periodnminutes'] = '{$a}分钟内';
+$string['timetosee'] = '多长时间未活动后删除（分）';
 
 ?>
