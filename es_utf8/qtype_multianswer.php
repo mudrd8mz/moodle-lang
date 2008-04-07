d636c300cde3d25b96138a7cc186742e7e701644
Benito Arias <barias>
2008-04-07 00:33:36 +0000

    *** empty log message ***

---
+++ es_utf8/qtype_multianswer.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.8.2+ (2007021520)
+
+
+$string['decodeverifyquestiontext'] = 'DECODIFICAR Y VERIFICAR EL TEXTO DE LA PREGUNTA';
+
+?>
