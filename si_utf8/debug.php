6f032701beebd10ebdf9bd72942e363ab22e9654
Malinda Siriwardana <malindahs>
2008-04-09 15:15:02 +0000

    New File

---
+++ si_utf8/debug.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 2.0 dev (Build: 20080402) (2008030700)
+
+
+$string['cannotsetupsite'] = 'බැරෑරුම් දෝෂයකි! අඩවිය සැකසිය නොහැක!';
+$string['cannotupdaterelease'] = 'දෝෂය : දත්ත ගබඩාවේ නිකුතු අනුවාදය යාවත් කාලීන කල නොහැක!!';
+$string['cannotupdateversion'] = 'යාවත් කාලීන කිරීම අසාර්ථකයි! (අනුවාදය සැකසීම් වගුවේ යාවත් කාලීන කල නොහැක)';
+
+?>
