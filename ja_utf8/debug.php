003a93c20e9d0e969898269899a904ea70aa44dd
Mitsuhiro Yoshida <mits>
2008-04-10 22:37:31 +0000

    Adding a new translated lang file for debug MDL-14129.

---
+++ ja_utf8/debug.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 2.0 dev (Build: 20080331) (2008030700)
+
+
+$string['cannotsetupsite'] = '重大なエラー! サイトをセットアップできません!';
+$string['cannotupdaterelease'] = 'エラー: データベース内のリリースバージョンを更新できませんでした!!';
+$string['cannotupdateversion'] = 'アップグレードに失敗しました! (configテーブルのバージョンを更新できませんでした。)';
+$string['cannotupgradecapabilities'] = 'ロールシステムのコアケイパビリティのアップグレードに問題が発生しました。';
+$string['configmoodle'] = 'Moodleは、まだ設定されていません。最初にconfig.phpを編集してください。';
+$string['dbnotinsert'] = 'データベースエラー - レコードを挿入できません ($a)';
+$string['dbnotsetup'] = 'エラー: メインデータベースが正常にセットアップされませんでした。';
+$string['dbnotsupport'] = 'エラー: あなたのデータベース ($a) がMoodleでサポートできないか、install.xmlが作成されていません。lib/dbディレクトリをご覧ください。';
+$string['dbnotupdate'] = 'データベースエラー - 更新できません ($a)';
+$string['doesnotworkwitholdversion'] = 'このスクリプトは、この古いバージョンのMoodleでは動作しません。';
+$string['erroroccur'] = 'この処理中にエラーが発生しました。';
+$string['fixsetting'] = 'あなたのconfig.php設定を修正してください: <p>あなたは次のように設定していますが:</p> <p>$CFG->dirroot = \"$a[0]\";</p> <p>実際には次のように設定する必要があります:</p> <p>$CFG->dirroot = \"$a[1]\"</p>';
+$string['notables'] = 'テーブルがありません!';
+$string['phpvaroff'] = 'PHPサーバ変数「 $a[0] 」をOffにしてください - $a[1]';
+$string['phpvaron'] = 'PHPサーバ変数「 $a[0] 」がOnにされていません - $a[1]';
+$string['prefixcannotbeempty'] = 'あなたのターゲットDB ($a[1]) では、テーブル接頭辞「 $a[0] 」を空にすることはできません。';
+$string['prefixlimit'] = 'Oracle DBで許可されるテーブル接頭辞「 $a 」の最大長は、2ccです。';
+$string['withoutversion'] = 'メインversion.phpが読めない、または指定されていません。';
+
+?>
