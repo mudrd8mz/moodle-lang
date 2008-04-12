7c0088fd4f1b51f51300c6203fe77f5e1e04d999
koen roggemans <koenr>
2008-04-12 20:51:20 +0000

    new file

---
+++ nl_utf8/debug.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 2.0 dev (Build: 20080327) (2008030700)
+
+
+$string['cannotsetupsite'] = 'Fatale fout: kon de site niet installeren!';
+$string['cannotupdaterelease'] = 'Fout: kon de versie niet updaten in de databank!';
+$string['cannotupdateversion'] = 'Upgrade mislukt! (Kon de versie niet upgraden in de config tabel)';
+$string['cannotupgradecapabilities'] = 'Problemen met het upgraden van de hoofdmogelijkheden van het rollensysteem';
+$string['configmoodle'] = 'Moodle is nog niet geconfigureerd. Je moet eerst config.php bewerken.';
+$string['dbnotinsert'] = 'Databank fout - kan $a niet invoegen';
+$string['dbnotsetup'] = 'Fout. Hoofddatabank niet correct geïnstalleerd';
+$string['dbnotsupport'] = 'Fout. Je databank ($a) is nog niet ondersteund door Moodle of install.xml ontbreekt. Zoek in de lib/db-map.';
+$string['dbnotupdate'] = 'Databankfout - kan $a niet updaten';
+$string['doesnotworkwitholdversion'] = 'Dit script werkt niet met deze oude Moodleversie';
+$string['erroroccur'] = 'Er is een fout opgetreden tijdens dit proces.';
+$string['fixsetting'] = 'Verbeter je instellingen in config.php: <p>Je hebt:</p> <p>$CFG->dirroot = \"$a[0]\";</p> <p>maar het moet:</p> <p>$CFG->dirroot = \"$a[1]\" zijn</p>';
+$string['notables'] = 'Geen tabellen!';
+$string['phpvaroff'] = 'De PHP server variable \'$a[0]\' moet Off zijn - $a[1]';
+$string['phpvaron'] = 'De PHP server variable \'$a[0]\' is niet ingeschakeld - $a[1]';
+$string['prefixcannotbeempty'] = 'Tabel prefix \"$a[0]\" kan voor je doeldatabank niet leeg zijn ($a[1])';
+$string['prefixlimit'] = 'Tabel prefix \"$a\": de maximum toegelaten lengte voor Oracle Databanken is 2 tekens.';
+$string['withoutversion'] = 'version.php is niet leesbaar of niet opgegeven';
+
+?>
