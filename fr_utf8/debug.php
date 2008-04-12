4e798e2b67cd3c1f59d5d184009c994f10a1f275
Nicolas Martignoni <martignoni>
2008-04-12 09:17:04 +0000

    MDL-14129, create a new debug.php

---
+++ fr_utf8/debug.php
@@ -0,0 +1,24 @@
+<?php // $Id$
+/*
+ * debug information for developer only
+ */
+$string['cannotupgradecapabilities'] = 'Problèmes lors de la mise à jour des capacités centrales du systèmes des rôles';
+$string['cannotupdateversion'] = 'Échec de la mise à jour&nbsp;! (Impossible de mettre à jour la version dans la table config)';
+$string['cannotupdaterelease'] = 'Erreur&nbsp;: impossible de mettre à jour la version dans la base de données&nbsp;!!';
+$string['cannotsetupsite'] = 'Erreur grave&nbsp;! Impossible de mettre en place le site&nbsp;!';
+$string['configmoodle'] = 'Moodle n\'a pas encore été configuré. Vous devez avant tout écrire le fichier config.php.';
+$string['dbnotinsert'] = 'Erreur de base de données - Insertion impossible ($a)';
+$string['dbnotupdate'] = 'Erreur de base de données - Modification impossible ($a)';
+$string['dbnotsupport'] = 'Erreur&nbsp;: votre base de données ($a) n\'est pas totalement supportée par Moodle ou le fichier install.xml n\'est pas présent. Voyez dans le dossier lib/db.';
+$string['dbnotsetup'] = 'Erreur&nbsp;: la base de données principale <b>n\'a pas été mise en place</b> correctement';
+$string['doesnotworkwitholdversion'] = 'Ce script ne fonctionne pas avec cette ancienne version de Moodle';
+$string['erroroccur'] = 'Une erreur est survenue durant cette procédure';
+$string['fixsetting'] = 'Veuillez corriger vos réglages dans le fichier config.php. <p>Vous avez&nbsp;:</p> <p>\$CFG->dirroot = \"$a[0]\";</p> <p>alors qu\'il devrait y avoir</p> <p>\$CFG->dirroot = \"$a[1]\"</p>';
+$string['notables'] = 'Pas de tables&nbsp;!';
+$string['phpvaroff'] = 'La variable «&nbsp;$a[0]&nbsp;» du serveur PHP devrait être sur Off - $a[1]';
+$string['phpvaron'] = 'La variable «&nbsp;$a[0]&nbsp;» du serveur PHP n\'set pas sur On - $a[1]';
+$string['prefixcannotbeempty'] = 'Le préfixe des tables «&nbsp;$a[0]&nbsp;» ne peut pas être vite avec ce type de base de données ($a[1])';
+$string['prefixlimit'] = 'La longueur maximale du préfixe des tables «&nbsp;$a[0]&nbsp;» dans les bases de données Oracle est de 2 caractères.';
+$string['withoutversion'] = 'Le fichier principal version.php n\'set pas accessible';
+
+?>
