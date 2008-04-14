37d2ae1132a8d191416af7c1d973f14d17569bc1
Lee Chan-young <yogibear270>
2008-04-14 03:35:57 +0000

    New file -2008.04.xx

---
+++ ko_utf8/debug.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 2.0 dev (Build: 20080414) (2008030700)
+
+
+$string['cannotsetupsite'] = '심각한 오류! 사이트를 설정할 수 없음!';
+$string['cannotupdaterelease'] = '오류: 새 판의 데이터베이스 갱신을 할 수 없음!';
+$string['cannotupdateversion'] = '업그레이드 실패! (새 판의 설정 테이블을 갱신할 수 없음)';
+$string['cannotupgradecapabilities'] = '역할 시스템에 대한 핵심 권한 갱신을 할 수 없음';
+$string['configmoodle'] = '아직 무들이 설정되지 않았음. 우선 config.php를 수정할 필요가 있음';
+$string['dbnotinsert'] = '데이터베이스 오류 - $a 를 삽입할 수 없음';
+$string['dbnotsetup'] = '오류: 주 데이터베이스가 바르게 설정되지 않음';
+$string['dbnotsupport'] = '오류: 데이터베이스($a)가 아직 무들에 의해 지원되지 않거나 install.xml가 존재하지 않음. lib/db의 경로를 참조하기 바람';
+$string['dbnotupdate'] = '데이터베이스 오류 - $a 를 갱신할 수 없음';
+$string['doesnotworkwitholdversion'] = '무들의 예전 판에서는 이 스크립트가 작동하지 않음';
+$string['erroroccur'] = '현재의 과정에서 오류가 발생함';
+$string['fixsetting'] = 'config.php의 설정을 조절하십시오. <p>현재설정</p> <p>$CFG->dirroot = \"$a[0]\"; 를</p> <p>$CFG->dirroot = \"$a[1]\" 로 바꾸어야 합니다</p>';
+$string['notables'] = '테이블 없음!';
+$string['phpvaroff'] = 'PHP서버의 변수 \'$a[0]\'는  off되어야 함 -   $a[1]';
+$string['phpvaron'] = 'PHP서버의 변수 \'$a[0]\'가 On 이 아님 -   $a[1]';
+$string['prefixcannotbeempty'] = '대상 DB ($a[1])의 테이블 접두사를 공난으로 둘 수 없음';
+$string['prefixlimit'] = '오라클 DB의 테이블 접두사의 최대 허용 길이는 2cc임';
+$string['withoutversion'] = '주 version.php가 지정되지 않았거나 읽을 수 없음';
+
+?>
