4938dfa70f6e2fe0b40a17b94ecc9e945736dbd3
Ethem Evlice <ethem>
2008-04-14 16:07:42 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

---
+++ tr_utf8/debug.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 2.0 dev (Build: 20080414) (2008030700)
+
+
+$string['cannotsetupsite'] = 'Ciddi hata! Site ayarlanamadı.';
+$string['cannotupdaterelease'] = 'HATA: Sürüm bilgisi veritabanına yazılamadı!';
+$string['cannotupdateversion'] = 'Güncelleme hatası! (Config tablosu güncellemesi yapılamadı)';
+$string['cannotupgradecapabilities'] = 'Rol Sistemindeki ana yetkiler güncellenirken hata oluştu';
+$string['configmoodle'] = 'Moodle, henüz yapılandırılmamış. Öncelikle config.php dosyasını düzenlemeniz gerekiyor.';
+$string['dbnotinsert'] = 'Veritabanı hatası - ($a) eklenemedi';
+$string['dbnotsetup'] = 'Hata: Ana tablolar başarıyla ayarlanamadı';
+$string['dbnotsupport'] = 'Hata: Veritabanınız ($a) Moodle tarafından tam olarak desteklenmiyor veya lib/db/install.xml dosyası yok.';
+$string['dbnotupdate'] = 'Veritabanı hatası - ($a) güncellenemedi';
+$string['doesnotworkwitholdversion'] = 'Bu betik, Moodle\'ın eski sürümüyle çalışmaz.';
+$string['erroroccur'] = 'İşlem sırasında hata oluştu';
+$string['fixsetting'] = 'Lütfen config.php deki ayarları düzeltin: <p>Şimdiki:</p> <p>$CFG->dirroot = \"$a[0]\";</p><p>Olması gereken:</p> <p>$CFG->dirroot = \"$a[1]\"</p>';
+$string['notables'] = 'Tablo Yok!';
+$string['phpvaroff'] = 'PHP değişkeni \'$a[0]\' Kapalı olmalı - $a[1]';
+$string['phpvaron'] = 'PHP değişkeni \'$a[0]\' Açık olmalı - $a[1]';
+$string['prefixcannotbeempty'] = 'Tablo öneki \"$a[0]\", bu veritabanı ($a[1]) için boş olamaz.';
+$string['prefixlimit'] = 'Oracle veritabanı için izin verilen tablo öneki \"$a\" en fazla 2 karakterdir.';
+$string['withoutversion'] = 'Ana version.php dosyası okunabilir değil veya yok';
+
+?>
