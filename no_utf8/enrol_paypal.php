c3cb428c96a587018bb32aaa23c549ee646cc531
John Harald Gartland <johnhg>
2008-04-18 16:45:09 +0000

    New file in Moodle 1.9

---
+++ no_utf8/enrol_paypal.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // enrol_paypal.php - created with Moodle 1.9 (2007101509)
+
+
+$string['business'] = 'Epostadresse til din forretnings PayPal-konto';
+$string['costorkey'] = 'Velg en av følgende metoder for innmelding';
+$string['description'] = 'PayPal modulen tillater at du setter opp betalte kurs. Hvis prisen for kurs er 0, så blir ikke studentene bedt om å betale for oppføringen. Du kan sette en nettstedspris som standard her og i kurs-installingene kan du sette pris for hvert enkelt kurs. Kursprisen overstyrer nettstedsprisen.';
+$string['enrolname'] = 'PayPal';
+$string['paypalaccepted'] = 'PayPal betalingsformer som er godkjent';
+$string['sendpaymentbutton'] = 'Send betaling via PayPal';
+
+?>
