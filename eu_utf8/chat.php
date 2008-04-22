9d251d064ddea9690f78c430bcd864fae3cbdb04
Abel Camacho <ueu_ueu>
2008-04-22 15:32:50 +0000

    Santurtziko Udal Euskaltegiak koordinatutako itzulpena

--- eu_utf8/chat.php
+++ eu_utf8/chat.php
@@ -1,18 +1,18 @@
 <?PHP // $Id$ 
-      // chat.php - created with Moodle 1.8.2+ (2007021520)
+      // chat.php - created with Moodle 1.9 + (Build: 20080409) (2007101509)
 
 
 $string['beep'] = 'Abisatu';
 $string['chat:chat'] = 'Hitz egin';
 $string['chat:deletelog'] = 'Txataren agerraldiak ezabatu';
 $string['chat:readlog'] = 'Txataren agerraldiak irakurri';
-$string['chatintro'] = 'Sarrera';
+$string['chatintro'] = 'Sarrera-testua';
 $string['chatname'] = 'Txat-gela honen izena';
 $string['chatreport'] = 'Txat-saioetako txostenak';
 $string['chattime'] = 'Hurrengo txat-saioaren ordua';
-$string['configmethod'] = 'Txataren ohiko metodoan erabiltzaileek aldiro konektatzen dute zerbitzariarekin eguneraketak egiteko. Ez du konfiguraziorik behar eta edonon funtzionatzen du baina zerbitzaria gainkarga dezake gelan erabiltzaile asko aldi berean ari badira. \'Daemon\'a erabiltzeak (bigarren mailako egikaritza-prozesu independientea)Unixerako sheell (komando-interpretaria) sarbidea eskatzen du, baina txat-giro arinago eta lorgarriagoa ematen du.';
+$string['configmethod'] = 'Txataren ohiko metodoan erabiltzaileek aldiro konektatzen dute zerbitzariarekin eguneraketak egiteko. Ez du konfiguraziorik behar eta edonon funtzionatzen du baina zerbitzaria gainkarga dezake gelan erabiltzaile asko aldi berean ari badira. \'Daemon\'a erabiltzeak (bigarren mailako egikaritza-prozesu independentea)Unixerako sheell (komando-interpretaria) sarbidea eskatzen du, baina txat-giro arinago eta lorgarriagoa ematen du.';
 $string['confignormalupdatemode'] = 'HTTP 1.1ren <em>Keep-Alive</em> ezaugarria erabilita zerbitzatzen dira normalean txat-gelaren eguneraketak baina zerbitzariarentzat nahikoa gogorra da lan hau. Erabiltzaileei eguneraketak bidaltzeko metodo aurreratuagoa da <em>Stream</em> estrategia erabiltzea. <em>Stream</em> erabilita eskalamendu hobea lortzen da (chatd metodoaren antzekoa da) baina agian zure zerbitzariak ez du onartuko aukera hau.';
-$string['configoldping'] = 'Jarduerarik gabe zenbat denbora (segundutan) igarota uste izan behar da erabiltzaileak utzi egin duela?';
+$string['configoldping'] = 'Jarduerarik gabe zenbat denbora (segundutan) igarota pentsatu behar da erabiltzaileak utzi egin duela?';
 $string['configrefreshroom'] = 'Zenbat segunduro eguneratu behar da txataren orria? Balore baxuak elkarrizketa arinagoa ahalbidetuko du baina zerbitzariarentzat karga handia izan daiteke gelan lagun asko dagoenean.';
 $string['configrefreshuserlist'] = 'Zenbat segunduro eguneratu behar da txatean dauden erabiltzaileen zerrenda?';
 $string['configserverhost'] = 'Zerbitzariaren \'daemnon\'a (egikaritza prozesu independientea) dagoen ordenagailuaren \'Hostame\'a (etxekoaren izena)';
@@ -37,6 +37,7 @@ $string['messagebeepsyou'] = '$a-(e)k dio: Aizu! Hemen nago!';
 $string['messageenter'] = '$a oraintxe sartu da gelan';
 $string['messageexit'] = '$a irten egin da gelatik';
 $string['messages'] = 'Mezuak';
+$string['method'] = 'Txat metodoa';
 $string['methoddaemon'] = 'Txat zerbitzariaren \'daemon\'a';
 $string['methodnormal'] = 'Metodo arrunta';
 $string['modulename'] = 'Txat-gela';
@@ -48,15 +49,25 @@ $string['nomessages'] = 'Ez dago mezurik oraindik';
 $string['normalkeepalive'] = 'KeepAlive';
 $string['normalstream'] = 'Stream';
 $string['noscheduledsession'] = 'Ez dago programatuta txat-saiorik';
+$string['oldping'] = 'Itxaroteko epea deskonektatu';
+$string['pastchats'] = 'Izandako txat-saioak';
+$string['refreshroom'] = 'Gela freskatu';
+$string['refreshuserlist'] = 'Erabiltzaileen zerrenda freskatu';
+$string['removemessages'] = 'Mezu guztiak ezabatu';
 $string['repeatdaily'] = 'Egunero ordu berean';
 $string['repeatnone'] = 'Ez errepikatu - zehaztutako ordua bakarrik argitaratu';
 $string['repeattimes'] = 'Errepikatu saioak';
 $string['repeatweekly'] = 'Astero ordu berean';
 $string['savemessages'] = 'Izan diren txat-saioak gorde';
 $string['seesession'] = 'Ikusi saio honetako txostena';
+$string['serverhost'] = 'Zerbitzariaren izena';
+$string['serverip'] = 'Zerbitzariaren IPa';
+$string['servermax'] = 'Gehienezko erabiltzaileak';
+$string['serverport'] = 'Zerbitzariaren portua';
 $string['sessions'] = 'Txat-saioetako txostenak';
 $string['strftimemessage'] = '%%H:%%M';
 $string['studentseereports'] = 'Denek ikus ditzakete izandako saioetako txostenak';
+$string['updatemethod'] = 'Metodoa eguneratu';
 $string['viewreport'] = 'Izan diren txat-saioak ikusi';
 
 ?>
