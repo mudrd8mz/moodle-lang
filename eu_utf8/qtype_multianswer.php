d5b838b21c9e47de8cf4986a944d4185e1b35869
Abel Camacho <ueu_ueu>
2008-04-22 10:26:25 +0000

    Santurtziko Udal Euskaltegiak koordinatutako itzulpena

---
+++ eu_utf8/qtype_multianswer.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.9 + (Build: 20080409) (2007101509)
+
+
+$string['decodeverifyquestiontext'] = 'Galderaren testua deskodetu eta egiaztatu';
+
+?>
