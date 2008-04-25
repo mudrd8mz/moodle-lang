6864c7c00542c8d741f3d0e23deb2a92e3c8a1d5
Malinda Siriwardana <malindahs>
2008-04-25 01:32:49 +0000

    New File and existing string translations

---
+++ si_utf8/feedback.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // feedback.php - created with Moodle 2.0 dev (Build: 20080420) (2008030700)
+
+
+$string['add_item'] = 'ක්‍රියාකාරකම සඳහා ප්‍රශ්නයක් එක් කරන්න';
+$string['add_items'] = 'ක්‍රියාකාරකම සඳහා ප්‍රශ්නයක් එක් කරන්න';
+$string['add_pagebreak'] = 'පිටු කඩනයක් එක් කරන්න';
+$string['adjustment'] = 'සීරුමාරු';
+$string['after_submit'] = 'ඉදිරිපත් කිරීමෙන් පසු';
+
+?>
