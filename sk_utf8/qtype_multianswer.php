a25d4fb3e20650a829a3700030bac5605278eb85
Miroslav Fikar <fikar>
2008-04-29 19:17:02 +0000

    minor fixes

--- sk_utf8/qtype_multianswer.php
+++ sk_utf8/qtype_multianswer.php
@@ -1,3 +1,7 @@
-<?php
-$string['decodeverifyquestiontext'] = 'SKONTROLUJTE TEXT OTÁZKY';
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.8.5+ (Build: 20080423) (2007021550)
+
+
+$string['decodeverifyquestiontext'] = 'Skontrolujte text otázky';
+
 ?>
