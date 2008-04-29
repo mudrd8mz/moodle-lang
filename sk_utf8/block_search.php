e45eac47c0367736c0151f4480f5f59b7910be93
Miroslav Fikar <fikar>
2008-04-29 19:14:28 +0000

    Initial revision

---
+++ sk_utf8/block_search.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // block_search.php - created with Moodle 1.8.5+ (Build: 20080423) (2007021550)
+
+
+$string['blockname'] = 'Globálne vyhľadávanie';
+$string['bytes'] = 'bajty (0 znamená bez obmedzenia)';
+$string['configbuttonlabel'] = 'Názov tlačidla';
+$string['configenablefileindexing'] = 'Povoliť indexovanie súborov';
+$string['configfiletypes'] = 'Spracovávané typy súborov';
+$string['configlimitindexbody'] = 'Obmedzenie na veľkosť indexovaných súborov';
+$string['configpdftotextcmd'] = 'Cesta k programu pdftotext';
+$string['configsearchtext'] = 'Text pre vyhľadávanie';
+$string['configwordtotextcmd'] = 'Cesta k programu doctotext';
+$string['configwordtotextenv'] = 'Nastavenie prostredia pre MSWord konvertor';
+$string['go'] = 'Hľadať';
+$string['searchmoodle'] = 'Vyhľadávať v Moodle';
+$string['usemoodleroot'] = 'Pridať Moodle adresár k ceste pre textové konvertory';
+
+?>
