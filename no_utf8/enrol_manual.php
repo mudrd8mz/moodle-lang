20998e8f0cd567446ceeeff032c861572d8edb79
John Harald Gartland <johnhg>
2008-04-29 18:57:30 +0000

    Changes in 1.9

--- no_utf8/enrol_manual.php
+++ no_utf8/enrol_manual.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_manual.php - created with Moodle 1.6.3 (2006050530)
+      // enrol_manual.php - created with Moodle 1.9 (2007101509)
 
 
 $string['description'] = 'Dette er standard form for innmelding. Det er to hovedmåter en student kan bli innmeldt på et spesielt kurs. 
@@ -8,5 +8,6 @@ $string['description'] = 'Dette er standard form for innmelding. Det er to hoved
 <li>Det kan være definert et passord for et kurs, kalt \"innmeldingsnøkkelen\". Alle som kjenner denne nøkkelen er i stand til å legge seg selv inn i et kurs.</li>
 </ul>';
 $string['enrolname'] = 'Intern påmelding.';
+$string['keyholderrole'] = 'Rollen til brukeren som har innmeldingskoden for kurset. Vises til studenter som prøver å melde seg på kurset.';
 
 ?>
