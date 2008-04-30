7fa3b460081cc5488422c58e5f1fd99ad63ce996
John Harald Gartland <johnhg>
2008-04-30 21:38:26 +0000

    New file in Moodle 1.9

---
+++ no_utf8/notes.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 1.9 + (Build: 20080430) (2007101509)
+
+
+$string['addnewnote'] = 'Legg til nytt notat';
+$string['addnewnoteselect'] = 'Velg brukere å skrive notatet om';
+$string['bynameondate'] = 'av $a->name - $a->date';
+$string['content'] = 'Innhold';
+$string['course'] = 'kurs';
+$string['coursenotes'] = 'Kursnotater';
+$string['created'] = 'laget';
+$string['deleteconfirm'] = 'Slett dette notatet?';
+$string['deletenotes'] = 'Slett alle notater';
+$string['editnote'] = 'Rediger notat';
+$string['groupaddnewnote'] = 'Legg til nytt notat for alle';
+$string['nocontent'] = 'Notatet kan ikke være tomt';
+$string['nonotes'] = 'Ingen notater av denne typen finnes ennå';
+$string['note'] = 'Notat';
+$string['notes'] = 'Notater';
+$string['notesnotvisible'] = 'Du har ikke tillatelse til å se notatene';
+$string['nouser'] = 'Du må velge en bruker';
+$string['personal'] = 'personlig';
+$string['personalnotes'] = 'Personlige notater';
+$string['publishstate'] = 'Status';
+$string['site'] = 'nettsted';
+$string['sitenotes'] = 'nettstedsnotater';
+$string['unknown'] = 'ukjent';
+
+?>
