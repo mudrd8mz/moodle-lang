83443d8d102f176ee4914dafbcc1e492e85874e9
John Harald Gartland <johnhg>
2008-05-01 06:13:58 +0000

    New file in Moodle 1.9

---
+++ no_utf8/qtype_shortanswer.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // qtype_shortanswer.php - created with Moodle 1.9 + (Build: 20080430) (2007101509)
+
+
+$string['addmoreanswerblanks'] = 'Blanke for {ingen} Flere svar';
+$string['answerno'] = 'Svar $a';
+$string['filloutoneanswer'] = 'Du må angi minst et mulig svar. Svar som er blanke vil ike brukes. \'*\' kan brukes som jokere for å matche et hvilket som helst tegn. Første riktige svar brukes for å gi score og tilbakemelding.';
+
+?>
---
+++ no_utf8/qtype_truefalse.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // qtype_truefalse.php - created with Moodle 1.9 + (Build: 20080430) (2007101509)
+
+
+$string['addingtruefalse'] = 'Legger til et Riktig/Feil-spørsmål';
+$string['correctanswer'] = 'Riktig svar';
+$string['editingtruefalse'] = 'Redigerer et Riktig/Feil-spørsmål';
+$string['false'] = 'Feil';
+$string['feedbackfalse'] = 'Tilbakemelding for svaret \'Feil\'';
+$string['feedbacktrue'] = 'Tilbakemelding for svaret \'Riktig\'';
+$string['true'] = 'Riktig';
+$string['truefalse'] = 'Riktig/Feil';
+
+?>
