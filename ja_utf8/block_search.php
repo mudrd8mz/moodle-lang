618bd686fcfb5965cd3576ada4838a21dc79fd9d
Mitsuhiro Yoshida <mits>
2008-05-03 06:19:23 +0000

    Translated new strings for block search MDL-14646.

--- ja_utf8/block_search.php
+++ ja_utf8/block_search.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // block_search.php - created with Moodle 2.0 dev (Build: 20080417) (2008030700)
+      // block_search.php - created with Moodle 2.0 dev (Build: 20080503) (2008050200)
 
 
 $string['blockname'] = 'グローバルサーチ';
+$string['blockssearchswitches'] = 'ブロックのインデックサ有効化';
 $string['bytes'] = 'バイト (0 は制限なしを意味します)';
 $string['configbuttonlabel'] = 'ボタンラベル';
 $string['configenablefileindexing'] = 'インデックシングを有効にする';
@@ -10,10 +11,19 @@ $string['configfiletypes'] = '検索対象ファイルタイプ';
 $string['configlimitindexbody'] = 'インデックスボディサイズの制限';
 $string['configpdftotextcmd'] = 'pdftotextコマンドのパス';
 $string['configsearchtext'] = '検索テキスト';
+$string['configtypetotxtcmd'] = 'コンバータのコマンドライン';
+$string['configtypetotxtenv'] = 'コンバータの環境定義';
 $string['configwordtotextcmd'] = 'doctotextコマンドのパス';
 $string['configwordtotextenv'] = 'MSWordコンバータの環境設定';
 $string['go'] = 'Go!';
+$string['handlingfor'] = '特別ハンドリング';
+$string['modulessearchswitches'] = 'モジュールのインデックサ有効化';
+$string['nosearchableblocks'] = '該当するブロックはありません。';
+$string['nosearchablemodules'] = '該当するモジュールはありません。';
+$string['pdfhandling'] = 'Acrobat PDFハンドリング';
+$string['searchdiscovery'] = '検索可能アイテムの調査';
 $string['searchmoodle'] = 'Moodleの検索';
 $string['usemoodleroot'] = '外部コンバータにMoodle rootを使用する';
+$string['wordhandling'] = 'Microsoft Wordハンドリング';
 
 ?>
