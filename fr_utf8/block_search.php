3be62be4da5f4e9e5946c581e8b8e1dfcc051ba4
Nicolas Martignoni <martignoni>
2008-05-04 15:22:05 +0000

    New strings for MDL-14646

--- fr_utf8/block_search.php
+++ fr_utf8/block_search.php
@@ -13,5 +13,15 @@ $string['go'] = 'Lancer la recherche';
 $string['bytes'] = ' octets (0 pour «&nbsp;pas de limite&nbsp;»)';
 $string['searchmoodle'] = 'Rechercher dans le site';
 $string['usemoodleroot'] = 'Utiliser la racine de Moodle pour les convertisseurs externes';
+$string['wordhandling'] = 'Traitement du format Microsoft Word';
+$string['pdfhandling'] = 'Traitement du format Acrobat PDF';
+$string['handlingfor'] = 'Traitement supplémentaire pour';
+$string['searchdiscovery'] = 'Découverte des éléments recherchables';
+$string['configtypetotxtcmd'] = 'Ligne de commande du convertisseur';
+$string['configtypetotxtenv'] = 'Définition de l\'environnement du convertisseur';
+$string['modulessearchswitches'] = 'Activation de l\'indexeur pour les modules';
+$string['blockssearchswitches'] = 'Activation de l\'indexeur pour les blocs';
+$string['nosearchablemodules'] = 'Aucun module recherchable';
+$string['nosearchableblocks'] = 'Aucun bloc recherchable';
 
 ?>
