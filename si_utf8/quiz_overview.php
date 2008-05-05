19dd0da8a6e104d5377eeb23e0380ac4e70d0439
Malinda Siriwardana <malindahs>
2008-05-05 16:54:36 +0000

    new strings

--- si_utf8/quiz_overview.php
+++ si_utf8/quiz_overview.php
@@ -1,12 +1,20 @@
 <?PHP // $Id$ 
-      // quiz_overview.php - created with Moodle 2.0 dev (2007101508)
+      // quiz_overview.php - created with Moodle 2.0 dev (Build: 20080505) (2008050200)
 
 
 $string['allattempts'] = 'සියලුම උත්සාහයන් පෙන්වන්න';
 $string['allstudents'] = '$a සියල්ල පෙන්වන්න';
 $string['attemptsonly'] = '$a උත්සාහයන් සමග පෙන්වන්න';
 $string['noattemptsonly'] = '$a නිර්උත්සාහයන් සමග පෙන්වන්න';
+$string['optallattempts'] = 'සියලු උත්සාහයන්';
+$string['optallstudents'] = 'සියලු $a';
+$string['optattemptsonly'] = 'උත්සාහයන් දරා ඇති $a පමණි';
+$string['optnoattemptsonly'] = 'උත්සාහයන් නොදරා ඇති $a පමණි';
 $string['overview'] = 'දළ විශ්ලේෂණය';
 $string['overviewdownload'] = 'බාගත කිරීම් දළ විශ්ලේෂණයක්?';
+$string['preferencespage'] = 'මෙම පිටුව සඳහා පමණක් අභිරුචි';
+$string['preferencessave'] = 'අභිරුචි සුරක්ෂිත කරන්න';
+$string['preferencesuser'] = 'මෙම වාර්තාව සඳහා ඔබගේ අභිරුචි';
+$string['show'] = 'පෙන්වන්න';
 
 ?>
