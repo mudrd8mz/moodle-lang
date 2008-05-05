cd3e666db97a65d086782c1d16a5597508812279
Adam Pawelczak <adamtpaw>
2008-05-05 08:11:02 +0000

    Small mistakes removed

--- pl_utf8/block_course_list.php
+++ pl_utf8/block_course_list.php
@@ -1,12 +1,7 @@
-﻿<?PHP // $Id$ 
-      // block_course_list.php - created with Moodle 1.9 Beta 3 (2007101505)
+<?PHP // $Id$ 
+      // block_course_list.php - created with Moodle 1.9 + (Build: 20080423) (2007101509)
 
 
-$string['allcourses'] = 'Administrator widzi wszystkie kursy';
-$string['blockname'] = 'Lista kursów';
-$string['configadminview'] = 'Konfiguracja tego, co widzi administrator';
-$string['confighideallcourseslink'] = 'Ukryj link \'Wszystkie kursy\' na dole bloku. Ukrycie linku nie dotyczy widoku administratora';
-$string['owncourses'] = 'Administrator widzi swoje kursy';
 $string['adminview'] = 'Widok Admina';
 $string['allcourses'] = 'Administrator widzi wszystkie kursy';
 $string['blockname'] = 'Lista kursów';
@@ -14,4 +9,5 @@ $string['configadminview'] = 'Konfiguracja tego, co widzi administrator';
 $string['confighideallcourseslink'] = 'Ukryj link \'Wszystkie kursy\' na dole bloku. Ukrycie linku nie dotyczy widoku administratora';
 $string['hideallcourseslink'] = 'Ukryj wszystkie link \"Wszystkie kursy\"';
 $string['owncourses'] = 'Administrator widzi swoje kursy';
+
 ?>
