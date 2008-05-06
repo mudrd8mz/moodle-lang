61a19218af40cdf21cc52967864c0b8ca540269b
Mitsuhiro Yoshida <mits>
2008-05-06 03:23:38 +0000

    Translated new strings for quiz overview report MDL-14185.

--- ja_utf8/quiz_overview.php
+++ ja_utf8/quiz_overview.php
@@ -1,12 +1,20 @@
 <?PHP // $Id$ 
-      // quiz_overview.php - created with Moodle 1.8 dev (2006120700)
+      // quiz_overview.php - created with Moodle 2.0 dev (Build: 20080504) (2008050200)
 
 
 $string['allattempts'] = 'すべての受験を表示する';
 $string['allstudents'] = 'すべての $a を表示する';
 $string['attemptsonly'] = '受験した $a のみ表示する';
 $string['noattemptsonly'] = '未受験の $a のみ表示する';
+$string['optallattempts'] = 'すべての受験';
+$string['optallstudents'] = 'すべての $a';
+$string['optattemptsonly'] = '受験した $a のみ';
+$string['optnoattemptsonly'] = '未受験の $a のみ';
 $string['overview'] = '概要';
 $string['overviewdownload'] = '概要のダウンロード';
+$string['preferencespage'] = 'このページのみに関する設定';
+$string['preferencessave'] = '設定を保存する';
+$string['preferencesuser'] = 'このレポートに関する、あなたの設定';
+$string['show'] = '表示';
 
 ?>
