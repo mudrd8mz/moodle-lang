6a78da461497a38d77c2ca3ddeca1aed03817eca
Lee Chan-young <yogibear270>
2008-05-06 12:28:32 +0000

    change some terms and translate added strings.

--- ko_utf8/quiz_overview.php
+++ ko_utf8/quiz_overview.php
@@ -1,12 +1,20 @@
 <?PHP // $Id$ 
-      // quiz_overview.php - created with Moodle 1.6 + (2006050506)
+      // quiz_overview.php - created with Moodle 2.0 dev (Build: 20080506) (2008050200)
 
 
-$string['allattempts'] = '모든 시도 보기';
-$string['allstudents'] = '모든 $a 보기';
-$string['attemptsonly'] = '시도가 있는 $a 보기';
-$string['noattemptsonly'] = '시도가 없는 $a 보기';
+$string['allattempts'] = '모든 시도';
+$string['allstudents'] = '모든 $a';
+$string['attemptsonly'] = '시도했던 $a 만';
+$string['noattemptsonly'] = '시도가 없었던 $a 만';
+$string['optallattempts'] = '모든 시도';
+$string['optallstudents'] = '모든 $a';
+$string['optattemptsonly'] = '시도만 했던 $a';
+$string['optnoattemptsonly'] = '시도 안했던 $a';
 $string['overview'] = '개요';
 $string['overviewdownload'] = '개요 내려받음';
+$string['preferencespage'] = '화면 표시 대상';
+$string['preferencessave'] = '설정 저장';
+$string['preferencesuser'] = '보고서 설정';
+$string['show'] = '표시';
 
 ?>
