5b0a0aa81d966fea17bdd724c87d9cd1c08c66fa
Nicolas Martignoni <martignoni>
2008-05-06 17:23:04 +0000

    MDL-14185, new strings added

--- fr_utf8/quiz_overview.php
+++ fr_utf8/quiz_overview.php
@@ -4,7 +4,15 @@ $string['attemptsonly'] = 'N\'afficher que les $a avec tentatives';
 $string['allattempts'] = 'Afficher toutes les tentatives';
 $string['allstudents'] = 'Afficher tous les $a';
 $string['noattemptsonly'] = 'N\'afficher que les $a sans tentatives';
+$string['optallattempts'] = 'toutes les tentatives';
+$string['optallstudents'] = 'tous les $a';
+$string['optattemptsonly'] = 'seulement $a avec tentatives';
+$string['optnoattemptsonly'] = 'seulement $a sans tentatives';
 $string['overview'] = 'Vue d\'ensemble';
 $string['overviewdownload'] = 'Téléchargement vue d\'ensemble';
+$string['preferencespage'] = 'Préférences pour cette page';
+$string['preferencessave'] = 'Enregistrer préférences';
+$string['preferencesuser'] = 'Vos préférences pour ce rapport';
+$string['show'] = 'Afficher';
 
 ?>
