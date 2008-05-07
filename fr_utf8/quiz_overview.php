9e5594f9677c0a15ab6b81e3844e18993e1a3df5
Nicolas Martignoni <martignoni>
2008-05-07 17:09:01 +0000

    MDL-14199 more strings added

--- fr_utf8/quiz_overview.php
+++ fr_utf8/quiz_overview.php
@@ -3,16 +3,20 @@
 $string['attemptsonly'] = 'N\'afficher que les $a avec tentatives';
 $string['allattempts'] = 'Afficher toutes les tentatives';
 $string['allstudents'] = 'Afficher tous les $a';
+$string['attemptsprepage'] = 'Tentatives affichées par page';
 $string['noattemptsonly'] = 'N\'afficher que les $a sans tentatives';
 $string['optallattempts'] = 'toutes les tentatives';
 $string['optallstudents'] = 'tous les $a';
 $string['optattemptsonly'] = 'seulement $a avec tentatives';
 $string['optnoattemptsonly'] = 'seulement $a sans tentatives';
+$string['optonlygradedattempts'] = 'seulement la tentative évaluée de chaque participant';
 $string['overview'] = 'Vue d\'ensemble';
 $string['overviewdownload'] = 'Téléchargement vue d\'ensemble';
+$string['pagesize'] = 'Taille de page';
 $string['preferencespage'] = 'Préférences pour cette page';
 $string['preferencessave'] = 'Enregistrer préférences';
 $string['preferencesuser'] = 'Vos préférences pour ce rapport';
 $string['show'] = 'Afficher';
+$string['showdetailedmarks'] = 'Afficher les points pour chaque question';
 
 ?>
