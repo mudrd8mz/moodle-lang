16f50925dc160b0a9670f03e2d6f4cfc429f9113
Abel Camacho <ueu_ueu>
2008-05-07 10:33:20 +0000

    Santurtziko Udal Euskaltegiak koordinatutako itzulpena

--- eu_utf8/filters.php
+++ eu_utf8/filters.php
@@ -1,8 +1,8 @@
 <?PHP // $Id$ 
-      // filters.php - created with Moodle 1.8.2+ (2007021520)
+      // filters.php - created with Moodle 1.9 + (Build: 20080409) (2007101509)
 
 
-$string['actfilterhdr'] = 'Indarrean dauden iragazkiak (ORed elkarrekin)';
+$string['actfilterhdr'] = 'Indarrean dauden iragazkiak';
 $string['addfilter'] = 'Iragazkia gehitu';
 $string['anycategory'] = 'edozein kategoria';
 $string['anycourse'] = 'edozein ikastaro';
@@ -12,22 +12,34 @@ $string['anyvalue'] = 'edozein balore';
 $string['categoryrole'] = 'Kategoria-rola';
 $string['contains'] = 'daukana';
 $string['courserole'] = 'Ikastaro-rola';
+$string['courserolelabel'] = '$a->categoryname kategoriako  $a->coursename ikastaroan $a->rolename  rolaren $a->label da';
+$string['courserolelabelerror'] = '$a->label errorea: $a->coursename ikastaroa ez da existitzen';
+$string['datelabelisafter'] = '$a->label $a->after -ren ondoren dago';
+$string['datelabelisbefore'] = '$a->label $a->after -ren aurretik dago';
+$string['datelabelisbetween'] = '$a->label $a->after eta $a->before -ren artean dago';
 $string['doesnotcontain'] = 'ez daukana';
 $string['endswith'] = 'honela bukatzen da';
 $string['firstaccess'] = 'Lehen sarbidea';
+$string['globalrolelabel'] = '$a->label $a->value da';
 $string['isafter'] = '$a-ren ondoren';
 $string['isanyvalue'] = 'edozein balore';
 $string['isbefore'] = '$a-ren aurretik';
-$string['isbetween'] = '$a[0] eta $a[1]-ren artekoa';
 $string['isdefined'] = 'definituta dago';
 $string['isempty'] = 'hutsik dago';
 $string['isequalto'] = 'honen berdina';
 $string['isnotdefined'] = 'ez dago definituta';
 $string['isnotequalto'] = 'ez da honen berdina';
 $string['newfilter'] = 'iragazki berria';
-$string['removeall'] = 'Ezabatu dena';
-$string['removeselected'] = 'Ezabatu aukeratutakoa';
-$string['setfilter'] = 'Iragazkia findu';
+$string['profilelabel'] = '$a->label: $a->profile $a->operator $a->value';
+$string['profilelabelnovalue'] = '$a->label: $a->profile $a->operator';
+$string['removeall'] = 'Iragazki guztiak ezabatu';
+$string['removeselected'] = 'Aukeratutakoa ezabatu';
+$string['selectlabel'] = '$a->label $a->operator $a->value';
 $string['startswith'] = 'Honela hasten da';
+$string['tablenosave'] = 'Goiko taulako aldaketak automatikoki gordetzen dira';
+$string['textlabel'] = '$a->label $a->operator $a->value';
+$string['textlabelnovalue'] = '$a->label $a->operator';
+$string['isbetween'] = '$a[0] eta $a[1]-ren artekoa'; // ORPHANED
+$string['setfilter'] = 'Iragazkia findu'; // ORPHANED
 
 ?>
