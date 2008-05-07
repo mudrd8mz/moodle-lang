548b55bd6703126ef95267f49c0d54736430c327
Mitsuhiro Yoshida <mits>
2008-05-07 15:54:11 +0000

    Translated new strings for debug and error MDL-14129. Brushed up a translated help file for IMS Enterprise enrolment. Updated transalted help files for richtext based on lang/en_utf8 change MDL-11113. Translated a new string for quiz overview report MDL-14201.

--- ja_utf8/quiz_overview.php
+++ ja_utf8/quiz_overview.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // quiz_overview.php - created with Moodle 2.0 dev (Build: 20080504) (2008050200)
+      // quiz_overview.php - created with Moodle 2.0 dev (Build: 20080507) (2008050700)
 
 
 $string['allattempts'] = 'すべての受験を表示する';
@@ -14,6 +14,7 @@ $string['optnoattemptsonly'] = '未受験の「 $a 」のみ';
 $string['optonlygradedattempts'] = '評定された受験のみ表示する';
 $string['overview'] = '概要';
 $string['overviewdownload'] = '概要のダウンロード';
+$string['overviewreportgraph'] = '到達した評点の範囲に関する学生数の棒グラフ';
 $string['pagesize'] = 'ページサイズ';
 $string['preferencespage'] = 'このページのみに関する設定';
 $string['preferencessave'] = '設定を保存する';
