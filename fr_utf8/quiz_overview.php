2dc7901b1a05f611f6cd37256dbcf1a873acdd8e
Nicolas Martignoni <martignoni>
2008-05-08 12:14:52 +0000

    MDL-14198 new strings

--- fr_utf8/quiz_overview.php
+++ fr_utf8/quiz_overview.php
@@ -2,10 +2,13 @@
 
 $string['attemptsonly'] = 'N\'afficher que les $a avec tentatives';
 $string['allattempts'] = 'Afficher toutes les tentatives';
+$string['allattemptscontributetograde'] = 'Toutes les tentatives comptent pour la note finale du participant.';
 $string['allstudents'] = 'Afficher tous les $a';
 $string['attemptsprepage'] = 'Tentatives affichées par page';
 $string['deleteselected'] = 'Supprimer les tentatives sélectionnées';
+$string['highlightinggraded'] = 'La tentative comptant pour la note finale est surlignée pour chaque participant.';
 $string['noattemptsonly'] = 'N\'afficher que les $a sans tentatives';
+$string['onlyoneattemptallowed'] = 'Une seule tentative par participant est autorisée pour ce test.';
 $string['optallattempts'] = 'toutes les tentatives';
 $string['optallstudents'] = 'tous les $a';
 $string['optattemptsonly'] = 'seulement $a avec tentatives';
@@ -20,5 +23,7 @@ $string['preferencessave'] = 'Enregistrer préférences';
 $string['preferencesuser'] = 'Vos préférences pour ce rapport';
 $string['show'] = 'Afficher';
 $string['showdetailedmarks'] = 'Afficher les points pour chaque question';
+$string['showinggraded'] = 'Afficher seulement les tentatives évaluées de chaque participant.';
+$string['showinggradedandungraded'] = 'Afficher toutes les tentatives de chaque participant. La tentative évaluée est surlignée pour chaque utilisateur. La méthode d\'évaluation pour ce test est $a.';
 
 ?>
