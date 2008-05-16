546745c340c9fb9edf287e0c3aad80da99b2fec1
Ralf Hilgenstock <ralf-bonn>
2008-05-16 16:32:22 +0000

    *** empty log message ***

---
+++ de_utf8/debug.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 2.0 dev (Build: 20080511) (2008050700)
+
+
+$string['cannotfindadmin'] = 'Der Nutzer Admin konnte nicht gefunden werden!';
+$string['cannotsetupsite'] = 'Schwerwiegender Fehler: Die Website konnte nicht eingerichtet werden!';
+$string['cannotupdaterelease'] = 'Fehler: Die Release-Version der Datenbank konnte nicht aktualisiert werden!';
+$string['cannotupdateversion'] = 'Die Aktualisierung ist fehlgeschlagen! In der Konfigurationstabelle konnte die Version nicht aktualisiert werden.';
+$string['cannotupgradecapabilities'] = 'Es sind Schwierigkeiten bei der Aktualisierung der Kernregeln für das Rollensystem aufgetreten.';
+$string['configmoodle'] = 'Moodle ist bisher noch nicht konfiguriert. Sie müssen zuerst die Datei config.php bearbeiten.';
+$string['dbnotinsert'] = 'Datenbankfehler - \"$a\" konnte nicht eingefügt werden.';
+$string['dbnotsetup'] = 'Fehler: Die Datenbank wurde nicht erfolgreich angelegt.';
+$string['dbnotsupport'] = 'Fehler: Ihre Datenbank ($a) wird nicht vollständig von Moodle unterstützt. Eventuell ist die install.xml nicht verfügbar. Kontrollieren Sie das Verzeichnis lib/db.';
+$string['dbnotupdate'] = 'Datenbankfehler - \"$a\" konnte nicht aktualisiert werden.';
+$string['doesnotworkwitholdversion'] = 'Dieses Script arbeitet nicht mit dieser alten Version von Moodle.';
+$string['erroroccur'] = 'Ein Fehler ist während des Vorgangs aufgetreten.';
+$string['fixsetting'] = 'Bitte korrigieren Sie die Einstellungen in der Datei config.php: <p>Aktuell vorhanden: </p> <p>$CFG->dirroot = \"$a[0]\";</p> <p>aber das sollte geändert werden in: </p> <p>$CFG->dirroot = \"$a[1]\"</p>';
+$string['nomodules'] = 'Keine Module gefunden!';
+$string['notables'] = 'Keine Tabellen!';
+$string['phpvaroff'] = 'Die PHP Server Variable \'$a[0]\' sollte auf  Off - $a[1] gesetzt werden.';
+$string['phpvaron'] = 'Die PHP Server Variable \'$a[0]\' sollte auf  On - $a[1] gesetzt werden.';
+$string['prefixcannotbeempty'] = 'Der Prefix für die Tabellen \"$a[0]\" der Ziel-DB ($a[1]) kann nicht leer sein.';
+$string['prefixlimit'] = 'Die Länge des Tabellen-Prefix \"$a\" für Oracle Datenbanken darf höchstens 2cc sein.';
+$string['sessionmissing'] = '$a Objekt fehlt für Session';
+$string['siteisnotdefined'] = 'Site ist nicht festgelegt!';
+$string['withoutversion'] = 'Haupt-version.php ist nicht lesbar oder festgelegt.';
+
+?>
