6b9167dfdc860ec760b0d729ff19abf818f6971b
Malinda Siriwardana <malindahs>
2008-05-19 02:48:07 +0000

    New Strings

--- si_utf8/qtype_multianswer.php
+++ si_utf8/qtype_multianswer.php
@@ -1,7 +1,11 @@
 <?PHP // $Id$ 
-      // qtype_multianswer.php - created with Moodle 2.0 dev (2007101508)
+      // qtype_multianswer.php - created with Moodle 2.0 dev (Build: 20080519) (2008051202)
 
 
+$string['correctanswer'] = 'නිවැරදි පිළිතුර';
+$string['correctanswerandfeedback'] = 'නිවැරදි පිළිතුර හා ප්‍රතිචාර';
 $string['decodeverifyquestiontext'] = 'ප්‍රතිකේතීකරනය කර ප්‍රශ්න පාඨය ස්ථි‍ර කර ගන්න';
+$string['questionnotfound'] = '#$a ප්‍රශ්න ‍කොටස වූ ප්‍රශ්නය සොයා ගැනීමට නොහැක';
+$string['questionsmissing'] = 'වලංගු ප්‍රශ්නයක් කිසිවක් නැත, අවම වශයෙන් එක් ප්‍රශ්නයක් වත් සාදන්න';
 
 ?>
