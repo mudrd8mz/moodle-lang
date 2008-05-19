d4e4e429f848210d4072b9d8be140299ff1efb01
Nicolas Martignoni <martignoni>
2008-05-19 20:52:28 +0000

    MDL-14054 New strings

--- fr_utf8/qtype_multianswer.php
+++ fr_utf8/qtype_multianswer.php
@@ -1,5 +1,9 @@
 <?php // $Id$
 
+$string['correctanswer'] = 'Réponse correcte';
+$string['correctanswerandfeedback'] = 'Réponse correcte et feedback';
 $string['decodeverifyquestiontext'] = 'Décoder et vérifier le texte de la question';
+$string['questionnotfound'] = 'Impossible de trouver la partie $a de la question';
+$string['questionsmissing'] = 'Aucune question valide. Veuillez créer au moins une question ';
 
 ?>
