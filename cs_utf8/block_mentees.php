3ce27bec428d232c2cd72b3d765409e996f508ee
David Mudrak <david.mudrak@gmail.com>
2008-05-21 21:19:06 +0000

    The translation of the word "Mentees" has been discussed for a while. Finally, I commit Frantisek Pinc's proposal.

---
+++ cs_utf8/block_mentees.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // block_mentees.php - created with Moodle 1.9 + (Build: 20080507) (2007101509)
+
+
+$string['blockname'] = 'Svěřenci';
+$string['configtitle'] = 'Název bloku';
+$string['leaveblanktohide'] = 'Ponechte prázdné, pokud chcete název skrýt';
+$string['newmenteesblock'] = '(nový blok Svěřenci)';
+
+?>
