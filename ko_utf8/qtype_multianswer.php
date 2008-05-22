6b8270f923ef45484587ba33d1716b3a36d7badd
Lee Chan-young <yogibear270>
2008-05-22 02:33:20 +0000

    All done.

--- ko_utf8/qtype_multianswer.php
+++ ko_utf8/qtype_multianswer.php
@@ -1,7 +1,11 @@
 <?PHP // $Id$ 
-      // qtype_multianswer.php - created with Moodle 2.0 dev (2007101507)
+      // qtype_multianswer.php - created with Moodle 2.0 dev (Build: 20080522) (2008051202)
 
 
+$string['correctanswer'] = '정답';
+$string['correctanswerandfeedback'] = '정답 및 피드백';
 $string['decodeverifyquestiontext'] = '문항 복호 및 검증';
+$string['questionnotfound'] = '문항영역 #$a 에서 문제를 찾을 수 없음';
+$string['questionsmissing'] = '문항 없음. 문제를 만들어야 함';
 
 ?>
