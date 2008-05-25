1ebdb136fbdc3ab56340c66a82037eb6a2daba6e
koen roggemans <koenr>
2008-05-25 14:53:13 +0000

    removing translation for non-standard module questionnaire. Please send the files to the module maintainer

--- uz_utf8/questionnaire.php
+++
@@ -1,9 +0,0 @@
-﻿<?PHP // $Id$ 
-      // questionnaire.php - created with Moodle 1.4 development (2004080300)
-
-
-$string['modulename'] = 'So\'rovnoma';
-$string['modulenameplural'] = 'So\'rovnomalar';
-
-?>
-
