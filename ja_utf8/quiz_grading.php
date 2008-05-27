0bc2201e2326ed2e3a36766c086547715c24e5ac
Mitsuhiro Yoshida <mits>
2008-05-27 07:18:51 +0000

    Translated new strings for quiz manual grading MDL-5241.

--- ja_utf8/quiz_grading.php
+++ ja_utf8/quiz_grading.php
@@ -1,9 +1,17 @@
 <?PHP // $Id$ 
-      // quiz_grading.php - created with Moodle 2.0 dev (Build: 20080331) (2008030700)
+      // quiz_grading.php - created with Moodle 2.0 dev (Build: 20080525) (2008051202)
 
 
 $string['essayonly'] = '以下の問題は手動評定する必要があります。';
+$string['gradeall'] = '$a 件すべての受験を評定する';
+$string['graded'] = '(評定済み)';
+$string['gradeungraded'] = '$a 件すべての未評定受験を評定する';
 $string['grading'] = '手動評定';
+$string['gradingall'] = 'この問題すべての受験件数は $a 件です。';
+$string['gradingattempt'] = '$a->fullname の受験 $a->attempt';
 $string['gradingnotallowed'] = 'あなたには、この小テストの解答を手動評定するパーミッションがありません。';
+$string['gradingungraded'] = '未評定の受験 $a';
+$string['gradinguser'] = '$a の受験';
+$string['questiontitle'] = '問題 $a->number: $a->name (評定済み受験結果 {$a->openspan}$a->gradedattempts{$a->closespan} / $a->totalattempts {$a->openspan}{$a->closespan})';
 
 ?>
