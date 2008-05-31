17a7cf2feda339789986e93dd436377c3bf3259d
Nicolas Martignoni <martignoni>
2008-05-31 09:57:56 +0000

    MDL-12824, more strings translated

--- fr_utf8/quiz_grading.php
+++ fr_utf8/quiz_grading.php
@@ -1,8 +1,14 @@
 <?php // $Id$
 
 $string['essayonly'] = 'Les questions suivantes doivent être notées manuellement';
-$string['grading'] = 'Évaluation manuelle';
 $string['gradeall'] = 'Évaluer toutes les $a tentatives';
+$string['graded'] = '(évalué)';
+$string['gradeungraded'] = 'Évaluer toutes les $a tentatives non évaluées';
+$string['grading'] = 'Évaluation manuelle';
+$string['gradinguser'] = 'Tentatives de $a';
+$string['gradingall'] = 'Toutes les $a tentatives pour cette question.';
+$string['gradingattempt'] = 'Tentative no $a->attempt de $a->fullname.';
 $string['gradingnotallowed'] = 'Vous n\'avez pas l\'autorisation d\'évaluer manuellement les réponses de ce test';
+$string['questiontitle'] = 'Question $a->number&nbsp;: «&nbsp;$a->name&nbsp;» ({$a->openspan}$a->gradedattempts{$a->closespan} / $a->totalattempts tentatives {$a->openspan}graded{$a->closespan}).';
 
 ?>
