f33b62924f7b838485c9e521dc8e0ba5eac2816a
Nicolas Martignoni <martignoni>
2008-05-31 09:47:59 +0000

    MDL-5241, string translated

--- fr_utf8/quiz_grading.php
+++ fr_utf8/quiz_grading.php
@@ -2,6 +2,7 @@
 
 $string['essayonly'] = 'Les questions suivantes doivent être notées manuellement';
 $string['grading'] = 'Évaluation manuelle';
+$string['gradeall'] = 'Évaluer toutes les $a tentatives';
 $string['gradingnotallowed'] = 'Vous n\'avez pas l\'autorisation d\'évaluer manuellement les réponses de ce test';
 
 ?>
