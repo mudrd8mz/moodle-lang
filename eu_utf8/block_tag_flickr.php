d4898a2ee4cb1f3cfdf4a88b678dd158c0d14ca4
Abel Camacho <ueu_ueu>
2008-06-03 10:08:02 +0000

    Santurtziko Udal Euskaltegiak koordinatutako itzulpena

--- eu_utf8/block_tag_flickr.php
+++ eu_utf8/block_tag_flickr.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // block_tag_flickr.php - created with Moodle 1.8.2+ (2007021520)
+      // block_tag_flickr.php - created with Moodle 1.9 + (Build: 20080409) (2007101509)
 
 
 $string['blockname'] = 'Flickr';
@@ -10,6 +10,9 @@ $string['date-taken-asc'] = 'Hartutako data GORA';
 $string['date-taken-desc'] = 'Hartutako data BEHERA';
 $string['defaulttile'] = 'Flickr';
 $string['getfromphotoset'] = 'Albuneko argazkiak lortu ID-arekin';
+$string['includerelatedtags'] = 'Lotutako etiketak sartu query-n';
+$string['interestingness-asc'] = 'Interesak GORA';
+$string['interestingness-desc'] = 'Interesak BEHERA';
 $string['numberofphotos'] = 'Argazki-kopurua';
 $string['relevance'] = 'Garrantzia';
 $string['sortby'] = 'Zeren arabera';
