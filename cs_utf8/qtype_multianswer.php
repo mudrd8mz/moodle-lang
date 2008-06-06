121f74d248c18279166d7bfb19687f02f99cab55
David Mudrak <david.mudrak@gmail.com>
2008-06-06 21:02:25 +0000

    New file in 1.9

---
+++ cs_utf8/qtype_multianswer.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.9.1+ (Build: 20080531) (2007101513)
+
+
+$string['correctanswer'] = 'Správná odpověď';
+$string['correctanswerandfeedback'] = 'Správná odpověď a komentář';
+$string['decodeverifyquestiontext'] = 'Dekódovat a ověřit správnost formátu úlohy';
+$string['questionnotfound'] = 'Není možno nalézt znění úlohy v části č.$a';
+$string['questionsmissing'] = 'Žádné platné úlohy, vytvořte alespoň jednu úlohu';
+
+?>
