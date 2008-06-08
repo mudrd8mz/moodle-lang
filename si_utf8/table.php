cced0206c7f5d47309082761d924eb5229310a7a
Malinda Siriwardana <malindahs>
2008-06-08 16:25:12 +0000

    New File

---
+++ si_utf8/table.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // table.php - created with Moodle 2.0 dev (Build: 20080608) (2008051202)
+
+
+$string['downloadas'] = 'වගු දත්ත ලෙස බාගත කරන්න';
+$string['downloadcsv'] = 'කොමාවකින් වෙන් කරන ලද අගයන් ඇති පාඨ ගොනුව';
+$string['downloadexcel'] = 'මයික්‍රො සොෆ්ට් එක්සෙල් පැතුරුම් පත';
+$string['downloadods'] = 'විවෘත ලිපි පැතුරුම් පත';
+$string['downloadtsv'] = 'ටැබයෙන් වෙන් කරන ලද පාඨ ගොනුව';
+$string['downloadxhtml'] = 'පිටුගත කරන නොලද XHTML ලේඛනය';
+$string['tableexportformats'] = 'වගු දත්ත බාගත කිරීම';
+
+?>
