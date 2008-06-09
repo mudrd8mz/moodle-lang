70272dbbad9a33c08ad27efeba0a40c4545032ea
koen roggemans <koenr>
2008-06-09 06:01:00 +0000

    new strings

--- nl_utf8/choice.php
+++ nl_utf8/choice.php
@@ -1,10 +1,12 @@
 <?PHP // $Id$ 
-      // choice.php - created with Moodle 2.0 dev (2007101506)
+      // choice.php - created with Moodle 2.0 dev (Build: 20080605) (2008051202)
 
 
 $string['addmorechoices'] = 'Voeg meer keuzes toe';
 $string['allowupdate'] = 'Laat toe de keuze aan te passen';
 $string['answered'] = 'Beantwoord';
+$string['cannotsavechoice'] = 'Kon je keuze niet bewaren';
+$string['cannotupdatechoice'] = 'Kon je keuze niet aanpassen door een databank fout';
 $string['choice'] = 'Keuze $a';
 $string['choice:choose'] = 'Bewaar een keuze';
 $string['choice:deleteresponses'] = 'Verwijder antwoorden';
--- nl_utf8/debug.php
+++ nl_utf8/debug.php
@@ -1,12 +1,18 @@
 <?PHP // $Id$ 
-      // debug.php - created with Moodle 2.0 dev (Build: 20080508) (2008050700)
+      // debug.php - created with Moodle 2.0 dev (Build: 20080605) (2008051202)
 
 
+$string['authpluginnotfound'] = 'Authenticatieplugin $a niet gevonden.';
+$string['cannotbenull'] = '$a kan niet nul zijn !!!';
+$string['cannotcreateadminuser'] = 'ERNSTIGE FOUT: kon geen gebruikersrecord voor de beheerder bewaren !!!';
+$string['cannotdowngrade'] = 'Kan niet terugdraaien van from $a[0] to $a[1].';
 $string['cannotfindadmin'] = 'Kon geen beheerder vinden!';
+$string['cannotinitpage'] = 'Kan pagina niet helemaal initialiseren: ongeldige $a[0] id $a[1]';
 $string['cannotsetupsite'] = 'Fatale fout: kon de site niet installeren!';
 $string['cannotupdaterelease'] = 'Fout: kon de versie niet updaten in de databank!';
 $string['cannotupdateversion'] = 'Upgrade mislukt! (Kon de versie niet upgraden in de config tabel)';
 $string['cannotupgradecapabilities'] = 'Problemen met het upgraden van de hoofdmogelijkheden van het rollensysteem';
+$string['cannotupgradedbcustom'] = 'Upgrade van de locale databankaanpassingen mislukt! (kon de versie niet aanpasssen in de config tabel)';
 $string['configmoodle'] = 'Moodle is nog niet geconfigureerd. Je moet eerst config.php bewerken.';
 $string['dbnotinsert'] = 'Databank fout - kan $a niet invoegen';
 $string['dbnotsetup'] = 'Fout. Hoofddatabank niet correct geïnstalleerd';
@@ -15,12 +21,29 @@ $string['dbnotupdate'] = 'Databankfout - kan $a niet updaten';
 $string['doesnotworkwitholdversion'] = 'Dit script werkt niet met deze oude Moodleversie';
 $string['erroroccur'] = 'Er is een fout opgetreden tijdens dit proces.';
 $string['fixsetting'] = 'Verbeter je instellingen in config.php: <p>Je hebt:</p> <p>$CFG->dirroot = \"$a[0]\";</p> <p>maar het moet:</p> <p>$CFG->dirroot = \"$a[1]\" zijn</p>';
-$string['nomodules'] = 'Geen modules gevonden!!';
+$string['invalidarraysize'] = 'Array grootte in params van $a fout';
+$string['invalideventdata'] = 'Foute gebeurtenisgegevens ingestuurd: $a';
+$string['morethanonerecordinfetch'] = 'Meer dan één record gevonden !';
+$string['mustbeoveride'] = 'Abstract $a methode moet overschreven worden.';
+$string['noactivityname'] = 'Pagina-object gehaald van page_generic_activity, maar $this->activityname is niet gedefiniëerd';
+$string['noadminrole'] = 'Er kon geen beheerderrol gevonden worden';
+$string['noblockbase'] = 'Klasse block_base is niet gedefinieerd of bestand niet gevonden voor /blocks/moodleblock.class.php';
+$string['noblocks'] = 'Geen blokken geïnstalleerd!';
+$string['nocaps'] = 'Fout: geen mogelijkheden (capabilities) gedefinieerd!';
+$string['nocate'] = 'Geen categorieën!';
+$string['nomodules'] = 'Geen modules gevonden!';
+$string['nopageclass'] = 'Geïmporteerde $a maar geen paginaklassen gevonden';
+$string['noreports'] = 'Geen toegankelijke rapporten';
 $string['notables'] = 'Geen tabellen!';
 $string['phpvaroff'] = 'De PHP server variable \'$a[0]\' moet Off zijn - $a[1]';
 $string['phpvaron'] = 'De PHP server variable \'$a[0]\' is niet ingeschakeld - $a[1]';
 $string['prefixcannotbeempty'] = 'Tabel prefix \"$a[0]\" kan voor je doeldatabank niet leeg zijn ($a[1])';
 $string['prefixlimit'] = 'Tabel prefix \"$a\": de maximum toegelaten lengte voor Oracle Databanken is 2 tekens.';
+$string['sessionmissing'] = '$a object ontbreekt in de sessie';
+$string['siteisnotdefined'] = 'Site is niet gedefiniëerd';
+$string['sqlrelyonobsoletetable'] = 'Deze SQL steunt op onbestaande tabellen: $a! Je code moet aangepast worden door een software-ontwikkelaar.';
+$string['upgradefail'] = 'Upgrade mislukt! zie: $a';
 $string['withoutversion'] = 'version.php is niet leesbaar of niet opgegeven';
+$string['xmlizeunavailable'] = 'xmlize functies zijn niet beschikbaar';
 
 ?>
