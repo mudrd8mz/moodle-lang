0e33c6614324a079f962c2c82c095d8ebc1315ed
Mitsuhiro Yoshida <mits>
2008-06-10 17:04:06 +0000

    Adding a translated lang file for Quiz Statistics report MDL-14203.

---
+++ ja_utf8/quiz_statistics.php
@@ -0,0 +1,26 @@
+<?PHP // $Id$ 
+      // quiz_statistics.php - created with Moodle 2.0 dev (Build: 20080605) (2008051202)
+
+
+$string['allattemptsavg'] = 'すべての受験評点の平均値';
+$string['attemptsall'] = 'すべての受験';
+$string['attemptsfirst'] = '最初の受験';
+$string['calculatefrom'] = '統計計算:';
+$string['coursename'] = 'コース名';
+$string['errormedian'] = '中央値の取得中にエラーが発生しました。';
+$string['errorpowers'] = '累乗の取得中にエラーが発生しました。';
+$string['firstattemptsavg'] = '最初の受験評点の平均値';
+$string['kurtosis'] = '評点分布の尖度';
+$string['median'] = '評点の中央値';
+$string['noofallattempts'] = '合計受験数';
+$string['nooffirstattempts'] = '最初の受験数';
+$string['quizinformation'] = '小テスト情報';
+$string['quizname'] = '小テスト名';
+$string['quizoverallstatistics'] = '小テスト全体の統計';
+$string['skewness'] = '評点分布の歪み';
+$string['standarddeviation'] = '標準偏差';
+$string['statistics'] = '小テスト統計';
+$string['statsforallattempts'] = 'すべての受験の統計';
+$string['statsforfirstattempts'] = '最初の受験の統計';
+
+?>
