930012143dcdad8168763364c377841f47247158
Nicolas Martignoni <martignoni>
2008-06-11 18:45:57 +0000

    MDL-14203, new file added

---
+++ fr_utf8/quiz_statistics.php
@@ -0,0 +1,27 @@
+<?php // $Id$ 
+
+$string['allattemptsavg'] = 'Note moyenne de toutes les tentatives';
+$string['attemptsall'] = 'Toutes les tentatives';
+$string['attemptsfirst'] = 'Première tentative';
+$string['calculatefrom'] = 'Calculer les statistiques de';
+$string['cic'] = 'Coefficient de cohérence interne';
+$string['coursename'] = 'Nom du cours';
+$string['errormedian'] = 'Erreur de récupération de la médiane';
+$string['errorpowerquestions'] = 'Erreur de récupération des données pour calculer la variance des notes de question';
+$string['errorpowers'] = 'Erreur de récupération des données pour calculer la variance des notes des tests';
+$string['errorratio'] = 'Erreur de rapport';
+$string['firstattemptsavg'] = 'Note moyenne des premières tentatives';
+$string['kurtosis'] = 'Aplatissement de la distribution des notes';
+$string['median'] = 'Note médiane';
+$string['noofallattempts'] = 'Nombre total des tentatives';
+$string['nooffirstattempts'] = 'Nombre des premières tentatives';
+$string['quizinformation'] = 'Information du test';
+$string['quizname'] = 'Nom du test';
+$string['quizoverallstatistics'] = 'Statistiques globales du test';
+$string['skewness'] = 'Dissymétrie de la distribution des notes';
+$string['standarddeviation'] = 'Écart type';
+$string['standarderror'] = 'Erreur standard';
+$string['statistics'] = 'Statistiques du test';
+$string['statsforallattempts'] = 'Statistiques de toutes les tentatives';
+$string['statsforfirstattempts'] = 'Statistiques des premières tentatives';
+?>
