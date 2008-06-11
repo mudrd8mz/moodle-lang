7f7e296e0e52336e67c1478226efc37abaf90974
Nicolas Martignoni <martignoni>
2008-06-11 09:49:25 +0000

    MDL-14187, new file added

---
+++ fr_utf8/table.php
@@ -0,0 +1,11 @@
+<?php  // $Id$
+
+$string['downloadas'] = 'Télécharger les données sous forme';
+$string['downloadexcel'] = 'Feuille de calcul Microsoft Excel';
+$string['downloadods'] = 'Feuille de calcul OpenDocument (ODS)';
+$string['downloadtsv'] = 'Fichier texte avec séparateur tabulation';
+$string['downloadcsv'] = 'Fichier texte avec séparateur virgule (CSV)';
+$string['downloadxhtml'] = 'Fichier XHTML';
+$string['tableexportformats'] = 'Téléchargement des données';
+
+?>
