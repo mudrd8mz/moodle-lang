f9c86ac067eff7510da2c6abb6ecd04699e57a45
Nicolas Martignoni <martignoni>
2008-06-12 14:21:48 +0000

    MDL-12824 one forgotten string translated

--- fr_utf8/quiz_grading.php
+++ fr_utf8/quiz_grading.php
@@ -11,6 +11,7 @@ $string['gradinguser'] = 'Tentatives de $a';
 $string['gradingall'] = 'Toutes les $a tentatives pour cette question.';
 $string['gradingattempt'] = 'Tentative no $a->attempt de $a->fullname.';
 $string['gradingnotallowed'] = 'Vous n\'avez pas l\'autorisation d\'évaluer manuellement les réponses de ce test';
+$string['gradingungraded'] = '$a tentatives non évaluées';
 $string['questiontitle'] = 'Question $a->number&nbsp;: «&nbsp;$a->name&nbsp;» ({$a->openspan}$a->gradedattempts{$a->closespan} / $a->totalattempts tentatives {$a->openspan}graded{$a->closespan}).';
 
 ?>
