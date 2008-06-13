2c378f4e8eef7e803a33f17c64cbc75af531857e
Lee Chan-young <yogibear270>
2008-06-13 00:08:49 +0000

    translate new string

---
+++ ko_utf8/quiz_statistics.php
@@ -0,0 +1,30 @@
+<?PHP // $Id$ 
+      // quiz_statistics.php - created with Moodle 2.0 dev (Build: 20080612) (2008051202)
+
+
+$string['allattemptsavg'] = '모든 시도의 평균점';
+$string['attemptsall'] = '모든 시도';
+$string['attemptsfirst'] = '최초 시도';
+$string['calculatefrom'] = '통계 산출';
+$string['cic'] = '내적 일관성 계수';
+$string['coursename'] = '강좌명';
+$string['errormedian'] = '오류제거 중앙값';
+$string['errorpowerquestions'] = '문항의  분산 계산을 위한 오류 제거 데이터';
+$string['errorpowers'] = '퀴즈의  분산 계산을 위한 오류 제거 데이터';
+$string['errorratio'] = '오류율';
+$string['firstattemptsavg'] = '최초 시도의 평균점';
+$string['kurtosis'] = '점수분포 첨도';
+$string['median'] = '중앙값';
+$string['noofallattempts'] = '시도의 총 수';
+$string['nooffirstattempts'] = '최초 시도의 수';
+$string['quizinformation'] = '퀴즈 소개';
+$string['quizname'] = '퀴즈명';
+$string['quizoverallstatistics'] = '퀴즈 전체 통계';
+$string['skewness'] = '점수분포 비대칭도';
+$string['standarddeviation'] = '표준편차';
+$string['standarderror'] = '표준오차';
+$string['statistics'] = '퀴즈 통계';
+$string['statsforallattempts'] = '총 시도 통계';
+$string['statsforfirstattempts'] = '최초 시도 통계';
+
+?>
