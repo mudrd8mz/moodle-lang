d02f290bac4655e0ebdc8b701f632cbbbfe2316d
Nicolas Martignoni <martignoni>
2008-06-16 17:39:59 +0000

    MDL-14625 more tolerant to missing subquestions

--- fr_utf8/qtype_multianswer.php
+++ fr_utf8/qtype_multianswer.php
@@ -3,7 +3,11 @@
 $string['correctanswer'] = 'Réponse correcte';
 $string['correctanswerandfeedback'] = 'Réponse correcte et feedback';
 $string['decodeverifyquestiontext'] = 'Décoder et vérifier le texte de la question';
+$string['nooptionsforsubquestion'] = 'Impossible d\'obtenir les options de la partie de question no $a->sub (question->id={$a->id})';
+$string['noquestions'] = 'La question Cloze (multianswer) «&nbsp;<strong>$a</strong>&nbsp;» ne contient aucune question';
+$string['qtypenotrecognized'] = 'Le type de question $a est inconnu';
 $string['questionnotfound'] = 'Impossible de trouver la partie $a de la question';
 $string['questionsmissing'] = 'Aucune question valide. Veuillez créer au moins une question ';
+$string['unknownquestiontypeofsubquestion'] = 'Type de question inconnu&nbsp;: $a->type de la partie de question no $a->sub';
 
 ?>
