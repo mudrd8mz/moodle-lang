0283b651a876b62b890f1c0990838d352a202f2e
Nicolas Martignoni <martignoni>
2008-06-16 17:36:13 +0000

    MDL-15123 Using true value limits to grade responses

--- fr_utf8/qtype_calculated.php
+++ fr_utf8/qtype_calculated.php
@@ -21,6 +21,8 @@ $string['nodataset'] = 'rien - ce n\'est pas un caractère joker';
 $string['nosharedwildcard'] = 'Pas de caractère joker dans cette catégorie';
 $string['possiblehdr'] = 'Des caractères joker sont peut-être présents seulement dans le texte de la question';
 $string['tolerance'] = 'Tolérance &plusmn;';
+$string['trueanswerinsidelimits'] = 'Réponse correcte&nbsp;: $a->correct dans les limites de la valeur réelle $a->true';
+$string['trueansweroutsidelimits'] = '<span class=\"error\">Erreur de réponse correcte&nbsp;: $a->correct en dehors des limites de la valeur réelle $a->true</span>';
 $string['updatecategory'] = 'Modifier la catégorie';
 $string['usedinquestion'] = 'Utilisé dans la question';
 $string['youmustenteramultiplierhere'] = 'Vous devez saisir un multiplicateur dans ce champ.';
