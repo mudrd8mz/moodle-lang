2e0d1ac3f005ae3e18d6f327f1d686c3ed2641bb
Nicolas Martignoni <martignoni>
2008-06-19 19:45:13 +0000

    More fixes

--- fr_utf8/langconfig.php
+++ fr_utf8/langconfig.php
@@ -2,17 +2,17 @@
 
 $string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
-$string['decsep'] = ','; // decimal point separator, for some languages it is ','
-$string['firstdayofweek'] = '1';
-$string['listsep'] = ';'; // if decsep is ',', then usually ';' is used instead here; these two can not be the same
-$string['locale'] = 'fr_FR.UTF-8';
+$string['decsep'] = ','; // Decimal separator, for most languages it is '.'. Cannot be the same as 'listsep'
+$string['firstdayofweek'] = '1'; // 0 = Sunday, 1 = Monday, etc.
+$string['listsep'] = ';'; // List separator, for most languages it is a comma ','. Cannot be the same as 'decsep'
+$string['locale'] = 'fr_FR.UTF-8'; // For France. If you live in Switzerland, use fr_CH.UTF-8
 $string['localewin'] = 'French_France.1252';
 $string['localewincharset'] = 'WINDOWS-1252';
 $string['oldcharset'] = 'ISO-8859-1';
 $string['strftimedate'] = '%%d %%B %%Y';
 $string['strftimedateshort'] = '%%d %%B';
 $string['strftimedatetime'] = '%%d %%B %%Y, %%H:%%M';
-$string['strftimedatetimeshort'] = '%%d %%B %%y, %%H:%%M';
+$string['strftimedatetimeshort'] = '%%d %%b %%y, %%H:%%M';
 $string['strftimedaydate'] = '%%A %%d %%B %%Y';
 $string['strftimedaydatetime'] = '%%A %%d %%B %%Y, %%H:%%M';
 $string['strftimedayshort'] = '%%A %%d %%B';
