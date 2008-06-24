414734d7405caf03749acfcdb987a161a7c44b62
Nicolas Martignoni <martignoni>
2008-06-24 21:01:45 +0000

    MDL-14129, more strings

--- fr_utf8/gradeexport_xml.php
+++ fr_utf8/gradeexport_xml.php
@@ -1,5 +1,6 @@
 <?php // $Id$ 
 
+$string['error'] = 'Des erreurs sont survenues';
 $string['modulename'] = 'Fichier XML';
 $string['xml:view'] = 'Exporter les notes en XML';
 $string['xml:publish'] = 'Publier l\'exportation en XML des notes';
