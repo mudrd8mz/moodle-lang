63f20f9d98f9859c3fe1143439481e53d8c1e17d
Nicolas Martignoni <martignoni>
2008-06-25 08:42:23 +0000

    MDL-15268, more strings translated

--- fr_utf8/quiz_statistics.php
+++ fr_utf8/quiz_statistics.php
@@ -1,6 +1,8 @@
 <?php // $Id$ 
 
+$string['allattempts'] = 'toutes les tentatives';
 $string['allattemptsavg'] = 'Note moyenne de toutes les tentatives';
+$string['attempts'] = 'Tentatives';
 $string['attemptsall'] = 'Toutes les tentatives';
 $string['attemptsfirst'] = 'Première tentative';
 $string['calculatefrom'] = 'Calculer les statistiques de';
@@ -10,6 +12,8 @@ $string['errormedian'] = 'Erreur de récupération de la médiane';
 $string['errorpowerquestions'] = 'Erreur de récupération des données pour calculer la variance des notes de question';
 $string['errorpowers'] = 'Erreur de récupération des données pour calculer la variance des notes des tests';
 $string['errorratio'] = 'Erreur de rapport';
+$string['facility'] = 'Indice de facilité';
+$string['firstattempts'] = 'premières tentatives';
 $string['firstattemptsavg'] = 'Note moyenne des premières tentatives';
 $string['intended_weight'] = 'Pondération de question désirée';
 $string['kurtosis'] = 'Aplatissement de la distribution des notes';
@@ -27,7 +31,7 @@ $string['skewness'] = 'Dissymétrie de la distribution des notes';
 $string['standarddeviation'] = 'Écart type';
 $string['standarderror'] = 'Erreur standard';
 $string['statistics'] = 'Statistiques du test';
-$string['statsforallattempts'] = 'Statistiques de toutes les tentatives';
+$string['statsfor'] = 'Statistiques pour $a';
 $string['statsforfirstattempts'] = 'Statistiques des premières tentatives';
 
 ?>
