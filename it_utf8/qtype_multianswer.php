10324c43856bf609af0dba820185ded804d0ee48
Andrea Bicciolo <andreabix>
2008-06-26 11:19:38 +0000

    Aggiornamento traduzione di Claudio Maggi

---
+++ it_utf8/qtype_multianswer.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.9.1 (Build: 20080515) (2007101512)
+
+
+$string['decodeverifyquestiontext'] = 'Decodifica e verifica il testo della domanda';
+
+?>
