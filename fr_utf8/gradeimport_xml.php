c030eaaddf9a2a9574b2b94c2f0737e2e5975c3f
Nicolas Martignoni <martignoni>
2008-06-27 09:57:48 +0000

    Stupid syntax error corrected

--- fr_utf8/gradeimport_xml.php
+++ fr_utf8/gradeimport_xml.php
@@ -6,7 +6,7 @@ $string['errduplicateidnumber'] = 'Erreur - identifiant en double';
 $string['errincorrectgradeidnumber'] = 'Erreur - l\'identifiant «&nbsp;$a&nbsp;» du fichier d\'importation ne correspond à aucun élément d\'évaluation.';
 $string['errincorrectidnumber'] = 'Erreur - identifiant incorrect';
 $string['errincorrectuseridnumber'] = 'Erreur - l\'identifiant «&nbsp;$a&nbsp;» du fichier d\'importation ne correspond à aucun utilisateur.';
-$string['error'] = = 'Des erreurs sont survenues';
+$string['error'] = 'Des erreurs sont survenues';
 $string['fileurl'] = 'URL du fichier distant';
 $string['modulename'] = 'Fichier XML';
 $string['xml:view'] = 'Importer des notes en XML';
