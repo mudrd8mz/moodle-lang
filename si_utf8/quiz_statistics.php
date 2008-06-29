a0fb7e760532093f02d5c4a29e884af596b933a6
Malinda Siriwardana <malindahs>
2008-06-29 14:03:54 +0000

    New File - starting translations again

---
+++ si_utf8/quiz_statistics.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // quiz_statistics.php - created with Moodle 2.0 dev (Build: 20080629) (2008051203)
+
+
+$string['allattempts'] = 'සියලු උත්සාහයන් සඳහා';
+$string['attempts'] = 'උත්සාහයන්';
+$string['attemptsall'] = 'සියලු උත්සාහයන්';
+$string['attemptsfirst'] = 'පළමු උත්සාහය';
+$string['coursename'] = 'පාඨමාලා නාමය';
+$string['errorratio'] = '‍දෝෂ අනුපාතය';
+
+?>
