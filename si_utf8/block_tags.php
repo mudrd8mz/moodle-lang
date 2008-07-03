25ca9c76ae17f6a6b9efde30da49576518d04883
Malinda Siriwardana <malindahs>
2008-07-03 12:56:14 +0000

    New File

---
+++ si_utf8/block_tags.php
@@ -0,0 +1,56 @@
+<?PHP // $Id$ 
+      // block_tags.php - created with Moodle 2.0 dev (Build: 20080703) (2008063002)
+
+
+$string['add'] = 'එක් කරන්න';
+$string['alltags'] = 'සියලු ටැග:';
+$string['alltags1'] = 'සියලු ටැග';
+$string['communitytags'] = 'ප්‍රජා ටැග:';
+$string['communitytags1'] = 'ප්‍රජා ටැග';
+$string['communitytags2'] = 'පාඨමාලා ටැග සෑදූ සියලු පරිශීලකයින් පෙන්වන්න';
+$string['coursetags'] = 'පාඨමාලා ටැග:';
+$string['coursetags1'] = 'පාඨමාලා ටැග';
+$string['coursetags2'] = 'මෙම පාඨමාලාව සඳහා සියලු ටැග පෙන්වන්න';
+$string['edit'] = 'සංස්කරණය...';
+$string['editdeletemytag'] = 'මෙම පාඨමාලාවෙන් ටැග ඉවත් කරන්න:';
+$string['editmytags'] = 'මගේ ටැග - සියලුම ටැග ගත කරන ලද ඔබගේ පාඨමාලා';
+$string['editmytagsfor'] = '$a සඳහා මගේ ටැග සංස්කරණය කරන්න';
+$string['editnopersonaltags'] = 'පෞද්ගලික ටැග කිසිවක් මෙතෙක් සාදා නැත';
+$string['edittags'] = 'මගේ ටැග සංස්කරණය කරන්න...';
+$string['edittagthisunit'] = 'මෙම පාඨමාලාව සඳහා ටැගයක් එක් කරන්න:';
+$string['editthiscoursetags'] = 'මෙම පාඨමාලාව සඳහා මගේ ටැග $a';
+$string['edittitle'] = 'මගේ ටැග';
+$string['entries'] = 'ඇතුලත් කිරීම්';
+$string['entry'] = 'ඇතුලත් කිරීම';
+$string['login'] = 'ලොගින් වන්න';
+$string['more'] = 'තවත්';
+$string['moreorder'] = 'ඇනවුම:';
+$string['moreorderalpha'] = 'අකරාදි අනුව';
+$string['moreorderdate'] = 'සාදන ලද දිනය';
+$string['moreorderpop'] = 'ජනප්‍රියතාව';
+$string['moreshow'] = 'පෙන්වන්න:';
+$string['moreshowalltags'] = 'සියලු ටැග';
+$string['moreshowcommtags'] = 'ප්‍රජා ටැග';
+$string['moreshowcoursetags'] = '\'$a\' සඳහා ටැග';
+$string['moreshowmytags'] = 'මගේ ටැග';
+$string['moreshowofficialtags'] = 'නිල ටැග';
+$string['moretags'] = 'තවත් ටැග ‍පෙරා පෙන්වන්න';
+$string['moretitle'] = 'තවත් ටැග';
+$string['mycoursetag1'] = 'මගේ පාඨමාලා ටැග පෙන්වන්න';
+$string['mycoursetag2'] = 'මගේ පාඨමාලා ටැග';
+$string['mycoursetags'] = 'මගේ පාඨමාලා ටැග:';
+$string['mytags'] = 'මගේ ටැග:';
+$string['mytags1'] = 'මගේ ටැග';
+$string['mytags2'] = 'මගේ පෞද්ගලික ටැග පෙන්වන්න';
+$string['notagsyet'] = 'තවම ටැග නැත';
+$string['officialtags'] = 'නිල පාඨමාලා ටැග:';
+$string['officialtags1'] = 'නිල ටැග';
+$string['officialtags2'] = 'නිල පාඨමාලා ටැග පෙන්වන්න';
+$string['please'] = 'කරුණාකර';
+$string['select'] = 'තෝරන්න:';
+$string['showcoursetags'] = 'පාඨමාලා ටැග පෙන්වන්න';
+$string['tags'] = 'ටැග';
+$string['tagthisunit'] = 'මෙම පාඨමාලාව ටැග ගත කරන්න:';
+$string['tagunits'] = 'ඔබටගේ ප්‍රියතම පාඨමාලා ටැග ගත කිරීමට';
+
+?>
