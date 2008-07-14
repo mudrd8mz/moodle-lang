04ddadb95944114e51e429b89cc22e65a3181d82
Ethem Evlice <ethem>
2008-07-14 15:03:38 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant...

---
+++ tr_utf8/block_tags.php
@@ -0,0 +1,56 @@
+<?PHP // $Id$ 
+      // block_tags.php - created with Moodle 2.0 dev (Build: 20080714) (2008070701)
+
+
+$string['add'] = 'Ekle';
+$string['alltags'] = 'Tüm etiketler:';
+$string['alltags1'] = 'tüm etiketler';
+$string['alltags2'] = 'Tüm etiketleri göster (kullanıcı profili, bloglar ve kurs etiketleri)';
+$string['arrowtitle'] = 'Önerilen metni girmek için burayı tıklayın (gri harfler).';
+$string['communitytags'] = 'Topluluk etiketleri:';
+$string['communitytags1'] = 'topluluk etiketleri';
+$string['coursetags'] = 'Ders etiketleri:';
+$string['coursetags1'] = 'ders etiketleri';
+$string['coursetags2'] = 'Bu ders için etiketleri göster';
+$string['edit'] = 'düzenle...';
+$string['editdeletemytag'] = 'Etiketi bu dersten sil:';
+$string['editmytagsfor'] = '$a için etiketlerimi düzenle';
+$string['editnopersonaltags'] = 'Henüz kişisel bir etiket oluşturulmamış.';
+$string['edittags'] = 'Etiketlerimi düzenle...';
+$string['edittagthisunit'] = 'Bu derse etiket ekle';
+$string['editthiscoursetags'] = 'Bu ders için etiketlerim: $a';
+$string['edittitle'] = 'Etiketlerim';
+$string['entries'] = 'girdiler';
+$string['entry'] = 'girdi';
+$string['jserror1'] = 'Etiketler en az 2, en fazla 50 karakter olmalıdır. Lütfen etiketi yeniden düzenleyin.';
+$string['login'] = 'giriş';
+$string['more'] = 'dahası...';
+$string['moreorder'] = 'Sırala:';
+$string['moreorderalpha'] = 'Alfabetik';
+$string['moreorderdate'] = 'Oluşturulma tarihi';
+$string['moreorderpop'] = 'Popülerlik';
+$string['moreshow'] = 'Göster';
+$string['moreshowalltags'] = 'Tüm etiketler';
+$string['moreshowcommtags'] = 'Topluluk etiketleri';
+$string['moreshowcoursetags'] = '\'$a\' için etiketler';
+$string['moreshowmytags'] = 'Etiketlerim';
+$string['moreshowofficialtags'] = 'Kurumsal etiketler';
+$string['moretitle'] = 'Daha fazla etiket';
+$string['mycoursetag1'] = 'Ders etiketlerimi göster';
+$string['mycoursetag2'] = 'ders etiketlerim';
+$string['mycoursetags'] = 'Ders etiketlerim:';
+$string['mytags'] = 'Etiketlerim:';
+$string['mytags1'] = 'etiketlerim';
+$string['mytags2'] = 'Kişisel etiketlerimi göster';
+$string['notagsyet'] = 'Henüz etiket yok';
+$string['officialtags'] = 'Kurumsal ders etiketleri';
+$string['officialtags1'] = 'kurumsal etiketler';
+$string['officialtags2'] = 'Kurumsal kurs etiketlerini göster';
+$string['please'] = 'Lütfen';
+$string['select'] = 'Seçin...';
+$string['showcoursetags'] = 'Ders etiketlerini göster';
+$string['tags'] = 'etiketler';
+$string['tagthisunit'] = 'Bu dersi etiketle:';
+$string['tagunits'] = 'beğenilen dersleri etiketlemek için';
+
+?>
