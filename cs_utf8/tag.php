e0ba4d778856a13ccaa67f7825d86318f0a9f7b2
Daniel Mikšík <miksik>
2008-07-15 21:35:38 +0000

    Translation added

---
+++ cs_utf8/tag.php
@@ -0,0 +1,56 @@
+<?PHP // $Id$ 
+      // tag.php - created with Moodle 1.9.2 (Build: 20080715) (2007101520)
+
+
+$string['addedotag'] = 'Přidána oficiální nálepka &bdquo;$a&ldquo;';
+$string['addotags'] = 'Přidat oficiální nálepky';
+$string['addtagtomyinterests'] = 'Přidat nálepku &bdquo;$a&ldquo; k mým nálepkám';
+$string['blocktagstitle'] = 'Nálepky';
+$string['changename'] = 'Změnit název nálepky';
+$string['changetype'] = 'Změnit typ nálepky';
+$string['count'] = 'Počet použití';
+$string['delete'] = 'Odstranit';
+$string['deleted'] = 'odstraněno';
+$string['description'] = 'Popis';
+$string['edittag'] = 'Upravit nálepku';
+$string['flag'] = 'Označena jako nevhodná';
+$string['flagasinappropriate'] = 'Označit nálepku jako nevhodnou';
+$string['helprelatedtags'] = 'Související nálepky oddělené čárkou';
+$string['id'] = 'id';
+$string['managetags'] = 'Spravovat nálepky';
+$string['name'] = 'Název nálepky';
+$string['namesalreadybeeingused'] = 'Názvy nálepek se již používají';
+$string['newname'] = 'Nový název nálepky';
+$string['noresultsfor'] = 'Hledání výrazu &bdquo;$a&ldquo; nepřineslo žádné výsledky';
+$string['officialtag'] = 'Oficiální nálepka';
+$string['otags'] = 'Oficiální nálepky';
+$string['owner'] = 'Vlastník';
+$string['ptags'] = 'Uživatelské nálepky (oddělené čárkou)';
+$string['relatedblogs'] = 'Poslední zápisy do blogu označené touto nálepkou';
+$string['relatedtags'] = 'Související nálepky';
+$string['removetagfrommyinterests'] = 'Odstranit nálepku &bdquo;$a&ldquo; z mých nálepek';
+$string['reset'] = 'příznak &bdquo;nevhodná&ldquo; zrušen';
+$string['resetflag'] = 'Zrušit příznak nevhodnosti';
+$string['responsiblewillbenotified'] = 'Odpovídajícímu uživateli bude zasláno oznámení';
+$string['search'] = 'Vyhledat';
+$string['searchresultsfor'] = 'Výsledky vyhledávání výrazu &bdquo;$a&ldquo;';
+$string['searchtags'] = 'Prohledat nálepky';
+$string['seeallblogs'] = 'Prohlédnout všechny blogy s touto nálepkou';
+$string['select'] = 'Vybrat';
+$string['tag'] = 'Nálepka';
+$string['tagdescription'] = 'Popis nálepky';
+$string['tags'] = 'Nálepky';
+$string['tagsaredisabled'] = 'Nálepky';
+$string['tagtype'] = 'Typ nálepky';
+$string['tagtype_default'] = 'Výchozí';
+$string['tagtype_official'] = 'Oficiální';
+$string['thingstaggedwith'] = 'Nálepkou &bdquo;$a->name&ldquo; je označeno $a->count položek.';
+$string['thistaghasnodesc'] = 'Nálepka nemá popis';
+$string['timemodified'] = 'Čas od poslední úpravy';
+$string['typechanged'] = 'byl změněn typ nálepky';
+$string['updated'] = 'aktualizováno';
+$string['updatetag'] = 'Aktualizovat';
+$string['userstaggedwith'] = 'Uživatelé používající nálepku &bdquo;$a&ldquo;';
+$string['withselectedtags'] = 'S vybranými nálepkami...';
+
+?>
