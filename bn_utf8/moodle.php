0556af77af994f296bafc3c3cef741b4ca98d079
koen roggemans <koenr>
2008-07-15 12:37:31 +0000

    missing file added

---
+++ bn_utf8/moodle.php
@@ -0,0 +1,65 @@
+<?PHP // $Id$ 
+      // moodle.php - created with Moodle 1.9 Beta 4 (2007101508.05)
+
+
+$string['abouttobeinstalled'] = 'ইনস্টল হতে চলেছে';
+$string['action'] = 'এ্যাকশন';
+$string['actions'] = 'এ্যাকশনস্';
+$string['active'] = 'একটিভ্';
+$string['activeusers'] = 'একটিভ্ ব্যবহারকারী';
+$string['activities'] = 'একটিভিটিজ';
+$string['activity'] = 'একটিভিটি';
+$string['activityclipboard'] = 'এই একটিভিটি টি সরানো হছেঃ<b>$a</b>';
+$string['activityiscurrentlyhidden'] = 'দুঃখিত, এই একটিভিটিটি বর্তমানে গোপন করা আছে';
+$string['activitymodule'] = 'একটিভিটি মডিউল';
+$string['activityreport'] = 'একটিভিটি রিপোর্ট';
+$string['activityreports'] = 'একটিভিটি রিপোর্টস';
+$string['activityselect'] = 'এই একটিভিটিটি অন্য কোথাও সরানোর জন্য নি্র্বাচিত কর';
+$string['activitysince'] = 'একটিভিটি  শুরু হয়েছিল $a';
+$string['activityweighted'] = 'ব্যবহারকারী প্রতি একটিভিটি';
+$string['add'] = 'যোগ';
+$string['addactivity'] = 'একটি একটিভিটি যোগ কর...';
+$string['addadmin'] = 'অ্যাডমিন যোগ কর';
+$string['addcreator'] = 'কোর্স  ক্রিয়েটর যোগ কর';
+$string['added'] = 'যোগ করা হয়েছে $a';
+$string['addedrecip'] = 'নতুন গ্রাহক $a  যোগ করা হয়েছে';
+$string['addedrecips'] = 'নতুন গ্রাহকগন $a  যোগ করা হয়েছে';
+$string['addedtogroup'] = 'গ্রুপে যোগ করা হয়েছে \"$a\"';
+$string['addedtogroupnot'] = 'গ্রুপে যোগ করা হয়নি \"$a\"';
+$string['addedtogroupnotenrolled'] = 'গ্রুপে যোগ করা হয়নি \"$a\", কারণ কোর্স তে এনরোল্ড না';
+$string['addinganew'] = 'নতুন $a যোগ করা হচ্ছে';
+$string['addinganewto'] = 'নতুন $a->what থেকে $a->toযোগ করা হচ্ছে';
+$string['addingdatatoexisting'] = 'বিদ্যমানে ডেটা যোগ করা হচ্ছে';
+$string['addnewcategory'] = 'নতুন ক্যাটাগরী যোগ কর';
+$string['addnewcourse'] = 'নতুন কোর্স  যোগ কর';
+$string['addnewuser'] = 'নতুন ব্যবহারকারী যোগ কর';
+$string['addnousersrecip'] = 'গ্রাহক তালিকাতে ব্যবহারকারী যোগ কর যারা এখনও $a লিস্টে নেই';
+$string['addresource'] = 'রিসোর্স যোগ কর...';
+$string['address'] = 'ঠিকানা';
+$string['addstudent'] = 'ছাত্র যোগ কর';
+$string['addsubcategory'] = 'একটি  উপ - ক্যাটাগরী যোগ কর';
+$string['addteacher'] = 'শিক্ষক যোগ কর';
+$string['admin'] = 'অ্যাডমিন';
+$string['adminbookmarks'] = 'অ্যাডমিন বুকমার্ক';
+$string['adminhelpaddnewuser'] = 'নতুন ব্যবহারকারী অ্যাকাউন্ট ম্যানুয়ালি তৈরি করতে হলে';
+$string['adminhelpassignadmins'] = 'অ্যাডমিন সাইটে যা কিছু ইচ্ছা করতে পারেন এবং  যেখানে ইচ্ছা যেতে পারেন';
+$string['adminhelpassigncreators'] = 'ক্রিয়েটররা নতুন কোর্স তৈরি করতে পারেন এবং তাতে শিক্ষা প্রদান করতে পারেন';
+$string['adminhelpassignsiteroles'] = 'নির্দিষ্ট ব্যবহারকারীকে নির্দিষ্ট সাইট রোল প্রদান কর';
+$string['adminhelpassignstudents'] = 'একটি কোর্স এ যাও এবং অ্যাডমিন মেনু থেকে ছাত্র যোগ কর';
+$string['adminhelpassignteachers'] = 'একটি কোর্স খুঁজ,তারপর শিক্ষক যোগ করতে আইকন ব্যবহার কর';
+$string['adminhelpauthentication'] = 'আপনি অভ্যন্তরীণ ব্যবহারকারী অ্যাকাউন্ট অথবা বাহ্যিক ডেটাবেস ব্যবহার করতে পারেন';
+$string['adminhelpbackup'] = 'অটোমেটেড ব্যাকআপ এবং তাদের নির্ধারিত সময় কনফিগার করুন';
+$string['adminhelpconfiguration'] = 'সাইটটি কেমন দেখাবে এবং কাজ করবে তা কনফিগার করুন';
+$string['adminhelpconfigvariables'] = 'সাইটের সাধারণ অপারেশন প্রভাবিত করে এমন ভেরিয়েবল কনফিগার করুন';
+$string['adminhelpcourses'] = 'কোর্সগুলো এবং ক্যাটাগরীগুলোতে সংজ্ঞায়িত কর এবং তাদেরকে মানুষ বরাদ্দ কর, অমীমাংসিত কোর্সগুলো সম্পাদনা কর';
+$string['adminhelpeditorsettings'] = 'এইচটিএমএল সম্পাদকের জন্য মৌলিক মানসমূহ সংজ্ঞায়িত করুন';
+$string['adminhelpedituser'] = 'ব্যবহারকারী অ্যাকাউন্টের তালিকা ব্রাউজ করুন এবং তাদের যেকোনটি সম্পাদনা করুন';
+$string['adminhelpenrolments'] = 'এনরোলমেন্টস নিয়ন্ত্রণ করতে অভ্যন্তরীণ অথবা বাহ্যিক উপায় বেছে নিন';
+$string['adminhelpenvironment'] = 'আপনার সার্ভার বর্তমান এবং ভবিষ্যৎ ইনস্টলেশনের জন্য কিভাবে স্যুট করে তা  পরীক্ষা করুন';
+$string['adminhelpfailurelogs'] = 'ব্যর্থ লগইনস্ এর কার্যবিবরণী ব্রাউজ করুন';
+$string['adminhelphealthcenter'] = 'সাইট সমস্যার স্বয়ংক্রিয় সনাক্তকরণ';
+$string['adminhelplanguage'] = 'বর্তমান ভাষা প্যাক পরীক্ষা এবং সম্পাদন করার জন্য';
+$string['adminhelplogs'] = 'এই সাইটটি সমস্ত ক্রিয়াকর্মের কার্যবিবরণী ব্রাউজ করুন';
+$string['adminhelpmanageblocks'] = 'ইনস্টলকৃত ব্লকগুলো ম্যানেজ করা এবং তাদের সেটিংস';
+
+?>
