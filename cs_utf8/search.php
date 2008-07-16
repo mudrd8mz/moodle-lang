5f9d20c4cd59acd8a3164e0681832acfe5f46201
Daniel Mikšík <miksik>
2008-07-16 20:44:06 +0000

    Translation added

---
+++ cs_utf8/search.php
@@ -0,0 +1,60 @@
+<?PHP // $Id$ 
+      // search.php - created with Moodle 1.9.2 (Build: 20080715) (2007101520)
+
+
+$string['advancedsearch'] = 'Pokročilé vyhledávání';
+$string['all'] = 'Všechny';
+$string['author'] = 'Autor';
+$string['authorname'] = 'Jméno autora';
+$string['back'] = 'Předchozí';
+$string['beadmin'] = 'Tuto stránku mohou používat pouze správci.';
+$string['checkdb'] = 'Zkontrolujte databázi';
+$string['checkdbadvice'] = 'Ověřte funkčnost databáze.';
+$string['checkdir'] = 'Zkontrolujte adresář';
+$string['checkdiradvice'] = 'Ověřte, zda je datový adresář vytvořen a lze do něj zapisovat.';
+$string['commenton'] = 'Komentář k položce';
+$string['createanindex'] = 'vytvořte index';
+$string['createdon'] = 'Datum a čas vytvoření indexu';
+$string['database'] = 'Databáze';
+$string['databasestate'] = 'Stav indexace databáze';
+$string['datadirectory'] = 'Datový adresář';
+$string['deletionsinindex'] = 'Smazaných položek v indexu';
+$string['doctype'] = 'Doctype';
+$string['documents'] = 'dokumentech';
+$string['documentsfor'] = 'Dokumentů pro';
+$string['documentsindatabase'] = 'Dokumentů v databázi';
+$string['documentsinindex'] = 'Dokumentů v indexu';
+$string['duration'] = 'Trvání';
+$string['emptydatabaseerror'] = 'Databáze nebyla vytvořena nebo neobsahuje žádné údaje indexu.';
+$string['enteryoursearchquery'] = 'Zadejte svůj dotaz';
+$string['errors'] = 'Chyby';
+$string['filesinindexdirectory'] = 'Počet souborů v adresáři';
+$string['globalsearchdisabled'] = 'Globální vyhledávání není aktivováno';
+$string['invalidindexerror'] = 'Datový adresář buď neobsahuje platný index nebo je prázdný.';
+$string['ittook'] = 'Vyhledávání trvalo&nbsp;';
+$string['next'] = 'Další';
+$string['noindexmessage'] = 'Správce: Nepodařilo se najít index pro vyhledávání. Prosím&nbsp;';
+$string['normalsearch'] = 'Základní vyhledávání';
+$string['openedon'] = 'zahájeno';
+$string['resultsreturnedfor'] = 'výsledků pro dotaz';
+$string['runindexer'] = 'Spustit (skutečné) indexování';
+$string['runindexertest'] = 'Spustit test indexování';
+$string['score'] = 'Skóre';
+$string['search'] = 'Hledej';
+$string['searching'] = 'Vyhledávám v ...';
+$string['seconds'] = 'sekund';
+$string['solutions'] = 'Nástroje';
+$string['statistics'] = 'Statistika';
+$string['thesewordshelpimproverank'] = 'Tato slova zvyšují pozici výsledku';
+$string['thesewordsmustappear'] = 'Dokument musí obsahovat tato slova';
+$string['thesewordsmustnotappear'] = 'Dokument nesmí obsahovat tato slova';
+$string['title'] = 'Title';
+$string['totalsize'] = 'Celková velikost';
+$string['type'] = 'Typ';
+$string['uncompleteindexingerror'] = 'Indexování nebylo dokončeno úspěšně, prosím spusťte je znovu.';
+$string['versiontoolow'] = 'Globální vyhledávání vyžaduje PHP 5.0.0 nebo novější.';
+$string['whichmodulestosearch?'] = 'Které moduly prohledávat';
+$string['wordsintitle'] = 'Slova v názvu';
+$string['tofetchtheseresults'] = '';
+
+?>
