c17fee70328c8ba2ba819ad5835788e554dc35c4
Mitsuhiro Yoshida <mits>
2008-07-20 16:12:16 +0000

    MDL-15631 - Translated a new string for quiz responses.

--- ja_utf8/quiz_responses.php
+++ ja_utf8/quiz_responses.php
@@ -1,10 +1,11 @@
 <?PHP // $Id$ 
-      // quiz_responses.php - created with Moodle 2.0 dev (Build: 20080620) (2008051203)
+      // quiz_responses.php - created with Moodle 2.0 dev (Build: 20080711) (2008070701)
 
 
 $string['pagesize'] = '1ページあたりの受験数:';
 $string['reportresponses'] = '解答';
 $string['responses'] = '解答';
+$string['responsesdownload'] = '解答ダウンロード';
 $string['responsesoptions'] = '解答オプション';
 $string['responsestitle'] = '解答詳細';
 
