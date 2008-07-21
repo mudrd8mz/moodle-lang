ccb816005085b6407afdc2f0dde07b096bea9235
Lee Chan-young <yogibear270>
2008-07-21 04:59:13 +0000

    change some terms and translate added strings.

--- ko_utf8/quiz_responses.php
+++ ko_utf8/quiz_responses.php
@@ -1,11 +1,12 @@
 <?PHP // $Id$ 
-      // quiz_responses.php - created with Moodle 2.0 dev (Build: 20080507) (2008050200)
+      // quiz_responses.php - created with Moodle 2.0 dev (Build: 20080721) (2008070701)
 
 
-$string['pagesize'] = '화면당 시도 수:';
-$string['reportresponses'] = '상세한 반응';
-$string['responses'] = '상세한 반응';
-$string['responsesoptions'] = '반응 옵션';
-$string['responsestitle'] = '상세한 반응';
+$string['pagesize'] = '화면당 시도 수';
+$string['reportresponses'] = '응답 결과';
+$string['responses'] = '응답';
+$string['responsesdownload'] = '응답 내려받기';
+$string['responsesoptions'] = '응답 옵션';
+$string['responsestitle'] = '응답 명세';
 
 ?>
