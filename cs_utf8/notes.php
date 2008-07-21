0340d81e6f94ebb08fe232b8e13027fdb92fed99
David Mudrak <david.mudrak@gmail.com>
2008-07-21 21:51:36 +0000

    New file notes.php added

---
+++ cs_utf8/notes.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 1.9.2+ (Build: 20080720) (2007101520)
+
+
+$string['addnewnote'] = 'Přidat novou poznámku';
+$string['addnewnoteselect'] = 'Vyberte uživatele, o nichž si chcete zapsat poznámku';
+$string['bynameondate'] = 'autor $a->name - $a->date';
+$string['content'] = 'Obsah';
+$string['course'] = 'kurz';
+$string['coursenotes'] = 'Poznámky kurzu';
+$string['created'] = 'vytvořeno';
+$string['deleteconfirm'] = 'Odstranit tuto poznámku?';
+$string['deletenotes'] = 'Odstranit všechny poznámky';
+$string['editnote'] = 'Upravit poznámku';
+$string['groupaddnewnote'] = 'Přidat společnou poznámku';
+$string['nocontent'] = 'Obsah poznámky nemůže zůstat prázdný';
+$string['nonotes'] = 'Žádné poznámky tohoto typu';
+$string['note'] = 'Poznámka';
+$string['notes'] = 'Poznámky';
+$string['notesnotvisible'] = 'Nemáte oprávnění vidět tyto poznámky.';
+$string['nouser'] = 'Musíte vybrat uživatele';
+$string['personal'] = 'osobní';
+$string['personalnotes'] = 'Osobní poznámky';
+$string['publishstate'] = 'Stav';
+$string['site'] = 'stránky';
+$string['sitenotes'] = 'Poznámky stránek';
+$string['unknown'] = 'neznámý';
+
+?>
