118914673a2a7b5b76cd7c3256f8ea1eea3d91c4
David Mudrak <david.mudrak@gmail.com>
2008-07-24 09:57:05 +0000

    "User key" changed to "Pristupovy klic" according to the help file

--- cs_utf8/userkey.php
+++ cs_utf8/userkey.php
@@ -2,16 +2,16 @@
       // userkey.php - created with Moodle 1.9.2+ (Build: 20080724) (2007101520.01)
 
 
-$string['createnewkey'] = 'Vytvořit nový uživatelský klíč';
-$string['createuserkey'] = 'Vytvořit uživatelský klíč';
-$string['deletekeyconfirm'] = 'Opravdu chcete odstranit tento uživatelský klíč?';
-$string['edituserkey'] = 'Upravit uživatelský klíč';
+$string['createnewkey'] = 'Vytvořit nový přístupový klíč';
+$string['createuserkey'] = 'Vytvořit přístupový klíč';
+$string['deletekeyconfirm'] = 'Opravdu chcete odstranit tento přístupový klíč?';
+$string['edituserkey'] = 'Upravit přístupový klíč';
 $string['keyiprestriction'] = 'Omezení IP adresy klíče';
 $string['keymanager'] = 'Správce klíčů';
 $string['keyvaliduntil'] = 'Klíč platný do';
 $string['keyvalue'] = 'Hodnota klíče';
-$string['newuserkey'] = 'Nový uživatelský klíč';
-$string['userkey'] = 'Uživatelský klíč';
-$string['userkeys'] = 'Uživatelské klíče';
+$string['newuserkey'] = 'Nový přístupový klíč';
+$string['userkey'] = 'Přístupový klíč';
+$string['userkeys'] = 'Přístupové klíče';
 
 ?>
