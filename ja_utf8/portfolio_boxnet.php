86e1b5f002959c2478b2dca5cae1822e3d090c1f
Mitsuhiro Yoshida <mits>
2008-07-28 03:52:52 +0000

    Translated new strings for portfolio.

--- ja_utf8/portfolio_boxnet.php
+++ ja_utf8/portfolio_boxnet.php
@@ -1,10 +1,17 @@
 <?PHP // $Id$ 
-      // portfolio_boxnet.php - created with Moodle 2.0 dev (Build: 20080725) (2008072500)
+      // portfolio_boxnet.php - created with Moodle 2.0 dev (Build: 20080726) (2008072600)
 
 
 $string['apikey'] = 'APIキー (Box.netより取得してください)';
 $string['err_noapikey'] = 'このプラグインに設定されたAPIキーがありません。あなたは、http://enabled.box.netからAPIキーを取得することができます。';
+$string['existingfolder'] = 'ファイルを保存する既存のフォルダ';
 $string['folderclash'] = 'あなたが作成を依頼したフォルダは、すでに登録されています!';
+$string['foldercreatefailed'] = 'あなたのターゲットフォルダのbox.netへの作成に失敗しました。';
+$string['folderlistfailed'] = 'box.netからフォルダ一覧の取得に失敗しました。';
+$string['newfolder'] = 'ファイルを保存する新しいフォルダ';
+$string['noauthtoken'] = 'このセッションで使用する認証トークンを取得できませんでした。';
+$string['notarget'] = 'あなたは、ファイルをアップロードする既存のフォルダまたは新しいフォルダを選択する必要があります。';
+$string['noticket'] = '認証を開始するためのチケットをbox.netから取得できませんでした。';
 $string['password'] = 'あなたのbox.netパスワード (保存されません)';
 $string['sharedfolder'] = 'Shared';
 $string['targetfolder'] = 'ターゲットフォルダ';
