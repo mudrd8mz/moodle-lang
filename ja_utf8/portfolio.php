c980209385aeff6e6ac80886faac7e05b8037f10
Mitsuhiro Yoshida <mits>
2008-07-29 07:03:54 +0000

    Translated new strings for portfolio.

--- ja_utf8/portfolio.php
+++ ja_utf8/portfolio.php
@@ -1,24 +1,52 @@
 <?PHP // $Id$ 
-      // portfolio.php - created with Moodle 2.0 dev (Build: 20080725) (2008072500)
+      // portfolio.php - created with Moodle 2.0 dev (Build: 20080728) (2008072800)
 
 
 $string['addnewportfolio'] = '新しいポートフォリオを追加する';
 $string['addtoportfolio'] = 'ポートフォリオに追加する';
+$string['alreadyexporting'] = 'このセッションでは、すでにアクティブなポートフォリオエクスポートがあります。このエクスポートを最初に完了してください。';
 $string['availableformats'] = '利用可能なエクスポートフォーマット';
+$string['callercouldnotpackage'] = 'あなたのエクスポートするデータをパックアップできませんでした。';
+$string['configexport'] = 'エクスポートデータの設定';
 $string['configplugin'] = 'ポートフォリオプラグインの設定';
 $string['configure'] = '設定';
+$string['confirmexport'] = '次のエクスポートを確認してください。';
+$string['confirmsummary'] = 'あなたのエクスポートの要約';
+$string['continuetoportfolio'] = 'あなたのポートフォリオを続ける';
+$string['err_uniquename'] = 'ポートフォリオ名は、(プラグインごとに) ユニークにしてください。';
 $string['exportcomplete'] = 'ポートフォリオのエクスポートが完了しました!';
+$string['exporting'] = 'ポートフォリオへのエクスポート';
+$string['failedtosendpackage'] = 'あなたのデータを選択されたポートフォリオシステムに送信できませんでした!';
 $string['format_file'] = 'ファイル';
 $string['format_mbkp'] = 'Moodleバックアップ';
+$string['hidden'] = '非表示';
 $string['instancedeleted'] = 'ポートフォリオが正常に削除されました。';
+$string['instanceismisconfigured'] = 'ポートフォリオインスタンスの設定が正しくありません、スキップ中。エラー内容は: $a';
 $string['instancenotdelete'] = 'ポートフォリオの削除に失敗しました。';
 $string['instancenotsaved'] = 'ポートフォリオの保存に失敗しました。';
 $string['instancesaved'] = 'ポートフォリオが正常に保存されました。';
+$string['invalidformat'] = '無効なフォーマットでエクスポートされています: $a';
 $string['manageportfolios'] = 'ポートフォリオの管理';
+$string['manageyourportfolios'] = 'あなたのポートフォリオの管理';
+$string['noavailableplugins'] = '申し訳ございません、あなたがエクスポートできるポートフォリオはありません。';
+$string['nocallbackfile'] = 'あなたがエクスポートを試みているモジュールが壊れているようです - 必要なファイル ($a) を見つけることができませんでした。';
+$string['nopermissions'] = '申し訳ございません、このエリアからファイルをエクスポートするため必要なパーミッションが、あなたにはありません。';
+$string['notexportable'] = '申し訳ございません、あなたがエクスポートを試みているコンテンツタイプは、エクスポートできません。';
+$string['nouploaddirectory'] = 'あなたのデータをまとめるための一時ディレクトリを作成できませんでした。';
 $string['plugin'] = 'ポートフォリオプラグイン';
+$string['plugincouldnotpackage'] = 'あなたのエクスポートするデータをパックアップできませんでした。';
 $string['portfolio'] = 'ポートフォリオ';
 $string['portfolios'] = 'ポートフォリオ';
+$string['returntowhereyouwere'] = '戻る';
 $string['save'] = '保存';
+$string['selectedformat'] = '選択されたエクスポートフォーマット';
+$string['selectplugin'] = 'エクスポートするポートフォリオプラグインを選択してください。';
+$string['someinstancesdisabled'] = '設定が正しくない、または他の要因により、いくつかの設定済みプラグインインスタンスは無効にされました。';
+$string['somepluginsdisabled'] = '設定が正しくない、または他の要因により、すべてのプラグインインスタンスは無効にされました。';
 $string['sure'] = '本当に「 $a 」を削除してもよろしいですか? 元に戻すことはできません。';
+$string['wanttowait_high'] = 'この転送が完了するまで、待つことはお勧めできません。あなたが何をしているか理解している場合、待つこともできます。';
+$string['wanttowait_moderate'] = 'この転送を待ちますか? 転送完了まで数分かかります。';
+$string['dontwait'] = '待たない'; // TBD
+$string['wait'] = '待つ'; // TBD
 
 ?>
