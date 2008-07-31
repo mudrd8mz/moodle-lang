dddf0980cca2f46f73a57575ec32b639fee242d4
koen roggemans <koenr>
2008-07-31 20:36:52 +0000

    new strings

---
+++ nl_utf8/portfolio.php
@@ -0,0 +1,41 @@
+<?PHP // $Id$ 
+      // portfolio.php - created with Moodle 2.0 dev (Build: 20080731) (2008073104)
+
+
+$string['addnewportfolio'] = 'Voeg een nieuw portfolio toe';
+$string['addtoportfolio'] = 'Voeg toe aan portfolio';
+$string['alreadyexporting'] = 'Je hebt al een actieve portfolioexport in deze sessie. Werk deze eerst af';
+$string['availableformats'] = 'Beschikbare exportformats';
+$string['callercouldnotpackage'] = 'Inpakken van je gegevens voor export mislukt';
+$string['cannotsetvisible'] = 'Kan dit niet zichtbaar zetten - de plugin is volledig uitgeschakeld door een foute configuratie';
+$string['configexport'] = 'configureer geëxporteerde gegevens';
+$string['configplugin'] = 'Configureer portfolioplugin';
+$string['configure'] = 'Configureer';
+$string['confirmexport'] = 'Bevestig deze export';
+$string['confirmsummary'] = 'Samenvatting van je export';
+$string['continuetoportfolio'] = 'Ga verder naar je portfolio';
+$string['deleteportfolio'] = 'Verwijder portfolio';
+$string['disabled'] = 'Het exporteren van portfolio\'s is niet ingeschakeld voor deze site';
+$string['dontwait'] = 'Wacht niet';
+$string['enabled'] = 'Portfolio\'s inschakelen';
+$string['enableddesc'] = 'Hiermee kunnen beheerders systemen op afstand configureren, zodat gebruikers er inhoud naartoe kunnen exporteren';
+$string['err_uniquename'] = 'Portfolionaam moet uniek zijn (per plugin)';
+$string['exportcomplete'] = 'Portfolioexport klaar!';
+$string['exportedpreviously'] = 'Vorige exports';
+$string['exporting'] = 'Exporteren naar portfolio';
+$string['failedtosendpackage'] = 'Gegevens sturen naar het geselecteerde portfoliosysteem mislukt!';
+$string['format_file'] = 'Bestand';
+$string['format_mbkp'] = 'Moodle backup';
+$string['hidden'] = 'Verborgen';
+$string['instancedeleted'] = 'Portfolio met succes verwijderd';
+$string['instanceismisconfigured'] = 'Portfolio is fout geconfigureerd, overgeslagen. De fout was: $a';
+$string['instancenotdelete'] = 'Portfolio verwijderen mislukt';
+$string['instancenotsaved'] = 'Portfolio bewaren mislukt';
+$string['instancesaved'] = 'Portfolio bewaren gelukt';
+$string['invalidformat'] = 'Iets is een ongelige format aan het exporteren, $a';
+$string['manageportfolios'] = 'Beheer portfolio\"s';
+$string['manageyourportfolios'] = 'Beheer jouw portfolio\'s';
+$string['noavailableplugins'] = 'Er zijn voor jou geen portfolio\'s beschikbaar om naar te exporteren';
+$string['nocallbackfile'] = 'Iets in de module van waaruit je wil exporteren werkt niet - kon een vereist bestan ($a) niet vinden';
+
+?>
