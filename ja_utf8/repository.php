432bbb6a6871bfdbccfe1d2172bbd11770fe68dc
Mitsuhiro Yoshida <mits>
2008-08-02 05:34:24 +0000

    MDL-15402 MDL-13766 Translated new strings for repository.

--- ja_utf8/repository.php
+++ ja_utf8/repository.php
@@ -1,11 +1,27 @@
 <?PHP // $Id$ 
-      // repository.php - created with Moodle 2.0 dev (Build: 20080728) (2008072800)
+      // repository.php - created with Moodle 2.0 dev (Build: 20080801) (2008073114)
 
 
+$string['activaterep'] = 'アクティブリポジトリ';
+$string['back'] = '&lt; 戻る';
+$string['close'] = '閉じる';
+$string['download'] = 'ダウンロード';
+$string['filename'] = 'ファイル名';
+$string['invalidplugin'] = 'リポジトリプラグインが正しくありません。';
+$string['invalidrepositoryid'] = 'リポジトリIDが正しくありません。';
+$string['listview'] = 'リストビュー';
+$string['loading'] = 'ローディング ...';
+$string['logout'] = 'ログアウト';
 $string['manageuserrepository'] = 'リポジトリの管理';
+$string['openpicker'] = 'ファイルピッカーを開く';
 $string['plugin'] = 'リポジトリプラグイン';
 $string['repositories'] = 'リポジトリ';
 $string['repository'] = 'リポジトリ';
+$string['search'] = '検索';
 $string['settings'] = '設定';
+$string['submit'] = '送信';
+$string['sync'] = '同期';
+$string['thumbview'] = 'サムネイルビュー';
+$string['title'] = 'リポジトリファイルピッカー';
 
 ?>
