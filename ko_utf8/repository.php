61ed71eed8a7e4706f856047c4aba75f7c755fa7
Lee Chan-young <yogibear270>
2008-08-05 07:20:00 +0000

    New file -20080730

---
+++ ko_utf8/repository.php
@@ -0,0 +1,27 @@
+<?PHP // $Id$ 
+      // repository.php - created with Moodle 2.0 dev (Build: 20080805) (2008080401)
+
+
+$string['activaterep'] = '저장고';
+$string['back'] = '&lt; 뒤로';
+$string['close'] = '닫기';
+$string['download'] = '내려받기';
+$string['filename'] = '파일명';
+$string['invalidplugin'] = '잘못된 저장 플러그인';
+$string['invalidrepositoryid'] = '잘못된 저장고 ID';
+$string['listview'] = '목록 보기';
+$string['loading'] = '불러오는 중...';
+$string['logout'] = '나가기';
+$string['manageuserrepository'] = '개별 저장고 관리';
+$string['openpicker'] = '파일 열기 프로그램';
+$string['plugin'] = '저장 플러그인';
+$string['repositories'] = '저장고';
+$string['repository'] = '저장고';
+$string['search'] = '탐색';
+$string['settings'] = '설정';
+$string['submit'] = '제출';
+$string['sync'] = '동기화';
+$string['thumbview'] = '작은 그림 보기';
+$string['title'] = '파일 관리 프로그램';
+
+?>
