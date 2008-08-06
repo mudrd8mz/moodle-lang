7581a70f38195003de6b121a35e36c2699358c85
Arnis Voitkans <arnivoit>
2008-08-06 08:06:53 +0000

    *** empty log message ***

---
+++ lv_utf8/notes.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 1.9.2+ (Build: 20080730) (2007101520.03)
+
+
+$string['addnewnote'] = 'Pievienot jaunu piezīmi';
+$string['addnewnoteselect'] = 'Izvēlieties lietotājus, par kuriem rakstīt piezīmes';
+$string['bynameondate'] = '$a->name - $a->date';
+$string['content'] = 'Saturs';
+$string['course'] = 'kurss';
+$string['coursenotes'] = 'Kursa piezīmes';
+$string['created'] = 'izveidots';
+$string['deleteconfirm'] = 'Dzēst šo piezīmi?';
+$string['deletenotes'] = 'Dzēst visas piezīmes';
+$string['editnote'] = 'Rediģēt piezīmi';
+$string['groupaddnewnote'] = 'Pievienot kopējo piezīmi';
+$string['nocontent'] = 'Piezīmes saturs nedrīkst būt tukšs';
+$string['nonotes'] = 'Pagaidām vēl nav šāda tipa piezīmju';
+$string['note'] = 'Piezīme';
+$string['notes'] = 'Piezīmes';
+$string['notesnotvisible'] = 'Jums nav pilnvaru skatīt piezīmes.';
+$string['nouser'] = 'Jums ir jāiezīme lietotājs';
+$string['personal'] = 'personīgi';
+$string['personalnotes'] = 'Personīgās piezīmes';
+$string['publishstate'] = 'Statuss';
+$string['site'] = 'vietne';
+$string['sitenotes'] = 'Vietnes piezīmes';
+$string['unknown'] = 'nezināms';
+
+?>
