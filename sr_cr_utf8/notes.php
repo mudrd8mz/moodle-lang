cb42f45fd6b9ddccfc90f8349b769f15aa25734a
Živana Komlenov <komlenovz>
2008-08-07 23:25:19 +0000

    Moodle 1.9 updates

--- sr_cr_utf8/notes.php
+++ sr_cr_utf8/notes.php
@@ -1,19 +1,23 @@
 <?PHP // $Id$ 
-      // notes.php - created with Moodle 1.8.2+ (2007021520)
+      // notes.php - created with Moodle 1.9 Beta 4 (2007101508.05)
 
 
 $string['addnewnote'] = 'Додај белешку';
 $string['addnewnoteselect'] = 'Избор корисникâ о којима се пишу белешке';
-$string['bynameondate'] = 'аутор $a->name - $a->date';
+$string['bynameondate'] = 'аутор $a->име - $a->датум';
+$string['cannotdeletepost'] = 'Дошло је до грешке при брисању поруке';
 $string['content'] = 'Садржај';
 $string['course'] = 'курс';
 $string['coursenotes'] = 'Белешке курса';
 $string['created'] = 'постављено';
 $string['deleteconfirm'] = 'Обриши белешку';
+$string['deletenotes'] = 'Обриши све белешке';
 $string['editnote'] = 'Уреди белешку';
 $string['groupaddnewnote'] = 'Додај белешку за све';
+$string['invalidid'] = 'Специфицирана је невалидна белешка';
 $string['nocontent'] = 'Унесите садржај';
 $string['nonotes'] = 'Нема белешки';
+$string['nopermissiontodelete'] = 'Не можете обрисати ову белешку';
 $string['note'] = 'Белешка';
 $string['notes'] = 'Белешке';
 $string['notesnotvisible'] = 'Није вам дозвољено да видите белешке';
