5227755bb711b9221a79c0737a32687732d83663
Nicolas Martignoni <martignoni>
2008-08-08 11:18:07 +0000

    MDL-13766, translation for repository settings

---
+++ fr_utf8/repository.php
@@ -0,0 +1,28 @@
+<?php // $Id$
+
+// Quel terme pour repositories ? Dépôt
+
+$string['activaterep'] = 'Activer les dépôts';
+$string['back'] = '&lt; Retour';
+$string['download'] = 'Télcharger';
+$string['close'] = 'Fermer';
+$string['filename'] = 'Nom de fichier';
+$string['filesaved'] = 'Fichier enregistré&nbsp;!';
+$string['invalidrepositoryid'] = 'Identifiant de dépôt non valide';
+$string['invalidplugin'] = 'Plugin de dépôt non valide';
+$string['manageuserrepository'] = 'Gérer le dépôt personnel';
+$string['plugin'] = 'Plugin dépôt';
+$string['settings'] = 'Réglages';
+$string['openpicker'] = 'Ouvrir le sélecteur de fichier';
+$string['listview'] = 'Liste';
+$string['logout'] = 'Déconnexion';
+$string['loading'] = 'Chargement...';
+$string['thumbview'] = 'Vignettes';
+$string['search'] = 'Recherche ';
+$string['submit'] = 'Envoyer';
+$string['sync'] = 'Sync';
+$string['title'] = 'Sélecteur de fichier du dépôt';
+$string['repository'] = 'Dépôt';
+$string['repositories'] = 'Dépôts';
+
+?>
