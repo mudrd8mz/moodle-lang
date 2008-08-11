ce42ff9dced9c6225befb0f8310cefcbf29d9926
koen roggemans <koenr>
2008-08-11 09:24:08 +0000

    new strings

---
+++ nl_utf8/portfolio_boxnet.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // portfolio_boxnet.php - created with Moodle 2.0 dev (Build: 20080811) (2008080701)
+
+
+$string['apikey'] = 'API-sleutel (die moet je krijgen van Box.net)';
+$string['err_noapikey'] = 'Voor deze plugin is er geen API-sleutel geconfigureerd. Je kunt er één bekomen van http://enabled.box.net';
+$string['existingfolder'] = 'Verlaat map om bestand(en) in te zetten.';
+$string['folderclash'] = 'De map die je wou maken bestaat al!';
+$string['foldercreatefailed'] = 'Het maken van je map op box.net is mislukt';
+$string['folderlistfailed'] = 'Kon geen mappenlijst krijgen van box.net';
+$string['newfolder'] = 'Nieuwe map om bestand(en) in te bewaren';
+$string['noauthtoken'] = 'Kon geen authenticatie-token krijgen om tijdens deze sessie te gebruiken';
+$string['notarget'] = 'Je moet ofwel een bestaande map kiezen of een nieuwe map maken om naar te uploaden';
+$string['noticket'] = 'Kon geen ticket krijgen van box.net om de authenticatiesessie te beginnen';
+$string['password'] = 'Je box.net wachtwoord (wordt niet bewaard)';
+$string['sharedfolder'] = 'Gedeeld';
+$string['targetfolder'] = 'Doelmap';
+$string['username'] = 'Je box.net gebruikersnaam (wordt niet bewaard)';
+
+?>
---
+++ nl_utf8/portfolio_download.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // portfolio_download.php - created with Moodle 2.0 dev (Build: 20080811) (2008080701)
+
+
+$string['downloadfile'] = 'Download je portfolio exportbestand';
+
+?>
