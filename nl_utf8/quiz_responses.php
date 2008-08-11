8110dc766c7b41013390bbc06bb104955e38e86d
koen roggemans <koenr>
2008-08-11 09:45:09 +0000

    new strings

--- nl_utf8/quiz_responses.php
+++ nl_utf8/quiz_responses.php
@@ -1,10 +1,11 @@
 <?PHP // $Id$ 
-      // quiz_responses.php - created with Moodle 1.7 dev (2006050502)
+      // quiz_responses.php - created with Moodle 2.0 dev (Build: 20080811) (2008080701)
 
 
 $string['pagesize'] = 'Pogingen per pagina';
 $string['reportresponses'] = 'Gedetailleerde antwoorden';
 $string['responses'] = 'Gedetailleerde antwoorden';
+$string['responsesdownload'] = 'Gedetailleerde antwoorden downloaden';
 $string['responsesoptions'] = 'Antwoordopties';
 $string['responsestitle'] = 'Gedetailleerde antwoorden';
 
