9f886f2a1d43d369a45e44a5d0fd8ef5a43d902b
Živana Komlenov <komlenovz>
2008-08-15 12:33:52 +0000

    Moodle 1.9.2 updates

---
+++ sr_cr_utf8/block_tags.php
@@ -0,0 +1,63 @@
+<?PHP // $Id$ 
+      // block_tags.php - created with Moodle 1.9.2 (Build: 20080711) (2007101520)
+
+
+$string['add'] = 'Додај';
+$string['alltags'] = 'Све ознаке:';
+$string['alltags1'] = 'све ознаке:';
+$string['alltags2'] = 'Прикажи све ознаке (кориснички профил, блогови и ознаке на курсу)';
+$string['arrowtitle'] = 'Кликни овде за унос предложеног текста (сива слова).';
+$string['communitytags'] = 'Ознаке из заједнице:';
+$string['communitytags1'] = 'ознаке из заједнице';
+$string['communitytags2'] = 'Прикажи све ознаке на курсевима које су креирали корисници';
+$string['coursetags'] = 'Ознаке на курсу:';
+$string['coursetags1'] = 'ознаке на курсу';
+$string['coursetags2'] = 'Прикажи ознаке за овај курс';
+$string['edit'] = 'уреди...';
+$string['editdeletemytag'] = 'Избриши ознаку са овог курса:';
+$string['editmytags'] = 'Моје ознаке - пречице до свих Ваших курсева са ознакама.';
+$string['editmytagsfor'] = 'Уреди моје ознаке за $a';
+$string['editnopersonaltags'] = 'Још увек нису креиране личне ознаке';
+$string['edittags'] = 'Уреди моје ознаке...';
+$string['edittagthisunit'] = 'Додај ознаку на овај курс';
+$string['editthiscoursetags'] = 'Моје ознаке за овај курс су $a';
+$string['edittitle'] = 'Моје ознаке';
+$string['entries'] = 'ставке';
+$string['entry'] = 'ставка';
+$string['jserror1'] = 'Ознаке морају имати више од једног и мање од 50 знакова. Молимо да прилагодите своју ознаку.';
+$string['jserror2'] = 'Ознаке не могу садржати ове специјалне знаке - , <, >. Молимо да прилагодите своју ознаку.';
+$string['login'] = 'пријава';
+$string['more'] = 'више...';
+$string['moreorder'] = 'Поредак:';
+$string['moreorderalpha'] = 'Азбучни';
+$string['moreorderdate'] = 'По датуму креирања';
+$string['moreorderpop'] = 'Популарност';
+$string['moreshow'] = 'Прикажи:';
+$string['moreshowalltags'] = 'Све ознаке';
+$string['moreshowcommtags'] = 'Ознаке заједнице';
+$string['moreshowcoursetags'] = 'Ознаке за \'$a\'';
+$string['moreshowmytags'] = 'Моје ознаке';
+$string['moreshowofficialtags'] = 'Службене ознаке';
+$string['moretags'] = 'Прикажи и издвој више ознака';
+$string['moretitle'] = 'Више ознака';
+$string['morewelcome'] = 'Добродошли у облачић за ознаке \'Више ознака\'.
+Ознаке (tag-ови) су линкови које креирају корисници до различитих ствари. Ознаке омогућавају да категоризујете и повезујете  ствари као што су Ваши омиљени курсеви, Ваши блогови или Ваш профил са Вашим сопственим речима. Различите групе ознака могу се приказати са линковима у реду \'Прикажи:\', а може се преуредити по редоследу преко линка у реду \'Поредак:\'.';
+$string['mycoursetag1'] = 'Прикажи моје ознаке на курсу';
+$string['mycoursetag2'] = 'моје ознаке на курсу';
+$string['mycoursetags'] = 'Моје ознаке на курсу:';
+$string['mytags'] = 'Моје ознаке:';
+$string['mytags1'] = 'моје ознаке';
+$string['mytags2'] = 'Прикажи моје личне ознаке';
+$string['notagsyet'] = 'Још увек нема ознака';
+$string['officialtags'] = 'Службене ознаке на курсу:';
+$string['officialtags1'] = 'службене ознаке';
+$string['officialtags2'] = 'Прикажи службене ознаке на курсу';
+$string['please'] = 'Молим';
+$string['select'] = 'Изабери...';
+$string['showcoursetags'] = 'Прикажи ознаке на курсу';
+$string['showcoursetagsdef'] = 'Прикажи особине означавања курсева у блоку ознака, и омогући студентима да стављају ознаке на курсевима.';
+$string['tags'] = 'ознаке';
+$string['tagthisunit'] = 'Означи овај курс:';
+$string['tagunits'] = 'да би означили Ваш омиљени курс';
+
+?>
---
+++ sr_cr_utf8/debug.php
@@ -0,0 +1,51 @@
+<?PHP // $Id$ 
+      // debug.php - created with Moodle 1.9.2 (Build: 20080711) (2007101520)
+
+
+$string['authpluginnotfound'] = 'Додатак за проверу идентитета $a није пронађен.';
+$string['cannotbenull'] = '$a не може бити празно (null)!';
+$string['cannotcreateadminuser'] = 'ОЗБИЉНА ГРЕШКА: Није могуће креирати запис о администраторском кориснику!!!';
+$string['cannotdowngrade'] = 'Не може се прећи на старију верзију са $a[0] на $a[1].';
+$string['cannotfindadmin'] = 'Не може се пронаћи администраторски корисник!';
+$string['cannotinitpage'] = 'Не може се потпуно покренути страница: неисправан $a[0] id $a[1]';
+$string['cannotsetupsite'] = 'Озбиљна грешка! Не може се поставити сајт!';
+$string['cannotsetuptable'] = '$a табеле се НЕ могу успешно поставити!';
+$string['cannotupdaterelease'] = 'ГРЕШКА: Не може се ажурирати верзија у бази података!!';
+$string['cannotupdateversion'] = 'Ажурирање НИЈЕ УСПЕЛО! (Не може се ажурирати верзија у конфигурационој табели)';
+$string['cannotupgradecapabilities'] = 'Било је проблема у ажурирању главних могућности Система улога.';
+$string['cannotupgradedbcustom'] = 'Ажурирање прилагођавања локалне базе података није успело! (Не може се ажурирати верзија у конфигурационој табели)';
+$string['configmoodle'] = 'Moodle још увек није конфигурисан. Морате уредити прво config.php.';
+$string['dbnotinsert'] = 'Грешка у бази података - унос није могућ ($a)';
+$string['dbnotsetup'] = 'Грешка: Главне базе података НИСУ успешно постављене.';
+$string['dbnotsupport'] = 'Грешка: Ваша база података ($a) још увек није потпуно подржана у Moodle-у или није присутна датотека install.xml. Видети директоријум lib/db.';
+$string['dbnotupdate'] = 'Грешка у бази података - ажурирање није могуће ($a)';
+$string['doesnotworkwitholdversion'] = 'Овај скрипт не ради са овом старијом верзијом Moodle-а';
+$string['erroroccur'] = 'Дошло је до грешке у току овог процеса';
+$string['fixsetting'] = 'Молимо да исправите подешавања у config.php: <p>Ви имате:</p> <p>$CFG->dirroot = \"$a[0]\";</p> <p>а треба да буде:</p> <p>$CFG->dirroot = \"$a[1]\"</p>';
+$string['invalidarraysize'] = 'Неисправна величина низова у параметрима $a';
+$string['invalideventdata'] = 'Прослеђени су неисправни подаци о догађању: $a';
+$string['modulenotexist'] = 'Модул $a не постоји';
+$string['morethanonerecordinfetch'] = 'Пронађено више од једног записа у fetch() !';
+$string['mustbeoveride'] = 'Апстрактни метод $a мора бити замењен.';
+$string['noactivityname'] = 'Објект странице добијен из page_generic_activity, али није дефинисано  $this->activityname';
+$string['noadminrole'] = 'Не може се наћи улога администратора';
+$string['noblockbase'] = 'Класа block_base није дефинисана или датотека није пронађена за /blocks/moodleblock.class.php';
+$string['noblocks'] = 'Нема инсталираних блокова!';
+$string['nocaps'] = 'Грешка: нема дефинисаних могућности!';
+$string['nocate'] = 'Нема категорија!';
+$string['nomodules'] = 'Није пронађен ниједан модул!';
+$string['nopageclass'] = 'Увежено $a, али нису пронађене класе странице.';
+$string['noreports'] = 'Нема извештаја коме се може приступити';
+$string['notables'] = 'Нема табела!';
+$string['phpvaroff'] = 'PHP променљива сервера \'$a[0]\' треба да буде Off - $a[1]';
+$string['phpvaron'] = 'PHP променљива сервера \'$a[0]\' није укључено On - $a[1]';
+$string['prefixcannotbeempty'] = 'Префикс имена табеле \"$a[0]\" не сме бити празан за Вашу одредишну базу података ($a[1])';
+$string['prefixlimit'] = 'Максимална дозвољена дужина префикса имена табеле \"$a\" за Oracle базе података је 2cc.';
+$string['sessionmissing'] = 'Објекат $a недостаје у сесији';
+$string['siteisnotdefined'] = 'Сајт није дефинисан!';
+$string['sqlrelyonobsoletetable'] = 'Овај SQL се заснива на застарелој(им) табели(ама)! Ваш кôд мора поправити програмер.';
+$string['upgradefail'] = 'Ажурирање није успело! видети: $a';
+$string['withoutversion'] = 'Главна датотека version.php није читљива или није наведена';
+$string['xmlizeunavailable'] = 'xmlize функције нису на располагању';
+
+?>
