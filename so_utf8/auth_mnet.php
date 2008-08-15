0273e0c8fb8f7dec3e3ebbd35906f666166af97c
Mohamed Abdi <Soomaali>
2008-08-15 13:13:10 +0000

    no message

---
+++ so_utf8/auth_mnet.php
@@ -0,0 +1,14 @@
+<?php // $Id$ 
+$string['sso_idp_name']                = 'SSO  (Aqoonsi Bixiye)';
+$string['sso_idp_description']         = 'Ku daabac adeeggan si aad ugu oggolaato isticmaalayaasha inay uga gooshaan barta Moodle ee $a iyada oon dib loo soo galin meeshaas. '.
+                                         '<ul><li><em>Ku-tiirsanaan</em>: Waa inaad adigu waliba <strong>rukunsataa</strong> adeegga SSO-ga (Aqoonsi Bixiye) ee ku yaal $a.</li></ul><br />'.
+                                         'Rukunso adeeggan si aad ugu oggolaato isticmaalayaasha la xaqiijiyey ee $a si ay marin ugu helaan goobtaada iyaga oon dib-u-soo galin '.
+                                         '<ul><li><em>Ku-tiirsanaan</em>: Waa inaad waliba  <strong>daabacdaa</strong> adeegga SSO-ga (Aqoonsi Bixiye) ee $a.</li></ul><br />';
+
+$string['sso_sp_name']                 = 'SSO (Aqoonsi Bixiye)';
+$string['sso_sp_description']          = 'Ku daabac adeeggan si aad ugu oggolaato isticmaalayaasha la xaqiijiyey ee $a si ay marin ugu helaan goobtaada iyaga oon dib-u-soo galin'.
+                                        '<ul><li><em>Ku-tiirsanaan</em>: Waa inaad adigu waliba <strong>rukunsataa</strong> adeegga SSO-ga (Aqoonsi Bixiye) ee ku yaal $a.</li></ul><br />'.
+                                         'Rukunso adeeggan si aad ugu oggolaato isticmaalayaasha la xaqiijiyey ee $a si ay marin ugu helaan goobtaada iyaga oon dib-u-soo galin '.
+                                         '<ul><li><em>Ku-tiirsanaan</em>: Waa inaad waliba  <strong>daabacdaa</strong> adeegga SSO-ga (Aqoonsi Bixiye) ee $a.</li></ul><br />';                                      
+                                         
+$string['sso_mnet_login_refused']      = 'Isticmaale-magac $a[0] looma oggola inuu ka soo galo $a[1].';
