e7f7d451833981406fea48ab4f7a1af6367cca77
Nicolas Martignoni <martignoni>
2008-08-16 09:27:32 +0000

    MDL-15402, repo admin strings translated

--- fr_utf8/repository.php
+++ fr_utf8/repository.php
@@ -4,10 +4,14 @@
 // Autres possibilités à envisager : entrepôt, stockage, zone de dépôt, ...
 
 $string['activaterep'] = 'Activer les dépôts de documents';
-$string['add'] = 'Ajouter';
+$string['add'] = 'Ajouter un dépôt';
 $string['back'] = '&lt; Retour';
 $string['close'] = 'Fermer';
-$string['download'] = 'Télcharger';
+$string['configsaved'] = 'Configuration enregistrée&nbsp;!';
+$string['confirmdelete'] = 'Voulez-vous vraiment supprimer le dépôt $a&nbsp;?';
+$string['createrepository'] = 'Créer un dépôt';
+$string['deleterepository'] = 'Supprimer ce dépôt';
+$string['download'] = 'Télécharger';
 $string['downloadsucc'] = 'Téléchargement réussi&nbsp;!';
 $string['filename'] = 'Nom de fichier';
 $string['filesaved'] = 'Fichier enregistré&nbsp;!';
@@ -16,6 +20,7 @@ $string['invalidrepositoryid'] = 'Identifiant de dépôt non valide';
 $string['listview'] = 'Liste';
 $string['loading'] = 'Chargement...';
 $string['logout'] = 'Déconnexion';
+$string['manage'] = 'Gérer les dépôts';
 $string['manageuserrepository'] = 'Gérer le dépôt personnel';
 $string['noenter'] = 'Pas de saisie';
 $string['openpicker'] = 'Ouvrir le sélecteur de fichier';
