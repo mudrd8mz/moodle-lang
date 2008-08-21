5f2654b232ca9cb6170f9f6d3f99db0f605d279a
Živana Komlenov <komlenovz>
2008-08-21 12:46:56 +0000

    Clean-up

--- sr_cr_utf8/block_feedback.php
+++
@@ -1,7 +0,0 @@
-<?PHP // $Id$ 
-      // block_feedback.php - created with Moodle 1.8.2+ (2007021520)
-
-
-$string['feedback'] = 'Анкета';
-
-?>
--- sr_lt_utf8/block_feedback.php
+++
@@ -1,7 +0,0 @@
-<?PHP // $Id$ 
-      // block_feedback.php - created with Moodle 1.8.1+ (2007021510)
-
-
-$string['feedback'] = 'Anketa';
-
-?>
