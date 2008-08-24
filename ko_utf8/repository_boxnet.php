8898e0ed9062fe2785f18e620a3535c5c7b35466
Lee Chan-young <yogibear270>
2008-08-24 08:11:24 +0000

    New file -200808xx

---
+++ ko_utf8/repository_boxnet.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // repository_boxnet.php - created with Moodle 2.0 dev (Build: 20080824) (2008081900)
+
+
+$string['apikey'] = 'API 키';
+$string['configplugin'] = 'Box.net 설정';
+$string['invalidpassword'] = '잘못된 비밀번호';
+$string['nullfilelist'] = '저장소에 파일 없음';
+$string['password'] = '비밀번호';
+$string['repositorydesc'] = 'Box.net 저장소';
+$string['repositoryname'] = 'Box.net';
+$string['saved'] = 'Box.net 자료 저장됨';
+$string['username'] = 'Box.net 계정';
+
+?>
---
+++ ko_utf8/repository_flickr.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // repository_flickr.php - created with Moodle 2.0 dev (Build: 20080824) (2008081900)
+
+
+$string['configplugin'] = 'Flickr 설정';
+$string['invalidemail'] = '잘못된 Flickr 메일 주소';
+$string['notitle'] = '제목없음';
+$string['nullphotolist'] = '본 계정으로 등록된 사진 없음';
+$string['remember'] = '기억하기';
+$string['repositorydesc'] = 'Flickr 저장소';
+$string['repositoryname'] = 'Flickr';
+$string['username'] = 'Flickr 계정';
+
+?>
