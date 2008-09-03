abd1858fb3e1a1cb5b835c1c06c23e7e44e5696c
Lee Chan-young <yogibear270>
2008-09-03 10:38:20 +0000

    New file -20080903

---
+++ ko_utf8/dbtransfer.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$ 
+      // dbtransfer.php - created with Moodle 2.0 dev (Build: 20080903) (2008090108)
+
+
+$string['dbexport'] = '데이터베이스 내보내기';
+$string['dbtransfer'] = '데이터베이스 전송';
+$string['differenttableexception'] = '테이블 $a 의 구조가 일치하지 않음';
+$string['exportdata'] = '데이터 내보내기';
+$string['exportschemaexception'] = '데이터베이스의 구조가 .xml 파일과 일치하지 않습니다.<br /> $a';
+$string['importschemaexception'] = '데이터베이스의 구조가 .xml 파일과 일치하지 않습니다.<br /> $a';
+$string['importversionmismatchexception'] = '현재의  $a->currentver 는 내보낸 $a->schemaver 판과 일치하지 않습니다.';
+$string['malformedxmlexception'] = '잘못된 XML 이므로 계속할 수 없습니다.';
+$string['notargetconectexception'] = '죄송합니다만 대상 데이터베이스와 연결할 수 없습니다.';
+$string['transferdata'] = '데이터 전송';
+$string['unknowntableexception'] = '내보내는 파일에서 알 수 없는 테이블 $a 가 발견되었습니다.';
+
+?>
