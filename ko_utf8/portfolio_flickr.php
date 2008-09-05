eea02f23ea403c2badbc0ddd0f14264596e86cf5
Lee Chan-young <yogibear270>
2008-09-05 10:09:09 +0000

    New file -20080905

---
+++ ko_utf8/portfolio_flickr.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // portfolio_flickr.php - created with Moodle 2.0 dev (Build: 20080905) (2008090108)
+
+
+$string['apikey'] = 'API 키(http://flickr.com/services/api/keys/apply 에서 얻어야 함)';
+$string['err_noapikey'] = '플러그인에 대한 API 키가 설정되지 않음. http://flickr.com/services/api/keys/apply 에서 얻을 수 있음';
+$string['noauthtoken'] = '본 세션에 사용한 인증 토큰을 찾을 수 없음';
+$string['pluginname'] = 'Flickr 사진 관리 시스템';
+$string['sharedsecret'] = '공유 암호(생성된 API 키에 제공됨)';
+
+?>
