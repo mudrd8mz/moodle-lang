43b9dce7b0d99d8e2cdcf8cbd8faab39e7689948
Nicolas Martignoni <martignoni>
2008-09-05 07:43:08 +0000

    More corrections

--- fr_utf8/repository_flickr.php
+++ fr_utf8/repository_flickr.php
@@ -4,9 +4,9 @@ $string['configplugin'] = 'Configuration Flickr';
 $string['repositoryname'] = 'Flickr';
 $string['repositorydesc'] = 'Dépôt sur flickr.com';
 $string['notitle'] = 'sans titre';
-$string['username'] = 'Courriel du compte Flickr&nbsp;:';
+$string['username'] = 'Courriel du compte Flickr';
 $string['remember'] = 'Enregistrer';
-$string['nullphotolist'] = 'Il n\'y a pas de photo dans ce compte.';
+$string['nullphotolist'] = 'Il n\'y a pas de photo dans ce compte';
 $string['invalidemail'] = 'Adresse de courriel non valide pour Flickr.';
 
 ?>
