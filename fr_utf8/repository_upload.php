4bb54f483481f0fe34fc99142293b0a57bd5786f
Nicolas Martignoni <martignoni>
2008-09-07 19:07:23 +0000

    MDL-13766 new lang file added

---
+++ fr_utf8/repository_upload.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+
+$string['configplugin'] = 'Configuration du plugin de dépôt de fichier';
+$string['repositoryname'] = 'Déposer une fichier';
+$string['repositorydesc'] = 'Déposer une fichier dans Moodle';
+
+?>
