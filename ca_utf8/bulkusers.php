a0e8934035cd4bf583f18046a30b5a9aceb50b74
Carles Bellver <carlesbellver>
2008-09-08 10:28:31 +0000

    *** empty log message ***

--- ca_utf8/bulkusers.php
+++ ca_utf8/bulkusers.php
@@ -1,9 +1,9 @@
 <?PHP // $Id$ 
-      // bulkusers.php - created with Moodle 1.9 + (Build: 20080304) (2007101509)
+      // bulkusers.php - created with Moodle 1.9.1+ (Build: 20080618) (2007101514)
 
 
-$string['addall'] = 'Afegeix tots els usuaris disponibles';
-$string['addsel'] = 'Afegeix els usuaris disponibles seleccionats';
+$string['addall'] = 'Afegeix tots a Seleccionats';
+$string['addsel'] = 'Afegeix els marcats a Seleccionats';
 $string['allfilteredusers'] = 'Tots els filtrats ($a->count/$a->total)';
 $string['allselectedusers'] = 'Tots els seleccionats ($a->count/$a->total)';
 $string['allusers'] = 'Tots els usuaris ($a)';
@@ -11,8 +11,8 @@ $string['available'] = 'Disponibles';
 $string['confirmmessage'] = 'Segur que voleu enviar aquest missatge a tots aquests usuaris?<br />$a';
 $string['nofilteredusers'] = 'No s\'ha trobat cap usuari (0/$a)';
 $string['noselectedusers'] = 'No s\'ha seleccionat cap usuari';
-$string['removeall'] = 'Suprimeix tots els usuaris disponibles';
-$string['removesel'] = 'Suprimeix els usuaris disponibles seleccionats';
+$string['removeall'] = 'Treu tots';
+$string['removesel'] = 'Treu els marcats';
 $string['selected'] = 'Seleccionats';
 $string['selectedlist'] = 'Llista d\'usuaris seleccionats...';
 $string['usersfound'] = 'S\'han trobat $a usuaris';
