03fa7378d8a58db97bffba8cd7dbff46572b0c43
Mitsuhiro Yoshida <mits>
2008-09-08 07:38:55 +0000

    Brushed up a translated help file for forum attachment. MDL-16343 - Adding a translated lang file for capability report.

---
+++ ja_utf8/report_capability.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$ 
+      // report_capability.php - created with Moodle 2.0 dev (Build: 20080908) (2008090800)
+
+
+$string['capability'] = 'ケイパビリティレポート';
+$string['capabilitylabel'] = 'ケイパビリティ:';
+$string['capabilityreport'] = 'ケイパビリティレポート';
+$string['changeoverrides'] = 'このコンテクスト内のオーバーライドを変更する';
+$string['changeroles'] = 'ロール定義を変更する';
+$string['forroles'] = 'ロール $a';
+$string['getreport'] = 'レポートを取得する';
+$string['intro'] = 'このレポートでは、特定のケイパビリティに関して、すべてのロール (または選択されたロール) 定義に、どのようなパーミッションが設定されているか、およびサイト全体のどこでそのケイパビリティがオーバーライドされているか表示します。';
+$string['reportforcapability'] = 'ケイパビリティ「 $a 」のレポート';
+$string['reportsettings'] = 'レポート設定';
+$string['roleslabel'] = 'ロール:';
+
+?>
