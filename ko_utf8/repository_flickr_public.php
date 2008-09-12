bc581ad03e184f27864c082149dda2ca9ccde736
Lee Chan-young <yogibear270>
2008-09-12 07:40:34 +0000

    New file -20080912

---
+++ ko_utf8/repository_flickr_public.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // repository_flickr_public.php - created with Moodle 2.0 dev (Build: 20080912) (2008091000)
+
+
+$string['apikey'] = 'API 키';
+$string['callbackurl'] = '회신 URL';
+$string['configplugin'] = 'Flickr 공개 설정';
+$string['emailaddress'] = '이메일 주소';
+$string['invalidemail'] = '잘못된 Flickr 이메일 주소';
+$string['notitle'] = '제목없음';
+$string['nullphotolist'] = '계정에 사진이 없음';
+$string['remember'] = '기억하기';
+$string['repositorydesc'] = 'flickr.com 저장소';
+$string['repositoryname'] = '공개 Flickr';
+$string['secret'] = '비밀';
+$string['username'] = 'Flickr 이메일 계정';
+
+?>
