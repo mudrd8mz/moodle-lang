b031f35a68b9e129e8f56f1a148ee68ba947859a
Nicolas Martignoni <martignoni>
2008-09-12 14:26:15 +0000

    MDL-16341 Added language file

---
+++ fr_utf8/portfolio_flickr.php
@@ -0,0 +1,9 @@
+<?php // $Id$
+
+$string['apikey'] = 'Clef API (vous l\'obtiendrez sur http://flickr.com/services/api/keys/apply)';
+$string['err_noapikey'] = 'Il n\'y a pas de clef API configurée pour ce plugin. Vous pouvez en obtenir une sur http://flickr.com/services/api/keys/apply';
+$string['noauthtoken'] = 'Impossible de récupérer un jeton d\'authentification à utiliser dans cette session';
+$string['pluginname'] = 'Système de gestion de photographie Flickr';
+$string['sharedsecret'] = 'Votre chaîne partagée secrète (sera fournie avec la clef API)';
+
+?>
