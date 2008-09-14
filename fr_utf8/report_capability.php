509adda4efb8bf2565c5ebc767270d1379a1168f
Nicolas Martignoni <martignoni>
2008-09-14 16:04:06 +0000

    MDL-16343 report capability strings

---
+++ fr_utf8/report_capability.php
@@ -0,0 +1,15 @@
+<?php  // $Id$
+
+$string['capability'] = 'Rapport de capacité';
+$string['capabilitylabel'] = 'Capacité&nbsp;:';
+$string['capabilityreport'] = 'Rapport de capacité';
+$string['changeoverrides'] = 'Modifications des dérogations dans ce contexte';
+$string['changeroles'] = 'Modifications des définitions de rôle';
+$string['forroles'] = 'Pour les rôles $a';
+$string['getreport'] = 'Afficher le rapport';
+$string['intro'] = 'Ce rapport indique, pour une capacité donnée, la permission qui est définie pour cette capacité dans chaque rôle (ou une sélection de rôles). Il indique également toutes les dérogations définies dans le site pour cette capacité.';
+$string['reportforcapability'] = 'Report pour la capacité «&nbsp;$a&nbsp;»';
+$string['reportsettings'] = 'Réglages du rapport';
+$string['roleslabel'] = 'Rôles&nbsp;:';
+
+?>
