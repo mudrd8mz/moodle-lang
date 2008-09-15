9e4746fe91f22784c7a39b5a4e8f49eb45bbd68b
Mitsuhiro Yoshida <mits>
2008-09-15 10:30:44 +0000

    MDL-15402 - Translated a new string for repository Flickr.

--- ja_utf8/repository_flickr.php
+++ ja_utf8/repository_flickr.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // repository_flickr.php - created with Moodle 2.0 dev (Build: 20080910) (2008091000)
+      // repository_flickr.php - created with Moodle 2.0 dev (Build: 20080915) (2008091500)
 
 
 $string['apikey'] = 'APIキー';
 $string['callbackurl'] = 'コールバックURI';
+$string['callbackwarning'] = 'あなたは、コールバックURIをFlickrにコピーする必要があります。このコールバックURIは、あなたがこの情報を保存した後、生成されます。生成されたコールバックURIを確認するため、「保存」ボタンをクリックした後、「設定」ボタンをクリックしてください。';
 $string['configplugin'] = 'Flickr設定';
 $string['emailaddress'] = 'メールアドレス';
 $string['invalidemail'] = '無効なFlickrメールアドレスです。';
