6d81c37cd8d9421fe7b2a689e777a9ad949c7712
Lee Chan-young <yogibear270>
2008-09-16 02:28:32 +0000

    New file -20080916

---
+++ ko_utf8/report_questioninstances.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$ 
+      // report_questioninstances.php - created with Moodle 2.0 dev (Build: 20080916) (2008091500)
+
+
+$string['editquestionshere'] = '이 영역의 문항 편집';
+$string['getreport'] = '보고서 생성';
+$string['hiddenquestions'] = '비공개';
+$string['intro'] = '본 보고서는 문형에 따라 문제들이 속해 있는 전 영역을 보여 줍니다.';
+$string['questioninstances'] = '문제 인스턴스';
+$string['reportforallqtypes'] = '문제 유형 보고서';
+$string['reportformissingqtypes'] = '비분류 문제 유형 보고서';
+$string['reportforqtype'] = '문제 유형 \'$a\' 보고서';
+$string['reportsettings'] = '보고서 설정';
+$string['totalquestions'] = '총계';
+$string['visiblequestions'] = '공개';
+
+?>
