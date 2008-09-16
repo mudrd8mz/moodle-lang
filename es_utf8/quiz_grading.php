3e159ab2200687f36c079322e65f9a951417017d
Benito Arias <barias>
2008-09-16 10:25:11 +0000

    *** empty log message ***

--- es_utf8/quiz_grading.php
+++ es_utf8/quiz_grading.php
@@ -3,7 +3,18 @@
 
 
 $string['essayonly'] = 'Las siguientes preguntas necesitan calificarse manualmente';
+$string['gradeall'] = 'Calificar todos los $ intentos';
+$string['graded'] = '(calificado)';
+$string['gradenextungraded'] = 'Calificar los siguientes $ intentos no calificados';
+$string['gradeungraded'] = 'Calificar todos los $ intentos no calificados';
 $string['grading'] = 'Calificación manual';
+$string['gradingall'] = 'Todos los $ intentos de contestar esta pregunta.';
+$string['gradingattempt'] = 'Intento número $a->attempt de $a->fullname.';
+$string['gradingnextungraded'] = 'Los siguientes $ intentos no calificados';
 $string['gradingnotallowed'] = 'No tiene permiso para calificar manualmente las respuestas de este cuestionario';
+$string['gradingreport'] = 'Informe de calificación manual';
+$string['gradingungraded'] = '$a intentos no calificados';
+$string['gradinguser'] = 'Intentos para $a';
+$string['questiontitle'] = 'Pregunta $a->number : \"$a->name\" ({$a->openspan}$a->gradedattempts{$a->closespan} / $a->totalattempts intentos {$a->openspan}graded{$a->closespan}).';
 
 ?>
