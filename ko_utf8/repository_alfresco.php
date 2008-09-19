b054b7876ad01e7792b7af2a5d4ee5c235704199
Lee Chan-young <yogibear270>
2008-09-19 09:36:06 +0000

    New file -20080919

---
+++ ko_utf8/repository_alfresco.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // repository_alfresco.php - created with Moodle 2.0 dev (Build: 20080919) (2008091611)
+
+
+$string['alfresco_url'] = 'Alfresco URL';
+$string['configplugin'] = 'Alfresco 설정';
+$string['notitle'] = '제목없음';
+$string['password'] = '비밀번호';
+$string['repositorydesc'] = 'Alfresco CMS 플러그인';
+$string['repositoryname'] = 'Alfresco 저장소';
+$string['username'] = '사용자 ID';
+
+?>
