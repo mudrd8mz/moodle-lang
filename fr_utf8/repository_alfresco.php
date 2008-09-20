d3e2ea9f74d078c1fb214660e2d26c5fd388f007
Nicolas Martignoni <martignoni>
2008-09-20 08:07:59 +0000

    MDL-16384, new lang file for Alfresco repo

---
+++ fr_utf8/repository_alfresco.php
@@ -0,0 +1,11 @@
+<?php // $Id$
+
+$string['configplugin'] = 'Configuration Alfresco';
+$string['notitle'] = 'Sans titre';
+$string['repositoryname'] = 'Dépôt Alfresco';
+$string['repositorydesc'] = 'Un plugin pour le CMS Alfresco';
+$string['username'] = 'Nom d\'utilisateur';
+$string['password'] = 'Mot de passe';
+$string['alfresco_url'] = 'URL Alfresco';
+
+?>
