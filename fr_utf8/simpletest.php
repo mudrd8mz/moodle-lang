4ae989f4e1ca27bcac67b91fb215435de272465b
Nicolas Martignoni <martignoni>
2008-09-20 08:05:00 +0000

    MDL-16486, new strings

--- fr_utf8/simpletest.php
+++ fr_utf8/simpletest.php
@@ -1,6 +1,8 @@
 <?php // $Id$
 
 $string['all'] = 'TOUT';
+$string['deletingnoninsertedrecord'] = 'Tentative de suppression d\'un enregistrement n\'ayant pas été inséré par ces tests unitaires (id $a->id dans la table $a->table).';
+$string['deletingnoninsertedrecords'] = 'Tentative de suppression d\'enregistrements n\'ayant pas été insérés par ces tests unitaires (de la table $a->table).';
 $string['exception'] = 'Exception';
 $string['fail'] = 'Échec';
 $string['ignorethisfile'] = 'Relancer les tests unitaires en ignorant ce fichier de test.';
@@ -23,9 +25,11 @@ $string['showpasses'] = 'Afficher les tests réussis en plus des échecs.';
 $string['showsearch'] = 'Afficher la recherche des fichiers de test.';
 $string['stacktrace'] = 'Trace de la pile&nbsp;:';
 $string['summary'] = '{$a->run}/{$a->total} tests terminés&nbsp;: <strong>{$a->passes}</strong> tests passés, <strong>{$a->fails}</strong> échecs et <strong>{$a->exceptions}</strong> exceptions.';
+$string['testtablescsvfileunwritable'] = 'Le fichier CSV des tables de test n\'est pas accessible en écriture ($a->filename)';
 $string['thorough'] = 'Lancer un test exhaustif (peut être lent).';
 $string['unittests'] = 'Tests unitaires';
 $string['uncaughtexception'] = 'Exception non traitée [{$a->getMessage()}] dans [{$a->getFile()}:{$a->getLine()}]&nbsp;: TESTS INTERROMPUS.';
+$string['updatingnoninsertedrecord'] = 'Tentative de modification d\'un enregistrement n\'ayant pas été inséré par ces tests unitaires (id $a->id dans la table $a->table).';
 $string['version'] = 'Utilise <a href=\"http://sourceforge.net/projects/simpletest/\">SimpleTest</a> version $a. ';
 
 ?>
