ff85838820dc14c41ec50e71df6f76ea17983476
Nicolas Martignoni <martignoni>
2008-09-21 08:59:59 +0000

    MDL-16409  new lang file

---
+++ fr_utf8/report_questioninstances.php
@@ -0,0 +1,15 @@
+<?php  // $Id$
+
+$string['editquestionshere'] = 'Modifier les questions de ce contexte';
+$string['getreport'] = 'Obtenir le rapport';
+$string['hiddenquestions'] = 'Caché';
+$string['intro'] = 'Ce rapport donne la liste de tous les contextes du système comprenant des questions d\'un type donné.';
+$string['reportforallqtypes'] = 'Rapport pour tous les types de question';
+$string['reportformissingqtypes'] = 'Rapport pour les questions de type inconnu';
+$string['reportforqtype'] = 'Rapport pour les questions de type «&nbsp;$a&nbsp»';
+$string['reportsettings'] = 'Réglages du rapport';
+$string['questioninstances'] = 'Instances de question';
+$string['totalquestions'] = 'Total';
+$string['visiblequestions'] = 'Visible';
+
+?>
