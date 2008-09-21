e1291b9b21a8e4af4a60d4a19c870887565ed975
Nicolas Martignoni <martignoni>
2008-09-21 09:16:08 +0000

    MDL-15402, more strings

--- fr_utf8/repository_flickr.php
+++ fr_utf8/repository_flickr.php
@@ -1,12 +1,15 @@
 <?php // $Id$
 
+$string['apikey'] = 'Clef API';
+$string['callbackurl'] = 'URL de rappel';
 $string['configplugin'] = 'Configuration Flickr';
+$string['emailaddress'] = 'Adresse de courriel';
+$string['invalidemail'] = 'Adresse de courriel non valide pour flickr';
+$string['notitle'] = 'Sans titre';
+$string['nullphotolist'] = 'Il n\'y a pas de photo dans ce compte';
+$string['remember'] = 'Se souvenir de moi';
 $string['repositoryname'] = 'Flickr';
 $string['repositorydesc'] = 'Dépôt sur flickr.com';
-$string['notitle'] = 'sans titre';
 $string['username'] = 'Courriel du compte Flickr';
-$string['remember'] = 'Enregistrer';
-$string['nullphotolist'] = 'Il n\'y a pas de photo dans ce compte';
-$string['invalidemail'] = 'Adresse de courriel non valide pour Flickr.';
 
 ?>
