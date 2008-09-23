dc76fae9f11a8b34e135dc94597ff2a5f6250ae1
Nicolas Martignoni <martignoni>
2008-09-23 18:54:21 +0000

    MDL-15488 new lang file added

---
+++ fr_utf8/repository_flickr_public.php
@@ -0,0 +1,17 @@
+<?php // $Id$
+
+$string['apikey'] = 'Clef API';
+$string['callbackurl'] = 'URL de rappel';
+$string['configplugin'] = 'Configuration Flickr public';
+$string['emailaddress'] = 'Adresse de courriel';
+$string['information'] = '<div style=\"font-size:0.8em\">Obtenir une <a href=\"http://www.flickr.com/services/api/keys/\">clef API Flickr</a> pour votre Moodle.</div>';
+$string['invalidemail'] = 'Adresse de courriel non valide pour flickr';
+$string['notitle'] = 'Sans titre';
+$string['nullphotolist'] = 'Il n\'y a pas de photo dans ce compte';
+$string['remember'] = 'Se souvenir de moi';
+$string['repositoryname'] = 'Flickr public';
+$string['repositorydesc'] = 'Dépôt sur flickr.com';
+$string['secret'] = 'Secret';
+$string['username'] = 'Courriel du compte Flickr';
+
+?>
