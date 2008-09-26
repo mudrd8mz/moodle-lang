a74e061bf9b6c901458c0c5cc4faa0d5dd91017c
Mitsuhiro Yoshida <mits>
2008-09-26 05:12:36 +0000

    MDL-15233 Translated new strings for answers tolerance modificaions.

--- ja_utf8/qtype_datasetdependent.php
+++ ja_utf8/qtype_datasetdependent.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // qtype_datasetdependent.php - created with Moodle 1.9 dev (2007052200)
+      // qtype_datasetdependent.php - created with Moodle 2.0 dev (Build: 20080924) (2008092400)
 
 
 $string['additem'] = 'アイテムを追加する';
+$string['answerstoleranceparam'] = '答えの許容誤差パラメータ';
 $string['atleastonerealdataset'] = '問題テキストの中には、少なくとも1つの実データセットを入れる必要があります。';
 $string['atleastonewildcard'] = '答えの公式または問題テキストの中には、少なくとも1つのワイルドカードを入れる必要があります。';
 $string['calcdistribution'] = '分布';
@@ -10,7 +11,7 @@ $string['calclength'] = '有効数字';
 $string['calcmax'] = '最大';
 $string['calcmin'] = '最小';
 $string['dataitemdefined'] = ': 利用可能な定義済み数値 $a 件';
-$string['datasetrole'] = 'ワイルドカード「<strong>{x..}</strong>」は、データセットの数値と置換されます';
+$string['datasetrole'] = 'ワイルドカード「<strong>{x..}</strong>」は、データセットの数値と置換されます。';
 $string['deletelastitem'] = '最後のアイテムを削除する';
 $string['existingcategory1'] = '文字 - このカテゴリ内の他の問題ですでに使用されている既存の文字セットより';
 $string['existingcategory2'] = 'ファイル - このカテゴリ内の他の問題ですでに使用されている既存のファイルセットより';
@@ -37,6 +38,7 @@ $string['newlocal1'] = '文字 - この問題のみで使用される新しい�
 $string['newlocal2'] = 'ファイル - この問題のみで使用される新しいファイルセットより';
 $string['newlocal3'] = 'リンク - この問題のみで使用される新しいリンクセットより';
 $string['nodataset'] = 'なし - これはワイルドカードではありません';
+$string['oneanswertrueansweroutsidelimits'] = '正解の範囲内で少なくとも1つの正解を表示する必要があります。<br />答えの許容誤差設定は、「拡張要素を表示する」ボタンをクリックすることで修正できます。';
 $string['param'] = '変数 {<strong>$a</strong>}';
 $string['replacewithrandom'] = 'ランダム値と置換する';
 $string['reuseifpossible'] = '可能な場合、前の値を再利用する';
@@ -44,6 +46,7 @@ $string['sharedwildcard'] = '共有ワイルドカード';
 $string['sharedwildcards'] = '共有ワイルドカード';
 $string['uniform'] = '一様分布';
 $string['updatedatasetparam'] = 'データセットパラメータを更新する';
+$string['updatetolerancesparam'] = '答えの許容誤差パラメータを更新する';
 $string['youmustaddatleastoneitem'] = 'この問題を保存する前に少なくとも1つのデータセットを追加してください。';
 
 ?>
