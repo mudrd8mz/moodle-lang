0e2142c9986059269fcb318e454b434da8f48f25
Ralf Hilgenstock <ralf-bonn>
2008-09-28 16:53:47 +0000

    *** empty log message ***

--- de_utf8/completion.php
+++ de_utf8/completion.php
@@ -1,11 +1,16 @@
 <?PHP // $Id$ 
-      // completion.php - created with Moodle 2.0 dev (Build: 20080822) (2008081900)
+      // completion.php - created with Moodle 2.0 dev (Build: 20080925) (2008092400)
 
 
+$string['activitycompletion'] = 'Bearbeitungsabschluß';
+$string['completedunlocked'] = 'Abschlußeinstellungen verfügbar';
+$string['completedwarning'] = 'Abschlußeinstellungen gesperrt';
+$string['completion'] = 'Fortschritte darstellen';
 $string['completion-alt-auto-n'] = 'Nicht abgeschlossen';
 $string['completion-alt-auto-y'] = 'Abgeschlossen';
 $string['completion-alt-manual-n'] = 'Nicht abgeschlossen';
 $string['completion-alt-manual-y'] = 'Abgeschlossen';
+$string['completionreport'] = 'Fortschrittsbericht';
 $string['csvdownload'] = 'Download in Tabellenformat (.csv, utf-8)';
 $string['excelcsvdownload'] = 'Download in Excel kompatiblem Textformat (.csv)';
 $string['progress-title'] = '$a->user, $a->activity: $a->state $a->date';
