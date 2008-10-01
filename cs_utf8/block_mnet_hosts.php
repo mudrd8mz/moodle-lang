ffaf1c003e342b6df60f1ce6e7b8a9e85ccbf6a0
David Mudrak <david.mudrak@gmail.com>
2008-10-01 23:04:18 +0000

    MDLCS-11: Fixed translation of MNet servers block
    The block is now called "Partnerske servery"

--- cs_utf8/block_mnet_hosts.php
+++ cs_utf8/block_mnet_hosts.php
@@ -1,8 +1,8 @@
 <?PHP // $Id$ 
-      // block_mnet_hosts.php - created with Moodle 1.8 + (2007021502)
+      // block_mnet_hosts.php - created with Moodle 1.9.2+ (Build: 20080925) (2007101523)
 
 
-$string['mnet_hosts'] = 'Síťové servery';
+$string['mnet_hosts'] = 'Partnerské servery';
 $string['server'] = 'Server';
 
 ?>
