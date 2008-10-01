f4cc432a304f85e45e07338ab1ada9ef8d34fa9c
David Mudrak <david.mudrak@gmail.com>
2008-10-01 22:50:58 +0000

    MDLCS-11 and MDL-16762: Fixed translation of MNet SSO/Enrolment
    As I propose in MDL-16762, I translate "Moodle Networking" as "MNet". Also, there were some critical mistranslations as well as some missing sentences.

--- cs_utf8/auth_mnet.php
+++ cs_utf8/auth_mnet.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // auth_mnet.php - created with Moodle 1.8 Beta (2007020400)
+      // auth_mnet.php - created with Moodle 1.9.2+ (Build: 20080925) (2007101523)
 
 
-$string['sso_idp_description'] = 'Publikujte tuto službu, abyste umožnili vašim uživatelům roaming na stránkách $a Moodle, aniž by se se zde odhlašovali a znovu přihlašovali. <ul><li><em>Závislost</em>: Musíte být <strong>registrováni</strong> také k poskytovateli služby SSO na $a.</li></ul><br />';
-$string['sso_idp_name'] = 'Poskytovatel identity SSO';
+$string['sso_idp_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, vaši uživatelé se budou moci přihlásit na vzdálený server $a, aniž by tam museli zadávat své jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj služeb\".</li></ul><br />Pokud budete <strong>využívat</strong>tuto službu, budou se moci vzdálení uživatelé ze serveru $a přihlásit na vašem serveru, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj služby\"  pro server $a. </li></ul>';
+$string['sso_idp_name'] = 'SSO - zdroj identit uživatelů';
 $string['sso_mnet_login_refused'] = 'Uživatel $a[0] nemá povoleno přihlašovat se z $a[1].';
-$string['sso_sp_description'] = 'Publikujte tuto službu, abyste umožnili vašim přihlášeným uživatelům přistupovat na vaše stránky, aniž by se se zde odhlašovali a znovu přihlašovali. <ul><li><em>Závislost</em>: Musíte být <strong>registrováni</strong> také k poskytovateli služby SSO na $a.</li></ul><br />';
-$string['sso_sp_name'] = 'Poskytovatel služby SSO';
+$string['sso_sp_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, budou se moci vzdálení uživatelé ze serveru $a přihlásit na vašem serveru, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj identit uživatelů\".</li></ul><br />Pokud budete <strong>využívat</strong>tuto službu, budou se moci vaši uživatelé přihlásit na vzdálený server $a, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj identit uživatelů\" pro server $a. </li></ul>';
+$string['sso_sp_name'] = 'SSO - zdroj služby';
 
 ?>
--- cs_utf8/enrol_mnet.php
+++ cs_utf8/enrol_mnet.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_mnet.php - created with Moodle 1.9 Beta 2 (2007101504)
+      // enrol_mnet.php - created with Moodle 1.9.2+ (Build: 20080925) (2007101523)
 
 
 $string['allcategories'] = '$a možných kategorií';
@@ -8,12 +8,15 @@ $string['allow_allcourses'] = 'Povolit zápis do všech vzdálených kurzů.';
 $string['allowedcategories'] = '$a povolených kategorií';
 $string['allowedcourses'] = '$a povolených kurzů';
 $string['allowedcourseslinktext'] = 'Zde upravte povolené kurzy a kategorie.';
-$string['description'] = 'Popis zapisování přes Moodle Networking.';
-$string['enrolname'] = 'Moodle Networking';
-$string['mnet_enrol_description'] = 'Publikujte tuto službu, abyste umožnili správcům u $a zapisovat své studenty do kurzů, které jste vytvořili na vašem serveru. <br/><ul><li><em>Závislost</em>: musíte také <strong>publikovat</strong> službu SSO (poskytovatel služby) pro $a.</li><li><em>Závislost</em>: musíte také být <strong>registrováni</strong> pro službu SSO (poskytovatel identity) na $a.</li></ul><br/>Registrujte se v této službě, abyste umožnili vašim studentům zapisovat se do kurzů na $a.<br/><ul><li><em>Závislost</em>: musíte také být <strong>registrováni</strong> pro službu SSO (poskytovatel služby)na $a.</li><li><em>Závislost</em>:musíte také   <strong>publikovat</strong> službu SSO (poskytovatel identity)pro $a.</li></ul><br/>';
-$string['mnet_enrol_name'] = 'Zapisování přes síť Moodle serverů';
+$string['description'] = 'Popis zápisu do kurzů přes MNet.';
+$string['enrolname'] = 'MNet';
+$string['mnet_enrol_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, budou moci správci na serveru $a zapisovat jejich studenty do kurzů na vašem serveru. <br/>
+<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj služby\" pro server $a.</li><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj identit uživatelů\".</li></ul><br />
+Pokud budete <strong>využívat</strong>tuto službu, budete moci vaše vlastní uživatele zapisovat do kurzů na serveru $a.<br/><ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj služeb\".</li><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj identit uživatelů\" pro server $a.</li></ul>
+<br/>';
+$string['mnet_enrol_name'] = 'MNet';
 $string['mnetlocalforexternal'] = 'Místní kurzy pro externí uživatele';
-$string['nocategoriesdefined'] = 'Nebyly nalezeny kategorie kurzů. Definujte nové kategorie <a href=\"$a\">zde</a>.';
-$string['nocoursesdefined'] = 'Nebyly nalezeny kurzy. Definujte nové  kurzy <a href=\"$a\">zde</a>.';
+$string['nocategoriesdefined'] = 'Nebyly nalezeny žádné kategorie kurzů. Definujte nové kategorie <a href=\"$a\">zde</a>.';
+$string['nocoursesdefined'] = 'Nebyly nalezeny žádné kurzy. Vytvořte nové kurzy <a href=\"$a\">zde</a>.';
 
 ?>
