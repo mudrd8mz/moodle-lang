00a0104c0b424d8cbb19bdd27ca5a2ed5f367c00
Benito Arias <barias>
2008-10-03 18:41:10 +0000

    *** empty log message ***

---
+++ es_utf8/repository_upload.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // repository_upload.php - created with Moodle 1.8.2+ (2007021520)
+
+
+$string['configplugin'] = 'Configuración para \'plugin\' de subida';
+$string['repositorydesc'] = 'Subir un archivo a Moodle';
+$string['repositoryname'] = 'Subir un archivo';
+
+?>
