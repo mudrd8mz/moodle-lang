9aa98c6a9d188a9f4577d00bd9c3911e3c6d8a81
Nicolas Martignoni <martignoni>
2008-10-05 08:08:31 +0000

    MDL-15402 strings added, corrected and sorted

--- fr_utf8/repository_boxnet.php
+++ fr_utf8/repository_boxnet.php
@@ -1,15 +1,16 @@
 <?php // $Id$
 
 $string['apikey'] = 'Clef API';
-$string['repositoryname'] = 'Box.net';
-$string['repositorydesc'] = 'Dépôt sur Box.net';
 $string['configplugin'] = 'Configuration Box.net';
-$string['username'] = 'Nom d\'utilisateur Box.net';
-$string['password'] = 'Mot de passe Box.net';
-$string['saved'] = 'Données Box.net enregistrées';
+$string['information'] = '<div style=\"font-size:0.8em\">Obtenir une <a href=\"http://enabled.box.net/\">clef API Box.net</a> pour votre Moodle.</div>';
 $string['invalidpassword'] = 'Mot de passe non valide';
-$stirng['invalidtoken'] = 'Jeton d\'authentification non valide';
+$string['invalidtoken'] = 'Jeton d\'authentification non valide';
 $string['nullfilelist'] = 'Il n\'y a aucun fichier dans ce dépôt';
+$string['password'] = 'Mot de passe Box.net';
+$string['repositorydesc'] = 'Dépôt sur Box.net';
+$string['repositoryname'] = 'Box.net';
+$string['saved'] = 'Données Box.net enregistrées';
 $string['shareurl'] = 'Partager l\'URL';
+$string['username'] = 'Nom d\'utilisateur Box.net';
 
 ?>
