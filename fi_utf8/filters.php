8fd99d102093716f6f7e6db3f14a1d7996788dd7
Petri Asikainen <paca70>
2008-10-11 17:56:46 +0000

    Updates from Maija Mustonen

---
+++ fi_utf8/filters.php
@@ -0,0 +1,21 @@
+<?php
+$string['actfilterhdr'] = 'Käytössä olevat suotimet';
+$string['addfilter'] = 'Lisää suodin';
+$string['anycategory'] = 'Mikä tahansa kategoria';
+$string['anyrole'] = 'Mikä tahansa rooli';
+$string['contains'] = 'sisältää';
+$string['doesnotcontain'] = 'ei sisällä';
+$string['endswith'] = 'loppuu';
+$string['firstaccess'] = 'Ensimmäinen kirjautuminen';
+$string['globalrolelabel'] = '$a->label on $a->value';
+$string['isafter'] = 'jälkeen';
+$string['isanyvalue'] = 'Mikä tahansa';
+$string['isbefore'] = 'ennen';
+$string['isempty'] = 'on tyhjä';
+$string['isequalto'] = 'on yhtä kuin';
+$string['isnotequalto'] = 'on eri kuin';
+$string['newfilter'] = 'Uusi suodin';
+$string['removeall'] = 'Poista kaikki suotimet';
+$string['removeselected'] = 'Poista valitut';
+$string['startswith'] = 'alkaa';
+?>
---
+++ fi_utf8/qtype_truefalse.php
@@ -0,0 +1,11 @@
+<?php 
+$string['addingtruefalse'] = 'Lisätään tosi/epätosi-tehtävä';
+$string['correctanswer'] = 'Oikea vastaus';
+$string['editingtruefalse'] = 'Muokataan tosi/epätosi-tehtävää';
+$string['false'] = 'Epätosi';
+$string['feedbackfalse'] = 'Palaute vastaukselle \'Epätosi\'.';
+$string['feedbacktrue'] = 'Palaute vastaukselle \'Tosi\'.';
+$string['true'] = 'Tosi';
+$string['truefalse'] = 'Tosi/Epätosi';
+?>
+
