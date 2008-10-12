b4530682f667f5b416d2583150056a9d31f23b72
Nicolas Martignoni <martignoni>
2008-10-12 20:47:48 +0000

    MDL-16598  Strings added

--- fr_utf8/enrol_manual.php
+++ fr_utf8/enrol_manual.php
@@ -7,6 +7,8 @@ $string['description'] = 'Contrôle des inscriptions par défaut. Un étudiant p
 <li>un enseignant ou l\'administrateur peut l\'inscrire manuellement en utilisant le lien disponible dans le menu d\'administration du cours&nbsp;;</li>
 <li>on peut définir dans un cours une «&nbsp;clef d\'inscription&nbsp;» (une sorte de mot de passe). Quiconque possède cette clef a la possibilité de s\'inscrire soi-même à ce cours.</li>
 </ul>';
+$string['enrol_manual_showhint'] = 'Activez ce réglage pour révéler le premier caractère de la clef d\'inscription à titre d\'indication, quand un utilisateur saisit une clef incorrecte.';
+$string['enrolmentkeyerror'] = 'La clef d\'inscription n\'est pas correcte. Veuillez essayez à nouveau.';
 $string['keyholderrole' ] = 'Le rôle de l\'utilisateur détenant la clef d\'inscription d\'un cours. Affiché pour les étudiants tentant de s\'inscrire au cours.';
 
 ?>
