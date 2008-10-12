61853efaa8f3b62bd10cd6a4668f24e9db1b5bf5
Ethem Evlice <ethem>
2008-10-12 11:34:46 +0000

    New files and new strings

---
+++ tr_utf8/messageprocessor_jabber.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // messageprocessor_jabber.php - created with Moodle 2.0 dev (Build: 20081012) (2008101000)
+
+
+$string['jabber'] = 'Jabber mesajı';
+$string['jabberid'] = 'Jabber ID';
+
+?>
---
+++ tr_utf8/messageprocessor_popup.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // messageprocessor_popup.php - created with Moodle 2.0 dev (Build: 20081012) (2008101000)
+
+
+$string['popup'] = 'Açılır pencere';
+
+?>
