18cc6003b5b4330d0a817b445da583b606fb5b8b
Nicolas Martignoni <martignoni>
2008-10-14 19:29:02 +0000

    MDL-14129, string added

--- fr_utf8/quiz_responses.php
+++ fr_utf8/quiz_responses.php
@@ -1,11 +1,12 @@
 <?php // $Id$
 
+$string['cannotloadoptions'] = 'Impossible de charger les options de question';
+$string['pagesize'] = 'Taille de page';
+$string['reportresponses'] = 'Réponses';
 $string['responses'] = 'Réponses';
 $string['responsesdownload'] = 'Téléchargement des réponses';
 $string['responsesoptions'] = 'Options des réponses';
 $string['responsesreport'] = 'Rapport de réponses';
 $string['responsestitle'] = 'Réponses';
-$string['pagesize'] = 'Taille de page';
-$string['reportresponses'] = 'Réponses';
 
 ?>
