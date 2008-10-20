bb7ec8b80c987b1860a1f68a4610e4c1ce384ea5
Mitsuhiro Yoshida <mits>
2008-10-20 15:29:23 +0000

    MDL-16907 Adding a translated lang file for smb repository.

---
+++ ja_utf8/repository_smb.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // repository_smb.php - created with Moodle 2.0 dev (Build: 20081014) (2008101300)
+
+
+$string['repositoryname'] = 'SMBリポジトリ';
+$string['smb_server'] = 'SMBサーバ';
+
+?>
