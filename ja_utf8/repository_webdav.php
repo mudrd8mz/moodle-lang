249be6ba38900f891f12badc95a35c5b90933c08
Mitsuhiro Yoshida <mits>
2008-10-21 07:01:00 +0000

    MDL-16908 Adding a translated lang file for webdav repository.

---
+++ ja_utf8/repository_webdav.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // repository_webdav.php - created with Moodle 2.0 dev (Build: 20081014) (2008101300)
+
+
+$string['repositoryname'] = 'WebDAVリポジトリ';
+$string['webdav_password'] = 'WebDAVサーバパスワード';
+$string['webdav_port'] = 'WebDAVサーバポート';
+$string['webdav_server'] = 'WebDAVサーバ';
+$string['webdav_user'] = 'WebDAVサーバユーザ';
+
+?>
