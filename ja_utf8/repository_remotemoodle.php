1b4ff1f39e9af27485659fc58bd162fd735dde28
Mitsuhiro Yoshida <mits>
2008-10-22 04:41:13 +0000

    MDL-15352 Adding a translated lang file for remote moodle.

---
+++ ja_utf8/repository_remotemoodle.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // repository_remotemoodle.php - created with Moodle 2.0 dev (Build: 20081014) (2008101300)
+
+
+$string['configplugin'] = 'リモートMoodleファイルリポジトリの設定';
+$string['emptyfilelist'] = '表示するファイルはありません';
+$string['notitle'] = 'タイトルなし';
+$string['peer'] = 'ピア';
+$string['remember'] = '次回から入力を省略する';
+$string['repositorydesc'] = 'リモートMoodleサーバのリポジトリ';
+$string['repositoryname'] = 'リモートMoodleファイル';
+
+?>
