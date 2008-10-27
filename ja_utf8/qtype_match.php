c3540496595f857bbe4e43eef8b05c7996515cf6
Mitsuhiro Yoshida <mits>
2008-10-27 06:15:59 +0000

    MDL-16450 Translated new strings for matching questions. MDL-17014 Translated a new string for alfresco plugin. MDL-15352 Translated a new string for remote moodle.

--- ja_utf8/qtype_match.php
+++ ja_utf8/qtype_match.php
@@ -1,9 +1,11 @@
 <?PHP // $Id$ 
-      // qtype_match.php - created with Moodle 1.8 dev (2007010404)
+      // qtype_match.php - created with Moodle 2.0 dev (Build: 20081014) (2008101300)
 
 
 $string['addmoreqblanks'] = 'さらに {no} 件の問題入力欄を追加する';
-$string['nomatchinganswerforq'] = 'この問題の答えを入力してください。';
-$string['notenoughquestions'] = '少なくとも $a 件の問題および答えの組み合わせを入力してください。';
+$string['filloutthreeqsandtwoas'] = 'あなたは、少なくとも2つの問題および3つの答えを提供する必要があります。答えが空白の問題を使用して、誤答を余分に提供することもできます。問題および答えが空白のエントリは、無視されます。';
+$string['nomatchinganswerforq'] = 'あなたは、この問題の答えを指定する必要があります。';
+$string['notenoughqsandas'] = 'あなたは、少なくとも $a->q 件の問題および $a->a 件の答えを提供する必要があります。';
+$string['notenoughquestions'] = 'あなたは、少なくとも $a 件の問題および答えの組み合わせを提供する必要があります。';
 
 ?>
--- ja_utf8/repository_alfresco.php
+++ ja_utf8/repository_alfresco.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // repository_alfresco.php - created with Moodle 2.0 dev (Build: 20080917) (2008091611)
+      // repository_alfresco.php - created with Moodle 2.0 dev (Build: 20081014) (2008101300)
 
 
 $string['alfresco_url'] = 'Alfresco URI';
@@ -8,6 +8,7 @@ $string['notitle'] = 'タイトルなし';
 $string['password'] = 'パスワード';
 $string['repositorydesc'] = 'Alfresco CMSのプラグインです。';
 $string['repositoryname'] = 'Alfrescoリカバリ';
+$string['soapmustbeenabled'] = 'alfrescoプラグインのため、SOAP拡張モジュールを有効にする必要があります。';
 $string['username'] = 'ユーザ名';
 
 ?>
