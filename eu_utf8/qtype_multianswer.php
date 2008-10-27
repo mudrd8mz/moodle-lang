b2e07299b139e70372b50868f078000e9996651f
Abel Camacho <ueu_ueu>
2008-10-27 16:49:50 +0000

    New strings added

--- eu_utf8/qtype_multianswer.php
+++ eu_utf8/qtype_multianswer.php
@@ -1,8 +1,15 @@
 <?PHP // $Id$ 
-      // qtype_multianswer.php - created with Moodle 1.9 + (Build: 20080409) (2007101509)
+      // qtype_multianswer.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
 
 
 $string['correctanswer'] = 'Erantzun zuzena';
+$string['correctanswerandfeedback'] = 'Erantzun zuzena eta feedbacka';
 $string['decodeverifyquestiontext'] = 'Galderaren testua deskodetu eta egiaztatu';
+$string['nooptionsforsubquestion'] = 'Ezin da aukerarik lortu  # $a->sub galderaren zatirako (galdera->id={$a->id})';
+$string['noquestions'] = 'Erantzun anitzeko \"<strong>$a</strong>\" galderak ez du galderarik';
+$string['qtypenotrecognized'] = '$a galdera-mota ezezaguna da';
+$string['questionnotfound'] = 'Ezin da aurkitu #$a galdera-zatiaren galdera';
+$string['questionsmissing'] = 'Ez dago balio duen galderarik, sortu gutxienez galdera bat';
+$string['unknownquestiontypeofsubquestion'] = 'Galdera-mota ezezaguna: # $a->sub galdera-zatiaren $a->type';
 
 ?>
--- eu_utf8/quiz_overview.php
+++ eu_utf8/quiz_overview.php
@@ -1,12 +1,31 @@
 <?PHP // $Id$ 
-      // quiz_overview.php - created with Moodle 1.6.1 (2006050510)
+      // quiz_overview.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
 
 
 $string['allattempts'] = 'Saiakera guztiak erakutsi';
+$string['allattemptscontributetograde'] = 'Saiakera guztiak hartzen dira erabiltzailearen azken kalifikaziorako.';
 $string['allstudents'] = '$a guztiak erakutsi';
 $string['attemptsonly'] = 'Saiakerak dituen $a baino ez erakutsi';
-$string['noattemptsonly'] = 'Saiakerarik ez duen $a baino ez erakutsi';
+$string['attemptsprepage'] = 'Orriko erakutsitako saiakerak';
+$string['deleteselected'] = 'Ezabatu aukeratutako saiakerak';
+$string['highlightinggraded'] = 'Erabiltzailearen azken kalifikaziorako erabiliko den saiakera nabarmenduta dago.';
+$string['noattemptsonly'] = 'Saiakerarik ez duen $a baino ez erakutsi / jaitsi';
+$string['onlyoneattemptallowed'] = 'Galdetegi honetan erabiltzailearen saiakera bat baino ez da agertzen';
+$string['optallattempts'] = 'saiakera guztiak';
+$string['optallstudents'] = '\'$a\' guztiak';
+$string['optattemptsonly'] = 'Saiakerak dituzten \'$a\' baino ez';
+$string['optnoattemptsonly'] = 'Saiakerarik ez duten \'$a\' bakarrik';
+$string['optonlygradedattempts'] = 'erabiltzaile bakoitzarentzat kalifikatutako saiakera bakarrik ($a)';
 $string['overview'] = 'Ikuspegi orokorra';
 $string['overviewdownload'] = 'Ikuspegi orokorra jaitsi';
+$string['overviewreportgraph'] = 'Kalifikazio-tarte ezberdinak lortzen dituzten ikasleen barra-diagrama';
+$string['pagesize'] = 'Orriaren tamaina';
+$string['preferencespage'] = 'Zure hobespenak orri honetarako';
+$string['preferencessave'] = 'Hobespenak gorde';
+$string['preferencesuser'] = 'Zure hobespenak txosten honetarako';
+$string['show'] = 'Erakutsi / jaitsi';
+$string['showdetailedmarks'] = 'Erakutsi / jaitsi galdera bakoitzerako markak';
+$string['showinggraded'] = 'Erabiltzaile bakoitzarentzat kalifikatutako saiakera bakarrik erakusten.';
+$string['showinggradedandungraded'] = 'Erabiltzaile bakoitzarentzat kalifikatutako eta kalifikatu gabeko saiakerak erakusten. Erabiltzaile bakoitzarentzat kalifikatutako saiakera bakarra nabarmenduta dago. Galdetegi honetan kalifikazio-metodoa $a da.';
 
 ?>
