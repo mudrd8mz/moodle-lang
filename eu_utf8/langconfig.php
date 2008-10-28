155f2d943d4faca20f85ed2362c5709926d0f86e
Abel Camacho <ueu_ueu>
2008-10-28 15:52:55 +0000

    Modification 28-10-2008

--- eu_utf8/langconfig.php
+++ eu_utf8/langconfig.php
@@ -1,5 +1,5 @@
 <?php
-$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ñ,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+$string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,Ã‘,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['firstdayofweek'] = '1'; // means monday
 $string['locale'] = 'eu_ES.UTF-8';
