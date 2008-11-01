c61802a0d55d1f013fbe0f98626b8e759f1fc3da
Nicolas Martignoni <martignoni>
2008-11-01 15:49:01 +0000

    MDL-15352, new file added

---
+++ fr_utf8/repository_remotemoodle.php
@@ -0,0 +1,19 @@
+<?php // $Id$
+
+$string['configplugin'] = 'Configuration du dépôt de fichiers distant Moodle';
+$string['repositoryname'] = 'Fichiers sur Moodle distant';
+$string['repositorydesc'] = 'Dépôt sur un serveur Moodle distant';
+$string['notitle'] = 'Sans titre';
+$string['peer'] = 'Pair';
+$string['remember'] = 'Se souvenir de moi';
+$string['emptyfilelist'] = 'Aucun fichier à afficher';
+$string['remoterep_name'] = 'Dépôt Moodle distant';
+$string['remoterep_description'] = 'Permettre la découverte du service<br /><br />';
+$string['connectionfailure'] = 'Échec de la récupération de la liste des fichiers. Soit le Moodle est d\'une version antérieure à la 2.0, soit son service Dépôt Moodle distant n\'a pas été activé';
+$string['failtoretrievelist'] = 'La liste de fichiers n\'a pas pu être récupérée ou est vide';
+$string['usernotfound'] = 'L\'utilisateur $a n\'est pas enregistré dans le Moodle distant';
+$string['usercannotaccess'] = 'Vous ($a) ne pouvez pas accéder à ce fichier';
+$string['nopeer'] = '<div style=\"font-size:0.8em\">Veuillez configurer un <a href=\"http://docs.moodle.org/en/admin/mnet/peers#Peer_to_Peer_Network\">serveur Moodle pair</a> pour votre site Moodle.</div>';
+$string['hostnotfound'] = 'Impossible de trouver le Moodle distant dans la base de données. Veuillez contacter l\'administrateur du serveur';
+
+?>
