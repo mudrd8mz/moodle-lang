cde70e2cab2f1224f496ffa6556e8bc81a6f628e
Nicolas Martignoni <martignoni>
2008-11-01 15:40:53 +0000

    MDL-16908, new lang file added

---
+++ fr_utf8/repository_webdav.php
@@ -0,0 +1,9 @@
+<?php // $Id$
+
+$string['repositoryname'] = 'Dépôt WebDAV';
+$string['webdav_server'] = 'Serveur WebDAV';
+$string['webdav_port'] = 'Port du serveur WebDAV';
+$string['webdav_user'] = 'Utilisateur WebDAV';
+$string['webdav_password'] = 'Mot de passe WebDAV';
+
+?>
