dbb59d61086eda47226060b7a9aaafa8b6895ed9
Lee Chan-young <yogibear270>
2008-11-13 02:56:24 +0000

    translate added strings.

--- ko_utf8/qtype_multianswer.php
+++ ko_utf8/qtype_multianswer.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // qtype_multianswer.php - created with Moodle 2.0 dev (Build: 20080909) (2008090800)
+      // qtype_multianswer.php - created with Moodle 2.0 dev (Build: 20081113) (2008111100)
 
 
 $string['addingmultianswer'] = '답 내장형(Cloze) 추가';
@@ -7,10 +7,16 @@ $string['correctanswer'] = '정답';
 $string['correctanswerandfeedback'] = '정답 및 피드백';
 $string['decodeverifyquestiontext'] = '문항 복호 및 검증';
 $string['editingmultianswer'] = '답 내장형(Cloze) 문항 편집';
+$string['layout'] = '서식';
+$string['layouthorizontal'] = '라디오 버튼의 수편 배치';
+$string['layoutselectinline'] = '문서내 펼침 목록';
+$string['layoutundefined'] = '정의되지 않은 서식';
+$string['layoutvertical'] = '라디오 버튼의 수직 배치';
 $string['multianswer'] = '답 내장형(Cloze)';
 $string['nooptionsforsubquestion'] = '문항영역 # $a->sub (question->id={$a->id})에서 설정을 탑재할 수 없음';
 $string['noquestions'] = '답 내장형(복수답) \"<strong>$a</strong>\" 의 질문이 없음';
 $string['qtypenotrecognized'] = '문제 유형 $a 를 인식할 수 없음';
+$string['questiondefinition'] = '문항의 정의';
 $string['questionnotfound'] = '문항영역 #$a 에서 문제를 찾을 수 없음';
 $string['questionsmissing'] = '문항 없음. 문제를 만들어야 함';
 $string['unknownquestiontypeofsubquestion'] = '알 수 없는 문제 유형: 문항영역 # $a->sub 의 $a->type';
