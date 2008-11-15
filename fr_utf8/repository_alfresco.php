0d3014e53117b37841f64c7049580c0118b6a478
Nicolas Martignoni <martignoni>
2008-11-15 19:34:11 +0000

    MDL-17014, string translated

--- fr_utf8/repository_alfresco.php
+++ fr_utf8/repository_alfresco.php
@@ -1,11 +1,12 @@
 <?php // $Id$
 
+$string['alfresco_url'] = 'URL Alfresco';
 $string['configplugin'] = 'Configuration Alfresco';
 $string['notitle'] = 'Sans titre';
-$string['repositoryname'] = 'Dépôt Alfresco';
-$string['repositorydesc'] = 'Un plugin pour le CMS Alfresco';
-$string['username'] = 'Nom d\'utilisateur';
 $string['password'] = 'Mot de passe';
-$string['alfresco_url'] = 'URL Alfresco';
+$string['repositorydesc'] = 'Un plugin pour le CMS Alfresco';
+$string['repositoryname'] = 'Dépôt Alfresco';
+$string['soapmustbeenabled'] = 'L\'extension SOAP de PHP doit être activée pour utiliser le plugin alfresco';
+$string['username'] = 'Nom d\'utilisateur';
 
 ?>
