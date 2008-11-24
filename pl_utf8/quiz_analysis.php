f0dda65f045c70da583c1c83407a320904d29434
Adam Pawelczak <adamtpaw>
2008-11-24 08:32:26 +0000

    Update of quiz_analysis

--- pl_utf8/quiz_analysis.php
+++ pl_utf8/quiz_analysis.php
@@ -1,37 +1,37 @@
 <?PHP // $Id$ 
-      // quiz_analysis.php - created with Moodle 1.9.3 (Build: 20081015) (2007101530)
+      // quiz_analysis.php - created with Moodle 1.9.3+ (Build: 20081112) (2007101531)
 
 
-$string['analysis'] = 'Pozycja analizy';
+$string['analysis'] = 'Analiza wyników';
 $string['analysisdownload'] = 'Pobierz analizę';
 $string['analysisoptions'] = 'Opcje analizy';
-$string['analysistitle'] = 'Pozycje tablicy analizy';
+$string['analysistitle'] = 'Analiza pytań';
 $string['attemptsall'] = 'Wszystkie próby';
 $string['attemptselection'] = 'Próby wybrane przez użytkownika';
 $string['attemptsfirst'] = 'Pierwsza próba';
 $string['attemptshighest'] = 'Najwyższy stopień';
 $string['attemptslast'] = 'Ostatnia próba';
-$string['dicsindextitle'] = 'Dysk. <br/> Indeks';
-$string['disccoefftitle'] = 'Dysk. <br/> Współczynnik';
+$string['dicsindextitle'] = 'Wskaźnik<br/>dyskrym. (DI)';
+$string['disccoefftitle'] = 'Współczynnik<br/>dyskrym. (DC)';
 $string['downloadooo'] = 'Pobierz w formacie OpenOffice';
-$string['facilitytitle'] = '%% Poprawność<br> Ułatwienia';
+$string['facilitytitle'] = 'Łatwość [%%]';
 $string['lowmarkslimit'] = 'Nie analizuj jeżeli liczba punktów jest niższa niż:';
 $string['pagesize'] = 'Pytań na stronę';
 $string['qcounttitle'] = 'Licznik pytań';
-$string['qidtitle'] = 'Q#';
+$string['qidtitle'] = 'Nr';
 $string['qnametitle'] = 'Nazwa pytania';
 $string['qtexttitle'] = 'Treść pytania';
 $string['qtypetitle'] = 'Typ pytania';
 $string['quizreportdir'] = 'Raport';
-$string['rcounttitle'] = 'R.<br/> Liczby';
-$string['reportanalysis'] = 'Pozycje raportu analizy';
+$string['rcounttitle'] = 'Częstość';
+$string['reportanalysis'] = 'Raport wyników';
 $string['responsestitle'] = 'Odpowiedź';
-$string['rfractiontitle'] = 'Cześciowy <br/> kredyt';
-$string['rpercenttitle'] = 'R. %%';
-$string['stddevtitle'] = 'SD';
+$string['rfractiontitle'] = 'Punktacja';
+$string['rpercenttitle'] = 'Częstość <br/> [%%]';
+$string['stddevtitle'] = 'Odch.std.';
 $string['categorydoesnotexist'] = 'Ta kategoria nie istnieje'; // ORPHANED
 $string['editingquestion'] = 'Edycja pytania'; // ORPHANED
-$string['fractionsnomax'] = 'Jedna z odpowiedzi powinna być warta 100%%%%%%%%, aby można było otrzymać całą ocenę za to pytanie.'; // ORPHANED
+$string['fractionsnomax'] = 'Jedna z odpowiedzi powinna być warta 100%%%%%%%%%%%%%%%%, aby można było otrzymać całą ocenę za to pytanie.'; // ORPHANED
 $string['missingimportantcode'] = 'W pytaniu pojawił się błąd kodu: $a.'; // ORPHANED
 $string['notenoughdatatoeditaquestion'] = 'Ani ID pytania ani ID kategorii i typ pytania nie zostały określone.'; // ORPHANED
 $string['questionbank'] = 'Baza pytań'; // ORPHANED
