7b10bd8a3802cf06655c329d7fbe2f4dc6236525
Bente Olsen <benteo>
2008-11-27 20:42:50 +0000

    New file

---
+++ da_utf8/auth_mnet.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.9.3+ (Build: 20081025) (2007101530)
+
+
+$string['sso_mnet_login_refused'] = 'Brugeren $a[0] har ikke tilladelse til at logge ind fra $a[1].';
+
+?>
---
+++ da_utf8/block_tag_youtube.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // block_tag_youtube.php - created with Moodle 1.9.3+ (Build: 20081025) (2007101530)
+
+
+$string['anycategory'] = 'Enhver kategori';
+$string['autosvehicles'] = 'Biler og køretøjer';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'Kategori';
+$string['comedy'] = 'Komedie';
+$string['configtitle'] = 'Titel';
+$string['entertainment'] = 'Underholdning';
+$string['filmsanimation'] = 'Film og animation';
+$string['gadgetsgames'] = 'Gadges og spil';
+$string['howtodiy'] = 'Vejledninger og gør-det-selv';
+$string['includeonlyvideosfromplaylist'] = 'Medtag kun videoer fra spillelisten med id';
+$string['music'] = 'Musik';
+$string['newspolitics'] = 'Nyheder og politik';
+$string['numberofvideos'] = 'Antal videoer';
+$string['peopleblogs'] = 'Mennesker og Blogs';
+$string['petsanimals'] = 'Dyr og kæledyr';
+$string['sports'] = 'Sport';
+$string['travel'] = 'Rejser og steder';
+
+?>
