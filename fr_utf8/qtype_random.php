20e4df7d1a2231fb54b56d1a74bfe51cfd9aad65
Nicolas Martignoni <martignoni>
2008-12-07 14:50:28 +0000

    MDL-8648 New lang file added

---
+++ fr_utf8/qtype_random.php
@@ -0,0 +1,6 @@
+<?php // $Id$
+
+$string['configselectmanualquestions'] = 'Le type de question aléatoire peut-il choisir des questions évaluées manuellement lorsqu\'il tire aléatoirement une question d\'une catégorie&nbsp;?';
+$string['selectmanualquestions'] = 'Les questions aléatoires peuvent utiliser les questions évaluées manuellement';
+
+?>
