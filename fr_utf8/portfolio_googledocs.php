d0e58319670bd065c6a51f8830c2d1498d2f46e4
Nicolas Martignoni <martignoni>
2008-12-07 15:13:06 +0000

    MDL-16417 New lang files translated

---
+++ fr_utf8/portfolio_googledocs.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+
+$string['pluginname'] = 'Google Docs';
+$string['noauthtoken'] = 'Aucun jeton d\'authentification n\'a été reçu de Google. Veuillez vous assurer que vous autorisez vraiment Moodle à accéder à votre compte Google';
+$string['nosessiontoken'] = 'Il n\'y a pas de jeton de session, ce qui empêche l\'exportation vers Google.';
+$string['sendfailed'] = 'Échec du transfert du fichier $a vers Google';
+
+?>
---
+++ fr_utf8/portfolio_picasa.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+
+$string['pluginname'] = 'Picasa';
+$string['noauthtoken'] = 'Aucun jeton d\'authentification n\'a été reçu de Google. Veuillez vous assurer que vous autorisez vraiment Moodle à accéder à votre compte Google';
+$string['sendfailed'] = 'Échec du transfert du fichier $a vers Picasa';
+
+?>
