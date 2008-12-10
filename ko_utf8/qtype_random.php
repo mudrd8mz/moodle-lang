f56766078d806809fb78617206531c198de4939b
Lee Chan-young <yogibear270>
2008-12-10 01:29:04 +0000

    translate added strings.

---
+++ ko_utf8/qtype_random.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qtype_random.php - created with Moodle 2.0 dev (Build: 20081210) (2008120801)
+
+
+$string['configselectmanualquestions'] = '무작위 문항을 선제할 때 범주내 수동 채점문항을 선택할 수 있게 할 것인가?';
+$string['selectmanualquestions'] = '무작위 문항은 수동 채점 문항들을 이용할 수 있음';
+
+?>
