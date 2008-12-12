7ea6fe0041cd817482254c7ed8d415d578117d8f
Adam Pawelczak <adamtpaw>
2008-12-12 11:36:55 +0000

    New files for version 1.9.3

---
+++ pl_utf8/gradereport_grader.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // gradereport_grader.php - created with Moodle 1.9.3 (Build: 20081015) (2007101530)
+      // local modifications from http://www.translation.e-wsb.pl
+
+
+$string['grader:manage'] = 'Zarządzaj dziennikiem ocen';
+$string['grader:view'] = 'Zobacz dziennik ocen';
+$string['modulename'] = 'Dziennik ocen';
+
+?>
---
+++ pl_utf8/gradereport_user.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // gradereport_user.php - created with Moodle 1.9.3 (Build: 20081015) (2007101530)
+      // local modifications from http://www.translation.e-wsb.pl
+
+
+$string['modulename'] = 'Twoje oceny';
+$string['user:view'] = 'Zobacz swoje oceny w dzienniku ocen';
+
+?>
