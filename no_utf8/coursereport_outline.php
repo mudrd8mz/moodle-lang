2b1e66b5eaef37b4c056780f77b20bdf98eb99d6
John Harald Gartland <johnhg>
2008-12-15 13:20:07 +0000

    New file 1.9.3+

---
+++ no_utf8/coursereport_outline.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // coursereport_outline.php - created with Moodle 1.9.3+ (Build: 20081210) (2007101532.1)
+
+
+$string['outline:view'] = 'Se rapport over kursaktivitet';
+
+?>
---
+++ no_utf8/coursereport_stats.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // coursereport_stats.php - created with Moodle 1.9.3+ (Build: 20081210) (2007101532.1)
+
+
+$string['stats:view'] = 'Se rapport over kursstatistikk';
+
+?>
