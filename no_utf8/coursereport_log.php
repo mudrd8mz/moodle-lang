163b7c0169f231468d0c8e63535573576869dca6
John Harald Gartland <johnhg>
2008-12-15 12:18:40 +0000

    New file 1.9.3+

---
+++ no_utf8/coursereport_log.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // coursereport_log.php - created with Moodle 1.9.3+ (Build: 20081210) (2007101532.1)
+
+
+$string['log:view'] = 'Se kurslogger';
+$string['log:viewlive'] = 'Se livelogger';
+$string['log:viewtoday'] = 'Se dagens logger';
+$string['loglive'] = 'Livelogger';
+
+?>
