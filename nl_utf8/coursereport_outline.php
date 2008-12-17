4e18875fac18bc1e067b7d68516521a42b4b0030
koen roggemans <koenr>
2008-12-17 09:10:47 +0000

    new strings

---
+++ nl_utf8/coursereport_outline.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // coursereport_outline.php - created with Moodle 2.0 dev (Build: 20081216) (2008121000)
+
+
+$string['outline:view'] = 'Bekijk cursusactiviteitenrapport';
+
+?>
