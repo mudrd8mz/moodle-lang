71bb3ac07bd8b7cb65f1dbab0ceabd569ce6058a
Andrea Bicciolo <andreabix>
2008-12-19 19:19:31 +0000

    Aggiornamento traduzione di Andrea Bicciolo

---
+++ it_utf8/condition.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // condition.php - created with Moodle 2.0 dev (Build: 20081219) (2008121701)
+
+
+$string['availabilityconditions'] = 'Condizioni per l\'accesso';
+$string['availablefrom'] = 'Disponibile dal';
+$string['availableuntil'] = 'Disponibile fino al';
+$string['completion_complete'] = 'deve essere stato completato';
+$string['completion_incomplete'] = 'non deve essere stato completato';
+$string['configenableavailability'] = 'Le attività condizionate consentono di subordinare l\'accesso ad un attività in base a condizioni come date, valutazioni e stati di avanzamento.';
+$string['enableavailability'] = 'Abilita attività condizionate';
+$string['none'] = '(non in uso)';
+$string['notavailableyet'] = 'Non ancora disponibile';
+$string['requires_completion_1'] = 'Accessibile dopo il completamento dell\'attività <strong>$a</strong>.';
+$string['requires_date'] = 'Accessibile a partire dal $a';
+$string['userrestriction_hidden'] = 'Attività condizionata (invisibile, senza messaggi): &lsquo;$a&rsquo;';
+$string['userrestriction_visible'] = 'Attività condizionata: &lsquo;$a&rsquo;';
+
+?>
