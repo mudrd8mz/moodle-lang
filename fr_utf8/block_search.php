49b0c1608063bfea11dbe0e02d732742104c2e0a
Nicolas Martignoni <martignoni>
2008-12-22 21:15:08 +0000

    New strings for global search

--- fr_utf8/block_search.php
+++ fr_utf8/block_search.php
@@ -1,27 +1,46 @@
 <?php  // $Id$
 
+$string['asynchronous'] = 'Asynchrone (Ajax)';
 $string['blockname'] = 'Recherche globale';
+$string['blockssearchswitches'] = 'Activation de l\'indexation des blocs';
+$string['blockssearchmnetfeatures'] = 'Recherche Réseau Moodle';
+$string['bytes'] = ' octets (0 = pas de limite)';
 $string['configbuttonlabel'] = 'Libellé du bouton';
 $string['configenablefileindexing'] = 'Activer l\'indexation des fichiers';
 $string['configfiletypes'] = 'Types de fichiers pris en charge';
 $string['configlimitindexbody'] = 'Taille limite du corps indexé';
 $string['configpdftotextcmd'] = 'Chemin vers la commande pdftotext';
+$string['configsearchtext'] = 'Rechercher dans le texte';
+$string['configtypetotxtcmd'] = 'Ligne de commande convertisseur';
+$string['configtypetotxtenv'] = 'Définition environnement convertisseur';
+$string['configusingsoftlock'] = 'Interlockage logiciel (pour grappes NFS)';
+$string['configutf8transcoding'] = 'Direction de transcodage UTF-8 des résultats';
 $string['configwordtotextcmd'] = 'Chemin vers la commande doctotext';
 $string['configwordtotextenv'] = 'Réglage d\'environnement du convertisseur MSWord';
-$string['configsearchtext'] = 'Rechercher dans le texte';
+$string['disabled'] = 'Désactivé';
+$string['disabledsearch'] = 'Le moteur de recherche globale est déactivé. Veuillez en informer votre administrateur.';
+$string['enabled'] = 'Activé';
 $string['go'] = 'Lancer la recherche';
-$string['bytes'] = ' octets (0 pour «&nbsp;pas de limite&nbsp;»)';
+$string['handlingfor'] = 'Traitement supplémentaire pour';
+$string['modulessearchswitches'] = 'Activation de l\'indexation des modules';
+$string['nosearchableblocks'] = 'Aucun bloc recherchable';
+$string['nosearchablemodules'] = 'Aucun module recherchable';
+$string['pdfhandling'] = 'Traitement Acrobat PDF';
+$string['search'] = 'Recherche';
+$string['searchdiscovery'] = 'Découverte des éléments recherchables';
 $string['searchmoodle'] = 'Rechercher dans le site';
 $string['usemoodleroot'] = 'Utiliser la racine de Moodle pour les convertisseurs externes';
 $string['wordhandling'] = 'Traitement du format Microsoft Word';
 $string['pdfhandling'] = 'Traitement du format Acrobat PDF';
 $string['handlingfor'] = 'Traitement supplémentaire pour';
 $string['searchdiscovery'] = 'Découverte des éléments recherchables';
+$string['synchronous'] = 'Synchrone';
 $string['configtypetotxtcmd'] = 'Ligne de commande du convertisseur';
 $string['configtypetotxtenv'] = 'Définition de l\'environnement du convertisseur';
 $string['modulessearchswitches'] = 'Activation de l\'indexeur pour les modules';
 $string['blockssearchswitches'] = 'Activation de l\'indexeur pour les blocs';
 $string['nosearchablemodules'] = 'Aucun module recherchable';
 $string['nosearchableblocks'] = 'Aucun bloc recherchable';
+$string['wordhandling'] = 'Traitement Microsoft Word';
 
 ?>
