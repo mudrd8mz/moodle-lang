2fa3d9c85b05c87086c0d2d4a49376b583ce867a
Lee Chan-young <yogibear270>
2008-12-24 05:17:45 +0000

    complete translating new files -20081224

---
+++ ko_utf8/repository_wikimedia.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // repository_wikimedia.php - created with Moodle 2.0 dev (Build: 20081224) (2008121701)
+
+
+$string['keyword'] = '그림 이름';
+$string['repositoryname'] = '위키미디아';
+
+?>
