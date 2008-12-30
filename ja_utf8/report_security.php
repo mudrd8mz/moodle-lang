0cded7a1db786ce93180e5d07febc0f26bcc08bf
Mitsuhiro Yoshida <mits>
2008-12-30 00:40:51 +0000

    MDL-17222 Adding a translated lang file for Security overview report.

---
+++ ja_utf8/report_security.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // report_security.php - created with Moodle 2.0 dev (Build: 20081217) (2008121701)
+
+
+$string['check_frontpagerole_name'] = 'フロントページロール';
+$string['check_guestrole_name'] = 'ゲストロール';
+$string['check_passwordpolicy_name'] = 'パスワードポリシー';
+$string['check_riskadmin_name'] = '管理者';
+$string['configuration'] = '設定';
+$string['description'] = '説明';
+$string['details'] = '詳細';
+$string['reportsecurity'] = 'セキュリティレビュー';
+$string['status'] = 'ステータス';
+$string['statuscritical'] = 'クリティカル';
+$string['statusinfo'] = '情報';
+$string['statusok'] = 'OK';
+$string['statusserious'] = 'シリアス';
+$string['statuswarning'] = '警告';
+
+?>
