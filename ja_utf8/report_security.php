7c326c2b9169f26a793f36ba50822ad7c4b1395e
Mitsuhiro Yoshida <mits>
2009-01-04 17:16:34 +0000

    MDL-17222 Translated new strings for Security overview report.

--- ja_utf8/report_security.php
+++ ja_utf8/report_security.php
@@ -2,6 +2,12 @@
       // report_security.php - created with Moodle 2.0 dev (Build: 20081231) (2008123102)
 
 
+$string['check_configrw_name'] = '書き込み可能なconfig.php';
+$string['check_configrw_ok'] = 'PHPスクリプトは、config.phpを修正することができません。';
+$string['check_configrw_warning'] = 'PHPスクリプトは、config.phpを修正することができます。';
+$string['check_cookiesecure_error'] = 'セキュアクッキーを有効にしてください。';
+$string['check_cookiesecure_name'] = 'セキュアクッキー';
+$string['check_cookiesecure_ok'] = 'セキュアクッキーは、有効にされています。';
 $string['check_courserole_name'] = 'コースデフォルトロール';
 $string['check_courserole_notyet'] = 'デフォルトのコースロールのみ使用されています。';
 $string['check_courserole_ok'] = 'コースデフォルトロール定義は、OKです。';
@@ -26,14 +32,21 @@ $string['check_guestrole_name'] = 'ゲストロール';
 $string['check_guestrole_notset'] = 'ゲストロールが設定されていません。';
 $string['check_guestrole_ok'] = 'ゲストロール定義は、OKです。';
 $string['check_mediafilterswf_ok'] = 'Flashメディアフィルタが有効にされていません。';
+$string['check_noauth_name'] = '認証なし';
+$string['check_noauth_ok'] = '無効にされた認証プラグインはありません。';
 $string['check_passwordpolicy_error'] = 'パスワードポリシーが設定されていません。';
 $string['check_passwordpolicy_name'] = 'パスワードポリシー';
+$string['check_passwordpolicy_ok'] = 'パスワードポリシーが有効にされています。';
 $string['check_riskadmin_name'] = '管理者';
 $string['check_unsecuredataroot_name'] = '安全でないdataroot';
+$string['check_unsecuredataroot_ok'] = 'datarootディレクトリは、ウェブからアクセスできないようにしてください。';
+$string['check_unsecuredataroot_warning'] = 'あなたのdatarootディレクトリ <code>$a</code> は、正しくない場所に配置され、ウェブにさらされた状態です。';
 $string['configuration'] = '設定';
 $string['description'] = '説明';
 $string['details'] = '詳細';
+$string['issue'] = '問題';
 $string['reportsecurity'] = 'セキュリティレビュー';
+$string['security:view'] = 'セキュリティレポートを表示する';
 $string['status'] = 'ステータス';
 $string['statuscritical'] = 'クリティカル';
 $string['statusinfo'] = '情報';
