0336387d652efe6de78da3daaa47d5822c20c845
Lee Chan-young <yogibear270>
2009-01-05 02:43:57 +0000

    New file -20090105

---
+++ ko_utf8/condition.php
@@ -0,0 +1,40 @@
+<?PHP // $Id$ 
+      // condition.php - created with Moodle 2.0 dev (Build: 20090104) (2008123102)
+
+
+$string['addcompletions'] = '양식에 활동 조건 추가';
+$string['addgrades'] = '양식에 채점 조건 추가';
+$string['availabilityconditions'] = '이용 조건';
+$string['availablefrom'] = '시작 조건부';
+$string['availableuntil'] = '종료 조건부';
+$string['completion_complete'] = '완료 표시';
+$string['completion_fail'] = '낙제 점수로 완료';
+$string['completion_incomplete'] = '미완료';
+$string['completion_pass'] = '통과 점수로 완료';
+$string['completioncondition'] = '활동 완료 조건';
+$string['configenableavailability'] = '켜져 있으면, 어떤 활동 수행에 대한 (날짜, 성적 혹은 완료 상태에 기반한) 조건을 설정할 수 있게 된다.';
+$string['enableavailability'] = '수행 조건 활성';
+$string['grade_atleast'] = 'must be &ge;';
+$string['grade_upto'] = 'and &lt;';
+$string['gradecondition'] = '성적 조건';
+$string['help_conditiondates'] = '이용 날짜';
+$string['help_showavailability'] = '이용할 수 없음 제시';
+$string['none'] = '(없음)';
+$string['notavailableyet'] = '아직 이용할 수 없음';
+$string['requires_completion_0'] = '<strong>$a</strong> 활동을 마치지 못하는 한,  이용할 수 없음';
+$string['requires_completion_1'] = '<strong>$a</strong> 활동이 완료로 표시되기 전까지 이용할 수 없음';
+$string['requires_completion_2'] = '<strong>$a</strong> 활동을 통과점수로 완료하기 전까지 이용할 수 없음';
+$string['requires_completion_3'] = '<strong>$a</strong> 활동을 낙제점수로 완료하기 전까지 이용할 수 없음';
+$string['requires_date'] = '$a 까지 이용할 수 없음';
+$string['requires_date_before'] = '$a 부터는 이용할 수 없음';
+$string['requires_grade_any'] = '<strong>$a</strong>에서 평점을 얻을 때까지 이용할 수 없음';
+$string['requires_grade_max'] = '<strong>$a</strong>에서 적정 점수를 얻지 못하는 한, 이용할 수 없음';
+$string['requires_grade_min'] = '<strong>$a</strong>에서 요구된 최소 점수을 얻을 때까지 이용할 수 없음';
+$string['requires_grade_range'] = '<strong>$a</strong>에서 일정 범위의 점수를 얻지 못하는 한, 이용할 수 없음';
+$string['showavailability'] = '사전 활동 공개';
+$string['showavailability_hide'] = '활동 완전 비공개';
+$string['showavailability_show'] = '제한 조건과 함께 비공식적으로 활동을 공개';
+$string['userrestriction_hidden'] = '활동 조건 제한(완전 비공개, 메시지 없이): &lsquo;$a&rsquo;';
+$string['userrestriction_visible'] = '활동 조건 제한: &lsquo;$a&rsquo;';
+
+?>
