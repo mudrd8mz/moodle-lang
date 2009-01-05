ed85409fe77eb6a9d20bf4a4b8d43bb6acaa287c
koen roggemans <koenr>
2009-01-05 22:23:43 +0000

    new strings

--- nl_utf8/qtype_match.php
+++ nl_utf8/qtype_match.php
@@ -1,9 +1,11 @@
 <?PHP // $Id$ 
-      // qtype_match.php - created with Moodle 1.8 Beta (2007020400)
+      // qtype_match.php - created with Moodle 2.0 dev (Build: 20090102) (2008123102)
 
 
 $string['addmoreqblanks'] = '{no} Meer lege lijnen';
+$string['filloutthreeqsandtwoas'] = 'Je moet minstens twee vragen en drie antwoorden opgeven. Je kunt extra foute antwoorden voorzien door een antwoord te geven met een lege vraag. Als zowel de vraag als het antwoord leeg is, zal dit genegeerd worden.';
 $string['nomatchinganswerforq'] = 'Je moet een antwoord voor deze vraag opgeven.';
+$string['notenoughqsandas'] = 'Je moet minstens $a->q vragen en $a->a vragen geven.';
 $string['notenoughquestions'] = 'Je moet minstens $a vraag- en antwoordparen opgeven.';
 
 ?>
