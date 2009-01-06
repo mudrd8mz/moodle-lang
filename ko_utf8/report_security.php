86b8c08be3f5c811f721ad2e5c7ac477a34553f4
Lee Chan-young <yogibear270>
2009-01-06 03:47:17 +0000

    translate added strings and correct some terms.

--- ko_utf8/report_security.php
+++ ko_utf8/report_security.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // report_security.php - created with Moodle 2.0 dev (Build: 20081231) (2008121701)
+      // report_security.php - created with Moodle 2.0 dev (Build: 20090106) (2009010500)
 
 
 $string['check_configrw_details'] = '<p>웹 서버에 의해 config.php 파일이 변조되는 것을 막기 위해 설치 후 이 파일의 읽고쓰기 권한을 변경시켜 둘 것을 권장. 다만 이러한 조치가 특별히 서버의 보안을 증진시킨다는 것이 아니라, 외부의 침탈 의도를 저하시키거나 부분적으로 제한할 수 있다는 사실을 주목하기 바랍니다.</p>';
@@ -68,9 +68,19 @@ $string['check_guestrole_error'] = '잘못 정의된 방문객 역할 \"$a\" 발
 $string['check_guestrole_name'] = '손님 역할';
 $string['check_guestrole_notset'] = '손님 역할이 설정되지 않음';
 $string['check_guestrole_ok'] = '손님 역활 정의 통과';
+$string['check_mediafilterswf_details'] = '<p>자동 swf 내장 프로그램은 매우 위험합니다. 임의의 등록 사용자가 타 서버의 사용자를 공격하기 위해 XSS 프로그램을 탑재할 우려가 있습니다. 상용 서버에서는 이를 꺼 두기 바랍니다.</p>';
+$string['check_mediafilterswf_error'] = '플래시 미디어 필터 켜짐 - 이는 대다수 서버에 위해를 가할 수 있음';
 $string['check_mediafilterswf_name'] = '.swf 필터 켬';
 $string['check_mediafilterswf_ok'] = '플래시 미디어 필터 끔';
+$string['check_noauth_details'] = '<p><em>인증절차 생략</em>플러그인은 어떠한 상용 서버에서건 추천하지 않음. 본 서버가 개발 사이트가 아닌이상 이를 꺼 두기 바람</p>';
+$string['check_noauth_error'] = '상용 서버에서는 인증 절차 생략 플러그인이 사용될 수 없음';
+$string['check_noauth_name'] = '인증절차 생략';
+$string['check_noauth_ok'] = '인증절차 생략 플러그인 끔';
+$string['check_openprofiles_details'] = '<p>공개된 사용자 신상명세는 간혹 스패머에 의해 오용될 소지가 있으므로, 신상명세를 보기 위해서는 <code>신상명세 보려면 로그인 필수</code> 나 <code>로그인 강요</code>를 켜 둘 것을 권고합니다.</p>';
+$string['check_openprofiles_error'] = '로그인 없이 사용자 신상명세 볼 수 있음';
 $string['check_openprofiles_name'] = '사용자 신상명세 공개';
+$string['check_openprofiles_ok'] = '사용자 신상명세를 보기위해 로그인이 필요함';
+$string['check_passwordpolicy_details'] = '<p>종종 가장 손쉬운 방법인 암호 추정을 통해 무단 접속이 이루어질 수 있으므로 가급적 비밀번호 정책을 켜 둘것을 권장합니다. 하지만 너무 엄격하게 이를 요구하게 되면 사용자가 비밀번호를 기억하지 못하거나 적어 두었다 하더라도 자주 까먹게 되므로 적당한 수준을 제시하는 것이 바람직하다.</p>';
 $string['check_passwordpolicy_error'] = '비밀번호 정책이 설정되지 않음';
 $string['check_passwordpolicy_name'] = '비밀번호 정책';
 $string['check_passwordpolicy_ok'] = '비밀번호 정책 활성화';
@@ -79,8 +89,11 @@ $string['check_riskadmin_detailswarning'] = '<p>다음의 관리자 목록을 �
 <p>단지 시스템 영역의 관리자 역할만을 부여할 것을 권장함. 다음의 사용자들은 지원되지 않는 관리자 역할을 부여받았음 <br />$a->unsupported</p>';
 $string['check_riskadmin_name'] = '관리자';
 $string['check_riskadmin_ok'] = '$a 서버 관리자 발견';
-$string['check_riskadmin_warning'] = '$a->admincount 서버 관리자 및  $a->unsupcount 의 지원되지 않는 관리자 역할 부여 발견';
+$string['check_riskadmin_warning'] = '$a 서버 관리자 및  $a->unsupcount 의 지원되지 않는 관리자 역할 부여 발견';
+$string['check_riskxss_details'] = '<p>인증된 사용자들에게 부여된 모든 위험성이 있는 권한에 대해서는 RISK_XSS 표식을 함. </p>
+<p>다음 사용자에 대해서는 본 서버에서 완벽하게 신뢰할 수 있는지 검증해 보기 바람:<br />$a </p>';
 $string['check_riskxss_name'] = 'XSS 인증된 사용자';
+$string['check_riskxss_warning'] = 'RISK_XSS - 검증할 $a 사용자 발견';
 $string['check_unsecuredataroot_details'] = '<p>데이터 경로는 웹으로 접속되어서는 안됨. 최선의 방법은 웹 디렉토리 외부에 있는 웹접속이 차단된 경로에 만들어 두는 것임</p>
 <p>경로를 옮긴 후, <code>config.php</code>의 <code>$CFG->dataroot</code>의 값도 변경해 주어야 함을 잊지 말 것</p>';
 $string['check_unsecuredataroot_error'] = '데이터 경로 <code>$a</code> 가 잘 못 지정되어 있으며 웹에 노출되어 있음!';
