fd4883c022c746940c14019d93aa170dc86e3342
Shamim Rezaie <rezaie9>
2009-01-07 20:51:40 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/qtype_match.php
@@ -0,0 +1,5 @@
+<?php   // $Id$
+$string['addmoreqblanks'] = '{no} جفت مورد دیگر اضافه شود';
+$string['notenoughquestions'] = 'باید حداقل $a جفت سؤال و جواب ارائه کنید';
+$string['nomatchinganswerforq'] = 'برای این سؤال باید یک جواب مشخص کنید.';
+?>
---
+++ fa_utf8/qtype_multianswer.php
@@ -0,0 +1,4 @@
+<?php   // $Id$
+        // author: Shamim Rezaie, http://rezaie.info
+$string['decodeverifyquestiontext'] = 'رمزگشایی و بازبینی متن سؤال';
+?>
