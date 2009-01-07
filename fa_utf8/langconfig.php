2bafa66425ca7dda37f16fa44324796e722a588b
Shamim Rezaie <rezaie9>
2009-01-07 21:12:04 +0000

    fixing comma character

--- fa_utf8/langconfig.php
+++ fa_utf8/langconfig.php
@@ -1,4 +1,5 @@
-<?php
+<?php   // $Id$
+        // author: Shamim Rezaie, http://rezaie.info
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['locale'] = 'fa_IR.UTF-8';
 $string['localewin'] = 'Farsi_Iran.1256';
@@ -6,11 +7,11 @@ $string['localewincharset'] = 'WINDOWS-1256';
 $string['oldcharset'] = 'WINDOWS-1256';
 $string['strftimedate'] = '%%d %%B %%Y';
 $string['strftimedateshort'] = '%%d %%B';
-$string['strftimedatetime'] = '%%d %%B %%Y, %%I:%%M %%p';
-$string['strftimedaydate'] = '%%A, %%d %%B %%Y';
-$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
-$string['strftimerecent'] = '%%d %%b, %%H:%%M';
-$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%I:%%M %%p';
+$string['strftimedatetime'] = '%%d %%B %%Y، %%I:%%M %%p';
+$string['strftimedaydate'] = '%%A، %%d %%B %%Y';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y، %%I:%%M %%p';
+$string['strftimerecent'] = '%%d %%b، %%H:%%M';
+$string['strftimerecentfull'] = '%%a، %%d %%b %%Y، %%I:%%M %%p';
 $string['strftimetime'] = '%%I:%%M %%p';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'rtl';
