6261bafe0a7dc8914b196284d6e9f134734164ef
Nicolas Martignoni <martignoni>
2009-01-11 09:19:51 +0000

    MDL-16910, new lang file translated

---
+++ fr_utf8/repository_s3.php
@@ -0,0 +1,9 @@
+<?php // $Id$
+
+$string['repositoryname'] = 'Amazon S3';
+$string['configplugin'] = 'Réglages Amazon S3';
+$string['access_key'] = 'Clef d\'accès';
+$string['secret_key'] = 'Clef secrète';
+$string['needaccesskey'] = 'Une clef d\'accès doit être fournie';
+
+?>
