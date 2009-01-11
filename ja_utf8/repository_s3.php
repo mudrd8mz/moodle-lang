b2275a5840c455c18e62c33802256b7cc58b1799
Mitsuhiro Yoshida <mits>
2009-01-11 04:13:26 +0000

    MDL-16910 Adding a translated lang file for amazon s3 repository plugin.

---
+++ ja_utf8/repository_s3.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // repository_s3.php - created with Moodle 2.0 dev (Build: 20090111) (2009011000)
+
+
+$string['access_key'] = 'アクセスキー';
+$string['configplugin'] = 'Amazon S3設定';
+$string['needaccesskey'] = 'アクセスキーを入力してください。';
+$string['repositoryname'] = 'Amazon S3';
+$string['secret_key'] = 'シークレットキー';
+
+?>
