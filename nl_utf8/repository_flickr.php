bf4a033db6dc4daf1fe21822fbd4e7796847671d
koen roggemans <koenr>
2009-01-12 21:59:59 +0000

    new strings

--- nl_utf8/repository_flickr.php
+++ nl_utf8/repository_flickr.php
@@ -1,14 +1,18 @@
 <?PHP // $Id$ 
-      // repository_flickr.php - created with Moodle 2.0 dev (Build: 20080815) (2008081506)
+      // repository_flickr.php - created with Moodle 2.0 dev (Build: 20090111) (2009011100)
 
 
+$string['apikey'] = 'API sleutel';
+$string['callbackurl'] = 'Callback URL';
 $string['configplugin'] = 'Flickr-configuratie';
+$string['emailaddress'] = 'E-mailadres';
 $string['invalidemail'] = 'Ongeldig Flickr e-mailadres';
 $string['notitle'] = 'geen titel';
 $string['nullphotolist'] = 'Er hoort geen foto bij deze account';
 $string['remember'] = 'Gegevens onthouden';
 $string['repositorydesc'] = 'Opslagruimte voor flickr.com';
 $string['repositoryname'] = 'Flickr';
+$string['secret'] = 'Geheim';
 $string['username'] = 'Flickr-account';
 
 ?>
