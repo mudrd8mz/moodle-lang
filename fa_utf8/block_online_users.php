2bba480034baeaedaf73e9f8a60c158a3a7ea680
Shamim Rezaie <rezaie9>
2009-01-14 11:44:34 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/block_online_users.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // block_online_users.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['blockname'] = 'کاربران حاضر';
+$string['configtimetosee'] = 'میزان زمانی که یک کاربر در صورت غیر فعال بودن بیش از آن دیگر حاضر محسوب نخواهد شد.';
+$string['periodnminutes'] = 'از $a دقیقه قبل';
+$string['timetosee'] = 'حذف پس از عدم فعالیت (دقیقه)';
+
+?>
