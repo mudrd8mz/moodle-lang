60ca93c72b93772116d5e6ac84c3271f68bf6529
Abel Camacho <ueu_ueu>
2009-01-16 10:29:54 +0000

    New file added

---
+++ eu_utf8/dbtransfer.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$ 
+      // dbtransfer.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+
+
+$string['dbexport'] = 'Datu-basearen esportazioa';
+$string['dbtransfer'] = 'Datu-basearen transferentzia';
+$string['differenttableexception'] = 'Taularen $a egitura ez dator bat';
+$string['exportdata'] = 'Esportatu datuak';
+$string['exportschemaexception'] = 'Oraingo datu-basearen egitura ez dator bat install.xml fitxategi guztiekin. <br /> $a';
+$string['importschemaexception'] = 'Oraingo datu-basearen egitura ez dator bat install.xml fitxategi guztiekin. <br /> $a';
+$string['importversionmismatchexception'] = 'Oraingo bertsioa $a->currentver ez dator bat esportatutako bertsioarekin $a->schemaver.';
+$string['malformedxmlexception'] = 'Aurkitutako XML-a ez da egokia, ezin da jarraitu.';
+$string['notargetconectexception'] = 'Barkatu, ezin da konektatu helburu datu-basearekin.';
+$string['transferdata'] = 'Transferitu datuak';
+$string['unknowntableexception'] = 'Esportatutako fitxategian $a taula ezezaguna aurkitu da';
+
+?>
---
+++ eu_utf8/table.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // table.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+
+
+$string['downloadas'] = 'Jaitsi honela datu-taula';
+$string['downloadcsv'] = 'komek banatutako baloreak dituen testu-fitxategia';
+$string['downloadexcel'] = 'Microsoft Excel kalkulu-orria';
+$string['downloadods'] = 'OpenDocument kalkulu-orria (ODS)';
+$string['downloadtsv'] = 'tabuladoreek banatutako baloreak dituen testu-fitxategia';
+$string['downloadxhtml'] = 'orrikatu gabeko XHTML dokumentua';
+$string['tableexportformats'] = 'Datu-taula jaisten';
+
+?>
