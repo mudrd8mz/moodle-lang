71fbae0e841f756327b01e37e817d68daf9165ff
Shamim Rezaie <rezaie9>
2009-01-16 12:24:47 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/block_search_forums.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // block_search_forums.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['advancedsearch'] = 'جستجوی پیشرفته';
+$string['blocktitle'] = 'جستجو در تالارها';
+
+?>
