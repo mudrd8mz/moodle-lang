e2b663a5708800f83496069314dc9c52daddb59e
Nicolas Martignoni <martignoni>
2009-01-22 21:08:39 +0000

    MDL-17878 New string added

---
+++ fr_utf8/report_configlog.php
@@ -0,0 +1,11 @@
+<?php  // $Id$
+
+$string['configlog'] = 'Modification de configuration';
+
+$string['timemodified'] = 'Date';
+$string['plugin'] = 'Plugin';
+$string['setting'] = 'Réglage';
+$string['value'] = 'Nouvelle valeur';
+$string['oldvalue'] = 'Valeur d\'origine';
+
+?>
