b5e556bb5feb281bd0b0edb312da9c959e6c6c4d
Abel Camacho <ueu_ueu>
2009-01-23 09:06:17 +0000

    New file added

---
+++ eu_utf8/portfolio_download.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // portfolio_download.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+
+
+$string['downloadfile'] = 'Jaitsi zure portfolioaren esportazio-fitxategia';
+$string['downloading'] = 'Jaisten...';
+$string['pluginname'] = 'Jaitsitako fitxategia';
+
+?>
