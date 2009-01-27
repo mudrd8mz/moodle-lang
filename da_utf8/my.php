a262d47ca73bd6c3cb32b4e7453282e2355fa3cc
Bente Olsen <benteo>
2009-01-27 20:11:46 +0000

    Modification

--- da_utf8/my.php
+++ da_utf8/my.php
@@ -1,9 +1,9 @@
 <?PHP // $Id$ 
-      // my.php - created with Moodle 1.9 Beta 4 (2007101506)
+      // my.php - created with Moodle 1.9.3+ (Build: 20081025) (2007101530)
 
 
 $string['mymoodle'] = 'Mine kurser';
-$string['nocourses'] = 'Du er ikke tilmeldt nogen kurser endnu.';
+$string['nocourses'] = 'Du er ikke tilmeldt nogen kurser.';
 $string['noguest'] = '\'Kursusoversigten\' er ikke tilgængelig for gæster.';
 $string['pinblocks'] = 'Konfigurer faste blokke i kursusoversigten.';
 $string['pinblocksexplan'] = 'Alle blokindstillinger du konfigurerer her vil være synlige (og kan ikke rettes) af andre brugere på deres \'Kursusoversigts\' side.';
