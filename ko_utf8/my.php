9906fb4a41e382ccc050892e7f06256e7d896bf2
Lee Chan-young <yogibear270>
2009-01-29 04:00:10 +0000

    correct some terms

--- ko_utf8/my.php
+++ ko_utf8/my.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // my.php - created with Moodle 1.7 dev (2006091702)
+      // my.php - created with Moodle 2.0 dev (Build: 20090129) (2009011900)
 
 
 $string['mymoodle'] = '내 공부방';
-$string['nocourses'] = '보여지는 교육과정의 정보가 없습니다.';
+$string['nocourses'] = '볼 수 있는 강좌 정보가 없습니다.';
 $string['noguest'] = '손님 계정을 사용하는 분들에게는 \"내 공부방\"이 제공되지 않습니다.';
-$string['pinblocks'] = '내 공부방을 의한 환경설정 잠금장치 블럭들';
-$string['pinblocksexplan'] = '여기에서 설정한 블럭은 모든 사용자들에게 그들의 공부방 화면으로 보여집니다. (편집은 할수 없음)';
+$string['pinblocks'] = '내 공부방을 위한 고정 블럭의 환경설정';
+$string['pinblocksexplan'] = '여기에서 설정한 블럭은 모든 사용자들에게 개인 공부방 화면으로 보여집니다. (편집은 할수 없음)';
 
 ?>
