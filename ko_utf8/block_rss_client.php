db0ab515eb788703f1ae944b6de2479af61b4da6
Lee Chan-young <yogibear270>
2009-01-29 03:54:55 +0000

    correct some terms

--- ko_utf8/block_rss_client.php
+++ ko_utf8/block_rss_client.php
@@ -1,52 +1,52 @@
 <?PHP // $Id$ 
-      // block_rss_client.php - created with Moodle 2.0 dev (Build: 20081213) (2008121000)
+      // block_rss_client.php - created with Moodle 2.0 dev (Build: 20090129) (2009011900)
 
 
-$string['addfeed'] = '새로운 RSS 피드 주소 추가하기';
-$string['addheadlineblock'] = 'RSS 헤드라인 블럭 추가하기';
+$string['addfeed'] = '새소식 피드 주소 추가 :';
+$string['addheadlineblock'] = 'RSS 헤드라인 블럭 추가';
 $string['addnew'] = '새로 추가';
 $string['blockname'] = 'RSS 수신자';
-$string['cannotmakemodification'] = 'RSS 피드기능을 변경할 권한이 없습니다';
-$string['choosefeedlabel'] = '이 블럭에서 사용하고 싶은 RSS 피드를 선택하시오';
-$string['clientchannellink'] = '소스 사이트';
+$string['cannotmakemodification'] = '이 RSS 피드에 대한 수정 권한이 없습니다';
+$string['choosefeedlabel'] = '이 블럭에서 사용하고 싶은 RSS 피드를 선택하시오 :';
+$string['clientchannellink'] = '원본 사이트...';
 $string['clientnumentries'] = '한 RSS 피드가 보여줄 기본 기사 수';
-$string['clientshowchannellinklabel'] = '송출한 원래 사이트에 링크 할 수 있습니까?(만약 아무런 RSS 피드 링크가 뉴스 RSS 피드에 제공되지 않으면 아무런 링크도 보이지 않을 것이라는 것을 주의히사오)';
-$string['clientshowimagelabel'] = '만약 가능하다면 채널 이미지를 보여주시오';
+$string['clientshowchannellinklabel'] = '송출한 원래 사이트의 링크를 보여줄까요? (만약 RSS 피드 링크가 뉴스 RSS 피드에 제공되지 않으면 아무 것도 보이지 않을 것이라는 점에 유의하시오)';
+$string['clientshowimagelabel'] = '가능하면 채널 이미지를 보여주시오 :';
 $string['configblock'] = '이 블럭을 설정하시오';
-$string['couldnotfindfeed'] = '아이디와 맞는 RSS 피드를 찾지 못했습니다.';
-$string['customtitlelabel'] = '제목(RSS 피드가 제공한 제목을 사용할 빈칸을 남기시오)';
+$string['couldnotfindfeed'] = '아이디와 맞는 RSS 피드를 찾지 못했음';
+$string['customtitlelabel'] = '제목(RSS 피드가 제공할 제목을 사용하려면 빈칸으로 둠)';
 $string['deletefeedconfirm'] = '이 RSS 피드를 지우겠습니까?';
 $string['displaydescriptionlabel'] = '각 링크의 설명을 보이겠습니까?';
-$string['editfeeds'] = 'RSS/Atom 뉴스 RSS 피드로부터 편집,구독하거나 구독하지 마시오';
+$string['editfeeds'] = 'RSS/Atom 뉴스 RSS 피드로부터 편집, 구독/구독해지';
 $string['editnewsfeeds'] = '새소식 RSS 피드를 편집';
 $string['editrssblock'] = '헤드라인 블럭을 편집';
 $string['feed'] = 'RSS 피드';
-$string['feedadded'] = '새 RSS 피드 추가';
-$string['feeddeleted'] = '새소식 RSS 피드 삭제';
-$string['feeds'] = '새소식 RSS 피드';
-$string['feedsaddedit'] = 'RSS 피드 추가/삭제';
+$string['feedadded'] = '새소식 피드 추가됨';
+$string['feeddeleted'] = '새소식 RSS 피드 삭제됨';
+$string['feeds'] = '새소식 피드';
+$string['feedsaddedit'] = '피드 추가/삭제';
 $string['feedsconfigurenewinstance'] = '이 블럭에 RSS 피드를 설정하려면 여기를 클릭';
-$string['feedstitle'] = 'RSS 피드';
-$string['feedupdated'] = '새로운 RSS 피드 업데이트';
+$string['feedstitle'] = '원격 RSS 피드';
+$string['feedupdated'] = '새소식 피드 갱신';
 $string['findmorefeeds'] = '더 많은 RSS 피드 찾기';
-$string['managefeeds'] = '내 RSS 피드 관리하기';
+$string['managefeeds'] = '내 피드 관리하기';
 $string['nofeeds'] = '이 사이트에 대한 정의된 RSS 피드가 없습니다.';
-$string['numentries'] = 'Feed당 항목수';
-$string['pickfeed'] = '새로운 RSS 피드 선택';
-$string['remotenewsfeed'] = '원격 뉴스 RSS 피드';
+$string['numentries'] = '피드당 항목수';
+$string['pickfeed'] = '새소식 피드 선택';
+$string['remotenewsfeed'] = '원격 뉴스 피드';
 $string['rss_client:createprivatefeeds'] = '개인 RSS 피드 만들기';
 $string['rss_client:createsharedfeeds'] = '공유 RSS 피드 만들기';
 $string['rss_client:manageanyfeeds'] = '모든 RSS 피드 관리';
-$string['rss_client:manageownfeeds'] = '자신의 RSS 피드 관리';
+$string['rss_client:manageownfeeds'] = '개인 RSS 피드 관리';
 $string['seeallfeeds'] = '모든 RSS 피드 보기';
-$string['sharedfeed'] = '공유 RSS 피드';
+$string['sharedfeed'] = '공유 피드';
 $string['shownumentrieslabel'] = '한 블럭에 보여줄  최대 숫자';
-$string['submitters'] = '누가 새 RSS 피드를 정의하게 허용하겠는가? 정의된 RSS 피드는 사이트의 어느 페이지에서도 사용 가능하다.';
+$string['submitters'] = '누가 새 RSS 피드를 정의할 수 있게 하겠는가? 정의된 피드는 사이트의 어느 페이지에서도 사용 가능하다.';
 $string['submitters2'] = '제출자';
-$string['timeout'] = '캐시에서 RSS 피드가 만료되기까지 시간(분). 이 시간은 만료 전 최소 시간을 정의합니다. 만료 후 다음 크론이 실행될때 feed가 새로 갱신될 것입니다. 추천값은 30분 이상이다.';
+$string['timeout'] = '캐시에서 RSS 피드가 만료되기까지 시간. 이는 만료 전 최소 시간을 정의합니다. 만료 후 다음 크론이 실행될 때 피드는 새로 갱신될 것입니다. 추천값은 30분 이상이다.';
 $string['timeout2'] = '타임아웃';
 $string['timeoutdesc'] = '케쉬에 있는 RSS 피드를 위한 분단위의 시간';
-$string['updatefeed'] = '새로운 뉴스 RSS 피드 주소 갱신';
-$string['validatefeed'] = 'RSS 피드 확인';
+$string['updatefeed'] = '뉴스 피드 주소 갱신 :';
+$string['validatefeed'] = 'RSS 피드 점검';
 
 ?>
