a25ea9dabd1b1f7fcfbf9315ab42ee8f893617a9
Nicolas Martignoni <martignoni>
2009-01-31 22:02:31 +0000

    File cleaned up for inheritance of parent lang pack

--- fr_ca_utf8/assignment.php
+++ fr_ca_utf8/assignment.php
@@ -1,52 +1,27 @@
-<?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.4.1 (2004083101)
+<?php // $Id$ 
 
+$string['allowresubmit'] = 'Permettre plusieurs remises d\'un devoir';
+$string['assignmentmail'] = '$a->teacher a fait des remarques sur votre devoir « $a->assignment ». 
 
-$string['allowresubmit'] = 'Accepter de nouvelles soumissions';
-$string['assignmentdetails'] = 'Détails du devoir';
-$string['assignmentmail'] = '$a->teacher a fait des remarques sur votre devoir \'$a->assignment\'. Vous pouvez les voir à :
+Vous pouvez les consulter en annexe à votre devoir :
 
 $a->url';
-$string['assignmentmailhtml'] = '$a->teacher a fait des remarques sur votre devoir \'<i>$a->assignment</i>\'. <br /> <br />
+$string['assignmentmailhtml'] = '$a->teacher a fait des remarques sur votre devoir «&nbsp;<em>$a->assignment</em>&nbsp;». <br /> <br />
 
-Elles ont été ajoutées à votre: <a href\"$a->url\">devoir<a>.';
-$string['assignmentname'] = 'Titre du devoir';
-$string['assignmenttype'] = 'Type de devoir';
-$string['configmaxbytes'] = 'Taille maximale par défaut pour tous les devoirs sur ce site (dépend aussi des limites pour les cours et autres ajustements locaux)';
-$string['description'] = 'Description';
-$string['duedate'] = 'À rendre avant le';
-$string['duedateno'] = 'Pas de date d\'échance';
-$string['early'] = '$a en avance';
-$string['existingfiledeleted'] = 'Le fichier existant a été effacé: $a';
+Vous pouvez les consulter en annexe à <a href=\'$a->url\'>votre devoir</a>.';
 $string['failedupdatefeedback'] = 'Impossible d\'enregistrer la rétroaction pour $a';
 $string['feedback'] = 'Rétroaction';
-$string['feedbackupdated'] = 'Rétroaction enregistré pour $a personnes';
-$string['late'] = '$a en retard';
-$string['maximumgrade'] = 'Note maximale';
-$string['maximumsize'] = 'Taille maximale';
-$string['modulename'] = 'Devoir';
-$string['modulenameplural'] = 'Devoirs';
-$string['newsubmissions'] = 'Devoirs soumis';
-$string['notgradedyet'] = 'Pas encore corrigé';
-$string['notsubmittedyet'] = 'Pas encore soumis';
-$string['overwritewarning'] = 'Attention: Si vous envoyez de nouveau votre devoir, ce dernier remplacera le devoir déjà remis';
+$string['feedbackupdated'] = 'Rétroaction enregistrée pour $a utilisateurs';
 $string['saveallfeedback'] = 'Enregistrer tous mes commentaires';
 $string['submissionfeedback'] = 'Rétroactions sur le devoir';
-$string['submissions'] = 'Devoirs soumis';
 $string['submitassignment'] = 'Envoyer votre devoir en utilisant ce formulaire';
-$string['submitted'] = 'Soumis';
-$string['typeoffline'] = 'Activité hors ligne';
-$string['typeuploadsingle'] = 'Téléverser un fichier unique';
-$string['uploadbadname'] = 'Le nom de ce fichier contient des caractères invalides : il ne peut être téléversé.';
+$string['typeuploadsingle'] = 'Téléverser un fichier';
+$string['uploadbadname'] = 'Ce nom de fichier contient d\'étranges caractères. Il ne peut être téléversé';
 $string['uploadedfiles'] = 'fichiers téléversés';
-$string['uploaderror'] = 'Une erreur est survenue lors de l\'enregistrement du fichier sur le serveur';
-$string['uploadfailnoupdate'] = 'Le fichier a été correctement téléversé, mais il n\'a pas été possible de mettre à jour votre soumission';
-$string['uploadfiletoobig'] = 'Désolé, ce fichier est trop gros (la limite est de $a octets)';
-$string['uploadnofilefound'] = 'Aucun fichier n\'a été trouvé, êtes-vous certain d\'en avoir choisi un à téléverser ?';
-$string['uploadnotregistered'] = '« $a » a été correctement téléversé mais votre soumission n\'a pu être enregistrée !';
-$string['uploadsuccess'] = '« $a » a été correctement téléversé';
-$string['viewfeedback'] = 'Afficher les notes et les rétroactions sur les devoirs';
-$string['viewsubmissions'] = 'Voir les devoirs soumis de $a';
-$string['yoursubmission'] = 'Votre devoir';
+$string['uploadfailnoupdate'] = 'Le fichier a été correctement téléversé, mais il n\'a pas été possible de mettre à jour votre devoir&nbsp;!';
+$string['uploadnofilefound'] = 'Aucun fichier n\'a été trouvé. En avez-vous vraiment choisi un à téléverser&nbsp;?';
+$string['uploadnotregistered'] = '«&nbsp;$a&nbsp;» a été correctement téléversé mais votre devoir n\'a pu être enregistré&nbsp;!';
+$string['uploadsuccess'] = '«&nbsp;$a&nbsp;» a été correctement téléversé';
+$string['viewfeedback'] = 'Afficher les évaluations et les rétroactions des devoirs';
 
 ?>
