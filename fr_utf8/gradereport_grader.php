592123a82f181323d0bbeb639af1f8450f310ce6
Nicolas Martignoni <martignoni>
2009-02-10 19:20:41 +0000

    MDL-18083 New strings added/modified

--- fr_utf8/gradereport_grader.php
+++ fr_utf8/gradereport_grader.php
@@ -3,5 +3,6 @@
 $string['modulename'] = 'Rapport de l\'évaluateur';
 $string['grader:manage'] = 'Gérer le rapport de l\'évaluateur';
 $string['grader:view'] = 'Afficher le rapport de l\'évaluateur';
+$string['preferences'] = 'Préférences du rapport de l\'évaluateur';
 
 ?>
