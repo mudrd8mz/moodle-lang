e81242fd9b7d1a578bdf2feed9b4a755adb578bb
Mitsuhiro Yoshida <mits>
2009-02-13 17:27:12 +0000

    MDL-18225 Translated new strings for dbtransfer script.

--- ja_utf8/dbtransfer.php
+++ ja_utf8/dbtransfer.php
@@ -1,10 +1,15 @@
 <?PHP // $Id$ 
-      // dbtransfer.php - created with Moodle 2.0 dev (Build: 20081126) (2008112400)
+      // dbtransfer.php - created with Moodle 2.0 dev (Build: 20090130) (2009012901)
 
 
+$string['checkingsourcetables'] = 'ソーステーブル構造のチェック';
+$string['copyingtable'] = 'テーブル $a のコピー';
+$string['copyingtables'] = 'テーブルコンテンツのコピー';
+$string['creatingtargettables'] = 'ターゲットデータベースにテーブルを作成';
 $string['dbexport'] = 'データベースのエクスポート';
 $string['dbtransfer'] = 'データベースの転送';
 $string['differenttableexception'] = 'テーブル $a の構造が合致しません。';
+$string['done'] = '完了';
 $string['exportdata'] = 'データをエクスポートする';
 $string['exportschemaexception'] = '現在のデータベース構造がすべてのinstall.xmlファイルに合致しません。<br /> $a';
 $string['importschemaexception'] = '現在のデータベース構造がすべてのinstall.xmlファイルに合致しません。<br /> $a';
@@ -12,6 +17,9 @@ $string['importversionmismatchexception'] = '現在のバージョン $a->curren
 $string['malformedxmlexception'] = '不正なXMLが見つかったため、継続できません。';
 $string['notargetconectexception'] = '申し訳ございません、ターゲットデータベースに接続できません。';
 $string['transferdata'] = 'データを転送する';
+$string['transferdbintro'] = 'このスクリプトでは、データベースのコンテンツすべてを別のデータベースサーバに転送します。';
+$string['transferdbtoserver'] = 'このMoodleデータベースを別のサーバに転送する';
+$string['transferringdbto'] = 'このデータベースを $a->dbhost の $a->dbtype データベース $a->dbname に転送する';
 $string['unknowntableexception'] = 'エクスポートファイルに不明なテーブル $a が見つかりました。';
 
 ?>
