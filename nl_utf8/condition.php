6f71e46191bdd3da5b0889190313770ca03d41c2
koen roggemans <koenr>
2009-02-15 22:25:07 +0000

    initial version

---
+++ nl_utf8/condition.php
@@ -0,0 +1,41 @@
+<?PHP // $Id$ 
+      // condition.php - created with Moodle 2.0 dev (Build: 20090210) (2009012901)
+
+
+$string['addcompletions'] = 'Voeg {no} activiteitsvoorwaarden toe aan formulier';
+$string['addgrades'] = 'Voeg {no} cijfervoorwaarden toe aan formulier';
+$string['availabilityconditions'] = 'Beperk beschikbaarheid';
+$string['availablefrom'] = 'Alleen beschikbaar vanaf';
+$string['availableuntil'] = 'Alleen beschikbaar tot';
+$string['badavailabledates'] = 'Ingeldige datum. De beschikbaar vanaf-datum moet vóór de beschikbaar tot-datum zijn.';
+$string['completion_complete'] = 'moet als afgewerkt gemarkeerd worden';
+$string['completion_fail'] = 'moet afgewerkt worden met een onvoldoende';
+$string['completion_incomplete'] = 'moet niet als afgewerkt gemarkeerd worden';
+$string['completion_pass'] = 'moet als afgewerkt worden met een voldoende';
+$string['completioncondition'] = 'Voorwaarde afgewerkte activeit';
+$string['configenableavailability'] = 'Wanneer dit ingeschakeld is, dan kun je voorwaarden instellen (gebaseerd op datum, cijfer of afgewerkt) die controleren wanneer een activiteit beschikbaar is.';
+$string['enableavailability'] = 'Voorwaarden voor beschikbaarheid inschakelen';
+$string['grade_atleast'] = 'moet &ge; zijn';
+$string['grade_upto'] = 'en &lt;';
+$string['gradecondition'] = 'Cijfervoorwaarde';
+$string['help_conditiondates'] = 'beschikbare data';
+$string['help_showavailability'] = 'tonen van niet-beschikbare activiteiten';
+$string['none'] = '(geen)';
+$string['notavailableyet'] = 'Nog niet beschikbaar';
+$string['requires_completion_0'] = 'Niet beschikbaar tenzij de activiteit  <strong>$a</strong> niet afgewerkt is.';
+$string['requires_completion_1'] = 'Niet beschikbaar tot de activiteit <strong>$a</strong> als afgewerkt is gemarkeerd.';
+$string['requires_completion_2'] = 'Niet beschikbaar tot de activiteit <strong>$a</strong> afgewerkt is en de leerling geslaagd is.';
+$string['requires_completion_3'] = 'Niet beschikbaar tot de activiteit <strong>$a</strong> afgewerkt is en de leerling niet geslaagd is.';
+$string['requires_date'] = 'Niet beschikbaar tot $a';
+$string['requires_date_before'] = 'Niet meer beschikbaar vanaf $a';
+$string['requires_grade_any'] = 'Niet beschikbaar tot je een cijfer behaald hebt in <strong>$a</strong>.';
+$string['requires_grade_max'] = 'Niet beschikbaar tot je een goed cijfer hebt gehaald in <strong>$a</strong>.';
+$string['requires_grade_min'] = 'Niet beschikbaar tot je het vereiste cijfer behaald in <strong>$a</strong>.';
+$string['requires_grade_range'] = 'Niet beschikbaar tot je een deelcijfer behaald in <strong>$a</strong>.';
+$string['showavailability'] = 'Voor de activiteit beschikbaar is';
+$string['showavailability_hide'] = 'Verberg de activiteit volledig';
+$string['showavailability_show'] = 'Toon de activiteit in het grijs, met beperkte informatie';
+$string['userrestriction_hidden'] = 'Deze activiteit is beperkt met voorwaarden (volledig verborgen, geen boodschap:  &lsquo;$a&rsquo;';
+$string['userrestriction_visible'] = 'Deze activiteit is beperkt met voorwaarden: &lsquo;$a&rsquo;';
+
+?>
