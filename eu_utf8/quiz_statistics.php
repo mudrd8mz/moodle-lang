9af3a0a855f63122ee27699aa40eb0d59c8f9750
Abel Camacho <ueu_ueu>
2009-02-17 10:34:05 +0000

    New file added

---
+++ eu_utf8/quiz_statistics.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // quiz_statistics.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+
+
+$string['allattempts'] = 'saiakera guztiak';
+$string['attempts'] = 'Saiakerak';
+$string['attemptsall'] = 'saiakera guztiak';
+$string['coursename'] = 'Ikastaroaren izena';
+$string['questioninformation'] = 'Galderaren informazioa';
+$string['questionname'] = 'Galderaren izena';
+$string['questiontype'] = 'Galdera-mota';
+$string['quizinformation'] = 'Galdetegiaren informazioa';
+$string['quizname'] = 'Galdetegiaren izena';
+$string['statistics'] = 'Estatistikak';
+$string['statistics:view'] = 'Estatistiken txostean ikusi';
+$string['statisticsreport'] = 'Estatistiken txostean';
+$string['statsfor'] = 'Galdetegiaren estatitstikak ($a-rentzat)';
+
+?>
