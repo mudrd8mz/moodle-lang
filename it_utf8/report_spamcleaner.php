285c8c17ba29fa4d0619fe3116a54b035e89a872
Andrea Bicciolo <andreabix>
2009-02-17 11:20:21 +0000

    Traduzione di Andrea Bicciolo

---
+++ it_utf8/report_spamcleaner.php
@@ -0,0 +1,21 @@
+<?PHP // $Id$ 
+      // report_spamcleaner.php - created with Moodle 1.9.4+ (Build: 20090215) (2007101541)
+
+
+$string['spamauto'] = 'Cerca automaticamente le tipiche parole da spammer';
+$string['spamcannotdelete'] = 'Non è possibile eliminare questo utente';
+$string['spamcannotfinduser'] = 'Non è stato trovato nessun utente che corrisponda ai criteri di ricerca';
+$string['spamcleaner'] = 'Spam cleaner';
+$string['spamcleanerintro'] = 'Questo script esegue ricerche nei profili utente per trovare ed eventualmente eliminare account palesemente creati da spammer. Potete usare parole chiave multiple separandole da virgole (ad esempio: casino, porn)<br /><br />Moodle Docs fornisce maggior informazioni sul tema: <a href=\"http://docs.moodle.org/en/Reducing_spam_in_Moodle\">Reducing spam in Moodle</a>.';
+$string['spamdeleteall'] = 'Elimina tutti questi account utente';
+$string['spamdeleteallconfirm'] = 'Sei sicuro di voler eliminare questo utente? Non è possibile tornare indietro.';
+$string['spamdeleteconfirm'] = 'Sei sicuro di voler eliminare questo record? Non è possibile tornare indietro.';
+$string['spamdesc'] = 'Descrizione';
+$string['spameg'] = 'Ad esempio, casino, porn, xxx';
+$string['spamfromblog'] = 'Dai post dei Blog:';
+$string['spaminvalidresult'] = 'Risultato sconosciuto e non valido.';
+$string['spamoperation'] = 'Operazione';
+$string['spamresult'] = 'Risultati della ricerca nei profili utente per:';
+$string['spamsearch'] = 'Cerca queste parole chiave';
+
+?>
