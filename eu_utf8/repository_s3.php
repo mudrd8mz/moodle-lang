e7d07ae3a6d0239a38ee10368c4587748476291a
Abel Camacho <ueu_ueu>
2009-02-17 10:06:13 +0000

    New file added

---
+++ eu_utf8/repository_s3.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // repository_s3.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+
+
+$string['access_key'] = 'Sarbide-giltza';
+$string['configplugin'] = 'Amazon S3 ezarpenak';
+$string['needaccesskey'] = 'Sarbide-gitza lortu behar da';
+$string['repositoryname'] = 'Amazon S3';
+$string['secret_key'] = 'Giltza sekretua';
+
+?>
