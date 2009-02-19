b486c6dbf1c55d8606bf0aaf688ff7f62ce17731
Andrea Bicciolo <andreabix>
2009-02-19 18:55:14 +0000

    Aggiornamento traduzione di Andrea Bicciolo

--- it_utf8/gradereport_grader.php
+++ it_utf8/gradereport_grader.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // gradereport_grader.php - created with Moodle 2.0 dev (Build: 20081127) (2008112600)
+      // gradereport_grader.php - created with Moodle 2.0 dev (Build: 20090219) (2009021800)
 
 
 $string['grader:manage'] = 'Gestire il registro valutatore';
 $string['grader:view'] = 'Visualizzare il registro valutatore';
 $string['modulename'] = 'Registro valutatore';
+$string['preferences'] = 'Preferenze registro valutatore';
 
 ?>
