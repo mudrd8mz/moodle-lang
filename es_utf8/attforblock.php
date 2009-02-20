4a70e2cfefa73c0ed3075021e224f4b57a4c49a4
Benito Arias <barias>
2009-02-20 11:30:26 +0000

    *** empty log message ***

--- es_utf8/attforblock.php
+++ es_utf8/attforblock.php
@@ -9,8 +9,8 @@ $string['attforblock:takeattendances'] = 'Tomar asistencia';
 $string['attforblock:view'] = 'Ver asistencia';
 $string['attforblock:viewreports'] = 'Ver informes';
 $string['description'] = 'Puede agregar sólo un módulo Asistencia por curso.<br />Si elimina este módulo, ello no afectará a las asistencias. Puede usar este módulo sólo con el bloque Asistencia. Este módulo sólo necesita para incluir calificaciones de las asistencias en las calificaciones de los cursos principales.';
-$string['modulename'] = 'Asistencia (para Bloque)';
-$string['modulenameplural'] = 'Asistencias (para Bloque)';
+$string['modulename'] = 'Asistencia';
+$string['modulenameplural'] = 'Asistencias';
 $string['notfound'] = 'No se ha encontrado actividad de asistencia en este curso.';
 $string['modulenamesimple'] = 'Asistencia'; // ORPHANED
 
