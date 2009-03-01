71cfe4e24eb73beac7b2b476b07179d794377ea4
Nicolas Martignoni <martignoni>
2009-03-01 17:59:26 +0000

    MDL-18355 Forgotten strings file

---
+++ fr_utf8/qtype_essay.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+
+$string['addingessay'] = 'Ajout d\'une question de composition';
+$string['editingessay'] = 'Modification d\'une question de composition';
+$string['essay'] = 'Composition';
+$string['essaysummary'] = 'Permet une réponse de plusieurs phrases ou paragraphes. Cette question doit être évaluée manuellement.'; 
+
+?>
