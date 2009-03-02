03ff3a56c807c0f7b1249c65f53f981ea57c40a7
Benito Arias <barias>
2009-03-02 22:00:52 +0000

    *** empty log message ***

--- es_utf8/block_search.php
+++ es_utf8/block_search.php
@@ -1,9 +1,9 @@
 <?PHP // $Id$ 
-      // block_search.php - created with Moodle 1.9 Beta + (2007082400)
-      // local modifications from http://127.0.0.1/moodle
+      // block_search.php - created with Moodle 1.9.3+ (Build: 20081126) (2007101532)
 
 
 $string['blockname'] = 'Búsqueda global';
+$string['blockssearchswitches'] = 'Activación del indizador para bloques';
 $string['bytes'] = 'bytes (0 significa sin límites)';
 $string['configbuttonlabel'] = 'Etiqueta de botón';
 $string['configenablefileindexing'] = 'Habilitar indexado de archivos';
@@ -11,10 +11,19 @@ $string['configfiletypes'] = 'Tipos de archivo gestionados';
 $string['configlimitindexbody'] = 'Limitación de tamaño de cuerpo indexado';
 $string['configpdftotextcmd'] = 'Ruta al comando pdftotext';
 $string['configsearchtext'] = 'Buscar texto';
+$string['configtypetotxtcmd'] = 'Línea de comandos del convertidor';
+$string['configtypetotxtenv'] = 'Definir contexto para convertidor';
 $string['configwordtotextcmd'] = 'Ruta al comando doctotext';
 $string['configwordtotextenv'] = 'Ajuste de entorno para el convertidor MSWord';
 $string['go'] = '¡Ir!';
+$string['handlingfor'] = 'Manejo extra para';
+$string['modulessearchswitches'] = 'Activación del indizador para módulos';
+$string['nosearchableblocks'] = 'No hay bloques que buscar';
+$string['nosearchablemodules'] = 'No hay módulos que buscar';
+$string['pdfhandling'] = 'Manejo de PDF de Acrobat';
+$string['searchdiscovery'] = 'Descubrir ítems a buscar';
 $string['searchmoodle'] = 'Buscar en Moodle';
 $string['usemoodleroot'] = 'Utilizar la \'root\' de Moodle para convertidores externos';
+$string['wordhandling'] = 'Manejo de Microsoft Word';
 
 ?>
