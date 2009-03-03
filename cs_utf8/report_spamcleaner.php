fc55e33db641f1ca406cd81503baaa0bece9fdd0
David Mudrak <david.mudrak@gmail.com>
2009-03-03 09:25:45 +0000

    New file for a new tool in 1.9.4+

---
+++ cs_utf8/report_spamcleaner.php
@@ -0,0 +1,21 @@
+<?PHP // $Id$ 
+      // report_spamcleaner.php - created with Moodle 1.9.4+ (Build: 20090302) (2007101541)
+
+
+$string['spamauto'] = 'Automaticky detekovat známé typy nežádoucího obsahu';
+$string['spamcannotdelete'] = 'Nelze odstranit tohoto uživatele';
+$string['spamcannotfinduser'] = 'Žádní uživatelé neodpovídají vašemu hledání';
+$string['spamcleaner'] = 'Odstranění nežádoucího obsahu';
+$string['spamcleanerintro'] = 'Tento nástroj prohledává všechny uživatelské profily a na základě nalezených slov odstraňuje takové účty, které jsou evidentně založeny za účelem šíření nežádoucího obsahu. Můžete vyhledávat více nežádoucích termínů najednou, pokud je oddělíte čárkou (např. porno, viagra, casino).<br /><br />Více informací o této problematice najdete v dokumentaci na stránce <a href=\"http://docs.moodle.org/cs/Ne%%C5%%BE%%C3%%A1douc%%C3%%AD_obsah\">Nežádoucí obsah</a>.';
+$string['spamdeleteall'] = 'Odstranit všechny tyto účty';
+$string['spamdeleteallconfirm'] = 'Opravdu mám odstranit tyto uživatele? Tato akce nemůže být vrácena zpět.';
+$string['spamdeleteconfirm'] = 'Opravdu mám odstranit tento záznam? Tato akce nemůže být vrácena zpět.';
+$string['spamdesc'] = 'Popis';
+$string['spameg'] = 'např.: porno, viagra, casino, xxx';
+$string['spamfromblog'] = 'V blogu:';
+$string['spaminvalidresult'] = 'Neplatný výsledek';
+$string['spamoperation'] = 'Operace';
+$string['spamresult'] = 'Výsledek hledání uživatelských profilů, které obsahují:';
+$string['spamsearch'] = 'Hledej výskyt těchto slov';
+
+?>
