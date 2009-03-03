4b9635a9f293d37e524ece6e2f89c16e2b6be0e1
David Mudrak <david.mudrak@gmail.com>
2009-03-03 13:48:44 +0000

    Make the report name shorter to fit into the admin tree block

--- cs_utf8/report_spamcleaner.php
+++ cs_utf8/report_spamcleaner.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // report_spamcleaner.php - created with Moodle 1.9.4+ (Build: 20090302) (2007101541)
+      // report_spamcleaner.php - created with Moodle 1.9.4+ (Build: 20090303) (2007101541)
 
 
 $string['spamauto'] = 'Automaticky detekovat známé typy nežádoucího obsahu';
 $string['spamcannotdelete'] = 'Nelze odstranit tohoto uživatele';
 $string['spamcannotfinduser'] = 'Žádní uživatelé neodpovídají vašemu hledání';
-$string['spamcleaner'] = 'Odstranění nežádoucího obsahu';
+$string['spamcleaner'] = 'Nežádoucí obsah';
 $string['spamcleanerintro'] = 'Tento nástroj prohledává všechny uživatelské profily a na základě nalezených slov odstraňuje takové účty, které jsou evidentně založeny za účelem šíření nežádoucího obsahu. Můžete vyhledávat více nežádoucích termínů najednou, pokud je oddělíte čárkou (např. porno, viagra, casino).<br /><br />Více informací o této problematice najdete v dokumentaci na stránce <a href=\"http://docs.moodle.org/cs/Ne%%C5%%BE%%C3%%A1douc%%C3%%AD_obsah\">Nežádoucí obsah</a>.';
 $string['spamdeleteall'] = 'Odstranit všechny tyto účty';
 $string['spamdeleteallconfirm'] = 'Opravdu mám odstranit tyto uživatele? Tato akce nemůže být vrácena zpět.';
