3a3cd10a953cf871ed7527c2929dee47c819a534
Abel Camacho <ueu_ueu>
2009-03-03 10:08:33 +0000

    New file added

---
+++ eu_utf8/qtype_essay.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // qtype_essay.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['addingessay'] = 'Gehitzen entsegu motako galdera';
+$string['editingessay'] = 'Editatzen entsegu motako galdera';
+$string['essay'] = 'Entsegua';
+$string['essaysummary'] = 'Pare bat esaldi edo paragrafoko erantzuna ematen uzten du. Hauek eskuz kalifikatu behar dira.';
+
+?>
