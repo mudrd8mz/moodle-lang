c6ef531af7b84ffb439b482c53851937f40c3c89
Abel Camacho <ueu_ueu>
2009-03-03 10:15:06 +0000

    New strings added

--- eu_utf8/report_capability.php
+++ eu_utf8/report_capability.php
@@ -1,10 +1,16 @@
 <?PHP // $Id$ 
-      // report_capability.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+      // report_capability.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
 $string['capability'] = 'Gaitasunen txostena';
 $string['capabilitylabel'] = 'Gaitasuna:';
 $string['capabilityreport'] = 'Gaitasunen txostena';
+$string['changeoverrides'] = 'Aldatu baimenak kentzea testuinguru honetan';
+$string['changeroles'] = 'Aldatu rolen definizioak';
+$string['forroles'] = '$a rolerako';
+$string['getreport'] = 'Lortu txostena';
+$string['intro'] = 'Txosten honek erakusten du gaitasun jakin baterako, zein baimen duen gaitasunaren definizioak rol bakoitzerako (edo aukeratutako rolek), eta gaitasuna anulatuta dagoen guneko leku guztietan.';
+$string['reportforcapability'] = '\'$a\' gaitasunerako txostena';
 $string['reportsettings'] = 'Txostenaren ezarpenak';
 $string['roleslabel'] = 'Rolak:';
 
