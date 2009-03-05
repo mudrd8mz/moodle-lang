bb8cdad5a1990e2a433a80fc3811f2649a2d4944
Shamim Rezaie <rezaie9>
2009-03-05 00:40:49 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

--- fa_utf8/langconfig.php
+++ fa_utf8/langconfig.php
@@ -1,6 +1,12 @@
-<?php   // $Id$
-        // author: Shamim Rezaie, http://rezaie.info
+<?php // $Id$
+      // langconfig.php - created with Moodle 1.7 beta + (2006101003)
+      // author: Shamim Rezaie, http://rezaie.info
+
+$string['alphabet'] = 'آ,ا,ب,پ,ت,ث,ج,چ,ح,خ,د,ذ,ر,ز,ژ,س,ش,ص,ض,ط,ظ,ع,غ,ف,ق,ک,گ,ل,م,ن,و,ه,ی';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
+$string['decsep'] = '.'; // decimal point separator, for some languages it is ','
+$string['firstdayofweek'] = '6';
+$string['listsep'] = ','; // if decsep is ',', then usually ';' is used instead here; these two can not be the same
 $string['locale'] = 'fa_IR.UTF-8';
 $string['localewin'] = 'Farsi_Iran.1256';
 $string['localewincharset'] = 'WINDOWS-1256';
@@ -8,12 +14,18 @@ $string['oldcharset'] = 'WINDOWS-1256';
 $string['strftimedate'] = '%%d %%B %%Y';
 $string['strftimedateshort'] = '%%d %%B';
 $string['strftimedatetime'] = '%%d %%B %%Y، %%I:%%M %%p';
+$string['strftimedatetimeshort'] = '%%y/%%m/%%d، %%H:%%M';
 $string['strftimedaydate'] = '%%A، %%d %%B %%Y';
-$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y، %%I:%%M %%p';
+$string['strftimedaydatetime'] = '%%A، %%d %%B %%Y، %%I:%%M %%p';
+$string['strftimedayshort'] = '%%A، %%d %%B';
+$string['strftimedaytime'] = '%%a، %%H:%%M';
+$string['strftimemonthyear'] = '%%B %%Y';
 $string['strftimerecent'] = '%%d %%b، %%H:%%M';
 $string['strftimerecentfull'] = '%%a، %%d %%b %%Y، %%I:%%M %%p';
 $string['strftimetime'] = '%%I:%%M %%p';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'rtl';
 $string['thislanguage'] = 'فارسی';
+$string['thousandssep'] = ','; // thousands separator, for some languages it is '.' or ' '
+
 ?>
