5cb021acf1c8e3dc8f25ce6df3a03375a5afdaa4
Andrea Bicciolo <andreabix>
2009-03-05 11:37:25 +0000

    Aggiornamento traduzione di Andrea Bicciolo

---
+++ it_utf8/qformat_gift.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qformat_gift.php - created with Moodle 2.0 dev (Build: 20090304) (2009030300)
+
+
+$string['nohandler'] = 'Non ci sono gestori per il tipo di domanda $a';
+
+?>
