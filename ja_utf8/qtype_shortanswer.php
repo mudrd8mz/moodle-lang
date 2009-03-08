415165ac85007ed658ecf93c502b02110252a9fb
Mitsuhiro Yoshida <mits>
2009-03-08 18:02:26 +0000

    MDL-18355 Translated a new string for question shortanswer. Adding a new lang file for google docs repository.

--- ja_utf8/qtype_shortanswer.php
+++ ja_utf8/qtype_shortanswer.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // qtype_shortanswer.php - created with Moodle 2.0 dev (Build: 20080909) (2008090800)
+      // qtype_shortanswer.php - created with Moodle 2.0 dev (Build: 20090308) (2009030501)
 
 
 $string['addingshortanswer'] = '記述問題の追加';
@@ -9,5 +9,6 @@ $string['answerno'] = '答え $a';
 $string['editingshortanswer'] = '記述問題の編集';
 $string['filloutoneanswer'] = '1つ以上の考えられる答えを入力してください。空白の答えは使用されません。「*」は、どのような文字にでも合致するワイルドカードとして使用することができます。最初の組み合わせの答えは、評点とフィードバックを決定するため使用されます。';
 $string['shortanswer'] = '記述問題';
+$string['shortanswersummary'] = '様々な答えのモデルと比較して評定される、1つまたは少数の言葉を入力することができます。答えには、ワイルドカードを含むことができます。';
 
 ?>
