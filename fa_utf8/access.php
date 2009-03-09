1d5f5a22128745f9a54350db9ed286efe9df3ed5
Shamim Rezaie <rezaie9>
2009-03-09 14:26:40 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/access.php
@@ -0,0 +1,29 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // access.php - created with Moodle 1.7 beta + (2006101003)
+      // Accessibility, usability and navigation language file.
+
+$string['access'] = 'دسترسی';
+$string['accesshelp'] = 'راهنمای دسترسی';
+$string['accesskey'] = 'کلید دستیابی، $a';
+$string['accessstatement'] = 'شرح میزان دسترسی';
+$string['activitynext'] = 'فعالیت بعدی';
+$string['activityprev'] = 'فعالیت قبلی';
+$string['breadcrumb'] = 'دنبالهٔ راهبری';
+$string['currentweek'] = 'هفتهٔ جاری ';
+$string['currenttopic'] = 'موضوع جاری ';
+$string['monthnext'] = 'ماه بعد';
+$string['monthprev'] = 'ماه قبل';
+$string['showblocka'] = 'نمایش بلوک $a';
+$string['hideblocka'] = 'نهفتن بلوک $a';
+$string['sitemap'] = 'معماری سایت';
+$string['skipa'] = 'گذشتن از $a';
+$string['skipblock'] = 'گذشتن از بلوک';
+$string['skipnavigation'] = 'گذشتن از راهبری';
+$string['tabledata'] = 'جدول داده‌ها، $a';
+$string['tablelayout'] = 'جدول طرح‌بندی، $a';
+$string['tocontent'] = 'پرش به محتوای اصلی';
+$string['tonavigation'] = 'رفتن به راهبری';
+$string['youarehere'] = 'شما اینجا هستید';
+
+?>
