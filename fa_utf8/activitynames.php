d22d64fe6ed3ed96d6c02254a245b495ebb44352
Shamim Rezaie <rezaie9>
2009-03-09 02:02:19 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/activitynames.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // activitynames.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['filtername'] = 'پیوند شدن خودکار نام فعالیت‌ها';
+
+?>
