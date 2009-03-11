3c3ba68aeb2a81c0682276d50b7f345562d87fd8
Shamim Rezaie <rezaie9>
2009-03-11 13:52:35 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/quiz_regrade.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // quiz_regrade.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['regrade'] = 'نمره‌دهی مجدد';
+
+?>
