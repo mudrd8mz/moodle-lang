d0d6824fc1986fef38a72bf8b75c4463e3851166
Nicolas Martignoni <martignoni>
2009-03-14 13:18:33 +0000

    MDL-18520, new lang file added

---
+++ fr_utf8/repository_filesystem.php
@@ -0,0 +1,9 @@
+<?php // $Id$
+
+$string['configplugin'] = 'Configuration dépôt système de fichiers';
+$string['repositoryname'] = 'Système de fichiers';
+$string['repositorydesc'] = 'Créer un dépôt à partir d\'un dossier local';
+$string['path'] = 'Chemin d\'accès';
+$string['invalidpath'] = 'Chemin d\'accès non valide';
+
+?>
