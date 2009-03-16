151f2dd6872045ad8b426a6c55ba151e2e4e4b6e
Mitsuhiro Yoshida <mits>
2009-03-16 20:27:09 +0000

    MDL-18555 Adding a translated lang file for unsupported question types.

---
+++ ja_utf8/qformat_xml.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qformat_xml.php - created with Moodle 2.0 dev (Build: 20090315) (2009030501)
+
+
+$string['invalidxml'] = '無効なxmlファイルです - ストリングが必要です (CDATAを使用しますか?)';
+$string['unsupportedexport'] = 'xmlエクスポートでは、問題タイプ $a はサポートされていません。';
+
+?>
