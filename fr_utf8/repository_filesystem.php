58218559606172ddcb4e6f73b7e322e8a8b415a4
Nicolas Martignoni <martignoni>
2009-03-17 18:29:26 +0000

    MDL-18520 New strings and sort strings

--- fr_utf8/repository_filesystem.php
+++ fr_utf8/repository_filesystem.php
@@ -1,9 +1,11 @@
 <?php // $Id$
 
+$string['blockedpath'] = 'Chemin d\'accès bloqué';
 $string['configplugin'] = 'Configuration dépôt système de fichiers';
+$string['donotusesysdir'] = 'N\'utilisez pas le dossier système comme dépôt de fichiers, notamment';
+$string['invalidpath'] = 'Chemin d\'accès non valide';
+$string['path'] = 'Chemin d\'accès';
 $string['repositoryname'] = 'Système de fichiers';
 $string['repositorydesc'] = 'Créer un dépôt à partir d\'un dossier local';
-$string['path'] = 'Chemin d\'accès';
-$string['invalidpath'] = 'Chemin d\'accès non valide';
 
 ?>
