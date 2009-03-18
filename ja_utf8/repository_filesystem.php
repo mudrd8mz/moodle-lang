4600468568e9e9379f1730c722c3e7fe23612ddb
Mitsuhiro Yoshida <mits>
2009-03-18 06:20:49 +0000

    MDL-18520 Translated new strings for file system plugin.

--- ja_utf8/repository_filesystem.php
+++ ja_utf8/repository_filesystem.php
@@ -2,7 +2,9 @@
       // repository_filesystem.php - created with Moodle 2.0 dev (Build: 20090315) (2009030501)
 
 
+$string['blockedpath'] = 'ブロックされたrootパス';
 $string['configplugin'] = 'ファイルシステムのリポジトリ設定';
+$string['donotusesysdir'] = '以下を含むシステムディレクトリをリポジトリに使用しないでください。';
 $string['invalidpath'] = '無効なrootパス';
 $string['path'] = 'パス';
 $string['repositorydesc'] = 'ローカルディレクトリからリポジトリを作成する';
