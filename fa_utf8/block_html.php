fbad4beef1878f2cce7be591eb5dbc76352d209c
Shamim Rezaie <rezaie9>
2009-03-18 15:07:02 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/block_html.php
@@ -0,0 +1,11 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // block_html.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['configcontent'] = 'محتوا';
+$string['configtitle'] = 'عنوان بلوک';
+$string['html'] = 'HTML';
+$string['leaveblanktohide'] = 'خالی بگذارید تا عنوان پنهان باشد';
+$string['newhtmlblock'] = '(بلوک HTML جدید)';
+
+?>
