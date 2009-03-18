589ca8e18bf1bb2e161ef41f3cafb00c85c530a6
Shamim Rezaie <rezaie9>
2009-03-18 18:47:05 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/tag.php
@@ -0,0 +1,55 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info      
+
+$string['addedotag'] = '«{$a}» بعنوان یک برچسب رسمی اضافه شد.';
+$string['addotags'] = 'اضافه کردن برچسب رسمی';
+$string['addtagtomyinterests'] =  'اضافه شدن «{$a}» به علایق من';
+$string['blocktagstitle'] =  'برچسب‌ها';
+$string['count'] = 'تعداد';
+$string['changename'] = 'تغییر نام بده';
+$string['changetype'] = 'تغییر نوع بده';
+$string['description'] = 'توصیف';
+$string['delete'] = 'حذف کن';
+$string['deleted'] = 'حذف شد';
+$string['edittag'] = 'ویرایش این برچسب';
+$string['flag'] = 'علامت';
+$string['flagasinappropriate'] = 'علامتگذاری بعنوان نامناسب';
+$string['helprelatedtags'] = 'برچسب‌های مرتبط جداشده توسط کاما';
+$string['id'] = 'شناسه';
+$string['relatedblogs'] = 'مطالب اخیر بلاگ که این برچسب را دارند';
+$string['managetags'] = 'مدیریت برچسب‌ها';
+$string['name'] = 'نام برچسب';
+$string['namesalreadybeeingused'] = 'نام برچسب‌ها هم‌اکنون در حال استفاده است';
+$string['newname'] = 'نام جدید برچسب';
+$string['noresultsfor'] = 'نتیجه‌ای برای «{$a}» وجود نداشت';
+$string['officialtag'] = 'برچسب رسمی';
+$string['owner'] = 'مالک';
+$string['otags'] = 'برچسب‌های رسمی';
+$string['ptags'] = 'برچسب‌های تعریف شده توسط کاربر (جدا شده توسط کاما)';
+$string['relatedtags'] = 'برچسب‌های مرتبط';
+$string['removetagfrommyinterests'] = 'حذف «{$a}» از لیست علایق من';
+$string['responsiblewillbenotified'] = 'شخص مسئول مطلع خواهد شد';
+$string['resetflag'] = 'بدون علامت کن';
+$string['reset'] = 'حذف علامت';
+$string['search'] = 'جستجو';
+$string['seeallblogs'] = 'مشاهدهٔ همهٔ بلاگ‌هایی که این برچسب را دارند';
+$string['searchresultsfor'] = 'نتایج جستجو بدنبال «{$a}»';
+$string['searchtags'] = 'جستجوی برچسب‌ها';
+$string['select'] = 'انتخاب';
+$string['tagdescription'] = 'توصیف برچسب';
+$string['tag'] = 'برچسب';
+$string['tagtype'] = 'نوع برچسب';
+$string['tagtype_official'] = 'رسمی';
+$string['tagtype_default'] = 'پیش‌فرض';
+$string['tags'] = 'برچسب‌ها';
+$string['tagsaredisabled'] = 'برچسب‌ها غیرفعال شده‌اند';
+$string['thingstaggedwith'] = '$a->count چیز برچسب «{$a->name}» را دارند';
+$string['thistaghasnodesc'] = 'این برچسب هم‌اکنون فاقد توصیف است.';
+$string['timemodified'] = 'تغییر';
+$string['typechanged'] = 'نوع برچسب تغییر یافت';
+$string['userstaggedwith'] = 'کاربرانی که برچسب «{$a}» را دارند';
+$string['updatetag'] = 'به‌روزرسانی';
+$string['updated'] = 'به‌روز شد';
+$string['withselectedtags'] = 'برچسب‌های انتخاب‌شده را...';
+
+?>
