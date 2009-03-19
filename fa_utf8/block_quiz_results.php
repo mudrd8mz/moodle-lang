3866a532e1fa3c6b2675c97cfe9754cdf3505213
Shamim Rezaie <rezaie9>
2009-03-19 10:17:32 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/block_quiz_results.php
@@ -0,0 +1,32 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // block_quiz_results.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['bestgrade'] = 'بیشترین نمره:';
+$string['bestgrades'] = 'بیشترین $a نمره:';
+$string['bestgroupgrade'] = 'گروهی که بالاترین میانگین را دارد:';
+$string['bestgroupgrades'] = '$a گروهی که بالاترین میانگین را دارند:';
+$string['config_format_absolute'] = 'اعداد خام';
+$string['config_format_fraction'] = 'کسری';
+$string['config_format_percentage'] = 'درصد';
+$string['config_grade_format'] = 'نمایش نمره‌ها به صورت:';
+$string['config_name_format'] = 'میزان پوشیدگی نتایج نمایش داده شده:';
+$string['config_names_anon'] = 'نتایج بدون نام';
+$string['config_names_full'] = 'نمایش نام‌ها به صورت کامل';
+$string['config_names_id'] = 'فقط نمایش کدهای شناسائی';
+$string['config_no_quizzes_in_course'] = 'این درس هیچ آزمونی ندارد. باید حداقل یک آزمون اضافه نمائید تا بتوانید بدرستی از این بلوک استفاده کنید.';
+$string['config_select_quiz'] = 'این بلوک باید نتایج کدام آزمون را نمایش دهد؟';
+$string['config_show_best'] = 'چند نمرهٔ بالا باید نمایش داده شود (برای غیرفعال شدن عدد صفر را وارد نمائید)؟';
+$string['config_show_worst'] = 'چند نمرهٔ پائین باید نمایش داده شود (برای غیرفعال شدن عدد صفر را وارد نمائید)؟';
+$string['config_use_groups'] = 'نمایش گروه‌ها به جای شاگردها (فقط در صورتی که آزمون از گروه‌ها پشتیبانی کند)؟';
+$string['configuredtoshownothing'] = 'پیکربندی این بلوک به گونه‌ای است که اجازهٔ نمایش هیچ نتیجه‌ای را نمی‌دهد. این بلوک را یا پیکربندی نمائید، یا پنهان کنید.';
+$string['error_emptyquizid'] = 'این بلوک در حال حاضر ایرادی دارد: باید تعیین کنید که نتایج کدام آزمون باید نمایش داده شود.';
+$string['error_emptyquizrecord'] = 'این بلوک در حال حاضر ایرادی دارد: آزمون انتخاب شده در پایگاه داده وجود ندارد.';
+$string['error_nogroupsexist'] = 'این بلوک در حال حاضر ایرادی دارد: تنظیم شده است که نمره‌ها را در حالت گروهی نمایش دهد، ولی در درس گروهی تعریف نشده است.';
+$string['formaltitle'] = 'نتایج آزمون';
+$string['worstgrade'] = 'کمترین نمره:';
+$string['worstgrades'] = 'کمترین $a نمره:';
+$string['worstgroupgrade'] = 'گروهی که کمترین میانگین را دارد:';
+$string['worstgroupgrades'] = '$a گروهی که کمترین میانگین را دارند:';
+
+?>
