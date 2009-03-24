f9fe443a286364d39279165ea3ccac87273a0717
Shamim Rezaie <rezaie9>
2009-03-24 21:10:43 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/quiz_analysis.php
@@ -0,0 +1,33 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // quiz_analysis.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['analysis'] = 'تحلیل موردی';
+$string['analysisdownload'] = 'دریافت فایل تحلیل';
+$string['analysisoptions'] = 'اختیارات تحلیل';
+$string['analysistitle'] = 'جدول تحلیل موردی';
+$string['attemptsall'] = 'همهٔ دفعات';
+$string['attemptselection'] = 'کدام دفعه شرکت‌های هر کاربر تحلیل شوند؟';
+$string['attemptsfirst'] = 'اولین دفعه';
+$string['attemptshighest'] = 'بیشترین نمره';
+$string['attemptslast'] = 'آخرین دفعه';
+$string['dicsindextitle'] = 'شاخص<br />تمایز';
+$string['disccoefftitle'] = 'ضریب<br />تمیز';
+$string['downloadooo'] = 'دریافت در قالب فایل اُپن آفیس';
+$string['facilitytitle'] = 'درصد پاسخ صحیح<br />میزان آسانی';
+$string['lowmarkslimit'] = 'در صورتیکه امتیاز کمتر از این مقدار بود، تحلیل نشود:';
+$string['pagesize'] = 'تعداد سؤال‌ها در هر صفحه:';
+$string['qcounttitle'] = 'تعداد سؤال';
+$string['qidtitle'] = 'شماره<br />سؤال';
+$string['qnametitle'] = 'نام سؤال';
+$string['qtexttitle'] = 'متن سؤال';
+$string['qtypetitle'] = 'نوع سؤال';
+$string['quizreportdir'] = 'gozaresh_haye_azmoon';
+$string['rcounttitle'] = 'تعداد پاسخ‌های<br />صحیح';
+$string['reportanalysis'] = 'گزارش تحلیل موردی';
+$string['responsestitle'] = 'متن پاسخ';
+$string['rfractiontitle'] = 'امتیاز<br />هر قسمت';
+$string['rpercenttitle'] = 'درصد پاسخ‌های<br />صحیح';
+$string['stddevtitle'] = 'انحراف معیار';
+
+?>
