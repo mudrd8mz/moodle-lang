253e6dd4cd0cffcf4126b82cc0495a624b60d273
Shamim Rezaie <rezaie9>
2009-03-24 13:08:46 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/quiz_grading.php
@@ -0,0 +1,18 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // quiz_grading.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['essayonly'] = 'سؤال‌های زیر باید به صورت دستی نمره‌دهی شوند';
+$string['grading'] = 'نمره‌دهی دستی';
+$string['gradeall'] = 'نمره‌دهی هر $a دفعه تلاش';
+$string['graded'] = '(نمره‌دهی شده است)';
+$string['gradeungraded'] = 'نمره‌دهی همهٔ $a دفعهٔ بدون نمره';
+$string['gradenextungraded'] = 'نمره‌دهی $a دفعه تلاش بعدی بدون نمره';
+$string['gradinguser'] = 'دفعات شرکت $a';
+$string['gradingall'] = 'همهٔ $a دفعه تلاش صورت گرفته روی این سؤال.';
+$string['gradingattempt'] = 'دفعهٔ $a->attempt ام از تلاش‌های $a->fullname.';
+$string['gradingnextungraded'] = '$a تلاش بدون نمرهٔ بعدی';
+$string['gradingnotallowed'] = 'شما مجوز نمره‌دهی دستی پاسخ‌ها در این آزمون را ندارید';
+$string['gradingungraded'] = '$a دفعه شرکت بدون نمره';
+$string['questiontitle'] = 'سؤال $a->number : «{$a->name}» ({$a->openspan}$a->gradedattempts{$a->closespan} دفعه از کل $a->totalattempts دفعه تلاش {$a->openspan}نمره‌دهی شده است{$a->closespan}).';
+?>
