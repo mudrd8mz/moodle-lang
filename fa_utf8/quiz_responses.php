2334cd3b71a735076a47b1b9340c8cbaccc1bbfc
Shamim Rezaie <rezaie9>
2009-03-24 16:47:49 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/quiz_responses.php
@@ -0,0 +1,11 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // quiz_responses.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['pagesize'] = 'تعداد دفعات در هر صفحه:';
+$string['reportresponses'] = 'پاسخ‌های مفصل';
+$string['responses'] = 'پاسخ‌های مفصل';
+$string['responsesoptions'] = 'اختیارات پاسخ‌ها';
+$string['responsestitle'] = 'پاسخ‌های مفصل';
+
+?>
