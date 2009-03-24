7357270db1d40dd66615d3e3c8ed9f145d47a23d
Shamim Rezaie <rezaie9>
2009-03-24 14:16:45 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/quiz_overview.php
@@ -0,0 +1,30 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // quiz_overview.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['allattempts'] = 'نمایش همهٔ دفعات';
+$string['allattemptscontributetograde'] = 'همهٔ دفعات شرکت در آزمون روی نمرهٔ نهایی کاربر تأثیرگذار هستند.';
+$string['allstudents'] = 'نمایش همهٔ $a';
+$string['attemptsonly'] = 'فقط نمایش آن دسته از «{$a}» که در آزمون شرکت کرده‌اند';
+$string['attemptsprepage'] = 'دفعات نمایش داده شده در هر صفحه';
+$string['deleteselected'] = 'حذف دفعات انتخاب شده';
+$string['highlightinggraded'] = 'دفعات شرکت در آزمونی که در نمرهٔ نهایی تأثیرگذار بوده‌اند به صورت رنگی مشخص شده‌اند.';
+$string['noattemptsonly'] = 'فقط نمایش / دریافت آن دسته از «{$a}» که در آزمون شرکت نکرده‌اند';
+$string['onlyoneattemptallowed'] = 'هر کاربر فقط یک بار مجاز به شرکت در این آزمون است.';
+$string['optallattempts'] = 'همهٔ دفعات';
+$string['optallstudents'] = 'همهٔ «{$a}»';
+$string['optattemptsonly'] = 'فقط آن دسته از «{$a}» که در آزمون شرکت کرده‌اند';
+$string['optnoattemptsonly'] = 'فقط آن دسته از «{$a}» که در آزمون شرکت نکرده‌اند';
+$string['optonlygradedattempts'] = 'فقط دفعه‌ای از شرکت‌های هر کاربر که نمره‌دهی شده است ($a)';
+$string['overview'] = 'مرور اجمالی';
+$string['overviewdownload'] = 'دریافت فایل مرور اجمالی';
+$string['overviewreportgraph'] = 'نمودار میله‌ای تعداد شاگردانی که نمرهٔ آن‌ها در هر یک از محدوده‌ها قرار دارد';
+$string['pagesize'] = 'اندازهٔ صفحه';
+$string['preferencespage'] = 'ترجیحات مربوط به این صفحه';
+$string['preferencessave'] = 'ذخیرهٔ ترجیحات';
+$string['preferencesuser'] = 'ترجیحات شما در مورد این گزارش';
+$string['show'] = 'نمایش / دریافت';
+$string['showinggraded'] = 'فقط نمایش دفعه‌ای از شرکت‌های هر کاربر که نمره‌دهی شده است.';
+$string['showinggradedandungraded'] = 'نمایش دفعه‌های شرکت نمره‌دهی شده و نشده برای هر کاربر. دفعه‌ای از شرکت‌ها که نمره‌دهی شده است به صورت رنگی مشخص شده است. نمره‌دهی این آزمون بر اساس $a است.';
+$string['showdetailedmarks'] = 'نمایش / دریافت امتیاز کسب شده از هر سؤال';
+?>
