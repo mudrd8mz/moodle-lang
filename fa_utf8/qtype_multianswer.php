58fba138359d18d3646cea1679f0af5a13674377
Shamim Rezaie <rezaie9>
2009-03-26 16:09:57 +0000

    new strings for Moodle 1.9 from Shamim Rezaie (http://rezaie.info)

--- fa_utf8/qtype_multianswer.php
+++ fa_utf8/qtype_multianswer.php
@@ -1,4 +1,21 @@
 <?php   // $Id$
         // author: Shamim Rezaie, http://rezaie.info
+$string['addingmultianswer'] = 'تعریف یک سؤال با جواب‌های جاسازی شده (Cloze)';
+$string['correctanswer'] = 'پاسخ صحیح';
+$string['correctanswerandfeedback'] = 'پاسخ صحیح و بازخورد';
 $string['decodeverifyquestiontext'] = 'رمزگشایی و بازبینی متن سؤال';
+$string['editingmultianswer'] = 'ویرایش یک سؤال با جواب‌های جاسازی شده (Cloze)';
+$string['layout'] = 'آرایش';
+$string['layoutselectinline'] = 'منوی بازشونده جایگذاری شده در متن';
+$string['layouthorizontal'] = 'مجموعه‌ای از گزینه‌ها در یک سطر افقی';
+$string['layoutundefined'] = 'آرایش تعریف‌نشده';
+$string['layoutvertical'] = 'مجموعه‌ای از گزینه‌ها در یک ستون عمودی';
+$string['multianswer'] = 'جواب‌های جاسازی شده (Cloze)';
+$string['nooptionsforsubquestion'] = 'دریافت گزینه‌ها برای سؤال قسمت $a->sub (question->id={$a->id}) مقدور نیست.';
+$string['noquestions'] = 'سؤال Cloze(دارای جواب‌های جاسازی شده) «<strong>$a</strong>» شامل هیچ سؤالی نیست ';
+$string['qtypenotrecognized'] = 'نوع $a تشخیص داده نشد';
+$string['questionnotfound'] = 'سؤالی برای قسمت شمارهٔ $a سؤال وجود ندارد';
+$string['questionsmissing'] = 'سؤال معتبری وجود ندارد، حداقل یک سؤال تعریف نمائید.';
+$string['questiondefinition'] = 'تعریف سؤال';
+$string['unknownquestiontypeofsubquestion'] = 'سؤال از نوع ناشناخته: $a->type مربوط به سؤال قسمت $a->sub';
 ?>
