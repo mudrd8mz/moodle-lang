952c878014d14b8abb8eb0bd766eb2bd2a7ecc1b
Shamim Rezaie <rezaie9>
2009-03-27 00:52:26 +0000

    new strings from Shamim Rezaie (http://rezaie.info)

---
+++ fa_utf8/question.php
@@ -0,0 +1,99 @@
+<?php // $Id$
+      // author: Shamim Rezaie, http://rezaie.info
+      // question.php - created with Moodle 1.8 dev
+$string['adminreport'] = 'گزارش مشکلات احتمالی در پایگاه دادهٔ سؤالات شما.';
+$string['bظroken'] = 'این یک «پیوند شکسته» است و به فایلی که وجود ندارد اشاره می‌کند.';
+$string['byandon'] = '<em>$a->time</em> توسط <em>$a->user</em>';
+$string['categorydoesnotexist'] = 'این دسته وجود ندارد';
+$string['categorycurrent'] = 'دستهٔ فعلی';
+$string['categorycurrentuse'] = 'استفاده از این دسته';
+$string['categorymoveto'] = 'ذخیره در دستهٔ';
+$string['changepublishstatuscat'] = 'وضعیت اشتراکی <a href=\"$a->caturl\">دستهٔ «{$a->name}»</a>در درس «{$a->coursename}» از <strong>$a->changefrom به $a->changeto</strong> تغییر پیدا خواهد کرد.';
+$string['cwrqpfs'] = 'سؤال‌های تصادفی ای که سؤال آن‌ها از زیردسته‌ها انتخاب می‌شود.';
+$string['cwrqpfsinfo'] = '<p>حین ارتقاء به مودل ۱٫۹ دسته‌های سؤال به فضاهای جداگانه تقسیم خواهند شد.
+وضعیت اشتراکی بعضی از دسته‌های سؤال و سؤال‌های سایت شما تغییر خواهد کرد.
+این کار در وضعیت نادری که یک یا چند سؤال «تصادفی» در یک آزمون به صورت انتخاب از بین مخلوطی از
+دسته‌های مشترک و غیرمشترک تنظیم شده‌اند (مانند این سایت) لازم است. این وضعیت زمانی رخ می‌دهد که یک سؤال تصادفی
+به صورت انتخاب از زیردسته‌ها تنظیم شده باشد و یک یا چند زیردسته وضعیت اشتراکی متفاوتی نسبت به دستهٔ منشأیی که
+سؤال تصادفی در آن ایجاد شده است داشته باشند.</p>
+<p>در حین ارتقاء به مودل ۱٫۹، وضعیت اشتراکی دسته‌های سؤال زیر، که سؤال‌های «تصادفی» دسته‌های بالاتر
+از بین آن‌ها انتخاب می‌شود، به وضعیت اشتراکی یکسانی نسبت به دسته‌ای که سؤال «تصادفی» در آن است
+تغییر خواهد کرد. وضعیت اشتراکی دسته‌های زیر تغییر خواهد کرد. سؤال‌هایی که تغییر کنند
+در تمام آزمون‌های موجود همچنان کار خواهند کرد تا زمانی که آن‌ها را از این آزمون‌ها حذف نمائید.</p>';
+$string['cwrqpfsnoprob'] = 'هیچ دستهٔ سؤالی در سایت شما دچار مشکل «سؤال‌های تصادفی ای که سؤال آن‌ها از زیردسته‌ها انتخاب می‌شود» نشده است.';
+$string['copy']= 'کپی شدن از $a و تغییر کردن پیوندها.';
+$string['created'] = 'ایجاد';
+$string['createdmodifiedheader'] = 'ایجاد / آخرین تغییر';
+$string['defaultfor'] = 'پیش‌فرض برای $a';
+$string['defaultinfofor'] = 'دستهٔ پیش‌فرض برای سؤال‌های مشترک در «{$a}».';
+$string['deletecoursecategorywithquestions'] = 'در بانک سؤال، سؤال‌هایی وجود دارد که به این طبقهٔ درسی وابسته است. در صورتی که ادامه دهید، آن‌ها حذف خواهند شد. ممکن است که ابتدا مایل به انتقال آن‌ها از طریق واسط بانک سؤال باشید.';
+$string['donothing']= 'فایل‌ها کپی یا منتقل نشده و پیوندها نیز تغییر داده نشوند.';
+$string['editingcategory'] = 'در حال ویرایش یک دسته';
+$string['editingquestion'] = 'در حال ویرایش یک سؤال';
+$string['erroraccessingcontext'] = 'فضا قابل دستیابی نیست';
+$string['errordeletingquestionsfromcategory'] = 'مواجهه با خطا هنگام حذف سؤال‌ها از دستهٔ $a.';
+$string['errorfilecannotbecopied'] = 'خطا! فایل $a قابل کپی شدن نیست.';
+$string['errorfilecannotbemoved'] = 'خطا! فایل $a قابل انتقال نیست.';
+$string['errorfileschanged'] = 'خطا! فایل‌هایی که از سؤال‌ها به آن‌ها پیوند داده شده است از زمان نمایش داده شدن فرم تغییر کرده‌اند.';
+$string['errormanualgradeoutofrange'] = 'نمرهٔ $a->grade برای سؤال «{$a->name}» بین ۰ و $a->maxgrade نیست. توضیح و نمره ذخیره نشدند.';
+$string['errormovingquestions'] = 'مواجهه با خطا هنگام انتقال سؤال‌ها با شناسه‌های $a.';
+$string['errorprocessingresponses'] = 'هنگام پردازش پاسخ‌های شما خطائی روی داد.';
+$string['errorsavingcomment'] = 'مواجهه با خطا هنگام ذخیرهٔ توضیح داده شده روی سؤال «{$a->name}» در پایگاه داده.';
+$string['errorupdatingattempt'] = 'مواجهه با خطا هنگام به‌روزرسانی تلاش $a->id در پایگاه داده.';
+$string['exportcategory'] = 'صدور دسته';
+$string['filesareasite']= 'محل فایل‌های سایت';
+$string['filesareacourse']= 'محل فایل‌های درس';
+$string['filestomove']= 'انتقال / کپی فایل‌ها به $a؟';
+$string['fractionsnomax'] = 'یکی از پاسخ‌ها باید نمرهٔ ۱۰۰٪ داشته باشد تا بتوان نمرهٔ کامل این سؤال را به دست آورد.';
+$string['getcategoryfromfile'] = 'گرفتن دسته از فایل';
+$string['getcontextfromfile'] = 'گرفتن فضا از فایل';
+$string['ignorebroken'] = 'نادیده گرفتن پیوندهای شکسته';
+$string['invalidcontextinhasanyquestions'] = 'فضای نامعتبری بعنوان پارامتر به تابع question_context_has_any_questions فرستاده شده است.';
+$string['linkedfiledoesntexist'] = 'فایل پیوند داده شدهٔ $a وجود ندارد';
+$string['makechildof'] = 'زیرمجموعهٔ «{$a}» شدن';
+$string['maketoplevelitem'] = 'انتقال به سطح مبدأ';
+$string['missingimportantcode'] = '<u>قسمت مهمی از کد</u> برای این نوع سؤال پیاده‌سازی نشده است: $a';
+$string['modified'] = 'آخرین تغییر';
+$string['move']= 'منتقل شدن از $a و تغییر کردن پیوندها.';
+$string['movecategory']= 'انتقال دسته';
+$string['movedquestionsandcategories'] = 'انتقال سؤال‌ها و دسته‌های سؤال از $a->oldplace به $a->newplace.';
+$string['movelinksonly']= 'فقط محل اشارهٔ پیوند تغییر یابد ولی فایل‌ها منتقل یا کپی نشوند.';
+$string['moveqtoanothercontext']= 'انتقال سؤال به فضایی دیگر.';
+$string['moveq']= 'انتقال سؤال(ها)';
+$string['movingcategory']= 'منتقل شدن دسته';
+$string['movingcategoryandfiles']= 'آیا مطمئن هستید که می‌خواهید دستهٔ {$a->name} و همهٔ دسته‌های زیرمجموعه را به فضای مربوط به «{$a->contextto}» منتقل نمائید؟<br />{$a->urlcount} فایل وجود دارد که از سؤال‌های موجود در {$a->fromareaname} به آن(ها) پیوند داده شده است. آیا مایل به کپی یا انتقال این فایل‌ها به {$a->toareaname} هستید؟';
+$string['movingcategorynofiles']= 'آیا مطمئن هستید که می‌خواهید دستهٔ {$a->name} و همهٔ دسته‌های زیرمجموعه را به فضای مربوط به «{$a->contextto}» منتقل نمائید؟';
+$string['movingquestions'] = 'انتقال سؤال‌ها و همهٔ فایل‌ها';
+$string['movingquestionsandfiles']= 'آیا مطمئن هستید که می‌خواهید سؤال(های) {$a->questions} را به فضای مربوط به <strong>«{$a->tocontext}»</strong> منتقل نمائید؟<br /><strong>{$a->urlcount} فایل</strong> در {$a->fromareaname} وجود دارد که از این سؤال(ها) به آن(ها) پیوند داده شده است. آیا مایلید که این فایل(ها) را به {$a->toareaname} منتقل، یا در آنجا کپی نمائید؟';
+$string['movingquestionsnofiles']=  'آیا مطمئن هستید که می‌خواهید سؤال(های) {$a->questions} را به فضای مربوط به <strong>«{$a->tocontext}»</strong> منتقل نمائید؟<br />فایلی در {$a->fromareaname} وجود <strong>ندارد</strong> که از این سؤال(ها) به آن پیوند داده شده باشد.';
+$string['needtochoosecat'] = 'باید یک دسته را برای انتقال این سؤال به آن انتخاب نمائید و یا اینکه دکمهٔ «لغو» را فشار دهید.';
+$string['nopermissionadd'] = 'شما مجوز اضافه کردن سؤال به این قسمت را ندارید.';
+$string['nopermissionmove'] = 'شما مجوز انتقال سؤال‌ها از اینجا را ندارید. باید سؤال را در این دسته ذخیره کنید یا اینکه آن را بعنوان یک سؤال جدید ذخیره نمائید.';
+$string['noprobs'] = 'مشکلی در پایگاه دادهٔ سؤالات شما وجود ندارد.';
+$string['notenoughdatatoeditaquestion'] = 'نه شناسهٔ سؤال مشخص شده است و نه شناسهٔ دسته و نوع سؤال.';
+$string['notenoughdatatomovequestions'] = 'باید شناسهٔ سؤال‌هایی که می‌خواهید منتقل شوند را ارائه نمائید.';
+$string['permissionedit'] = 'این سؤال را ویرایش کنید';
+$string['permissionmove'] = 'این سؤال را منتقل کنید';
+$string['permissionsaveasnew'] = 'این سؤال را به صورت یک سؤال جدید ذخیره کنید';
+$string['permissionto'] = 'شما می‌توانید:';
+$string['published'] = 'به اشتراک گذاشته شده';
+$string['questionaffected'] = '<a href=\"$a->qurl\">سؤال «{$a->name}» ($a->qtype)</a> در این دستهٔ سؤال‌ها قرار دارد ولی در <a href=\"$a->qurl\">آزمون «{$a->quizname}»</a> در درس «{$a->coursename}» نیز در حال استفاده است.';
+$string['questionbank'] = 'بانک سؤال';
+$string['questioncategory'] = 'دستهٔ سؤال‌ها';
+$string['questioncatsfor'] = 'دسته‌های سؤال مربوط به «{$a}»';
+$string['questiondoesnotexist'] = 'این سؤال وجود ندارد';
+$string['questionsmovedto'] = 'سؤال‌هایی که همچنان در حال استفاده هستند به «{$a}» در طبقهٔ درسی مبدأ منتقل شدند.';
+$string['questionsrescuedfrom'] = 'سؤال‌هایی که از فضای $a ذخیره شده‌اند.';
+$string['questionsrescuedfrominfo'] = 'این سؤال‌ها (که برخی از آن‌ها ممکن است پنهان باشند) هنگامی که فضای $a حذف شد ذخیره شدند زیرا همچنان توسط بعضی از آزمون‌ها یا سایر فعالیت‌ها در حال استفاده بودند.';
+$string['questionuse'] = 'استفاده از سؤال در این دسته';
+$string['shareincontext'] = 'به اشتراک گذاشته شدن در فضای مربوط به «{$a}»';
+$string['tofilecategory'] = 'نوشتن دسته در فایل';
+$string['tofilecontext'] = 'نوشتن فضا در فایل';
+$string['unknown'] = 'نامعلوم';
+$string['unknownquestiontype'] = 'نوع سؤال ناشناخته است: $a.';
+$string['unpublished'] = 'به اشتراک گذاشته نشده';
+$string['upgradeproblemcategoryloop'] = 'هنگام ارتقاء دسته‌های سؤال مشکلی تشخیص داده شد. در یاختار درختی دسته‌ها حلقه‌ای وجود دارد. شناسهٔ دستهٔ مشکل دار $a است.';
+$string['upgradeproblemcouldnotupdatecategory'] = 'دستهٔ سؤال‌های $a->name ($a->id) قابل به‌روزرسانی نیست.';
+$string['upgradeproblemunknowncategory'] = 'هنگام ارتقاء دسته‌های سؤال مشکلی تشخیص داده شد. دستهٔ $a->id زیرمجموعهٔ دستهٔ $a->parent است که وجود ندارد. برای حل مشکل محل دسته تغییر داده شد.';
+$string['yourfileshoulddownload'] = 'دریافت فایل صدور باید به‌زودی شروع شود. اگر نشد، لطفاً روی <a href=\"$a\">این پیوند</a> کلیک نمائید. برای حل مشکل مبدأ تغییر داده شد.';
+?>
