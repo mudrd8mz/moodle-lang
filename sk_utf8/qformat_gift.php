7831b0d3fb57ae53ec721b4b9c2e2479fc6ad556
Miroslav Fikar <fikar>
2009-04-02 19:53:07 +0000

    Initial revision

---
+++ sk_utf8/qformat_gift.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qformat_gift.php - created with Moodle 1.9.4+ (Build: 20090401) (2007101545.01)
+
+
+$string['nohandler'] = 'Pre otázku typu $a chýba metóda spracovania';
+
+?>
---
+++ sk_utf8/qformat_xml.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qformat_xml.php - created with Moodle 1.9.4+ (Build: 20090401) (2007101545.01)
+
+
+$string['invalidxml'] = 'Nevalidný xml súbor - bol očakávaný reťazec (použitie CDATA?)';
+$string['unsupportedexport'] = 'Otázka typu $a nie je v xml exporte podporovaná';
+
+?>
