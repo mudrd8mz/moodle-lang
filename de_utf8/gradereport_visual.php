61b54473ddec2a95ad7a3fa0044541da4ea432cc
Ralf Hilgenstock <ralf-bonn>
2009-04-05 10:31:46 +0000

    new strings

---
+++ de_utf8/gradereport_visual.php
@@ -0,0 +1,46 @@
+<?PHP // $Id$ 
+      // gradereport_visual.php - created with Moodle 2.0 dev (Build: 20090315) (2009030501)
+
+
+$string['aggregationposition'] = 'Position des Gesamtergebnisses';
+$string['aggregationview'] = 'Ansicht des Gesamtergebnisses';
+$string['allgroups'] = 'Alle Gruppen';
+$string['axes'] = 'Achsen';
+$string['continuousgradedistribution'] = 'Aggregierte Bewertungsverteilung';
+$string['grade'] = 'Bewertung';
+$string['gradedistribution'] = 'Verteilung (Linie)';
+$string['gradedistribution:title'] = 'Verteilung';
+$string['gradedistributionbar'] = 'Verteilung nach Werten (Balken)';
+$string['gradedistributiongroupbar'] = 'Bewertung nach Gruppen (Balken)';
+$string['gradesvsitems'] = 'Durchschnitt vs. Werte';
+$string['gradesvsitems:title'] = 'Durchschnitt vs. Werte';
+$string['gradesvsstudents'] = 'Normalisierte Werte vs. Teilnehmer';
+$string['gradesvsstudents:title'] = 'Normalisierte Werte vs. Teilnehmer';
+$string['hide'] = 'Verbergen';
+$string['incompleasmin'] = 'Unvollständige Bewertungen als Minimum';
+$string['invertaxes'] = 'Achsen tauschen';
+$string['item'] = 'Wert';
+$string['modulename'] = 'Grafische Darstellung';
+$string['nogroup'] = 'Keine Gruppe';
+$string['numberstudents'] = 'Zahl der Teilnehmer/innen';
+$string['percentstudents'] = 'Prozent Teilnehmer/innen';
+$string['prefcalc'] = 'Berechnungen';
+$string['preferences'] = 'Einstellungen für grafische Darstellung';
+$string['prefgeneral'] = 'Generell';
+$string['printable'] = 'Druck-Version';
+$string['show'] = 'Anzeigen';
+$string['showscaleitems'] = 'Werte mit Skala zeigen';
+$string['showvalueitems'] = 'Werte anzeigen';
+$string['student'] = 'Teilnehmer/in';
+$string['usehidden'] = 'Verborgene Bewertungen zählen';
+$string['uselocked'] = 'Gesperrte Bewertungen zählen';
+$string['visual:view'] = 'Grafischen Kursbericht anzeigen';
+$string['visual:vis:continuous_grade_distribution'] = 'Aggregierte Bewertung (grafisch)';
+$string['visual:vis:grade_distribution'] = 'Bewertung (grafisch)';
+$string['visual:vis:grade_distribution_bar'] = 'Bewertung-Balkenansicht';
+$string['visual:vis:grade_distribution_group_bar'] = 'Bewertungen gruppiert';
+$string['visualselector'] = 'Darstellung:';
+$string['xlabels'] = 'X-Bezeichnung';
+$string['ylabels'] = 'Y-Bezeichnung';
+
+?>
