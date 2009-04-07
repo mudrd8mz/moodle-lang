1ea6718bfb0fb64eb6d0e65ec937f7b20fefe121
Abel Camacho <ueu_ueu>
2009-04-07 14:32:00 +0000

    New file added

---
+++ eu_utf8/webservice.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // webservice.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['fail'] = 'KALE';
+$string['moodlepath'] = 'Moodle BIDEA';
+$string['ok'] = 'ONDO';
+$string['protocolenable'] = 'Gaitu $a[0] protokoloa';
+$string['webservicesenable'] = 'Gaitu web zerbitzuak';
+$string['wspagetitle'] = 'Web zerbitzuen dokumentazioa';
+
+?>
