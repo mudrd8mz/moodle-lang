385b7ff728c52c8c497c283d02356c2524043bfd
Ralf Hilgenstock <ralf-bonn>
2009-04-08 19:05:30 +0000

    *** empty log message ***

---
+++ de_utf8/qtype_essay.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // qtype_essay.php - created with Moodle 2.0 dev (Build: 20090226) (2009021800)
+
+
+$string['addingessay'] = 'Eine Abhandlungs-Frage hinzufügen';
+$string['editingessay'] = 'Eine Abhandlungs-Frage ändern';
+$string['essay'] = 'Abhandlung';
+$string['essaysummary'] = 'Erlaubt eine Antwort von einigen Sätzen oder Absätzen. Diese muss manuell bewertet werden.';
+
+?>
