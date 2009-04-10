6339d60ef37eda07e5bdcbd9cccaa9bfc23eb108
Nicolas Martignoni <martignoni>
2009-04-10 19:29:30 +0000

    Comment added

--- fr_utf8/block_tag_flickr.php
+++ fr_utf8/block_tag_flickr.php
@@ -3,8 +3,8 @@
 $string['blockname'] = 'Flickr';
 $string['configtitle'] = 'Titre';
 $string['getfromphotoset'] = 'Obtenir les photos du photoset d\'identifiant';
-$string['date-posted-asc'] = 'Date d\'importation &#8593;';
-$string['date-posted-desc'] = 'Date d\'importation &#8595;';
+$string['date-posted-asc'] = 'Date d\'importation &#8593;';  // Flèche haut ↑
+$string['date-posted-desc'] = 'Date d\'importation &#8595;';  // Flèche bas ↓
 $string['date-taken-asc'] = 'Date de prise de vue &#8593;';
 $string['date-taken-desc'] = 'Date de prise de vue &#8595;';
 $string['defaulttile'] = 'Flickr';
