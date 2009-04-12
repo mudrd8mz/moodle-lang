4ad9f419206447a166bc4399e48234a46409e9bb
Mitsuhiro Yoshida <mits>
2009-04-12 15:00:18 +0000

    MDL-12886 Translated new strings for web service documentation.

--- ja_utf8/webservice.php
+++ ja_utf8/webservice.php
@@ -1,10 +1,16 @@
 <?PHP // $Id$ 
-      // webservice.php - created with Moodle 2.0 dev (Build: 20090221) (2009021800)
+      // webservice.php - created with Moodle 2.0 dev (Build: 20090411) (2009040100)
 
 
+$string['amfdebug'] = 'AMFサーバデバッグモード';
+$string['debugdisplayon'] = '「デバックメッセージを表示する」が、ONに設定されています。XMLRPCサーバは、動作しません。他のウェブサービスのサーバでは、問題が発生する可能性もあります。<br/>Moodle管理者に、デバックメッセージを表示しないよう、注意を喚起してください。';
 $string['fail'] = 'FAIL';
+$string['functionlist'] = 'ウェブサービス機能一覧';
 $string['moodlepath'] = 'Moodleパス';
 $string['ok'] = 'OK';
+$string['protocolenable'] = '$a[0] プロトコル有効化';
+$string['webservicesenable'] = 'ウェブサービス有効化';
 $string['wspagetitle'] = 'ウェブサービスドキュメンテーション';
+$string['wsuserreminder'] = '注意: このサイトのMoodle管理者は、あなたにmoodle/site:usewebservicesケイパビリティを与える必要があります。';
 
 ?>
