488c81da2d8519eb966d2513b9df05715b51f700
Benito Arias <barias>
2009-04-26 10:21:19 +0000

    *** empty log message ***

--- es_utf8/quiz_grading.php
+++ es_utf8/quiz_grading.php
@@ -1,7 +1,8 @@
 <?PHP // $Id$ 
-      // quiz_grading.php - created with Moodle 1.8.2+ (2007021520)
+      // quiz_grading.php - created with Moodle 1.9.3+ (Build: 20081126) (2007101532)
 
 
+$string['cannotloadquestioninfo'] = 'No se ha podido cargar la información sobre el tipo de pregunta específica';
 $string['essayonly'] = 'Las siguientes preguntas necesitan calificarse manualmente';
 $string['gradeall'] = 'Calificar todos los $ intentos';
 $string['graded'] = '(calificado)';
@@ -15,6 +16,8 @@ $string['gradingnotallowed'] = 'No tiene permiso para calificar manualmente las 
 $string['gradingreport'] = 'Informe de calificación manual';
 $string['gradingungraded'] = '$a intentos no calificados';
 $string['gradinguser'] = 'Intentos para $a';
+$string['invalidattemptid'] = 'No existe la ID de tal intento';
+$string['invalidquestionid'] = 'No se ha encontrado una pregunta calificable con la ID $a';
 $string['questiontitle'] = 'Pregunta $a->number : \"$a->name\" ({$a->openspan}$a->gradedattempts{$a->closespan} / $a->totalattempts intentos {$a->openspan}graded{$a->closespan}).';
 
 ?>
