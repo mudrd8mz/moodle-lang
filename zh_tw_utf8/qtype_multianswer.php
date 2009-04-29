f69afd643cc6b5e54ced2ff62244254f5b775a82
Finjon Kiang <kiang>
2009-04-29 06:26:46 +0000

    Commit the translation from Stephen

--- zh_tw_utf8/qtype_multianswer.php
+++ zh_tw_utf8/qtype_multianswer.php
@@ -1,7 +1,25 @@
 <?PHP // $Id$ 
-      // qtype_multianswer.php - created with Moodle 1.9 + (Build: 20080409) (2007101509)
+      // qtype_multianswer.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+      // local modifications from http://localhost/moodle
+      // Stephen Liu http://job.achi.idv.tw
 
 
-$string['decodeverifyquestiontext'] = '解碼並驗證這題目文字';
+$string['addingmultianswer'] = '新增一填空題(克漏字)';
+$string['correctanswer'] = '正確答案';
+$string['correctanswerandfeedback'] = '正確答案的回饋';
+$string['editingmultianswer'] = '編輯填空題(克漏字)';
+$string['layout'] = '版面配置';
+$string['layouthorizontal'] = '橫式單選按鈕';
+$string['layoutselectinline'] = '下拉選單';
+$string['layoutundefined'] = '未定義';
+$string['layoutvertical'] = '直式單選按鈕';
+$string['multianswer'] = '填空題(克漏字)';
+$string['nooptionsforsubquestion'] = '無法取得問題的選項部份# $a->sub (question->id={$a->id})';
+$string['noquestions'] = '克漏字試題\"<strong>$a</strong>\"沒有任何題目';
+$string['qtypenotrecognized'] = '試題型態 $a 無法識別';
+$string['questiondefinition'] = '題目定義';
+$string['questionnotfound'] = '無法找到試題的題目部份 #$a';
+$string['questionsmissing'] = '沒有正確的題目，請新增至少一個題目';
+$string['unknownquestiontypeofsubquestion'] = '未知的試題型態：題目部份# $a->sub 的 $a->type';
 
 ?>
