008ec78cbda189d3e048229eda5b8037e55daa61
koen roggemans <koenr>
2009-05-13 19:12:45 +0000

    new help files from Karoly

---
+++ hu_utf8/repository_filesystem.php
@@ -0,0 +1,14 @@
+<?PHP // $Id$ 
+      // repository_filesystem.php - created with Moodle 1.9.3+ (Build: 20081217) (2007101532.11)
+      // local modifications from http://127.1.0.1
+
+
+$string['blockedpath'] = 'Lezárt útvonal a gyökérhez';
+$string['configplugin'] = 'Állományrendszer adattárának létrehozása';
+$string['donotusesysdir'] = 'Ne használja a rendszerkönyvtárat adattárként, ideértve';
+$string['invalidpath'] = 'Érvénytelen útvonal a gyökérhez';
+$string['path'] = 'Útvonal';
+$string['repositorydesc'] = 'Adattár létrehozása helyi könyvtárból';
+$string['repositoryname'] = 'Állományrendszer';
+
+?>
---
+++ hu_utf8/repository_googledocs.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // repository_googledocs.php - created with Moodle 1.9.3+ (Build: 20081217) (2007101532.11)
+      // local modifications from http://127.1.0.1
+
+
+$string['repositoryname'] = 'Google Docs';
+
+?>
