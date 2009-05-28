4c54bbf2a4173fa62cd4c4e72ac6ab2466b0edc8
Abel Camacho <ueu_ueu>
2009-05-28 08:30:55 +0000

    Modification 28-05-2009

--- eu_utf8/block_feedback.php
+++ eu_utf8/block_feedback.php
@@ -1,5 +1,7 @@
-<?php
+<?PHP // $Id$ 
+      // block_feedback.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
-$string['feedback'] = 'Feedback';
+
+$string['feedback'] = 'Inkesta';
 
 ?>
