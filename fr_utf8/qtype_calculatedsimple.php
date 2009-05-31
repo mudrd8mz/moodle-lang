7eca991472a9b7377fcc855376a6fd3603bd61d4
Nicolas Martignoni <martignoni>
2009-05-31 11:41:05 +0000

    MDL-17366 adding the calculatedsimple questiontype

---
+++ fr_utf8/qtype_calculatedsimple.php
@@ -0,0 +1,15 @@
+<?php
+
+$string['addingcalculatedsimple'] = 'Ajout d\'une question calculée simple';
+$string['calculatedsimple'] = 'Calculée simple';
+$string['calculatedsimplesummary'] = 'Version simplifiée des questions calculées (questions numériques dont les nombres sont tirés aléatoirement d\'un jeu de données lorsque le test est effectué).';
+$string['converttocalculated'] = 'Enregistrer comme une nouvelle question calculée normale';
+$string['editingcalculatedsimple'] = 'Modification d\'une question calculée simple';
+$string['findwildcards'] = 'Rechercher les caractères joker {x..} présents dans les formules de réponses correctes';
+$string['generatenewitemsset'] = 'Générer un nouveau jeu de ';
+$string['mustbenumeric'] = 'Vous devez saisir ici un nombre.';
+$string['mustnotbenumeric'] = 'Ceci ne peut pas être un nombre.';
+$string['wildcarditems'] = 'Éléments générés';
+$string['wildcardrole'] = ' Les caractères joker <strong>{x..}</strong> seront remplacés par une valeur numérique tirée des valeurs générées';
+$string['wildcardparam'] = 'Caractères joker utilisés pour générer les éléments ';
+$string['willconverttocalculated'] = 'Si activé, le bouton <strong>Enregistrer comme nouvelle question</strong> enregistrera cette question comme une nouvelle question calculée';
