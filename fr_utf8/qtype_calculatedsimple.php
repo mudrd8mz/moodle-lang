03eb6ab6b35a5bef848cb06ca24b648fdd36c488
Nicolas Martignoni <martignoni>
2009-06-07 07:19:04 +0000

    Tweaks for wild card terminology

--- fr_utf8/qtype_calculatedsimple.php
+++ fr_utf8/qtype_calculatedsimple.php
@@ -1,25 +1,25 @@
 <?php
 
 $string['addingcalculatedsimple'] = 'Ajout d\'une question calculée simple';
-$string['atleastonewildcard'] = 'Vous devez fournir au moins un caractère joker <strong>{x..}</strong> dans les formules de la réponse correcte';
+$string['atleastonewildcard'] = 'Vous devez fournir au moins un joker <strong>{x..}</strong> dans les formules de la réponse correcte';
 $string['calculatedsimple'] = 'Calculée simple';
 $string['calculatedsimplesummary'] = 'Version simplifiée des questions calculées (questions numériques dont les nombres sont tirés aléatoirement d\'un jeu de données lorsque le test est effectué).';
 $string['converttocalculated'] = 'Enregistrer comme une nouvelle question calculée normale';
 $string['editingcalculatedsimple'] = 'Modification d\'une question calculée simple';
-$string['findwildcards'] = 'Rechercher les caractères joker {x..} présents dans les formules de réponses correctes';
+$string['findwildcards'] = 'Rechercher les jokers {x..} présents dans les formules de réponses correctes';
 $string['generatenewitemsset'] = 'Générer';
 $string['mustbenumeric'] = 'Vous devez saisir ici un nombre.';
 $string['mustnotbenumeric'] = 'Ceci ne peut pas être un nombre.';
-$string['newsetwildcardvalues']= 'nouveau(x) jeu(x) de valeur(s) de caractère(s) joker ';
+$string['newsetwildcardvalues'] = 'nouveau(x) jeu(x) de valeur(s) pour le(s) joker(s) ';
 $string['setno'] = 'Jeu $a';
-$string['setwildcardvalues']= 'jeu(x) de valeur(s) de caractère(s) joker ';
+$string['setwildcardvalues'] = 'jeu(x) de valeur(s) pour le(s) joker(s) ';
 $string['showitems'] = 'Afficher ';
-$string['updatewildcardvalues'] = 'Modifier les valeurs des caractères joker';
+$string['updatewildcardvalues'] = 'Modifier les valeurs des jokers';
 $string['useadvance'] = 'Utiliser le bouton suite pour voir les erreurs' ;
-$string['wildcard'] = 'Caractère joker {<strong>$a</strong>}';
-$string['wildcardvalues'] = 'Valeur du/des caractère/s joker ';
-$string['wildcardvaluesgenerated'] = 'Valeurs générées pour le(s) caractère(s) joker';
-$string['wildcardrole'] = ' Les caractères joker <strong>{x..}</strong> seront remplacés par une valeur numérique tirée parmi les valeurs générées';
-$string['wildcardparam'] = 'Caractères joker utilisés pour générer les valeurs ';
+$string['wildcard'] = 'Joker {<strong>$a</strong>}';
+$string['wildcardvalues'] = 'Valeur du(des) joker(s) ';
+$string['wildcardvaluesgenerated'] = 'Valeurs générées pour le(s) joker(s)';
+$string['wildcardrole'] = ' Les jokers <strong>{x..}</strong> seront remplacés par une valeur numérique tirée parmi les valeurs générées';
+$string['wildcardparam'] = 'Jokers utilisés pour générer les valeurs ';
 $string['willconverttocalculated'] = 'Si activé, le bouton <strong>Enregistrer comme nouvelle question</strong> enregistrera cette question comme une nouvelle question calculée';
-$string['youmustaddatleastonevalue'] = 'Vous devez ajouter au moins un jeu de valeurs pour les caractères joker avant d\'enregistrer cette question.';
+$string['youmustaddatleastonevalue'] = 'Vous devez ajouter au moins un jeu de valeurs pour les jokers avant d\'enregistrer cette question.';
