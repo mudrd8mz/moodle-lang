1c7c9c670e74ea7229fb6d43a1385968bbe9ec1a
Nicolas Martignoni <martignoni>
2009-06-07 07:13:52 +0000

    MDL-17366 Strings added

--- fr_utf8/qtype_calculatedsimple.php
+++ fr_utf8/qtype_calculatedsimple.php
@@ -1,6 +1,7 @@
 <?php
 
 $string['addingcalculatedsimple'] = 'Ajout d\'une question calculée simple';
+$string['atleastonewildcard'] = 'Vous devez fournir au moins un caractère joker <strong>{x..}</strong> dans les formules de la réponse correcte';
 $string['calculatedsimple'] = 'Calculée simple';
 $string['calculatedsimplesummary'] = 'Version simplifiée des questions calculées (questions numériques dont les nombres sont tirés aléatoirement d\'un jeu de données lorsque le test est effectué).';
 $string['converttocalculated'] = 'Enregistrer comme une nouvelle question calculée normale';
@@ -9,7 +10,13 @@ $string['findwildcards'] = 'Rechercher les caractères joker {x..} présents dan
 $string['generatenewitemsset'] = 'Générer un nouveau jeu de ';
 $string['mustbenumeric'] = 'Vous devez saisir ici un nombre.';
 $string['mustnotbenumeric'] = 'Ceci ne peut pas être un nombre.';
-$string['wildcarditems'] = 'Éléments générés';
-$string['wildcardrole'] = ' Les caractères joker <strong>{x..}</strong> seront remplacés par une valeur numérique tirée des valeurs générées';
-$string['wildcardparam'] = 'Caractères joker utilisés pour générer les éléments ';
+$string['showitems'] = 'Afficher ';
+$string['updatewildcardvalues'] = 'Modifier les valeurs des caractères joker';
+$string['useadvance'] = 'Utiliser le bouton suite pour voir les erreurs' ;
+$string['wildcard'] = 'Caractère joker {<strong>$a</strong>}';
+$string['wildcardvalues'] = 'Valeur du/des caractère/s joker ';
+$string['wildcardvaluesgenerated'] = 'Valeurs générées pour le(s) caractère(s) joker';
+$string['wildcardrole'] = ' Les caractères joker <strong>{x..}</strong> seront remplacés par une valeur numérique tirée parmi les valeurs générées';
+$string['wildcardparam'] = 'Caractères joker utilisés pour générer les valeurs ';
 $string['willconverttocalculated'] = 'Si activé, le bouton <strong>Enregistrer comme nouvelle question</strong> enregistrera cette question comme une nouvelle question calculée';
+$string['youmustaddatleastonevalue'] = 'Vous devez ajouter au moins un jeu de valeurs pour les caractères joker avant d\'enregistrer cette question.';
