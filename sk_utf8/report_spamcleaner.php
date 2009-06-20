22ad12a0aa696d1818744615660982919277ff95
Miroslav Fikar <fikar>
2009-06-20 12:59:09 +0000

    Initial revision

---
+++ sk_utf8/report_spamcleaner.php
@@ -0,0 +1,21 @@
+<?PHP // $Id$ 
+      // report_spamcleaner.php - created with Moodle 1.9.4+ (Build: 20090422) (2007101546)
+
+
+$string['spamauto'] = 'Automaticky detekovať známe typy nežiadúceho obsahu';
+$string['spamcannotdelete'] = 'Nie je možné vymazať tohto používateľa';
+$string['spamcannotfinduser'] = 'Vašemu vyhľadávaniu nevyhovujú žiadni používatelia';
+$string['spamcleaner'] = 'Nežiadúci obsah';
+$string['spamcleanerintro'] = 'Tento skript umožňuje prehľadávať používateľské profily a zistiť, či neobsahujú nejaké reťazce, ktoré by identifikovali spamerov. Potom je možné tieto kontá vymazať. Vyhľadávanie umožňuje zadať viaceré slová oddelené čiarkami (napríklad casino, porn). <br /><br />Moodle docs obsahuje obšírnejšie informácie o <a href=\"http://docs.moodle.org/en/Reducing_spam_in_Moodle\">Reducing spam in Moodle</a>.';
+$string['spamdeleteall'] = 'Odstrániť všetky tieto používateľské kontá';
+$string['spamdeleteallconfirm'] = 'Naozaj chcete odstrániť všetky tieto používateľské kontá? Tento krok sa nedá vrátiť späť.';
+$string['spamdeleteconfirm'] = 'Naozaj chcete vymazať tento záznam? Tento krok sa nedá vrátiť späť.';
+$string['spamdesc'] = 'Opis';
+$string['spameg'] = 'napríklad casino, porn, xx';
+$string['spamfromblog'] = 'V blogu:';
+$string['spaminvalidresult'] = 'Neplatný výsledok';
+$string['spamoperation'] = 'Operácia';
+$string['spamresult'] = 'Výsledok vyhľadávania používateľských profilov obsahujúcich:';
+$string['spamsearch'] = 'Vyhľadávať tieto slová';
+
+?>
