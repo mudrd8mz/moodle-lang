c1aa9a8bd717510457846d942dbd7f7876ba6178
Ling Li <liling>
2009-06-21 12:32:52 +0000

    Fix some strings

--- zh_cn_utf8/block_tag_youtube.php
+++ zh_cn_utf8/block_tag_youtube.php
@@ -1,24 +1,24 @@
 <?PHP // $Id$ 
-      // block_tag_youtube.php - created with Moodle 1.9 (2007101509)
-      // local modifications from http://guijishu.oicp.net
+      // block_tag_youtube.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
-$string['blockname'] = 'Youtube';
+
 $string['anycategory'] = '任何类别';
-$string['autosvehicles'] = '汽车&amp; 车辆';
+$string['autosvehicles'] = '汽车和交通工具';
+$string['blockname'] = 'Youtube';
 $string['category'] = '类别';
 $string['comedy'] = '喜剧';
 $string['configtitle'] = '标题';
 $string['entertainment'] = '娱乐';
-$string['filmsanimation'] = '电影&amp; 动画片';
-$string['gadgetsgames'] = '新玩意 &amp; 游戏';
-$string['howtodiy'] = '如何做&amp; DIY';
+$string['filmsanimation'] = '电影和动画片';
+$string['gadgetsgames'] = '新玩意和游戏';
+$string['howtodiy'] = '如何做和 DIY';
 $string['includeonlyvideosfromplaylist'] = '仅从播放列表中包括视频';
 $string['music'] = '音乐';
-$string['newspolitics'] = '新闻&amp; 政治';
+$string['newspolitics'] = '新闻和政治';
 $string['numberofvideos'] = '视频数';
-$string['peopleblogs'] = '人 &amp; 博客';
-$string['petsanimals'] = '宠物&amp; 动物';
+$string['peopleblogs'] = '人和博客';
+$string['petsanimals'] = '宠物和动物';
 $string['sports'] = '体育';
-$string['travel'] = '旅行&amp; 景点';
+$string['travel'] = '旅行和景点';
 
 ?>
