ce274f738b4f0d94dcda4cda519f0465278f6215
Ling Li <liling>
2009-06-21 15:40:58 +0000

    Add spaces between English and Chinese word.

--- zh_cn_utf8/enrol_mnet.php
+++ zh_cn_utf8/enrol_mnet.php
@@ -1,25 +1,21 @@
 <?PHP // $Id$ 
-      // enrol_mnet.php - created with Moodle 1.8 (2007021501)
+      // enrol_mnet.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
-$string['allcategories'] = '{$a}中可能的类别';
-$string['allcourses'] = '{$a}中可能的课程';
+$string['allcategories'] = '{$a} 中可能的类别';
+$string['allcourses'] = '{$a} 中可能的课程';
 $string['allow_allcourses'] = '允许选修所有远程课程';
-$string['allowedcategories'] = '{$a}允许的类别';
-$string['allowedcourses'] = '{$a}允许的课程';
+$string['allowedcategories'] = '{$a} 允许的类别';
+$string['allowedcourses'] = '{$a} 允许的课程';
 $string['allowedcourseslinktext'] = '在这里编辑允许的课程和类别。';
-$string['description'] = '在Moodle网络上的选课描述。';
-$string['enrolname'] = 'Moodle网络';
-$string['mnet_enrol_description'] = '发布该服务将允许在{$a}上的管理员可以选择自己服务器上已建课程中学生。<br/><ul><li><em>依赖性</em>: 您也必须在{$a}上<strong>发布</strong>SSO (服务提供商) 服务。</li><li><em>依赖性</em>: 您也必须在{$a}上<strong>订阅</strong> SSO (身份验证提供商) 服务。</li></ul><br/>订阅该服务将可以将{$a}课程中的学生注册进来。<br/><ul><li><em>依赖性</em>: 您也必须在{$a}上<strong>发布</strong>SSO (服务提供商) 服务。</li><li><em>依赖性</em>: 您也必须在{$a}上<strong>订阅</strong> SSO (身份验证提供商) 服务。</li></ul><br/>';
-$string['mnet_enrol_name'] = 'Moodle网络注册';
+$string['description'] = '在 Moodle 网络上的选课描述。';
+$string['enrolname'] = 'Moodle 网络';
+$string['mnet_enrol_description'] = '发布该服务将允许在 {$a} 上的管理员可以选择自己服务器上已建课程中学生。<br/><ul><li><em>依赖性</em>: 您必须向 {$a} <strong>发布</strong>SSO (Service Provicder) 服务。</li><li><em>依赖性</em>: 您也必须<strong>订阅</strong> {$a} 的 SSO (Identity Provider) 服务。</li></ul><br/>订阅该服务将可以将 {$a} 课程中的学生注册进来。<br/><ul><li><em>依赖性</em>：您必须<strong>订阅</strong> {$a} 的 SSO (Service Provider) 服务。</li><li><em>依赖性</em>: 您也必须向 {$a} <strong>发布</strong> SSO (Identity Provider) 服务。</li></ul><br/>';
+$string['mnet_enrol_name'] = 'Moodle 网络注册';
+$string['mnetlocalforexternal'] = '外部用户使用的本地课程';
 $string['nocategoriesdefined'] = '没有发现课程类别。在<a href=\"{$a}\">这里</a>
 定义新的课程类别。';
 $string['nocoursesdefined'] = '没有发现课程。在<a href=\"{$a}\">这里</a>
 定义新的课程。';
 
-	  //翻译者：况亮
-	  //Email：kuangliang12345@163.com
-	  //Moodle 1.9 2008/04
-
-$string['mnetlocalforexternal'] = '外部用户使用的本地课程';
 ?>
