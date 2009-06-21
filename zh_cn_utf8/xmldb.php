53daab9269eb5ffa9fe12e4e1fd8d66bf813d779
Ling Li <liling>
2009-06-21 17:21:42 +0000

    Add spaces between English and Chinese word and fix some strings.

--- zh_cn_utf8/xmldb.php
+++ zh_cn_utf8/xmldb.php
@@ -1,17 +1,24 @@
 <?PHP // $Id$ 
-      // xmldb.php - created with Moodle 1.8.2+ (2007021520)
+      // xmldb.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['aftertable'] = '放在此表后：';
 $string['back'] = '后退';
 $string['backtomainview'] = '返回主视图';
 $string['binaryincorrectlength'] = '二进制字段的长度不正确';
-$string['cannotuseidfield'] = '不能插入\"id\"字段。它是自动编号的列';
+$string['butis'] = '但是(?)';
+$string['cannotuseidfield'] = '不能插入“id”字段。它是自动编号的列';
 $string['change'] = '更改';
 $string['charincorrectlength'] = '字符字段的不正确长度';
-$string['check_indexes'] = '查找缺失的DB索引';
+$string['check_bigints'] = '查询不正确 DB 整数';
+$string['check_defaults'] = '查找不一致的默认值';
+$string['check_indexes'] = '查找缺失的 DB 索引';
+$string['checkbigints'] = '检查';
+$string['checkdefaults'] = '检查默认值';
 $string['checkindexes'] = '检查索引';
 $string['completelogbelow'] = '查看下面的完整日志';
+$string['confirmcheckbigints'] = '本功能将搜索 <a href=\"http://tracker.moodle.org/browse/MDL-11038\">整数字段的可能错误</a>。强烈推荐使用最新版本。';
+$string['confirmcheckdefaults'] = '本功能将在本机上搜索不一致的默认值。';
 $string['confirmcheckindexes'] = '该功能将查找Moodle服务器上潜在的缺失索引，由必须的SQL语句保持更新时自动产生。一旦产生后您可以拷贝这些语句到其他的SQL接口中执行。<BR/></BR>
 强烈推荐使用最新版本的MOODLE。';
 $string['confirmdeletefield'] = '您是否非常确信要删除此字段：';
@@ -32,7 +39,7 @@ $string['delete_key'] = '删除键值';
 $string['delete_sentence'] = '删除子句';
 $string['delete_statement'] = '删除语句';
 $string['delete_table'] = '删除表';
-$string['delete_xml_file'] = '删除XML文件';
+$string['delete_xml_file'] = '删除 XML 文件';
 $string['down'] = '向下';
 $string['duplicate'] = '复制';
 $string['duplicatefieldname'] = '已存在一个同名的字段';
@@ -43,7 +50,7 @@ $string['edit_key'] = '编辑键值';
 $string['edit_sentence'] = '编辑子句';
 $string['edit_statement'] = '编辑语句';
 $string['edit_table'] = '编辑表';
-$string['edit_xml_file'] = '编辑XML文件';
+$string['edit_xml_file'] = '编辑 XML 文件';
 $string['enumvaluesincorrect'] = '枚举字段中不正确的值';
 $string['field'] = '字段';
 $string['fieldnameempty'] = '字段名为空';
@@ -58,7 +65,7 @@ $string['indexes'] = '索引';
 $string['integerincorrectlength'] = '整数字段的长度不正确';
 $string['key'] = '键值';
 $string['keys'] = '键值';
-$string['listreservedwords'] = '保留字列表<br/>（用来保持<a href=\"http://docs.moodle.org/en/XMLDB_reserved_words\" target=\"_blank\">XMLDB保留字</a>的更新)';
+$string['listreservedwords'] = '保留字列表<br/>（用来保持 <a href=\"http://docs.moodle.org/en/XMLDB_reserved_words\" target=\"_blank\">XMLDB 保留字</a>的更新)';
 $string['load'] = '载入';
 $string['main_view'] = '主视图';
 $string['missing'] = '缺失';
@@ -68,16 +75,19 @@ $string['missingvaluesinsentence'] = '子句中没有值';
 $string['mustselectonefield'] = '您必须选择一个字段来查看与字段相关的动作！';
 $string['mustselectoneindex'] = '您必须选择一个索引来查看与索引相关的动作！';
 $string['mustselectonekey'] = '您必须选择一个键值来查看与键值相关的动作！';
+$string['mysqlextracheckbigints'] = '如使用 MySQL 数据库，它还将查找不正确的大整数标记。';
 $string['new_statement'] = '新建语句';
-$string['new_table_from_mysql'] = '从MySQL建新表';
+$string['new_table_from_mysql'] = '从 MySQL 建新表';
 $string['newfield'] = '新建字段';
 $string['newindex'] = '新建索引';
 $string['newkey'] = '新建键值';
 $string['newsentence'] = '新建子句';
 $string['newstatement'] = '新建语句';
 $string['newtable'] = '创建新表';
-$string['newtablefrommysql'] = '从MySQL建新表';
-$string['nomissingindexesfound'] = '没有发现缺失字段，您的DB不需要做任何更改。';
+$string['newtablefrommysql'] = '从 MySQL 建新表';
+$string['nomissingindexesfound'] = '没有发现缺失字段，您的 DB 不需要做任何更改。';
+$string['nowrongdefaultsfound'] = '没有发现不一致的默认值。';
+$string['nowrongintsfound'] = '没发现整数错误。';
 $string['numberincorrectdecimals'] = '数字字段的小数位数不正确';
 $string['numberincorrectlength'] = '数字字段的长度不正确';
 $string['reserved'] = '保留';
@@ -89,10 +99,11 @@ $string['searchresults'] = '查找结果';
 $string['selectaction'] = '选择动作：';
 $string['selectdb'] = '选择数据库：';
 $string['selectfieldkeyindex'] = '选择字段／键／索引：';
-$string['selectonecommand'] = '为了查看PHP代码，请在列表中选择一个动作';
-$string['selectonefieldkeyindex'] = '为了查看PHP代码，请在列表中选择一个字段/键/索引';
+$string['selectonecommand'] = '为了查看 PHP 代码，请在列表中选择一个动作';
+$string['selectonefieldkeyindex'] = '为了查看 PHP 代码，请在列表中选择一个字段/键/索引';
 $string['selecttable'] = '选择表：';
 $string['sentences'] = '子句';
+$string['shouldbe'] = '应该';
 $string['statements'] = '语句';
 $string['statementtable'] = '语句表：';
 $string['statementtype'] = '语句类型：';
@@ -104,27 +115,22 @@ $string['unload'] = '卸载';
 $string['up'] = '向上';
 $string['view'] = '查看';
 $string['view_reserved_words'] = '查看保留字';
-$string['view_structure_php'] = '查看结构化PHP';
-$string['view_structure_sql'] = '查看结构化SQL';
-$string['view_table_php'] = '查看表PHP';
-$string['view_table_sql'] = '查看表SQL';
+$string['view_structure_php'] = '查看结构化 PHP';
+$string['view_structure_sql'] = '查看结构化 SQL';
+$string['view_table_php'] = '查看表 PHP';
+$string['view_table_sql'] = '查看表 SQL';
 $string['viewedited'] = '查看已编辑的';
 $string['vieworiginal'] = '查看原始代码';
-$string['viewphpcode'] = '查看PHP代码';
-$string['viewsqlcode'] = '查看SQL代码';
+$string['viewphpcode'] = '查看 PHP 代码';
+$string['viewsqlcode'] = '查看 SQL 代码';
+$string['wrong'] = '错误';
+$string['wrongdefaults'] = '错误的默认值';
+$string['wrongints'] = '错误的整型';
 $string['wronglengthforenum'] = '枚举字段的长度不正确';
 $string['wrongnumberoffieldsorvalues'] = '子句中字段或值的数量不正确';
 $string['wrongreservedwords'] = '当前使用的保留字<br />（如果表名使用{$CFG->prefix}，就不用留意这个问题）';
-$string['yesmissingindexesfound'] = '一些缺失索引在您的DB中发现，这里有详细信息和需要执行的SQL语句<BR/><BR/>
-完成后，强烈建议再次执行一次，查看是否还有缺失的索引。';
+$string['yesmissingindexesfound'] = '在数据库中发现了缺失的索引。以下是详细，需要执行 SQL 命令来修正(注意先备份)。<br /><br />我们强烈建议您在修改完成后，重新用此工具进行检查以确认没有其它错误。';
+$string['yeswrongdefaultsfound'] = '在数据库中已经发现了不一致的默认值。以下是详细资料，需要执行 SQL 命令来修正(注意先备份)。<br /><br />我们强烈建议您在修改完成后，重新用此工具进行检查以确认没有其它错误。';
+$string['yeswrongintsfound'] = '在数据库中已经发现了整形错误。以下是详细资料，需要执行 SQL 命令来修正(注意先备份)。<br /><br />我们强烈建议您在修改完成后，重新用此工具进行检查以确认没有其它错误。';
 
-	  //翻译者：况亮
-	  //Email：kuangliang12345@163.com
-	  //Moodle 1.9 2008/04
-
-$string['check_bigints'] = '查询不正确DB整数';
-$string['checkbigints'] = '检查';
-$string['checkindexes'] = '检查索引Bigints';
-$string['wrong'] = '错误';
-$string['wrongints'] = '发现错误整型';
 ?>
