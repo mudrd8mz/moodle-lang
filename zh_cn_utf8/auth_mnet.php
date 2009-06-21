ab1b96579d0850c86aa1925926285f2494f33eab
Ling Li <liling>
2009-06-21 11:53:19 +0000

    Correct wrong translations

--- zh_cn_utf8/auth_mnet.php
+++ zh_cn_utf8/auth_mnet.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // auth_mnet.php - created with Moodle 1.8 (2007021501)
+      // auth_mnet.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
-$string['sso_idp_description'] = '发布该服务将允许用户到{$a} Moodle站点，而不需要重新登录。<ul><li><em>依赖性</em>: 您必须在{$a}上<strong>订阅</strong>SSO(一个服务器提供商)服务。</li></ul><br />定义该服务后运行用户从{$a} 访问到您的Moodle站点而不用重新登录。。<ul><li><em>依赖性</em>: 同时您必须在{$a}上<strong>发布</strong>SSO(一个服务器提供商)服务。</li></ul><br />';
-$string['sso_idp_name'] = 'SSO（身份验证提供商）';
-$string['sso_mnet_login_refused'] = '用户 {$a[0]}不允许从 {$a[1]}登录。';
-$string['sso_sp_description'] = '发布该服务将允许用户到{$a} Moodle站点，而不需要重新登录。<ul><li><em>依赖性</em>: 您必须在{$a}上<strong>订阅</strong>SSO(身份验证提供商)服务。</li></ul><br />定义该服务后运行用户从{$a} 访问到您的Moodle站点而不用重新登录。。<ul><li><em>依赖性</em>: 同时您必须在{$a}上<strong>发布</strong>SSO(身份验证提供商)服务。</li></ul><br />';
-$string['sso_sp_name'] = 'SSO（服务提供商）';
+$string['sso_idp_description'] = '发布该服务后，用户浏览到 {$a} Moodle 站点时无需重新登录。<ul><li><em>依赖性</em>: 您必须<strong>订阅</strong> {$a} 上的 SSO (Service Provider)服务。</li></ul><br />定义该服务后，用户从 {$a} 访问到您的 Moodle 站点时无需重新登录。<ul><li><em>依赖性</em>: 同时您必须向 {$a} <strong>发布</strong> SSO (Service Provider) 服务。</li></ul><br />';
+$string['sso_idp_name'] = 'SSO(Identity Provider)';
+$string['sso_mnet_login_refused'] = '不允许用户 {$a[0]} 从 {$a[1]} 登录。';
+$string['sso_sp_description'] = '发布该服务后，则允许 {$a} 上的认证用户访问 您的Moodle 站点时无需重新登录。<ul><li><em>依赖性</em>: 您必须<strong>订阅</strong> {$a} 上的 SSO (Identity Provider) 服务。</li></ul><br />定义该服务后，用户从 {$a} 访问到您的 Moodle 站点时无需重新登录。<ul><li><em>依赖性</em>: 同时您必须向 {$a} <strong>发布</strong> SSO (Identity Provider) 服务。</li></ul><br />';
+$string['sso_sp_name'] = 'SSO(Service Provider)';
 
 ?>
