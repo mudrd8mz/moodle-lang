23a0152965234f3605f089c6ca7f6ccd4cc92a51
Ling Li <liling>
2009-06-21 17:40:25 +0000

    Add new strings

---
+++ zh_cn_utf8/qformat_gift.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // qformat_gift.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
+
+
+$string['nohandler'] = '找不到问题类型“{$a}”的处理程序';
+
+?>
--- zh_cn_utf8/qtype_shortanswer.php
+++ zh_cn_utf8/qtype_shortanswer.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // qtype_shortanswer.php - created with Moodle 1.8 (2007021501)
+      // qtype_shortanswer.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['addmoreanswerblanks'] = '更多的答案空白';
-$string['answerno'] = '答案{$a}';
-$string['filloutoneanswer'] = '必须提供至少一个可能的答案。答案不能为空.*可以做为一个通配符，用来匹配任何字符。第一个匹配的答案将用来决定分数和反馈。';
+$string['answermustbegiven'] = '此题目有成绩/反馈信息，因此您必须输入一个答案。';
+$string['answerno'] = '答案 {$a}';
+$string['filloutoneanswer'] = '必须提供至少一个可能的答案。答案不能为空，“*”可以做为一个通配符，用来匹配任何字符。第一个匹配的答案将用来决定分数和反馈。';
 
 ?>
