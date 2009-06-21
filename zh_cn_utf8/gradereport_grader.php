568d684b8fe938b64eddb4a84a204bec2078e583
Ling Li <liling>
2009-06-21 16:20:31 +0000

    Add one new string

--- zh_cn_utf8/gradereport_grader.php
+++ zh_cn_utf8/gradereport_grader.php
@@ -1,10 +1,10 @@
 <?PHP // $Id$ 
-      // gradereport_grader.php - created with Moodle 1.9 (2007101509)
-      // local modifications from http://guijishu.oicp.net
+      // gradereport_grader.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['grader:manage'] = '管理成绩单';
 $string['grader:view'] = '查看成绩单';
 $string['modulename'] = '成绩单';
+$string['preferences'] = '成绩单使用偏好';
 
 ?>
