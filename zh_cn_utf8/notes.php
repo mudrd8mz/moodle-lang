d43e55b670330a73edb8f1c44ab2c851497e690f
Ling Li <liling>
2009-06-21 17:33:19 +0000

    Add new strings

--- zh_cn_utf8/notes.php
+++ zh_cn_utf8/notes.php
@@ -1,8 +1,12 @@
-<?PHP // $Id$
-      // note.php 
-$string['addnewnote'] = '添加一个新评语';
+<?PHP // $Id$ 
+      // notes.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
+
+
+$string['addnewnote'] = '添加一个条新评语';
 $string['addnewnoteselect'] = '选择用户撰写评语';
 $string['bynameondate'] = '$a->name —$a->date';
+$string['cannotdeletepost'] = '删除帖子时发生错误';
+$string['configenablenotes'] = '允许单独为每个用户记评语';
 $string['content'] = '内容';
 $string['course'] = '课程';
 $string['coursenotes'] = '课程评语';
@@ -10,11 +14,15 @@ $string['created'] = '已创建';
 $string['deleteconfirm'] = '删除该评语吗？';
 $string['deletenotes'] = '删除所有评语';
 $string['editnote'] = '编辑评语';
+$string['enablenotes'] = '激活评语';
 $string['groupaddnewnote'] = '为所有人添加评语';
-$string['nocontent'] = '笔记内不能为空';
+$string['invalidid'] = '错误的评语 ID';
+$string['nocontent'] = '评语内不能为空';
 $string['nonotes'] = '没有这种类型的评语';
+$string['nopermissiontodelete'] = '您不能删除此评语';
 $string['note'] = '评语';
 $string['notes'] = '评语';
+$string['notesdisabled'] = '对不起，评语功能已禁用';
 $string['notesnotvisible'] = '您无权查看评语！';
 $string['nouser'] = '您必须选择一个用户';
 $string['personal'] = '个人';
