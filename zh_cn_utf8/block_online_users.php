215aece07f3cea3d3ccdc9ec6f5699f7ab5777de
Ling Li <liling>
2009-06-21 12:03:32 +0000

    Fix some strings

--- zh_cn_utf8/block_online_users.php
+++ zh_cn_utf8/block_online_users.php
@@ -1,15 +1,11 @@
 <?PHP // $Id$ 
-      // block_online_users.php - created with Moodle 1.4.3 + (2004083132)
+      // block_online_users.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['blockname'] = '在线用户';
 $string['configtimetosee'] = '认为多少分钟内访问过网站的用户是在线的';
-$string['periodnminutes'] = '{$a}分钟内';
-	
-	  
-	  //翻译者：况亮
-	  //Email：kuangliang12345@163.com
-	  //Moodle 1.9 2008/04
+$string['online_users:viewlist'] = '查看在线用户列表';
+$string['periodnminutes'] = '{$a} 分钟内';
+$string['timetosee'] = '多长时间（分钟）无操作则移除';
 
-$string['timetosee'] = '停止运行多长时间后移除（分钟）';
 ?>
