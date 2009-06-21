99c3bb13d17077914aefbb80e6fac68b254d0f99
Ling Li <liling>
2009-06-21 15:35:23 +0000

    Add new strings

--- zh_cn_utf8/enrol_manual.php
+++ zh_cn_utf8/enrol_manual.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_manual.php - created with Moodle 1.8.2+ (2007021520)
+      // enrol_manual.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['description'] = '这是选课的缺省形式。学生有两种方法可以进入到课程中:
@@ -7,7 +7,11 @@ $string['description'] = '这是选课的缺省形式。学生有两种方法可
 <li>教师或管理员可以通过课程中的课程管理目录手工将学生加入到课程中。</li>
 <li>课程可以定义一个密码，即“选课密钥”。所有知道这个密钥的人都能够进入课程。</li>
 </ul>';
+$string['enrol_manual_requirekey'] = '强制在新课程中使用选课密钥，且不能删除已有的密钥。';
+$string['enrol_manual_showhint'] = '开启此选项，则用户输入了错误的选课密钥时，会提示他们密钥的第一个字母。';
+$string['enrol_manual_usepasswordpolicy'] = '用当前的用户密码策略作为选课密钥设置策略。';
+$string['enrolmentkeyerror'] = '选课密钥错误，请重试。';
 $string['enrolname'] = '内部选课';
-$string['keyholderrole'] = '用户的角色用于控制选课，在学生尝试选课时会对学生显示。';
+$string['keyholderrole'] = '本角色把握一个课程的登录密码，显示给学生以便登录课程。';
 
 ?>
