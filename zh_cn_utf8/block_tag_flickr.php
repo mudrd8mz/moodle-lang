b76fd30cd2fa9c94dcde13638bf1f17e4e72b047
Ling Li <liling>
2009-06-21 14:13:04 +0000

    Add comments generated by moodle

--- zh_cn_utf8/block_tag_flickr.php
+++ zh_cn_utf8/block_tag_flickr.php
@@ -1,16 +1,14 @@
-<?php
+<?PHP // $Id$ 
+      // block_tag_flickr.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
-	  //翻译者：杨英
-
-	  //Moodle 1.9 2008/04
-$string['defaulttile'] = 'Flickr';
 $string['blockname'] = 'Flickr';
 $string['configtitle'] = '标题';
 $string['date-posted-asc'] = '发表日期升序排列';
 $string['date-posted-desc'] = '发表日期降序排列';
 $string['date-taken-asc'] = '拍摄日期升序排列';
 $string['date-taken-desc'] = '拍摄日期降序排列';
+$string['defaulttile'] = 'Flickr';
 $string['getfromphotoset'] = '从有地址的相册集获取相片';
 $string['includerelatedtags'] = '包含查询的相关标签';
 $string['interestingness-asc'] = '关注度升序排列';
@@ -18,5 +16,5 @@ $string['interestingness-desc'] = '关注度降序排列';
 $string['numberofphotos'] = '相片数';
 $string['relevance'] = '参数';
 $string['sortby'] = '分类依据';
-                                   
+
 ?>
