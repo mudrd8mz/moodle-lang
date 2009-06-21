2fcc95929d25b60cee55ee39e3a0d52a07072860
Ling Li <liling>
2009-06-21 17:26:37 +0000

    Add spaces between English and Chinese word and add new strings.

--- zh_cn_utf8/resource.php
+++ zh_cn_utf8/resource.php
@@ -1,23 +1,26 @@
 <?PHP // $Id$ 
-      // resource.php - created with Moodle 1.8.2+ (2007021520)
+      // resource.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['addresource'] = '添加资源';
 $string['affectedresources'] = '受影响的资源';
+$string['allowlocalfiles'] = '允许本地文件';
+$string['autofilerename'] = '如果文件重命名则自动更新链接';
 $string['back'] = '后退';
+$string['blockdeletingfile'] = '版块删除引用文件';
 $string['browserepository'] = '浏览仓库';
 $string['choose'] = '选择';
 $string['chooseafile'] = '选择或上传一个文件';
 $string['chooseparameter'] = '选择一个参数';
 $string['clicktoopen'] = '如果要打开文档，点击此链接。';
-$string['configallowlocalfiles'] = '允许在创建新文件资源时使用指向本地文件系统的链接，如光驱或者硬盘。这对于学生都在一个教室内访问网络驱动器或CD上的文件是很有用出的。是用这个功能可能要修改您的浏览器的安全设置。';
+$string['configallowlocalfiles'] = '允许在创建新文件资源时使用指向本地文件系统的链接，如光驱或者硬盘。这对于学生都在一个教室内访问网络驱动器或 CD 上的文件是很有用出的。是用这个功能可能要修改您的浏览器的安全设置。';
 $string['configautofilerenamesettings'] = '当在文件管理器中对文件重命名时，系统将自动更新所有对文件和目录的引用。';
-$string['configblockdeletingfilesettings'] = '阻止删除被资源引用的文件和目录的请求。请注意，从html引用的图片和其它文件不受保护。';
-$string['configdefaulturl'] = '当添加一个基于URL的资源时，该值用于预填URL表格.';
-$string['configfilterexternalpages'] = '使用该值将使所有外部资源(网页,上传的HTML文件) 按当前设定过滤(如词汇自动链接).使用此项将拖慢访问速度 - 当您确实需要时再谨慎的使用它.';
-$string['configframesize'] = '当页面或上传的文件显示在框架中时,该象素值设定顶层框架的大小';
+$string['configblockdeletingfilesettings'] = '阻止删除被资源引用的文件和目录的请求。请注意，从 html 引用的图片和其它文件不受保护。';
+$string['configdefaulturl'] = '当添加一个基于 URL 的资源时，该值用于预填 URL 表格。';
+$string['configfilterexternalpages'] = '使用该值将使所有外部资源(网页、上传的 HTML 文件)按当前设定过滤(如词汇自动链接)。使用此项将拖慢访问速度——当您确实需要时再谨慎的使用它。';
+$string['configframesize'] = '当页面或上传的文件显示在框架中时，该象素值设定顶层框架的大小。';
 $string['configparametersettings'] = '当添加新的资源时，以此作为参数设置框的缺省值。在第一次设置之后，就允许每个用户自己设置。';
-$string['configpopup'] = '所添加的新资源显示在弹出窗口, 设其为默认值吗?';
+$string['configpopup'] = '所添加的新资源显示在弹出窗口，设其为默认值吗？';
 $string['configpopupdirectories'] = '弹出窗口是否显示缺省目录链接？';
 $string['configpopupheight'] = '新窗口缺省高度？';
 $string['configpopuplocation'] = '弹出窗口是否显示地址栏？';
@@ -27,8 +30,8 @@ $string['configpopupscrollbars'] = '弹出窗口是否可滚动？';
 $string['configpopupstatus'] = '弹出窗口是否显示状态栏？';
 $string['configpopuptoolbar'] = '弹出窗口是否显示工具栏？';
 $string['configpopupwidth'] = '新窗口缺省宽度？';
-$string['configsecretphrase'] = '这个保密口令用于制作加密码传递给一些资源作为参数。加密码是当前用户的IP地址加上保密口令后用md5算法生成的，即code = md5(IP.secretprahse)。这允许目标资源进行额外的安全检查。';
-$string['configwebsearch'] = '当添加一个URL作为网页或链接时,该站将帮助用户寻找其想要的URL.';
+$string['configsecretphrase'] = '这个保密口令用于制作加密码传递给一些资源作为参数。加密码是当前用户的IP地址加上保密口令后用 md5 算法生成的，即 code = md5(IP.secretprahse)。这允许目标资源进行额外的安全检查。';
+$string['configwebsearch'] = '当添加一个 URL 作为网页或链接时，该站将帮助用户寻找其想要的 URL。';
 $string['configwindowsettings'] = '当添加新资源时，此处设置将作为窗口设置框的缺省值。在第一次设之后，就允许每个用户自己设置。';
 $string['deploy'] = '部署';
 $string['deployall'] = '部署所有';
@@ -40,20 +43,25 @@ $string['emptyfolder'] = '空文件夹';
 $string['encryptedcode'] = '加密代码';
 $string['example'] = '例子';
 $string['exampleurl'] = 'http://www.example.com/somedirectory/somefile.html';
-$string['fetchclienterror'] = '当试图下载网页时您的Web客户程序发生错误(可能是错误的URL)。';
+$string['fetchclienterror'] = '当试图下载网页时您的Web客户程序发生错误(可能是错误的 URL)。';
 $string['fetcherror'] = '当试图下载网页时发生错误。';
 $string['fetchservererror'] = '当试图下载网页时远程服务器发生错误(可能是程序错误)。';
 $string['filename'] = '文件名';
 $string['filtername'] = '资源名称自动链接';
+$string['forcedownload'] = '强制下载';
 $string['frameifpossible'] = '将资源放在单独框架中以保证站点导航';
+$string['framesize'] = '页框大小';
 $string['fulltext'] = '全文';
-$string['htmlfragment'] = 'HTML片段';
-$string['imspackageloaded'] = '包已载入。';
+$string['htmlfragment'] = 'HTML 片段';
+$string['imspackageloaded'] = '包已载入';
 $string['keepnavigationvisible'] = '保持页面导航在同一页中可见';
+$string['keepnavigationvisibleno'] = '不用';
+$string['keepnavigationvisibleyesframe'] = '是的，通过页框显示';
+$string['keepnavigationvisibleyesobject'] = '是的，不通过页框显示';
 $string['localfile'] = '本地文件';
 $string['localfilechoose'] = '选择一个本地文件(CD-ROM)';
 $string['localfilehelp'] = '帮助显示本地文件';
-$string['localfileinfo'] = '<p>从您的计算机上选择一个本地文件。这个文件将不会被上传到Web站点上，但Moodle会在想要浏览此资源的的计算机的同样位置寻找它。</p><p>当您有很大的多媒体文件存储在CD-ROM并发给参与者时，这个功能是很有用的。每个参与者都可以通过<a href=\"{$a}\" target=\"_blank\">修改个人信息</a>来选择他自己的本地路径。</p>';
+$string['localfileinfo'] = '<p>从您的计算机上选择一个本地文件。这个文件将不会被上传到 Web 站点上，但 Moodle 会在想要浏览此资源的的计算机的同样位置寻找它。</p><p>当您有很大的多媒体文件存储在 CD-ROM 并发给参与者时，这个功能是很有用的。每个参与者都可以通过<a href=\"{$a}\" target=\"_blank\">修改个人信息</a>来选择他自己的本地路径。</p>';
 $string['localfilepath'] = '要为此资源设置您自己的本度路径，请随便从您电脑的保存资源的驱动器(通常是光驱)上选择一个文件。这个文件不会被上传，但其信息将会被存储并用于本地文件资源。';
 $string['localfileselect'] = '选择这个路径。';
 $string['maindirectory'] = '主文件目录';
@@ -82,8 +90,8 @@ $string['note'] = '注意';
 $string['notefile'] = '要上载更多文件到课程中（这样它们就会出现于该列表），请用 
 <a href=\"{$a}\">文件管理器</a>.';
 $string['notypechosen'] = '您必需选择一个类型。用浏览器的后退按钮退回并重试。';
-$string['packagechanged'] = 'IMS包已经修改了。';
-$string['packagenotdeplyed'] = 'IMS包尚未部署。';
+$string['packagechanged'] = 'IMS 包已经修改了。';
+$string['packagenotdeplyed'] = 'IMS 包尚未部署。';
 $string['pagedisplay'] = '在当前窗口显示此资源';
 $string['pagewindow'] = '同一窗口';
 $string['pan'] = '平衡';
@@ -94,22 +102,23 @@ $string['popupresourcelink'] = '若不行, 点击这里: {$a}';
 $string['preview'] = '预览';
 $string['previous'] = '前面的';
 $string['redeploy'] = '重新部署';
-$string['repository'] = 'IMS仓库';
+$string['repository'] = 'IMS 仓库';
 $string['resource:view'] = '查看资源';
+$string['resourcedefaulturl'] = '默认 URL';
 $string['resourcetype'] = '资源类型';
 $string['resourcetype1'] = '参考书目';
 $string['resourcetype2'] = '网页';
 $string['resourcetype3'] = '上载的文件';
 $string['resourcetype4'] = '纯文本';
 $string['resourcetype5'] = '网页链接';
-$string['resourcetype6'] = 'HTML文本';
+$string['resourcetype6'] = 'HTML 文本';
 $string['resourcetype7'] = '程序';
-$string['resourcetype8'] = 'Wiki文本';
+$string['resourcetype8'] = 'Wiki 文本';
 $string['resourcetype9'] = '目录';
 $string['resourcetypedirectory'] = '显示一个目录';
 $string['resourcetypefile'] = '链接到文件或站点';
 $string['resourcetypehtml'] = '编写网页';
-$string['resourcetypeims'] = '部署IMS包';
+$string['resourcetypeims'] = '部署 IMS 包';
 $string['resourcetypelabel'] = '插入标签';
 $string['resourcetyperepository'] = '链接到仓库中的对象';
 $string['resourcetypetext'] = '编写文本页';
@@ -118,22 +127,14 @@ $string['serverurl'] = '服务器链接({$a->wwwroot})';
 $string['showcourseblocks'] = '显示课程版块';
 $string['skipsubmenus'] = '跳过子菜单页面';
 $string['tableofcontents'] = '显示目录内容';
+$string['tableofcontentsabbrev'] = 'TOC';
 $string['upbutton'] = '向上';
 $string['variablename'] = '变量名';
-$string['viewims'] = '查看IMS包';
+$string['viewims'] = '查看 IMS 包';
 $string['vol'] = '音量';
 $string['warningblockingdelete'] = '警告：删除操作被现存的资源阻止。首先更新您的资源，或者请管理员在全局资源配置中禁止这项安全机制。';
 $string['warningdisabledrename'] = '警告：自动重命名资源中的引用的机制被禁止。如需要，请手动更新受影响的资源。管理员也可以在全局资源配置中使自动重命名有效。';
+$string['websearchdefault'] = '默认 Web 搜索引擎';
+$string['resourcetypejmol'] = '分子结构3D图'; // ORPHANED
 
-	  //翻译者：况亮
-	  //Email：kuangliang12345@163.com
-	  //Moodle 1.9 2008/04
-
-$string['allowlocalfiles'] = '允许本地文件';
-$string['autofilerename'] = '如果文件重命名则自动更新链接';
-$string['blockdeletingfile'] = '版块删除引用文件';
-$string['framesize'] = 'Frame大小';
-$string['resourcedefaulturl'] = '默认URL';
-$string['tableofcontentsabbrev'] = 'TOC';
-$string['websearchdefault'] = '默认Web搜索引擎';
 ?>
