fdc9bd4cb6b595867a922c1332dc39ba5dcc6860
Abel Camacho <ueu_ueu>
2009-06-22 09:46:17 +0000

    New file added

---
+++ eu_utf8/auth_ldap.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // auth_ldap.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['auth_ldap_ad_create_req'] = 'Ezin da sortu kontu berria Active Directory-n. Ziurtatu lan honetarako baldintza guztiak betetzen dituzula (LDAPS konexioa, eskubide egokiak dituen erabiltzailea,e.a.).';
+$string['auth_ldap_attrcreators'] = 'Talde edo testuinguru zerrenda, zeinetako partaideek atributuak sortzeko baimena duten. Banatu taldeak \',\' erabilita. Orokorrean, honelako zerbait: \'cn=teachers,ou=staff,o=myorg\'';
+$string['auth_ldap_attrcreators_key'] = 'Atributu-sortzaileak';
+$string['auth_ldap_auth_user_create_key'] = 'Erabiltzaileak kanpotik sortu';
+$string['auth_ldap_bind_dn'] = 'Erabiltzaileak bilatzeko bind-user erabili nahi baduzu, hemen zehaztu. Honen antzeko zerbait: \'cn=ldapuser,ou=public,o=org\'';
+$string['auth_ldap_bind_dn_key'] = 'Izen gorena';
+$string['auth_ldap_bind_pw'] = 'bind-user erabiltzailearen pasahitza.';
+$string['auth_ldap_bind_pw_key'] = 'Pasahitza';
+$string['auth_ldap_bind_settings'] = 'Ezarpenak finkatu';
+$string['auth_ldap_changepasswordurl_key'] = 'Pasahitza aldatzeko URLa';
+$string['auth_ldap_contexts_key'] = 'Testuinguruak';
+$string['auth_ldap_user_type_key'] = 'Erabiltzaile-mota';
+
+?>
---
+++ eu_utf8/auth_none.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // auth_none.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['auth_nonedescription'] = 'Erabiltzaileek berehala eman dezakete izena eta baliozko kontuak sortu, kanpo-zerbitzari baten aurkako autentifikaziorik gabe eta e-posta bidez berretsi gabe. Kontuz aukera hau erabiltzean - honek sor ditzakeen segurtasun eta kudeaketa-arazoak kontuan hartu.';
+$string['auth_nonetitle'] = 'Autentifikaziorik ez';
+
+?>
