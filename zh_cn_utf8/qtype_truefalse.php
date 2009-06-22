76cdcf34686f0a6d39d608cf7f2b30f52168780a
Ling Li <liling>
2009-06-22 12:39:33 +0000

    Fix some strings

--- zh_cn_utf8/qtype_truefalse.php
+++ zh_cn_utf8/qtype_truefalse.php
@@ -1,18 +1,14 @@
 <?PHP // $Id$ 
-      // qtype_truefalse.php - created with Moodle 1.8 (2007021501)
+      // qtype_truefalse.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
+$string['addingtruefalse'] = '添加判断题';
 $string['correctanswer'] = '正确答案';
+$string['editingtruefalse'] = '编辑判断题';
 $string['false'] = '错误';
-$string['feedbackfalse'] = '反馈（错误）';
-$string['feedbacktrue'] = '反馈（正确）';
+$string['feedbackfalse'] = '选择“错误”时的反馈信息';
+$string['feedbacktrue'] = '选择“正确”时的反馈信息';
 $string['true'] = '正确';
 $string['truefalse'] = '正确/错误';
-	 
-	  //翻译者：况亮
-	  //Email：kuangliang12345@163.com
-	  //Moodle 1.9 2008/04
 
-$string['addingtruefalse'] = '添加一个正确/错误试题';
-$string['editingtruefalse'] = '编辑一个正确/错误试题';
 ?>
