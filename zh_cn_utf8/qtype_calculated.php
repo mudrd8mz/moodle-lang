3f80ad6980343aa15aff1696cdb2b00fcf484124
Ling Li <liling>
2009-06-22 12:45:19 +0000

    Add spaces between English and Chinese word.

--- zh_cn_utf8/qtype_calculated.php
+++ zh_cn_utf8/qtype_calculated.php
@@ -1,9 +1,9 @@
 <?PHP // $Id$ 
-      // qtype_calculated.php - created with Moodle 1.8.2+ (2007021520)
+      // qtype_calculated.php - created with Moodle 1.9.5 (Build: 20090520) (2007101550)
 
 
 $string['addmoreanswerblanks'] = '添加另一个答案空白';
-$string['addmoreunitblanks'] = '{$a}更多的单位空白';
+$string['addmoreunitblanks'] = '{$a} 更多的单位空白';
 $string['answerhdr'] = '答案';
 $string['atleastoneanswer'] = '至少需要提供一个答案';
 $string['correctanswershows'] = '显示正确的答案';
@@ -17,12 +17,14 @@ $string['mustbenumeric'] = '必须键入一个数字';
 $string['mustnotbenumeric'] = '不能是一个数字';
 $string['newcategory1'] = '使用一个新的共享数据集';
 $string['newlocal1'] = '使用一个新的私有数据集';
-$string['nextitemtoadd'] = '下一步‘添加条目’';
+$string['nextitemtoadd'] = '下一步“添加条目”';
 $string['nextpage'] = '下页';
 $string['nodataset'] = '无数据集';
 $string['nosharedwildcard'] = '在类别中没有共享的通配符';
 $string['possiblehdr'] = '通配符仅仅在问题文本中呈现';
 $string['tolerance'] = '允许误差';
+$string['trueanswerinsidelimits'] = '正确的回答：';
+$string['trueansweroutsidelimits'] = '<span class=\"error\">错误：{$a->correct} 在 {$a->true} 的限定之外。</span>';
 $string['updatecategory'] = '更新类别';
 $string['usedinquestion'] = '在试题中使用';
 $string['youmustenteramultiplierhere'] = '必须键入一个乘数';
