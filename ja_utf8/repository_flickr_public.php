710e0562d7d93a23c5e9d87858a005feb3c84aa6
Mitsuhiro Yoshida <mits>
2009-06-24 16:27:56 +0000

    MDL-17022 Updated a translated string for admin based on lang/en_utf8 rewording. MDL-18830 Translated new strings for repository flickr. MDL-16909 Adding a translated lang file for repository Mahara.

--- ja_utf8/repository_flickr_public.php
+++ ja_utf8/repository_flickr_public.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // repository_flickr_public.php - created with Moodle 2.0 dev (Build: 20090423) (2009041700)
+      // repository_flickr_public.php - created with Moodle 2.0 dev (Build: 20090619) (2009061706)
 
 
 $string['all'] = 'すべて';
@@ -11,6 +11,7 @@ $string['by-nc-sa'] = '帰属-非商用-派生許可ライセンス (Attribution
 $string['by-nd'] = '帰属-派生禁止ライセンス (Attribution-NoDerivs License)';
 $string['by-sa'] = '帰属-派生許可ライセンス (Attribution-ShareAlike License)';
 $string['callbackurl'] = 'コールバックURI';
+$string['commercialuse'] = 'あなたの作品の商用利用を許可しますか?';
 $string['configplugin'] = 'Flickrパブリック設定';
 $string['creativecommonscommercial'] = 'クリエイティブ・コモンズ商用 (creative commons commercial) のみ';
 $string['emailaddress'] = 'メールアドレス';
@@ -18,12 +19,14 @@ $string['fulltext'] = 'フルテキスト';
 $string['information'] = 'あなたのMoodleサイトに<a href=\"http://www.flickr.com/services/api/keys/\">Flickr APIキー</a>を取得してください。';
 $string['invalidemail'] = '無効なFlickrメールアドレスです。';
 $string['license'] = 'ライセンス';
+$string['modification'] = 'あなたの作品の修正を許可しますか?';
 $string['notitle'] = 'タイトルなし';
 $string['nullphotolist'] = 'このアカウントに関連する写真はありません。';
 $string['remember'] = '次回から入力を省略する';
 $string['repositorydesc'] = 'flickr.comのリポジトリ';
 $string['repositoryname'] = 'Flickrパブリック';
 $string['secret'] = 'シークレット';
+$string['sharealike'] = 'はい、他のユーザが同様に共有する限り';
 $string['tag'] = 'タグ';
 $string['username'] = 'Flickrアカウントメール';
 
