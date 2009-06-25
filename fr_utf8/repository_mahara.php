de9f5c5e0c947a524e39616bd273885d5c95877b
Nicolas Martignoni <martignoni>
2009-06-25 20:14:40 +0000

    MDL-16909 New file added

---
+++ fr_utf8/repository_mahara.php
@@ -0,0 +1,17 @@
+<?php // $Id$
+
+$string['configplugin'] = 'Configuration du dépôt Mahara';
+$string['repositoryname'] = 'Mahara';
+$string['repositorydesc'] = 'Fichiers sur un serveur Moodle distant';
+$string['notitle'] = 'sans titre';
+$string['peer'] = 'Pair';
+$string['remember'] = 'Se souvenir';
+$string['emptyfilelist'] = 'Il n\'y a plus de fichier à afficher';
+$string['remoterep_name'] = 'Mahara';
+$string['remoterep_description'] = 'Permettre la découverte du service';
+$string['connectionfailure'] = 'Échec de la récupération de la liste de fichiers. Le Moodle hôte a une version antérieure à la version 2.0 ou son service de dépôt Mahara n\'a pas été activé';
+$string['failtoretrievelist'] = 'La liste n\'a pas pu être récupérée ou est vide';
+$string['usernotfound'] = 'L\'utilisateur $a n\'est pas enregistré dans Mahara';
+$string['usercannotaccess'] = 'Vous ($a) ne pouvez pas accéder à ce fichier';
+$string['nopeer'] = 'Veuillez configurer quelques <a href="http://docs.moodle.org/fr/R%C3%A9seau_Moodle#R.C3.A9seau_serveur_.C3.A0_serveur">pairs Moodle</a> pour votre site Mahara.';
+$string['hostnotfound'] = 'Impossible de trouver le site Mahara dans la base de données. Veuillez contacter votre administrateur système';
