deae1e7cc944401e6d94ba008a77d4395602c890
Mitsuhiro Yoshida <mits>
2009-06-29 15:13:32 +0000

    MDL-19654 Adding a translated lang file for repository draft files.

---
+++ ja_utf8/repository_draft.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // repository_draft.php - created with Moodle 2.0 dev (Build: 20090619) (2009061706)
+
+
+$string['repositorydesc'] = '現在の下書きファイルを閲覧するためのプラグイン';
+$string['repositoryname'] = '現在のファイル';
+
+?>
