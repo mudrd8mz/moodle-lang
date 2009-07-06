1328b9d66b31c3fb3680d50904682a53ba5d29a9
Mitsuhiro Yoshida <mits>
2009-07-06 10:59:50 +0000

    Brushed up translated strings for LAMS.

--- ja_utf8/lams.php
+++ ja_utf8/lams.php
@@ -1,9 +1,9 @@
 <?PHP // $Id$ 
-      // lams.php - created with Moodle 2.0 dev (2007101505)
+      // lams.php - created with Moodle 2.0 dev (Build: 20090703) (2009070100)
 
 
-$string['createSequence'] = '新しいシーケンスの作成';
-$string['editSequence'] = '選択したシーケンスの編集';
+$string['createSequence'] = '新しいシーケンスを作成する';
+$string['editSequence'] = '選択したシーケンスを編集する';
 $string['error'] = '申し訳ございません、不明なエラーが発生しました。';
 $string['introduction'] = 'イントロダクション';
 $string['lams'] = '-- LAMS - Learning Activity Management System --';
@@ -14,22 +14,22 @@ $string['lesson'] = '学習セッション';
 $string['modulename'] = 'LAMS';
 $string['modulenameplural'] = 'LAMS';
 $string['notsetup'] = 'NOT_SET_UP';
-$string['openauthor'] = 'LAMS作成者のオープン';
-$string['openlearner'] = 'LAMS学習者のオープン';
-$string['openmonitor'] = 'LAMSモニターのオープン';
-$string['refreshSequenceList'] = 'シーケンスリストのリフレッシュ';
+$string['openauthor'] = 'LAMS作成者をオープンする';
+$string['openlearner'] = 'LAMS学習者をオープンする';
+$string['openmonitor'] = 'LAMSモニターをオープンする';
+$string['refreshSequenceList'] = 'シーケンスリストをリフレッシュする';
 $string['selectExistingSequence'] = '登録済みシーケンスを選択するか、新しいシーケンスを作成してください。';
-$string['sequence'] = 'シーケンスの選択';
+$string['sequence'] = 'シーケンスを選択する';
 $string['server_id'] = 'サーバID';
 $string['server_key'] = 'サーバキー';
 $string['server_url'] = 'サーバURI';
 $string['serverid'] = '<a href=\"http://www.lamsinternational.com\">LAMS international</a> から取得したサーバIDを入力してください。';
 $string['serverkey'] = '<a href=\"http://www.lamsinternational.com\">LAMS international</a> から取得したサーバkeyを入力してください。';
 $string['serverurl'] = 'LAMSサーバにアクセスするためのURIを入力してください。例: http://localhost:8080/lams';
-$string['useSequence'] = '選択したシーケンスを使用';
+$string['useSequence'] = '選択したシーケンスを使用する';
 $string['visibletostudents'] = '活動を学生に表示する';
 $string['wikistartederror'] = 'すでにWikiの登録データがあります - 変更できません。';
-$string['workspace'] = 'ワークスペースの選択';
+$string['workspace'] = 'ワークスペースを選択する';
 $string['wrongversionrange'] = '$a は正しい範囲ではありません!';
 
 ?>
