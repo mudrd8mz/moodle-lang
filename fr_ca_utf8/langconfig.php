c676ace10533ea631f5746107df4ed49dbcca9e9
Nicolas Martignoni <martignoni>
2009-07-12 21:04:14 +0000

    File cleaned up

--- fr_ca_utf8/langconfig.php
+++ fr_ca_utf8/langconfig.php
@@ -2,23 +2,16 @@
 
 $string['alphabet'] = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
-$string['firstdayofweek'] = '1';
+$string['decsep'] = ','; // Decimal separator, for most languages it is '.'. Cannot be the same as 'listsep'
+$string['firstdayofweek'] = '1'; // 0 = Sunday, 1 = Monday, etc.
+$string['listsep'] = ';'; // List separator, for most languages it is a comma ','. Cannot be the same as 'decsep'
 $string['locale'] = 'fr_CA.UTF-8';
-$string['oldcharset'] = 'ISO-8859-15';
+$string['localewin'] = 'French_Canada.1252';
+$string['oldcharset'] = 'ISO-8859-1';
 $string['parentlanguage'] = 'fr_utf8';
-$string['strftimedate'] = '%%Y - %%m - %%d';
-$string['strftimedateshort'] = '%%d %%B';
-$string['strftimedatetime'] = '%%Y - %%m - %%d, %%H:%%M';
-$string['strftimedaydate'] = '%%A, %%d %%B %%Y';
-$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%H:%%M ';
-$string['strftimedayshort'] = '%%A, %%d %%B';
-$string['strftimedaytime'] = '%%a, %%H:%%M';
-$string['strftimemonthyear'] = '%%B %%Y';
-$string['strftimerecent'] = '%%d %%b, %%H:%%M';
-$string['strftimerecentfull'] = '%%a, %%d %%b %%Y, %%H:%%M';
-$string['strftimetime'] = '%%H:%%M';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Français - Canada';
+$string['thousandssep'] = ' '; // thousands separator, for some languages it is '.' or '\''
 
 ?>
