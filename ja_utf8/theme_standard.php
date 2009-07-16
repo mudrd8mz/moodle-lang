ce8995468e6f856476aeb5e0dd171a674a295a6d
Mitsuhiro Yoshida <mits>
2009-07-16 23:15:13 +0000

    MDL-19398 Adding translated lang files for blocks editing ui.

---
+++ ja_utf8/theme_standard.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // theme_standard.php - created with Moodle 2.0 dev (Build: 20090715) (2009071300)
+
+
+$string['region-side-post'] = '右';
+$string['region-side-pre'] = '左';
+
+?>
