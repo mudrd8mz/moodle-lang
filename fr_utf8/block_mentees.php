837656ecbcc346d46b6b8e6720e0ca85050a8a7f
Nicolas Martignoni <martignoni>
2009-07-29 17:16:06 +0000

    MDL-19889 New string

--- fr_utf8/block_mentees.php
+++ fr_utf8/block_mentees.php
@@ -2,6 +2,7 @@
 
 $string['blockname'] = 'Participants suivis';
 $string['configtitle'] = 'Titre du bloc';
+$string['configtitleblankhides'] = 'Titre du bloc (pas de titre si vide)';
 $string['leaveblanktohide'] = 'Laisser vide pour cacher le titre';
 $string['newmenteesblock'] = '(Nouveau bloc participants suivis)';
 
