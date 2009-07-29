ae6d3c9f727e484412d87617ff92eef7bfbe76a9
Mitsuhiro Yoshida <mits>
2009-07-29 07:18:49 +0000

    MDL-19889 Translated a new string for mentees block.

--- ja_utf8/block_mentees.php
+++ ja_utf8/block_mentees.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_mentees.php - created with Moodle 2.0 dev (Build: 20080503) (2008050200)
+      // block_mentees.php - created with Moodle 2.0 dev (Build: 20090727) (2009072700)
 
 
 $string['blockname'] = 'メンティ';
 $string['configtitle'] = 'ブロックタイトル';
+$string['configtitleblankhides'] = 'ブロックタイトル (空白の場合、タイトルなし)';
 $string['leaveblanktohide'] = '空白でタイトルを非表示';
 $string['newmenteesblock'] = '(新しいメンティブロック)';
 
