75aee11d77019145059fd6deee6838eae08ab71c
Nicolas Martignoni <martignoni>
2009-08-12 10:04:08 +0000

    Sorting strings

--- fr_utf8/repository_local.php
+++ fr_utf8/repository_local.php
@@ -2,11 +2,11 @@
 
 $string['configplugin'] = 'Configuration du dépôt de fichiers local';
 $string['currentusefiles'] = 'Fichiers utilisés actuellement';
+$string['emptyfilelist'] = 'Il n\'y a aucun fichier à afficher';
 $string['local:view'] = 'Consulter le dépôt de fichiers local';
-$string['repositoryname'] = 'Fichiers locaux';
-$string['repositorydesc'] = 'Dépôt dans le serveur Moodle local';
 $string['notitle'] = 'sans titre';
 $string['remember'] = 'Enregistrer';
-$string['emptyfilelist'] = 'Il n\'y a aucun fichier à afficher';
+$string['repositorydesc'] = 'Dépôt dans le serveur Moodle local';
+$string['repositoryname'] = 'Fichiers locaux';
 
 ?>
