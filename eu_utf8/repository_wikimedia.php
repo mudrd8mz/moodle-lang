6ae26e5497c8a8b89ef74094993535eea77e10f2
Abel Camacho <ueu_ueu>
2009-09-07 10:20:08 +0000

    New string added

--- eu_utf8/repository_wikimedia.php
+++ eu_utf8/repository_wikimedia.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$ 
-      // repository_wikimedia.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+      // repository_wikimedia.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
 $string['keyword'] = 'Irudiaren izena';
 $string['repositoryname'] = 'Wikimedia';
+$string['wikimedia:view'] = 'Wikimedia biltegia ikusi';
 
 ?>
