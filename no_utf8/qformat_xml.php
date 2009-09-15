fe32ffa48b565714f296d46e954463901581ab3b
efaktorcvs <efaktorcvs>
2009-09-15 05:01:43 +0000

    New translation.

---
+++ no_utf8/qformat_xml.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qformat_xml.php - created with Moodle 1.9.5+ (Build: 20090826) (2007101550)
+
+
+$string['invalidxml'] = 'Ugyldig xml-fil - streng forventet (bruke CDATA?)';
+$string['unsupportedexport'] = 'Spørsmålstypen $a er ikke støttet ved xml-eksport';
+
+?>
