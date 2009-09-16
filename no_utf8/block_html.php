afdbe1bdbe493542fe397ab670d30c27dc9739f3
efaktorcvs <efaktorcvs>
2009-09-16 08:35:51 +0000

    Language string cleaning by eFaktor as

--- no_utf8/block_html.php
+++ no_utf8/block_html.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // block_html.php - created with Moodle 1.5.2 (2005060220)
+      // block_html.php - created with Moodle 1.9.5+ (Build: 20090909) (2007101551)
 
 
 $string['configcontent'] = 'Innhold';
 $string['configtitle'] = 'Boksoverskrift';
 $string['html'] = 'HTML';
 $string['leaveblanktohide'] = 'la denne stå tom for å skjule tittelen';
-$string['newhtmlblock'] = 'Ny HTML-boks';
+$string['newhtmlblock'] = '(Ny HTML-boks)';
 
 ?>
--- no_utf8/bulkusers.php
+++ no_utf8/bulkusers.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // bulkusers.php - created with Moodle 1.9 (2007101509)
+      // bulkusers.php - created with Moodle 1.9.5+ (Build: 20090909) (2007101551)
 
 
 $string['addall'] = 'Legg til alle';
@@ -12,9 +12,9 @@ $string['confirmmessage'] = 'Vil du virkelig sende denne meldingen til alle bruk
 $string['nofilteredusers'] = 'Ingen brukere funnet (0/$a)';
 $string['noselectedusers'] = 'Ingen brukere valgt';
 $string['removeall'] = 'Fjern alle';
-$string['removesel'] = 'Ta bort fra seksjon';
+$string['removesel'] = 'Fjern fra utvalget';
 $string['selected'] = 'Valgt';
-$string['selectedlist'] = 'Valgt bruker liste';
+$string['selectedlist'] = 'Valgt brukerliste';
 $string['usersfound'] = '$a bruker(e) funnet.';
 $string['usersinlist'] = 'Brukere på listen';
 $string['usersselected'] = '$a bruker(e) valgt.';
