a69613079853752dd02e6918a13db39647e43924
Lee Chan-young <yogibear270>
2009-09-17 05:52:43 +0000

    new version

---
+++ ko_utf8/auth_cas.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // auth_cas.php - created with Moodle 2.0 dev (Build: 20090911) (2009090800)
+
+
+$string['CASform'] = '인증서 선택';
+$string['accesCAS'] = 'CAS 사용자';
+$string['accesNOCAS'] = '기타 사용자';
+$string['auth_cas_auth_user_create'] = '외부 사용자 생성';
+
+?>
