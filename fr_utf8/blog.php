22a14030361c4b7d8d2245b1935c5e4347bd6137
Nicolas Martignoni <martignoni>
2009-09-18 09:33:43 +0000

    MDL-19676 strings tranlated

--- fr_utf8/blog.php
+++ fr_utf8/blog.php
@@ -1,6 +1,7 @@
 <?php  // $Id$
 
 $string['addnewentry'] = 'Ajouter un nouvel article';
+$string['assocdescription'] = 'Si vous écrivez au sujet d\'un cours et/ou d\'une activité, sélectionnez-les ici.';
 $string['backupblogshelp'] = 'Ce réglage active la sauvegarde des blogs dans les sauvegardes automatiques du site';
 $string['blockmenutitle'] = 'Menu blog';
 $string['blocktagstitle'] = 'Tags de blog';
@@ -36,6 +37,7 @@ $string['groupblogdisable'] = 'Le blog de groupe n\'est pas activé';
 $string['groupblogs'] = 'Les utilisateurs ne peuvent voir que les articles des blogs d\'utilisateurs dans les mêmes groupes qu\'eux';
 $string['incorrectblogfilter'] = 'Type de filtre de blog incorrect';
 $string['intro'] = 'Ce flux RSS a été généré automatiquement à partir d\'un ou plusieurs blogs.';
+$string['invalidgroupid'] = 'Identifiant de groupe non valide';
 $string['noentriesyet'] = 'Aucun article visible ici';
 $string['noguestpost'] = 'Les invités ne peuvent écrire dans le blog !';
 $string['norighttodeletetag'] = 'Vous n\'êtes pas autorisé à supprimer ce tag : $a';
@@ -50,6 +52,7 @@ $string['publishto'] = 'Publier pour';
 $string['publishtonoone'] = 'Vous-même (brouillon)';
 $string['publishtosite'] = 'Tout le monde sur ce site';
 $string['publishtoworld'] = 'Tout le monde (grand public)';
+$string['readfirst'] = 'Veuillez avant tout lire ceci';
 $string['settingsupdatederror'] = 'Une erreur est survenue. Les préférences de blog n\'ont pas été enregistrées';
 $string['siteblog'] = 'Blog de site : $a';
 $string['siteblogdisable'] = 'Le blog du site n\'est pas activé';
