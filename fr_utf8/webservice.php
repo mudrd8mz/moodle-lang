45e19e3319fd955dd8dd9e14d260306da88902f3
Nicolas Martignoni <martignoni>
2009-09-18 13:25:52 +0000

    MDL-12886 New strings translated

--- fr_utf8/webservice.php
+++ fr_utf8/webservice.php
@@ -1,5 +1,38 @@
 <?php // $Id$
 
+$string['activated'] = 'Activé';
+$string['activatedfunctions'] = 'Fonctions activées';
+$string['amfdebug'] = 'Mode débogage du serveur AMF';
+$string['clicktoactivate'] = 'Cliquer pour activer';
+$string['clicktodeactivate'] = 'Cliquer pour désactiver';
+$string['component'] = 'Composant';
+$string['createservicelabel'] = 'Créer un service personnalisé';
+$string['custom'] = 'Personnalisé';
+$string['debugdisplayon'] = 'Le réglage « Afficher les messages de débogage » est activé. Le serveur XMLRPC ne fonctionnera pas. Les autres serveurs de services web pourraient en outre retourner des problèmes.<br />Veuillez  en informer l\'administrateur du Moodle, afin qu\'il désactive ce réglage.';
+$string['enabled'] = 'Activé';
+$string['fail'] = 'Échec';
+$string['functionlist'] = 'Liste des fonctions services web';
+$string['functionname'] = 'Nom de fonction';
+$string['moodlepath'] = 'Chemin Moodle';
+$string['ok'] = 'OK';
+$string['protocolenable'] = 'Activer le protocole $a[0]';
+$string['protocols'] = 'Protocoles';
+$string['save'] = 'Enregistrer';
+$string['servicelist'] = 'Services';
+$string['servicename'] = 'Nom du service';
+$string['systemsettings'] = 'Réglages communs';
+$string['user'] = 'Utilisateur';
+$string['usersettings'] = 'Utilisateurs avec autorisations services web';
+$string['webservicesenable'] = 'Activer les services web';
+$string['webservicesenable'] = 'Web services enable';
+$string['wsdeletefunction'] = '<b>$a->functionname</b> function has been deleted from the <b>$a->servicename</b> service.';
+$string['wsinsertfunction'] = '<b>$a->functionname</b> function has been inserted into the <b>$a->servicename</b> service.';
+$string['wspagetitle'] = 'Documentation services web';
+$string['wsuserreminder'] = 'Rappel ! L\'administrateur Moodle de ce site doit vous donner la capacité moodle/site:usewebservices.';
+$string['wsuserreminder'] = 'Reminder: the Moodle administrator of this site needs to give you moodle/site:usewebservices capability.';
+
+
+
 $string['soapdocumentation'] = '<h2>Manuel SOAP</h2>
         <b>1.</b> Appeler la méthode <b>get_token</b> sur « <i>http://remotemoodle/webservice/soap/server.php?wsdl</i> »<br />
         Le paramètre de la fonction est un tableau : en PHP, on écrirait array(\"username\" => \"wsuser\", \"password\" => \"wspassword\")<br />
@@ -28,15 +61,3 @@ $string['xmlrpcdocumentation'] = '<h2>Manuel XMLRPC</h2>
         on appellera quelque chose comme :<br />
         your_client->call(\"user.tmp_delete_user\", array(array(\"username\" => \"username_to_delete\",\"mnethostid\" => 1)))<br /><br />
 ';
-$string['functionlist'] = 'Liste des fonctions service web';
-$string['moodlepath'] = 'Chemin Moodle';
-$string['wspagetitle'] = 'Documentation services web';
-$string['webservicesenable'] = 'Activer les services web';
-$string['protocolenable'] = 'Activer le protocole $a[0]';
-$string['ok'] = 'OK';
-$string['fail'] = 'Échec';
-$string['wsuserreminder'] = 'Rappel ! L\'administrateur Moodle de ce site doit vous donner la capacité moodle/site:usewebservices.';
-$string['debugdisplayon'] = 'Le réglage « Afficher les messages de débogage » est activé. Le serveur XMLRPC ne fonctionnera pas. Les autres serveurs de services web pourraient en outre retourner des problèmes.<br />Veuillez  en informer l\'administrateur du Moodle, afin qu\'il désactive ce réglage.';
-$string['amfdebug'] = 'Mode débogage du serveur AMF';
-
-?>
