496953de2f2e36913ed68e65d5563f79ddb15756
Nicolas Martignoni <martignoni>
2009-09-18 09:20:35 +0000

    MDL-20219 strings translated

--- fr_utf8/block.php
+++ fr_utf8/block.php
@@ -3,6 +3,7 @@
 $string['appearsinsubcontexts'] = 'Apparaît dans des sous-contextes';
 $string['anypagematchingtheabove'] = 'Toutes les pages correspondant aux critères ci-dessus';
 $string['blocksettings'] = 'Réglage du bloc';
+$string['blogadministration'] = 'Administration du blog';
 $string['bracketfirst'] = '$a (premier)';
 $string['bracketlast'] = '$a (dernier)';
 $string['defaultregion'] = 'Région par défaut';
@@ -11,6 +12,7 @@ $string['moveblockhere'] = 'Déplacer le bloc ici';
 $string['movingthisblockcancel'] = 'Déplacement de ce bloc ($a)';
 $string['onthispage'] = 'Sur cette page';
 $string['pagetypes'] = 'Types de page';
+$string['preferences'] = 'Préférences';
 $string['region'] = 'Région';
 $string['subpages'] = 'Sous-page spécifique';
 $string['thisblockbelongsto'] = 'Ce bloc appartient à';
