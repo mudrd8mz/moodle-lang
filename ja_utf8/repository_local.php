ce6bee8367c71975011308462affa4ab0e0e0c52
Mitsuhiro Yoshida <mits>
2009-09-22 22:11:21 +0000

    MDL-16597 Updated translated strings for repository based on en_utf8 wording change.

--- ja_utf8/repository_local.php
+++ ja_utf8/repository_local.php
@@ -1,14 +1,14 @@
 <?PHP // $Id$ 
-      // repository_local.php - created with Moodle 2.0 dev (Build: 20090807) (2009080700)
+      // repository_local.php - created with Moodle 2.0 dev (Build: 20090919) (2009091700)
 
 
-$string['configplugin'] = 'ローカルファイルリポジトリの設定';
+$string['configplugin'] = 'サーバファイルリポジトリの設定';
 $string['currentusefiles'] = '現在使用中のファイル';
 $string['emptyfilelist'] = '表示するファイルはありません。';
-$string['local:view'] = 'ローカルファイルリポジトリを表示する';
+$string['local:view'] = 'サーバリポジトリを表示する';
 $string['notitle'] = 'タイトルなし';
 $string['remember'] = '次回から入力を省略する';
-$string['repositorydesc'] = 'ローカルMoodleサーバのリポジトリ';
-$string['repositoryname'] = 'ローカルファイル';
+$string['repositorydesc'] = '前回、Moodleサーバにアップロードされたファイル';
+$string['repositoryname'] = 'サーバファイル';
 
 ?>
