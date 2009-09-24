c53166e9365ffb4d585ce278001de0d17190e750
Lee Chan-young <yogibear270>
2009-09-24 07:35:29 +0000

    translate added strings

--- ko_utf8/repository_filesystem.php
+++ ko_utf8/repository_filesystem.php
@@ -1,10 +1,12 @@
 <?PHP // $Id$ 
-      // repository_filesystem.php - created with Moodle 2.0 dev (Build: 20090321) (2009030501)
+      // repository_filesystem.php - created with Moodle 2.0 dev (Build: 20090911) (2009090800)
 
 
 $string['blockedpath'] = '폐쇄된 루트 경로';
 $string['configplugin'] = '파일 시스템의 저장소 설정';
 $string['donotusesysdir'] = '시스템 경로를 저장소로 사용하지 말 것';
+$string['filesystem:view'] = '파일시스템 저장소 보기';
+$string['information'] = '%%moodledata%%/repository/ 에 대한 상대 경로임';
 $string['invalidpath'] = '잘못된 루트 경로';
 $string['path'] = '경로';
 $string['repositorydesc'] = '로컬 디렉토리에 저장소 생성';
