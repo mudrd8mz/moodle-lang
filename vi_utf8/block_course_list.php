aa7506dbdd124e304e8da291014842f4031933a2
dlgserver <dlgserver>
2009-10-08 15:44:54 +0000

    no message

--- vi_utf8/block_course_list.php
+++ vi_utf8/block_course_list.php
@@ -1,8 +1,14 @@
-<?php // $Id$ 
+<?PHP // $Id$ 
+      // block_course_list.php - created with Moodle 1.9.2+ (Build: 20080813) (2007101521)
+      // local modifications from http://hochanh.info/moodle
 
-$string['blockname'] = 'Danh sách các cua học';
-$string['allcourses'] = 'Người quản trị nhìn thấy tất cả các cua học';
-$string['configadminview'] = 'Người quản trị nên xem xét gì trong khối danh sách cua học ?';
-$string['owncourses'] = 'Người quản trị hiểu rõ các cua học của riêng mình ';
+
+$string['adminview'] = 'Xem với tư cách quản trị';
+$string['allcourses'] = 'Quản trị viên thấy được tất cả các khoá học';
+$string['blockname'] = 'Danh sách khoá học';
+$string['configadminview'] = 'Cấu hình hiển thị khối danh sách khoá học đối với quản trị viên';
+$string['confighideallcourseslink'] = 'Ẩn liên kết \"Tất cả các khoá học\" bên dưới khối. Thiết lập này không có hiệu lực đối với quản trị viên';
+$string['hideallcourseslink'] = 'Ẩn liên kết \"Tất cả các khoá học\"';
+$string['owncourses'] = 'Quản trị viên chỉ thấy được các khoá học của mình';
 
 ?>
---
+++ vi_utf8/block_loancalc.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // block_loancalc.php - created with Moodle 1.9.2+ (Build: 20080813) (2007101521)
+      // local modifications from http://localhost/moodle192
+
+
+$string['amountofloan'] = 'Số tiền vay';
+$string['fortnightly'] = 'Mỗi hai tháng';
+$string['interestrate'] = 'Lãi suất';
+$string['loancalc'] = 'Máy tính vay tiền';
+$string['loanterm'] = 'Thời hạn vay';
+$string['monthly'] = 'Hàng tháng';
+$string['repaymentamount'] = 'Số tiền trả';
+$string['repaymentfreq'] = 'Định kì trả';
+$string['weekly'] = 'Hàng tuần';
+
+?>
