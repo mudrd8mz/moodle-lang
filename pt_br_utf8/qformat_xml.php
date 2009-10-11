bcd2dfde62c36103e7aa03a890a24e0e3941f3ea
Paula de Waal <papita>
2009-10-11 17:53:56 +0000

    *** empty log message ***

---
+++ pt_br_utf8/qformat_xml.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qformat_xml.php - created with Moodle 1.9.5+ (Build: 20091007) (2007101553)
+
+
+$string['invalidxml'] = 'Arquivo XML inválido - usar CDATA?';
+$string['unsupportedexport'] = 'Questões de tipo $a não são suportadas pela exportação xml';
+
+?>
