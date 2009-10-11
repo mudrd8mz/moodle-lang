a3523e2a7487da7d4f89d3322b9e0bf85a7176fc
Paula de Waal <papita>
2009-10-11 18:02:26 +0000

    *** empty log message ***

---
+++ pt_br_utf8/coursereport_outline.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // coursereport_outline.php - created with Moodle 1.9.5+ (Build: 20091007) (2007101553)
+
+
+$string['outline:view'] = 'Ver relatório das atividades do curso';
+
+?>
