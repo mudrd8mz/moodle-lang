ba4110bf6eb7a428e3f5a4f02e7a8af46dea9e28
Paula de Waal <papita>
2009-10-11 17:48:12 +0000

    *** empty log message ***

---
+++ pt_br_utf8/qtype_random.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qtype_random.php - created with Moodle 1.9.5+ (Build: 20091007) (2007101553)
+
+
+$string['configselectmanualquestions'] = 'Permitir a inclusão de perguntas com avaliação manual?';
+$string['selectmanualquestions'] = 'Questões aleatórias podem incluir perguntas com avaliação manual';
+
+?>
