b04cd9f152b3593f6b556d4d2aba3ad43c8e6324
Paula de Waal <papita>
2009-10-11 10:41:11 +0000

    one string added

--- pt_br_utf8/block_online_users.php
+++ pt_br_utf8/block_online_users.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_online_users.php - created with Moodle 1.9.2+ (Build: 20080903) (2007101522)
+      // block_online_users.php - created with Moodle 1.9.5+ (Build: 20091007) (2007101553)
 
 
 $string['blockname'] = 'Usuários Online';
 $string['configtimetosee'] = 'Periodo de inatividade (minutos) que determina a exclusão do usuário da lista.';
+$string['online_users:viewlist'] = 'Ver lista de usuários online';
 $string['periodnminutes'] = 'últimos $a minutos';
 $string['timetosee'] = 'Remover após inatividade (minutos)';
 
