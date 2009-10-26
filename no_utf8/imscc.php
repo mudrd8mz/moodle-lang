cc3a54327b08270238c82b02dfa39af36f9d70cf
efaktorcvs <efaktorcvs>
2009-10-26 19:07:56 +0000

    New translation

---
+++ no_utf8/imscc.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // imscc.php - created with Moodle 1.9.6 (Build: 20091021) (2007101560)
+
+
+$string['cc2moodle_checking_schema'] = 'CC format! Sjekker skjemaet...';
+$string['cc2moodle_invalid_schema'] = 'Skjemaet er ikke gyldig.';
+$string['cc2moodle_manifest_dont_load'] = 'Ikke mulig å laste inn XML-manifestet.';
+$string['cc2moodle_req_auth'] = 'FEIL: Common Cartridge-pakken krever autorisasjon.';
+$string['cc2moodle_valid_schema'] = 'Skjemaet er gyldig!';
+$string['cc_import_req_dom'] = 'FEIL: Import av denne Common Cartridge-pakken krever et DOM tillegg.';
+$string['cc_import_req_libxml'] = 'FEIL: Import av denne Common Cartridge-pakken krever et LIBXML-tillegg.';
+$string['cc_import_req_libxmlminversion'] = 'FEIL: Import av denne Common Cartridge-pakken krever LIBXML ver-2.6.30 eller nyere.';
+$string['cc_import_req_php5'] = 'FEIL: Import av denne Common Cartridge-pakken krever PHP 5 eller nyere.';
+$string['cc_import_req_xsl'] = 'FEIL: Import av denne Common Cartridge-pakken krever XSL.';
+$string['checkingforimscc'] = 'Sjekker pakken mot IMS-CC...';
+$string['enable_cc_import'] = 'Aktiver CC import';
+$string['enable_cc_import_description'] = 'Denne innstillingen aktiverer import av Common Cartridge (IMS-CC). (Common Cartridge er en spesifikasjon for strukturert digitalt læringsinnhold.) Merk at aktiveringen krever PHP5 og tilleggene for DOM, XSL og LIBXML(2.6.30 eller nyere) er installert på serveren.';
+
+?>
