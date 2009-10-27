dc20896aa557ffa7a002d5151612abcfe9626680
David Mudrak <david.mudrak@gmail.com>
2009-10-27 20:39:31 +0000

    Resaved sam's recent changes

--- cs_utf8/auth.php
+++ cs_utf8/auth.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // auth.php - created with Moodle 1.9.4+ (Build: 20090426) (2007101546.02)
+      // auth.php - created with Moodle 1.9.6+ (Build: 20091027) (2007101560)
 
 
 $string['CASform'] = 'Volba autentizace';
@@ -366,16 +366,10 @@ $string['update_never'] = 'Nikdy';
 $string['update_oncreate'] = 'Při vytváření';
 $string['update_onlogin'] = 'Při každém přihlášení';
 $string['update_onupdate'] = 'Při aktualizaci';
-
-
-// Strings below here are module specific and will be duplicated in auth_* files
-// Module specific language strings should also be copied into their respective
-// auth_*.php files to ensure compatibility in all versions of Moodle
-
-$string['sso_idp_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, vaši uživatelé se budou moci přihlásit na vzdálený server $a, aniž by tam museli zadávat své jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj služeb\".</li></ul><br />Pokud budete <strong>využívat</strong>tuto službu, budou se moci vzdálení uživatelé ze serveru $a přihlásit na vašem serveru, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj služby\"  pro server $a. </li></ul>';
-$string['sso_idp_name'] = 'SSO - zdroj identit uživatelů';
-$string['sso_mnet_login_refused'] = 'Uživatel $a[0] nemá povoleno přihlašovat se z $a[1].';
-$string['sso_sp_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, budou se moci vzdálení uživatelé ze serveru $a přihlásit na vašem serveru, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj identit uživatelů\".</li></ul><br />Pokud budete <strong>využívat</strong>tuto službu, budou se moci vaši uživatelé přihlásit na vzdálený server $a, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj identit uživatelů\" pro server $a. </li></ul>';
-$string['sso_sp_name'] = 'SSO - zdroj služby';
+$string['sso_idp_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, vaši uživatelé se budou moci přihlásit na vzdálený server $a, aniž by tam museli zadávat své jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj služeb\".</li></ul><br />Pokud budete <strong>využívat</strong>tuto službu, budou se moci vzdálení uživatelé ze serveru $a přihlásit na vašem serveru, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj služby\"  pro server $a. </li></ul>'; // ORPHANED
+$string['sso_idp_name'] = 'SSO - zdroj identit uživatelů'; // ORPHANED
+$string['sso_mnet_login_refused'] = 'Uživatel $a[0] nemá povoleno přihlašovat se z $a[1].'; // ORPHANED
+$string['sso_sp_description'] = 'Pokud budete <strong>poskytovat</strong> tuto službu, budou se moci vzdálení uživatelé ze serveru $a přihlásit na vašem serveru, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete využívat server $a jako \"SSO - zdroj identit uživatelů\".</li></ul><br />Pokud budete <strong>využívat</strong>tuto službu, budou se moci vaši uživatelé přihlásit na vzdálený server $a, aniž by museli zadávat jméno a heslo.<ul><li><em>Závislost</em>: Musíte zároveň nastavit, že chcete sebe poskytovat jako \"SSO - zdroj identit uživatelů\" pro server $a. </li></ul>'; // ORPHANED
+$string['sso_sp_name'] = 'SSO - zdroj služby'; // ORPHANED
 
 ?>
