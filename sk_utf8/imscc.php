b81bed1e240114c7b0eca5010ceabbd708312f94
Miroslav Fikar <fikar>
2009-10-31 17:38:53 +0000

    Initial revision

---
+++ sk_utf8/imscc.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // imscc.php - created with Moodle 1.9.6 (Build: 20091021) (2007101560)
+
+
+$string['cc2moodle_checking_schema'] = 'Formát CC! Kontrola schémy';
+$string['cc2moodle_invalid_schema'] = 'Schéma nie je platná';
+$string['cc2moodle_manifest_dont_load'] = 'Nie je možné nahrať XML manifest';
+$string['cc2moodle_req_auth'] = 'CHYBA: Tento balíček Common Cartridge vyžaduje autorizáciu';
+$string['cc2moodle_valid_schema'] = 'Schéma je platná!';
+$string['cc_import_req_dom'] = 'CHYBA: Pre import Common Cartidge je vyžadované rozšírenie DOM';
+$string['cc_import_req_libxml'] = 'CHYBA: Pre import Common Cartidge je vyžadované rozšírenie LIBXML';
+$string['cc_import_req_libxmlminversion'] = 'CHYBA: Pre import Common Cartidge je vyžadované rozšírenie LIBXML verzie 2.6.30 a vyššej';
+$string['cc_import_req_php5'] = 'CHYBA: Pre import Common Cartidge je vyžadované PHP verzie 5 a vyšší';
+$string['cc_import_req_xsl'] = 'CHYBA: Pre import Common Cartidge je vyžadované XSL';
+$string['checkingforimscc'] = 'Testovanie IMS-CC...';
+$string['enable_cc_import'] = 'Povoliť import CC';
+$string['enable_cc_import_description'] = 'Týmto nastavením umožníte importovať kurzy uložené vo formáte IMS Common Cartridge (IMS-CC) pomocou bežnej obnovy kurzu. Táto funkcia vyžaduje PHP5 s rozšíreniami DOM, XSL a LIBXML (2.6.30 a vyššie).';
+
+?>
