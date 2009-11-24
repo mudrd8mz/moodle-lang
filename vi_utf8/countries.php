70f612e3b435575c04c942def12fe1b336befbcf
dlgserver <dlgserver>
2009-11-24 14:32:26 +0000

    no message

--- vi_utf8/countries.php
+++ vi_utf8/countries.php
@@ -1,66 +1,34 @@
 <?PHP // $Id$ 
-      // countries.php - created with Moodle 1.9.2+ (Build: 20080827) (2007101522)
-      // local modifications from http://localhost/online
+      // countries.php - created with Moodle 1.9.5+ (Build: 20090812) (2007101550)
+      // local modifications from http://vnslearning.com/online
 
 
-$string['AE'] = 'Các Tiểu Vương quốc Arab Thống nhất';
-$string['AO'] = 'Ăng gô la';
-$string['AQ'] = 'Châu Nam Cực';
-$string['AX'] = 'Quần đảo Åland';
-$string['BE'] = 'Bỉ';
-$string['BG'] = 'Bun ga ri';
-$string['BL'] = 'Saint Barthélemy';
-$string['BR'] = 'Bra xin';
-$string['CA'] = 'Ca na đa';
-$string['CD'] = 'Cộng hoà Dân chủ Congo';
-$string['CF'] = 'Cộng hoà Trung Phi';
-$string['CI'] = 'Bờ Biển Ngà';
-$string['CK'] = 'Quần đảo Coóc';
-$string['CL'] = 'Chi lê';
-$string['CU'] = 'Cu ba';
-$string['CX'] = 'Đảo Giáng sinh';
-$string['CZ'] = 'Cộng hoà Séc';
-$string['DO'] = 'Cộng hoà Dominican';
-$string['EH'] = 'Tây Sahara';
-$string['FI'] = 'Phần Lan';
-$string['FK'] = 'Qunầ đảo Falkland (Malvinas)';
-$string['FM'] = 'Liên bang Micronesia';
-$string['FO'] = 'Quần đảo Faroe';
-$string['GF'] = 'Guiana Pháp';
-$string['GG'] = 'Guernsey';
-$string['GQ'] = 'Guinea Xích đạo';
-$string['GR'] = 'Hi Lạp';
-$string['GS'] = 'Nam Georgia và Quần đảo South Sandwich';
-$string['HK'] = 'Hồng Kông';
-$string['HM'] = 'Quầ đảo Heard và Mc Donald';
-$string['IE'] = 'Ireland';
-$string['IM'] = 'Isle Of Man';
-$string['IQ'] = 'I rắc';
-$string['IS'] = 'Iceland';
-$string['JE'] = 'Jersey';
-$string['JP'] = 'Nhật Bản';
-$string['KH'] = 'Căm pu chia';
-$string['KP'] = 'Cộng hoà Dân chủ Nhân dân Triều Tiên';
-$string['KW'] = 'Kuwait';
-$string['KY'] = 'Quần đảo Cayman';
-$string['LA'] = 'Lào';
-$string['MD'] = 'Cộng hoà Moldova';
-$string['ME'] = 'Montenegro';
-$string['MF'] = 'Saint Martin';
-$string['MH'] = 'Quần đảo Marshall';
-$string['MP'] = 'Quần đảo Northern Mariana';
-$string['NC'] = 'Tân Caledonia';
-$string['NZ'] = 'Tân Tây Lan';
-$string['PT'] = 'Bồ Đào Nha';
-$string['RS'] = 'Serbia';
-$string['RU'] = 'Liên bang Nga';
-$string['SA'] = 'Saudi Arabia';
-$string['SB'] = 'Quần đảo Solomon';
-$string['SE'] = 'Thuỵ Điển';
-$string['TH'] = 'Thái Lan';
-$string['TL'] = 'Đông Timor';
-$string['TR'] = 'Thổ Nhĩ Kì';
-$string['UM'] = 'United States Minor Outlying Islands';
-$string['US'] = 'Hoa Kì';
+$string['AE'] = 'Các Tiểu Vương quốc Ả Rập Thống nhất';
+$string['AU'] = 'Australia';
+$string['AX'] = 'Quần đảo Ai Len';
+$string['CD'] = 'Cộng hoà Dân chủ Công gô';
+$string['CK'] = 'Quần đảo Cook';
+$string['CL'] = 'Chi Lê';
+$string['CU'] = 'Cu Ba';
+$string['CZ'] = 'Cộng Hoà Séc';
+$string['DO'] = 'Cộng Hoà Dominican';
+$string['FR'] = 'Pháp';
+$string['GB'] = 'Vương Quốc Anh';
+$string['HK'] = 'Hồng Công';
+$string['HM'] = 'Quần đảo Heard và Mc Donald';
+$string['HU'] = 'Hung ga ri';
+$string['IE'] = 'Ai Len';
+$string['IN'] = 'Ấn Độ';
+$string['IT'] = 'Ý';
+$string['KP'] = 'Triều Tiên';
+$string['KR'] = 'Hàn Quốc';
+$string['MM'] = 'Miến Điện';
+$string['MN'] = 'Mông cổ';
+$string['MX'] = 'Mê Hi Cô';
+$string['NO'] = 'Na Uy';
+$string['PK'] = 'Phi líp pin';
+$string['SG'] = 'Sinh ga po';
+$string['US'] = 'Mỹ';
+$string['VN'] = 'Việt Nam';
 
 ?>
