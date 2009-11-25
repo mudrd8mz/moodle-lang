009493d7c0f01c30943612c9db7251b81823f458
koen roggemans <koenr>
2009-11-25 17:34:12 +0000

    weird error in langconfig

--- ro_utf8/langconfig.php
+++ ro_utf8/langconfig.php
@@ -7,12 +7,4 @@ $string['localewincharset'] = 'WINDOWS-1250';
 $string['oldcharset'] = 'ISO-8859-2';
 $string['thischarset'] = 'UTF-8';
 $string['thislanguage'] = 'Română';
-$string['decsep'] = ',';
-$string['listsep'] = ';';
-$string['locale'] = 'ro_RO.UTF-8';
-$string['localewin'] = 'Romanian_Romania.1250';
-$string['localewincharset'] = 'WINDOWS-1250';
-$string['oldcharset'] = 'ISO-8859-2';
-$string['thischarset'] = 'UTF-8';
-$string['thislanguage'] = 'Română';
 ?>
