6eaa5e9245d34e92dae620d6965757dd65aea37d
Abel Camacho <ueu_ueu>
2009-11-25 08:30:11 +0000

    New strings added

--- eu_utf8/imscp.php
+++ eu_utf8/imscp.php
@@ -2,6 +2,16 @@
       // imscp.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
+$string['areabackup'] = 'Pakete-biltegia';
+$string['areacontent'] = 'Edukia';
+$string['contentheader'] = 'Edukia';
+$string['deploymenterror'] = 'Errorea eduki-paketea egitean!';
+$string['keepold'] = 'Paketeak gorde';
+$string['keepoldexplain'] = 'Zenbat pakete gorde  behar dira?';
+$string['modulename'] = 'IMS eduki-paketea';
+$string['modulenameplural'] = 'IMS eduki-paketeak';
 $string['navigation'] = 'Nabigazioa';
+$string['packagefile'] = 'Paketearen fitxategia';
+$string['toc'] = 'TOC';
 
 ?>
