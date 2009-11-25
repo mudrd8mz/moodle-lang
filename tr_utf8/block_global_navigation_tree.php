3147a55f21e88ae8623f1463ea912871f50d840b
Ethem Evlice <ethem>
2009-11-25 11:13:01 +0000

    block_global_navigation_tree.php

---
+++ tr_utf8/block_global_navigation_tree.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // block_global_navigation_tree.php - created with Moodle 2.0 dev (Build: 20091124) (2009112400)
+
+
+$string['blockname'] = 'Gezinme';
+$string['courseactivities'] = 'Kategoriler, dersler ve ders etkinlikleri';
+$string['courses'] = 'Kategoriler ve dersler';
+$string['coursestructures'] = 'Kategoriler, dersler, ve ders yapıları';
+$string['enablehoverexpansiondesc'] = 'Fare ile üzerine gelindiğinde bloğun genişlemesine izin ver';
+$string['enablesidebarpopoutdesc'] = 'Kullanıcıların blok ve kenar sekmesi arasında geçiş yapmasına izin ver';
+$string['everything'] = 'Hepsi';
+$string['expansionlimit'] = 'Aşağıdakiler için gezinme oluştur';
+$string['showemptybranchesdesc'] = 'Boş ders bölümü dallarını göster';
+$string['showmycoursesdesc'] = 'Derslerimi gezinmede göster';
+$string['showmyhistorydesc'] = 'Geçmişi gezinmede dal olarak göster';
+$string['showmyhistorytitle'] = 'Geçmiş';
+$string['toggleblockdisplay'] = 'Blok konumuna taşı';
+$string['togglesidetabdisplay'] = 'Kenar sekmesine taşı';
+
+?>
