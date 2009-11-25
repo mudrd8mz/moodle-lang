bd59bf9f6db65e7d79418fe3caa3d11dca1e41b7
Abel Camacho <ueu_ueu>
2009-11-25 10:45:00 +0000

    New strings added

--- eu_utf8/repository_flickr_public.php
+++ eu_utf8/repository_flickr_public.php
@@ -1,14 +1,23 @@
 <?PHP // $Id$ 
-      // repository_flickr_public.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+      // repository_flickr_public.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
+$string['all'] = 'Guztia';
 $string['apikey'] = 'API giltza';
+$string['by'] = 'Eskubide lizentzia';
+$string['by-nc'] = 'CC NonCommercial eskubidea';
+$string['by-nc-nd'] = 'CC NonCommercial-NoDerivs eskubidea';
+$string['by-nc-sa'] = 'CC ShareAlike eskubidea';
+$string['by-nd'] = 'CC NoDerivs eskubidea';
+$string['by-sa'] = 'CC ShareAlike eskubidea';
 $string['callbackurl'] = 'Atzera deiaren URLa';
 $string['configplugin'] = 'Flickr Publikoaren ezarpenak';
+$string['creativecommonscommercial'] = 'creative commons commercial baino ez';
 $string['emailaddress'] = 'E-posta helbiea';
 $string['fulltext'] = 'Testu osoa';
 $string['information'] = '<div>1. Lortu <a href=\"http://www.flickr.com/services/api/keys/\">Flickr API Giltza</a> zure Moodle gunerako.<br/>';
 $string['invalidemail'] = 'E-posta helbideak ez du balio flickr-erako';
+$string['license'] = 'Lizentzia';
 $string['notitle'] = 'Izenbururik gabe';
 $string['nullphotolist'] = 'Ez dago argazkirik kontu honetan';
 $string['remember'] = 'Gogoratu';
