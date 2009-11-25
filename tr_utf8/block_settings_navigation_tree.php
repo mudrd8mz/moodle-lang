ddd2263cd1448f7209c247ac9b578e54ec152401
Ethem Evlice <ethem>
2009-11-25 11:26:19 +0000

    block_settings_navigation_tree.php

---
+++ tr_utf8/block_settings_navigation_tree.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // block_settings_navigation_tree.php - created with Moodle 2.0 dev (Build: 20091125) (2009112400)
+
+
+$string['blockname'] = 'Ayarlar';
+$string['enablehoverexpansion'] = 'Fare ile üzerine gelindiğinde bloğun genişlemesine izin ver';
+$string['enablesidebarpopout'] = 'Kullanıcıların blok ve kenar sekmesi arasında geçiş yapmasına izin ver';
+$string['toggleblockdisplay'] = 'Blok konumuna taşı';
+$string['togglesidetabdisplay'] = 'Kenar sekmesine taşı';
+
+?>
