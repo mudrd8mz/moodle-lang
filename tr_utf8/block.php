4268f81b41465a558ce74633842b36be1d6b2ce5
Ethem Evlice <ethem>
2009-11-25 11:41:43 +0000

    block.php

---
+++ tr_utf8/block.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // block.php - created with Moodle 2.0 dev (Build: 20091125) (2009112400)
+
+
+$string['anypagematchingtheabove'] = 'Yukarıdaki ile eşleşen herhangi sayfa';
+$string['appearsinsubcontexts'] = 'Alt-bağlamlarda görünür';
+$string['blocksettings'] = 'Blok ayarları';
+$string['bracketfirst'] = '$a (ilk)';
+$string['bracketlast'] = '$a (son)';
+$string['defaultregion'] = 'Varsayılan bölge';
+$string['defaultweight'] = 'Varsayılan ağırlık';
+$string['moveblockhere'] = 'Bloğu buraya taşı';
+$string['movingthisblockcancel'] = 'Bu blok taşınıyor ($a)';
+$string['onthispage'] = 'Bu sayfada';
+$string['pagetypes'] = 'Sayfa tipleri';
+$string['region'] = 'Bölge';
+$string['subpages'] = 'Özel alt-sayfa';
+$string['thisblockbelongsto'] = 'Bu blok buna aittir:';
+$string['thisspecificpage'] = 'Bu özel sayfa (sayfa $a)';
+$string['visible'] = 'Görünür';
+$string['weight'] = 'Ağırlık';
+$string['wherethisblockappears'] = 'Bu blok nerede görünecek';
+
+?>
