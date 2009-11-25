d3821c1f106f3179251ebef15e97e0038dd7116f
Abel Camacho <ueu_ueu>
2009-11-25 08:21:53 +0000

    New strings added

--- eu_utf8/imscc.php
+++ eu_utf8/imscc.php
@@ -2,5 +2,11 @@
       // imscc.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
+$string['cc2moodle_invalid_schema'] = 'Eskemak ez du balio!';
+$string['cc2moodle_manifest_dont_load'] = 'Ezin da XML manifestua kargatu.';
+$string['cc2moodle_valid_schema'] = 'Eskemak balio du!';
+$string['cc_import_req_php5'] = 'ERROREA: Common Cartridge import-ek PHP 5 edo altuagoa behar du.';
+$string['checkingforimscc'] = 'IMS-CC egiaztatzen...';
+$string['enable_cc_import'] = 'Gaitu CC Import';
 
 ?>
