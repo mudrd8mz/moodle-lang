ec3a1027523d5d7ddbcb5c6ebbb4e00ab665def1
Abel Camacho <ueu_ueu>
2009-11-25 08:28:47 +0000

    New string added

--- eu_utf8/webservice_amf.php
+++ eu_utf8/webservice_amf.php
@@ -2,6 +2,7 @@
       // webservice_amf.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
+$string['amfdebug'] = 'AMF zerbitzaria arazteko modua';
 $string['pluginname'] = 'AMF protokoloa';
 
 ?>
