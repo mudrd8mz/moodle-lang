79b1da40f020a3fd11f3a719044bc2e4b8225c0a
koen roggemans <koenr>
2009-11-25 17:41:32 +0000

    MDL-14152: it_utf8/portfolio.php: ERROR - Malformed nocommonformats mixed  and ->, allowed options are:

--- it_utf8/portfolio.php
+++ it_utf8/portfolio.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // portfolio.php - created with Moodle 2.0 dev (Build: 20091117) (2009111800)
+      // portfolio.php - created with Moodle 2.0 dev (Build: 20091021) (2009100605)
 
 
 $string['activeexport'] = 'Decisioni sulle esportazioni già in corso';
@@ -10,7 +10,6 @@ $string['addtoportfolio'] = 'Esporta...';
 $string['alreadyalt'] = 'Esportazione già attiva - per favore fate click qui per decidere cosa fare';
 $string['alreadyexporting'] = 'E\' già attiva una tua esportazione verso un portfolio. Prima di proseguire dovresti completare l\'esportazione attiva oppure annullarla. Vuoi proseguire con l\'esportazione attiva? (no equivale ad annullarla)';
 $string['availableformats'] = 'Formati di esportazione disponibili';
-$string['callbackclassinvalid'] = 'La callback class specificata non è valida oppure non fa parte della gerarchia del portfolio_caller';
 $string['callercouldnotpackage'] = 'Non è stato possibile impacchettare i dati per l\'esportazione: l\'errore è $a';
 $string['cannotsetvisible'] = 'Non è possibile rendere il plugin attivo - il plugin è stato disabilitato poiché non configurato correttamente';
 $string['commonsettingsdesc'] = '<p>E\' possibile stabilire se un trasferimento sia da considerarsi \"Lungo\" o \"Breve\" in base a quanto tempo l\'utente può aspettare prima che il trasferimento si concluda.</p>
@@ -19,7 +18,6 @@ $string['commonsettingsdesc'] = '<p>E\' possibile stabilire se un trasferimento 
 $string['configexport'] = 'Configura i dati esportati';
 $string['configplugin'] = 'Configura portfolio plugin';
 $string['configure'] = 'Configura';
-$string['confirmcancel'] = 'Sei sicuro di annullare questa l\'esportazione?';
 $string['confirmexport'] = 'Per favore confermate questa esportazione';
 $string['confirmsummary'] = 'Elenco delle tue esportazioni';
 $string['continuetoportfolio'] = 'Prosegui verso il tuo portfolio';
@@ -33,8 +31,6 @@ $string['dontwait'] = 'Non aspettare';
 $string['enabled'] = 'Abilita portfolio';
 $string['enableddesc'] = 'Gli amministratori saranno abilitati a configurare sistemi remoti di ePortfolio dove gli utenti potranno esportare contenuti';
 $string['err_uniquename'] = 'Il nome del Portfolio deve essere univoco (per ciascun plugin)';
-$string['exportalreadyfinished'] = 'Esportazione portfolio completata.';
-$string['exportalreadyfinisheddesc'] = 'Esportazione portfolio completata.';
 $string['exportcomplete'] = 'Esportazione del portfolio completata!';
 $string['exportedpreviously'] = 'Esportazioni già effettuate';
 $string['exportexceptionnoexporter'] = 'E\' stato lanciato un portfolio_export_exception con una sessione attiva ma senza exporter object';
@@ -98,22 +94,18 @@ $string['logs'] = 'Log dei trasferimenti';
 $string['logsummary'] = 'Trasferimenti completati';
 $string['manageportfolios'] = 'Gestione Portfolio';
 $string['manageyourportfolios'] = 'Gestisci i tuoi portfolio';
-$string['mimecheckfail'] = 'Il portfolio plugin $a->plugin non supporta il mimetype $a->mimetype';
 $string['missingcallbackarg'] = 'Manca l\'argomento di callback $a->arg  per la classe $a->class';
 $string['moderatedbsizethreshold'] = 'Trasferimento db breve';
 $string['moderatedbsizethresholddesc'] = 'Numero di record di database sotto il quale il trasferimento sarà considerato breve';
 $string['moderatefilesizethreshold'] = 'Trasferimento file breve';
 $string['moderatefilesizethresholddesc'] = 'Dimensione dei file sotto la quale il trasferimento sarà considerato breve';
-$string['multipleinstancesdisallowed'] = 'Si sta tentando di creare una ulteriore istanza di una plugin non abilitata ad avere istanze multiple ($a)';
+$string['multipledisallowed'] = 'E\' stata tentata la creazione di un ulteriore istanza di un plugin per il quale non sono consentite istanze multiple ($a)';
 $string['mustsetcallbackoptions'] = 'Dovete impostare l\'opzione di callback nel costructor portfolio_add_button oppure utilizzando il metodo set_callback_options';
 $string['noavailableplugins'] = 'Spiacente, non ci sono portfolio disponibili per l\'esportazione';
 $string['nocallbackclass'] = 'Non è stato possibile trovare la classe di callback da utilizzare ($a)';
 $string['nocallbackfile'] = 'Qualcosa non funziona all\'interno del modulo dal quale state tentando di esportare - non è stato possibile trovare un file necessario ($a)';
 $string['noclassbeforeformats'] = 'Dovete impostare la classe di callback prima di chiamare  set_formats in portfolio_button';
-$string['nocommonformats'] = 'Non ci sono formati comuni tra i portfolio plugin disponibili e la location di chiamata $a (il chiamante supporta $a->formats)';
-$string['noinstanceyet'] = 'Non ancora selezionato';
-$string['nologs'] = 'Non ci sono log da visualizzare';
-$string['nomultipleexports'] = 'Spiacente, il portfolio di destinazione ($a->plugin) non supporta esportazioni multiple contemporanee. Per favore <a href=\"$a->link\">completate l\'esportazione attiva</a> e riprovate.';
+$string['nocommonformats'] = 'Non ci sono formati comuni tra i portfolio plugin disponibili e la location di chiamata $a (il chiamante supporta $a)';
 $string['nonprimative'] = 'E\' stato passato un valore non primitivo come argomento di callback a portfolio_add_button. Non è possibile proseguire. La chiave era $a->key ed il valore $a->value';
 $string['nopermissions'] = 'Spiacente, non siete autorizzati ad esportare file da questa area';
 $string['notexportable'] = 'Spiacente, non è possibile esportare il tipo di contenuto che state tentando di esportare';
@@ -132,7 +124,6 @@ $string['save'] = 'Salva';
 $string['selectedformat'] = 'Formati di esportazione selezionati';
 $string['selectedwait'] = 'Impostato per l\'attesa?';
 $string['selectplugin'] = 'Scegli dove esportare';
-$string['singleinstancenomultiallowed'] = 'E\' disponibile solo una singola istanza di portfolio plugin, non supporta esportazioni multiple nella stessa sessione ed è già attiva una esportazione che fa uso di questo plugin!';
 $string['someinstancesdisabled'] = 'Alcune istanze di portfolio plugin sono state disabilitate in quanto non configurate correttamente oppure perché dipendono dal altri elementi, in particolare:';
 $string['somepluginsdisabled'] = 'Alcuni portfolio plugin sono stati disabilitati in quanto non configurati correttamente oppure perché dipendono dal altri elementi, in particolare:';
 $string['sure'] = 'Sei sicuro di voler eliminare \'$a\'? Non puoi tornare indietro.';
@@ -142,6 +133,15 @@ $string['unknownplugin'] = 'Sconosciuto (probabilmente disinstallata da un ammin
 $string['wait'] = 'Attendi';
 $string['wanttowait_high'] = 'Non si consiglia di attendere il completamento di questo trasferimento, tuttavia se preferite attendere e siete consci della richiesta, potete farlo.';
 $string['wanttowait_moderate'] = 'Desiderate attendere il completamento del trasferimento? Il trasferimento potrebbe richiedere alcuni minuti.';
-$string['multipledisallowed'] = 'E\' stata tentata la creazione di un ulteriore istanza di un plugin per il quale non sono consentite istanze multiple ($a)'; // ORPHANED
+$string['callbackclassinvalid'] = 'La callback class specificata non è valida oppure non fa parte della gerarchia del portfolio_caller'; // ORPHANED
+$string['confirmcancel'] = 'Sei sicuro di annullare questa l\'esportazione?'; // ORPHANED
+$string['exportalreadyfinished'] = 'Esportazione portfolio completata.'; // ORPHANED
+$string['exportalreadyfinisheddesc'] = 'Esportazione portfolio completata.'; // ORPHANED
+$string['mimecheckfail'] = 'Il portfolio plugin $a->plugin non supporta il mimetype $a->mimetype'; // ORPHANED
+$string['multipleinstancesdisallowed'] = 'Si sta tentando di creare una ulteriore istanza di una plugin non abilitata ad avere istanze multiple ($a)'; // ORPHANED
+$string['noinstanceyet'] = 'Non ancora selezionato'; // ORPHANED
+$string['nologs'] = 'Non ci sono log da visualizzare'; // ORPHANED
+$string['nomultipleexports'] = 'Spiacente, il portfolio di destinazione ($a->plugin) non supporta esportazioni multiple contemporanee. Per favore <a href=\"$a->link\">completate l\'esportazione attiva</a> e riprovate.'; // ORPHANED
+$string['singleinstancenomultiallowed'] = 'E\' disponibile solo una singola istanza di portfolio plugin, non supporta esportazioni multiple nella stessa sessione ed è già attiva una esportazione che fa uso di questo plugin!'; // ORPHANED
 
 ?>
