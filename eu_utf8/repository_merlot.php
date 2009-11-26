59a7f91ee3c9a3e74ffcd9d209a290c629f327a5
Abel Camacho <ueu_ueu>
2009-11-26 08:02:00 +0000

    New file added

---
+++ eu_utf8/repository_merlot.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // repository_merlot.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['configplugin'] = 'Merlot.org-ren ezarpenak';
+$string['licensekey'] = 'Lizentzia giltza';
+$string['repositorydesc'] = 'Merlot.org';
+$string['repositoryname'] = 'Merlot.org';
+
+?>
