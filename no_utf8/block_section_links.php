f9c1d431a32e61803cf71162445b9ebf2f334837
efaktorcvs <efaktorcvs>
2009-12-09 21:30:53 +0000

    Small update by eFaktor as

--- no_utf8/block_section_links.php
+++ no_utf8/block_section_links.php
@@ -1,10 +1,18 @@
 <?PHP // $Id$ 
-      // block_section_links.php - created with Moodle 1.8 + (2007021503)
+      // block_section_links.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
 
 
 $string['blockname'] = 'Seksjonslenker';
-$string['jumptocurrenttopic'] = 'Hopp til gjeldende emne';
-$string['jumptocurrentweek'] = 'Hopp til gjeldende uke';
+$string['incby1'] = 'Øk med';
+$string['incby2'] = 'Alternativt øk med';
+$string['incbydesc1'] = 'Dette er verdien det økes med fra 1 hver gang en seksjonslenke vises.';
+$string['incbydesc2'] = 'Dette er verdien det økes med fra 1 hver gang en seksjonslenke vises.';
+$string['jumptocurrenttopic'] = 'Gå til gjeldende emne';
+$string['jumptocurrentweek'] = 'Gå til gjeldende uke';
+$string['numsections1'] = 'Antall seksjoner';
+$string['numsections2'] = 'Alternativt antall seksjoner';
+$string['numsectionsdesc1'] = 'Straks antallet seksjoner i kurset når angitt antall vil \"Øk med\"-verdien bli benyttet.';
+$string['numsectionsdesc2'] = 'Straks antallet seksjoner i kurset når angitt antall vil \"Alternativt øk med\"-verdien bli benyttet.';
 $string['topics'] = 'Emner';
 $string['weeks'] = 'Uker';
 
