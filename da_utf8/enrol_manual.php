0f0c6afd4d711ad22cbc01e08edc28abe04a7757
Bente Olsen <benteo>
2009-12-10 21:11:19 +0000

    Adds & modifications

--- da_utf8/enrol_manual.php
+++ da_utf8/enrol_manual.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_manual.php - created with Moodle 1.9.3+ (Build: 20081025) (2007101530)
+      // enrol_manual.php - created with Moodle 1.9.7+ (Build: 20091130) (2007101570)
 
 
 $string['description'] = 'Denne metode standard for tilmeldinger. Der er to hovedmetoder en elev kan tilmeldes et kursus på.
@@ -7,11 +7,13 @@ $string['description'] = 'Denne metode standard for tilmeldinger. Der er to hove
 <li>
 En lærer eller administrator kan tilmelde dem manuelt via linket i administrationsblokken på kurset.
 </li>
-<li>Et kursus kan have en kode, en såkaldt \"kursusnøgle\". Alle der kender denne køgle kan tilmelde sig  kurset.</li>
+<li>Et kursus kan have en kode, en såkaldt \"tilmeldingsnøgle\". Alle der kender denne køgle kan tilmelde sig  kurset.</li>
 </ul>';
-$string['enrol_manual_showhint'] = 'Aktivering af denne indstilling afslører det første tegn i kursusnøglen som et hint hvis nogen skriver en forkert nøgle.';
-$string['enrolmentkeyerror'] = 'Kursusnøglen var ikke rigtig, prøv igen!';
+$string['enrol_manual_requirekey'] = 'Kræv tilmeldingsnøgler for nye kurser og undgå at eksisterende nøgler fjernes.';
+$string['enrol_manual_showhint'] = 'Aktivering af denne indstilling afslører det første tegn i tilmeldingsnøglen som et hint hvis nogen skriver en forkert nøgle.';
+$string['enrol_manual_usepasswordpolicy'] = 'Brug samme politik for tilmeldingsnøgler til kurser som til brugernes adgangskoder';
+$string['enrolmentkeyerror'] = 'Tilmeldingsnøglen var ikke rigtig, prøv igen!';
 $string['enrolname'] = 'Intern tilmelding';
-$string['keyholderrole'] = 'Rollen på den bruger der har kursusnøglen. Vedkommendes navn vises til elever der forsøger at tilmelde sig kurset.';
+$string['keyholderrole'] = 'Rollen på den bruger der har tilmeldingsnøglen. Vedkommendes navn vises til elever der forsøger at tilmelde sig kurset.';
 
 ?>
