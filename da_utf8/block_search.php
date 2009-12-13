cd2d18c09f7c6a9090db1f59574294cdd82a3624
Bente Olsen <benteo>
2009-12-13 16:17:37 +0000

    Added strings

--- da_utf8/block_search.php
+++ da_utf8/block_search.php
@@ -1,14 +1,23 @@
 <?PHP // $Id$ 
-      // block_search.php - created with Moodle 1.9.3+ (Build: 20081025) (2007101530)
+      // block_search.php - created with Moodle 1.9.7+ (Build: 20091130) (2007101570)
 
 
+$string['asynchronous'] = 'Asynkront (Ajax)';
 $string['blockname'] = 'Global søgning';
 $string['bytes'] = 'byte (0 står for ingen begrænsning)';
 $string['configbuttonlabel'] = 'Knapetiket';
 $string['configenablefileindexing'] = 'Aktiver filindexering';
 $string['configpdftotextcmd'] = 'Sti til kommandoen pdftotext';
 $string['configsearchtext'] = 'Søg tekst';
+$string['configtypetotxtcmd'] = 'Konverterens kommandolinje';
 $string['configwordtotextcmd'] = 'Sti til kommandoen doctotext';
+$string['disabled'] = 'Deaktiveret';
+$string['disabledsearch'] = 'Den globale søgemaskine er slukket. Underret administrator.';
+$string['enabled'] = 'Aktiveret';
+$string['nosearchableblocks'] = 'Ingen søgbare blokke';
+$string['nosearchablemodules'] = 'Ingen søgbare moduler';
+$string['search'] = 'Søg';
 $string['searchmoodle'] = 'Søg i Moodle';
+$string['synchronous'] = 'Synkront';
 
 ?>
