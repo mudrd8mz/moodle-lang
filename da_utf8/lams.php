61468e3783e8a9835654cf6fdcbbe506e0ce5c4a
Bente Olsen <benteo>
2010-01-02 15:43:14 +0000

    New strings

--- da_utf8/lams.php
+++ da_utf8/lams.php
@@ -1,7 +1,20 @@
 <?PHP // $Id$ 
-      // lams.php - created with Moodle 1.8 dev (2006102600)
+      // lams.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
 
 
+$string['error'] = 'Beklager, en ukendt fejl er opstået';
 $string['introduction'] = 'Introduktion';
+$string['lams'] = '-- LAMS - Learning Activity Management System --';
+$string['lams:manage'] = 'Administrer LAMS-aktiviteter';
+$string['lams:participate'] = 'Deltag i LAMS-aktiviteter';
+$string['modulename'] = 'LAMS';
+$string['modulenameplural'] = 'LAMS';
+$string['notsetup'] = 'NOT_SET_UP';
+$string['server_id'] = 'Server-ID';
+$string['server_key'] = 'Servernøgle';
+$string['server_url'] = 'Server-URL';
+$string['serverid'] = 'Tast server-ID\'en fra <a href=\"http://www.lamsinternational.com\">LAMS international</a>.';
+$string['serverkey'] = 'Tast server-nøglen fra <a href=\"http://www.lamsinternational.com\">LAMS international</a>.';
+$string['serverurl'] = 'Tast basis-URL\'en der bruges til LAMS-serveren. F.eks. http://localhost:8080/lams';
 
 ?>
