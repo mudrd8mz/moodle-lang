61e6517a1a71e275c87b88fe4b1bf8748a18f5c9
Bente Olsen <benteo>
2010-01-02 13:37:31 +0000

    Adds & corrections

--- da_utf8/enrol_database.php
+++ da_utf8/enrol_database.php
@@ -1,34 +1,50 @@
 <?PHP // $Id$ 
-      // enrol_database.php - created with Moodle 1.7+ (2006101007)
+      // enrol_database.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
 
 
-$string['autocreate'] = 'Kurser oprettes automatisk hvis der er tilmeldinger til et kursus er ikke allerede eksisterer i Moodle.';
-$string['autocreation_settings'] = 'Autooprettelses indstillinger';
-$string['category'] = 'Kategorien for automatisk oprettede kurser.';
-$string['course_fullname'] = 'Navnet på feltet hvor kursets fulde navn kan findes.';
-$string['course_id'] = 'Navnet på feltet hvor kursets ID er gemt. Værdien i dette felt bliver brugt til at sammenligne feltet \"enrol_db_l_coursefield\" i moodle\'s kursustabel';
-$string['course_shortname'] = 'Navnet på feltet hvor kursets korte navn kan findes.';
+$string['autocreate'] = 'Kurser oprettes automatisk hvis der er tilmeldinger til et kursus der ikke allerede eksisterer i Moodle.';
+$string['autocreation_settings'] = 'Indstillinger på automatisk oprettede kurser';
+$string['category'] = 'Kategorien til automatisk oprettede kurser.';
+$string['course_fullname'] = 'Navnet på feltet med kursets fulde navn.';
+$string['course_id'] = 'Navnet på feltet hvor kursets ID er gemt. Værdien i dette felt bliver brugt til at sammenligne feltet \"enrol_db_l_coursefield\" i Moodles kursustabel';
+$string['course_shortname'] = 'Navnet på feltet med kursets korte navn.';
 $string['course_table'] = 'Navnet på tabellen hvor det kan forventes at finde kursusdetaljer som \"kort navn, fulde navn, ID\" osv.';
-$string['description'] = 'Du kan bruge en ekstern database (af næsten enhver slags) til at kontrollere dine tilmeldinger til kurser. Det forudsættes at den eksterne database indeholder et felt med et kursusID og et felt der indeholder et brugerID. Disse bliver så sammenlignet med de felter du vælger i det lokale kursus og brugertabel.';
+$string['dbhost'] = 'Server-IP, navn eller nummer';
+$string['dbname'] = 'Databasenavn';
+$string['dbpass'] = 'Serveradgangskode';
+$string['dbtable'] = 'Databasetabel';
+$string['dbtype'] = 'Databasetype';
+$string['dbuser'] = 'Serverbruger';
+$string['defaultcourseroleid'] = 'Den rolle, der tildeles automatisk, hvis ingen anden rolle er specificeret.';
+$string['description'] = 'Du kan bruge en ekstern database (af næsten enhver slags) til at kontrollere dine tilmeldinger til kurser. Det forudsættes at den eksterne database indeholder et felt med et kursus-ID og et felt der indeholder et bruger-ID. Disse bliver sammenlignet med de felter du vælger lokalt i kursus- og brugertabel.';
+$string['disableunenrol'] = 'Hvis du vælger ja her vil brugere, der tidligere er tilmeldt via det eksterne database-plugin, ikke frameldes af samme plugin uanset databasens indhold.';
+$string['enrol_database_autocreation_settings'] = 'Automatisk oprettelse af nye kurser';
 $string['enrolname'] = 'Ekstern database';
 $string['general_options'] = 'Generelle indstillinger';
 $string['host'] = 'Databasens værtsnavn';
-$string['local_coursefield'] = 'Navnet på feltet i kursustabellen der kan bruges til at sammenkæde data med den eksterne database. (eksempelvis idnummer)';
-$string['local_fields_mapping'] = 'Moodle (lokalt) database felt';
+$string['ignorehiddencourse'] = 'Hvis du vælger ja her vil brugere ikke blive tilmeldt kurser, der ikke er tilgængelige for studerende.';
+$string['local_fields_mapping'] = 'Moodles (lokale) databasefelter';
+$string['localcoursefield'] = 'Navnet på feltet i kursustabellen som skal matche med den eksterne database (f.eks. ID-nummer)';
+$string['localrolefield'] = 'Navnet på feltet i rolletabellen der bruges til at matche med den eksterne databases felt (f.eks. kortnavn)';
+$string['localuserfield'] = 'Navnet på feltet i brugertabellen der bruges til at matche med den eksterne databases felt (f.eks. ID-nummer)';
 $string['name'] = 'Databasen der skal bruges.';
-$string['pass'] = 'Password til serveren.';
-$string['remote_fields_mapping'] = 'Tilmelding (ekstern) database felter.';
-$string['server_settings'] = 'Ekstern databaseserver indstillinger';
-$string['student_coursefield'] = 'Navnet på det felt i elevtilmeldingstabellen som der forventes at indeholde kursusID feltet.';
-$string['student_l_userfield'] = 'Navnet på det felt i den lokale brugertabel der bruges til at sammenkæde en bruger til den eksterne tabel med elever (f.eks. idnummer)';
-$string['student_r_userfield'] = 'Navnet på feltet i den eksterne elevtilmeldings tabel hvor elevens id kan findes.';
+$string['pass'] = 'Adgangskode til serveren.';
+$string['remote_fields_mapping'] = 'Databasefelter (eksternt) til tilmeldinger.';
+$string['remotecoursefield'] = 'Navnet på feltet i den eksterne tabel der bruges til at matche poster i kursustabellen';
+$string['remoterolefield'] = 'Navnet på feltet i den eksterne tabel der bruges til at matche poster i rolletabellen.';
+$string['remoteuserfield'] = 'Navnet på feltet i den eksterne tabel der bruges til at matche poster i brugertabellen.';
+$string['server_settings'] = 'Ekstern databaseserverindstillinger';
+$string['student_coursefield'] = 'Navnet på det felt i elevtilmeldingstabellen som forventes at indeholde feltet med kursus-ID.';
+$string['student_l_userfield'] = 'Navnet på feltet i den lokale brugertabel der bruges til at matche en bruger med en ekstern tabel med studerende (f.eks. ID-nummer)';
+$string['student_r_userfield'] = 'Navnet på feltet i den eksterne elevtilmeldingstabel som forventes at indeholde feltet med bruger-ID.';
 $string['student_table'] = 'Navnet på tabellen hvor elevtilmeldingerne er gemt.';
-$string['teacher_coursefield'] = 'Navnet på feltet i undervisertilmeldingstabellen hvor det kan forventes at finde kursets IDnummer i.';
-$string['teacher_l_userfield'] = 'Navnet på feltet i den lokale brugertabel der kan bruges til at sammenkæde brugeren til en ekstern tabel med undervisere (f.eks. IDnummer)';
-$string['teacher_r_userfield'] = 'Navnet på feltet i den eksterne undervisertilmeldingstabel hvor brugerens ID kan findes.';
-$string['teacher_table'] = 'Navnet på tabellen hvor underviserens tilmeldinger er gemt.';
-$string['template'] = 'Valgfrit: auto-oprettede kurser kan kopiere deres indstillinger fra et skabelons/meta kursus. Hvis det er tilfældet kan du skrive det korte navn på det meta / skabelons kursus der der kopieres fra.';
-$string['type'] = 'Databaseserver type.';
-$string['user'] = 'Brugernavn til at tilgå serveren.';
+$string['teacher_coursefield'] = 'Navnet på feltet i lærertilmeldingstabellen som forventes at indeholde kursets ID-nummer.';
+$string['teacher_l_userfield'] = 'Navnet på feltet i den lokale brugertabel der bruges til at matche en bruger med en ekstern tabel med lærere (f.eks. ID-nummer)';
+$string['teacher_r_userfield'] = 'Navnet på feltet i den eksterne lærertilmeldingstabel som forventes at indeholde feltet med bruger-ID.';
+$string['teacher_table'] = 'Navnet på tabellen hvor lærertilmeldinger er gemt.';
+$string['template'] = 'Valgfrit: Automatisk oprettede kurser kan kopiere deres indstillinger fra en kursusskabelon. Hvis det er tilfældet kan du skrive det korte navn på det skabelonkursus der kopieres fra.';
+$string['type'] = 'Databaseservertype.';
+$string['user'] = 'Brugernavn til serveren.';
+$string['local_coursefield'] = 'Navnet på feltet i kursustabellen der kan bruges til at sammenkæde data med den eksterne database. (eksempelvis idnummer)'; // ORPHANED
 
 ?>
