c12b933c320c1977bf54f99c455ac10b1ed2bf03
Bente Olsen <benteo>
2010-01-02 15:54:20 +0000

    New strings

---
+++ da_utf8/qtype_datasetdependent.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // qtype_datasetdependent.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
+
+
+$string['atleastonerealdataset'] = 'Der skal være mindst et rigtigt datasæt i spørgsmålsteksten';
+$string['atleastonewildcard'] = 'Der skal være mindst et jokertegn i svarformularen eller spørgsmålsteksten';
+$string['calcdistribution'] = 'Distribution';
+$string['calclength'] = 'Decimaler';
+$string['calcmax'] = 'Maximum';
+$string['calcmin'] = 'Minimum';
+$string['datasetrole'] = 'Jokertegnene <strong>{x..}</strong> vil blive erstattet med en numerisk værdi fra deres datasæt';
+$string['existingcategory2'] = 'en fil fra et allerede eksisterende sæt filer der også bruges af andre spørgsmål i denne kategori';
+$string['existingcategory3'] = 'et link fra et allerede eksisterende sæt links der også bruges af andre spørgsmål i denne kategori';
+$string['minmax'] = 'Værdiinterval';
+$string['newcategory2'] = 'en fil fra et nyt filsæt der måske også bruges af andre spørgsmål i denne kategori';
+$string['newcategory3'] = 'et link fra et nyt sæt links der måske også bruges af andre spørgsmål i denne kategori';
+$string['newlocal2'] = 'en fil fra et nyt filsæt der kun vil blive brugt i dette spørgsmål';
+$string['newlocal3'] = 'et link fra et nyt sæt links der kun vil blive brugt i dette spørgsmål';
+$string['nodataset'] = 'ingenting - det er ikke et jokertegn';
+$string['param'] = 'Param {<strong>$a</strong>}';
+$string['replacewithrandom'] = 'Erstat med en tilfældig værdi';
+$string['reuseifpossible'] = 'genbrug forrige værdi hvis der er en tilgængelig';
+$string['sharedwildcard'] = 'Delt jokertegn';
+$string['sharedwildcards'] = 'Delte jokertegn';
+$string['updatedatasetparam'] = 'Opdater datasættets parametre';
+$string['youmustaddatleastoneitem'] = 'Du skal tilføje mindst en værdi fra datasættet før du kan gemme spørgsmålet';
+
+?>
---
+++ da_utf8/qtype_multianswer.php
@@ -0,0 +1,21 @@
+<?PHP // $Id$ 
+      // qtype_multianswer.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
+
+
+$string['addingmultianswer'] = 'Tilføjer quiz med indlejrede svar (Cloze)';
+$string['correctanswer'] = 'Korrekt svar';
+$string['correctanswerandfeedback'] = 'Korrekt svar og feedback';
+$string['decodeverifyquestiontext'] = 'Afkod og bekræft spørgsmålsteksten';
+$string['editingmultianswer'] = 'Rediger et Embedded svar (Cloze)';
+$string['layout'] = 'Layout';
+$string['layouthorizontal'] = 'Vandret række med radioknapper';
+$string['layoutselectinline'] = 'Dropdown-menu in-line i teksten';
+$string['layoutundefined'] = 'Udefineret layout';
+$string['layoutvertical'] = 'Lodret kolonne med radioknapper';
+$string['multianswer'] = 'Indlejrede svar (cloze)';
+$string['noquestions'] = 'Cloze-spørgsmålet \"<strong>$a</strong>\" indeholder ikke nogen spørgsmål';
+$string['qtypenotrecognized'] = 'spørgsmålstypen $a blev ikke genkendt';
+$string['questiondefinition'] = 'Spørgsmål';
+$string['questionsmissing'] = 'Ingen valide spørgsmål, opret mindst et spørgsmål';
+
+?>
