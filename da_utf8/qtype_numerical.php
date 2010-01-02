7fb5c79dde2c0d1318e5916adb28c91fc8cac3b8
Bente Olsen <benteo>
2010-01-02 16:01:31 +0000

    New strings

---
+++ da_utf8/qtype_numerical.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // qtype_numerical.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
+
+
+$string['addmoreanswerblanks'] = 'Tomme til {no} flere svar';
+$string['answermustbenumberorstar'] = 'Svaret skal være et tal eller \'*\'.';
+$string['answerno'] = 'Svar $a';
+$string['errornomultiplier'] = 'Du skal skrive en multiplikator til denne enhed';
+$string['errorrepeatedunit'] = 'Du kan ikke have to enheder med samme navn.';
+$string['notenoughanswers'] = 'Du skal lægge mindst et svar.';
+$string['unithdr'] = 'Enhed $a';
+
+?>
