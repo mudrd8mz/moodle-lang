ae49b4ee85332244997caad60e0dda4cc6a8c262
Bente Olsen <benteo>
2010-01-02 15:47:27 +0000

    New file

---
+++ da_utf8/qformat_xml.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // qformat_xml.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
+
+
+$string['invalidxml'] = 'Ugyldig xml-fil -  en streng var ventet (brug CDATA?)';
+$string['unsupportedexport'] = 'Spørgsmålstypen $a supporteres ikke ved xml-eksport';
+
+?>
