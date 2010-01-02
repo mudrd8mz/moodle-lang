bd506598a93915364887637f6cc8ce1171466633
Bente Olsen <benteo>
2010-01-02 15:02:06 +0000

    String added

--- da_utf8/block_online_users.php
+++ da_utf8/block_online_users.php
@@ -1,9 +1,10 @@
 <?PHP // $Id$ 
-      // block_online_users.php - created with Moodle 1.9 Beta 4 (2007101506)
+      // block_online_users.php - created with Moodle 1.9.7+ (Build: 20091209) (2007101570)
 
 
 $string['blockname'] = 'Brugere online';
 $string['configtimetosee'] = 'Antal minutter en bruger skal være inaktiv på systemet før brugeren betragtes som offline.';
+$string['online_users:viewlist'] = 'Se en liste over online brugere';
 $string['periodnminutes'] = 'sidste $a minutter';
 $string['timetosee'] = 'Fjern efter (minutters) inaktivitet';
 
