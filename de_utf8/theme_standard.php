ef1a03363e7f1dbff09528f884b2ef08baa6c074
krause <krause>
2010-01-20 23:43:24 +0000

    new strings

---
+++ de_utf8/theme_standard.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // theme_standard.php - created with Moodle 2.0 dev (Build: 20100114) (2010011400)
+
+
+$string['pluginname'] = 'Standard';
+$string['region-side-post'] = 'Rechts';
+$string['region-side-pre'] = 'Links';
+
+?>
