f5b1c2210e83e8a48869a95eeab2c61585d4e81f
Nicolas Martignoni <martignoni>
2010-01-21 21:16:54 +0000

    Sorted strings

--- fr_utf8/block_tag_youtube.php
+++ fr_utf8/block_tag_youtube.php
@@ -1,22 +1,22 @@
 <?php // $Id$
 
-$string['blockname'] = 'Youtube';
-$string['configtitle'] = 'Titre';
-$string['numberofvideos'] = 'Nombre de vidéos';
-$string['category'] = 'Catégorie';
 $string['anycategory'] = 'Toute catégorie';
-$string['includeonlyvideosfromplaylist'] = 'N\'inclure que les vidéos de la liste de lecture d\'identifiant';
-$string['filmsanimation'] = 'Films &amp; animations';
 $string['autosvehicles'] = 'Autos &amp; véhicules';
+$string['blockname'] = 'Youtube';
+$string['category'] = 'Catégorie';
 $string['comedy'] = 'Comédie';
+$string['configtitle'] = 'Titre';
 $string['entertainment'] = 'Entertainment';
+$string['filmsanimation'] = 'Films &amp; animations';
+$string['gadgetsgames'] = 'Gadgets &amp; jeux';
+$string['howtodiy'] = 'Instructions &amp; bricolage';
+$string['includeonlyvideosfromplaylist'] = 'N\'inclure que les vidéos de la liste de lecture d\'identifiant';
 $string['music'] = 'Musique';
 $string['newspolitics'] = 'Nouvelles &amp; politique';
+$string['numberofvideos'] = 'Nombre de vidéos';
 $string['peopleblogs'] = 'Personnes &amp; blogs';
 $string['petsanimals'] = 'Animaux domestiques &amp; autres animaux';
-$string['howtodiy'] = 'Instructions &amp; bricolage';
 $string['sports'] = 'Sports';
 $string['travel'] = 'Voyages &amp; Lieux';
-$string['gadgetsgames'] = 'Gadgets &amp; jeux';
 
 ?>
