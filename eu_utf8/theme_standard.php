3403f98100188acb0fd0f7ab1b290ce79f575266
Abel Camacho <ueu_ueu>
2010-01-22 10:25:52 +0000

    New string added

--- eu_utf8/theme_standard.php
+++ eu_utf8/theme_standard.php
@@ -1,8 +1,5 @@
-<?PHP // $Id$ 
-      // theme_standard.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+<?php
 
-
-$string['region-side-post'] = 'Eskuina';
-$string['region-side-pre'] = 'Ezkerra';
-
-?>
+$string['pluginname'] = "Standard";
+$string['region-side-pre'] = 'Left';
+$string['region-side-post'] = 'Right';
