2db66bcbefe8c5c937d0b5a1ec482826723867ce
Benito Arias <barias>
2010-02-08 19:52:09 +0000

    *** empty log message ***

--- es_utf8/filters.php
+++ es_utf8/filters.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // filters.php - created with Moodle 1.8.2+ (2007021520)
+      // filters.php - created with Moodle 1.9.3+ (Build: 20081126) (2007101532)
 
 
 $string['actfilterhdr'] = 'Filtros activos (ORed juntos)';
@@ -9,18 +9,27 @@ $string['anycourse'] = 'cualquier curso';
 $string['anyfield'] = 'cualquier campo';
 $string['anyrole'] = 'cualquier rol';
 $string['anyvalue'] = 'cualquier valor';
+$string['applyto'] = 'Aplicar a';
 $string['categoryrole'] = 'Regla de categoría';
 $string['contains'] = 'contiene';
+$string['content'] = 'Contenido';
+$string['contentandheadings'] = 'Contenido y cabeceras';
 $string['courserole'] = 'Rol de curso';
 $string['courserolelabel'] = '$a->label es $a->rolename en $a->coursename de $a->categoryname';
 $string['courserolelabelerror'] = 'error $a->label: el curso $a->coursename no existe';
 $string['datelabelisafter'] = '$a->label está después de $a->after';
 $string['datelabelisbefore'] = '$a->label está antes de $a->before';
 $string['datelabelisbetween'] = '$a->label está entre $a->after y $a->before';
+$string['defaultx'] = 'Por defecto ($a)';
+$string['disabled'] = 'Deshabilitado';
 $string['doesnotcontain'] = 'no contiene';
 $string['endswith'] = 'termina en';
+$string['filterallwarning'] = 'Aplicar filtros a las cabeceras y al contenido puede incrementar la sobrecarga del servidor. Por favor, use los ajustes \'Aplicar a\' con moderación. El uso principal es con el filtro multi-idioma.';
+$string['filtersettingsforin'] = 'Ajustes de filtro para $a->filter in $a->context';
+$string['filtersettingsin'] = 'Ajustes de filtro en $a';
 $string['firstaccess'] = 'Primer acceso';
 $string['globalrolelabel'] = '$a->label es $a->value';
+$string['isactive'] = '¿Activo?';
 $string['isafter'] = 'es después de $a';
 $string['isanyvalue'] = 'es cualquier valor';
 $string['isbefore'] = 'es antes de $a';
@@ -30,6 +39,10 @@ $string['isequalto'] = 'es igual a';
 $string['isnotdefined'] = 'no está definido';
 $string['isnotequalto'] = 'no es igual a';
 $string['newfilter'] = 'Nuevo filtro';
+$string['nofiltersenabled'] = 'No se han habilitado conectores de flitros en este sitio.';
+$string['off'] = 'Desconectado';
+$string['offbutavailable'] = 'Desconectado, pero disponible';
+$string['on'] = 'Conectado';
 $string['profilelabel'] = '$a->label: $a->profile $a->operator $a->value';
 $string['profilelabelnovalue'] = '$a->label: $a->profile $a->operator';
 $string['removeall'] = 'Eliminar todo';
