142304942c1453b3a07aa07305ea035435124d58
krause <krause>
2010-02-09 16:45:16 +0000

    new strings

---
+++ de_utf8/repository_draft.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // repository_draft.php - created with Moodle 2.0 dev (Build: 20100207) (2010020700)
+
+
+$string['draft:view'] = 'Entwurfsdateien in der Dateiauswahl sehen';
+$string['repositorydesc'] = 'Plugin zur Anzeige von Entwurfsdateien';
+$string['repositoryname'] = 'Aktuelle Dateien';
+
+?>
---
+++ de_utf8/repository_merlot.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // repository_merlot.php - created with Moodle 2.0 dev (Build: 20100207) (2010020700)
+
+
+$string['configplugin'] = 'Einstellungen für Merlot.org';
+$string['licensekey'] = 'Lizenzschlüssel';
+$string['repositorydesc'] = 'Merlot.org';
+$string['repositoryname'] = 'Merlot.org';
+
+?>
