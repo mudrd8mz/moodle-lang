bf3e9f6a9c7441e2699d330052e6094fb61f223c
Abel Camacho <ueu_ueu>
2010-02-10 08:51:02 +0000

    New string added

--- eu_utf8/auth_mnet.php
+++ eu_utf8/auth_mnet.php
@@ -1,7 +1,6 @@
-<?php
+<?PHP // $Id$ 
+      // auth_mnet.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
-// All of the language strings in this file should also exist in
-// auth.php to ensure compatibility in all versions of Moodle.
 
 $string['auth_mnet_auto_add_remote_users'] = 'Bai aukeratuta, berez sortzen da erabiltzaile lokalaren erregistroa urrutiko erabiltzailea lehen aldiz sartzen denean.';
 $string['auth_mnet_roamin'] = 'Ostalari honetako erabiltzaileak zure gunean ibil daitezke';
@@ -10,8 +9,11 @@ $string['auth_mnet_rpc_negotiation_timeout'] = 'XMLRCP-ren bidez autentifikatzek
 $string['auth_mnetdescription'] = 'Zure Moodle Sareko ezarpenetan definitutako web-konfidantzaren arabera autentifikatuko dira erabiltzaileak.';
 $string['auth_mnettitle'] = 'Moodle Sarea motako autentifikazioa';
 $string['auto_add_remote_users'] = 'Gehitu automatikoki urrutiko erabiltzaileak';
-$string['sso_idp_description'] = 'Publika ezazu zerbitzu hau zure erabiltzaileak zure $a Moodle gunera bertan berriz identifikatu gabe joan ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzuan (Zerbitzu-hornitzailea) <strong>harpidetu</strong> beharko duzu $a.</li></ul><br />-n. Harpidetu zerbitzu honetan $a-n autentifikatutako erabiltzaileei zure gunean saioa berriz hasi beharrik izan gabe sartzen uzteko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzua (Zerbitzu-hornitzailea) <strong>publikatu</strong> beharko duzu $a.</li></ul><br />-n.';
+$string['rpc_negotiation_timeout'] = 'RPC negoziazioaren denbora-muga';
+$string['sso_idp_description'] = 'Ema ezazu argitara zerbitzu hau zure erabiltzaileak $a Moodle gunera bertan berriz identifikatu gabe joan ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzuan (Zerbitzu-hornitzailea) <strong>harpidetu</strong> beharko duzu $a.</li></ul><br />-n. Harpidetu zerbitzu honetan $a-n autentifikatutako erabiltzaileei zure gunean saioa berriz hasi beharrik izan gabe sartzen uzteko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzua (Zerbitzu-hornitzailea) <strong>publikatu</strong> beharko duzu $a.</li></ul><br />-n.';
 $string['sso_idp_name'] = 'SSO (Nortasun-hornitzailea)';
 $string['sso_mnet_login_refused'] = '$a[0] izeneko erabiltzaileak ezin du $a[1]-tik saioa hasi.';
 $string['sso_sp_description'] = 'Publika ezazu zerbitzu hau autentifikatutako erabiltzaileak zure gunera $a-tik bertan berriz identifikatu gabe sartu ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzuan (Zerbitzu-hornitzailea) <strong>harpidetu</strong> beharko duzu $a.</li></ul><br />-n. Publika ezazu zerbitzu hau zure erabiltzaileak zure $a Moodle gunera bertan berriz identifikatu gabe joan ahal izateko. <ul><li><em>Dependentzia</em>: Era berean, SSO zerbitzua (Zerbitzu-hornitzailea) <strong>publikatu</strong> beharko duzu $a.</li></ul><br />-n.';
 $string['sso_sp_name'] = 'SSO (Zerbitzu-hornitzailea)';
+
+?>
