05f206210e4021c0653a8b0addc6ef6513f37d2a
Abel Camacho <ueu_ueu>
2010-02-10 09:51:18 +0000

    New strings added

---
+++ eu_utf8/block_blog_recent.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // block_blog_recent.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['blockname'] = 'Azken aldiko blog-sarrerak';
+$string['norecentblogentries'] = 'Ez da izan sarrerarik azken aldian';
+$string['numentriestodisplay'] = 'Erakusteko azken aldiko sarrera-kopurua';
+$string['recentinterval'] = '\"Azken aldiko\"tzat hartzeko denbora-tartea';
+
+?>
