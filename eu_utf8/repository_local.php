01f931b4a5577c2469b360e9800985e082ca6d08
Abel Camacho <ueu_ueu>
2010-02-12 09:55:42 +0000

    New strings added

--- eu_utf8/repository_local.php
+++ eu_utf8/repository_local.php
@@ -1,9 +1,11 @@
 <?PHP // $Id$ 
-      // repository_local.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+      // repository_local.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
 $string['configplugin'] = 'Fitxategi lokalen biltegiaren ezarpenak';
+$string['currentusefiles'] = 'Oraintxe erabilitako fitxategiak';
 $string['emptyfilelist'] = 'Ez dago erakusteko fitxategirik';
+$string['local:view'] = 'Ikusi zerbitzariaren biltegia';
 $string['notitle'] = 'Izenbururik gabe';
 $string['remember'] = 'Gogoratu';
 $string['repositorydesc'] = 'Moodle zerbitzari lokalaren biltegia';
