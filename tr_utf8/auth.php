43e268943bfd5b0d87a10e703bf8999784588e24
koen roggemans <koenr>
2010-02-22 14:07:08 +0000

    url missing from email confirmation, see http://moodle.org/mod/forum/discuss.php?d=130651

--- tr_utf8/auth.php
+++ tr_utf8/auth.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // auth.php - created with Moodle 2.0 dev (Build: 20091216) (2009112400)
+      // auth.php - created with Moodle 1.9.7+ (Build: 20100208) (2007101571.04)
 
 
 $string['CASform'] = 'Kimlik denetimi seçeneği';
@@ -100,7 +100,9 @@ $string['auth_emailrecaptcha_key'] = 'reCAPTCHA etkinleştir';
 $string['auth_emailsettings'] = 'Ayarlar';
 $string['auth_emailtitle'] = 'Eposta temelli kendi kendine kayıt';
 $string['auth_emailupdate'] = 'E-posta adresi güncellemesi';
-$string['auth_emailupdatemessage'] = '$a->site\'deki kullanıcı hesabınız için e-posta adresinizde değişiklik isteğinde bulundunuz. Lütfen bu değişikliği teyit etmek için tarayıcınızda aşağıdaki URL\'yi açın.';
+$string['auth_emailupdatemessage'] = '$a->site\'deki kullanıcı hesabınız için e-posta adresinizde değişiklik isteğinde bulundunuz. Lütfen bu değişikliği teyit etmek için tarayıcınızda aşağıdaki URL\'yi açın.
+
+$a->url';
 $string['auth_emailupdatesuccess'] = '<em>$a->fullname</em> kullanıcısının e-posta adresi başarılı bir şekilde <em>$a->email</em> olarak güncellendi.';
 $string['auth_emailupdatetitle'] = '$a->site\'deki e-posta güncellemesinin teyidi';
 $string['auth_fcchangepasswordurl'] = 'Şifre değişim URL';
@@ -305,8 +307,6 @@ $string['auth_updateremote_ldap'] = '<p><b>Not:</b> Harici LDAP verisi güncelle
 $string['auth_user_create'] = 'Kullanıcı oluşturmayı etkinleştir';
 $string['auth_user_creation'] = 'Yeni (anonim) kullanıcılar, harici yetkilendirme kaynağında veya e-postayla onay ile hesap açabilirler. Bunu etkinleştirirseniz aynı zamanda kullanıcı oluşturma için modüle-özel seçenekleri de yapılandırmayı unutmayın.';
 $string['auth_usernameexists'] = 'Seçilen kullanıcı zaten var. Başka bir tane seçin.';
-$string['auth_webservicedescription'] = 'Web servisleri için elle oluşturulan hesaplar.';
-$string['auth_webservicetitle'] = 'Web servisleri';
 $string['authenticationoptions'] = 'Kimlik denetimi seçenekleri';
 $string['authinstructions'] = 'Buraya kullanıcılarınız için yönergeleri yazabilirsiniz. Kullanıcılarınız böylece hangi kullanıcı adı ve şifreyi kullanmaları gerektiğini bileceklerdir. Buraya yazdığınız metin, giriş sayfasında gösterilecektir. Hiçbir şey yazmazsanız hiçbir yönerge ekranda gösterilmez.';
 $string['auto_add_remote_users'] = 'Uzak kullanıcıları otomatik ekle';
@@ -316,7 +316,6 @@ $string['chooseauthmethod'] = 'Bir kimlik denetimi metodu seçin';
 $string['createpasswordifneeded'] = 'Gerekirse şifre oluştur';
 $string['enterthenumbersyouhear'] = 'Duyduğunuz rakamları yazın:';
 $string['enterthewordsabove'] = 'Yukarıdaki sözcükleri yazın:';
-$string['errormaxconsecutiveidentchars'] = 'Şifreler en fazla $a ardışık özdeş karakter içermelidir.';
 $string['errorminpassworddigits'] = 'Şifreler en az $a rakam içermeli.';
 $string['errorminpasswordlength'] = 'Şifreler en az $a karakter uzunluğunda olmalı.';
 $string['errorminpasswordlower'] = 'Şifreler en az $a küçük karakter içermeli.';
@@ -356,11 +355,6 @@ $string['shib_no_attributes_error'] = 'Shibboleth yetkilendirmesi kullandığın
 $string['shib_not_all_attributes_error'] = 'Moodle, sizde olmayan Shibboleth niteliklerine gereksinim duyuyor. Bu nitelikler şunlardır: $a<br />Lütfen bu sitenin yöneticisi veya Kimlik Sağlayıcı ile iletişim kurun.';
 $string['shib_not_set_up_error'] = 'Bu sayfa için Shibboleth çevre değişkenleri sunulmadığı için Shibboleth yetkilendirmesi doğru ayarlanmış olarak görünmüyor. Lütfen, Shibboleth yetkilendirmesi ayarlaması hakkında daha fazla yönerge için <a href=\"README.txt\">BENİOKU</a> dosyasına başvurun ya da bu sitenin yöneticisi ile iletişim kurun.';
 $string['showguestlogin'] = 'Ziyaretçi düğmesini giriş sayfasından gizleyebilir veya gösterebilirsiniz.';
-$string['sso_idp_description'] = 'Kullanıcılarınızın $a Moodle sitesinde yeniden giriş yapmaksızın gezinebilmesi için bu servisi duyur.<ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Servis Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />$a sitesinden yetkilendirilmiş kullanıcıların sitenize yeniden giriş yapmaksızın erişebilmesi için bu servise abone ol.<ul><li><em>Bağımlılık</em>: SSO (Servis Sağlayıcı) servisini ayrıca $a sitesine <strong>duyurmalısınız.</strong></li></ul><br />';
-$string['sso_idp_name'] = 'SSO (Kimlik Sağlayıcı)';
-$string['sso_mnet_login_refused'] = '$a[0] kullanıcısı, $a[1] sitesinden giriş yapmaya yetkili değil.';
-$string['sso_sp_description'] = '$a sitesinden yetkilendirilmiş kullanıcıların sitenize yeniden giriş yapmaksızın erişebilmesi için bu servisi duyur.<ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Kimlik Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />Kullanıcılarınızın $a Moodle sitesinde yeniden giriş yapmaksızın gezinebilmesi için bu servise abone ol.<ul><li><em>Bağımlılık</em>: SSO (Kimlik Sağlayıcı) servisini ayrıca $a sitesine <strong>duyurmalısınız.</strong></li></ul><br />';
-$string['sso_sp_name'] = 'SSO (Servis Sağlayıcı)';
 $string['stdchangepassword'] = 'Standard Şifre Değiştirme Sayfası kullan';
 $string['stdchangepassword_expl'] = 'Harici yetkilendirme sistemi Moodle yoluyla şifre değişikliklerine izin veriyorsa bunu Evet olarak değiştirin. Bu ayar \'Şifre Değiştirme Sayfasını\' hükümsüz kılar.';
 $string['stdchangepassword_explldap'] = 'DİKKAT: LDAP sunucuya uzaktan erişiliyorsa şifreli SSL bağlantısı (ldaps://) kullanmanız tavsiye edilir.';
@@ -370,7 +364,15 @@ $string['update_never'] = 'Asla';
 $string['update_oncreate'] = 'Oluşturulduğunda';
 $string['update_onlogin'] = 'Her giriş yapıldığında';
 $string['update_onupdate'] = 'Güncellendiğinde';
-$string['user_activatenotsupportusertype'] = 'auth: ldap user_activate() seçili kullanıcı tipini desteklemiyor: $a';
-$string['user_disablenotsupportusertype'] = 'auth: ldap user_disable() seçili kullanıcı tipini desteklemiyor (..şimdilik)';
+$string['auth_webservicedescription'] = 'Web servisleri için elle oluşturulan hesaplar.'; // ORPHANED
+$string['auth_webservicetitle'] = 'Web servisleri'; // ORPHANED
+$string['errormaxconsecutiveidentchars'] = 'Şifreler en fazla $a ardışık özdeş karakter içermelidir.'; // ORPHANED
+$string['sso_idp_description'] = 'Kullanıcılarınızın $a Moodle sitesinde yeniden giriş yapmaksızın gezinebilmesi için bu servisi duyur.<ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Servis Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />$a sitesinden yetkilendirilmiş kullanıcıların sitenize yeniden giriş yapmaksızın erişebilmesi için bu servise abone ol.<ul><li><em>Bağımlılık</em>: SSO (Servis Sağlayıcı) servisini ayrıca $a sitesine <strong>duyurmalısınız.</strong></li></ul><br />'; // ORPHANED
+$string['sso_idp_name'] = 'SSO (Kimlik Sağlayıcı)'; // ORPHANED
+$string['sso_mnet_login_refused'] = '$a[0] kullanıcısı, $a[1] sitesinden giriş yapmaya yetkili değil.'; // ORPHANED
+$string['sso_sp_description'] = '$a sitesinden yetkilendirilmiş kullanıcıların sitenize yeniden giriş yapmaksızın erişebilmesi için bu servisi duyur.<ul><li><em>Bağımlılık</em>: $a sitesindeki SSO (Kimlik Sağlayıcı) servisine ayrıca <strong>abone olmalısınız</strong>.</li></ul><br />Kullanıcılarınızın $a Moodle sitesinde yeniden giriş yapmaksızın gezinebilmesi için bu servise abone ol.<ul><li><em>Bağımlılık</em>: SSO (Kimlik Sağlayıcı) servisini ayrıca $a sitesine <strong>duyurmalısınız.</strong></li></ul><br />'; // ORPHANED
+$string['sso_sp_name'] = 'SSO (Servis Sağlayıcı)'; // ORPHANED
+$string['user_activatenotsupportusertype'] = 'auth: ldap user_activate() seçili kullanıcı tipini desteklemiyor: $a'; // ORPHANED
+$string['user_disablenotsupportusertype'] = 'auth: ldap user_disable() seçili kullanıcı tipini desteklemiyor (..şimdilik)'; // ORPHANED
 
 ?>
