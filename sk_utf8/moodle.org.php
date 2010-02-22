9abab66e1ce3767d7afe265e4555311f0b9bdf5b
Miroslav Fikar <fikar>
2010-02-22 22:00:45 +0000

    MB joined translators

---
+++ sk_utf8/moodle.org.php
@@ -0,0 +1,157 @@
+<?PHP // $Id$ 
+      // moodle.org.php - created with Moodle 1.9.7+ (Build: 20100203) (2007101571.03)
+
+
+$string['aboutdemosite'] = 'Náš demonštračný portál je Moodle s ukážkami vzorových kurzov, ktoré si môžete vyskúšať, ako učiteľ, študent, alebo dokonca ako správca.  Nemajte obavy, že niečo pokazíte. Celý portál sa vráti do pôvodného stavu každú hodinu.';
+$string['aboutdemositetitle'] = 'Demonštračný portál';
+$string['aboutintro'] = 'Moodle je Open Source Course Management System (CMS), tiež známy ako systém riadenia výučby (Learning Management System - LMS) alebo virtuálne výučbové prostredie (Virtual Learning Environment - VLE).  Stal sa veľmi populárny medzi pedagógmi po celom svete ako on-line nástroj pre tvorbu dynamických webových stránok pre študentov.  Aby fungoval, musí byť nainštalovaný niekde na web serveri, a to buď na jednom z vašich počítačov, alebo u webhostingovej spoločnosti.';
+$string['aboutoverview'] = '<p>Snahou Moodle je vždy poskytnúť pedagógom najlepšie nástroje pre správu a podporu učenia sa, ale existuje veľa spôsobov, ako využiť Moodle:</p>
+<ul>
+<li>Moodle má vlastnosti, ktoré mu umožňujú rozsahom veľké nasadenie so stovkami tisícov študentov, ale môže byť taktiež použitý pre základné školy alebo ho môžu použiť nadšenci pre seba.</li>
+<li>Mnoho inštitúcií používa Moodle ako svoju platformu pre poskytovanie plne on-line kurzov, zatiaľ čo iní ho používajú na jednoduché rozšírenie prezenčných (face-to-face) kurzov (tzv. blended learning).</li>
+<li>Veľa našich užívateľov používa moduly aktivít (napr. fórum, wiki, databázy a podobne) na výstavbu široko spolupracujúcich učiacich sa komunít vystavaných okolo ich predmetu záujmu (v tradícií so sociálnym konštruktivizmom), zatiaľ čo iní dávajú prednosť použitiu Moodle ako prostriedku na poskytovanie obsahu pre študentov (napríklad pomocou štandardných SCORM balíčkov) a hodnotenia učenia sa pomocou zadaní a testov.</li>
+</ul>';
+$string['aboutoverviewtitle'] = 'Čo je to Moodle?';
+$string['aboutstatistics'] = 'Moodle je voľne k stiahnutiu a registrácia je dobrovoľná, takže nemáme kompletné informácie o všetkých, ktorý ho používajú.  Avšak, s použitím toho, čo vieme, môžeme automaticky generovať niekoľko zaujímavých grafov a čísel na našej stránke Štatistiky.';
+$string['aboutstatisticstitle'] = 'Štatistiky';
+$string['browsecvstitle'] = 'Prechádzať zdrojový kód';
+$string['communityintro'] = 'Moodle je veľká a rozmanitá používateľská komunita s viac ako <a href=\"http://moodle.org/userpics\">$a->userCount registrovaných užívateľov</a> len na tomto Moodle portáli. Hovoríme <a href=\"http://download.moodle.org/lang16/\">$a->langcount jazykmi</a>, <a href=\"http://moodle. org / sites  \">$a->countrycount krajinách</a> (máme aj <a href=\"http://moodle.org/stats\">viac štatistík</a>).';
+$string['conferencecenter'] = 'Moodle konferenčné centrum';
+$string['connectedsites'] = 'Niektoré Moodle sídla sú pripojené na moodle.org pomocou Siete Moodle  takže sa môžete na ne dostať priamo, keď ste už overení tu.';
+$string['connectedsitestitle'] = 'Pripojené sídla';
+$string['contactbugs'] = 'Môžete nahlásiť problémy v softvéri Moodle na akomkoľvek mieste moodle.org nahlásením chyby do Moodle Tracker-u.';
+$string['contactbugstitle'] = 'Správa o chybách';
+$string['contactintro'] = 'Prosím vyberte si z nasledujúcich možností kontaktu:';
+$string['contactlogin'] = 'Pre všetky ostatné otázky sa prihláste na tomto portáli a potom použite kontaktný formulár, ktorý sa objaví na tejto stránke.';
+$string['contactmoodlecom'] = 'V prípade akýchkoľvek otázok týkajúcich sa licencie, ochrannej známky, obchodných záležitosti a hlavného vývoja Moodle, obráťte sa na help desk na <a href=\"http://moodle.com/\">Moodle.com</a>';
+$string['contactmoodlecomhelpdesk'] = 'Moodle.com Helpdesk';
+$string['contactmoodlecomtitle'] = 'Moodle.com';
+$string['contactmoodleorgemail'] = 'Pri problémoch s potvrdením konta na moodle.org napíšte na e-mailovú adresu:';
+$string['contactmoodleorglogin'] = 'Prihláste sa na túto stránku teraz';
+$string['contactmoodleorgother'] = 'Pre všetky ostatné otázky, použite prosím kontaktný formulár nižšie.';
+$string['contactmoodleorgtitle'] = 'Moodle.org';
+$string['contactsecurity'] = 'Otázky bezpečnosti môžu byť tiež odoslané bezpečne pomocou Tracker-a, ktorý poskytuje možnosť nastaviť úroveň zabezpečenia zodpovedajúcim spôsobom.';
+$string['contactsent'] = 'Vaša správa bola zaslaná e-mailom na Manažéra Moodle Komunity.';
+$string['contactsupport'] = 'Pre pomoc s využitím softvéru Moodle, prečítajte si našu stránku podpory, ktorá uvádza mnoho užitočných zdrojov, ktoré máte k dispozícii.';
+$string['contactsupporttitle'] = 'Podpora';
+$string['developercontributors'] = 'Poďakovania';
+$string['developerlist'] = 'Úplný zoznam vývojárov s možnosťou zápisu';
+$string['developermeetings'] = 'Stretnutia vývojárov';
+$string['developers'] = 'Hlavný vývoj Moodle je vedený tímom v Moodle.com, ktorému pomáhajú iní vývojári na celom svete. Viacerí z nich sú aj priamo zodpovední za rozličné zásuvné moduly a doplnky.';
+$string['developerstitle'] = 'Vývojári';
+$string['developmentdocs'] = 'Veľká časť našej dokumentácie je v Moodle Docs - vrátane špecifikácií, brainstormingov, postupov, štandardov a finálnych užívateľských príručiek.';
+$string['developmentdocstitle'] = 'Vývojárska dokumentácia';
+$string['developmentintro'] = 'Usilujeme sa o transparentný a otvorený vývojový proces, ktorý vyzýva prispievať celú používateľskú komunitu. Vítame samozrejme PHP programátorov, ale všetci môžete prispievať prostredníctvom diskusií, testovania, spätnej väzby a dokumentácie.';
+$string['developmenttracker'] = 'Všetky záležitosti okolo rozvoja (ako sú chyby, nové funkcie a vylepšenia) sú zaznamenané a spravované v Moodle Tracker-i. Môžete vidieť, kto na čom pracuje a prispieť k akejkoľvek konverzácii. Je treba hľadať v prvom rade v Tracker-i, ak sa snažíte zistiť podrobnosti o konkrétnych otázkach. Uistite sa, že ste <strong>hlasovali</strong> o problémoch, ktoré chcete vidieť opravené ako prvé, pretože vaše hlasy pomôžu určiť priority vývoja.';
+$string['donations'] = 'Moodle je úplne oslobodené od finančných požiadaviek. Avšak, viacerí účastníci v komunite darovali niečo nášmu projektu, aby bolo možné zaplatiť vývojárov, ktorí pomáhajú.';
+$string['donationstitle'] = 'Dary';
+$string['downloadcore'] = 'Náš hlavný spôsob distribúcie je prostredníctvom týchto štandardných základných balíčkov.  Obsahujú moduly, ktoré sú udržiavané hlavnými vývojármi a sú vhodné pre inštaláciu na takmer akejkoľvek platforme, kde už máte nainštalovaný PHP a databázu.';
+$string['downloadcoretitle'] = 'Štandardné Moodle balíky';
+$string['downloadcvs'] = 'Ďalšou možnosťou, ako získať zdrojový kód jadra, je pomocou CVS. Môžete sa pripojiť priamo k jednému z našich zrkadiel úložiska kódu, ktorý používajú vývojári Moodle.  Vďaka tomu sú aktualizácie veľmi jednoduché, aj keď ste vykonali miestne zmeny zdrojového kódu.';
+$string['downloadcvstitle'] = 'Moodle cez CVS';
+$string['downloadintro'] = 'Moodle je softvér s otvoreným kódom pod licenciou GPL <a href=\"http://docs.moodle.org/en/License\"></a>.  Všetko, čo produkujeme, je pre vás k dispozícii na stiahnutie a používanie zadarmo.';
+$string['downloadlang'] = 'V aktuálnych verziách Moodle bolo pridanie podpory pre ďalšie jazyky automatizované pomocou menu Administratíva portálu na vašom webe.  Avšak, pre niektoré staršie verzie Moodle je nové jazykové balíčky potrebné stiahnuť ručne.';
+$string['downloadlangtitle'] = 'Jazykové balíčky';
+$string['downloadlogotitle'] = 'Logá Moodle';
+$string['downloadmac'] = 'Ak nemáte nastavený webový server tieto všetko-v-jednom balíčky vám umožnia jednoduchú inštaláciu Moodle na akomkoľvek počítači Apple so systémom Mac OS X.';
+$string['downloadmactitle'] = 'Moodle pre Mac OS X';
+$string['downloadmodules'] = 'Prechádzať komplexnou databázou ďalších aktivít a zásuvných modulov, ktoré môžu byť pridané do vášho Moodle, aby ste získali ďalšie funkcie okrem základných.';
+$string['downloadmodulestitle'] = 'Aktivity a zásuvné moduly';
+$string['downloadthemes'] = 'Vzhľad Moodle môžete zásadne zmeniť pomocou inej témy.  Vedieme databázu Moodle tém, ktoré vytvorili a zdieľajú používatelia.';
+$string['downloadthemestitle'] = 'Témy';
+$string['downloadwin'] = 'Ak nemáte nastavený webový server tieto všetko-v-jednom balíčky vám umožnia jednoduchú inštaláciu Moodle na akomkoľvek počítači so systémom Microsoft Windows.';
+$string['downloadwintitle'] = 'Moodle pre Windows';
+$string['events'] = 'Hoci sa špecializujeme na online vzdelávanie, je stále príjemné sa spolu stretnúť v jednej miestnosti. Pozrite si tieto odkazy pre viac informácií o našich konferenciách (nazývame ich MoodleMoots) a iných udalostiach.';
+$string['eventstitle'] = 'Udalosti';
+$string['frontpagewelcome1'] = 'Moodle je Open Source Course Management System (CMS), tiež známy ako systém riadenia výučby (Learning Management System - LMS) alebo virtuálne výučbové prostredie (Virtual Learning Environment - VLE).  Ide o webové aplikácie zadarmo, ktoré pedagógovia môžu využiť na vytvorenie efektívnych internetových stránok na učenia sa.';
+$string['frontpagewelcome2'] = 'Moodle.org je náš komunitný web, kde sa Moodle vyvíja a diskutuje.  Prosím použite menu aby ste ho preskúmali a zapojili sa!';
+$string['frontpagewelcometitle'] = 'Vitajte v komunite Moodle!';
+$string['generaldeveloperforum'] = 'Všeobecné vývojárske fórum';
+$string['jobs'] = 'Navštívte našu databázu pozícií pre informáciu o práci ohľadom Moodle.';
+$string['jobstitle'] = 'Moodle Pracovné Pozície';
+$string['moodlecertification'] = 'Moodle Certifikácia';
+$string['moodleconsulting'] = 'Moodle Konzultácie';
+$string['moodlecourseware'] = 'Moodle Courseware';
+$string['moodlecustomisation'] = 'Moodle Prispôsobenie';
+$string['moodledocs'] = 'Moodle Dokumentácia';
+$string['moodlehosting'] = 'Moodle Hosting';
+$string['moodleinstallation'] = 'Moodle Inštalácia';
+$string['moodleintegrations'] = 'Moodle Integrácie';
+$string['moodlesupport'] = 'Moodle Podpora';
+$string['moodlethemes'] = 'Moodle Témy';
+$string['moodletrackertitle'] = 'Moodle Tracker';
+$string['moodletraining'] = 'Moodle Tréning';
+$string['mycoursesintro'] = 'Ste zapísaný v týchto \"kurzoch\" na tomto komunitnom webe, z ktorých každý obsahuje jedno alebo viac diskusných fór.';
+$string['nameabout'] = 'O';
+$string['nameaboutmoodle'] = 'O Moodle';
+$string['namecommercial'] = 'Komerčné služby';
+$string['namecommunity'] = 'Komunita';
+$string['namecontact'] = 'Kontakt';
+$string['namedevelopment'] = 'Vývoj';
+$string['namedocumentation'] = 'Dokumentácia';
+$string['namedownloads'] = 'Na stiahnutie';
+$string['nameevents'] = 'Udalosti';
+$string['nameforums'] = 'Fóra';
+$string['namemycourses'] = 'Moje kurzy';
+$string['namenetwork'] = 'Prepojené sídla';
+$string['namenews'] = 'Novinky';
+$string['namesecurity'] = 'Novinky o bezpečnosti';
+$string['namesupport'] = 'Podpora';
+$string['networkintro'] = 'Tieto Moodle portály boli pripojené k moodle.org pomocou Siete Moodle, takže sa môžete na ne priamo prihlásiť odtiaľto len kliknutím.';
+$string['newsbuzz'] = 'Moodle Buzz';
+$string['newsbuzzdetail'] = 'Moodle publikácie zo sveta';
+$string['newscalendar'] = 'Kalendár';
+$string['newscalendardetail'] = 'Budúce Moodle akcie';
+$string['newsofficial'] = 'Oficiálne informácie o novinkách v Moodle';
+$string['newsplanet'] = 'Planéta Moodle';
+$string['newsplanetdetail'] = 'Súhrnné blogy od vývojárov Moodle';
+$string['newsrecent'] = 'Najnovšie správy';
+$string['newssecurity'] = 'Novinky o bezpečnosti';
+$string['newssecuritydetail'] = 'Dôležité informácie o bezpečnosti';
+$string['noenrolledcoursesyet'] = 'Doteraz ste sa nezapísali do žiadneho kurzu na tejto stránke';
+$string['overviewabout'] = 'Prehľad o Moodle';
+$string['overviewcommunity'] = 'Prehľad komunity Moodle';
+$string['overviewdevelopment'] = 'Prehľad vývoja Moodle';
+$string['overviewdownloads'] = 'Prehľad o sťahovaní Moodle';
+$string['overviewmy'] = 'Vidieť všetky kurzy na moodle.org, v ktorých ste zapísaný';
+$string['overviewnews'] = 'Prehľad aktuálnych noviniek Moodle';
+$string['overviewsupport'] = 'Prehľad možností podpory Moodle';
+$string['popularissues'] = 'Najčastejšie otázky';
+$string['recentpart'] = 'Táto stránka zobrazuje ľudí, ktorí používajú moodle.org.';
+$string['recentparttitle'] = 'Naposledy prihlásení účastníci';
+$string['registeredsites'] = 'Inštitúcie nie sú povinné informovať nikoho o používaní Moodle, pretože Moodle je open source.  Avšak niektoré Moodle sídla sa rozhodli u nás zaregistrovať a my ich vedieme v zozname.';
+$string['registeredsitestitle'] = 'Registrované portály';
+$string['roadmap'] = 'Moodle cestovná mapa';
+$string['shop'] = 'Hľadáte nejaký spôsob, ako ukázať, že ste súčasťou našej komunity?  Pozrite sa na obchod s tričkami, hrnčekmi a všelijakými inými vecami.';
+$string['shoptitle'] = 'Moodle Obchod';
+$string['sourcecodetitle'] = 'Otvorený zdrojový kód';
+$string['supportbooks'] = 'Zoznam kníh o Moodle k dispozícii na zakúpenie.';
+$string['supportbookstitle'] = 'Knihy a manuály';
+$string['supportcommercial'] = '<a href=\"http://moodle.com/\">Moodle Partneri</a> je celosvetová skupina autorizovaných servisných spoločností, ktoré sa zaviazali finančne podporovať Moodle open source projekt. Poskytujú množstvo voliteľných komerčných služieb pre používateľov Moodle na celom svete:';
+$string['supportcommercialtitle'] = 'Komerčné služby';
+$string['supportdocs1'] = 'Dokumentácia Moodle je skvelé miesto kde začať. Upozorňujeme najmä na stránky s Často kladenými otázkami (FAQ).';
+$string['supportdocs2'] = 'Dobrý spôsob prístupu k príslušnej dokumentácii je použiť odkaz \"Moodle Docs pre túto stránku\" v dolnej časti každej stránky na vašich stránkach Moodle.';
+$string['supportdocsfaq'] = 'Často kladené otázky (FAQ)';
+$string['supportdocslang'] = 'Moodle Docs v iných jazykoch';
+$string['supportdocstitle'] = 'Dokumentácia';
+$string['supportforums'] = 'Naša používateľská komunita väčšinou komunikuje prostredníctvom fór v \"kurzoch\" na tomto Moodle portáli.  Musíte sa \"prihlásiť\" do kurzu, aby ste mohli prispievať do fór (zápis je úplne zadarmo).';
+$string['supportforumsenglish'] = 'Hlavná podpora a diskusia o vývoji medzi užívateľmi a vývojármi je v angličtine tu:';
+$string['supportforumsenglishname'] = 'Používanie Moodle';
+$string['supportforumslang'] = 'Hlavné fórom pre nadväzovať kontaktov v angličtine je tu:';
+$string['supportforumslangname'] = 'Moodle Lóža (Lounge)';
+$string['supportforumslangurl'] = 'http://moodle.org/course/view.php?id=55';
+$string['supportforumslist'] = 'Máme veľa iných fór, vo viacerých jazykoch:';
+$string['supportforumslistname'] = 'Zoznam všetkých fór';
+$string['supportforumstitle'] = 'Fóra';
+$string['supportintro'] = 'Nie ste sami!  Existujú rôzne spôsoby, ako získať pomoc od Moodle, ak ju budete potrebovať.';
+$string['contactcommunity'] = 'Všetky ostatné otázky by mali smerovať na Manažéra Moodle Komunity.'; // ORPHANED
+$string['vývojárov'] = 'Hlavné vývoj Moodle je vedený hlavným tímom Moodle.com, ktorému pomáhali stovky ďalších vývojárov z celého sveta.  Mnohí z týchto vývojárov sú tiež priamo zodpovední za rôzne moduly a pluginy.'; // ORPHANED
+$string['dary'] = 'Moodle je úplne bez finančných záväzkov voči vám, ale mnohí ľudia darovali niečo na náš projekt, čo pomohlo zaplatiť vývojárov, ktorí to všetko vytvorili.'; // ORPHANED
+$string['akcie'] = 'Aj keď sa špecializujeme na on-line vzdelávanie, je stále pekné stretnúť sa spoločne!  Pozri odkazy na informácie o našich konferenciách (hovoríme im MoodleMoots) a ďalšie akcie.'; // ORPHANED
+$string['práca'] = 'Navštívte našu databázu s informáciami o pracovných pozíciách súvisiacich s Moodle na celom svete.'; // ORPHANED
+$string['MoodleThemes'] = 'Moodle Témy'; // ORPHANED
+$string['namehome'] = 'Domovská stránka'; // ORPHANED
+$string['overview'] = 'Prehľad'; // ORPHANED
+$string['plánu'] = 'Moodle cestovná mapa'; // ORPHANED
+
+?>
