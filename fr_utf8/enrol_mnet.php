81e4b294800e206d35243b25386a566027cd97c3
Nicolas Martignoni <martignoni>
2010-02-24 19:22:01 +0000

    Strings sorted

--- fr_utf8/enrol_mnet.php
+++ fr_utf8/enrol_mnet.php
@@ -1,17 +1,14 @@
 <?php // $Id$
 
-$string['description'] = 'La méthode d\'inscription Réseau Moodle permet à des sites connectés d\'inscrire leurs utilisateurs à des cours de ce site Moodle.';
-$string['enrolname'] = 'Réseau Moodle';
+$string['allcategories'] = '$a catégories potentielles';
+$string['allcourses'] = '$a cours potentiels';
 $string['allow_allcourses'] = 'Permettre l\'inscription dans tous les cours distants.';
 $string['allow_allcourses2'] = 'Permettre l\'inscription des utilisateurs distants à tous les cours de ce site Moodle.';
-$string['allcategories'] = '$a catégories potentielles';
 $string['allowedcategories'] = '$a catégories autorisées';
-$string['nocategoriesdefined'] = 'Aucune catégorie trouvée. Veuillez définir de nouvelles catégories <a href=\"$a\">ici</a>.';
-$string['allcourses'] = '$a cours potentiels';
 $string['allowedcourses'] = '$a cours autorisés';
-$string['nocoursesdefined'] = 'Aucun cours trouvé. Veuillez définir de nouveaux cours <a href=\"$a\">ici</a>.';
 $string['allowedcourseslinktext'] = 'Modifier les cours et catégories autorisés';
-$string['mnet_enrol_name'] = 'Inscription via Réseau Moodle';
+$string['description'] = 'La méthode d\'inscription Réseau Moodle permet à des sites connectés d\'inscrire leurs utilisateurs à des cours de ce site Moodle.';
+$string['enrolname'] = 'Réseau Moodle';
 $string['mnet_enrol_description'] = 'En publiant ce service, vous autorisez les administrateurs de $a à inscrire leurs étudiants à des cours sur votre Moodle.<br />
 <ul>
 <li><em>Dépendance</em> : vous devez également <strong>publier</strong> le service SSO (fournisseur de service) pour $a.</li>
@@ -22,6 +19,9 @@ En vous abonnant à ce service, vous pourrez inscrire vos étudiants aux cours s
 <li><em>Dépendance</em> : vous devez également <strong>vous abonner</strong> au service SSO (fournisseur de service) de $a.</li>
 <li><em>Dépendance</em> : vous devez également <strong>publier</strong> le service SSO (fournisseur d\'identité) pour $a.</li>
 </ul><br />';
+$string['mnet_enrol_name'] = 'Inscription via Réseau Moodle';
 $string['mnetlocalforexternal'] = 'Cours locaux pour utilisateurs externes';
+$string['nocategoriesdefined'] = 'Aucune catégorie trouvée. Veuillez définir de nouvelles catégories <a href=\"$a\">ici</a>.';
+$string['nocoursesdefined'] = 'Aucun cours trouvé. Veuillez définir de nouveaux cours <a href=\"$a\">ici</a>.';
 
 ?>
