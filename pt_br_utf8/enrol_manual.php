153d21b15e8af0f873630b180951f795e014a417
danielneis <danielneis>
2010-04-22 16:37:23 +0000

    Padronizando "enrolment key" como "código de inscrição".

--- pt_br_utf8/enrol_manual.php
+++ pt_br_utf8/enrol_manual.php
@@ -2,16 +2,16 @@
       // enrol_manual.php - created with Moodle 1.9.5+ (Build: 20091007) (2007101553)
 
 
-$string['description'] = 'Este é o formulário padrão de inscrição. Existem dois modos de inscrição de um estudante em um curso.
+$string['description'] = 'Este é o formulário padrão de inscrição. Existem dois modos de inscrever um estudante em um curso.
 <ul>
-<li>Um professor ou administrador pode inscrever os alunos manualmente usando o Painel de Administração do curso.</li>
-<li>Um curso pode ter uma chave de inscrição definida. Todas as pessoas cadastradas no site que conhecem a chave de inscrição de um curso podem fazer a inscrição autonomamente.</li>
+<li>Um professor ou administrador pode inscrever os alunos manualmente usando o link na caixa Administração do curso.</li>
+<li>Um curso pode ter um código de inscrição definido. Todas as pessoas que conhecem este código podem inscrever-se no curso.</li>
 </ul>';
-$string['enrol_manual_requirekey'] = 'Requerer chaves de inscrição nos novos cursos e evitar a remoção das chaves existentes.';
-$string['enrol_manual_showhint'] = 'Habilitar esta opção para mostrar como dica a primeira letra da chave de inscrição em casos de erros dos usuários.';
-$string['enrol_manual_usepasswordpolicy'] = 'Utilizar a atual política de senhas do curso nas chaves de inscrição.';
-$string['enrolmentkeyerror'] = 'A chave de inscrição utilizada é errada, é necessário utilizar outra chave.';
-$string['enrolname'] = 'Inscrição Interna';
+$string['enrol_manual_requirekey'] = 'Exigir códigos de inscrição nos novos cursos e evitar a remoção dos códigos existentes.';
+$string['enrol_manual_showhint'] = 'Habilitar esta opção para mostrar como dica a primeira letra do código de inscrição em casos de erros dos usuários.';
+$string['enrol_manual_usepasswordpolicy'] = 'Utilizar a atual política de senhas do curso nos códigos de inscrição.';
+$string['enrolmentkeyerror'] = 'O código de inscrição utilizado está errado, por favor, tente de novo.';
+$string['enrolname'] = 'Inscrição interna';
 $string['keyholderrole'] = 'A função do usuário que possui o código de inscrição no curso. Mostrado para estudantes tentando inscrição no curso.';
 
 ?>
