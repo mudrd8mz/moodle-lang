4bbde73e7c6cb3235e02e5ff871cdfbb609ce805
Abel Camacho <ueu_ueu>
2010-05-24 15:50:41 +0000

    New file added

---
+++ eu_utf8/license.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // license.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
+
+
+$string['allrightsreserved'] = 'Eskubide guztiak erreserbatuta';
+$string['cc'] = 'Creative Commons';
+$string['cc-nc'] = 'Creative Commons - EzKomertziala';
+$string['cc-nc-nd'] = 'Creative Commons - Aitortu-EzKomertziala-LanEratorririkGabe';
+$string['cc-nc-sa'] = 'Creative Commons - Aitortu-EzKomertziala-PartekatuBerdin';
+$string['cc-nd'] = 'Creative Commons - Aitortu-LanEratorririkGabe';
+$string['cc-sa'] = 'Creative Commons - ShareAlike';
+$string['public'] = 'Domeinu publikoa';
+$string['unknown'] = 'Lizentzia ezezaguna';
+
+?>
