72966baf8c04f131b2a8d9cee7451faf054aa60c
Abel Camacho <ueu_ueu>
2010-06-04 08:35:06 +0000

    New strings added

--- eu_utf8/edufields.php
+++ eu_utf8/edufields.php
@@ -2,8 +2,66 @@
       // edufields.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
+$string['field10101'] = 'Matematika zientziak';
 $string['field1010101'] = 'Matematika';
 $string['field1010103'] = 'Estatistika';
+$string['field1010199'] = 'Matematika zientziak (bestelakoak)';
+$string['field10103'] = 'Fisika eta Astronomia';
+$string['field1010301'] = 'Fisika';
+$string['field1010303'] = 'Astronomia';
 $string['field10107'] = 'Lurraren zientziak';
+$string['field1010703'] = 'Geologia';
+$string['field1010711'] = 'Hidrologia';
+$string['field1010903'] = 'Botanika';
+$string['field1010911'] = 'Mikrobiologia';
+$string['field1010915'] = 'Zoologia';
+$string['field1019901'] = 'Medikuntza Zientzia';
+$string['field1019907'] = 'Farmakologia';
+$string['field102'] = 'Informazioaren teknologia';
+$string['field1020103'] = 'Programazioa';
+$string['field10203'] = 'Informazio-sistemak';
+$string['field1020399'] = 'Informazio-sistemak (bestelakoak)';
+$string['field1029999'] = 'Informazioaren teknologia (bestelakoak)';
+$string['field106'] = 'Osasuna';
+$string['field10601'] = 'Medikuntza ikasketak';
+$string['field1060101'] = 'Medikuntza orokorra';
+$string['field1060103'] = 'Kirurgia';
+$string['field1060105'] = 'Psikiatria';
+$string['field1060109'] = 'Pediatria';
+$string['field1060113'] = 'Patologia';
+$string['field1060115'] = 'Erradiologia';
+$string['field10603'] = 'Erizaintza';
+$string['field1060301'] = 'Erizaintza orokorra';
+$string['field1060399'] = 'Erizaintza (bestelakoak)';
+$string['field10605'] = 'Farmazia';
+$string['field1061311'] = 'Epidemiologia';
+$string['field1061501'] = 'Erradiografia';
+$string['field1061701'] = 'Fisioterapia';
+$string['field1061903'] = 'Akupuntura';
+$string['field1069999'] = 'Osasuna (bestelakoak)';
+$string['field107'] = 'Hezkuntza';
+$string['field1079999'] = 'Hezkuntza (bestelakoak)';
+$string['field10803'] = 'Negozio-kudeaketa';
+$string['field1080303'] = 'Giza baliabideen kudeaketa';
+$string['field1080307'] = 'Erakundeen kudeaketa';
+$string['field10805'] = 'Salmentak eta marketina';
+$string['field1080501'] = 'Salmentak';
+$string['field1080505'] = 'Marketina';
+$string['field10807'] = 'Turismoa';
+$string['field1080701'] = 'Turismoa';
+$string['field109'] = 'Gizartea eta kultura';
+$string['field1090301'] = 'Soziologia';
+$string['field1090303'] = 'Antropologia';
+$string['field1090307'] = 'Arkeologia';
+$string['field1090701'] = 'Psikologia';
+$string['field10915'] = 'Hizkuntza eta literatura';
+$string['field1091503'] = 'Iparraldeko europar hizkuntzak';
+$string['field1091505'] = 'Hegoaldeko europar hizkuntzak';
+$string['field1091507'] = 'Ekialdeko europar hizkuntzak';
+$string['field1091521'] = 'Hizkuntzalaritza';
+$string['field1091523'] = 'Literatura';
+$string['field1091599'] = 'Hizkuntza eta literatuta (bestelakoak)';
+$string['field1100101'] = 'Musika';
+$string['field1100703'] = 'Kazetaritza';
 
 ?>
