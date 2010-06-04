787d26a0b1fe393b584dd1cf263ee4e44202d616
Abel Camacho <ueu_ueu>
2010-06-04 08:54:26 +0000

    New strings added

--- eu_utf8/rating.php
+++ eu_utf8/rating.php
@@ -2,6 +2,15 @@
       // rating.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
-$string['aggregatetype'] = 'Agregazio-mora';
+$string['aggregatetype'] = 'Agregazio-mota';
+$string['allowratings'] = 'Gaitu elementuak sailkatu ahal izateko?';
+$string['norate'] = 'Elementuak sailkatzea ez dago gaituta!';
+$string['noviewanyrate'] = 'Zeure mezuen emaitzak bakarrik ikus ditzakezu';
+$string['noviewrate'] = 'Ez duzu mezuen sailkapena ikusteko gaitasuna';
+$string['rate'] = 'Sailkatu';
+$string['ratepermissiondenied'] = 'Ez duzu elementu hau sailkatzeko baimenik';
+$string['rating'] = 'Sailkapena';
+$string['ratings'] = 'Sailkapenak';
+$string['ratingsuse'] = 'Erabili sailkapenak';
 
 ?>
