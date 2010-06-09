6655709a07feced512f25602752c07de28441bd6
danielneis <danielneis>
2010-06-09 13:28:12 +0000

    Corrigindo concordância em diversos termos.

--- pt_br_utf8/notes.php
+++ pt_br_utf8/notes.php
@@ -1,27 +1,27 @@
 <?PHP
 
-$string['addnewnote'] = 'Escrever um novo anotação';
+$string['addnewnote'] = 'Escrever uma novo anotação';
 $string['addnewnoteselect'] = 'Escolher os usuários que serão objeto de anotações';
 $string['bynameondate'] = 'por $a->name - $a->date';
-$string['cannotdeletepost'] = 'Erro na exclusão do anotação';
+$string['cannotdeletepost'] = 'Erro na exclusão da anotação';
 $string['configenablenotes'] = 'Habilitar a arquivação de anotações sobre cada usuário.';
 $string['content'] = 'Conteúdo';
 $string['course'] = 'curso';
 $string['coursenotes'] = 'Anotações do Curso';
 $string['created'] = 'criado';
-$string['deleteconfirm'] = 'Excluir este anotação?';
-$string['deletenotes'] = 'Excluir todos os anotações';
-$string['editnote'] = 'Editar o anotação';
+$string['deleteconfirm'] = 'Excluir esta anotação?';
+$string['deletenotes'] = 'Excluir todas as anotações';
+$string['editnote'] = 'Editar a anotação';
 $string['enablenotes'] = 'Habilitar anotações';
-$string['groupaddnewnote'] = 'Adicionar o mesmo anotação ao grupo';
+$string['groupaddnewnote'] = 'Adicionar a mesma anotação ao grupo';
 $string['invalidid'] = 'ID inválido';
-$string['nocontent'] = 'O conteúdo do anotação não pode estar em branco';
-$string['nonotes'] = 'Ainda não existe nenhum anotação deste tipo';
+$string['nocontent'] = 'O conteúdo da anotação não pode estar em branco';
+$string['nonotes'] = 'Ainda não existe nenhuma anotação deste tipo';
 $string['nopermissiontodelete'] = 'Você não pode excluir esta anotação';
 $string['note'] = 'Anotação';
 $string['notes'] = 'Anotações';
-$string['notesdisabled'] = 'Os anotações estão desabilitados';
-$string['notesnotvisible'] = 'Você não tem permissão para ler os anotações';
+$string['notesdisabled'] = 'As anotações estão desabilitados';
+$string['notesnotvisible'] = 'Você não tem permissão para ler as anotações';
 $string['nouser'] = 'Você deve selecionar um usuário';
 $string['personal'] = 'individual';
 $string['personalnotes'] = 'Anotações individuais';
