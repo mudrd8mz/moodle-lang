42fa6b0fe0752cc5c0490503cff89b311a9eec16
Benito Arias <barias>
2010-06-20 18:33:09 +0000

    *** empty log message ***

---
+++ es_utf8/report_courseoverview.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // report_courseoverview.php - created with Moodle 1.9.8 (Build: 20100325) (2007101580)
+
+
+$string['courseoverview:view'] = 'Ver informe de revisión del curso';
+
+?>
