b50e70f7357a0512558332d6a3eb36d84de5d111
Abel Camacho <ueu_ueu>
2010-06-22 14:59:52 +0000

    New strings added

--- eu_utf8/portfolio_googledocs.php
+++ eu_utf8/portfolio_googledocs.php
@@ -2,6 +2,8 @@
       // portfolio_googledocs.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
+$string['noauthtoken'] = 'Ez da autentifikazio tokenik jaso google-tik. Mesedez, ziurtatu baimena duzula moodle-n zure google kontura sartzeko';
+$string['nosessiontoken'] = 'Saioko tokenik ez dago google-ra esportatzea debekatuz';
 $string['pluginname'] = 'Google Docs';
 $string['sendfailed'] = '$a fitxategiak kale egin du Google-ra bidaltzean';
 
--- eu_utf8/qtype_shortanswer.php
+++ eu_utf8/qtype_shortanswer.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // qtype_shortanswer.php - created with Moodle 1.9.2+ (Build: 20080910) (2007101522)
+      // qtype_shortanswer.php - created with Moodle 1.9.4+ (Build: 20090218) (2007101541)
 
 
 $string['addingshortanswer'] = 'Gehitu erantzun laburreko galdera';
@@ -9,5 +9,6 @@ $string['answerno'] = '$a erantzuna';
 $string['editingshortanswer'] = 'Editatu Erantzun laburreko galdera';
 $string['filloutoneanswer'] = 'Gutxienez erantzun egoki bat eman behar duzu. Zuri utzitako erantzunak ez dira erabiliko. \'*\' erabiliko da komodin gisa beste edozein karakterretarako. Bat datorren lehen erantzuna erabiliko da puntuazioa eta atzera elikadura zehazteko.';
 $string['shortanswer'] = 'Erantzun laburreko galdera';
+$string['shortanswersummary'] = 'Hitz bat edo hitz gutxiko erantzunak onartzen ditu eta komodinak izan ditzaketen hainbat erantzun-eredurekin alderatuta kalifikatzen da.';
 
 ?>
