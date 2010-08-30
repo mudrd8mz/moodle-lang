d0b7a1e379b22dd7d7e1e16b98d6321d4b818518
koen roggemans <koenr>
2010-08-30 10:59:15 +0000

    missing thislanguageint

--- dz_utf8/langconfig.php
+++ dz_utf8/langconfig.php
@@ -26,6 +26,7 @@ $string['strftimetime'] = '%%I:%%M %%p';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Dzongkha';
+$string['thislanguageint'] = 'Dzongkha';
 $string['thousandssep'] = ','; // thousands separator, for some languages it is '.' or ' '
 
 ?>
