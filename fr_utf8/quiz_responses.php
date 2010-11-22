91d84a4ea331630b15b09ba3465da0a41b740b50
Nicolas Martignoni <martignoni>
2010-11-22 12:12:27 +0000

    Some modifications to quiz terminology to cope the distinction between answer and response

--- fr_utf8/quiz_responses.php
+++ fr_utf8/quiz_responses.php
@@ -2,8 +2,8 @@
 
 $string['cannotloadoptions'] = 'Impossible de charger les options de question';
 $string['pagesize'] = 'Taille de page';
-$string['reportresponses'] = 'Réponses';
-$string['responses'] = 'Réponses';
+$string['reportresponses'] = 'Réponses des participants';
+$string['responses'] = 'Réponses des participants';
 $string['responsesdownload'] = 'Téléchargement des réponses';
 $string['responsesoptions'] = 'Options des réponses';
 $string['responsesreport'] = 'Rapport de réponses';
