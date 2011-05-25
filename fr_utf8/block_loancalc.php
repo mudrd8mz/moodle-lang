fd9444d98cc6e0305aeae73c6875d70d8ef0beec
Nicolas Martignoni <nicolas@martignoni.net>
2011-05-25 08:59:38 +0000

    Strings sorted

--- fr_utf8/block_loancalc.php
+++ fr_utf8/block_loancalc.php
@@ -1,13 +1,13 @@
 <?php // $Id$ 
 
-$string['loancalc'] = 'Calculatrice de prêt';
 $string['amountofloan'] = 'Montant du prêt';
-$string['repaymentamount'] = 'Montant des paiements';
-$string['loanterm']  = 'Durée du prêt (années)';
+$string['fortnightly']  = 'Bimensuelle';
 $string['interestrate'] = 'Taux d\'interêt';
+$string['loancalc'] = 'Calculatrice de prêt';
+$string['loanterm']  = 'Durée du prêt (années)';
+$string['monthly'] = 'Mensuelle';
+$string['repaymentamount'] = 'Montant des paiements';
 $string['repaymentfreq'] = 'Fréquence des paiements';
 $string['weekly'] = 'Hebdomadaire';
-$string['fortnightly']  = 'Bimensuelle';
-$string['monthly'] = 'Mensuelle';
 
 ?>
