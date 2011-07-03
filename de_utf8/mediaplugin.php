8aa266251503aec141fc7f34d55793bf69adc6b1
Ralf Krause <rkra@gmx.de>
2011-07-03 12:58:20 +0000

    new string for 1.9 - thanks to Ralf Hilgenstock

--- de_utf8/mediaplugin.php
+++ de_utf8/mediaplugin.php
@@ -1,10 +1,16 @@
 <?PHP // $Id$ 
-      // mediaplugin.php - created with Moodle 1.8 dev (2007012500)
+      // mediaplugin.php - created with Moodle 1.9.12+ (Build: 20110623) (2007101591.03)
 
 
 $string['filtername'] = 'Multimedia-Plugins';
-$string['flashanimation'] = 'Flash-Animation';
-$string['flashvideo'] = 'Flash-Video';
-$string['mp3audio'] = 'MP3-Audio';
+$string['flashanimation'] = 'Flash Animation';
+$string['flashvideo'] = 'Flash Video';
+$string['mp3audio'] = 'MP3 Audio';
+$string['oggaudio'] = 'OGG Audio';
+$string['ogvvideo'] = 'OGV Video';
+$string['quicktime'] = 'Quicktime';
+$string['realaudio'] = 'Real Audio';
+$string['unsupportedplugins'] = '(Der Browser unterstützt diesen Dateityp ({$a}) nicht.';
+$string['wmpvideo'] = 'WMP Video';
 
 ?>
