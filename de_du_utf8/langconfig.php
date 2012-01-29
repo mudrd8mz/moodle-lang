16c07e64522ff5e622eb1580719f955ab342c7f9
Ralf Krause <rkra@gmx.de>
2012-01-29 11:23:08 +0000

    add missing strings

--- de_du_utf8/langconfig.php
+++ de_du_utf8/langconfig.php
@@ -1,18 +1,22 @@
 <?php
+
 $string['alphabet'] = 'A,Ä,B,C,D,E,F,G,H,I,J,K,L,M,N,O,Ö,P,Q,R,S,T,U,Ü,V,W,X,Y,Z';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['decsep'] = ',';
 $string['firstdayofweek'] = '1';
 $string['listsep'] = ';';
 $string['locale'] = 'de_DE.UTF-8';
+$string['localewin'] = 'German_Germany.1252';
+$string['localewincharset'] = 'WINDOWS-1252';
 $string['oldcharset'] = 'ISO-8859-1';
 $string['parentlanguage'] = 'de_utf8';
 $string['strftimedate'] = '%%d. %%B %%Y';
 $string['strftimedateshort'] = '%%d. %%B';
 $string['strftimedatetime'] = '%%d. %%B %%Y, %%H:%%M';
+$string['strftimedatetimeshort'] = '%%d.%%m.%%y, %%H:%%M';
 $string['strftimedaydate'] = '%%A, %%d. %%B %%Y';
 $string['strftimedaydatetime'] = '%%A, %%d. %%B %%Y, %%H:%%M';
-$string['strftimedayshort'] = '%%A, %%d.%%B';
+$string['strftimedayshort'] = '%%A, %%d. %%B';
 $string['strftimedaytime'] = '%%a, %%H:%%M';
 $string['strftimemonthyear'] = '%%B %%Y';
 $string['strftimerecent'] = '%%d. %%b, %%H:%%M';
@@ -22,4 +26,6 @@ $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Deutsch - Du';
 $string['thislanguageint'] = 'German (Personal)';
+$string['thousandssep'] = '.';
+
 ?>
