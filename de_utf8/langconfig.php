083b07f6b6e992ffd39ddb3fcc66bcc58b432c9e
Ralf Krause <rkra@gmx.de>
2012-01-29 09:29:04 +0000

    add strings

--- de_utf8/langconfig.php
+++ de_utf8/langconfig.php
@@ -1,4 +1,5 @@
 <?php
+
 $string['alphabet'] = 'A,Ä,B,C,D,E,F,G,H,I,J,K,L,M,N,O,Ö,P,Q,R,S,T,U,Ü,V,W,X,Y,Z';
 $string['backupnameformat'] = '%%Y%%m%%d-%%H%%M';
 $string['decsep'] = ',';
@@ -12,6 +13,7 @@ $string['parentlanguage'] = '';
 $string['strftimedate'] = '%%d. %%B %%Y';
 $string['strftimedateshort'] = '%%d. %%B';
 $string['strftimedatetime'] = '%%d. %%B %%Y, %%H:%%M';
+$string['strftimedatetimeshort'] = '%%d.%%m.%%y, %%H:%%M';
 $string['strftimedaydate'] = '%%A, %%d. %%B %%Y';
 $string['strftimedaydatetime'] = '%%A, %%d. %%B %%Y, %%H:%%M';
 $string['strftimedayshort'] = '%%A, %%d. %%B';
@@ -24,4 +26,6 @@ $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Deutsch';
 $string['thislanguageint'] = 'German';
+$string['thousandssep'] = '.';
+
 ?>
