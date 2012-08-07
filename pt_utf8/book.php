6e66257c1999af225321a0f4f2ce12b639547dc9
Koen Roggemans <koen@roggemans.net>
2012-08-07 08:15:15 +0000

    book strings by Fabio Gunkel

---
+++ pt_utf8/book.php
@@ -0,0 +1,52 @@
+<?PHP // $Id$
+
+$string['modulename'] = 'Livro';
+$string['modulenameplural'] = 'Livros';
+
+$string['toc'] = 'Sumário';
+$string['tocwidth'] = 'Selecionar largura do Sumário para todos os Livros';
+$string['book_tocwidth'] = 'Largura do Sumário';
+$string['faq'] = 'FAQ dos Livros';
+
+$string['disableprinting'] = 'Desabilitar Impressão';
+$string['customtitles'] = 'Títulos Customizados';
+
+$string['editingchapter'] = 'Editar Capítulo';
+$string['chaptertitle'] = 'Título do Capítulo';
+$string['content'] = 'Conteúdo';
+$string['subchapter'] = 'Subcapítulo';
+
+$string['numbering'] = 'Numeração do capítulo';
+$string['numbering0'] = 'Nenhum';
+$string['numbering1'] = 'Numeros';
+$string['numbering2'] = 'Marcadores';
+$string['numbering3'] = 'Indentado';
+
+$string['chapterscount'] = 'Capítulos';
+
+$string['addafter'] = 'Adicionar novo Capítulo';
+$string['confchapterdelete'] = 'Você realmente deseja deletar esse Capítulo?';
+$string['confchapterdeleteall'] = 'Você realmente deseja deletar esse Capítulo e todos os seus Subcapítulos?';
+
+$string['generateimscp'] = 'Gerar arquivo ZIP';
+$string['printbook'] = 'Imprimir Livro Inteiro';
+$string['printchapter'] = 'Imprimir Esse Capítulo';
+$string['printdate'] = 'Data';
+$string['printedby'] = 'Impresso por:';
+$string['top'] = 'topo';
+
+$string['navprev'] = 'Anterior';
+$string['navnext'] = 'Próximo';
+$string['navexit'] = 'Sair do Livro';
+
+$string['importingchapters'] = 'Importar Capítulos para o Livro';
+$string['import'] = 'Importar';
+$string['doimport'] = 'Importar';
+$string['doexport'] = 'Exportar';
+$string['importing'] = 'Importando';
+$string['relinking'] = 'Relinkando';
+$string['importinfo'] = 'importar o arquivo ou diretório HTML selecionado <br /> Os Capítulos são organizados alfabeticamente usando os nomes dos arquivos<br /> Arquivos nomeados como \'sub_.\' são sempre importados como Subcapítulos.';
+$string['maindirectory'] = 'Diretório Principal';
+$string['fileordir'] = 'Arquivo ou diretório';
+
+?>
